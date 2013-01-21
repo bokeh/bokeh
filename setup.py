60195cfe440410922fa699d6d6840feefb836954
Peter Wang <pwang@continuum.io>
2013-01-21 15:43:24 -0600

    Improving argv handling and descriptions in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # needs to be tested
 import sys
-if sys.argv[1] == 'develop':
+if len(sys.argv)>1 and sys.argv[1] == 'develop':
+    # Only import setuptools if we have to
     import setuptools
+
 from distutils.core import setup
 import os
 import sys
@@ -22,6 +24,7 @@ for dirname, _, files in os.walk('bokeh/templates'):
     dirname = os.path.relpath(dirname, 'bokeh')
     for f in files:
         package_data_dirs.append(os.path.join(dirname, f))
+
 setup(
     name = 'bokeh',
     version = '.'.join([str(x) for x in __version__]),
@@ -30,9 +33,9 @@ setup(
                 'bokeh.server.test'],
     package_data = {'bokeh' : package_data_dirs},
     author = 'Continuum Analytics',
-    author_email = '',
-    url = '',
-    description = 'Bokeh',
+    author_email = 'info@continuum.io',
+    url = 'http://github.com/ContinuumIO/Bokeh',
+    description = 'Statistical and novel interactive HTML plots for Python',
     zip_safe=False,
     license = 'New BSD',
 )
