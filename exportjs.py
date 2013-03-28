f3e3c0df2ae9a46fbbe0fc7e4e3cfa3ca6ed8708
Hugo <humongo.shi@gmail.com>
2013-03-28 16:49:10 -0400

    changed  to store in s3

--- exportjs.py
+++ exportjs.py
@@ -3,28 +3,36 @@ import argparse
 import shutil
 from os.path import join
 import subprocess
-
-parser = argparse.ArgumentParser("export js to your local bokehjs-build repo")
-parser.add_argument("path", help="path to bokehjs-build repo")
+import json
+import boto
+    
+parser = argparse.ArgumentParser("export js to your s3")
+parser.add_argument("path", help="path to s3 keys")
 args = parser.parse_args()
+with open(args.path) as f:
+    keys = json.load(f)
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+conn = S3Connection(keys['AWS_ACCESS_KEY'], keys['AWS_SECRET_KEY'])
+bucket = conn.get_bucket('bokeh-builds')
 
 subprocess.check_call("hem build -d -s slug.json",
                  shell=True, cwd="bokeh/server")
-shutil.copy("bokeh/server/static/js/application.js", args.path)
+k = Key(bucket)
+k.key = "/application.js"
+k.set_contents_from_filename("bokeh/server/static/js/application.js")
+bucket.set_acl('public-read', k.key)
 
 # subprocess.check_call("hem build -s slug.json",
 #                  shell=True, cwd="bokeh/server")
-# shutil.copy("bokeh/server/static/js/application.js",
-#             join(args.path, "application.min.js"))
-            
 subprocess.check_call("hem build -d -s slug.notebook.json",
                  shell=True, cwd="bokeh/server")
-shutil.copy("bokeh/server/static/js/bokehnotebook.js", args.path)
+k = Key(bucket)
+k.key = "/bokehnotebook.js"
+k.set_contents_from_filename("bokeh/server/static/js/bokehnotebook.js")
+bucket.set_acl('public-read', k.key)
 
 # subprocess.check_call("hem build -s slug.notebook.json",
 #                  shell=True, cwd="bokeh/server")
-# shutil.copy("bokeh/server/static/js/bokehnotebook.js",
-#             join(args.path, "bokehnotebook.min.js"))
-
 
 
