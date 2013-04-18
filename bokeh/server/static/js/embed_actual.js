9bb2486876cbb6f69be46b6a0c678cd90003dc7c
Paddy Mullen <paddy@paddymullen.com>
2013-04-18 17:43:54 -0400

    now two plots display

--- bokeh/server/static/js/embed_actual.js
+++ bokeh/server/static/js/embed_actual.js
@@ -4,11 +4,12 @@ function addPlotWrap(settings){
     addPlot(settings.bokeh_modelid, settings.bokeh_modeltype, settings.element);
 };
 function addPlot(modelid, modeltype, element) {
+    console.log("addPlot");
     var view;
     base = require("./base")
     console.log(modelid, modeltype, element);
     base.load_models(window.Bokeh.models);
-    var model = base.Collections(modeltype).get(modelid)
+    var model = base.Collections(modeltype).get(modelid);
     window.model=model;
     var view = new model.default_view(
         {model : model}
