790aee6265e6fdb109a63dcbd4774d6a55be54ab
Hugo <humongo.shi@gmail.com>
2013-06-02 20:45:05 -0400

    migrated to flask blueprints

--- bokeh/server/app.py
+++ bokeh/server/app.py
@@ -2,7 +2,9 @@ import flask
 import logging
 from logging import Formatter
 
-app = flask.Flask('bokeh.server', static_url_path='/bokeh/static')
-
-app.NODE_INSTALLED = False
-#file_handler = logging.FileHandler(lConfig.settings['LOG_FILE'])
+app = flask.Blueprint('bokeh.server',
+                      'bokeh.server',
+                      static_url_path='/bokeh/static',
+                      template_folder='templates'
+                      )
+bokeh_app = app
