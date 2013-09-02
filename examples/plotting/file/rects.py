9c35b5816150be2229df05538c7496fc77ead280
Bryan Van de Ven <bryanv@continuum.io>
2013-09-02 13:25:12 -0500

    start examples reorganization: file, server, notebook examples separated

---
+++ examples/plotting/file/rects.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from bokeh.plotting import *
+
+N = 80
+
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+
+output_file("rects.html", title="Rectangles")
+
+# fixed width and height
+rects(x, y, 4, 2, color="#ff0000", tools="pan,zoom,save,resize")
+
+# variable width and height
+rects(
+    x, y, 0.01*np.exp(1+0.1*x), 0.2*abs(np.cos(x)),
+    fill="green", line_color="none", fill_alpha=0.6, tools="pan,zoom,resize"
+)
+
+# angle
+rects(x, y, 1, 8, color="#4444aa", angle=-np.pi/6, tools="pan,zoom,resize")
+
+# open a browser
+show()
---
+++ examples/plotting/server/rects.py
@@ -0,0 +1,26 @@
+
+# The plot server must be running
+# Go to http://localhost:5006/bokeh to view this plot
+
+import numpy as np
+from bokeh.plotting import *
+
+N = 80
+
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+
+output_server("rects.py example")
+
+# fixed width and height
+rects(x, y, 4, 2, color="#ff0000", tools="pan,zoom,save,resize")
+
+# variable width and height
+rects(
+    x, y, 0.01*np.exp(1+0.1*x), 0.2*abs(np.cos(x)),
+    fill="green", line_color="none", fill_alpha=0.6, tools="pan,zoom,resize"
+)
+
+# angle
+rects(x, y, 1, 8, color="#4444aa", angle=-np.pi/6, tools="pan,zoom,resize")
+
