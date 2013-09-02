85dee5a526bddac1e69e18bc85effc56ebae2ca4
Bryan Van de Ven <bryanv@continuum.io>
2013-09-02 14:57:14 -0500

    slider.py examples for server (file, notebook waiting on improvements)

--- examples/plotting/server/slider.py
+++ examples/plotting/server/slider.py
@@ -1,32 +1,40 @@
 # The plot server must be running
+# Go to http://localhost:5006/bokeh to view this plot
 
 import numpy as np
 from bokeh.plotting import *
-import bokeh.plotting as plotting
 from bokeh.objects import ColumnDataSource, DataSlider
+
 x = np.linspace(-7, 7, 100)
 y = np.sin(x)
 
-# Go to http://localhost:5006/bokeh to view this plot
-output_server("slider example")
-hold(True)
+output_server("slider.py example")
+
 source = ColumnDataSource()
 source.add(x, name='x')
 source.add(y, name='y')
 source.add(2*y, name='2y')
 source.add(3*y, name='3y')
+
+hold()
+
 scatter('x','y', source=source, tools="pan,zoom,resize")
 scatter('x','2y', source=source, tools="pan,zoom,resize")
 plot = scatter('x','3y', source=source, color="green", tools="pan,zoom,resize")
-slider1 = DataSlider(plot=plot, data_source=source, field='x')
-slider2 = DataSlider(plot=plot, data_source=source, field='y')
-plot.tools.append(slider1)
-plot.tools.append(slider2)
+
+slider_x = DataSlider(plot=plot, data_source=source, field='x')
+slider_y = DataSlider(plot=plot, data_source=source, field='y')
+
+plot.tools.append(slider_x)
+plot.tools.append(slider_y)
+
+# This ugly line will be fixed and made unnecessary in a future release
 plot._dirty = True
-sess = plotting._config["session"]
-sess.add(slider1)
-sess.add(slider2)
-plotting._config["session"].store_all()
+
+sess = session()
+sess.add(slider_x)
+sess.add(slider_y)
+sess.store_all()
 
 
 
