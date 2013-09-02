daa415371ad928565731dda14ec474630b89c6b3
Bryan Van de Ven <bryanv@continuum.io>
2013-09-02 13:49:17 -0500

    separate scatter.py examples for file, server, notebook

---
+++ examples/plotting/file/scatter.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+from bokeh.plotting import *
+
+N = 100
+
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+
+output_file("scatter.html", title="scatter.py example")
+
+scatter(x,y, color="#FF00FF", tools="pan,zoom,resize")
+scatter(x,y, color="red", tools="pan,zoom,resize")
+scatter(x,y, type="rect", color="green", tools="pan,zoom,resize")
+scatter(x,y, type="square", color="blue", tools="pan,zoom,resize")
+
+# open a browser
+show()
---
+++ examples/plotting/server/scatter.py
@@ -0,0 +1,20 @@
+# The plot server must be running
+# Go to http://localhost:5006/bokeh to view this plot
+
+import numpy as np
+from bokeh.plotting import *
+
+N = 100
+
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+
+output_server("scatter.py example")
+
+scatter(x,y, color="#FF00FF", tools="pan,zoom,resize")
+scatter(x,y, color="red", tools="pan,zoom,resize")
+scatter(x,y, type="rect", color="green", tools="pan,zoom,resize")
+scatter(x,y, type="square", color="blue", tools="pan,zoom,resize")
+
+
+
