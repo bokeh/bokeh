8e31a825405d014baa568a54080198ff884702ca
Bryan Van de Ven <bryanv@continuum.io>
2013-09-03 17:09:33 -0500

    remove dependency on continuumweb

--- bokeh/server/services.py
+++ bokeh/server/services.py
@@ -1,8 +1,65 @@
 import subprocess
 import os
 from os.path import join, dirname
-from continuumweb.launch_process import ManagedProcess
 import sys
+import json
+import signal
+
+class ManagedProcess(object):
+    def __init__(self, args, name, pidfilename,
+                 stdout=None, stdin=None, stderr=None,
+                 kill_old=True):
+        self.name = name
+        self.pidfilename = pidfilename
+        data = self.read_pidfile()
+        pid = data.get(name)
+        if pid and kill_old:
+            try:
+                os.kill(pid, signal.SIGINT)
+            except OSError:
+                #this is ok, just means process is not running
+                pass
+        elif pid and not kill_old:
+            raise Exception, "proces %s is running on PID %s" % (name, pid)
+        if stdout is None: stdout = subprocess.PIPE
+        if stderr is None: stderr = subprocess.PIPE
+        if stdin is None: stdin = subprocess.PIPE
+        self.proc = subprocess.Popen(args,
+                                     stdout=stdout,
+                                     stderr=stderr,
+                                     stdin=stdin)
+        self.add_to_pidfile()
+        self.closed = False
+
+    def read_pidfile(self):
+        if os.path.exists(self.pidfilename):
+            with open(self.pidfilename, "r") as f:
+                data = json.load(f)
+        else:
+            data = {}
+        return data
+
+    def add_to_pidfile(self):
+        data = self.read_pidfile()
+        data[self.name] = self.proc.pid
+        with open(self.pidfilename, "w+") as f:
+            json.dump(data, f)
+
+    def remove_from_pidfile(self):
+        data = self.read_pidfile()
+        if self.name in data:
+            del data[self.name]
+        with open(self.pidfilename, "w+") as f:
+            json.dump(data, f)
+
+
+    def close(self):
+        if not self.closed:
+            self.proc.kill()
+            self.proc.communicate()
+            self.remove_from_pidfile()
+
+
 
 def start_redis(pidfilename, port, data_dir, data_file='redis.db', save=True):
     base_config = os.path.join(os.path.dirname(__file__), 'redis.conf')
