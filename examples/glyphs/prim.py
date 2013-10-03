a919d244c258054f369e3d8af237223a0d97377d
Peter Wang <pwang@continuum.io>
2013-10-03 00:52:34 -0500

    adding helpful text at the end of prim.py example with plot server URL

--- examples/glyphs/prim.py
+++ examples/glyphs/prim.py
@@ -66,6 +66,7 @@ make_plot('rect', Rect(x="x", y="y", width=0.5, height=0.8, angle=-0.6))
 make_plot('text', Text(x="x", y="y", text="foo", angle=0.6))
 make_plot('wedge', Wedge(x="x", y="y", radius=0.5, start_angle=0.9, end_angle=3.2))
 
+print "\nPlease visit http://localhost:5006/bokeh to see the plots\n"
 
 
 
