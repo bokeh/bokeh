6fa810fa6b9d85781b20dbcd712e4d7dbc5313c6
Peter Wang <pwang@continuum.io>
2013-10-03 00:25:03 -0500

    Moving DataSpec and ColorSpec into properties.py, and merging functionality of MetaGlyph into MetaHasProps, and removing MetaGlyph
    The root purpose of this was so that I could put ColorSpecs into the mix-in classes of LineProps and FillProps.
    
    TextProps does not declare its color to be a ColorSpec, because in general, text properties are commonly used in places where color is not data-driven.  If data-varying color is desired for text, then explicitly redeclare the relevant text properties as ColorSpecs in the Glyph subclass itself.

--- bokeh/properties.py
+++ bokeh/properties.py
@@ -64,6 +64,342 @@ class Include(BaseProperty):
         super(Include, self).__init__()
 
 
+class DataSpec(BaseProperty):
+    """ Because the BokehJS glyphs support a fixed value or a named
+    field for most data fields, we capture that in this descriptor.
+    Fields can have a fixed value, or be a name that is looked up
+    on the datasource (usually as a column or record array field).
+    A default value can also be provided for when a particular row
+    in the datasource has a missing value.
+    Numerical data can also have units of screen or data space.
+
+    We mirror the JS convention in this Python descriptor.  For details,
+    see renderers/properties.coffee in BokehJS, and specifically the
+    select() function.
+
+    There are multiple ways to set a DataSpec, illustrated below with comments
+    and example code.
+
+    Setting DataSpecs
+    -----------------
+
+    class Foo(HasProps):
+        x = DataSpec("x", units="data")
+
+    f = Foo()
+    f.x = "fieldname"  # Use the datasource field named "fieldname"
+    f.x = 12           # A fixed value of 12
+    f.x = ("foo", 16)  # a field name, and a default value
+
+    # Can provide a dict with the fields explicitly named
+    f.width = {"name": "foo", "default": 16}
+    f.size = {"name": "foo", "units": "screen", "default": 16}
+
+    Reading DataSpecs
+    -----------------
+
+    In the cases when the dataspec is set to just a field name or a
+    fixed value, then those are returned.  If the user has overridden
+    the default value in the DataSpec with a new default value, or
+    if no values have been set, then the value of to_dict() is returned.
+
+    In all cases, to determine the full dict that will be used to
+    represent this dataspec, use the to_dict() method.
+
+    Implementation
+    --------------
+
+    The DataSpec instance is stored in the class dict, and acts as a
+    descriptor.  Thus, it is shared between all instances of the class.
+    Instance-specific data is stored in the instance dict, in a private
+    variable named _[attrname].  This stores the actual value that the
+    user last set (and does not exist if the user has not yet set the
+    value).
+
+    """
+
+    def __init__(self, field=None, units="data", default=None):
+        """
+        Parameters
+        ==========
+        **field** is the string name of a data column to look up.
+        **units** is either "data" or "screen"
+        **default** is the default value to use if a datapoint is
+        missing the field specified in **name**
+        """
+        # Don't use .name because the HasProps metaclass uses that to
+        # store the attribute name on this descriptor.
+        self.field = field
+        self.units = units
+        self.default = default
+
+    @classmethod
+    def autocreate(cls, name=None):
+        # In this case, use the name the user assigned this DataSpec to
+        # as the default field name.
+        d = cls(field=name)
+        return d
+
+    def __get__(self, obj, cls=None):
+        """ Try to implement a "natural" interface: if the user just set
+        simple values or field names, the getter just returns those.
+        However, if the user has also overridden the "units" or "default"
+        settings, then a dictionary is returned.
+        """
+        attrname = "_" + self.name
+        if hasattr(obj, attrname):
+            setval = getattr(obj, attrname)
+            if isinstance(setval, basestring) and self.default is None:
+                # A string representing the field
+                return setval
+            elif not isinstance(setval, dict):
+                # Typically a number presenting the fixed value
+                return setval
+            else:
+                return self.to_dict(obj)
+        else:
+            # If the user hasn't set anything, just return the field name
+            # if there are not defaults, or a dict with the field name
+            # and the default value.
+            if self.default is not None:
+                return {"field": self.field, "default": self.default}
+            else:
+                return self.field
+
+    def __set__(self, obj, arg):
+        attrname = "_" + self.name
+        if isinstance(arg, tuple):
+            field, default = arg
+            if not isinstance(field, basestring):
+                raise RuntimeError("String is required for field name when assigning tuple to a DataSpec")
+            setattr(obj, attrname, {"field": field, "default": default})
+        else:
+            setattr(obj, attrname, arg)
+
+    def __delete__(self, obj):
+        if hasattr(obj, self.name + "_dict"):
+            delattr(obj, self.name + "_dict")
+        BaseProperty.__delete__(self, obj)
+
+    def to_dict(self, obj):
+        # Build the complete dict
+        setval = getattr(obj, "_"+self.name, None)
+        if isinstance(setval, basestring):
+            d = {"field": setval, "units": self.units}
+            if self.default is not None:
+                d["default"] = self.default
+        elif isinstance(setval, dict):
+            d = {"units": self.units, "default": self.default}
+            d.update(setval)
+            if d["default"] is None:
+                del d["default"]
+            if "value" in d and "default" in d:
+                del d["default"]
+        elif setval is not None:
+            # a fixed value of some sort; no need to store the default value
+            d = {"value": setval, "units": self.units}
+        else:
+            # If the user never set a value
+            d = {"field": self.field, "units": self.units}
+            if self.default is not None:
+                d["default"] = self.default
+        return d
+
+    def __repr__(self):
+        return "DataSpec(field=%r, units=%r, default=%r)" % (
+            self.field, self.units, self.default)
+
+
+class ColorSpec(DataSpec):
+    """ Subclass of DataSpec for specifying colors.
+
+    Although this serves the same role as a DataSpec, its usage is somewhat
+    different because:
+      * Specifying a fixed value is much more common
+      * Strings can be both field identifiers or refer to one of the SVG
+        Named Colors (or be a hex value starting with "#")
+      * There are no units
+
+    For colors, because we support named colors and hex values prefaced
+    with a "#", when we are handed a string value, there is a little
+    interpretation: if the value is one of the 147 SVG named colors or
+    it starts with a "#", then it is interpreted as a value.  Otherwise,
+    it is treated as a field name.
+
+    If a 3-tuple is provided, then it is treated as an RGB (0..255).
+    If a 4-tuple is provided, then it is treated as an RGBa (0..255), with
+    alpha as a float between 0 and 1.  (This follows the HTML5 Canvas API.)
+
+    If a 2-tuple is provided, then it is treated as (value/fieldname, default).
+    This is the same as the behavior in the base class DataSpec.
+    Unlike DataSpec, ColorSpecs do not have a "units" property.
+
+    When reading out a ColorSpec, it returns a tuple, hex value, field name,
+    or a dict of (field, default).
+
+    There are two common use cases for ColorSpec: setting a constant value,
+    and indicating a field name to look for on the datasource:
+
+        class Bar(HasProps):
+            col = ColorSpec("green")
+            col2 = ColorSpec("colorfield")
+            col3 = ColorSpec("colorfield", default="aqua")
+
+    >>> b = Bar()
+    >>> b.col = "red"  # sets a fixed value of red
+    >>> b.col
+    "red"
+    >>> b.col = "myfield"  # Use the datasource field named "myfield"
+    >>> b.col
+    "myfield"
+    >>> b.col = {"name": "mycolor", "default": "#FF126D"}
+
+    For more examples, see tests/test_glyphs.py
+    """
+
+    NAMEDCOLORS = {'indigo', 'gold', 'firebrick', 'indianred', 'yellow',
+    'darkolivegreen', 'darkseagreen', 'darkslategrey', 'mediumvioletred',
+    'mediumorchid', 'chartreuse', 'mediumblue', 'black', 'springgreen',
+    'orange', 'lightsalmon', 'brown', 'turquoise', 'olivedrab', 'cyan',
+    'silver', 'skyblue', 'gray', 'darkturquoise', 'goldenrod', 'darkgreen',
+    'darkviolet', 'darkgray', 'lightpink', 'teal', 'darkmagenta',
+    'lightgoldenrodyellow', 'lavender', 'yellowgreen', 'thistle', 'violet',
+    'navy', 'dimgrey', 'orchid', 'blue', 'ghostwhite', 'honeydew',
+    'cornflowerblue', 'purple', 'darkkhaki', 'mediumpurple', 'cornsilk', 'red',
+    'bisque', 'slategray', 'darkcyan', 'khaki', 'wheat', 'deepskyblue',
+    'darkred', 'steelblue', 'aliceblue', 'lightslategrey', 'gainsboro',
+    'mediumturquoise', 'floralwhite', 'coral', 'aqua', 'burlywood',
+    'darksalmon', 'beige', 'azure', 'lightsteelblue', 'oldlace', 'greenyellow',
+    'royalblue', 'lightseagreen', 'mistyrose', 'sienna', 'lightcoral',
+    'orangered', 'navajowhite', 'lime', 'palegreen', 'lightcyan', 'seashell',
+    'mediumspringgreen', 'fuchsia', 'papayawhip', 'blanchedalmond', 'peru',
+    'aquamarine', 'white', 'darkslategray', 'ivory', 'darkgoldenrod',
+    'lawngreen', 'lightgreen', 'crimson', 'forestgreen', 'maroon', 'olive',
+    'mintcream', 'antiquewhite', 'dimgray', 'hotpink', 'moccasin', 'limegreen',
+    'saddlebrown', 'grey', 'darkslateblue', 'lightskyblue', 'deeppink',
+    'plum', 'lightgrey', 'dodgerblue', 'slateblue', 'sandybrown', 'magenta',
+    'tan', 'rosybrown', 'pink', 'lightblue', 'palevioletred', 'mediumseagreen',
+    'linen', 'darkorange', 'powderblue', 'seagreen', 'snow', 'mediumslateblue',
+    'midnightblue', 'paleturquoise', 'palegoldenrod', 'whitesmoke',
+    'darkorchid', 'salmon', 'lightslategray', 'lemonchiffon', 'chocolate',
+    'tomato', 'cadetblue', 'lightyellow', 'lavenderblush', 'darkblue',
+    'mediumaquamarine', 'green', 'blueviolet', 'peachpuff', 'darkgrey'}
+
+
+    def __init__(self, field_or_value=None, field=None, default=None, value=None):
+        """ ColorSpec(field_or_value=None, field=None, default=None, value=None)
+        """
+        # The fancy footwork below is so we auto-interpret the first positional
+        # parameter as either a field or a fixed value.  If either "field" or
+        # "value" are then supplied as keyword arguments, then those will
+        # override the inferred value from the positional argument.
+
+        self.field = field
+        self.default = default
+        self.value = value
+        if field_or_value is not None:
+            if self._isconst(field_or_value):
+                self.value = field_or_value
+            else:
+                self.field = field_or_value
+
+    def _isconst(self, arg):
+        """ Returns True if the argument is a literal color.  Check for a
+        well-formed hexadecimal color value.
+        """
+        return isinstance(arg, basestring) and \
+               ((len(arg) == 7 and arg[0] == "#") or arg in self.NAMEDCOLORS)
+
+    def _formattuple(self, colortuple):
+        if isinstance(colortuple, tuple):
+            if len(colortuple) == 3:
+                return "rgb%r" % (colortuple,)
+            else:
+                return "rgba%r" % (colortuple,)
+        else:
+            return colortuple
+
+    def __get__(self, obj, cls=None):
+        # One key difference in ColorSpec.__get__ from the base class is
+        # that we do not call self.to_dict() in any circumstance, because
+        # this could lead to formatting color tuples as "rgb(R,G,B)" instead
+        # of keeping them as tuples.
+        attrname = "_" + self.name
+        if hasattr(obj, attrname):
+            setval = getattr(obj, attrname)
+            if self._isconst(setval) or isinstance(setval, tuple):
+                # Fixed color value
+                return setval
+            elif isinstance(setval, basestring):
+                if self.default is None:
+                    # Field name
+                    return setval
+                else:
+                    return {"field": setval, "default": self.default}
+            else:
+                # setval should be a dict at this point
+                assert(isinstance(setval, dict))
+                return setval
+        else:
+            if self.value is not None:
+                return self.value
+            elif self.default is not None:
+                return {"field": self.field, "default": self.default}
+            else:
+                return self.field
+
+    def __set__(self, obj, arg):
+        attrname = "_" + self.name
+        if isinstance(arg, tuple):
+            if len(arg) == 2:
+                if not isinstance(arg[0], basestring):
+                    raise RuntimeError("String is required for field name when assigning 2-tuple to ColorSpec")
+                setattr(obj, attrname, {"field": arg[0], "default": arg[1]})
+            elif len(arg) in (3, 4):
+                # RGB or RGBa
+                setattr(obj, attrname, arg)
+            else:
+                raise RuntimeError("Invalid tuple being assigned to ColorSpec; must be length 2, 3, or 4.")
+        else:
+            setattr(obj, attrname, arg)
+
+    def to_dict(self, obj):
+        setval = getattr(obj, "_" + self.name, None)
+        if setval is not None:
+            if self._isconst(setval):
+                # Hexadecimal or named color
+                return {"value": setval}
+            elif isinstance(setval, tuple):
+                # RGB or RGBa
+                # TODO: Should we validate that alpha is between 0..1?
+                return {"value": self._formattuple(setval)}
+            elif isinstance(setval, basestring):
+                d = {"field": setval}
+                if self.default is not None:
+                    d["default"] = self._formattuple(self.default)
+                return d
+            elif isinstance(setval, dict):
+                # this is considerably simpler than the DataSpec case because
+                # there are no units involved, and we've handled all of the
+                # value cases above.
+                d = setval.copy()
+                if isinstance(d.get("default", None), tuple):
+                    d["default"] = self._formattuple(d["default"])
+                return d
+        else:
+            # If the user never set a value
+            if self.value is not None:
+                return {"value": self.value}
+            else:
+                d = {"field": self.field}
+                if self.default is not None:
+                    d["default"] = self._formattuple(self.default)
+                return d
+
+    def __repr__(self):
+        return "ColorSpec(field=%r, default=%r)" % (self.field, self.default)
+
+
 class MetaHasProps(type):
     def __new__(cls, class_name, bases, class_dict):
         names = set()
@@ -105,6 +441,7 @@ class MetaHasProps(type):
         for tmp in removes:
             del class_dict[tmp]
 
+        dataspecs = {}
         for name, prop in class_dict.iteritems():
             if isinstance(prop, BaseProperty):
                 prop.name = name
@@ -113,6 +450,9 @@ class MetaHasProps(type):
                 elif isinstance(prop, ContainerProp):
                     container_names.add(name)
                 names.add(name)
+                if isinstance(prop, DataSpec):
+                    dataspecs[name] = prop
+
             elif isinstance(prop, type) and issubclass(prop, BaseProperty):
                 # Support the user adding a property without using parens,
                 # i.e. using just the BaseProperty subclass instead of an
@@ -121,9 +461,16 @@ class MetaHasProps(type):
                 class_dict[name] = newprop
                 newprop.name = name
                 names.add(name)
+
+                # Process dataspecs
+                if issubclass(prop, DataSpec):
+                    dataspecs[name] = newprop
+
         class_dict["__properties__"] = names
         class_dict["__properties_with_refs__"] = names_with_refs
         class_dict["__container_props__"] = container_names
+        if dataspecs:
+            class_dict["_dataspecs"] = dataspecs
         return type.__new__(cls, class_name, bases, class_dict)
     
 def accumulate_from_subclasses(cls, propname):
@@ -394,16 +741,16 @@ class Percent(Float):
     semantically meaningful than Float(0..1) 
     """
 
-# These classes can be mixed-in to HasProps classes to get them the 
+# These classes can be mixed-in to HasProps classes to get them the
 # corresponding attributes
 class FillProps(HasProps):
     """ Mirrors the BokehJS properties.fill_properties class """
-    fill_color = Color("gray")
+    fill_color = ColorSpec("gray")
     fill_alpha = Percent(1.0)
 
 class LineProps(HasProps):
     """ Mirrors the BokehJS properties.line_properties class """
-    line_color = Color("black")
+    line_color = ColorSpec("black")
     line_width = Size(1)
     line_alpha = Percent(1.0)
     line_join = String("miter")
