a6653978d3603fb9cc9e68efff1f9103984fab4e
Peter Wang <pwang@continuum.io>
2013-10-04 06:57:11 -0500

    Squashed 'subtree/bokehjs/' changes from b153965..4143e1a
    4143e1a forgot to update axes modules in base.coffee
    1dbd703 Merge branch 'master' of github.com:ContinuumIO/bokehjs
    045e7ff removing unused d3 sources
    db25f29 python -> coffee boolean typo
    8b856c6 better widths for candlesticks
    5130e20 roughly working date time axis and updated candlestick example
    0cb0093 move linear axis to linear_axis.coffee and also factor out formatter to initializer in preparation for datetime axis
    
    git-subtree-dir: subtree/bokehjs
    git-subtree-split: 4143e1a34e7b71269e0b28deb9a0d0bf0816f689

---
+++ static/vendor/timezone/sq_AL.js
@@ -0,0 +1,62 @@
+module.exports = {
+  "name": "sq_AL",
+  "day": {
+    "abbrev": [
+      "Die ",
+      "Hën ",
+      "Mar ",
+      "Mër ",
+      "Enj ",
+      "Pre ",
+      "Sht "
+    ],
+    "full": [
+      "e diel ",
+      "e hënë ",
+      "e martë ",
+      "e mërkurë ",
+      "e enjte ",
+      "e premte ",
+      "e shtunë "
+    ]
+  },
+  "month": {
+    "abbrev": [
+      "Jan",
+      "Shk",
+      "Mar",
+      "Pri",
+      "Maj",
+      "Qer",
+      "Kor",
+      "Gsh",
+      "Sht",
+      "Tet",
+      "Nën",
+      "Dhj"
+    ],
+    "full": [
+      "janar",
+      "shkurt",
+      "mars",
+      "prill",
+      "maj",
+      "qershor",
+      "korrik",
+      "gusht",
+      "shtator",
+      "tetor",
+      "nëntor",
+      "dhjetor"
+    ]
+  },
+  "meridiem": [
+    "PD",
+    "MD"
+  ],
+  "date": "%Y-%b-%d",
+  "time24": "%I.%M.%S. %Z",
+  "dateTime": "%Y-%b-%d %I.%M.%S.%p %Z",
+  "time12": "%I.%M.%S.%p %Z",
+  "full": "%a %b %e %H:%M:%S %Z %Y"
+}
