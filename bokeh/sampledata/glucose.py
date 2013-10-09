e5680ea999de55f5dfd9b36c95e3ac22ee4adeaa
Bryan Van de Ven <bryanv@continuum.io>
2013-10-09 09:48:47 -0400

    add glucose.py module for glucose monitor sample data

---
+++ bokeh/sampledata/glucose.py
@@ -0,0 +1,19 @@
+
+from os.path import join
+
+try:
+    import pandas as pd
+except ImportError as e:
+    raise RuntimeError("glucose data requires pandas (http://pandas.pydata.org) to be installed")
+
+from . import _data_dir
+
+data_dir = _data_dir()
+
+data = pd.read_csv(
+    join(data_dir, 'CGM.csv'),
+    sep=',',
+    parse_dates=[1],
+    index_col=1
+)
+
