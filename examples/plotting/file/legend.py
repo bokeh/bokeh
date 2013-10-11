07673aa90ac2535652a76ff2b09510a50c217447
Bryan Van de Ven <bryanv@continuum.io>
2013-10-11 13:45:03 -0400

    fixed #88

--- examples/plotting/file/legend.py
+++ examples/plotting/file/legend.py
@@ -11,15 +11,19 @@ output_file("legend.html", title="legend.py example")
 
 hold()
 
-scatter(x,y, tools="pan,zoom,resize", legend="sin(x)")
-scatter(x,2*y, tools="pan,zoom,resize", legend="2*sin(x)")
-scatter(x,3*y, color="green", tools="pan,zoom,resize", legend="3*sin(x)")
+scatter(x, y, tools="pan,zoom,resize", legend="sin(x)")
+scatter(x, 2*y, tools="pan,zoom,resize", legend="2*sin(x)")
+scatter(x, 3*y, color="green", tools="pan,zoom,resize", legend="3*sin(x)")
 
 figure()
 
-plot(x,y, points=True, radius=2, tools="pan,zoom,resize,select", legend="sin(x)")
-plot(x,2*y, points=False, line_dash="4 4", line_width=2, legend="2*sin(x)")
-plot(x,3*y, points=True, fill_color=None, line_color="green", radius=4, legend="3*sin(x)")
+scatter(x, y, radius=2, tools="pan,zoom,resize,select", legend="sin(x)")
+line(x, y, radius=2, tools="pan,zoom,resize,select", legend="sin(x)")
+
+line(x, 2*y, line_dash="4 4", line_color="orange", line_width=2, legend="2*sin(x)")
+
+scatter(x, 3*y, fill_color=None, line_color="green", radius=4, legend="3*sin(x)")
+line(x, 3*y, fill_color=None, line_color="green", radius=4, legend="3*sin(x)")
 
 # open a browser
 show()
