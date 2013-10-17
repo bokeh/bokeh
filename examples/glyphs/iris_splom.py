baad0a3c7718c1b757c252cbbc07e2a13caf5ad5
Bryan Van de Ven <bryanv@continuum.io>
2013-10-17 07:05:25 -0500

    improve styling of iris splom example

--- examples/glyphs/iris_splom.py
+++ examples/glyphs/iris_splom.py
@@ -1,11 +1,12 @@
 
 import os
+from math import pi
 
 from bokeh.sampledata.iris import flowers
 from bokeh.objects import (
     ColumnDataSource, GlyphRenderer, Grid, GridPlot, LinearAxis, Plot, DataRange1d, Range1d
 )
-from bokeh.glyphs import Circle
+from bokeh.glyphs import Circle, Text
 from bokeh import session
 
 colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
@@ -26,14 +27,18 @@ source = ColumnDataSource(
 #ydr = DataRange1d(sources=[source.columns(["petal_length", "petal_width", "sepal_width", "sepal_length"])])
 xdr = Range1d(start=-1, end=9)
 ydr = Range1d(start=-1, end=9)
-print xdr.start
 
-def make_plot(xname, yname):
+def make_plot(xname, yname, xax=False, yax=False, text=None):
     plot = Plot(
-        x_range=xdr, y_range=ydr, data_sources=[source],
-        width=250, height=250, border_fill='white', title="")
-    xaxis = LinearAxis(plot=plot, dimension=0, location="bottom", axis_line_alpha=0)
-    yaxis = LinearAxis(plot=plot, dimension=1, location="left", axis_line_alpha=0)
+        x_range=xdr, y_range=ydr, data_sources=[source], background_fill="#ffeedd",
+        width=250, height=250, border_fill='white', title="", border_symmetry="", min_border=2)
+    objs = []
+    if xax:
+        xaxis = LinearAxis(plot=plot, dimension=0, location="bottom")
+        objs.append(xaxis)
+    if yax:
+        yaxis = LinearAxis(plot=plot, dimension=1, location="left")
+        objs.append(yaxis)
     xgrid = Grid(plot=plot, dimension=0)
     ygrid = Grid(plot=plot, dimension=1)
     circle = Circle(x=xname, y=yname, fill_color="color", fill_alpha=0.2, radius=2, line_color="color")
@@ -44,16 +49,31 @@ def make_plot(xname, yname):
         glyph = circle,
     )
     plot.renderers.append(circle_renderer)
-    return plot, (circle_renderer, xaxis, yaxis, xgrid, ygrid)
+    if text:
+        text = " ".join(text.split('_'))
+        text = Text(x=4, y=4, text=text, angle=pi/4, text_font_style="bold", text_baseline="top",
+                    text_color="#ffaaaa", text_alpha=0.2, text_align="center", text_font_size="28pt")
+        text_renderer = GlyphRenderer(
+            data_source=source,
+            xdata_range = xdr,
+            ydata_range = ydr,
+            glyph = text,
+        )
+        plot.renderers.append(text_renderer)
+        objs.append(text_renderer)
+    return plot, objs + [circle_renderer, xgrid, ygrid]
 
 sess = session.HTMLFileSession("iris_splom.html")
 attrs = ["petal_length", "petal_width", "sepal_width", "sepal_length"]
 
 plots = []
-for attr1 in attrs:
+for y in attrs:
     row = []
-    for attr2 in attrs:
-        plot, objs = make_plot(attr1, attr2)
+    for x in attrs:
+        xax = (y == attrs[-1])
+        yax = (x == attrs[0])
+        text = x if (x==y) else None
+        plot, objs = make_plot(y, x, xax, yax, text)
         sess.add(plot, *objs)
         row.append(plot)
     plots.append(row)
