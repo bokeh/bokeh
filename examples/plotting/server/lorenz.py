fee69b5e49b2b74bae45953a7b43eeab0930c7e6
Bryan Van de Ven <bryanv@continuum.io>
2013-10-18 23:51:41 -0500

    actually add files for prev commit

---
+++ examples/plotting/server/lorenz.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+from scipy.integrate import odeint
+from bokeh.plotting import *
+
+
+sigma = 10
+rho = 28
+beta = 8.0/3
+theta = 3 * np.pi / 4
+
+def lorenz(xyz, t):
+    x, y, z = xyz
+    x_dot = sigma * (y - x)
+    y_dot = x * rho - x * z - y
+    z_dot = x * y - beta* z
+    return [x_dot, y_dot, z_dot]
+
+initial = (-10, -7, 35)
+t = np.arange(0, 100, 0.001)
+
+solution = odeint(lorenz, initial, t)
+
+x = solution[:, 0]
+y = solution[:, 1]
+z = solution[:, 2]
+xprime = np.cos(theta) * x - np.sin(theta) * y
+
+colors = ["#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08306B",]
+
+output_server("lorenz.py example")
+
+multi_line(np.array_split(xprime, 7), np.array_split(z, 7),
+           line_color=colors, line_alpha=0.8, line_width=1.5,
+           tools="pan,zoom,resize", title="lorenz example")
+
+show()  # open a browser
