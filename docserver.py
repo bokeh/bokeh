f4bdf4117fe93c17753fd3f5dad38c1dd456e309
Paddy Mullen <paddy@paddymullen.com>
2013-10-21 15:35:43 -0400

    actual images in the plot gallery

--- docserver.py
+++ docserver.py
@@ -11,12 +11,14 @@ symlink.
 
 """
 
+
 @app.route('/static/<path:filename>')
 def bokeh_static(filename):
     static_dir = os.path.join(_basedir,"bokeh/server/static/")
     print filename
     print static_dir
     return flask.send_from_directory(static_dir, filename)
+
 
 @app.route('/<path:filename>')
 def send_pic(filename):
