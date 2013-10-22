5ff624539245b74676bb56adfba81e14829ed86b
Peter Wang <pwang@continuum.io>
2013-10-21 23:59:38 -0500

    Fixing typo in change to use favicon.ico

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -116,7 +116,7 @@ html_logo = "_static/bokeh-transparent.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "bokeh-white-32.ico"
+html_favicon = "bokeh_white_32.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
