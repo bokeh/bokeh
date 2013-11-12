2610816ab654b5486419576805b97d6e23279734
Hugo <humongo.shi@gmail.com>
2013-11-11 20:18:48 -0500

    removing bokeh subtree

--- subtree/bokehjs/demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-hem server -d -s slug.all.json &
-echo
-echo Please open a web browser to http://localhost:5000
-echo
-python demoserver.py debug
-
--- subtree/bokehjs/demo/spectrogram/demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hem server -d &
-echo
-echo     Point your web browser to http://localhost:5000
-echo     then make noises and whistle into your microphone
-echo
-python soundserver.py 2> /dev/null
-
