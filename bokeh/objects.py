22f83b7101d25b1d0692e309e0b5b4b3039fbfb3
Bryan Van de Ven <bryanv@continuum.io>
2013-11-15 13:06:08 -0600

    expose plot outline line properties

--- bokeh/objects.py
+++ bokeh/objects.py
@@ -564,6 +564,8 @@ class Plot(PlotObject):
     y_range = Instance(DataRange1d, has_ref=True)
     png = String('')
     title = String('')
+    outline_props = Include(LineProps, prefix="outline")
+
     # We shouldn't need to create mappers manually on the Python side
     #xmapper = Instance(LinearMapper)
     #ymapper = Instance(LinearMapper)
