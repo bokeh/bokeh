73950134116479d5ff96501f102a01c9faba652a
Paddy Mullen <paddy@paddymullen.com>
2013-11-18 14:02:45 -0600

    unified template for embed_direct and the server embed.js, the server doesn't yet call the embed_direct template

--- bokeh/templates/embed_direct.js
+++ bokeh/templates/embed_direct.js
@@ -1,66 +1,44 @@
 console.log("embed.js");
 
 (function(global) {
-
     var host = "{{host}}";
     var bokehUrl = host + 'js/bokeh.js';
-
-
-    var all_models = {{ all_models }};
+    var all_models = {{ all_models|default('{}') }};
     var modeltype = "{{ modeltype }}";
     var elementid = "{{ elementid }}";
     var plotID = "{{ plotid }}";
     var dd = {};
     dd[plotID] = all_models;
 
-
+    var bokehUrl = 'http://' + host +'/static/js/bokeh.js';
     function addEvent(el, eventName, func){
         if(el.attachEvent){
             return el.attachEvent('on' + eventName, func);}
         else {
             el.addEventListener(eventName, func, false);}}
-    
-    var bokeh_defined = !(typeof(_embed_bokeh_inject_application) == "undefined");
-    
-    var script_injected = bokeh_defined && _embed_bokeh_inject_application;
-    /*
-    console.log(
-        "plotID", plotID,
-        "bokeh_defined", bokeh_defined, 
-        "script_injected", script_injected,
-        "typeof rrequire", typeof rrequire);
-    */
-    if(typeof rrequire == "function"){
+    var script_injected = !(typeof(_embed_bokeh_inject_application) == "undefined") && _embed_bokeh_inject_application;
+    if(typeof Bokeh == "object"){
         // application.js is already loaded
-        console.log("application.js is already loaded, going straight to plotting");
-        embed_core = rrequire("./embed_core");
-        embed_core.search_and_plot(dd);
-    }
-    else {
+        console.log("bokeh.js is already loaded, going straight to plotting");
+        setTimeout(function () {
+            embed_core = Bokeh.embed_core;
+            embed_core.search_and_plot(dd);}, 10);}
+
+    else if(!script_injected){
         // application.js isn't loaded and it hasn't been scheduled to be injected
-        if(!script_injected){ 
-            var s = document.createElement('script');
-            s.async = true; s.src = bokehUrl; s.id="embed.js"}
-        else {
-            // in this case, the script block for application.js has been
-            // injected, but it hasn't yet loaded.
-            var s = document.getElementById('embed.js');
-        }
+        var s = document.createElement('script');
+        s.async = true; s.src = bokehUrl;
 
         _embed_bokeh_inject_application = true;
         addEvent(
             s,'load', 
             function() {
-                console.log("application.js loaded callback");
-                embed_core = rrequire("./embed_core");
-                console.log("embed_core loaded")
+                console.log("Bokeh.js loaded callback");
+                embed_core = Bokeh.embed_core;
+                console.log("embed_core loaded");
                 embed_core.search_and_plot(dd);
                 embed_core.injectCss(host);
-                console.log("search_and_plot called");
-            });
-        document.body.appendChild(s);        
-    }
+                console.log("search_and_plot called");});
+        document.body.appendChild(s);}
 
-    window._embed_bokeh = true;
-}(this));
-
+    window._embed_bokeh = true;}(this));
