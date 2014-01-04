2bf0ef71a2bb0adeded6faaf8a08cc421d375ed3
Mateusz Paprocki <mattpap@gmail.com>
2014-01-04 01:11:35 +0100

    Don't modify Bokeh's sample datasets in examples

--- examples/glyphs/choropleth.py
+++ examples/glyphs/choropleth.py
@@ -9,24 +9,28 @@ from bokeh.objects import (
 from bokeh.glyphs import Patches
 from bokeh import session
 
-del us_states.data['HI']
-del us_states.data['AK']
+us_states = us_states.data.copy()
+us_counties = us_counties.data
+unemployment = unemployment.data
+
+del us_states['HI']
+del us_states['AK']
 
 state_source = ColumnDataSource(
     data=dict(
-        state_xs=[us_states.data[code]['lons'] for code in us_states.data],
-        state_ys=[us_states.data[code]['lats'] for code in us_states.data],
+        state_xs=[us_states[code]['lons'] for code in us_states],
+        state_ys=[us_states[code]['lats'] for code in us_states],
     )
 )
 
 colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
 
 county_colors = []
-for county_id in us_counties.data:
-    if us_counties.data[county_id]['state'] in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']:
+for county_id in us_counties:
+    if us_counties[county_id]['state'] in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']:
         continue
     try:
-        rate = unemployment.data[county_id]
+        rate = unemployment[county_id]
         idx = min(int(rate/2), 5)
         county_colors.append(colors[idx])
     except KeyError:
@@ -34,8 +38,8 @@ for county_id in us_counties.data:
 
 county_source = ColumnDataSource(
     data=dict(
-        county_xs=[us_counties.data[code]['lons'] for code in us_counties.data if us_counties.data[code]['state'] not in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']],
-        county_ys=[us_counties.data[code]['lats'] for code in us_counties.data if us_counties.data[code]['state'] not in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']],
+        county_xs=[us_counties[code]['lons'] for code in us_counties if us_counties[code]['state'] not in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']],
+        county_ys=[us_counties[code]['lats'] for code in us_counties if us_counties[code]['state'] not in ['ak', 'hi', 'pr', 'gu', 'vi', 'mp', 'as']],
         county_colors=county_colors
     )
 )
