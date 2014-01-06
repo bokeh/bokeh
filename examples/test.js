bd6aa0f0f3d6681a77225da08e9f27cb3f035205
Mateusz Paprocki <mattpap@gmail.com>
2014-01-06 13:44:29 +0100

    Collect resource errors in tested examples

--- examples/test.js
+++ examples/test.js
@@ -6,6 +6,8 @@ var url = system.args[1];
 var png = system.args[2];
 
 var errors = [];
+var messages = [];
+var resources = [];
 
 page.onError = function(msg, trace) {
     errors.push({
@@ -14,6 +16,25 @@ page.onError = function(msg, trace) {
             return { file: item.file, line: item.line };
         }),
     });
+};
+
+page.onConsoleMessage = function(msg, line, source) {
+    messages.push({
+        msg: msg,
+        line: line,
+        source: source,
+    });
+};
+
+page.onResourceReceived = function(response) {
+    if (response.stage === 'end') {
+        var status = response.status;
+
+        // XXX: status is null for file:// protocol
+        if (status && status !== 200) {
+            resources.push(response);
+        }
+    }
 };
 
 // TODO: fit viewport's size to content
@@ -33,6 +54,8 @@ page.open(url, function(status) {
         console.log(JSON.stringify({
             status: status,
             errors: errors,
+            messages: messages,
+            resources: resources,
         }));
 
         phantom.exit();
