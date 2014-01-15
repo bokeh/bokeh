7ce03c7e203068a3ec667bdfeece642745c53eb6
Mateusz Paprocki <mattpap@gmail.com>
2014-01-15 20:12:47 +0100

    Don't delay but render view's contents to an existing DOM element

--- bokeh/templates/plots.js
+++ bokeh/templates/plots.js
@@ -5,9 +5,4 @@ var elementid = "{{ elementid }}";
 console.log(modelid, modeltype, elementid);
 Bokeh.load_models(all_models);
 var model = Bokeh.Collections(modeltype).get(modelid);
-window.model = model;
-var view = new model.default_view({model: model});
-window.view = view;
-_.delay(function() {
-    $('#{{ elementid }}').append(view.$el);
-}, 1000);
+var view = new model.default_view({model: model, el: '#{{ elementid }}'});
