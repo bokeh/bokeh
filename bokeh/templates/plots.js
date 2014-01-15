8d662e33c2b0e50a7d0ed7f95c16f4942539600b
Mateusz Paprocki <mattpap@gmail.com>
2014-01-15 19:56:22 +0100

    Cleanup base.html, plots.html and plots.js templates
    This removes unnecessary whitespace using {%- and -%} Jinja
    tags and fixes JavaScript syntax (add semicolons, etc.).

--- bokeh/templates/plots.js
+++ bokeh/templates/plots.js
@@ -2,15 +2,12 @@ var all_models = {{ all_models }};
 var modelid = "{{ modelid }}";
 var modeltype = "{{ modeltype }}";
 var elementid = "{{ elementid }}";
-var view;
 console.log(modelid, modeltype, elementid);
 Bokeh.load_models(all_models);
-var model = Bokeh.Collections(modeltype).get(modelid)
-window.model=model;
-var view = new model.default_view(
-    {model : model}
-);
+var model = Bokeh.Collections(modeltype).get(modelid);
+window.model = model;
+var view = new model.default_view({model: model});
 window.view = view;
-_.delay(function(){
-    $('#{{ elementid }}').append(view.$el)
+_.delay(function() {
+    $('#{{ elementid }}').append(view.$el);
 }, 1000);
