51217ea9d1d73df0afaa923ee26f7698c707bcdd
hugo <hugo>
2014-01-31 15:21:42 -0500

    updated stocks example to use new datetime axis

--- examples/plotting/cloud/stocks.py
+++ examples/plotting/cloud/stocks.py
@@ -8,20 +8,20 @@ output_cloud("stocks")
 
 hold()
 
-line(np.array(AAPL['date'], dtype=np.datetime64).astype('int'), AAPL['adj_close'],
+line(np.array(AAPL['date'], dtype=np.datetime64), AAPL['adj_close'],
      x_axis_type = "datetime",
      color='#A6CEE3', tools="pan,wheel_zoom,resize",
      legend='AAPL')
-line(np.array(FB['date'], dtype=np.datetime64).astype('int'), FB['adj_close'],
+line(np.array(FB['date'], dtype=np.datetime64), FB['adj_close'],
      color='#1F78B4', tools="pan,wheel_zoom,resize",
      legend='FB')
-line(np.array(GOOG['date'], dtype=np.datetime64).astype('int'), GOOG['adj_close'],
-      olor='#B2DF8A', tools="pan,wheel_zoom,resize",
+line(np.array(GOOG['date'], dtype=np.datetime64), GOOG['adj_close'],
+     color='#B2DF8A', tools="pan,wheel_zoom,resize",
      legend='GOOG')
-line(np.array(IBM['date'], dtype=np.datetime64).astype('int'), IBM['adj_close'],
+line(np.array(IBM['date'], dtype=np.datetime64), IBM['adj_close'],
      color='#33A02C', tools="pan,wheel_zoom,resize",
      legend='IBM')
-line(np.array(MSFT['date'], dtype=np.datetime64).astype('int'), MSFT['adj_close'],
+line(np.array(MSFT['date'], dtype=np.datetime64), MSFT['adj_close'],
      color='#FB9A99', tools="pan,wheel_zoom,resize",
      legend='MSFT')
 
@@ -31,7 +31,7 @@ grid().grid_line_alpha=0.3
 figure()
 
 aapl = np.array(AAPL['adj_close'])
-aapl_dates = np.array(AAPL['date'], dtype=np.datetime64).astype('int')
+aapl_dates = np.array(AAPL['date'], dtype=np.datetime64)
 
 window_size = 30
 window = np.ones(window_size)/float(window_size)
@@ -41,6 +41,7 @@ scatter(aapl_dates, aapl,
         x_axis_type = "datetime",
         color='#A6CEE3', radius=1, tools="pan,wheel_zoom,resize", legend='close')
 line(aapl_dates, aapl_avg,
+     x_axis_type = "datetime",
      color='red', tools="pan,wheel_zoom,resize", legend='avg')
 
 curplot().title = "AAPL One-Month Average"
