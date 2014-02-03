7c81570053ea579ea299c9216e06ca4837e5deb4
Bryan Van de Ven <bryanv@continuum.io>
2014-02-03 11:16:26 -0600

    need to set bounds explicitly on image plots for now

--- examples/plotting/file/image.py
+++ examples/plotting/file/image.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from bokeh.plotting import *
+from bokeh.objects import Range1d
 
 N = 1000
 
@@ -13,6 +14,7 @@ output_file("image.html", title="scatter.py example")
 
 image(
     image=[d], x=[0], y=[0], dw=[10], dh=[10], palette=["Spectral-11"],
+    x_range = Range1d(start=0, end=10), y_range = Range1d(start=0, end=10),
     tools="pan,wheel_zoom,resize", name="image_example")
 
 show()  # open a browser
