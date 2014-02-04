038dd41ddc98ec7297cf417e3b93a320d52134eb
Mateusz Paprocki <mattpap@gmail.com>
2014-02-04 01:19:17 +0100

    Add a note about the location of gevent.monkey.patch_all() call

--- bokeh/__init__.py
+++ bokeh/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import, print_function
 
+# NOTE: Execute patch_all() before everything else, especially before
+# importing threading module. Otherwise, annoying KeyError exception
+# will be thrown. gevent is optional, so don't fail if not installed.
 try:
     import gevent.monkey
 except ImportError:
