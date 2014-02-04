d055290ab5a607f635b0741884d25c33cf399bbd
hugo <hugo>
2014-02-04 15:34:12 -0500

    re-adding Server and Cloud to top level namespace

--- bokeh/__init__.py
+++ bokeh/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, print_function
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
 from . import sampledata
+from .serverconfig import Server, Cloud
 
 def print_versions():
     """Print all the versions of software that Bokeh relies on."""
