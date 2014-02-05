8ae6fa8bda9c815f894586a53aaa28d12f8c6967
hugo <hugo>
2014-02-05 14:11:29 -0500

    added some remote data test sources

---
+++ scripts/make_remote_data.py
@@ -0,0 +1,25 @@
+from arraymanagement.client import ArrayClient
+from os.path import abspath
+from bokeh.sampledata import stocks
+import pandas as pd
+c = ArrayClient(abspath("../remotedata"), 
+                configname="bokeh.server.hdf5_backend_config")
+
+aapl = pd.DataFrame(stocks.AAPL)
+fb = pd.DataFrame(stocks.FB)
+goog = pd.DataFrame(stocks.GOOG)
+msft = pd.DataFrame(stocks.MSFT)
+aapl['date'] = aapl['date'].astype('datetime64[ns]')
+fb['date'] = fb['date'].astype('datetime64[ns]')
+goog['date'] = goog['date'].astype('datetime64[ns]')
+msft['date'] = msft['date'].astype('datetime64[ns]')
+c['defaultuser'].put('AAPL', aapl)
+c['defaultuser'].put('MSFT', msft)
+c['defaultuser'].put('GOOG', goog)
+c['defaultuser'].put('FB', fb)
+
+print c['/defaultuser/AAPL'].select()
+print c['/defaultuser/MSFT'].select()
+print c['/defaultuser/GOOG'].select()
+print c['/defaultuser/FB'].select()
+
