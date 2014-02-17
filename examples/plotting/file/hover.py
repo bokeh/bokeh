a39d6112ea438e73a130216406e2fb1923f4da7c
Bryan Van de Ven <bryanv@continuum.io>
2014-02-16 18:08:40 -0600

    update hover python example

--- examples/plotting/file/hover.py
+++ examples/plotting/file/hover.py
@@ -4,16 +4,17 @@ from six.moves import zip
 from bokeh.plotting import *
 from bokeh.objects import HoverTool
 
-N = 500
 
-x = np.random.random(size=N) * 100
-y = np.random.random(size=N) * 100
-size = np.random.random(size=N) * 10 +  10
+xx, yy = np.meshgrid(xrange(0,101,4), xrange(0,101,4))
+
+x = xx.flatten()
+y = yy.flatten()
+radii = np.random.random(size=len(x))*0.4 + 1.7
 colors = ["#%02x%02x%02x" % (r, g, 150) for r, g in zip(np.floor(50+2*x), np.floor(30+2*y))]
 
 output_file("hover.html", title="hover.py example")
 
-square(x,y, size=size, radius_units="data",
+circle(x, y, radius=radii, radius_units="data",
        fill_color=colors, fill_alpha=0.6,
        line_color=None, Name="color_scatter_example",
        tools="pan,wheel_zoom,box_zoom,reset,previewsave,hover")
@@ -22,7 +23,7 @@ hover = [t for t in curplot().tools if isinstance(t, HoverTool)][0]
 hover.tooltips.update({
     "index": "$index",
     "fill_color": "$color[hex,swatch]:fill_color",
-    "size": "@size",
+    "radius": "@radius",
     "data (x, y)": "(@x, @y)",
     "cursor (x, y)": "($x, $y)",
     "canvas (x, y)": "($sx, $sy)",
