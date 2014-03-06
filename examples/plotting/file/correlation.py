0457d76b700cddec3ae552639c03f0c33209ad9b
Bryan Van de Ven <bryanv@continuum.io>
2014-03-06 16:55:30 -0500

    correlation

--- examples/plotting/file/correlation.py
+++ examples/plotting/file/correlation.py
@@ -6,11 +6,6 @@ from bokeh.sampledata.stocks import AAPL, FB, GOOG, IBM, MSFT
 from bokeh.plotting import *
 from bokeh.objects import GridPlot
 
-
-output_file("correlation.html", title="correlation.py example")
-
-hold()
-
 num_points = 300
 
 now = time.time()
@@ -19,20 +14,23 @@ dates = linspace(now, now + num_points*dt, num_points)
 acme = cumprod(random.lognormal(0.0, 0.04, size=num_points))
 choam = cumprod(random.lognormal(0.0, 0.04, size=num_points))
 
-scatter(
-    acme, choam, color='#A6CEE3', tools="pan,wheel_zoom,box_zoom,reset,previewsave", legend='close',
-)
+output_file("correlation.html", title="correlation.py example")
 
-curplot().title = "ACME / CHOAM Correlations"
-grid().grid_line_alpha=0.3
+figure(x_axis_type = "datetime", tools="pan,wheel_zoom,box_zoom,reset,previewsave")
 
-figure()
-line(dates, acme,
-     x_axis_type = "datetime",
-     color='#1F78B4', tools="pan,wheel_zoom,box_zoom,reset,previewsave", legend='ACME', name="correlation")
+hold()
+
+line(dates, acme, color='#1F78B4', legend='ACME')
 line(dates, choam, color='#FB9A99', legend='CHOAM')
 
 curplot().title = "Stock Returns"
+grid().grid_line_alpha=0.3
+
+figure(tools="pan,wheel_zoom,box_zoom,reset,previewsave")
+
+scatter(acme, choam, color='#A6CEE3', legend='close')
+
+curplot().title = "ACME / CHOAM Correlations"
 grid().grid_line_alpha=0.3
 
 show()  # open a browser
