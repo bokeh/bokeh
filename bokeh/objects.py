3eec5d08d1e8230b738ee7708e607983fb78052e
Mateusz Paprocki <mattpap@gmail.com>
2014-03-19 16:42:37 +0100

    Make Legend and DataSlider inherit from Renderer

--- bokeh/objects.py
+++ bokeh/objects.py
@@ -360,7 +360,7 @@ class DataRangeBoxSelectTool(Tool):
     xselect = List(Instance(Range), has_ref=True)
     yselect = List(Instance(Range), has_ref=True)
 
-class Legend(PlotObject):
+class Legend(Renderer):
     plot = Instance(Plot, has_ref=True)
     orientation = Enum(Orientation)
     border = Include(LineProps, prefix="border")
@@ -376,7 +376,7 @@ class Legend(PlotObject):
     legend_spacing = Int(3)
     legends = Dict()
 
-class DataSlider(PlotObject):
+class DataSlider(Renderer):
     plot = Instance(Plot, has_ref=True)
     data_source = Instance(DataSource, has_ref=True)
     field = String()
