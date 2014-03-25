aed5ee87b799a5d56f6ece88ac4369a10802c2b6
Mateusz Paprocki <mattpap@gmail.com>
2014-03-25 14:50:37 +0100

    Move bokeh-server's code to bokeh/server/__init__.py

--- bokeh/server/__init__.py
+++ bokeh/server/__init__.py
@@ -7,3 +7,96 @@ except ImportError:
     pass
 else:
     gevent.monkey.patch_all()
+
+import argparse, sys
+import logging
+
+DEFAULT_BACKEND = 'redis'
+if 'win32' in sys.platform:
+    DEFAULT_BACKEND = 'memory'
+
+def build_parser():
+    parser = argparse.ArgumentParser(description="Start the Bokeh plot server")
+    parser.add_argument("-d", "--debug",
+                        action="store_true",
+                        default=False,
+                        help="debug mode for flask"
+                        )
+    parser.add_argument("-j", "--debugjs",
+                        action="store_true",
+                        default=False,
+                        help="Whether to use bokehjs from the bokehjs build directory in the source tree or not"
+                        )
+    parser.add_argument("-s", "--splitjs",
+                        action="store_true",
+                        default=False,
+                        help="don't serve compiled bokeh.js file.  This can only be True if debugjs is True"
+                        )
+    parser.add_argument("-v", "--verbose", action="store_true", default=False)
+    parser.add_argument("--backend",
+                        help="storage backend: [ redis | memory | shelve ], default: %s" % DEFAULT_BACKEND,
+                        type=str,
+                        default=DEFAULT_BACKEND
+                        )
+    parser.add_argument("--ip",
+                        help="The IP address the bokeh server will listen on",
+                        type=str,
+                        default="127.0.0.1"
+                        )
+    parser.add_argument("--bokeh-port",
+                        help="port for bokeh server",
+                        type=int,
+                        default=5006
+                        )
+    parser.add_argument("--redis-port",
+                        help="port for redis",
+                        type=int,
+                        default=7001
+                        )
+    parser.add_argument("-r", "--start-redis",
+                        help="start redis",
+                        action="store_false",
+                        default=True
+                        )
+    parser.add_argument("-m", "--multi-user",
+                        help="multi user",
+                        action="store_true",
+                        default=False
+                        )
+    parser.add_argument("-D", "--data-directory",
+                        help="data directory",
+                        type=str
+                        )
+    return parser
+
+def run():
+    from . import start
+
+    parser = build_parser()
+    args = parser.parse_args(sys.argv[1:])
+
+    start.bokeh_app.debug = False
+    start.app.debug = False
+    start.bokeh_app.splitjs = args.splitjs
+    start.bokeh_app.debugjs = args.debugjs
+
+    backend = {
+        "type": args.backend,
+        "redis_port": args.redis_port,
+        "start_redis": args.start_redis,
+    }
+
+    start.prepare_app(backend, single_user_mode=not args.multi_user,
+                      data_directory=args.data_directory)
+
+    if args.debug:
+        start.bokeh_app.debug = True
+        logging.basicConfig(level=logging.DEBUG)
+
+        import werkzeug.serving
+        @werkzeug.serving.run_with_reloader
+        def helper():
+            start.start_app(host=args.ip, port=args.bokeh_port, verbose=True)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        start.start_app(host=args.ip, port=args.bokeh_port, verbose=args.verbose)
