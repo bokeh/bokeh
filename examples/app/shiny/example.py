6bc19ce28395c2caf48e16ed109433f6c6cd0f4b
hugo <hugo>
2014-04-04 15:18:39 -0400

    Added comments

--- examples/app/shiny/example.py
+++ examples/app/shiny/example.py
@@ -8,6 +8,16 @@ from bokeh.properties import (Dict, Float, String, Instance)
 import numpy as np
 
 class MyModel(VBoxModelForm):
+    """Input Widgets, define the fields you want to 
+    read from the input here as bokeh properties
+    input_specs is a list of dictionary, specifying
+    how the kind of input widget you want for each
+    property.  the name field must match
+    one of the properties, for example here, 
+    we use names of offset and scale.  You can
+    also specify title, if you want a different
+    label in the generated form
+    """
     offset = Float(1.0)
     scale = Float(1.0)
     input_specs = [
@@ -24,6 +34,10 @@ class MyApp(ShinyApp):
     source = Instance(ColumnDataSource, has_ref=True)
     
     def create(self, session):
+        """
+        This function is called once, and is responsible for 
+        creating all objects (plots, datasources, etc)
+        """
         self.modelform = MyModel()
         session.add(self.modelform)        
         self.modelform.create_inputs(session)
@@ -35,6 +49,17 @@ class MyApp(ShinyApp):
         self.children.append(self.modelform)
         self.children.append(self.plot)
 
+    def input_change(self, obj, attrname, old, new):
+        """
+        This function is called whenever the input form changes
+        This is responsible for updating the plot, or whatever
+        you want.  The signature is 
+        obj : the object that changed
+        attrname : the attr that changed
+        old : old value of attr
+        new : new value of attr
+        """
+        self.update_data()
 
     def update_data(self):
         N = 80
@@ -42,8 +67,8 @@ class MyApp(ShinyApp):
         y = np.sin(x)
         y = self.modelform.offset + y * self.modelform.scale
         self.source.data = {'x' : x, 'y' : y}
+
+# the following addes "/exampleapp" as a url which renders MyApp
         
-    def input_change(self, obj, attrname, old, new):
-        self.update_data()
 bokeh_url = "http://localhost:5006"
 MyApp.add_route("/exampleapp", bokeh_url)
