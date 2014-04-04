ea6e9a35bd05922d7bcbe3b90263b61995a99e6b
hugo <hugo>
2014-04-04 15:14:09 -0400

    cleaned up example code

--- bokeh/widgetobjects.py
+++ bokeh/widgetobjects.py
@@ -3,6 +3,7 @@ from .properties import (HasProps, Dict, Enum,
                          Either, Float, Instance, Int,
                          List, String, Color, Include, Bool, 
                          Tuple, Any)
+import copy
 
 class HBox(PlotObject):
     children = List(Instance(PlotObject, has_ref=True), has_ref=True)
@@ -13,6 +14,8 @@ class VBox(PlotObject):
 class VBoxModelForm(PlotObject):
     _children  = List(Instance(PlotObject, has_ref=True), has_ref=True)
     _field_defs = Dict()
+    input_specs = None
+    jsmodel = "VBoxModelForm"
     def __init__(self, *args, **kwargs):
         super(VBoxModelForm, self).__init__(*args, **kwargs)
         for prop in self.__properties__:
@@ -23,7 +26,22 @@ class VBoxModelForm(PlotObject):
                 self._field_defs[prop] = "Int"
             else:
                 self._field_defs[prop] = "String"
-            
+    def create_inputs(self, session):
+        session.add(self)        
+        if self.input_specs:
+            for input_spec in self.input_specs:
+                input_spec = copy.copy(input_spec)
+                widget = input_spec.pop('widget')
+                if input_spec.get('title') is None:
+                    input_spec['title'] = input_spec['name']
+                if input_spec.get('value') is not None:
+                    input_spec['value'] = str(input_spec.get('value'))
+                print (input_spec)
+                widget = widget(**input_spec)
+                session.add(widget)
+                self._children.append(widget)
+
+
 class InputWidget(PlotObject):
     title = String()
     name = String()
@@ -35,6 +53,8 @@ class TextInput(InputWidget):
 class ShinyApp(PlotObject):
     modelform = Instance(VBoxModelForm, has_ref=True)
     children = List(Instance(PlotObject, has_ref=True), has_ref=True)
+    jsmodel = "HBox"
+    extra_generated_classes = List(Any)
 
     def update(self, **kwargs):
         super(ShinyApp, self).update(**kwargs)
@@ -51,3 +71,39 @@ class ShinyApp(PlotObject):
 
     def create(self):
         pass
+        
+    @classmethod
+    def add_route(cls, route, bokeh_url):
+        from bokeh.server.app import bokeh_app
+        from bokeh.pluginutils import app_document
+        from bokeh.plotting import session
+        from flask import render_template
+        @app_document(cls.__view_model__, bokeh_url)
+        def make_app():
+            app = cls()
+            app.create(session())
+            return app
+            
+        @bokeh_app.route(route)
+        def exampleapp():
+            app = make_app()
+            docname = session().docname
+            docid = session().docid
+            extra_generated_classes = app.extra_generated_classes
+            if len(extra_generated_classes) == 0:
+                extra_generated_classes.append((
+                    app.__view_model__,
+                    app.__view_model__,
+                    app.jsmodel))
+                extra_generated_classes.append((
+                    app.modelform.__view_model__,
+                    app.modelform.__view_model__,
+                    app.modelform.jsmodel))
+            return render_template(
+                'shiny.html', 
+                extra_generated_classes=extra_generated_classes,
+                title=app.__class__.__view_model__,
+                docname=docname, 
+                docid=docid,
+                splitjs=bokeh_app.splitjs)
+    
--- examples/app/shiny/example.py
+++ examples/app/shiny/example.py
@@ -1,42 +1,32 @@
-from flask import render_template
-from bokeh.server.app import bokeh_app
-from bokeh.plotting import line, session
-from bokeh.pluginutils import app_document
-from bokeh.widgetobjects import (VBoxModelForm, VBox, HBox,
+from bokeh.plotting import line
+
+from bokeh.widgetobjects import (VBoxModelForm, HBox,
                                  ShinyApp, TextInput)
 from bokeh.objects import Plot, ColumnDataSource
 from bokeh.plotobject import PlotObject
-from bokeh.properties import (HasProps, Dict, Enum, 
-                         Either, Float, Instance, Int,
-                         List, String, Color, Include, Bool, 
-                         Tuple, Any)
+from bokeh.properties import (Dict, Float, String, Instance)
 import numpy as np
-from bokeh.pluginutils import app_document
-
 
 class MyModel(VBoxModelForm):
     offset = Float(1.0)
     scale = Float(1.0)
-
+    input_specs = [
+        {"widget" : TextInput,
+         "name" : "offset",
+         "value" : 1.0},
+        {"widget" : TextInput,
+         "name" : "scale",
+         "value" : 1.0}
+    ]
+    
 class MyApp(ShinyApp):
     plot = Instance(Plot, has_ref=True)
     source = Instance(ColumnDataSource, has_ref=True)
-
-    def create(self):
+    
+    def create(self, session):
         self.modelform = MyModel()
-        input1 = TextInput(title="offset", 
-                           name="offset", 
-                           value=str(self.modelform.offset)
-        )
-        input2 = TextInput(title="scale", 
-                           name="scale", 
-                           value=str(self.modelform.offset)
-        )
-        session().add(input1)
-        session().add(input2)
-        self.modelform._children.append(input1)
-        self.modelform._children.append(input2)        
-        
+        session.add(self.modelform)        
+        self.modelform.create_inputs(session)
         self.source = ColumnDataSource(data={'x':[], 'y':[]})
         self.update_data()
         self.plot = line('x', 'y', source=self.source,
@@ -44,7 +34,7 @@ class MyApp(ShinyApp):
         )
         self.children.append(self.modelform)
         self.children.append(self.plot)
-        session().add(self.modelform)
+
 
     def update_data(self):
         N = 80
@@ -55,28 +45,5 @@ class MyApp(ShinyApp):
         
     def input_change(self, obj, attrname, old, new):
         self.update_data()
-    
-bokeh_location = "localhost:5006"
-bokeh_url = "http://" + bokeh_location
-@app_document("sampleapp", bokeh_url)
-def make_app():
-    app = MyApp()
-    app.create()
-    return app
-
-@bokeh_app.route("/exampleapp")
-def exampleapp():
-    app = make_app()
-    docname = session().docname
-    docid = session().docid
-    extra_generated_classes = [
-        ('MyModel', 'MyModel', 'VBoxModelForm'),
-        ('MyApp', 'MyApp', 'HBox'),
-    ]
-    return render_template(
-        'shiny.html', 
-        extra_generated_classes=extra_generated_classes,
-        title=docname, 
-        docid=docid,
-        splitjs=bokeh_app.splitjs)
-    
+bokeh_url = "http://localhost:5006"
+MyApp.add_route("/exampleapp", bokeh_url)
