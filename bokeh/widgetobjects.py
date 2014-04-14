d7bae7e45f2c68405c153d5a86bb039a82f9bc17
Mateusz Paprocki <mattpap@gmail.com>
2014-04-15 01:36:35 +0200

    Cleanup bokeh/widgetobjects.py

--- bokeh/widgetobjects.py
+++ bokeh/widgetobjects.py
@@ -1,8 +1,8 @@
 import six
 from .plotobject import PlotObject
-from .properties import (HasProps, Dict, Enum, 
+from .properties import (HasProps, Dict, Enum,
                          Either, Float, Instance, Int,
-                         List, String, Color, Include, Bool, 
+                         List, String, Color, Include, Bool,
                          Tuple, Any, lookup_descriptor)
 import copy
 import logging
@@ -30,7 +30,7 @@ class VBoxModelForm(PlotObject):
             else:
                 self._field_defs[prop] = "String"
     def create_inputs(self, session):
-        session.add(self)        
+        session.add(self)
         if self.input_specs:
             for input_spec in self.input_specs:
                 input_spec = copy.copy(input_spec)
@@ -53,10 +53,10 @@ class InputWidget(PlotObject):
             return int(val)
         if isinstance(prop_obj, String):
             return str(val)
-            
+
     @classmethod
     def create(cls, *args, **kwargs):
-        """Only called the first time we make an object, 
+        """Only called the first time we make an object,
         whereas __init__ is called every time it's loaded
         """
         if kwargs.get('title') is None:
@@ -66,26 +66,26 @@ class InputWidget(PlotObject):
         return cls(**kwargs)
 
 class TextInput(InputWidget):
-    value = String()    
+    value = String()
 
 class BokehApplet(PlotObject):
     modelform = Instance(VBoxModelForm, has_ref=True)
     children = List(Instance(PlotObject, has_ref=True), has_ref=True)
     jsmodel = "HBox"
     extra_generated_classes = List(Tuple(String, String, String))
-    
+
     def update(self, **kwargs):
         super(BokehApplet, self).update(**kwargs)
         self.setup_events()
-        
+
     def setup_events(self):
         if self.modelform:
             self.bind_modelform()
-        
+
     def bind_modelform(self):
         for prop in self.modelform.__properties__:
             if not prop.startswith("_"):
-                self.modelform.on_change(prop, self, 
+                self.modelform.on_change(prop, self,
                                          'input_change')
 
     def input_change(self, obj, attrname, old, new):
@@ -109,7 +109,7 @@ class BokehApplet(PlotObject):
             app = cls()
             app.create(session())
             return app
-            
+
         def exampleapp():
             app = make_app()
             docname = session().docname
@@ -125,18 +125,18 @@ class BokehApplet(PlotObject):
                     app.modelform.__view_model__,
                     app.modelform.jsmodel))
             return render_template(
-                'applet.html', 
+                'applet.html',
                 extra_generated_classes=extra_generated_classes,
                 title=app.__class__.__view_model__,
-                docname=docname, 
+                docname=docname,
                 docid=docid,
                 splitjs=bokeh_app.splitjs)
         exampleapp.__name__ = cls.__view_model__
         bokeh_app.route(route)(exampleapp)
-    
+
 class Paragraph(PlotObject):
     text = String()
-    
+
 class PreText(Paragraph):
     pass
 
@@ -154,7 +154,7 @@ class Select(InputWidget):
             new_options.append(opt)
         kwargs['options'] = new_options
         return super(Select, self).create(*args, **kwargs)
-        
+
 class Slider(InputWidget):
     value = Float()
     start = Float()
