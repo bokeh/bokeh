8a40c680c115ce9a74609224f89c338f96a7e07f
Bryan Van de Ven <bryanv@continuum.io>
2014-04-21 18:36:26 -0500

    always install built js with --build_js

--- setup.py
+++ setup.py
@@ -183,11 +183,6 @@ if '--deploy' in sys.argv:
     sys.argv.remove("--deploy")
     sys.argv.append("--build_js")
 
-if 'develop' in sys.argv:
-    APP = [join(BOKEHJSBUILD, 'js', 'bokeh.js'),
-           join(BOKEHJSBUILD, 'js', 'bokeh.min.js')]
-    CSS = join(BOKEHJSBUILD, 'css')
-
 if '--build_js' in sys.argv:
     os.chdir('bokehjs')
     try:
@@ -197,6 +192,9 @@ if '--build_js' in sys.argv:
     except subprocess.CalledProcessError:
         print("ERROR: could not deploy bokehjs")
         sys.exit(1)
+    APP = [join(BOKEHJSBUILD, 'js', 'bokeh.js'),
+           join(BOKEHJSBUILD, 'js', 'bokeh.min.js')]
+    CSS = join(BOKEHJSBUILD, 'css')
     os.chdir('..')
 
 if exists(join(SERVER, 'static', 'js')):
