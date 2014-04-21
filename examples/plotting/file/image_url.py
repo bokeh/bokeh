d1a913f3fc1548db0fb3d35d55e76b5fe270ef10
Bryan Van de Ven <bryanv@continuum.io>
2014-04-21 11:30:28 -0500

    py3 fix

--- examples/plotting/file/image_url.py
+++ examples/plotting/file/image_url.py
@@ -4,8 +4,8 @@ output_file("image_url.html")
 
 image_url(
     url=["http://bokeh.pydata.org/_static/bokeh-transparent.png"]*10, 
-    x=range(0, 100, 10), 
-    y=range(0, 100, 10), 
+    x=list(range(0, 100, 10)), 
+    y=list(range(0, 100, 10)), 
     angle=0
 )
 
