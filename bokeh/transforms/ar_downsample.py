d4fa5735a8d21a418a80050d73d490cdac2aac84
Joseph Cottam <jcottam@indiana.edu>
2014-04-22 01:00:48 -0400

    AR Downsample stub.

---
+++ bokeh/transforms/ar_downsample.py
@@ -0,0 +1,13 @@
+def downsample(data, glyphspec, grid, transfer):
+  """AR Downsample uses abstract rendering to convert a dataset
+     into another.  The glyphspec is used to conver the dataset
+     into a visualization (well...just the center part, not the axes/legend/etc).
+     The grid is used to discretize that visualization and 
+     the transfer is used do analysis on the discretized data.
+     The result of transfer is an arbitrary dataset...which then
+     may be visualized."""
+
+
+  pass
+
+
