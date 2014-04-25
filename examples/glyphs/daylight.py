4dc71c7c09b53afe979ab196286a52efc32e2d7f
Mateusz Paprocki <mattpap@gmail.com>
2014-04-25 16:50:24 +0200

    Show how to use DatetimeTickFormatter in daylight.py example

--- examples/glyphs/daylight.py
+++ examples/glyphs/daylight.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 import numpy as np
 import datetime as dt
 
-from bokeh.objects import Plot, DataRange1d, DatetimeAxis, ColumnDataSource, Glyph, Grid, Legend
+from bokeh.objects import (Plot, DataRange1d, DatetimeAxis,
+    DatetimeTickFormatter, ColumnDataSource, Glyph, Grid, Legend)
 from bokeh.glyphs import Patch, Line, Text
 from bokeh.session import HTMLFileSession
 from bokeh.sampledata import daylight
@@ -72,7 +73,8 @@ text = Text(x="dates", y="times", text="texts", angle=0, text_align="center")
 text_glyph = Glyph(data_source=text_source, xdata_range=xdr, ydata_range=ydr, glyph=text)
 plot.renderers.append(text_glyph)
 
-xaxis = DatetimeAxis(plot=plot, dimension=0)
+xformatter = DatetimeTickFormatter(formats=dict(months=["%b %Y"]))
+xaxis = DatetimeAxis(plot=plot, dimension=0, formatter=xformatter)
 yaxis = DatetimeAxis(plot=plot, dimension=1)
 xgrid = Grid(plot=plot, dimension=0, axis=xaxis)
 ygrid = Grid(plot=plot, dimension=1, axis=yaxis)
