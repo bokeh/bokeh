ee723c54e5a96f7ffc10186469b6c990c318c61d
damianavila <damianavila@gmail.com>
2014-04-26 12:16:42 -0300

    Created mpl_helpers.py and reordering of main code.

--- bokeh/mpl.py
+++ bokeh/mpl.py
@@ -5,10 +5,9 @@
 import warnings
 import numpy as np
 import matplotlib as mpl
-from itertools import (cycle, islice)
 
-from scipy import interpolate, signal
-
+from .mpl_helpers import (_convert_color, _convert_dashes, _delete_last_col, _get_props_cycled,
+                         _xkcd_line)
 from .objects import (Plot, DataRange1d, LinearAxis, ColumnDataSource, Glyph,
                       Grid, PanTool, WheelZoomTool)
 from .glyphs import (Line, Circle, Square, Cross, Triangle, InvertedTriangle,
@@ -34,108 +33,55 @@ def axes2plot(ax, xkcd):
     if background_fill == 'w':
         background_fill = 'white'
     title = ax.get_title()
-    plot = Plot(title=title, background_fill=background_fill)
+
+    xdr = DataRange1d()
+    ydr = DataRange1d()
+    source = ColumnDataSource()
+
+    plot = Plot(title=title, data_sources=[source], x_range=xdr, y_range=ydr,
+                background_fill=background_fill)
+
     if xkcd:
         plot.title_text_font = "Comic Sans MS, Textile, cursive"
         plot.title_text_font_style = "bold"
         plot.title_text_color = "black"
-    if _PLOTLIST is not None:
-        _PLOTLIST.append(plot)
-    plot.x_range = DataRange1d()
-    plot.y_range = DataRange1d()
-    datasource = ColumnDataSource()
-    plot.data_sources = [datasource]
 
     # Break up the lines and markers by filtering on linestyle and marker style
-    lines = [line for line in ax.lines if line.get_linestyle() not in ("", " ", "None", "none", None)]
+    lines = [l for l in ax.lines if l.get_linestyle() not in ("", " ", "None", "none", None)]
     markers = [m for m in ax.lines if m.get_marker() not in ("", " ", "None", "none", None)]
     cols = [col for col in ax.collections if col.get_paths() not in ("", " ", "None", "none", None)]
-    renderers = [_make_line(datasource, plot.x_range, plot.y_range, line, xkcd) for line in lines]
-    renderers.extend(_make_marker(datasource, plot.x_range, plot.y_range, marker) for marker in markers)
-    renderers.extend(_make_lines_collection(datasource, plot.x_range, plot.y_range, col, xkcd) \
-                        for col in cols if isinstance(col, mpl.collections.LineCollection))
-    renderers.extend(_make_polys_collection(datasource, plot.x_range, plot.y_range, col) \
-                        for col in cols if isinstance(col, mpl.collections.PolyCollection))
-    plot.renderers.extend(renderers)
+
+    rends = []
+    rends.extend(make_line(source, plot.x_range, plot.y_range, line, xkcd) for line in lines)
+    rends.extend(make_marker(source, plot.x_range, plot.y_range, marker) for marker in markers)
+    rends.extend(make_line_collection(source, plot.x_range, plot.y_range, col, xkcd)
+                     for col in cols if isinstance(col, mpl.collections.LineCollection))
+    rends.extend(make_poly_collection(source, plot.x_range, plot.y_range, col)
+                     for col in cols if isinstance(col, mpl.collections.PolyCollection))
+
+    plot.renderers.extend(rends)
 
     # xaxis
-    xaxis = _make_axis(plot, ax.xaxis, 0, xkcd)
+    xaxis = make_axis(plot, ax.xaxis, 0, xkcd)
 
     # yaxis
-    yaxis = _make_axis(plot, ax.yaxis, 1, xkcd)
+    yaxis = make_axis(plot, ax.yaxis, 1, xkcd)
 
     # xgrid
-    _make_grid(plot, ax.get_xgridlines()[0], xaxis, 0)
+    make_grid(plot, ax.get_xgridlines()[0], xaxis, 0)
 
     # ygrid
-    _make_grid(plot, ax.get_xgridlines()[0], yaxis, 1)
+    make_grid(plot, ax.get_xgridlines()[0], yaxis, 1)
 
     # Add tools
     pantool = PanTool(dimensions=["width", "height"])
     wheelzoom = WheelZoomTool(dimensions=["width", "height"])
     plot.tools = [pantool, wheelzoom]
+
+    if _PLOTLIST is not None:
+        _PLOTLIST.append(plot)
+
     return plot
-
-
-def _convert_color(mplcolor):
-    charmap = dict(b="blue", g="green", r="red", c="cyan", m="magenta",
-                   y="yellow", k="black", w="white")
-    if mplcolor in charmap:
-        return charmap[mplcolor]
-
-    try:
-        colorfloat = float(mplcolor)
-        if 0 <= colorfloat <= 1.0:
-            # This is a grayscale value
-            return tuple([int(255 * colorfloat)] * 3)
-    except:
-        pass
-
-    if isinstance(mplcolor, tuple):
-        # These will be floats in the range 0..1
-        return int(255 * mplcolor[0]), int(255 * mplcolor[1]), int(255 * mplcolor[2])
-
-    return mplcolor
-
-
-def _convert_dashes(dash):
-    """ Converts a Matplotlib dash specification
-
-    bokeh.properties.DashPattern supports the matplotlib named dash styles,
-    but not the little shorthand characters.  This function takes care of
-    mapping those.
-    """
-    mpl_dash_map = {
-        "-": "solid",
-        "--": "dashed",
-        ":": "dotted",
-        "-.": "dashdot",
-    }
-    # If the value doesn't exist in the map, then just return the value back.
-    return mpl_dash_map.get(dash, dash)
-
-
-def _get_props_cycled(col, prop, fx=lambda x: x):
-    """ We need to cycle the `get.property` list (where property can be colors,
-    line_width, etc) as matplotlib does. We use itertools tools for do this
-    cycling ans slice manipulation.
-
-    Parameters:
-
-    col: matplotlib collection object
-    prop: property we want to get from matplotlib collection
-    fx: funtion (optional) to transform the elements from list obtained
-        after the property call. Deafults to identity function.
-    """
-    n = len(col.get_paths())
-    t_prop = [fx(x) for x in prop]
-    sliced = islice(cycle(t_prop), None, n)
-    return list(sliced)
-
-
-def _delete_last_col(x):
-    x = np.delete(x, (-1), axis=1)
-    return x
 
 
 def line_props(line, line2d):
@@ -218,7 +164,7 @@ def text_props(mplText, obj, prefix=""):
     setattr(obj, prefix+"text_font", mplText.get_fontfamily()[0])
 
 
-def _make_axis(plot, ax, dimension, xkcd):
+def make_axis(plot, ax, dimension, xkcd):
     """ Given an mpl.Axis instance, returns a bokeh LinearAxis """
     # TODO:
     #  * handle `axis_date`, which treats axis as dates
@@ -253,19 +199,19 @@ def _make_axis(plot, ax, dimension, xkcd):
     return laxis
 
 
-def _make_grid(plot, grid, ax, dimension):
+def make_grid(plot, grid, ax, dimension):
 
     Grid(plot=plot, dimension=dimension, axis=ax,
          grid_line_color=grid.get_color(), grid_line_width=grid.get_linewidth())
 
 
-def _make_line(source, xdr, ydr, line2d, xkcd):
+def make_line(source, xdr, ydr, line2d, xkcd):
     ""
     xydata = line2d.get_xydata()
     x = xydata[:, 0]
     y = xydata[:, 1]
     if xkcd:
-        x, y = xkcd_line(x, y)
+        x, y = _xkcd_line(x, y)
 
     line = Line()
     line.x = source.add(x)
@@ -281,7 +227,7 @@ def _make_line(source, xdr, ydr, line2d, xkcd):
     return line_glyph
 
 
-def _make_marker(source, xdr, ydr, line2d):
+def make_marker(source, xdr, ydr, line2d):
     """ Given a matplotlib line2d instance that has non-null marker type,
     return an appropriate Bokeh Marker glyph.
     """
@@ -313,15 +259,15 @@ def _make_marker(source, xdr, ydr, line2d):
     return marker_glyph
 
 
-def _make_lines_collection(source, xdr, ydr, col, xkcd):
+def make_line_collection(source, xdr, ydr, col, xkcd):
     ""
     xydata = col.get_segments()
     t_xydata = [np.transpose(seg) for seg in xydata]
     xs = [t_xydata[x][0] for x in range(len(t_xydata))]
     ys = [t_xydata[x][1] for x in range(len(t_xydata))]
     if xkcd:
-        xkcd_xs = [xkcd_line(xs[i], ys[i])[0] for i in range(len(xs))]
-        xkcd_ys = [xkcd_line(xs[i], ys[i])[1] for i in range(len(ys))]
+        xkcd_xs = [_xkcd_line(xs[i], ys[i])[0] for i in range(len(xs))]
+        xkcd_ys = [_xkcd_line(xs[i], ys[i])[1] for i in range(len(ys))]
         xs = xkcd_xs
         ys = xkcd_ys
 
@@ -337,7 +283,7 @@ def _make_lines_collection(source, xdr, ydr, col, xkcd):
     return multiline_glyph
 
 
-def _make_polys_collection(source, xdr, ydr, col):
+def make_poly_collection(source, xdr, ydr, col):
     ""
     paths = col.get_paths()
     polygons = [paths[i].to_polygons() for i in range(len(paths))]
@@ -355,79 +301,3 @@ def _make_polys_collection(source, xdr, ydr, col):
 
     patches_glyph = Glyph(data_source=source, xdata_range=xdr, ydata_range=ydr, glyph=patches)
     return patches_glyph
-
-
-def xkcd_line(x, y, xlim=None, ylim=None, mag=1.0, f1=30, f2=0.001, f3=5):
-    """
-    Mimic a hand-drawn line from (x, y) data
-    Source: http://jakevdp.github.io/blog/2012/10/07/xkcd-style-plots-in-matplotlib/
-
-    Parameters
-    ----------
-    x, y : array_like
-        arrays to be modified
-    xlim, ylim : data range
-        the assumed plot range for the modification.  If not specified,
-        they will be guessed from the  data
-    mag : float
-        magnitude of distortions
-    f1, f2, f3 : int, float, int
-        filtering parameters.  f1 gives the size of the window, f2 gives
-        the high-frequency cutoff, f3 gives the size of the filter
-
-    Returns
-    -------
-    x, y : ndarrays
-        The modified lines
-    """
-    x = np.asarray(x)
-    y = np.asarray(y)
-
-    # get limits for rescaling
-    if xlim is None:
-        xlim = (x.min(), x.max())
-    if ylim is None:
-        ylim = (y.min(), y.max())
-
-    if xlim[1] == xlim[0]:
-        xlim = ylim
-
-    if ylim[1] == ylim[0]:
-        ylim = xlim
-
-    # scale the data
-    x_scaled = (x - xlim[0]) * 1. / (xlim[1] - xlim[0])
-    y_scaled = (y - ylim[0]) * 1. / (ylim[1] - ylim[0])
-
-    # compute the total distance along the path
-    dx = x_scaled[1:] - x_scaled[:-1]
-    dy = y_scaled[1:] - y_scaled[:-1]
-    dist_tot = np.sum(np.sqrt(dx * dx + dy * dy))
-
-    # number of interpolated points is proportional to the distance
-    Nu = int(200 * dist_tot)
-    u = np.arange(-1, Nu + 1) * 1. / (Nu - 1)
-
-    # interpolate curve at sampled points
-    k = min(3, len(x) - 1)
-    res = interpolate.splprep([x_scaled, y_scaled], s=0, k=k)
-    x_int, y_int = interpolate.splev(u, res[0])
-
-    # we'll perturb perpendicular to the drawn line
-    dx = x_int[2:] - x_int[:-2]
-    dy = y_int[2:] - y_int[:-2]
-    dist = np.sqrt(dx * dx + dy * dy)
-
-    # create a filtered perturbation
-    coeffs = mag * np.random.normal(0, 0.01, len(x_int) - 2)
-    b = signal.firwin(f1, f2 * dist_tot, window=('kaiser', f3))
-    response = signal.lfilter(b, 1, coeffs)
-
-    x_int[1:-1] += response * dy / dist
-    y_int[1:-1] += response * dx / dist
-
-    # un-scale data
-    x_int = x_int[1:-1] * (xlim[1] - xlim[0]) + xlim[0]
-    y_int = y_int[1:-1] * (ylim[1] - ylim[0]) + ylim[0]
-
-    return x_int, y_int
