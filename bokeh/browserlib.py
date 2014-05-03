28d1875008dc1201e14391aac7724a52471866fa
Bryan Van de Ven <bryanv@continuum.io>
2014-05-02 17:23:19 -0700

    add view funtion to browserlib

--- bokeh/browserlib.py
+++ bokeh/browserlib.py
@@ -1,3 +1,5 @@
+
+from os.path import abspath
 import webbrowser
 
 from . import settings
@@ -18,3 +20,23 @@ def get_browser_controller(browser=None):
         controller = webbrowser
 
     return controller
+
+def view(filename, browser=None, new=False, autoraise=True):
+        """ Opens a browser to view the file pointed to by this sessions.
+
+        **new** can be None, "tab", or "window" to view the file in the
+        existing page, a new tab, or a new windows.  **autoraise** causes
+        the browser to be brought to the foreground; this may happen
+        automatically on some platforms regardless of the setting of this
+        variable.
+        """
+        new_map = { False: 0, "window": 1, "tab": 2 }
+        file_url = "file://" + abspath(filename)
+
+        try:
+            controller = get_browser_controller(browser)
+            controller.open(file_url, new=new_map[new], autoraise=autoraise)
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except:
+            pass
