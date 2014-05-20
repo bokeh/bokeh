549db492990135f1d29ebeec7f5b5bb41430ea05
Benjamin Zaitlen <quasiben@gmail.com>
2014-05-20 00:01:39 -0500

    forgot pushd/popd calls

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# Recipe and source are stored together
+SRC_DIR=$RECIPE_DIR/..
+pushd $SRC_DIR
+ls
+conda install --yes --force -c wakari nodejs
+conda install --yes --force -c wakari grunt-cli
+ls
+pushd bokehjs
+ls
+npm --loglevel warn install -g
+popd
+
 $PYTHON setup.py install --build_js --single-version-externally-managed --record=record.txt
+popd
+
 cd $PREFIX
 echo $PREFIX
 find . -name '*.pyc' -delete
