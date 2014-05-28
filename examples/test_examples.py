534eaf7a1b4ae1658c3bc8cc27e12b71e94d4554
Mateusz Paprocki <mattpap@gmail.com>
2014-05-28 14:35:24 +0200

    Make examples/test --timeout's default value conditional

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -6,5 +6,5 @@ class TestExamples(unittest.TestCase):
 
     def test_examples(self):
         script = os.path.join(os.path.dirname(__file__), "test")
-        proc = subprocess.Popen(["python", script, "--timeout=20"])
+        proc = subprocess.Popen(["python", script])
         self.assertTrue(proc.wait() == 0, "examples do *NOT* work properly")
