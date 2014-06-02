9c835b81fca2de1c4022571c8264e171041c0a91
Benjamin Zaitlen <quasiben@gmail.com>
2014-06-02 17:19:06 -0500

    adding bokeh user to upload command

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -13,6 +13,10 @@ conda convert -p win-64 -f $BUILD_PATH/bokeh*$date*.tar.bz2;
 conda convert -p osx-64 -f $BUILD_PATH/bokeh*$date*.tar.bz2;
 conda convert -p linux-64 -f $BUILD_PATH/bokeh*$date*.tar.bz2;
 
-binstar upload osx-64/bokeh*$date*.tar.bz2;
-binstar upload win-64/bokeh*$date*.tar.bz2;
-binstar upload linux-64/bokeh*$date*.tar.bz2;
+binstar upload -u bokeh osx-64/bokeh*$date*.tar.bz2 -c dev;
+binstar upload -u bokeh win-64/bokeh*$date*.tar.bz2 -c dev;
+binstar upload -u bokeh linux-64/bokeh*$date*.tar.bz2 -c dev;
+
+rm -rf osx-64 linux-64 win-64
+
+echo "I'm done uploading"
