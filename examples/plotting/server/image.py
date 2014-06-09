3cc34a779ba692f9fcf67840556c738e2e15ff56
Bryan Van de Ven <bryanv@continuum.io>
2014-06-09 14:31:15 -0500

    allow ranges to be specified as sequences

--- examples/plotting/server/image.py
+++ examples/plotting/server/image.py
@@ -9,7 +9,7 @@ d = [[0,1,2,3,4],
 output_server('image')
 image(
     image=[d], x=[1], y=[2], dw=[10], dh=[10], palette=["Reds-9"],
-    x_range = Range1d(start=0, end=10), y_range = Range1d(start=0, end=10),
+    x_range=[0, 10], y_range=[0, 10],
     tools="pan,wheel_zoom,box_zoom,reset,previewsave", name="image_example"
 )
 
