09822a56c119daf7c5b06b5919487d894ee41499
Maggie-M <maggie.mari@continuum.io>
2014-06-16 14:41:05 -0500

    Increased number of example directories script can test in.

--- examples/interactiveTester.py
+++ examples/interactiveTester.py
@@ -1,10 +1,11 @@
-# "bokeh is imported and unused as a quick way to check for directory bokeh/bokeh/static/js
+# bokeh is imported and unused as a quick way to check for directory bokeh/bokeh/static/js
 # which is required for many (but not all) examples to run properly.
 import bokeh
 import glob
 import os
 from six.moves import input
 import sys
+import time
 
 
 # TODO: --no-log option
@@ -12,12 +13,23 @@ import sys
 #           - This is currently the default behavior
 #       --clean option (remove .html files created by running examples)
 
+directories = {
+    'file'    : 'plotting/file',
+    'notebook': 'plotting/notebook',
+    'server'  : 'plotting/server',
+    'ggplot'  : 'ggplot',
+    'glyphs'  : 'glyphs',
+    'mpl'     : 'mpl',
+    'pandas'  : 'pandas',
+    'seaborn' : 'seaborn'
+}
+
 
 def tester(TestingGround, HomeDir):
     """
     Collect and run all .py or .ipynb files in an examples directory, ignoring __init__.py
-
     User input is collected to determine a properly or improperly displayed page
+
     """
 
     os.chdir(TestingGround)
@@ -31,13 +43,13 @@ def tester(TestingGround, HomeDir):
 
     for fileName in TestFiles:
         try:
-            print("\nOpening %s.\n" % fileName)
+            print("\nOpening %s\n" % fileName)
             runner(fileName)
 
             TestStatus = input("Did the plot(s) in %s display correctly? (y/n) " % fileName)
             while not TestStatus.startswith(('y', 'n')):
                 print()
-                TestStatus = input("Unexpected answer.  Please type y or n.  ")
+                TestStatus = input("Unexpected answer. Please type y or n. ")
             if TestStatus.startswith('n'):
                 ErrorReport = input("Please describe the problem: ")
                 Log.append("\n\n%s: \n %s" % (fileName, ErrorReport))
@@ -88,19 +100,23 @@ def logger(ErrorArray):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] in ['notebook', 'server', 'file']:
+    if len(sys.argv) == 2 and sys.argv[1] in directories:
         target = sys.argv[1]
+
+        if sys.argv[1] == 'server':
+            print("Server examples require bokeh-server to run.  Make sure you've typed 'bokeh-server' in another terminal tab.")
+            time.sleep(5)
 
         logfile = "%sExamplesTestlog.txt" % target
         if os.path.exists(logfile):
             os.remove(logfile)
 
         base_dir = os.getcwd()
-        test_dir = os.path.join(base_dir, 'plotting', target)
+        test_dir = os.path.join(base_dir, directories[target])
         tester(test_dir, base_dir)
     else:
         # # This is kept necessarily explicit so that you don't
         # accidentally provide a directory that has .html files
         # you don't want to have deleted.
-        print("Please choose an examples directory to test in ('python bokehTest.py <notebook/server/file>)")
+        print("Please choose an examples directory to test in ('python interactiveTester.py <plotting/file>")
         sys.exit(1)
