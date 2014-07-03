46c5b933f5a7aad995e7af7b156d9d447ab6df01
Bryan Van de Ven <bryanv@continuum.io>
2014-07-02 19:49:49 -0500

    Add click tool, allow click and hover tools to be always on

--- bokeh/objects.py
+++ bokeh/objects.py
@@ -44,7 +44,7 @@ class ColumnDataSource(DataSource):
     # field is not in the dict, then a range is created automatically.
     cont_ranges = Dict(String, Instance(".objects.Range"))
     discrete_ranges = Dict(String, Instance(".objects.Range"))
-    
+
     def __init__(self, *args, **kw):
         """ Modify the basic DataSource/PlotObj constructor so that if we
         are called with a single argument that is a dict, then we treat
@@ -63,11 +63,11 @@ class ColumnDataSource(DataSource):
         for name, data in raw_data.items():
             self.add(data, name)
         super(ColumnDataSource, self).__init__(**kw)
-        
+
     def to_df(self):
         """convert data source to pandas dataframe
         local import of pandas because of possible compatability issues (pypy?)
-        if we have column_names set, we use those, otherwise we let 
+        if we have column_names set, we use those, otherwise we let
         pandas infer the column names.  column_names can be used to
         either order or filter the columns here.
         """
@@ -76,7 +76,7 @@ class ColumnDataSource(DataSource):
             return pd.DataFrame(self.data, columns=self.column_names)
         else:
             return pd.DataFrame(self.data)
-            
+
     def add(self, data, name=None):
         """ Appends the data to the list of columns.  Returns the name
         that was inserted.
@@ -100,12 +100,12 @@ class ColumnDataSource(DataSource):
 class ServerDataSource(DataSource):
     data_url = String()
     owner_username = String()
-    
+
     # allow us to add some data that isn't on the remote source
     # and join it to the remote data
     data = Dict(String, Any)
 
-    # Paramters of data transformation operations 
+    # Paramters of data transformation operations
     # The 'Any' is used to pass primtives around.  Minimally, a tag to say which downsample routine to use.  In some downsamplers, parameters are passed this way too.
     # TODO: Find/create a property type for 'any primitive/atomic value'
     transform = Dict(String,Either(Instance(PlotObject), Any))
@@ -222,7 +222,8 @@ class Glyph(Renderer):
                  "server_data_source" : self.server_data_source,
                  "xdata_range": self.xdata_range,
                  "ydata_range": self.ydata_range,
-                 "glyphspec": self.glyph.to_glyphspec()
+                 "glyphspec": self.glyph.to_glyphspec(),
+                 "name": self.name,
                  }
         if self.selection_glyph:
             data['selection_glyphspec'] = self.selection_glyph.to_glyphspec()
@@ -468,6 +469,11 @@ class ResetTool(Tool):
 class ResizeTool(Tool):
     pass
 
+class ClickTool(Tool):
+    names = List(String)
+    always_active = Bool(True)
+    pass
+
 class CrosshairTool(Tool):
     pass
 
@@ -478,7 +484,7 @@ class BoxSelectTool(Tool):
     renderers = List(Instance(Renderer))
     select_every_mousemove = Bool(True)
     select_x = Bool(True)
-    select_y = Bool(True)    
+    select_y = Bool(True)
 
 class BoxSelectionOverlay(Renderer):
     __view_model__ = 'BoxSelection'
@@ -487,6 +493,7 @@ class BoxSelectionOverlay(Renderer):
 class HoverTool(Tool):
     renderers = List(Instance(Renderer))
     tooltips = Dict(String, String)
+    always_active = Bool(True)
 
 class ObjectExplorerTool(Tool):
     pass
