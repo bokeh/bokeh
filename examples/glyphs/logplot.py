d295a9a9b7ea5cb1268c26e9019f335b19a031fe
Tabish Chasmawala <tabchas@tabishs-mbp.gateway.2wire.net>
2014-07-06 09:33:58 -0500

    added log plot example

---
+++ examples/glyphs/logplot.py
@@ -0,0 +1,23 @@
+import numpy as np
+from bokeh.plotting import *
+from bokeh.objects import Range1d
+
+N = 100
+
+x = np.linspace(0.1, 10, N)
+
+output_file("log.html", title="log axis example")
+
+hold()
+
+figure(tools="pan,wheel_zoom,box_zoom,reset,previewsave", y_axis_type="log")
+
+scatter(x, x, legend="y=x", name="legend_example")
+line(x, x, legend="y=x")
+
+line(x, np.exp(x), line_dash=[4, 4], line_color="orange", line_width=2, legend="y=exp(x)")
+
+square(x, x**2, fill_color=None, line_color="green", legend="y=x**2")
+#line(x, x**2, fill_color=None, line_color="green", legend="y=x**2")
+
+show()  # open a browser
