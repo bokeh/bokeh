5079ce6965a5bf1adc06889c14826b17704c8640
Mateusz Paprocki <mattpap@gmail.com>
2014-07-17 11:21:07 +0200

    Fix references to bokeh_white_32.ico

--- sphinx/tutorial_source/conf.py
+++ sphinx/tutorial_source/conf.py
@@ -182,7 +182,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "bokeh_white_32.ico"
+html_favicon = "../_static/bokeh_white_32.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
