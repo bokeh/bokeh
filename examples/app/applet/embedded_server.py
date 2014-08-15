c703bffe7306e28b7b588db3e0865e3e6c3c976f
Bryan Van de Ven <bryanv@continuum.io>
2014-08-15 15:39:26 -0500

    cleanup flask server

--- examples/app/applet/embedded_server.py
+++ examples/app/applet/embedded_server.py
@@ -1,48 +1,43 @@
-from flask import Flask, render_template
+"""
+This is an example applet embedded into another flask app. See the
+README.md file in this rectory for instructions on running.
+"""
+from __future__ import print_function
+
 from os.path import dirname, join, abspath
-import logging
-approot = abspath(dirname(dirname(__file__)))
-app = Flask('sampleapp')
-app.debug = True
+
 from bokeh.pluginutils import app_document
+from flask import Flask, render_template
+import jinja2
+
 from stock_example_embedded import StockApp
 
-"""
-This is an example applet embedded into another flask app.  You will need to download some sample data from quantquote, which can be executed from the download.py script
+app = Flask('sampleapp')
 
-Then, execute bokeh-server --script stock_example_embedded.py, and python embedded_server.py
-point your browser at http://localhost:5050/app
-"""
-
-bokeh_location = "localhost:5006"
-bokeh_url = "http://" + bokeh_location
+bokeh_url = "http://localhost:5006"
+applet_url = "http://localhost:5050/applet"
 
 @app_document("stock_example", bokeh_url)
 def make_stock_applet():
     app = StockApp.create()
     return app
 
-@app.route("/app")
-def main():
-    app = make_stock_applet()
-    html = """
-    <html>
-    <head>
-    <script src="%s"></script>
-    <script>
-    window._bokeh_onload_callbacks = [window.StockApp.main];
-    </script>
-    </head>
-    <body>
-    %s
-    </body>
-    </html>
-    """
-    html = html % (bokeh_url + "/bokeh/jsgenerate/VBox/StockApp/StockApp", app.tag)
-    return html
+@app.route("/")
+def root():
+    return r"Example is located at <a href='%s'>%s</a>" % (applet_url, applet_url)
+
+@app.route("/applet")
+def applet():
+    applet = make_stock_applet()
+    template = jinja2.Template(open("stocks.html").read())
+    return template.render(
+        app_url = bokeh_url + "/bokeh/jsgenerate/VBox/StockApp/StockApp",
+        app_tag = applet.tag
+    )
 
 if __name__ == "__main__":
     import logging
     logging.basicConfig(level=logging.DEBUG)
+    print("\nView this example at: %s\n" % applet_url)
     app.debug = True
     app.run(host='0.0.0.0', port=5050)
