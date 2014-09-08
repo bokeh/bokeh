79d7b17ea02a2f769c7b8b8eb77d6bf80251d9be
damianavila <damianavila@gmail.com>
2014-09-07 21:44:40 -0300

    Deactivated all the AR testing.

--- bokeh/tests/test_ar_downsample.py
+++ bokeh/tests/test_ar_downsample.py
@@ -7,18 +7,18 @@ from .test_utils import skipIfPy3
 from ..utils import is_py3
 
 # Only import in python 2...
-try:
+#try:
     #import abstract_rendering.numeric as numeric
-    import abstract_rendering.categories as categories
-    import abstract_rendering.contour as contour
-    import abstract_rendering.general as general
-    import abstract_rendering.glyphset as glyphset
-    import abstract_rendering.core as ar
-    import abstract_rendering.numpyglyphs as npg
-    import abstract_rendering.infos as infos
-except:
-    if not is_py3():
-        raise
+    #import abstract_rendering.categories as categories
+    #import abstract_rendering.contour as contour
+    #import abstract_rendering.general as general
+    #import abstract_rendering.glyphset as glyphset
+    #import abstract_rendering.core as ar
+    #import abstract_rendering.numpyglyphs as npg
+    #import abstract_rendering.infos as infos
+#except:
+    #if not is_py3():
+        #raise
 
 
 def sort_init_first(_, a, b):
@@ -46,7 +46,7 @@ class _FailsProxyReify(object):
     def reify(self):
         raise NotImplementedError
 
-
+@unittest.skip
 @skipIfPy3("AR does not run in python 3")
 class Test_AR(unittest.TestCase):
     # -------------- Process and Utility Tests ----------
@@ -189,7 +189,6 @@ class Test_AR(unittest.TestCase):
         for (proxy, target, kwargs) in configs:
             self._reify_tester(proxy, target, kwargs)
 
-    @unittest.skip
     def test_aggregators(self):
         aggregators = [(ar_downsample.Sum(), numeric.Sum, {}),
                        (ar_downsample.Count(), numeric.Count, {}),
@@ -202,7 +201,7 @@ class Test_AR(unittest.TestCase):
     def test_reify_tester(self):
         self.assertRaises(NotImplementedError, self._reify_tester, *(_FailsProxyReify(), object, {}))
 
-    @unittest.skip
+
     def test_shaders(self):
         shaders = [(ar_downsample.BinarySegment(low=1, high=2, divider=10), numeric.BinarySegment, {}),
                    (ar_downsample.Contour(), contour.Contour, {}),
