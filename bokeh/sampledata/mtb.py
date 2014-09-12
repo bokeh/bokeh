7992b5b2bb1e65ef0e9b7436e3ed78e81b8be39d
Mateusz Paprocki <mattpap@gmail.com>
2014-09-12 16:45:47 +0200

    Add glyphs/trail example (altitude profile + map)

---
+++ bokeh/sampledata/mtb.py
@@ -0,0 +1,8 @@
+from os.path import dirname, join
+
+try:
+    import pandas as pd
+except ImportError as e:
+    raise RuntimeError("mtb data requires pandas (http://pandas.pydata.org) to be installed")
+
+obiszow_mtb_xcm = pd.read_csv(join(dirname(__file__), 'obiszow_mtb_xcm.csv'))
