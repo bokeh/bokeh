a595a168f447513b2ecd3e44fae04e472301fb48
Bryan Van de Ven <bryanv@continuum.io>
2014-09-19 17:05:42 -0500

    some prelim docs for bbauth.py api

--- bokeh/server/views/bbauth.py
+++ bokeh/server/views/bbauth.py
@@ -29,29 +29,58 @@ def check_write_authentication_and_create_client(func):
 
 @bokeh_app.route('/bokeh/login', methods=['GET'])
 def login_get():
+    ''' Log in a user from a form.
+
+    :status 200: render login view
+
+    '''
     return bokeh_app.authentication.login_get()
 
 @bokeh_app.route('/bokeh/login', methods=['POST'])
 def login_post():
+    ''' Log in user from a submission.
+
+    :status 200: if API flag set, log in status
+    :status 302: if API flag not set, redirect to index on
+        success, to login on failue
+
+    '''
     return bokeh_app.authentication.login_post()
 
 @bokeh_app.route('/bokeh/loginfromapikey', methods=['GET'])
 def login_from_apikey():
+    ''' Log in a user from an API key.
+
+    :status 302: redirect to index on success, to login on failure
+
+    '''
     return bokeh_app.authentication.login_from_apikey()
 
 @bokeh_app.route('/bokeh/register', methods=['GET'])
 def register_get():
+    ''' Register a new user via a view.
+
+    :status 200: render registration form
+
+    '''
     return bokeh_app.authentication.register_get()
 
 @bokeh_app.route('/bokeh/register', methods=['POST'])
 def register_post():
-    result = bokeh_app.authentication.register_post()
-    return result
+    ''' Register a new user via a submission.
 
+    :status 200: registration result
+
+    '''
+    return bokeh_app.authentication.register_post()
 
 @bokeh_app.route('/bokeh/logout')
 def logout():
-    result = bokeh_app.authentication.logout()
-    return result
+    ''' Log out the current user.
+
+    :status 302: redirect to index
+
+    '''
+    return bokeh_app.authentication.logout()
 
 
