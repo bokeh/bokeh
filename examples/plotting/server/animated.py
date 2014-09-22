d301205cbbc41f5c01d5d0f606e0059350d8200a
Mateusz Paprocki <mattpap@gmail.com>
2014-09-22 13:42:55 +0200

    Remove useless code from animated examples

--- examples/plotting/server/animated.py
+++ examples/plotting/server/animated.py
@@ -35,13 +35,13 @@ annular_wedge(
 renderer = [r for r in curplot().renderers if isinstance(r, Glyph)][0]
 ds = renderer.data_source
 show()
+
 while True:
-    for i in linspace(-2*pi, 2*pi, 50):
-        rmin = ds.data["inner_radius"]
-        rmin = roll(rmin, 1)
-        ds.data["inner_radius"] = rmin
-        rmax = ds.data["outer_radius"]
-        rmax = roll(rmax, -1)
-        ds.data["outer_radius"] = rmax
-        cursession().store_objects(ds)
-        time.sleep(.10)
+    rmin = ds.data["inner_radius"]
+    rmin = roll(rmin, 1)
+    ds.data["inner_radius"] = rmin
+    rmax = ds.data["outer_radius"]
+    rmax = roll(rmax, -1)
+    ds.data["outer_radius"] = rmax
+    cursession().store_objects(ds)
+    time.sleep(.10)
