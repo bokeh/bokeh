88d06e22535351fc88cd13306e68b4d4d8b25e44
Mateusz Paprocki <mattpap@gmail.com>
2014-10-18 18:34:42 +0200

    Move explicit imports from objects to bokeh.models

--- bokeh/models/__init__.py
+++ bokeh/models/__init__.py
@@ -1,1 +1,15 @@
 from __future__ import absolute_import
+
+from .axes import *
+from .canvas import *
+from .formatters import *
+from .grids import *
+from .map_plots import *
+from .mappers import *
+from .plots import *
+from .ranges import *
+from .renderers import *
+from .sources import *
+from .tickers import *
+from .tools import *
+from .widget import *
--- bokeh/objects.py
+++ bokeh/objects.py
@@ -1,15 +1,3 @@
 from __future__ import absolute_import
 
-from .models.axes import *
-from .models.canvas import *
-from .models.formatters import *
-from .models.grids import *
-from .models.map_plots import *
-from .models.mappers import *
-from .models.plots import *
-from .models.ranges import *
-from .models.renderers import *
-from .models.sources import *
-from .models.tickers import *
-from .models.tools import *
-from .models.widget import *
+from .models import *
