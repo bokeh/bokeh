c65c5a4473f43ffa745ad0efe72a937c3c9bb8b2
hugo <hugo>
2014-11-02 07:44:03 -0500

    removing gevent

--- bokeh/server/start.py
+++ bokeh/server/start.py
@@ -35,33 +35,7 @@ from . import services
 from .app import bokeh_app
 from .models import user
 
-try:
-    from gevent.pywsgi import WSGIServer, WSGIHandler
-except ImportError:
-    log.info("no gevent - your websockets won't work")
-    from wsgiref.simple_server import make_server
-else:
-    class BokehWSGIServer(WSGIServer):
-        logger = log
-
-    class BokehWSGIHandler(WSGIHandler):
-        _http_suffix = re.compile("\s*HTTP/\d+\.\d+$")
-
-        def format_request(self):
-            request = self._http_suffix.sub("", getattr(self, 'requestline', '-'))
-            status = getattr(self, 'status', '-')
-            length = self.response_length or '-'
-            time = "%.1f%s" % scale_delta(self.time_finish - self.time_start) if self.time_finish else '-'
-            return '%s %s %s %s' % (request, status, length, time)
-
-        def log_request(self):
-            log = self.server.log
-            if log:
-                log.debug(self.format_request() + '\n')
-
-    def make_server(host, port, app):
-        http_server = BokehWSGIServer((host, port), app, handler_class=BokehWSGIHandler, log=log)
-        return http_server
+from wsgiref.simple_server import make_server
 
 PORT = 5006
 REDIS_PORT = 6379
