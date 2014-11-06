96f9819ab67b48135a61c8a1e15bc808cf82d194
Mateusz Paprocki <mattpap@gmail.com>
2014-11-06 13:50:26 +0100

    Implement display protocol for Widget (_repr_html_)
    This effectively allows us to automatically display plots and widgets.

--- bokeh/models/widget.py
+++ bokeh/models/widget.py
@@ -2,6 +2,15 @@ from __future__ import absolute_import
 
 from ..plot_object import PlotObject
 from ..properties import Bool
+from ..embed import notebook_div
 
 class Widget(PlotObject):
     disabled = Bool(False)
+
+    def _repr_html_(self):
+        return notebook_div(self)
+
+    @property
+    def html(self):
+        from IPython.core.display import HTML
+        return HTML(self._repr_html_())
