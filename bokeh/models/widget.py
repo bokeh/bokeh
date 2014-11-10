772fd10ff6a36750c4dd91bc272a4aebc7f07639
Mateusz Paprocki <mattpap@gmail.com>
2014-11-10 23:21:13 +0100

    Disable Widget._repr_html_ for now because it breaks notebook examples

--- bokeh/models/widget.py
+++ bokeh/models/widget.py
@@ -7,10 +7,12 @@ from ..embed import notebook_div
 class Widget(PlotObject):
     disabled = Bool(False)
 
-    def _repr_html_(self):
+    # XXX: Not yet, because it breaks plotting/notebook examples.
+    # Rename to _repr_html_ if we decide to enable this by default.
+    def __repr_html__(self):
         return notebook_div(self)
 
     @property
     def html(self):
         from IPython.core.display import HTML
-        return HTML(self._repr_html_())
+        return HTML(self.__repr_html__())
