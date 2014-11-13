55a4acd4c5e9de70accc3ee44862fe8a2389255f
Bryan Van de Ven <bryanv@continuum.io>
2014-11-13 15:38:01 -0600

    working Figure subclass of Plot

--- examples/plotting/file/categorical.py
+++ examples/plotting/file/categorical.py
@@ -10,20 +10,25 @@ x =  [50, 40, 65, 10, 25, 37, 80, 60]
 
 output_file("categorical.html", title="categorical.py example")
 
+figure(
+    title="Dot Plot", tools="resize,previewsave",
+    y_range=factors, x_range=[0,100])
+
 hold()
 
-segment(x0, factors, x, factors, y_range=factors, x_range=[0,100],
-        line_width=2, line_color="green", tools="resize,previewsave", title="Dot Plot")
-circle(x, factors, size=15, fill_color="orange", line_color="green", line_width=3, Name="categorical example")
+segment(x0, factors, x, factors, line_width=2, line_color="green", )
+circle(x, factors, size=15, fill_color="orange", line_color="green", line_width=3, )
 
-figure()
 
 factors = ["foo", "bar", "baz"]
 x = ["foo", "foo", "foo", "bar", "bar", "bar", "baz", "baz", "baz"]
 y = ["foo", "bar", "baz", "foo", "bar", "baz", "foo", "bar", "baz"]
 colors = ["#0B486B", "#79BD9A", "#CFF09E", "#79BD9A", "#0B486B", "#79BD9A", "#CFF09E", "#79BD9A", "#0B486B"]
 
-rect(x, y, color=colors, x_range=factors, y_range=factors,
-    width=1, height=1, tools="resize, hover,previewsave", title="Categorical Heatmap")
+figure(
+    title="Categorical Heatmap", tools="resize, hover,previewsave",
+    x_range=factors, y_range=factors)
+
+rect(x, y, color=colors, width=1, height=1)
 
 show()  # open a browser
