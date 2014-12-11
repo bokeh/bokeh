c3fc0a7f0c3115c65e28965e59018ea7b82f46a2
damianavila <damianavila@gmail.com>
2014-12-10 23:37:41 -0300

    Get version ealier to avoid unsuccessful build.

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -5,7 +5,7 @@ if [ "$1" == "-h" ]; then
 
     where:
         -h     show this help text
-        --tag X.X.X  the tag string
+        --tag X.X.X-devel the tag string
     "
     echo "$usage"
     exit 0
@@ -16,64 +16,64 @@ elif [ "$1" == "--tag" ]; then
 fi
 
 # create a new branch from master
-git checkout -b builds/devel task/devel_build #remove task/devel build whe hit master
+git checkout -b builds/devel task/devel_build #remove task/devel_build
 
 # tag the branch
 git tag -a $tag -m 'devel'
 
-### build for each python version
-##for py in 27 33 34;
-##do
-##    echo "Building py$py pkg"
-##    CONDA_PY=$py conda build conda.recipe --quiet
-##done
+# get version number
+version=`python scripts/get_bump_version.py`
 
-### get conda info about root_prefix and platform
-##function conda_info {
-##    conda info --json | python -c "import json, sys; print(json.load(sys.stdin)['$1'])"
-##}
+# exit if there is no new tag
+if [ "$version" == "You need to tag before building." ]; then
+    echo You need to tag before building.
+    exit 0
 
-##CONDA_ENV=$(conda_info root_prefix)
-##PLATFORM=$(conda_info platform)
-##BUILD_PATH=$CONDA_ENV/conda-bld/$PLATFORM
+# build for each python version
+for py in 27 33 34;
+do
+    echo "Building py$py pkg"
+    CONDA_PY=$py conda build conda.recipe --quiet
+done
 
-### get version number
-##version=`python scripts/get_bump_version.py`
+# get conda info about root_prefix and platform
+function conda_info {
+    conda info --json | python -c "import json, sys; print(json.load(sys.stdin)['$1'])"
+}
 
-### exit if there is no new tag
-##if [ "$version" == "You need to tag before building." ]; then
-##    echo You need to tag before building.
-##    exit 1
+CONDA_ENV=$(conda_info root_prefix)
+PLATFORM=$(conda_info platform)
+BUILD_PATH=$CONDA_ENV/conda-bld/$PLATFORM
 
-### convert to platform-specific builds
-##conda convert -p all -f $BUILD_PATH/bokeh*$version*.tar.bz2;
+# convert to platform-specific builds
+conda convert -p all -f $BUILD_PATH/bokeh*$version*.tar.bz2;
 
-###upload conda pkgs to binstar
-##array=(osx-64 linux-64 win-64 linux-32 win-32)
-##for i in "${array[@]}"
-##do
-##    echo Uploading: $i;
-##    binstar upload -u bokeh $i/bokeh*$version*.tar.bz2 -c dev --force;
-##done
+#upload conda pkgs to binstar
+array=(osx-64 linux-64 win-64 linux-32 win-32)
+for i in "${array[@]}"
+do
+    echo Uploading: $i;
+    binstar upload -u bokeh $i/bokeh*$version*.tar.bz2 -c dev --force;
+done
 
-###create and upload pypi pkgs to binstar
-###zip is currently not working
+#create and upload pypi pkgs to binstar
+#zip is currently not working
 
-##BOKEH_DEV_VERSION=$version python setup.py sdist --formats=gztar
-##binstar upload -u bokeh dist/bokeh*$version* --package-type pypi -c dev --force;
+BOKEH_DEV_VERSION=$version python setup.py sdist --formats=gztar
+binstar upload -u bokeh dist/bokeh*$version* --package-type pypi -c dev --force;
 
-##echo "I'm done uploading"
+echo "I'm done uploading"
 
-#clean up
-
-#clean up
-touch a.txt
-git add .
-git commit -m "Added a.txt"
+#general clean up
 
 # delete the tag
 git tag -d $tag
 
+# add all the generated files
+git add .
+git commit -m "Add all files."
+
+# add all the generated files
 git checkout -- .
 git checkout task/devel_build #change to master
 git branch -D builds/devel
