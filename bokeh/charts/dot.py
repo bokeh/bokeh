9334fd03cc9c2a1e6056fcca824de258a186e9df
Fabio Pliger <fabio.pliger@gmail.com>
2014-12-13 14:48:56 +0100

    improve module docstrings and code

--- bokeh/charts/dot.py
+++ bokeh/charts/dot.py
@@ -3,7 +3,6 @@ complex plot is a simple way.
 
 This is the Dot class which lets you build your Dot charts just
 passing the arguments to the Chart class and calling the proper functions.
-It also add detection of the incomming input to see if it is a pandas dataframe.
 """
 #-----------------------------------------------------------------------------
 # Copyright (c) 2012 - 2014, Continuum Analytics, Inc. All rights reserved.
@@ -23,27 +22,27 @@ try:
 except ImportError:
     pd = None
 
-from .bar import Bar
+from ._chartobject import ChartObject, DataAdapter
+from ..models import ColumnDataSource, FactorRange, Range1d
+
 #-----------------------------------------------------------------------------
 # Classes and functions
 #-----------------------------------------------------------------------------
-
-
-class Dot(Bar):
-    """This is the Dot class and it is in charge of plotting
-    Dot chart (grouped and stacked) in an easy and intuitive way.
+class Dot(ChartObject):
+    """This is the Dot class and it is in charge of plotting Dot chart
+     in an easy and intuitive way.
 
     Essentially, it provides a way to ingest the data, make the proper
     calculations and push the references into a source object.
     We additionally make calculations for the ranges.
-    And finally add the needed glyphs (rects) taking the references
-    from the source.
+    And finally add the needed glyphs (segments and circles) taking
+    the references from the source.
 
     """
     # disable x grid
     xgrid=False
 
-    def __init__(self, values, cat=None, show_segment=False,
+    def __init__(self, values, cat=None, show_segment=True,
                  title=None, xlabel=None, ylabel=None, legend=False,
                  xscale="categorical", yscale="linear", width=800, height=600,
                  tools=True, filename=False, server=False, notebook=False,
@@ -52,43 +51,45 @@ class Dot(Bar):
         Args:
             values (dict): a dict containing the data with names as a key
                 and the data as a value.
-            cat (list or bool, optional): list of string representing the categories.
+            cat (list or bool, optional): list of string representing the
+                categories.
                 Defaults to None.
-            stacked (bool, optional): to see the bars stacked or grouped.
-                Defaults to False, so grouping is assumed.
-            title (str, optional): the title of your plot. Defaults to None.
-            xlabel (str, optional): the x-axis label of your plot.
+            show_segment (bool, optional): shows a segment from x label to
+                the rendered dot.
+            title (str, optional): the title of your chart. Defaults to None.
+            xlabel (str, optional): the x-axis label of your chart.
                 Defaults to None.
-            ylabel (str, optional): the y-axis label of your plot.
+            ylabel (str, optional): the y-axis label of your chart.
                 Defaults to None.
-            legend (str, optional): the legend of your plot. The legend content is
-                inferred from incoming input.It can be ``top_left``,
-                ``top_right``, ``bottom_left``, ``bottom_right``.
-                It is ``top_right`` is you set it as True.
-                Defaults to None.
-            xscale (str, optional): the x-axis type scale of your plot. It can be
-                ``linear``, ``datetime`` or ``categorical``.
+            legend (str, optional): the legend of your chart. The legend
+                content is inferred from incoming input.It can be
+                ``top_left``, ``top_right``, ``bottom_left``, ``bottom_right``.
+                It is ``top_right`` is you set it as True. Defaults to None.
+            xscale (str, optional): the x-axis type scale of your chart.
+                It can be ``linear``, ``datetime`` or ``categorical``.
+                Defaults to ``datetime``.
+            yscale (str, optional): the y-axis type scale of your chart.
+                It can be ``linear``, ``datetime`` or ``categorical``.
                 Defaults to ``linear``.
-            yscale (str, optional): the y-axis type scale of your plot. It can be
-                ``linear``, ``datetime`` or ``categorical``.
-                Defaults to ``linear``.
-            width (int, optional): the width of your plot in pixels.
+            width (int, optional): the width of your chart in pixels.
                 Defaults to 800.
-            height (int, optional): the height of you plot in pixels.
+            height (int, optional): the height of you chart in pixels.
                 Defaults to 600.
-            tools (bool, optional): to enable or disable the tools in your plot.
-                Defaults to True
-            filename (str or bool, optional): the name of the file where your plot.
-                will be written. If you pass True to this argument, it will use
-                ``untitled`` as a filename.
+            tools (bool, optional): to enable or disable the tools in your
+                chart. Defaults to True
+            filename (str or bool, optional): the name of the file where
+                your chart. will be written. If you pass True to this
+                argument, it will use ``untitled`` as a filename.
                 Defaults to False.
-            server (str or bool, optional): the name of your plot in the server.
-                If you pass True to this argument, it will use ``untitled``
-                as the name in the server.
+            server (str or bool, optional): the name of your chart in
+                the server. If you pass True to this argument, it will
+                use ``untitled`` as the name in the server.
                 Defaults to False.
-            notebook (bool or optional):if you want to output (or not) your plot into the
-                IPython notebook.
+            notebook (bool, optional):if you want to output (or not)
+                your chart into the IPython notebook.
                 Defaults to False.
+            facet (bool, optional): generate multiple areas on multiple
+                separate charts for each series if True. Defaults to False
 
         Attributes:
             source (obj): datasource object for your plot,
@@ -107,20 +108,25 @@ class Dot(Bar):
                 Needed for _set_And_get method.
         """
         self.show_segment = show_segment
-        super(Dot, self).__init__(
-            values, cat, False, # stacked is always false
-            title, xlabel, ylabel, legend,
-            xscale, yscale, width, height,
-            tools, filename, server, notebook, facet)
+        self.cat = cat
+        self.values = values
+        self.source = None
+        self.xdr = None
+        self.ydr = None
+        self.groups = []
+        self.data = dict()
+        self.attr = []
+        super(Dot, self).__init__(title, xlabel, ylabel, legend,
+                                  xscale, yscale, width, height,
+                                  tools, filename, server, notebook, facet)
 
 
     def draw(self):
         """Use the rect glyphs to display the bars.
 
-        Takes reference points from data loaded at the source   .
-
-        Args:
-            stacked (bool): whether to stack the bars in your plot.
+        Takes reference points from data loaded at the source and
+        renders circle glyphs (and segments) on the related
+        coordinates.
         """
         self.tuples = list(self._chunker(self.attr, 4))
         colors = self._set_colors(self.tuples)
@@ -129,16 +135,15 @@ class Dot(Bar):
         for i, quartet in enumerate(self.tuples):
             # draw segment first so when scatter will be place on top of it
             # and it won't show segment chunk on top of the circle
-            self.chart.make_segment(
-                self.source, quartet[1], quartet[2],
-                quartet[1], quartet[3], 'black', 2,
-            )
+            if self.show_segment:
+                self.chart.make_segment(
+                    self.source, quartet[1], quartet[2],
+                    quartet[1], quartet[3], 'black', 2,
+                )
 
             self.chart.make_scatter(
                 self.source, quartet[1], quartet[0], 'circle',
-                colors[i - 1], line_color='black',
-                size=15,
-                fill_alpha=1.,
+                colors[i - 1], line_color='black', size=15, fill_alpha=1.,
             )
 
             if i < len(self.tuples):
@@ -154,13 +159,9 @@ class Dot(Bar):
         the rect glyph inside the ``draw`` method.
 
         """
-        # TODO: Clean useless code inherited from bars...
-        self.zero = np.zeros(len(self.cat))
-        self.data = dict(cat=self.cat, zero=self.zero)
-
+        self.data = dict(cat=self.cat, zero=np.zeros(len(self.cat)))
         # list to save all the attributes we are going to create
         self.attr = []
-
         # list to save all the groups available in the incomming input
         # Grouping
         self.groups.extend(self.values.keys())
@@ -169,14 +170,50 @@ class Dot(Bar):
         for i, val in enumerate(self.values.keys()):
             values = self.values[val]
             # original y value
-            self.set_and_get("", val, self.values[val])
+            self.set_and_get("", val, values)
             # x value
-            self.set_and_get("cat", val, [c + ":" + str(step[i + 1]) for c in self.cat])
+            cats = [c + ":" + str(step[i + 1]) for c in self.cat]
+            self.set_and_get("cat", val, cats)
             # zeros
-            self.set_and_get("z_", val, np.zeros(len(self.values[val])))
+            self.set_and_get("z_", val, np.zeros(len(values)))
             # segment top y value
-            self.set_and_get("seg_top_", val, self.values[val])
+            self.set_and_get("seg_top_", val, values)
+
+    def get_source(self):
+        """Push the Dot data into the ColumnDataSource and calculate
+        the proper ranges.
+        """
+        self.source = ColumnDataSource(self.data)
+        self.xdr = FactorRange(factors=self.source.data["cat"])
+        cat = [i for i in self.attr if not i.startswith(("cat",))]
+        end = 1.1 * max(max(self.data[i]) for i in cat)
+        self.ydr = Range1d(start=0, end=end)
+
+    def _setup_show(self):
+        """
+        Prepare context before main show method is invoked
+        """
+        super(Dot, self)._setup_show()
+
+        # normalize input to the common DataAdapter Interface
+        if not isinstance(self.values, DataAdapter):
+            self.values = DataAdapter(self.values, force_alias=False)
+
+        if not self.cat:
+            self.cat = [str(x) for x in self.values.index]
 
     def _make_legend_glyph(self, source_legend, color):
+        '''Create a new glyph to represent one of the chart data series with the
+        specified color
+
+        The glyph is added to chart.glyphs.
+
+        NOTE: Overwrites default ChartObject in order to dray a circle glyph
+        on the legend instead of the default `rect`
+
+        Args:
+            source_legend (ColumnDataSource): source to be used when creating the glyph
+            color (str): color of the glyph
+        '''
         self.chart.make_scatter(source_legend, "groups", None, 'circle',
                                  color, "black", fill_alpha=1.)
