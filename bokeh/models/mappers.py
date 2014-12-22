fc740373b772d46c8a0a3bf2cacef503df7e69ad
Mateusz Paprocki <mattpap@gmail.com>
2014-12-22 10:48:12 +0100

    Remove useless code from LinearColorMapper

--- bokeh/models/mappers.py
+++ bokeh/models/mappers.py
@@ -15,6 +15,7 @@ class LinearColorMapper(ColorMapper):
 
     # TODO (bev) use Array property
     palette = Any # Array
+
     low = Float
     high = Float
 
@@ -35,26 +36,3 @@ class LinearColorMapper(ColorMapper):
             kwargs['palette'] = np.array(pal)
 
         super(LinearColorMapper, self).__init__(**kwargs)
-
-    def map_from_index(self, indices):
-        return self.palette[np.array(indices)]
-
-    def map_from_value(self, values):
-        x = np.array(values)
-
-        if self.low: low = self.low
-        else: low = min(values)
-
-        if self.high: high = self.high
-        else: high = max(values)
-
-        N = len(self.palette)
-        scale = N/float(high-low)
-        offset = -scale*low
-
-        indices = np.floor(x*scale+offset).astype('int')
-        indices[indices==len(self.palette)] -= 1
-        return self.palette[indices]
-
-    def reverse(self):
-        self.palette = self.palette[::-1]
