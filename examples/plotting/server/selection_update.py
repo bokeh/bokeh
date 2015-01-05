e8cc808fa3882951720714e4a33a47962cf0aa8a
Bryan Van de Ven <bryanv@continuum.io>
2015-01-05 13:56:25 -0600

    typos, support histogram of unselected

--- examples/plotting/server/selection_update.py
+++ examples/plotting/server/selection_update.py
@@ -10,6 +10,7 @@ N = 1000
 
 x = np.random.normal(size=N) * 100
 y = np.random.normal(size=N) * 100
+all_inds = np.arange(len(x))
 
 output_server("selection_update")
 
@@ -27,32 +28,36 @@ box_select_tool.select_every_mousemove = False
 lasso_select_tool = p.select(dict(type=LassoSelectTool))
 lasso_select_tool.select_every_mousemove = False
 
-# create the horizontal historgram
+# create the horizontal histogram
 hhist, hedges = np.histogram(x, bins=20)
 hzeros = [0]*(len(hedges)-1)
+hmax = max(hhist)*1.1
 
-ph = figure(toolbar_location=None, plot_width=p.plot_width, plot_height=200, x_range=p.x_range, title=None, min_border=10, min_border_left=50)
+ph = figure(toolbar_location=None, plot_width=p.plot_width, plot_height=200, x_range=p.x_range, y_range=(-hmax, hmax), title=None, min_border=10, min_border_left=50)
 ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hhist, color="white", line_color="#3A5785")
-ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros, color="#3A5785", alpha=0.6, line_color=None, name="hhist")
+ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros, color="#3A5785", alpha=0.5, line_color=None, name="hhist")
+ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros, color="#3A5785", alpha=0.1, line_color=None, name="hhist2")
 ph.xgrid.grid_line_color = None
 
-renderer = ph.select(dict(name="hhist"))
-ph_source = renderer[0].data_source
+ph_source = ph.select(dict(name="hhist"))[0].data_source
+ph_source2 = ph.select(dict(name="hhist2"))[0].data_source
 
-# create the vertical historgram
+# create the vertical histogram
 vhist, vedges = np.histogram(y, bins=20)
 vzeros = [0]*(len(vedges)-1)
+vmax = max(vhist)*1.1
 
 # need to adjust for toolbar height, unfortunately
 th = 42
 
-pv = figure(toolbar_location=None, plot_width=200, plot_height=p.plot_height+th-10, y_range=p.y_range, title=None, min_border=10, min_border_top=th)
+pv = figure(toolbar_location=None, plot_width=200, plot_height=p.plot_height+th-10, x_range=(-vmax, vmax), y_range=p.y_range, title=None, min_border=10, min_border_top=th)
 pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vhist, color="white", line_color="#3A5785")
-pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, color="#3A5785", alpha=0.6, line_color=None, name="vhist")
+pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, color="#3A5785", alpha=0.5, line_color=None, name="vhist")
+pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, color="#3A5785", alpha=0.1, line_color=None, name="vhist2")
 pv.ygrid.grid_line_color = None
 
-renderer = pv.select(dict(name="vhist"))
-pv_source = renderer[0].data_source
+pv_source = pv.select(dict(name="vhist"))[0].data_source
+pv_source2 = pv.select(dict(name="vhist2"))[0].data_source
 
 # set up callbacks
 def on_selection_change(obj, attr, old, new):
@@ -63,11 +68,17 @@ def on_selection_change(obj, attr, old, new):
     else:
         hhist, _ = np.histogram(x[inds], bins=hedges)
         vhist, _ = np.histogram(y[inds], bins=vedges)
+        negative_inds = np.ones_like(x, dtype=np.bool)
+        negative_inds[inds] = False
+        hhist2, _ = np.histogram(x[negative_inds], bins=hedges)
+        vhist2, _ = np.histogram(y[negative_inds], bins=vedges)
 
     ph_source.data["top"] = hhist
     pv_source.data["right"] = vhist
+    ph_source2.data["top"] = -hhist2
+    pv_source2.data["right"] = -vhist2
 
-    cursession().store_objects(ph_source, pv_source)
+    cursession().store_objects(ph_source, pv_source, ph_source2, pv_source2)
 
 scatter_ds.on_change('selected', on_selection_change)
 
