95a5fe89b9174036ca0c197e6d7f208cf75f592b
damianavila <damianavila@gmail.com>
2015-01-19 16:57:10 -0300

    Use hours instead of seconds to have the same version for all the built packages and because we are not uploading more than one per hours because the total time to upload a devel build is about one hour.

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -10,7 +10,8 @@ pushd $SRC_DIR
 
 version=`$PYTHON scripts/get_bump_version.py`
 
-date=`date "+%s"`
+seconds=`date "+%s"`
+date=$((seconds / 3600))
 echo $version.dev.$date > __conda_version__.txt
 
 cp __conda_version__.txt $BLD_DIR
