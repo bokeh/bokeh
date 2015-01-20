a8ef4c1d4a70a33ac60a4ac3918222c69255e5c9
damianavila <damianavila@gmail.com>
2015-01-20 09:10:01 -0300

    Modify curl to hit the new container structure. Defaults to release container.

--- scripts/upload_js_css.sh
+++ scripts/upload_js_css.sh
@@ -5,14 +5,17 @@ if [ "$1" == "-h" ]; then
     where:
         -h     show this help text
 
-        -t     the tag in the form X.X.X
-        -s     the subdirectory to upload: release or dev
+        -t     the tag in the form X.X.X or X.X.X.dev.xxxxxx for dev builds
+        -s     the subdirectory to upload: release or dev, defaults release
         -u     RackSpace username
         -k     RackSpace APIkey
     "
     echo "$usage"
     exit 0
 fi
+
+#defaults
+subdir=release
 
 #options management
 while getopts t:s:i:u:k: option;
@@ -46,11 +49,12 @@ id=`curl -s -XPOST https://identity.api.rackspacecloud.com/v2.0/tokens \
 -H"Content-type:application/json" | python -c 'import sys,json;data=json.loads(sys.stdin.read());print(data["access"]["serviceCatalog"][-1]["endpoints"][0]["tenantId"])'`
 
 #push the js and css files
-curl -XPUT -T ../bokehjs/build/js/bokeh.js -v -H "X-Auth-Token:$token" -H "Content-Type: text/plain" \
+curl -XPUT -T ../bokehjs/build/js/bokeh.js -v -H "X-Auth-Token:$token" -H "Content-Type: application/javascript" -H "Origin: https://mycloud.rackspace.com" \
 "https://storage101.dfw1.clouddrive.com/v1/$id/bokeh/bokeh/$subdir/bokeh.$tag.js";
-curl -XPUT -T ../bokehjs/build/js/bokeh.min.js -v -H "X-Auth-Token:$token" -H "Content-Type: text/plain" \
+curl -XPUT -T ../bokehjs/build/js/bokeh.min.js -v -H "X-Auth-Token:$token" -H "Content-Type: application/javascript" -H "Origin: https://mycloud.rackspace.com" \
 "https://storage101.dfw1.clouddrive.com/v1/$id/bokeh/bokeh/$subdir/bokeh.$tag.min.js";
-curl -XPUT -T ../bokehjs/build/css/bokeh.css -v -H "X-Auth-Token:$token" -H "Content-Type: text/plain" \
+curl -XPUT -T ../bokehjs/build/css/bokeh.css -v -H "X-Auth-Token:$token" -H "Content-Type: text/css" -H "Origin: https://mycloud.rackspace.com" \
 "https://storage101.dfw1.clouddrive.com/v1/$id/bokeh/bokeh/$subdir/bokeh.$tag.css";
-curl -XPUT -T ../bokehjs/build/css/bokeh.min.css -v -H "X-Auth-Token:$token" -H "Content-Type: text/plain" \
+curl -XPUT -T ../bokehjs/build/css/bokeh.min.css -v -H "X-Auth-Token:$token" -H "Content-Type: text/css" -H "Origin: https://mycloud.rackspace.com" \
 "https://storage101.dfw1.clouddrive.com/v1/$id/bokeh/bokeh/$subdir/bokeh.$tag.min.css";
+
