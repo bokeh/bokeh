6f3b398c595077a17d9ed9e9c4b32d440f8fadcf
Bryan Van de Ven <bryanv@continuum.io>
2015-01-23 22:09:23 -0600

    alphabetize extensions

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -26,10 +26,10 @@ import sphinx_bootstrap_theme
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.ifconfig',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.graphviz',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.inheritance_diagram',
     'sphinxcontrib.napoleon',
     'sphinxcontrib.autohttp.flask',
