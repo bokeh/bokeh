88dec0bd00e811b12c2fee9c2923c115ab9b7bca
Nick Roth <nlr06886@gmail.com>
2015-01-27 23:11:18 -0600

    Added python3 support for data extraction script. Factored out the zip extraction portion into its own function. Used context managers where possible.

--- examples/app/stock_applet/stock_data.py
+++ examples/app/stock_applet/stock_data.py
@@ -1,42 +1,58 @@
 import os
-import urllib
+from six.moves import urllib
 import zipfile
 
 
-def download_data(url, save_dir, exclude_term):
-    name = os.path.join(save_dir, 'temp.zip')
+def extract_hosted_zip(data_url, save_dir, exclude_term=None):
+    """Downloads, then extracts a zip file."""
 
-    #get the zip file
+    zip_name = os.path.join(save_dir, 'temp.zip')
+
+    # get the zip file
     try:
-        name, hdrs = urllib.urlretrieve(url, name)
+        zip_name, hdrs = urllib.request.urlretrieve(url=data_url, filename=zip_name)
     except IOError, e:
-      print "Can't retrieve %r to %r: %s" % (url, save_dir, e)
+      print "Can't retrieve %r to %r: %s" % (data_url, save_dir, e)
       return
 
-    # open the zip file
+    # extract, then remove temp file
+    extract_zip(zip_name=zip_name, exclude_term=exclude_term)
+    os.unlink(zip_name)
+
+
+def extract_zip(zip_name, exclude_term=None):
+    """Extracts a zip file to its containing directory."""
+
+    zip_dir = os.path.dirname(os.path.abspath(zip_name))
+
     try:
-        z = zipfile.ZipFile(name)
+        with zipfile.ZipFile(zip_name) as z:
+
+            # write each zipped file out if it isn't a directory
+            files = [file for file in z.namelist() if not file.endswith('/')]
+            for file in files:
+
+                # remove any provided extra directory term from zip file
+                if exclude_term:
+                    dest_file = file.replace(exclude_term, '')
+                else:
+                    dest_file = file
+
+                dest_file = os.path.normpath(os.path.join(zip_dir, dest_file))
+                dest_dir = os.path.dirname(dest_file)
+
+                # make directory if it does not exist
+                if not os.path.isdir(dest_dir):
+                    os.makedirs(dest_dir)
+
+                # read file from zip, then write to
+                data = z.read(file)
+                with open(dest_file, 'w') as f:
+                    f.write(data)
+
     except zipfile.error, e:
-        print "Bad zipfile (from %r): %s"%(url, e)
+        print "Bad zipfile (%r): %s"%(zip_name, e)
         return
-
-    # loop over files in list and extract each, if it isn't a directory
-    for n in z.namelist():
-
-        if not n.endswith('/'):
-            rel_file = n.replace(exclude_term, '')
-
-            dest = os.path.normpath(os.path.join(save_dir, rel_file))
-            destdir = os.path.dirname(dest)
-            if not os.path.isdir(destdir):
-                os.makedirs(destdir)
-            data = z.read(n)
-            f = open(dest, 'w')
-            f.write(data)
-            f.close()
-
-    z.close()
-    os.unlink(name)
 
 
 if __name__ == '__main__':
@@ -46,4 +62,4 @@ if __name__ == '__main__':
     zip_file = 'http://quantquote.com/files/quantquote_daily_sp500_83986.zip'
     zip_dir = 'quantquote_daily_sp500_83986/'
 
-    download_data(url=zip_file, save_dir=this_dir, exclude_term=zip_dir)
+    extract_hosted_zip(data_url=zip_file, save_dir=this_dir, exclude_term=zip_dir)
