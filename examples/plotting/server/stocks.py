e10ddd1902dadbd80dd925e7e8ca5db7fee926de
Bryan Van de Ven <bryanv@continuum.io>
2015-01-31 09:50:48 -0600

    update stocks examples to use grid bands

--- examples/plotting/server/stocks.py
+++ examples/plotting/server/stocks.py
@@ -30,13 +30,15 @@ aapl_avg = np.convolve(aapl, window, 'same')
 
 p2 = figure(x_axis_type="datetime")
 
-p2.circle(aapl_dates, aapl, size=4, color='#A6CEE3', legend='close')
-p2.line(aapl_dates, aapl_avg, color='red', legend='avg')
+p2.circle(aapl_dates, aapl, size=4, color='darkgrey', alpha=0.2, legend='close')
+p2.line(aapl_dates, aapl_avg, color='navy', legend='avg')
 
 p2.title = "AAPL One-Month Average"
-p2.grid.grid_line_alpha=0.3
+p2.grid.grid_line_alpha=0
 p2.xaxis.axis_label = 'Date'
 p2.yaxis.axis_label = 'Price'
+p2.ygrid.alt_fill_color="olive"
+p2.ygrid.alt_fill_alpha = 0.1
 
 show(VBox(p1,p2))  # open a browser
 
