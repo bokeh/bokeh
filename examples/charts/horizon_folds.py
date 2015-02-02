9367a1d86eb4d486ed04aa0f70d27255e08511fd
Mathieu Drapeau <mathieu.drapeau@intel.com>
2015-02-02 16:30:28 -0500

    Fixed example filename

--- examples/charts/horizon_folds.py
+++ examples/charts/horizon_folds.py
@@ -10,6 +10,6 @@ xx = np.hstack([-1*x[::-1], x])
 yy = np.hstack([-1*y[::-1], y])
 xyvalues = OrderedDict(x=xx, y=yy, y2=yy, y3=yy, y4=yy, y5=yy)
 
-h = Horizon(xyvalues, index='x', title="test horizon", ylabel='Random', filename="horizon.html")
+h = Horizon(xyvalues, index='x', title="test horizon", ylabel='Random', filename="horizon_folds.html")
 h.show()
 
