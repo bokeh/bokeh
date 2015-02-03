8b850eead6e6b0760669cfbf25ee7a37fae81354
Fabio Pliger <fabio.pliger@gmail.com>
2015-02-03 23:04:47 +0100

    improve builder docstrings

--- bokeh/charts/_builder.py
+++ bokeh/charts/_builder.py
@@ -1,9 +1,8 @@
 """This is the Bokeh charts interface. It gives you a high level API to build
 complex plot is a simple way.
 
-This is the ChartObject class, a minimal prototype class to build more chart
-types on top of it. It provides the mechanisms to support the shared chained
-methods.
+This is the Builder class, a minimal prototype class to build more chart
+types on top of it. 
 """
 #-----------------------------------------------------------------------------
 # Copyright (c) 2012 - 2014, Continuum Analytics, Inc. All rights reserved.
@@ -44,24 +43,62 @@ def create_and_build(builder_class, values, **kws):
 
 
 class Builder(object):
-    """A prototype class to inherit each new chart type.
+    """ A prototype class to inherit each new chart Builder type.
 
-    It provides useful methods to be used by the inherited chart classes,
-    such as:
-        * chained methods
-        * dynamically chart cration attributes, ie. `self.chart`
-        * composition to easily use Chart clase methods
+    It provides useful methods to be used by the inherited builder classes,
+    in order to automate most of the charts creation tasks and leave the
+    core customization to specialized builder classes. In that pattern
+    inherited builders just need to provide:
+
+     - the following methods:
+        * draw: yields the glyphs to be rendered into the plot (and
+            eventually create the self._legends attribute to be used to
+            create the proper legends when builder is called to build
+            the glyphs on a Chart object
+        * get_data(optional): Get the input data and calculates the 'data'
+            attribute to be used to calculate the source data
+        * get_source(optional): Push data into the self.source attribute
+            (of type ColumnDataSource) and build the proper ranges
+            (self.x_range and self.y_range).
+
+    - the following attributes:
+        x_range:
+        y_range:
+        _legends:
+
+
+    so Builder can use it all to draw on a chart when called with the
+    create method.
+
     """
     def __init__(self, values=None, legend=False, palette=None, **kws):
         """Common arguments to be used by all the inherited classes.
 
         Args:
+            values (iterable): iterable 2d representing the data series
+                values matrix.
             legend (str, bool): the legend of your plot. The legend content is
                 inferred from incoming input.It can be ``top_left``,
                 ``top_right``, ``bottom_left``, ``bottom_right``.
                 It is ``top_right`` is you set it as True.
             palette(list, optional): a list containing the colormap as hex values.
 
+
+        Attributes:
+            source (obj): datasource object for your plot,
+                initialized as a dummy None.
+            x_range (obj): x-associated datarange object for you plot,
+                initialized as a dummy None.
+            y_range (obj): y-associated datarange object for you plot,
+                initialized as a dummy None.
+            groups (list): to be filled with the incoming groups of data.
+                Useful for legend construction.
+            data (dict): to be filled with the incoming data and be passed
+                to the ColumnDataSource in each chart inherited class.
+                Needed for _set_And_get method.
+            attr (list): to be filled with the new attributes created after
+                loading the data dict.
+                Needed for _set_And_get method.
         """
         if values is None:
             values = []
@@ -69,6 +106,8 @@ class Builder(object):
         self.values = values
         self._legend = legend
         self._palette = palette or DEFAULT_PALETTE
+        # TODO: No real reason why legends should be *private*, should be
+        # legends
         self._legends = []
         self.data = {}
         self.groups = []
@@ -99,7 +138,8 @@ class Builder(object):
         pass
 
     def get_source(self):
-        """Push data into the ColumnDataSource and build the proper ranges.
+        """Push data into the ColumnDataSource and build the
+        proper ranges.
 
         It has to be implemented by any of the inherited class
         representing each different chart type.
@@ -107,26 +147,25 @@ class Builder(object):
         pass
 
     def draw(self):
-        """Draw the glyphs into the plot.
+        """ Generator that yields the glyphs to be draw on the plot
 
         It has to be implemented by any of the inherited class
         representing each different chart type.
         """
         pass
 
-    def _pre_create(self):
+    def pre_create(self):
         """ Hook method that can be overwritten to inject chart logic to
         be executed before make renderers method"""
 
     def make_renderers(self):
         """
-        Executes chart show core operations:
+        Executes the methods to build the chart in sequence:
 
-         - checks for chain methods
-         - prepare chart data & source
-         - create indexes
-         - create glyphs
-         - draw glyphs
+         - prepare values
+         - get data (creating data)
+         - get source (building source and ranges)
+         - and return created glyphs
         """
         # prepare values to for the specific chart type
         self.prepare_values()
@@ -139,7 +178,7 @@ class Builder(object):
 
 
     def create(self, chart=None):
-        self._pre_create()
+        self.pre_create()
 
         # pass these renderers to the chart and then forget about the chart
         renderers = self.make_renderers()
@@ -148,10 +187,10 @@ class Builder(object):
         # create chart ranges..
         if not chart.x_range:
             chart.x_range = self.x_range
-
         if not chart.y_range:
             chart.y_range = self.y_range
 
+        # create the legends if needed
         if self._legend:
             if self._legend is True:
                 orientation = "top_right"
