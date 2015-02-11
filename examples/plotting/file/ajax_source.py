800929ea8d2216cb21b32393fc57e1e219ba4bf3
hugo <hugo.r.shi@gmail.com>
2015-02-11 10:27:14 -0500

    added animation to ajax data source example

--- examples/plotting/file/ajax_source.py
+++ examples/plotting/file/ajax_source.py
@@ -1,10 +1,13 @@
+import numpy as np
+
 from bokeh.plotting import figure, show, output_file
 from bokeh.models.sources import AjaxDataSource
 from bokeh.models.ranges import Range1d
 
 output_file("ajax_source.html", title="ajax_source.py example")
-source = AjaxDataSource(data_url='http://localhost:5050/data')
-p = figure(x_range=Range1d(start=0, end=5), y_range=Range1d(start=0, end=5))
+source = AjaxDataSource(data_url='http://localhost:5050/data',
+                        polling_interval=1000)
+p = figure()
 p.circle('x', 'y', source=source)
 show(p)
 
@@ -16,7 +19,7 @@ app = Flask(__name__)
 @app.route('/data', methods=['GET', 'OPTIONS'])
 @crossdomain(origin="*", methods=['GET', 'POST'], headers=None)
 def hello_world():
-    return jsonify(x=[1,2,3,4,5], y=[5,4,3,2,1])
+    return jsonify(x=np.random.random(5).tolist(), y=np.random.random(5).tolist())
 
 if __name__ ==  "__main__":
     app.run(port=5050)
