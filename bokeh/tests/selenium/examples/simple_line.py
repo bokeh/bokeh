a75dfb822ea4971c9229e7dc95a6eba5af3be050
Kamil Kwiek (irritum) <kamil.kwiek@continuum.io>
2015-03-12 13:08:40 +0100

    Added push invocation to first selenium example

--- bokeh/tests/selenium/examples/simple_line.py
+++ bokeh/tests/selenium/examples/simple_line.py
@@ -1,4 +1,4 @@
-from bokeh.plotting import figure, output_server, show
+from bokeh.plotting import figure, output_server, show, push
 
 x = [1, 2, 3, 4, 5, 6, 7]
 y = [5, 5, 7, 7, 8, 8, 9]
@@ -7,3 +7,5 @@ output_server("test_document")
 
 p = figure(title="simple line server example")
 p.line(x, y, x_axis_label='x', y_axis_label='y')
+
+push()
