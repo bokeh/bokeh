03ff157c34b2b07ccc1a9702bf78d0170c9c5a02
Almar Klein <almar.klein@gmail.com>
2015-03-21 17:29:45 +0100

    revert import *

--- bokeh/models/__init__.py
+++ bokeh/models/__init__.py
@@ -2,32 +2,19 @@ from __future__ import absolute_import
 
 # This file is excluded from flake8 checking in setup.cfg
 
-from .actions import PlotObject, Action, OpenURL; PlotObject; Action; OpenURL
-from .axes import (Axis, ContinuousAxis, LinearAxis, LogAxis, CategoricalAxis,
-                   DatetimeAxis)
-from .formatters import (TickFormatter, BasicTickFormatter, NumeralTickFormatter,
-                         PrintfTickFormatter, LogTickFormatter,
-                         CategoricalTickFormatter, DatetimeTickFormatter)
-from .glyphs import (Glyph, AnnularWedge, Annulus, Arc, Bezier, Gear, Image,
-                     ImageRGBA, ImageURL, Line, MultiLine, Oval, Patch, Patches,
-                     Quad, Quadratic, Ray, Rect, Segment, Text, Wedge)
-from .grids import Grid
-from .map_plots import GMapOptions, GMapPlot, GeoJSOptions, GeoJSPlot
-from .markers import (Marker, Asterisk, Circle, CircleCross, CircleX, Cross,
-                      Diamond, DiamondCross, InvertedTriangle, Square,
-                      SquareCross, SquareX, Triangle, X)
-from .mappers import ColorMapper, LinearColorMapper
-from .plots import PlotContext, PlotList, Plot, GridPlot
-from .ranges import Range, Range1d, DataRange, DataRange1d, FactorRange
-from .renderers import Renderer, GlyphRenderer, Legend, GuideRenderer
-from .sources import (DataSource, ColumnsRef, ColumnDataSource, RemoteSource,
-                      AjaxDataSource, BlazeDataSource, ServerDataSource)
-from .tickers import (Ticker, AdaptiveTicker, CompositeTicker, SingleIntervalTicker,
-                      DaysTicker, MonthsTicker, YearsTicker, BasicTicker, LogTicker,
-                      CategoricalTicker, DatetimeTicker)
-from .tools import (ToolEvents, Tool, PanTool, WheelZoomTool, PreviewSaveTool,
-                    ResetTool, ResizeTool, TapTool, CrosshairTool, BoxZoomTool,
-                    BoxSelectTool, BoxSelectionOverlay, LassoSelectTool,
-                    PolySelectTool, HoverTool)
-from .widget import Widget
-from .widgets import *  # is already a clean namespace with exactly what we want to import
+from .actions import *
+from .axes import *
+from .formatters import *
+from .glyphs import *
+from .grids import *
+from .map_plots import *
+from .markers import *
+from .mappers import *
+from .plots import *
+from .ranges import *
+from .renderers import *
+from .sources import *
+from .tickers import *
+from .tools import *
+from .widget import *
+from .widgets import *
--- bokeh/models/widgets/__init__.py
+++ bokeh/models/widgets/__init__.py
@@ -2,18 +2,12 @@ from __future__ import absolute_import
 
 # This file is excluded from flake8 checking in setup.cfg
 
-from .buttons import AbstractButton, Button, Toggle, Dropdown
-from .dialogs import Dialog
-from .groups import (AbstractGroup, ButtonGroup, Group, CheckboxGroup,
-                     RadioGroup, CheckboxButtonGroup, RadioButtonGroup)
-from .icons import AbstractIcon, Icon
-from .inputs import (InputWidget, TextInput, AutocompleteInput, Select,
-                     MultiSelect, Slider, DateRangeSlider, DatePicker)
-from .layouts import Layout, BaseBox, HBox, VBox, VBoxForm
-from .markups import Paragraph, PreText
-from .panels import Panel, Tabs
-from .tables import (CellFormatter, CellEditor, StringFormatter, NumberFormatter,
-                     BooleanFormatter, DateFormatter, StringEditor, TextEditor,
-                     SelectEditor, PercentEditor, CheckboxEditor, IntEditor,
-                     NumberEditor, TimeEditor, DateEditor, TableColumn,
-                     TableWidget, DataTable)
+from .buttons import *
+from .dialogs import *
+from .groups import *
+from .icons import *
+from .inputs import *
+from .layouts import *
+from .markups import *
+from .panels import *
+from .tables import *
