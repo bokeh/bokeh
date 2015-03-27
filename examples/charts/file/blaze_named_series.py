590659a33fdb40a1e4028f3885964cecf39d3a84
Fabio Pliger <fabio.pliger@gmail.com>
2015-03-27 16:43:47 +0100

    simplify example

--- examples/charts/file/blaze_named_series.py
+++ examples/charts/file/blaze_named_series.py
@@ -4,92 +4,50 @@ import pandas as pd
 import numpy as np
 from bokeh.sampledata import iris
 from bokeh.charts import * #Line, show, output_file, vplot, hplot, Step, Area
+from bokeh.models.widgets import Panel, Tabs
 
 bbvalues = Data(join(dirname(iris.__file__), 'iris.csv'))
-# columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-# # result = bbvalues[columns]
 output_file("blaze_input.html")
 
 df = pd.DataFrame(np.asarray(bbvalues))
 df['myx'] = range(100, 100+bbvalues.nrows)
 datasource = ColumnDataSource(data=df)
 
-scatter1 = Scatter(
-    datasource, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="Scatter Chart", width=300, height=300,
-    ylabel='Petals', legend="top_left",
-    tools="pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
-)
-scatter2 = Scatter(
-    datasource, y_names = ['sepal_length', 'petal_length'],
-    x_names = ['sepal_width', 'petal_width'],
-    title="Scatter Length X Width", width=300, height=300,
-    ylabel='Petals', legend="top_left",
-    tools="pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
-)
-line = Line(
-    bbvalues, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="Line Chart", width=300, height=300,
-    ylabel='Petals', legend="top_left"
-)
-
-mix = Line(
-    df, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    index = ['myx'], width=1000, height=500,
-    title="Mixed Chart",
-    ylabel='Petals', legend="top_left"
-)
-Scatter(
-    df, y_names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    x_names=['myx'],
-    chart=mix
-)
-
-line2 = Line(
-    datasource, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    x_names = ['myx'],
-    # index = ['myx'],
-    width=400, height=300,
-    title="Line Chart",
-    ylabel='Petals', legend="top_left"
-)
-
-step = Step(
-    datasource, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    x_names = ['myx'],
-    title="Step Chart", width=400, height=300,
-    ylabel='Petals', legend="top_left", x_range=line2.x_range, y_range=line2.y_range,
-)
-
-area = Area(
-    bbvalues, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="Step Chart", width=400, height=300, #stacked=True,
-    ylabel='Petals', legend="top_left"
-)
-bp = BoxPlot(
-    bbvalues, y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="BoxPlot", width=400, height=500,
+pkws = dict(
+    width=400, height=400,
+    tools="pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select",
+    legend="top_left",
     ylabel='Petals'
 )
-hist = Histogram(
-    bbvalues, bins=10,
-    y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="BoxPlot", width=400, height=500,
-    ylabel='Petals'
-)
-hist = Histogram(
-    bbvalues, bins=10,
-    y_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'],
-    title="BoxPlot", width=400, height=500,
-    ylabel='Petals'
-)
+
+ys = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+ys_sh = ['sepal_length', 'petal_length']
+xs_sh = ['sepal_width', 'petal_width']
+
+scatter1 = Scatter(datasource, y_names=ys, title="Scatter Chart", **pkws)
+scatter2 = Scatter(datasource, y_names=ys_sh, x_names=xs_sh,
+                   title="Scatter Length X Width", **pkws)
+line = Line(bbvalues, y_names=ys, title="Line Chart", **pkws)
+mix = Line(df, y_names=ys,index = ['myx'], width=1000, height=500,
+           title="Mixed Chart", ylabel='Petals', legend="top_left")
+Scatter(df, y_names=ys, x_names=['myx'], chart=mix)
+
+line2 = Line(datasource, y_names=ys, x_names=['myx'], **pkws)
+step = Step(datasource, y_names=ys, x_names=['myx'], title="Step Chart",
+            x_range=line2.x_range, y_range=line2.y_range, **pkws)
+
+area = Area(bbvalues, y_names=ys, title="Step Chart", **pkws)
+bp = BoxPlot(bbvalues, title="BoxPlot", y_names=ys, width=400, height=400,)
+hist = Histogram(bbvalues, bins=10, y_names=ys, title="Histogram Chart", width=400, height=400)
 
 show(
     vplot(
+        Tabs(tabs=[
+            Panel(child=hplot(scatter1, scatter2), title="Linked Brushing"),
+            Panel(child=hplot(step, line2), title="Linked Panning"),
+        ]),
         mix,
-        hplot(scatter1, scatter2),
         hplot(bp, hist),
-        hplot(line, line2),
-        hplot(step, area),
+        hplot(line, area),
     )
 )
