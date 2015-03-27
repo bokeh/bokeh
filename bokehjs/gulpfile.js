435697c9e30c6b04b5afc915ef945515befba9d9
Travis Swicegood <development@domain51.com>
2015-03-26 19:55:21 -0700

    Add a develop and install-js task for getting the JS to Python for testing

--- bokehjs/gulpfile.js
+++ bokehjs/gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require("gulp");
+var gutil = require("gulp-util");
+var spawn = require("child_process").spawn;
 var uglify = require("gulp-uglifyjs");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
@@ -26,7 +28,32 @@ gulp.task("minify", ["scripts"], function() {
   return gulp.src("./build/js/bokeh.js")
     .pipe(uglify("bokeh.min.js"))
     .pipe(gulp.dest("./build/js/"))
-})
+});
+
+var serverOutput = function(data) {
+  var prefix = gutil.colors.cyan("setup.py:")
+  data.replace(/\s*$/, "")
+    .split("\n")
+    .forEach(function(line) {
+      gutil.log(prefix + " " + gutil.colors.grey(line))
+    });
+};
+
+gulp.task("install-js", ["minify"], function(cb) {
+  var setup = spawn("python", ["../setup.py", "develop", "--install_js"])
+  setup.stdout.setEncoding("utf8");
+  setup.stdout.on("data", serverOutput)
+  setup.stderr.setEncoding("utf8");
+  setup.stderr.on("data", serverOutput);
+  setup.on("exit", function() {
+    gutil.log("setup.py DONE!");
+    cb();
+  })
+});
+
+gulp.task("develop", ["install-js"], function() {
+  gulp.watch("./src/coffee/**/*.coffee", ["install-js"])
+});
 
 gulp.task("default", ["minify"], function() {
   gulp.watch("./src/coffee/**/*.coffee", ["minify"]);
