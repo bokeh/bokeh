5a7e960856852a09600cbf48a188c0a30491aad8
Kamil Kwiek (irritum) <kamil.kwiek@continuum.io>
2015-04-21 13:41:28 +0200

    Reduced sample test

--- bokeh/tests/selenium/tests/test_sample.py
+++ bokeh/tests/selenium/tests/test_sample.py
@@ -16,7 +16,7 @@ class TestSample(BasicSeleniumTestFixture):
     Sample test - just to check if basic selenium environment works as it is expected.
     """
 
-    #@unittest.skip("Just a simple test - only for internal testing purposes.")
+    @unittest.skip("Just a simple test - only for internal testing purposes.")
     def test_sample(self):
         """Check if we are able to load basic document to bokeh server."""
 
@@ -26,18 +26,4 @@ class TestSample(BasicSeleniumTestFixture):
 
         document_url = self.load_document(doc_name)
 
-        # TODO:
-        # - Put here valid test.
-
         plot = look_for_element(self.driver, "div.bk-canvas-events")
-
-        now = self.actions.move_to_element(plot)
-        now.drag_and_drop_by_offset(plot, 100, 0)
-        now.perform()
-
-        self.driver.save_screenshot(gen_file)
-
-        if not check_if_images_are_the_same(ref_file, gen_file):
-            assert 0, "No expected output from screenshot, according to {0}".format(ref_file)
-
-        #self.unload_document(doc_name)
