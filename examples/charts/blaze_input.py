86cfd93d180d7373d7e2ceb8b16e30e73f8f45cf
Fabio Pliger <fabio.pliger@gmail.com>
2015-04-27 21:03:47 +0200

    disable test from yaml tests file until blaze dependency is fixed

--- examples/charts/blaze_input.py
+++ examples/charts/blaze_input.py
@@ -1,20 +1,14 @@
-from __future__ import print_function
 from os.path import dirname, join
+from blaze import Data
 from bokeh.sampledata import iris
 from bokeh.charts import Line, show, output_file
 
-try:
-    from blaze import Data
-    bbvalues = Data(join(dirname(iris.__file__), 'iris.csv'))
-except ImportError:
-    print ("WARNING!!!! Unable to import blaze, switching to pandas!")
-    import pandas as pd
-    bbvalues = pd.read_csv(join(dirname(iris.__file__), 'iris.csv'))
-
+bbvalues = Data(join(dirname(iris.__file__), 'iris.csv'))
 columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 result = bbvalues[columns]
 output_file("blaze_input.html")
 line = Line(
-    result, title="Line Chart", ylabel='Petals', notebook=True, legend="top_left"
+    result, title="Line Chart",
+    ylabel='Petals', notebook=True, legend="top_left"
 )
 show(line)
