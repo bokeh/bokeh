d0bedcfafe2a02f6d5da8642445cdb4c9b27867c
Luke Barnard <luke.barnard99@gmail.com>
2015-06-11 09:05:20 +0200

    Fixed bug, using log instead of printing

--- bokeh/server/start.py
+++ bokeh/server/start.py
@@ -57,12 +57,12 @@ def start_simple_server(args=None):
     if args is not None and args.https:
         if args.https_certfile and args.https_keyfile:
             server = HTTPServer(tornado_app, ssl_options={"certfile": args.https_certfile, "keyfile": args.https_keyfile})
+            log.info('HTTPS Enabled)
         else:
             server = HTTPServer(tornado_app)
-            print('WARNING: --https-certfile or --https-keyfile are not specified, using http instead')
+            log.warning('WARNING: --https-certfile or --https-keyfile are not specified, using http instead')
     else:
         server = HTTPServer(tornado_app)
-    server = HTTPServer(tornado_app)
     server.listen(server_settings.port, server_settings.ip)
     ioloop.IOLoop.instance().start()
 
