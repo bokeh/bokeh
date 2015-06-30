1285368300ab146f723676bdbae5dd65f211ac53
Sarah Bird <sarah@bonvaya.com>
2015-06-30 23:12:04 +0200

    Fix typo in docstring

--- bokeh/models/tools.py
+++ bokeh/models/tools.py
@@ -192,7 +192,7 @@ class CrosshairTool(Tool):
 
     dimensions = List(Enum(Dimension), default=["width", "height"], help="""
     Which dimensions the crosshair tool is to track. By default, both a
-    vertical and horizontal line will be dran. If only "width" is supplied,
+    vertical and horizontal line will be drawn. If only "width" is supplied,
     only a horizontal line will be drawn. If only "height" is supplied,
     only a vertical line will be drawn.
     """)
