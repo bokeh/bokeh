3b12133ca2562f2cc14281322a9b272b3807b3e4
Almar Klein <almar.klein@gmail.com>
2015-07-20 15:17:38 +0200

    webgl: allow selections also when we have over 65k points

--- examples/plotting/file/scatter10k.py
+++ examples/plotting/file/scatter10k.py
@@ -7,8 +7,10 @@ N = 10000
 x = np.random.normal(0, np.pi, N)
 y = np.sin(x) + np.random.normal(0, 0.2, N)
 
+TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select"
+
 output_file("scatter10k.html", title="scatter 10k points")
 
-p = figure(webgl=True)
-p.scatter(x,y, alpha=0.1)
+p = figure(tools=TOOLS  , webgl=True)
+p.scatter(x,y, alpha=0.1, nonselection_alpha=0.001)
 show(p)
