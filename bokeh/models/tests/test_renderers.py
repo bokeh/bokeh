2f3fee556be1b9db53749b6dc1ef7bdb7994dea7
Jānis Ābele <janis.abele@gmail.com>
2015-07-26 00:22:33 +0200

    Add colon in column label validator unit test

---
+++ bokeh/models/tests/test_renderers.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+import unittest
+
+from mock import patch
+
+from bokeh.models.renderers import GlyphRenderer
+from bokeh.plotting import ColumnDataSource, figure
+from bokeh.validation import check_integrity
+
+
+class TestGlyphRenderer(unittest.TestCase):
+    def test_warning_about_colons_in_column_labels(self):
+        sh = ['0', '1:0']
+        p = figure()
+        p.rect('a', 'b', 1, 1, source=ColumnDataSource(data={'a': sh, 'b': sh}))
+        renderer = p.renderers[-1]
+
+        errors = renderer._check_colon_in_category_label()
+
+        self.assertEqual(errors, [(
+            1003,
+            'COLON_IN_CATEGORY_LABEL',
+            'Category label contains colons',
+            '[field:a] [first_value: 1:0] [field:b] [first_value: 1:0] '
+            '[renderer: '
+            'GlyphRenderer, ViewModel:GlyphRenderer, ref _id: '
+            '%s]' % renderer._id
+        )])
+
+
+if __name__ == '__main__':
+    unittest.main()
