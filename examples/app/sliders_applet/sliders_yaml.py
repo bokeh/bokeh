1df8511c4a10f25a889258a47caf00ee089a9510
Fabio Pliger <fabio.pliger@gmail.com>
2015-08-28 11:38:34 +0200

    simplify example

--- examples/app/sliders_applet/sliders_yaml.py
+++ examples/app/sliders_applet/sliders_yaml.py
@@ -5,16 +5,13 @@ instructions on running.
 """
 from __future__ import print_function
 
-import logging
-import os
-logging.basicConfig(level=logging.DEBUG)
-
+from os.path import dirname, abspath, join
 import numpy as np
 from bokeh.appmaker import bokeh_app
 
 N = 200
 x = np.linspace(0, 4*np.pi, N)
-here = os.path.dirname(os.path.abspath(__file__))
+here = dirname(abspath(__file__))
 
 def compute_y(amplitude=1, offset=0, phase=1, freq=0, **kws):
     return amplitude * np.sin(freq*x + phase) + offset
@@ -29,8 +26,8 @@ def update_data(app):
     source.data = {'x': x, 'y': compute_y(**app._values)}
 
 
-sliders_app = bokeh_app(os.path.join(here, 'sliders.yaml'), route='/sliders', handler=update_data,
-                        theme=os.path.join(here, 'style.yaml'))
-sliders_app.sources['source'].data = {'x': x, 'y': compute_y(**sliders_app._values)}
+sl_app = bokeh_app(join(here, 'sliders.yaml'), route='/sliders',
+                   handler=update_data, theme=join(here, 'style.yaml'))
+sl_app.sources['source'].data = {'x': x, 'y': compute_y(**sl_app._values)}
 
 print ("Access the demo at: http://127.0.0.1:5006/sliders")
