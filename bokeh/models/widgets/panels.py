411aca8c08508cb9b42cbbdcfa76b514e0166a1c
Luke Canavan <canavandl@gmail.com>
2015-09-09 08:09:31 -0700

    remove unused import for flake8

--- bokeh/models/widgets/panels.py
+++ bokeh/models/widgets/panels.py
@@ -3,7 +3,6 @@
 """
 from __future__ import absolute_import
 
-from ...properties import abstract
 from ...properties import Bool, Int, String, Instance, List
 from ..widget import Widget
 from ..callbacks import Callback
