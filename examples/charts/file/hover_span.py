7f8a382c8db18479a79271031aee536721152adc
Nick Roth <nroth@dealnews.com>
2015-09-17 14:30:08 -0500

    Fix hover span example.

--- examples/charts/file/hover_span.py
+++ examples/charts/file/hover_span.py
@@ -13,24 +13,30 @@ xyvalues = pd.melt(xyvalues, id_vars=['Year'],
 
 TOOLS='box_zoom,box_select,hover,crosshair,resize,reset'
 output_file("lines.html", title="line.py example")
+
 vline = Line(xyvalues, y='Count', color='Degree', title="Lines VLine", ylabel='measures',
              tools=TOOLS)
+
 hline = Line(xyvalues, y='Count', color='Degree', title="Lines HLine",
              ylabel='measures', tools=TOOLS)
+
 int_vline = Line(xyvalues, y='Count', color='Degree', title="Lines VLine Interp",
                  ylabel='measures', tools=TOOLS)
+
 int_hline = Line(xyvalues, y='Count', color='Degree', title="Lines HLine Interp",
                  ylabel='measures', tools=TOOLS)
 
-scatter_point = Scatter(xyvalues, x='Year', y='Degree', title="Scatter mouse",
-                        ylabel='measures', legend=True, tools=TOOLS)
-# scatter = Scatter(svalues, title="Scatter V Line", ylabel='measures',
-#              legend=True, tools=TOOLS)
-#
-# int_point_line = Line(xyvalues, title="Lines Mouse Interp.", ylabel='measures',
-#              tools=TOOLS)
-# point_line = Line(xyvalues, title="Lines Mouse", ylabel='measures',
-#              tools=TOOLS)
+scatter_point = Scatter(xyvalues, x='Year', y='Count', color='Degree',
+                        title="Scatter mouse", ylabel='measures', legend=True,
+                        tools=TOOLS)
+scatter = Scatter(xyvalues, x='Year', y='Count', color='Degree',
+                  title="Scatter V Line", ylabel='measures', legend=True, tools=TOOLS)
+
+int_point_line = Line(xyvalues, x='Year', y='Count', color='Degree',
+                      title="Lines Mouse Interp.", ylabel='measures', tools=TOOLS)
+
+point_line = Line(xyvalues, x='Year', y='Count', color='Degree',
+                  title="Lines Mouse", ylabel='measures', tools=TOOLS)
 
 
 hhover = hline.select(dict(type=HoverTool))
@@ -49,18 +55,18 @@ int_vhover = int_vline.select(dict(type=HoverTool))
 int_vhover.mode = 'vline'
 int_vhover.line_policy = 'interp'
 
-# iphover = int_point_line.select(dict(type=HoverTool))
-# iphover.mode = 'mouse'
-# iphover.line_policy = 'interp'
-#
-# tphover = point_line.select(dict(type=HoverTool))
-# tphover.mode = 'mouse'
-#
-# shover = scatter.select(dict(type=HoverTool))
-# shover.mode = 'vline'
-#
-# shoverp = scatter_point.select(dict(type=HoverTool))
-# shoverp.mode = 'mouse'
+iphover = int_point_line.select(dict(type=HoverTool))
+iphover.mode = 'mouse'
+iphover.line_policy = 'interp'
+
+tphover = point_line.select(dict(type=HoverTool))
+tphover.mode = 'mouse'
+
+shover = scatter.select(dict(type=HoverTool))
+shover.mode = 'vline'
+
+shoverp = scatter_point.select(dict(type=HoverTool))
+shoverp.mode = 'mouse'
 
 TOOLTIPS = [
     ("y", "$~y"),
@@ -68,16 +74,15 @@ TOOLTIPS = [
 ]
 
 int_vhover.tooltips = int_hhover.tooltips = TOOLTIPS
-# tphover.tooltips = iphover.tooltips = TOOLTIPS
-# shover.tooltips = shoverp.tooltips = TOOLTIPS
+tphover.tooltips = iphover.tooltips = TOOLTIPS
+shover.tooltips = shoverp.tooltips = TOOLTIPS
 vhover.tooltips = hhover.tooltips = TOOLTIPS
 
 show(
     vplot(
         hplot(hline, vline),
         hplot(int_hline, int_vline),
-        hplot(scatter_point)
-        # hplot(int_point_line, point_line),
-        # hplot(scatter_point, scatter),
+        hplot(int_point_line, point_line),
+        hplot(scatter_point, scatter),
     )
 )
