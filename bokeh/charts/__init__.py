51edef1058dab0cbbc6c9627810e7ab276cae342
Bryan Van de Ven <bryanv@continuum.io>
2015-09-17 12:54:21 -0500

    remove old charts from bokeh.charts

--- bokeh/charts/__init__.py
+++ bokeh/charts/__init__.py
@@ -12,18 +12,11 @@ from ._attributes import color
 from .operations import stack, blend
 
 # builders
-from .builder.area_builder import Area
-from .builder.donut_builder import Donut
-from .builder.dot_builder import Dot
 from .builder.line_builder import Line
-from .builder.step_builder import Step
 from .builder.histogram_builder import Histogram
 from .builder.bar_builder import Bar
 from .builder.scatter_builder import Scatter
 from .builder.boxplot_builder import BoxPlot
-from .builder.timeseries_builder import TimeSeries
-from .builder.heatmap_builder import HeatMap
-from .builder.horizon_builder import Horizon
 
 # easy access to required bokeh components
 from ..models import ColumnDataSource
