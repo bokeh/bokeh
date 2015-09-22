e98e0decf33163186aaab2815887a80809763ced
Havoc Pennington <hp@pobox.com>
2015-09-22 13:25:41 -0400

    add Document test showing that cycles break refcounting
    (commented out for now, maybe we ban cycles?)

--- bokeh/tests/test_document.py
+++ bokeh/tests/test_document.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import unittest
 
@@ -39,6 +39,8 @@ class TestDocument(unittest.TestCase):
         assert len(d._all_models) == 1
         m.child = m2
         assert len(d._all_models) == 2
+        d.remove_root(m)
+        assert len(d._all_models) == 0
 
     def test_all_models_with_multiple_references(self):
         d = document.Document()
@@ -59,6 +61,12 @@ class TestDocument(unittest.TestCase):
         assert len(d._all_models) == 2
         root1.child = child1
         assert len(d._all_models) == 3
+        root2.child = child1
+        assert len(d._all_models) == 3
+        d.remove_root(root1)
+        assert len(d._all_models) == 2
+        d.remove_root(root2)
+        assert len(d._all_models) == 0
 
     def test_all_models_with_cycles(self):
         d = document.Document()
@@ -70,16 +78,33 @@ class TestDocument(unittest.TestCase):
         root1.child = child1
         root2.child = child1
         child1.child = root1
+        print("adding root1")
         d.add_root(root1)
+        print("adding root2")
         d.add_root(root2)
         assert len(d.roots) == 2
         assert len(d._all_models) == 3
+        print("clearing child of root1")
         root1.child = None
         assert len(d._all_models) == 3
+        print("clearing child of root2")
         root2.child = None
         assert len(d._all_models) == 2
+        print("putting child1 back in root1")
         root1.child = child1
         assert len(d._all_models) == 3
+# This will fail if uncommented because refcounting isn't
+# a reliable GC in the face of cycles. We
+# could implement mark-and-sweep or another
+# GC algorithm for _all_models but it would be
+# less efficient. Do we need to allow cycles?
+# (when do we use them?)
+#        print("Removing root1")
+#        d.remove_root(root1)
+#        assert len(d._all_models) == 1
+#        print("Removing root2")
+#        d.remove_root(root2)
+#        assert len(d._all_models) == 0
 
     def test_change_notification(self):
         d = document.Document()
