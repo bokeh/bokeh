66913ecfbe688d6b843bf368506da7d6ebde640b
Luke Canavan <canavandl@gmail.com>
2015-09-22 11:28:41 -0500

    Fix whitespace

--- sphinx/source/docs/user_guide/source_examples/plotting_line_annotation.py
+++ sphinx/source/docs/user_guide/source_examples/plotting_line_annotation.py
@@ -13,15 +13,12 @@ p.line(daylight_warsaw_2013.Date, daylight_warsaw_2013.Sunset,
 p.line(daylight_warsaw_2013.Date, daylight_warsaw_2013.Sunrise,
        line_dash='dotted', line_width=2, legend="Sunrise")
 
-
 daylight_savings_start = LineAnnotation(location=dt(2013, 3, 31, 2, 0, 0).timestamp()*1000,
                                         dimension='height', line_color='green',
-                                        line_dash='dashed',
-                                        line_width=3)
+                                        line_dash='dashed', line_width=3)
 daylight_savings_end = LineAnnotation(location=dt(2013, 10, 27, 3, 0, 0).timestamp()*1000,
                                       dimension='height', line_color='red',
-                                      line_dash='dashed',
-                                      line_width=3)
+                                      line_dash='dashed', line_width=3)
 p.renderers.extend([daylight_savings_start, daylight_savings_end])
 
 p.title = "2013 Sunrise and Sunset times in Warsaw"
