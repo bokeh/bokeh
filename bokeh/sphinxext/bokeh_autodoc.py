af3c147fd70a6b585f912243ba6a5dce02ce8904
Bryan Van de Ven <bryanv@continuum.io>
2015-09-28 17:47:25 -0400

    add bokeh-jinja directive

--- bokeh/sphinxext/bokeh_autodoc.py
+++ bokeh/sphinxext/bokeh_autodoc.py
@@ -5,58 +5,48 @@ use Bokeh-specific directives (e.g., ``bokeh-prop`` and ``bokeh-model``)
 when appropriate.
 
 """
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
+import jinja2
 from six import class_types
-from sphinx.ext.autodoc import AttributeDocumenter, ClassDocumenter
+from sphinx.ext.autodoc import AttributeDocumenter, ClassDocumenter, DataDocumenter
 
 from bokeh.plot_object import PlotObject
 from bokeh.properties import Property
 
+def _add_directive_header(self, sig):
+    # Note: we are supplying our own version of this function because
+    # our directives should not be passed `sig` as an argument, and there
+    # is no way to suppress this behavior in the default version
+    domain = getattr(self, 'domain', 'py')
+    directive = getattr(self, 'directivetype', self.objtype)
+    name = self.format_name()
+    self.add_line(u'.. %s:%s:: %s.%s' % (domain, directive, self.modname, name),
+                  '<autodoc>')
+    if self.options.noindex:
+        self.add_line(u'   :noindex:', '<autodoc>')
+
 class PropDocumenter(AttributeDocumenter):
     directivetype = 'bokeh-prop'
     objtype = 'prop'
-    priority = 20 # prefer over default autoattribute directive
+    priority = 20
 
     @classmethod
     def can_document_member(cls, member, membername, isattr, parent):
-        # MetaHasProps creates instances even if user just
-        # supplies property class name
         return isinstance(member, Property)
 
-    def add_directive_header(self, sig):
-        # Note: we are supplying our own version of this function because
-        # bokeh-model should not be passed `sig` as an argument, and there is
-        # no way to suppress this behaviour in the default version
-        domain = getattr(self, 'domain', 'py')
-        directive = getattr(self, 'directivetype', self.objtype)
-        name = self.format_name()
-        self.add_line(u'.. %s:%s:: %s.%s' % (domain, directive, self.modname, name),
-                      '<autodoc>')
-        if self.options.noindex:
-            self.add_line(u'   :noindex:', '<autodoc>')
+    add_directive_header = _add_directive_header
 
 class ModelDocumenter(ClassDocumenter):
     directivetype = 'bokeh-model'
     objtype = 'model'
-    priority = 20 # prefer over default autoclass directive
+    priority = 20
 
     @classmethod
     def can_document_member(cls, member, membername, isattr, parent):
         return isinstance(member, class_types) and issubclass(member, PlotObject)
 
-    def add_directive_header(self, sig):
-        # Note: we are supplying our own version of this function because
-        # bokeh-model should not be passed `sig` as an argument, and there is
-        # no way to suppress this behaviour in the default version
-        domain = getattr(self, 'domain', 'py')
-        directive = getattr(self, 'directivetype', self.objtype)
-        name = self.format_name()
-        self.add_line(u'.. %s:%s:: %s.%s' % (domain, directive, self.modname, name),
-                      '<autodoc>')
-        if self.options.noindex:
-            self.add_line(u'   :noindex:', '<autodoc>')
-
+    add_directive_header = _add_directive_header
 
 def setup(app):
     app.add_autodocumenter(PropDocumenter)
