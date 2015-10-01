9587a1c3a77a5eba897996e641ab7e61cad99301
Havoc Pennington <hp@pobox.com>
2015-10-01 07:17:19 -0400

    Remove broken/unused references() method from Document

--- bokeh/document.py
+++ bokeh/document.py
@@ -47,9 +47,6 @@ class Document(object):
         self._all_models = dict()
         self._callbacks = []
 
-    def references(self):
-        return self.context.references()
-
     def clear(self):
         ''' Remove all content from the document (including roots, vars, stores) '''
         while len(self._roots) > 0:
