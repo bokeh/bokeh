a7fec51ba1cb88f44005e86dbce4136745267d8d
Havoc Pennington <hp@pobox.com>
2015-10-02 13:49:11 -0400

    Make bk command log the port it starts on

--- bokeh/bk.py
+++ bokeh/bk.py
@@ -1,8 +1,10 @@
-
 from tornado.httpserver import HTTPServer
 
 from bokeh.application import Application
 from bokeh.server.server import Server
+
+import logging
+log = logging.getLogger(__name__)
 
 def main():
     # TODO we need to fill in Application with handlers,
@@ -11,9 +13,9 @@ def main():
     # TODO allow specifiying multiple applications with routes to each
     applications = { '/' : application }
     server = Server(applications)
+    log.info("Starting Bokeh server on port %d", server.port)
     server.start()
 
 if __name__ == "__main__":
-    import logging
     logging.basicConfig(level=logging.DEBUG)
     main()
