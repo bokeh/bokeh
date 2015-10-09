ddaee559192b7db852ee3c5fc4685c09334c2909
Havoc Pennington <hp@pobox.com>
2015-10-09 15:56:26 -0400

    Fix silly bugs in doc_js.js

--- bokeh/_templates/doc_js.js
+++ bokeh/_templates/doc_js.js
@@ -11,9 +11,9 @@ for (key in docs_json) {
 var render_items = {{ render_items }};
 for (idx in render_items) {
   var item = render_items[idx];
-  if ('modelid' in item) {
-     add_model_static($('#' + item['elementid']), item['modelid'], docs[item['docid']]);
+  if ('modelid' in item && item['modelid'] !== null) {
+     Bokeh.embed.add_model_static(Bokeh.$('#' + item['elementid']), item['modelid'], docs[item['docid']]);
   } else {
-     add_document_static($('#' + item['elementid']), docs[item['docid']]);
+     Bokeh.embed.add_document_static(Bokeh.$('#' + item['elementid']), docs[item['docid']]);
   }
 }
