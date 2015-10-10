207f138e1b7d68eec1a13df385faf95ee225795c
Justace Clutter <justacec@gmail.com>
2015-10-10 09:18:49 -0400

    Import the LinearBreaksColorMapper function from the models sub-package.

--- examples/plotting/file/linear_breaks_color_mapper.py
+++ examples/plotting/file/linear_breaks_color_mapper.py
@@ -2,7 +2,8 @@ from __future__ import division
 
 import numpy as np
 
-from bokeh.plotting import figure, show, output_server
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import LinearBreaksColorMapper
 
 # Generate the nessessary data
 xs = np.linspace(-10, 10, 50)
@@ -11,7 +12,7 @@ ys = ys[:,np.newaxis]
 z = np.matrix(np.exp(-4*np.log(2)*((xs - 0)**2 + (ys-0)**2) / 80))
 
 # Define the color mapper
-lcmap = bkmdl.LinearBreaksColorMapper(palette = {0:'#FFFFFF', 0.2:'#01Fa10', 0.5:'#0000FF', 1:'#FF0000'}, alpha = [0.1, 1, 0.75, 0.5])
+lcmap = LinearBreaksColorMapper(palette = {0:'#FFFFFF', 0.2:'#01Fa10', 0.5:'#0000FF', 1:'#FF0000'}, alpha = [0.1, 1, 0.75, 0.5])
 
 output_file("linear_breaks_color_mapper.html", title="linear_breaks_color_mapper.py example")
 
