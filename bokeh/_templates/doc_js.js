3c3f127ab7ef2c0b742b31f20a1ad2e2afa351a1
Havoc Pennington <hp@pobox.com>
2015-10-16 13:12:15 -0400

    Log errors from doc_js.js async operations (we were ignoring the returned promises)

--- bokeh/_templates/doc_js.js
+++ bokeh/_templates/doc_js.js
@@ -12,17 +12,23 @@ var render_items = {{ render_items }};
 for (idx in render_items) {
   var item = render_items[idx];
   var elem = Bokeh.$('#' + item['elementid']);
+  var promise = null;
   if ('modelid' in item && item['modelid'] !== null) {
     if ('docid' in item && item['docid'] !== null) {
       Bokeh.embed.add_model_static(elem, item['modelid'], docs[item['docid']]);
     } else {
-      Bokeh.embed.add_model_from_session(elem, item['modelid'], item['sessionid']);
+      promise = Bokeh.embed.add_model_from_session(elem, item['modelid'], item['sessionid']);
     }
   } else {
     if ('docid' in item && item['docid'] !== null) {
        Bokeh.embed.add_document_static(elem, docs[item['docid']]);
     } else {
-       Bokeh.embed.add_document_from_session(elem, item['sessionid']);
+       promise = Bokeh.embed.add_document_from_session(elem, item['sessionid']);
     }
+  }
+  if (promise !== null) {
+    promise.then(function(value) {}, function(error) {
+      console.log("Error rendering Bokeh items ", error);
+    });
   }
 }
