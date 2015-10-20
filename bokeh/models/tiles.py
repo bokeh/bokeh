1a5edeab75226f81aea9c34e4579e462d6f31723
bcollins <bcollins@continuum.io>
2015-10-20 16:55:22 -0500

    added docstrings and .rst for tiles module

--- bokeh/models/tiles.py
+++ bokeh/models/tiles.py
@@ -5,6 +5,9 @@ from ..properties import (abstract, AngleSpec, Any, Array, Bool, Dict, DistanceS
                           Include, Instance, NumberSpec, StringSpec, String, Int, HasProps)
 
 class TileSource(PlotObject):
+    """ A base class for all tile source types. ``TileSource`` is
+    not generally useful to instantiate on its own. In general, tile sources are used as a required input for ``TileRenderer``.
+    """
 
     _args = ('url', 'tile_size', 'min_zoom', 'max_zoom', 'x_origin_offset', 'y_origin_offset', 'extra_url_vars', 'initial_resolution')
 
@@ -41,10 +44,25 @@ class TileSource(PlotObject):
     """)
 
 class TMSTileSource(TileSource):
+    """
+    The TMSTileSource contains tileset configuration information and provides urls for individual tiles based on an extent in plot coordinates and a templated url (ex. http://your.tms.server.host/{Z}/{X}/{Y}.png). The defining feature of TMS is the tile-origin in located at the bottom-left.
+
+    The TMSTileSource can also be helpful in implementing tile renderers for custom tile sets, including non-spatial datasets.
+    """
     pass
 
 class WMTSTileSource(TileSource):
+    """
+    The ``WMTSTileSource`` behaves much like ``TMSTileSource`` but has its tile-origin in the top-left. This is the most common used tile source for web mapping applications.  Such companies as Google, MapQuest, Stamen, Esri, and OpenStreetMap provide service which use the WMTS specification.
+
+    Example url: http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png
+    """
     pass
 
 class QUADKEYTileSource(TileSource):
+    """
+    The QUADKEYTileSource has the same tile origin as the WMTSTileSource but requests tiles using a `quadkey` argument instead of X,Y,Z.
+
+    Example url: http://your.quadkey.tile.host/{Q}.png
+    """
     pass
