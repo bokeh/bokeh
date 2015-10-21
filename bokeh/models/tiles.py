2b15094baf25e291dcf761d0f23026356b413dd5
bcollins <bcollins@continuum.io>
2015-10-21 12:14:07 -0500

    flake8 fixes for tiles.py

--- bokeh/models/tiles.py
+++ bokeh/models/tiles.py
@@ -1,8 +1,7 @@
 from __future__ import absolute_import
 from ..plot_object import PlotObject
 
-from ..properties import (abstract, AngleSpec, Any, Array, Bool, Dict, DistanceSpec, Enum, Float,
-                          Include, Instance, NumberSpec, StringSpec, String, Int, HasProps)
+from ..properties import (Any, Dict, Float, String, Int)
 
 class TileSource(PlotObject):
     """ A base class for all tile source types. ``TileSource`` is
@@ -18,7 +17,7 @@ class TileSource(PlotObject):
     tile_size = Int(default=256, help="""
     tile size in pixels (e.g. 256)
     """)
-    
+
     min_zoom = Int(default=0, help="""
     the minimum zoom level for the tile layer. This is the most "zoomed-out" level.
     """)
@@ -34,9 +33,10 @@ class TileSource(PlotObject):
     y_origin_offset = Float(default=20037508.34, help="""
     y offset in plot coordinates
     """)
-    
+
     extra_url_vars = Dict(String, Any(String, Int), help="""
-    A dictionary that maps url variable template keys to values.  These variables are useful for parts of tile urls which do not change from tile to tile (e.g. server host name, or layer name).
+    A dictionary that maps url variable template keys to values.
+    These variables are useful for parts of tile urls which do not change from tile to tile (e.g. server host name, or layer name).
     """)
 
     initial_resolution = Float(default=1.0, help="""
@@ -45,7 +45,8 @@ class TileSource(PlotObject):
 
 class TMSTileSource(TileSource):
     """
-    The TMSTileSource contains tileset configuration information and provides urls for individual tiles based on an extent in plot coordinates and a templated url (ex. http://your.tms.server.host/{Z}/{X}/{Y}.png). The defining feature of TMS is the tile-origin in located at the bottom-left.
+    The TMSTileSource contains tile config info and provides urls for tiles based on a templated url (ex. http://your.tms.server.host/{Z}/{X}/{Y}.png).
+    The defining feature of TMS is the tile-origin in located at the bottom-left.
 
     The TMSTileSource can also be helpful in implementing tile renderers for custom tile sets, including non-spatial datasets.
     """
@@ -53,7 +54,9 @@ class TMSTileSource(TileSource):
 
 class WMTSTileSource(TileSource):
     """
-    The ``WMTSTileSource`` behaves much like ``TMSTileSource`` but has its tile-origin in the top-left. This is the most common used tile source for web mapping applications.  Such companies as Google, MapQuest, Stamen, Esri, and OpenStreetMap provide service which use the WMTS specification.
+    The ``WMTSTileSource`` behaves much like ``TMSTileSource`` but has its tile-origin in the top-left.
+    This is the most common used tile source for web mapping applications.
+    Such companies as Google, MapQuest, Stamen, Esri, and OpenStreetMap provide service which use the WMTS specification.
 
     Example url: http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png
     """
