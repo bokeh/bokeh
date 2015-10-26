fb276087b55a69a1a39a3946d339f4473f03bc1f
Havoc Pennington <hp@pobox.com>
2015-10-26 16:42:48 -0400

    Complain about and ignore io calls in scripts passed to bokeh command

--- bokeh/application/spellings/script.py
+++ bokeh/application/spellings/script.py
@@ -9,6 +9,9 @@ from bokeh.io import set_curdoc, curdoc
 class ScriptHandler(SpellingHandler):
     """Run a script which modifies a Document"""
 
+    _io_functions = ['output_server', 'output_notebook', 'output_file',
+                     'show', 'save', 'push', 'reset_output']
+
     def __init__(self, *args, **kwargs):
         super(ScriptHandler, self).__init__(*args, **kwargs)
         import ast
@@ -29,12 +32,39 @@ class ScriptHandler(SpellingHandler):
         else:
             raise ValueError('Must pass a filename to ScriptHandler')
 
+        self._complainers = {}
+        for f in ScriptHandler._io_functions:
+            self._complainers[f] = self._make_io_complainer(f)
+
     def url_path(self):
         if self.failed:
             return None
         else:
             # TODO should fix invalid URL characters
             return '/' + os.path.splitext(os.path.basename(self._path))[0]
+
+    def _make_io_complainer(self, name):
+        def complainer(*args, **kwargs):
+            print("%s: Warning: call to %s() should not be needed in scripts run by the 'bokeh' command, try running this with 'python' or remove the call to %s(). Ignoring %s() call." % (self._path, name, name, name),
+                  file=sys.stderr)
+        return complainer
+
+    # monkeypatching is a little ugly, but in this case there's no reason any legitimate
+    # code should be calling these functions, and we're only making a best effort to
+    # warn people so no big deal if we fail.
+    def _monkeypatch_io(self):
+        import bokeh.io as io
+        old = {}
+        for f in ScriptHandler._io_functions:
+            old[f] = getattr(io, f)
+            setattr(io, f, self._complainers[f])
+
+        return old
+
+    def _unmonkeypatch_io(self, old):
+        import bokeh.io as io
+        for f in old:
+            setattr(io, f, old[f])
 
     def modify_document(self, doc):
         if self.failed:
@@ -43,6 +73,7 @@ class ScriptHandler(SpellingHandler):
         self._module_name = 'bk_script_' + str(uuid.uuid4()).replace('-', '')
         self._module = ModuleType(self._module_name)
         set_curdoc(doc)
+        old_io = self._monkeypatch_io()
         try:
             exec(self._code, self._module.__dict__)
             newdoc = curdoc()
@@ -58,5 +89,6 @@ class ScriptHandler(SpellingHandler):
             filename, line_number, func, txt = traceback.extract_tb(exc_traceback)[-1]
 
             self._error = "%s\nFile \"%s\", line %d, in %s:\n%s" % (str(e), os.path.basename(filename), line_number, func, txt)
-
+        finally:
+            self._unmonkeypatch_io(old_io)
 
