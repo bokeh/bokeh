93b6d8266aa051f87eb037257df63f3a2ae9d3e4
damianavila <damianavila@gmail.com>
2015-10-29 12:13:00 -0300

    Fixing missing modules, add the script entry point, load the yaml themes files and fix the conda recipe to call bokeh instead of bokeh-server [ci enable examples]."

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ versioneer.parentdir_prefix = 'Bokeh-'  # dirname like 'myproject-1.2.0'
 
 copy("LICENSE.txt", "bokeh/")
 
-package_data = ['LICENSE.txt']
+package_data = ['LICENSE.txt', 'themes/*.yaml']
 
 def package_path(path, filters=()):
     if not os.path.exists(path):
@@ -530,6 +530,10 @@ setup(
     cmdclass=_cmdclass,
     packages=[
         'bokeh',
+        'bokeh.application',
+        'bokeh.application.tests',
+        'bokeh.application.spellings',
+        'bokeh.application.spellings.tests',
         'bokeh.models',
         'bokeh.models.tests',
         'bokeh.models.widgets',
@@ -541,24 +545,22 @@ setup(
         'bokeh._legacy_charts.builder',
         'bokeh._legacy_charts.builder.tests',
         'bokeh._legacy_charts.tests',
+        'bokeh.client',
+        'bokeh.command',
         'bokeh.compat',
         'bokeh.compat.mplexporter',
         'bokeh.compat.mplexporter.renderers',
         'bokeh.crossfilter',
         'bokeh.sampledata',
         'bokeh.server',
-        'bokeh.server.core',
-        'bokeh.server.core.tests',
         'bokeh.server.protocol',
         'bokeh.server.protocol.messages',
         'bokeh.server.protocol.messages.tests',
         'bokeh.server.protocol.tests',
-        'bokeh.server.storage',
-        'bokeh.server.storage.tests',
         'bokeh.server.tests',
         'bokeh.server.views',
-        'bokeh.server.views.tests',
         'bokeh.sphinxext',
+        'bokeh.themes',
         'bokeh.tests',
         'bokeh.transforms',
         'bokeh.util',
@@ -571,7 +573,7 @@ setup(
     url='http://github.com/bokeh/bokeh',
     description='Statistical and novel interactive HTML plots for Python',
     license='New BSD',
-    #scripts=scripts,
+    scripts=['bin/bokeh'],
     zip_safe=False,
     install_requires=REQUIRES
 )
