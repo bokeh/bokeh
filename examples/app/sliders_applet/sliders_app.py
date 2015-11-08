23b2d5ac5119c17af01041f309b98d76f0ccb9a2
Bryan Van de Ven <bryanv@continuum.io>
2015-11-07 22:02:27 -0600

    cleanup sliders app

--- examples/app/sliders_applet/sliders_app.py
+++ examples/app/sliders_applet/sliders_app.py
@@ -3,10 +3,6 @@ This file demonstrates a bokeh server application, which you can
 run with `bokeh serve sliders_app.py`
 """
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
 import numpy as np
 
 from bokeh.plotting import Figure
@@ -14,63 +10,44 @@ from bokeh.models import Plot, ColumnDataSource
 from bokeh.models.widgets import HBox, Slider, TextInput, VBoxForm
 from bokeh.io import curdoc
 
-source = ColumnDataSource(data=dict(x=[], y=[]))
+# Set up data
+N = 200
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+source = ColumnDataSource(data=dict(x=x, y=y))
 
-text = TextInput(
-    title="title", name='title', value='my sine wave'
-)
 
-offset = Slider(
-    title="offset", name='offset',
-    value=0.0, start=-5.0, end=5.0, step=0.1
-)
-amplitude = Slider(
-    title="amplitude", name='amplitude',
-    value=1.0, start=-5.0, end=5.0
-)
-phase = Slider(
-    title="phase", name='phase',
-    value=0.0, start=0.0, end=2*np.pi
-)
-freq = Slider(
-    title="frequency", name='frequency',
-    value=1.0, start=0.1, end=5.1
-)
+# Set up plot
+plot = Figure(plot_height=400, plot_width=400, title="my sine wave",
+              tools="crosshair,pan,reset,resize,save,wheel_zoom",
+              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
 
-toolset = "crosshair,pan,reset,resize,save,wheel_zoom"
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
-# Generate a figure container
-plot = Figure(title_text_font_size="12pt",
-              plot_height=400,
-              plot_width=400,
-              tools=toolset,
-              title=text.value,
-              x_range=[0, 4*np.pi],
-              y_range=[-2.5, 2.5]
-        )
 
-# Plot the line by the x,y values in the source property
-plot.line('x', 'y', source=source,
-          line_width=3,
-          line_alpha=0.6
-)
+# Set up widgets
+text = TextInput(title="title", name='title', value='my sine wave')
 
-inputs = VBoxForm(
-    children=[
-        text, offset, amplitude, phase, freq
-    ]
-)
+offset = Slider(title="offset", name='offset',
+                value=0.0, start=-5.0, end=5.0, step=0.1)
 
-hbox = HBox(children=[inputs, plot])
+amplitude = Slider(title="amplitude", name='amplitude',
+                   value=1.0, start=-5.0, end=5.0)
 
-def update_data():
-    """Called each time that any watched property changes.
+phase = Slider(title="phase", name='phase',
+               value=0.0, start=0.0, end=2*np.pi)
 
-        This updates the sin wave data with the most recent values of the
-        sliders. This is stored as two numpy arrays in a dict into the app's
-        data source property.
-        """
-    N = 200
+freq = Slider(title="frequency", name='frequency',
+              value=1.0, start=0.1, end=5.1)
+
+
+# Set up callbacks
+def update_title(attrname, old, new):
+    plot.title = text.value
+
+text.on_change('value', update_title)
+
+def update_data(attrname, old, new):
 
     # Get the current slider values
     a = amplitude.value
@@ -78,38 +55,19 @@ def update_data():
     w = phase.value
     k = freq.value
 
-    # Generate the sine wave
+    # Generate the new curve
     x = np.linspace(0, 4*np.pi, N)
     y = a*np.sin(k*x + w) + b
 
-    logging.debug(
-        "PARAMS: offset: %s amplitude: %s", offset.value,
-        amplitude.value
-    )
-
     source.data = dict(x=x, y=y)
 
-update_data()
-
-def input_change(attrname, old, new):
-    """Executes whenever the input form changes.
-
-    It is responsible for updating the plot, or anything else you want.
-
-    Args:
-        attrname : the attr that changed
-        old : old value of attr
-        new : new value of attr
-        """
-    update_data()
-    plot.title = text.value
-
-# Text box event registration
-text.on_change('value', input_change)
-
-# Slider event registration
 for w in [offset, amplitude, phase, freq]:
-    w.on_change('value', input_change)
+    w.on_change('value', update_data)
 
-# put ourselves in the document
+
+# Set up layouts and add to document
+inputs = VBoxForm(children=[text, offset, amplitude, phase, freq])
+
+hbox = HBox(children=[inputs, plot])
+
 curdoc().add(hbox)
