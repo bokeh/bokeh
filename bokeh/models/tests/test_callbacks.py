21abc7afb853078fe34787b12045fc107906fc14
Almar Klein <almar.klein@gmail.com>
2015-11-16 11:38:42 +0100

    python callbacks test (but do not test example) (also add forgotten file)

---
+++ bokeh/models/tests/test_callbacks.py
@@ -0,0 +1,39 @@
+
+import sys
+
+from pytest import raises, mark
+from bokeh.models import CustomJS, Slider
+
+
+def test_js_callback():
+    
+    slider = Slider()
+    
+    cb = CustomJS(code="foo();", args={'x': slider})
+    assert cb.lang == 'javascript'
+    assert 'foo()' in cb.code
+    assert cb.args['x'] is slider
+    
+    with raises(ValueError):  # not a plot object
+        CustomJS(code="foo();", args={'x': 3})
+    
+    with raises(AttributeError):  # kwargs not supported
+        CustomJS(code="foo();", x=slider)
+
+
+@mark.skipif(sys.version_info < (3, ), reason='requires Python 3')
+def test_py_callback():
+    
+    slider = Slider()
+    
+    @CustomJS
+    def cb(x=slider):
+        foo()
+    assert cb.lang == 'javascript'
+    assert 'foo()' in cb.code
+    assert cb.args['x'] is slider
+    
+    with raises(ValueError):  # not a plot object
+        @CustomJS
+        def cb(x=4):
+            foo()
