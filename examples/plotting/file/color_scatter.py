9f0297d7e1dc70bc1b144922f8e08e31cfdf38d7
damianavila <damianavila@gmail.com>
2015-11-17 18:30:57 -0300

    Make numpy.float as type int to avoid TypeError on formatting.

--- examples/plotting/file/color_scatter.py
+++ examples/plotting/file/color_scatter.py
@@ -8,7 +8,7 @@ N = 4000
 x = np.random.random(size=N) * 100
 y = np.random.random(size=N) * 100
 radii = np.random.random(size=N) * 1.5
-colors = ["#%02x%02x%02x" % (r, g, 150) for r, g in zip(np.floor(50+2*x), np.floor(30+2*y))]
+colors = ["#%02x%02x%02x" % (r, g, 150) for int(r), int(g) in zip(np.floor(50+2*x), np.floor(30+2*y))]
 
 TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,tap,previewsave,box_select,poly_select,lasso_select"
 
