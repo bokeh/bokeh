7f69c3ccc4b98d6fd1a261789c332a2f23bf026e
Mateusz Paprocki <mattpap@gmail.com>
2015-11-18 16:42:33 +0100

    Add examples/plotting/png/bollinger.py

---
+++ examples/plotting/png/bollinger.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from bokeh.plotting import figure, show, output_png
+
+# Define Bollinger Bands.
+upperband = np.random.random_integers(100, 150, size=100)
+lowerband = upperband - 100
+x_data = np.arange(1, 101)
+
+# Bollinger shading glyph:
+band_x = np.append(x_data, x_data[::-1])
+band_y = np.append(lowerband, upperband[::-1])
+
+output_png('bollinger.png') # TODO: title='Bollinger bands (file)'
+
+p = figure(x_axis_type='datetime')
+p.patch(band_x, band_y, color='#7570B3', fill_alpha=0.2)
+
+p.title = 'Bollinger Bands'
+p.plot_height = 600
+p.plot_width = 800
+p.grid.grid_line_alpha = 0.4
+
+show(p)
