b550a6b31e7c2b141a0f7038410d8a9bc1464983
Sarah Bird <sarah@bonvaya.com>
2015-11-22 20:05:09 -0800

    Don't use relative dev in example

--- examples/interactions/prevent_user_zooming_or_panning_too_far.py
+++ examples/interactions/prevent_user_zooming_or_panning_too_far.py
@@ -5,7 +5,7 @@ from bokeh._legacy_charts import HeatMap
 from bokeh.plotting import figure
 from bokeh.models import Range1d
 
-output_file('prevent_user_zooming_or_panning_too_far.html', mode='relative-dev')
+output_file('prevent_user_zooming_or_panning_too_far.html')
 
 ## Plot where limits are manually set
 x_range = Range1d(0, 3, bound_lower=-1, bound_upper=3.5)
