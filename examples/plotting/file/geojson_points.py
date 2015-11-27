ee86d7930c5da73772d38bfc9874f2c80bd5e69d
Mateusz Paprocki <mattpap@gmail.com>
2015-11-27 11:03:33 +0100

    Change geo{->json}_points.html to make tests pass

--- examples/plotting/file/geojson_points.py
+++ examples/plotting/file/geojson_points.py
@@ -3,7 +3,7 @@ from bokeh.models import GeoJSONDataSource
 from bokeh.plotting import figure
 from bokeh.sampledata.sample_geojson import geojson
 
-output_file("geo_points.html", title="GeoJSON Points")
+output_file("geojson_points.html", title="GeoJSON Points")
 
 p = figure()
 p.circle(line_color=None, fill_alpha=0.8, source=GeoJSONDataSource(geojson=geojson))
