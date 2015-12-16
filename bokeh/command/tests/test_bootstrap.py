3e3401665aa01b84e4158312ab3db4126ec0f94a
Havoc Pennington <hp@pobox.com>
2015-12-16 09:08:06 -0500

    Add tests for `bokeh --version` and make it use `bokeh.__version__`

--- bokeh/command/tests/test_bootstrap.py
+++ bokeh/command/tests/test_bootstrap.py
@@ -1,10 +1,25 @@
 import pytest
 
 from bokeh.command.bootstrap import main
+from bokeh import __version__
 
 def test_no_subcommand(capsys):
     with pytest.raises(SystemExit):
         main(["bokeh"])
     out, err = capsys.readouterr()
     assert err == "ERROR: Must specify subcommand, one of: html, json or serve\n"
+    assert out == ""
+
+def test_version(capsys):
+    with pytest.raises(SystemExit):
+        main(["bokeh", "--version"])
+    out, err = capsys.readouterr()
+    assert err == ("%s\n" % __version__)
+    assert out == ""
+
+def test_version_short(capsys):
+    with pytest.raises(SystemExit):
+        main(["bokeh", "-v"])
+    out, err = capsys.readouterr()
+    assert err == ("%s\n" % __version__)
     assert out == ""
