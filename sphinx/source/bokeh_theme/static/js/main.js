ce3f353cc472b4e97fa8bb734ed2f9aa84a96a5e
Sarah Bird <sarah@bonvaya.com>
2015-12-17 17:02:09 -0800

    Add new bokeh-theme to match bokehplots.com
    Lots of things still broken

---
+++ sphinx/source/bokeh_theme/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
+
