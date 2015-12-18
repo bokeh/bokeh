0243f78494990682448f4b8abdad960141121235
Sarah Bird <sarah@bonvaya.com>
2015-12-18 03:44:59 -0800

    Clean up TOC

--- sphinx/source/bokeh_theme/static/js/main.js
+++ sphinx/source/bokeh_theme/static/js/main.js
@@ -12,38 +12,47 @@ $(document).ready(function() {
   });
 
   var loc = window.location.pathname;
-  var loc_part = loc.split('docs/')[1].split('.html')[0];
 
-  if (loc_part.split('/').length > 1) {
-    loc_part = loc_part.split('/')[0]
-  }
+  if (loc.split('docs/').length == 1 ) {
+    // If index.html only show top-level toc
+    $('.toc .toctree-l1').addClass('hide');
+    $('.toc').addClass('obfuscate');
 
-  var l1_links = $('.toctree-l1');
-  l1_links.each(function(i, l1_link) {
-    if ( $(this).hasClass('current') ) {
-      $(this).addClass('show');
-    } else {
-      var href = l1_link.childNodes[0].href;
-      var href_part = href.split('docs/')[1].split('/')[0];
-      if (href_part == loc_part) {
+  } else {
+    // Else, selectively hide toc
+    var loc_part = loc.split('docs/')[1].split('.html')[0];
+
+    if (loc_part.split('/').length > 1) {
+      loc_part = loc_part.split('/')[0]
+    }
+
+    var l1_links = $('.toctree-l1');
+    l1_links.each(function(i, l1_link) {
+      if ( $(this).hasClass('current') ) {
         $(this).addClass('show');
       } else {
-        $(this).addClass('hide');
+        var href = l1_link.childNodes[0].href;
+        var href_part = href.split('docs/')[1].split('/')[0];
+        if (href_part == loc_part) {
+          $(this).addClass('show');
+        } else {
+          $(this).addClass('hide');
+        }
       }
-    }
-  });
+    });
 
-  var nav_links = $('.navigation.doc .nav-link a');
-  nav_links.each(function(i, nav_link) {
-    var href = nav_link.href;
-    var href_part = href.split('docs/')[1].split('.html')[0];
-    if (href_part.split('/').length > 1) {
-      href_part = href_part.split('/')[0]
-    }
-    if (loc_part == href_part) {
-      $(this).addClass('current');
-    }
-  });
+    var nav_links = $('.navigation.doc .nav-link a');
+    nav_links.each(function(i, nav_link) {
+      var href = nav_link.href;
+      var href_part = href.split('docs/')[1].split('.html')[0];
+      if (href_part.split('/').length > 1) {
+        href_part = href_part.split('/')[0]
+      }
+      if (loc_part == href_part) {
+        $(this).addClass('current');
+      }
+    });
+  }
 
 });
 
