5a2bb1a3a12b50e11b5d6c5080982fa37d8f998b
Sarah Bird <sarah@bonvaya.com>
2015-12-22 16:28:15 -0500

    Clean up example and make more intuitive

--- examples/plotting/file/patches_with_hole.py
+++ examples/plotting/file/patches_with_hole.py
@@ -1,20 +1,17 @@
 import numpy as np
-from bokeh.plotting import figure, show, output_file, vplot
+from bokeh.plotting import figure, show, output_file
+from bokeh.palettes import Spectral4
+from bokeh.models import HoverTool
 
 output_file("patches_with_holes.html", title="patches_with_holes.py example")
 
-p1 = figure(title="patches with holes 1")
-p1.patches(
-    xs=[[[1, 1.1, 2, 1.9], [1.8, 1.7, 1.2, 1.2]], [3, 3.1, 4, 3.9]],
-    ys=[[[1, 2.0, 2, 1.0], [1.1, 1.9, 1.9, 1.2]], [3, 4, 4, 3]],
-    fill_color=[None, 'pink'], line_color='blue'
-)
-
-p2 = figure(title="patches with holes 2", tools='tap')
-p2.patches(
+hover = HoverTool(tooltips=None)
+p = figure(title="Hover over to see different groups", tools=[hover])
+p.patches(
     xs=[[[1, 1.1, 2, 1.9], [1.8, 1.7, 1.2, 1.2]], [3, 3.1, 4, 3.9], [5, 6, 5.5, np.NaN, 5, 6, 5.5], [[1, 1.1, 2, 1.9], [1.8, 1.7, 1.2, 1.2], np.NaN, [3, 3.1, 4, 3.9]]],
     ys=[[[1, 2.0, 2, 1.0], [1.1, 1.9, 1.9, 1.2]], [3, 4.0, 4, 3.0], [3, 4, 4.0, np.NaN, 1, 2, 2.0], [[3, 4.0, 4, 3.0], [3.1, 3.9, 3.9, 3.2], np.NaN, [1, 2.0, 2, 1.0]]],
-    fill_color=['pink', 'orange', 'blue', 'red'], line_color=None
+    fill_color=Spectral4, line_color=None, fill_alpha=0.6,
+    hover_fill_color='grey', hover_line_color=None,
 )
 
-show(vplot(p1, p2))
+show(p)
