771a19574c74c4fbd69f4dc71ad527f6d6292cd7
Bryan Van de Ven <bryanv@continuum.io>
2015-12-22 11:07:03 -0600

    Add a basic model for Tooltip
    This is not really ueful from python, but it may show up in 'references'
    (even if it is a nonserializable name) and needs python counterpart.
    
    Conflicts:
    	bokeh/models/annotations.py

--- bokeh/models/annotations.py
+++ bokeh/models/annotations.py
@@ -4,11 +4,11 @@ Bokeh plots
 """
 from __future__ import absolute_import
 
-from ..core.enums import LegendLocation, SpatialUnits, RenderLevel, Dimension, RenderMode
+from ..core.enums import LegendLocation, SpatialUnits, RenderLevel, Dimension, RenderMode, Side
 from ..core.property_mixins import LineProps, FillProps, TextProps
 from ..core.properties import abstract
 from ..core.properties import (
-    Int, String, Enum, Instance, List, Dict, Tuple,
+    Bool Int, String, Enum, Instance, List, Dict, Tuple,
     Include, NumberSpec, Either, Auto, Float, Override, Seq
 )
 from ..util.deprecate import deprecated
@@ -274,4 +274,22 @@ class Span(Annotation):
 
     line_props = Include(LineProps, use_prefix=False, help="""
     The %s values for the span.
+    """)
+
+class Tooltip(Annotation):
+    """ Render a tooltip.
+
+    .. note::
+        This model is typically managed by BokehJS and is not useful
+        directly from python.
+
+    """
+
+    side = Either(Auto, Enum(Side), default="auto", help="""
+    Whether the tooltip should display to the left or right off the cursor
+    position, or if it should be automatically placed.
+    """)
+
+    inner_only = Bool(default=True, help="""
+    Whether to display outside a central plot frame area.
     """)
