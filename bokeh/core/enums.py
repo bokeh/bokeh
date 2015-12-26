09a6bb3862c08ea472749d221ce1d9a96d41c7aa
Bryan Van de Ven <bryanv@continuum.io>
2015-12-26 12:14:11 -0600

    add a Start/End enumeration

--- bokeh/core/enums.py
+++ bokeh/core/enums.py
@@ -74,3 +74,4 @@ RenderLevel = enumeration("image", "underlay", "glyph", "annotation",
 RenderMode = enumeration("canvas", "css")
 Aggregation = enumeration("sum", "mean", "count", "nunique", "median", "min", "max")
 ScriptingLanguage = enumeration("javascript", "coffeescript")
+StartEnd = enumeration("start", "end")
