7587e9e1efa3ce3524da363c8d4a935177208185
Bryan Van de Ven <bryanv@continuum.io>
2015-12-28 15:06:59 -0600

    some cleanup

--- examples/app/movies/main.py
+++ examples/app/movies/main.py
@@ -1,130 +1,99 @@
-from bokeh.plotting import Figure
-from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.models.widgets import HBox, Slider, TextInput, VBoxForm, Select, TextInput, Paragraph, PreText
-from bokeh.io import curdoc
-from bokeh.sampledata.movies_data import movie_path
-import pandas as pd
+from os.path import dirname, join
+
 import numpy as np
 import pandas.io.sql as psql
 import sqlite3 as sql
 
-# To get the movies sample data execute the following from a python prompt
-#   >>> import bokeh.sampledata
-#   >>> bokeh.sampledata.download()
+from bokeh.plotting import Figure
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models.widgets import HBox, Slider, VBoxForm, Select, TextInput
+from bokeh.io import curdoc
+from bokeh.sampledata.movies_data import movie_path
 
-con = sql.connect(movie_path)
-movies = psql.read_sql('select omdb.ID, imdbID, Title, Year, omdb.Rating, Runtime, Genre, Released, \
-    Director, Writer, imdbRating, imdbVotes, Language, Country, Oscars, \
-    tomatoes.Rating, Meter, Reviews, Fresh, Rotten, userMeter, userRating, userReviews, \
-    BoxOffice, Production \
-    from omdb, tomatoes \
-    where omdb.ID = tomatoes.ID \
-    and Reviews >= 10', con)
+conn = sql.connect(movie_path)
+query = open(join(dirname(__file__), 'query.sql')).read()
+movies = psql.read_sql(query, conn)
 
-movies["has_oscars"] = movies["Oscars"] > 0
-movies["pt_color"] = np.where(movies["Oscars"] > 0, "orange", "grey")
-movies.rename(columns={'Rating_right':'Rating'}, inplace=True)
-# Replace missing BoxOffice values with zeroes
-# and then format with commas so easier to read.
-movies.fillna(0, inplace=True)
-print("Movies.BoxOffice class: ", movies.BoxOffice.__class__)
-print("BoxOffice: ", movies.BoxOffice[:20])
+movies["color"] = np.where(movies["Oscars"] > 0, "orange", "grey")
+movies.fillna(0, inplace=True)  # just replace missing values with zero
 movies["revenue"] = movies.BoxOffice.apply(lambda x: '{:,d}'.format(int(x)))
 
-# Dictionary to map axis selection drop down to column of data
-axis_vars = {"Tomato Meter": "Meter",
-             "Numeric Rating": "Rating",
-             "Number of reviews":"Reviews",
-             "Dollars at box office":"BoxOffice",
-             "Year": "Year",
-             "Length (minutes)":"Runtime"}
+axis_map = {
+    "Tomato Meter": "Meter",
+    "Numeric Rating": "numericRating",
+    "Number of Reviews": "Reviews",
+    "Box Office (dollars)": "BoxOffice",
+    "Length (minutes)": "Runtime",
+    "Year": "Year",
+}
 
 # Create Input controls
-reviews = Slider(title="Minimum number of reviews on Rotten Tomatoes", value=80, start=10, end=300, step=10)
+reviews = Slider(title="Minimum number of reviews", value=80, start=10, end=300, step=10)
 min_year = Slider(title="Year released", start=1940, end=2014, value=1970, step=1)
 max_year = Slider(title="End Year released", start=1940, end=2014, value=2014, step=1)
-oscars = Slider(title="Minimum number of Oscar wins (all categories)", start=0, end=4, value=0, step=1)
+oscars = Slider(title="Minimum number of Oscar wins", start=0, end=4, value=0, step=1)
 boxoffice = Slider(title="Dollars at Box Office (millions)", start=0, end=800, value=0, step=1)
-genre = Select(title="Genre (a movie can have multiple genres)",
-            options=["All", "Action", "Adventure", "Animation", "Biography", "Comedy",
-            "Crime", "Documentary", "Drama", "Family", "Fantasy", "History",
-            "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi",
-            "Short", "Sport", "Thriller", "War", "Western"], value="All")
-director = TextInput(title="Director name contains (e.g., Miyazaki)")
-cast = TextInput(title="Cast names contains (e.g. Tom Hanks)")
-x_axis = Select(title="X Axis", options=list(axis_vars.keys()), value="Tomato Meter")
-y_axis = Select(title="Y Axis", options=list(axis_vars.keys()), value="Number of reviews")
-# movie_count_txt = Paragraph(text="Number of selected movies: 0")
-movie_count_txt = Paragraph(text="Number of selected movies: 0")
-
-def select_movies():
-    genre_choice = genre.value
-    director_entry = director.value.strip()
-    cast_entry = cast.value.strip()
-    selected = movies[(movies.Reviews >= reviews.value) &
-                        (movies.BoxOffice >= (boxoffice.value * 1e6)) &
-                        (movies.Year >= min_year.value) &
-                        (movies.Year <= max_year.value) &
-                        (movies.Oscars >= oscars.value) ]
-    if (genre_choice != "All"):
-        selected = selected[selected.Genre.str.contains(genre_choice)==True]
-    if (director_entry != ""):
-        selected = selected[selected.Director.str.contains(director_entry)==True]
-    if (cast_entry != ""):
-        selected = selected[selected.Cast.str.contains(cast_entry)==True]
-    return selected
+genre = Select(title="Genre", value="All",
+               options=open(join(dirname(__file__), 'genres.txt')).read().split())
+director = TextInput(title="Director name contains")
+cast = TextInput(title="Cast names contains")
+x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Tomato Meter")
+y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="Number of Reviews")
 
 # Create Column Data Source that will be used by the plot
-df = ColumnDataSource(data=dict(x="", y="", pt_color="", title="", year="", revenue=""))
+source = ColumnDataSource(data=dict(x=[], y=[], color=[], title=[], year=[], revenue=[]))
 
-hover = HoverTool(
-        tooltips=[
-            ("Title","@title"),
-            ("Year", "@year"),
-            ("$", "@revenue")
-        ]
-)
+hover = HoverTool(tooltips=[
+    ("Title","@title"),
+    ("Year", "@year"),
+    ("$", "@revenue")
+])
 
-p = Figure(plot_height=600, plot_width=800, title="",
-              toolbar_location=None, tools=[hover])
-p.circle(x="x", y="y", source=df, size=6, color="pt_color", fill_alpha=0.2)
-p.yaxis.axis_label = "Number of Reviews"
-p.xaxis.axis_label = "Tomato Meter"
+p = Figure(plot_height=600, plot_width=800, title="", toolbar_location=None, tools=[hover])
+p.circle(x="x", y="y", source=source, size=7, color="color", line_color=None, fill_alpha=0.4)
 
-# Callback to update the plot when an input changes
-def update_data(attrname, old, new):
-    # Get the name of the variable selected from the Axis dropdowns
-    print("----- in update_data")
-    x_var = axis_vars[x_axis.value]
-    y_var = axis_vars[y_axis.value]
-    # Get data that meets criteria and update the plot
-    new_df = select_movies()
-    df.data = dict(x=new_df[x_var], y=new_df[y_var], pt_color=new_df["pt_color"], title=new_df["Title"], year=new_df["Year"], revenue=new_df["revenue"])
+def select_movies():
+    genre_val = genre.value
+    director_val = director.value.strip()
+    cast_val = cast.value.strip()
+    selected = movies[
+        (movies.Reviews >= reviews.value) &
+        (movies.BoxOffice >= (boxoffice.value * 1e6)) &
+        (movies.Year >= min_year.value) &
+        (movies.Year <= max_year.value) &
+        (movies.Oscars >= oscars.value)
+    ]
+    if (genre_val != "All"):
+        selected = selected[selected.Genre.str.contains(genre_val)==True]
+    if (director_val != ""):
+        selected = selected[selected.Director.str.contains(director_val)==True]
+    if (cast_val != ""):
+        selected = selected[selected.Cast.str.contains(cast_val)==True]
+    return selected
+
+def update(attrname, old, new):
+    df = select_movies()
+    x_name = axis_map[x_axis.value]
+    y_name = axis_map[y_axis.value]
+
     p.xaxis.axis_label = x_axis.value
     p.yaxis.axis_label = y_axis.value
+    p.title = "%d movies selected" % len(df)
+    source.data = dict(
+        x=df[x_name],
+        y=df[y_name],
+        color=df["color"],
+        title=df["Title"],
+        year=df["Year"],
+        revenue=df["revenue"],
+    )
 
-# Assign callback to variables. Currently using just a single callback.
-for control in [reviews, boxoffice, genre, director, cast, min_year, max_year, oscars, x_axis, y_axis]:
-    control.on_change('value', update_data)
+controls = [reviews, boxoffice, genre, min_year, max_year, oscars, director, cast, x_axis, y_axis]
+for control in controls:
+    control.on_change('value', update)
 
-# Initial load of the data so visibile on first display.
-# Could have loaded the intial data when creating the ColumnDataSource.
-update_data("", "", "")
+inputs = HBox(VBoxForm(controls), width=300)
 
-# Place all inputs in a column. Use HBox to limit width of controls and
-# set the hights so the controls will be closer together than default.
-inputs=VBoxForm(children=[HBox(reviews, width=250, height=80),
-                        HBox(min_year, width=250, height=50),
-                        HBox(max_year, width=250, height=70),
-                        HBox(oscars, width=250, height=90),
-                        HBox(boxoffice, width=250, height=80),
-                        HBox(genre, width=200, height=80),
-                        HBox(director, width=200, height=90),
-                        HBox(cast, width=200, height=90),
-                        HBox(x_axis, width=150, height=60),
-                        HBox(y_axis, width=150, height=60)])
+update(None, None, None) # initial load of the data
 
-plot_out=VBoxForm(children=[p, movie_count_txt])
-
-# Add the inputs and plot to the document so they will be displayed.
-curdoc().add(HBox(inputs, plot_out))
+curdoc().add_root(HBox(inputs, p))
