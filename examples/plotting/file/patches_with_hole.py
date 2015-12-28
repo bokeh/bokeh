c18cbae9637dbdf4f92f531cdbc842c9cc15e2ad
Sarah Bird <sarah@bonvaya.com>
2015-12-28 15:20:53 -0500

    Improve example

--- examples/plotting/file/patches_with_hole.py
+++ examples/plotting/file/patches_with_hole.py
@@ -1,37 +1,28 @@
 import numpy as np
-from bokeh.plotting import figure, show, output_file
-from bokeh.models import HoverTool, TapTool, ColumnDataSource
+from bokeh.io import output_file, vplot, show
+from bokeh.plotting import figure
+from bokeh.models import HoverTool, ColumnDataSource
 
 output_file("patches_with_hole.html", title="patches_with_holes.py example")
 
 shapes = [
     {
-        'description': 'A single patch with a hole (filled)',
+        'description': 'A single patch with a hole',
         'xs': [[1, 1.1, 2, 1.9], [1.8, 1.7, 1.2, 1.2]],
         'ys': [[1, 2.0, 2, 1.0], [1.1, 1.9, 1.9, 1.2]],
-        'fill_color': 'PowderBlue',
-        'line_color': None,
-    },
-    {
-        'description': 'A single patch with a hole (no fill)',
-        'xs': [[0, 0.1, 1, 0.9], [0.8, 0.7, 0.2, 0.2]],
-        'ys': [[1, 2.0, 2, 1.0], [1.1, 1.9, 1.9, 1.2]],
-        'fill_color': None,
-        'line_color': 'PowderBlue',
+        'color': 'PowderBlue',
     },
     {
         'description': 'A single solid patch',
         'xs': [3, 3.1, 4, 3.9],
         'ys': [3, 4.0, 4, 3.0],
-        'fill_color': 'green',
-        'line_color': None,
+        'color': 'green',
     },
     {
         'description': 'Two patches with no holes (seperated by np.NaN)',
         'xs': [5, 6, 5.5, np.NaN, 5, 6, 5.5],
         'ys': [3, 4, 4.0, np.NaN, 1, 2, 2.0],
-        'fill_color': 'pink',
-        'line_color': None,
+        'color': 'pink',
     },
     {
         'description': 'Two patches, one with a hole, one with no hole',
@@ -45,8 +36,7 @@ shapes = [
             np.NaN,
             [1, 2.0, 2, 1.0]
         ],
-        'fill_color': 'gold',
-        'line_color': None,
+        'color': 'gold',
     },
 ]
 
@@ -54,17 +44,13 @@ cds = ColumnDataSource({
     'description': [shape['description'] for shape in shapes],
     'xs': [shape['xs'] for shape in shapes],
     'ys': [shape['ys'] for shape in shapes],
-    'fill_colors': [shape['fill_color'] for shape in shapes],
-    'line_colors': [shape['line_color'] for shape in shapes],
+    'colors': [shape['color'] for shape in shapes],
 })
 
 
-hover = HoverTool(tooltips='@description')
-tap = TapTool()
-plot = figure(title="Shapes are grouped by color (hover for more info)", tools=[hover, tap])
-plot.patches(
-    xs='xs', ys='ys', fill_color='fill_colors', line_color='line_colors',
-    fill_alpha=0.6, source=cds,
-)
+filled_plot = figure(title="With fill (hover for more info)", tools=[HoverTool(tooltips='@description')])
+filled_plot.patches(xs='xs', ys='ys', fill_color='colors', fill_alpha=0.6, line_color=None, source=cds)
+stroked_plot = figure(title="With stroke (hover for more info)", tools=[HoverTool(tooltips='@description')])
+stroked_plot.patches(xs='xs', ys='ys', fill_color=None, line_color='colors', source=cds)
 
-show(plot)
+show(vplot(filled_plot, stroked_plot))
