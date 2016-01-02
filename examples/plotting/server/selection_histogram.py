01ed27a095a68a6560cb0080269f77ad72f09fa6
fpliger <fabio.pliger@gmail.com>
2016-01-02 11:41:54 -0600

    add comment about space tweaking

--- examples/plotting/server/selection_histogram.py
+++ examples/plotting/server/selection_histogram.py
@@ -58,6 +58,9 @@ pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vhist, color="white", 
 vh1 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.5, **LINE_ARGS)
 vh2 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.1, **LINE_ARGS)
 
+# NOTE: Version 0.11 has introduced auto spacing by default on VBox/HBox/vplot/hplot
+#       so for now we must tweak some spacing and borders to have it closely
+#       aligned. 
 pv.min_border_top = 80
 pv.min_border_left = 0
 ph.min_border_top = 10
