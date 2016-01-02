fb3b7d65f95f17075c7e44fe5aea03beb6a4b618
Nick Roth <nlr06886@gmail.com>
2016-01-01 22:25:27 -0600

    Fix indenting in donut.

--- bokeh/charts/builders/donut_builder.py
+++ bokeh/charts/builders/donut_builder.py
@@ -74,29 +74,29 @@ def Donut(data, label='index', values=None,  color=None, agg=None,
     .. bokeh-plot::
         :source-position: above
 
-            from bokeh.charts import Donut, show, output_file
-            from bokeh.charts.utils import df_from_json
-            from bokeh.sampledata.olympics2014 import data
+        from bokeh.charts import Donut, show, output_file
+        from bokeh.charts.utils import df_from_json
+        from bokeh.sampledata.olympics2014 import data
 
-            import pandas as pd
+        import pandas as pd
 
-            # utilize utility to make it easy to get json/dict data converted to a dataframe
-            df = df_from_json(data)
+        # utilize utility to make it easy to get json/dict data converted to a dataframe
+        df = df_from_json(data)
 
-            # filter by countries with at least one medal and sort by total medals
-            df = df[df['total'] > 8]
-            df = df.sort("total", ascending=False)
-            df = pd.melt(df, id_vars=['abbr'],
-                         value_vars=['bronze', 'silver', 'gold'],
-                         value_name='medal_count', var_name='medal')
+        # filter by countries with at least one medal and sort by total medals
+        df = df[df['total'] > 8]
+        df = df.sort("total", ascending=False)
+        df = pd.melt(df, id_vars=['abbr'],
+                     value_vars=['bronze', 'silver', 'gold'],
+                     value_name='medal_count', var_name='medal')
 
-            # original example
-            d = Donut(df, label=['abbr', 'medal'], values='medal_count',
-                      text_font_size='8pt', hover_text='medal_count')
+        # original example
+        d = Donut(df, label=['abbr', 'medal'], values='medal_count',
+                  text_font_size='8pt', hover_text='medal_count')
 
-            output_file("donut.html")
+        output_file("donut.html")
 
-            show(d)
+        show(d)
 
     """
 
