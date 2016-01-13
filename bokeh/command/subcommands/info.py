cb9404e759b66814d9627a6d5f6dda2cce0c6f1a
Maggie-M <maggie.mari@continuum.io>
2016-01-13 12:43:33 -0600

    Add version information to info command, update tests

--- bokeh/command/subcommands/info.py
+++ bokeh/command/subcommands/info.py
@@ -7,22 +7,41 @@ to standard output.
 '''
 from __future__ import absolute_import
 
+import sys
+
+from bokeh import __version__
 from bokeh.settings import settings
 
 from ..subcommand import Subcommand
 
 class Info(Subcommand):
-    ''' Subcommand to display the locations of BokehJS static files.
+    ''' Subcommand to print information about Bokeh and Bokeh server configuration.
 
     '''
 
     name = "info"
 
-    help = "output the locations of BokehJS static files"
+    help = "print information about Bokeh and Bokeh server configuration"
 
     args = (
+
+        ('--static', dict(
+            action='store_true',
+            help="Print the locations of BokehJS static files",
+        )),
+
     )
 
     def invoke(self, args):
-        print(settings.bokehjsdir()
-)
+        if args.static:
+            print(settings.bokehjsdir())
+        else:
+            try:
+                import IPython
+                ipy_version = IPython.__version__
+            except ImportError:
+                ipy_version = "Not installed"
+            print("Python version      :  %s" % sys.version.split('\n')[0])
+            print("IPython version     :  %s" % ipy_version)
+            print("Bokeh version       :  %s" % __version__)
+            print("BokehJS static path :  %s" % settings.bokehjsdir())
