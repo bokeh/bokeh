0635cb86dab7dbb04e969fec563a5707026f4833
Luke Canavan <canavandl@gmail.com>
2016-01-19 12:09:26 -0600

    Change Layout base class to Component from Layout

--- bokeh/models/widgets/layouts.py
+++ bokeh/models/widgets/layouts.py
@@ -15,7 +15,7 @@ from ..component import Component
 from .widget import Widget
 
 @abstract
-class Layout(Widget):
+class Layout(Component):
     """ An abstract base class for layout widgets. ``Layout`` is not
     generally useful to instantiate on its own.
 
@@ -37,12 +37,7 @@ class BaseBox(Layout):
     def __init__(self, *args, **kwargs):
         if len(args) > 0 and "children" in kwargs:
             raise ValueError("'children' keyword cannot be used with positional arguments")
-        if (len(args) == 1 and hasattr(args[0], '__iter__') and
-            not isinstance(args[0], Component)):
-            # Note: check that not Component, in case Widget/Layout ever gets __iter__
-            kwargs["children"] = list(args[0])
-        elif len(args) > 0:
-            kwargs["children"] = list(args)
+        kwargs["children"] = list(args)
         super(BaseBox, self).__init__(**kwargs)
 
     @validation.warning(EMPTY_LAYOUT)
