8db49520311596f7bd0a88ceef6f0b57c5a027ef
Sarah Bird <sarah@bonvaya.com>
2016-01-25 20:52:40 -0800

    Use capabilities fixture instead of capabilities.json

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,18 @@ from __future__ import print_function
 
 import os
 import boto
+import pytest
 from boto.s3.key import Key as S3Key
 from boto.exception import NoAuthHandlerFound
 from os.path import join
 
 s3_bucket = "bokeh-travis"
 s3 = "https://s3.amazonaws.com/%s" % s3_bucket
-build_id = os.environ.get('TRAVIS_BUILD_ID')
+build_id = os.environ.get("TRAVIS_BUILD_ID")
 
 
 def pytest_sessionfinish(session, exitstatus):
-    if os.environ.get('UPLOAD_PYTEST_HTML', "False") == "True":
+    if os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True":
         try:
             conn = boto.connect_s3()
             bucket = conn.get_bucket(s3_bucket)
@@ -23,10 +24,17 @@ def pytest_sessionfinish(session, exitstatus):
 
         if upload is True:
             # Can we make this not hard coded and read in the report location from pytest?
-            with open('tests/pytest-report.html', 'r') as f:
+            with open("tests/pytest-report.html", "r") as f:
                 html = f.read()
             filename = join(build_id, "report.html")
             key = S3Key(bucket, filename)
             key.set_metadata("Content-Type", "text/html")
             key.set_contents_from_string(html, policy="public-read")
             print("\n%s Access report at: %s" % ("---", join(s3, filename)))
+
+
+@pytest.fixture(scope="session")
+def capabilities(capabilities):
+    capabilities["browserName"] = "firefox"
+    capabilities["tunnel-identifier"] = os.environ.get("TRAVIS_JOB_NUMBER")
+    return capabilities
