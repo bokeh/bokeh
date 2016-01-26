dc326e22812cb6fa341eee1bbfad1811a6881198
bcollins <bcollins@continuum.io>
2016-01-26 09:20:11 -0600

    changed import after layouts change

--- examples/app/crossfilter/models/models.py
+++ examples/app/crossfilter/models/models.py
@@ -1,11 +1,13 @@
 from bokeh.properties import Dict, String, Any
-from bokeh.models.widgets.layouts import BaseBox
+from bokeh.models.layouts import BaseBox
+from bokeh.core import validation
+from bokeh.core.validation.warnings import EMPTY_LAYOUT
+
 from examples.app.crossfilter.models.helpers import load_component
 
 class StyleableBox(BaseBox):
     '''
-    styleable box inherits from base box and is able to take a dictionary
-    of arbitary css to apply at the element level.
+    styleable box provides element level css_properties as a dictionary
     '''
     __implementation__ = load_component('./styleable_box.coffee')
     css_properties = Dict(String, Any, default=None)
@@ -15,3 +17,7 @@ class StatsBox(BaseBox):
     __implementation__ = load_component('./stats_box.coffee')
     styles = String(default=None)
     display_items = Dict(String, Any, default=None)
+    
+    @validation.warning(EMPTY_LAYOUT)
+    def _check_empty_layout(self):
+        pass
