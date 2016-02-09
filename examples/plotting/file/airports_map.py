a7badba3522929bd89bd7edf473789d92964ffc9
Bryan Van de Ven <bryanv@continuum.io>
2016-02-09 16:10:19 -0600

    use bokeh.plotting in plotting/file

--- examples/plotting/file/airports_map.py
+++ examples/plotting/file/airports_map.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 from bokeh.models import (
-    Range1d, WMTSTileSource, ColumnDataSource, HoverTool, Circle
+    Range1d, WMTSTileSource, ColumnDataSource, HoverTool,
 )
 from bokeh.plotting import figure, show, output_file
 from bokeh.sampledata.airports import data as airports
@@ -22,20 +22,12 @@ y_range = Range1d(start=airports['y'].min() - 10000, end=airports['y'].max() + 1
 
 # create plot and add tools
 p = figure(tools='wheel_zoom,pan', x_range=x_range, y_range=y_range, title=title)
-p.axis.visible = False
+#p.axis.visible = False
 hover_tool = HoverTool(tooltips=[("Name", "@name"), ("Elevation", "@elevation (m)")])
 p.add_tools(hover_tool)
-p.add_tile(tile_source)
+#p.add_tile(tile_source)
 
 # create point glyphs
-point_options = {}
-point_options['x'] = 'x'
-point_options['y'] = 'y'
-point_options['size'] = 9
-point_options['fill_color'] = "#60ACA1"
-point_options['line_color'] = "#D2C4C1"
-point_options['line_width'] = 1.5
-points_glyph = Circle(**point_options)
-p.add_glyph(points_source, points_glyph)
+p.circle(x='x', y='y', size=9, fill_color="#60ACA1", line_color="#D2C4C1", line_width=1.5, source=points_source)
 
 show(p)
