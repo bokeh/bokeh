6a9e844f76b41ce101310900cf0bb93c6f63486c
Sarah Bird <sarah@bonvaya.com>
2016-02-11 16:24:13 -0800

    Got back to firefox for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,8 +35,7 @@ def pytest_sessionfinish(session, exitstatus):
 
 @pytest.fixture(scope="session")
 def capabilities(capabilities):
-    capabilities["browserName"] = "chrome"
-    capabilities["platform"] = "Windows 7"
+    capabilities["browserName"] = "firefox"
     capabilities["tunnel-identifier"] = os.environ.get("TRAVIS_JOB_NUMBER")
     capabilities["webdriverRemoteQuietExceptions"] = False
     capabilities["captureHtml"] = False
