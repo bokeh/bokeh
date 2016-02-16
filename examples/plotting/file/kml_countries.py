0fe9bf14f1b11083c9ad1ca050b3b87b223fa48a
bcollins <bcollins@continuum.io>
2016-02-15 21:56:25 -0600

    small pyflakes fixes

--- examples/plotting/file/kml_countries.py
+++ examples/plotting/file/kml_countries.py
@@ -1,12 +1,9 @@
-from itertools import chain
-
-from bokeh.models import GeoJSONDataSource 
+from bokeh.models import GeoJSONDataSource
 from bokeh.plotting import figure, show, output_file
+from bokeh.sampledata.countries_kml import countries_kml_path
 
 from fastkml import kml
 from geojson import GeometryCollection, dumps
-
-from bokeh.sampledata.countries_kml import countries_kml_path
 
 with open(countries_kml_path) as f:
     content = kml.KML()
@@ -22,12 +19,10 @@ for f in features:
 geom = GeometryCollection(geometries)
 geo_source = GeoJSONDataSource(geojson=dumps(geom))
 
-p = figure(plot_height=600, plot_width=900, tools='pan,wheel_zoom')
-p.background_fill = 'black'
+p = figure(plot_height=600, plot_width=900, tools='pan,wheel_zoom', background_fill='black')
 p.axis.visible = False
 p.grid.grid_line_alpha = 0
-p.patches(xs='xs', ys='ys',
-          source=geo_source, line_color='white', 
+p.patches(xs='xs', ys='ys', source=geo_source, line_color='white',
           line_alpha=.2, fill_alpha=.85)
 
 output_file('kml_countries_example.html', title='KML Countries Example')
