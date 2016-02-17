779889f3fc0feb4227f320cd15d2e026c17fe052
Sarah Bird <sarah@bonvaya.com>
2016-02-17 12:35:26 -0800

    Re-instate the report upload to s3

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,25 +12,25 @@ s3 = "https://s3.amazonaws.com/%s" % s3_bucket
 build_id = os.environ.get("TRAVIS_BUILD_ID")
 
 
-#def pytest_sessionfinish(session, exitstatus):
-#    if os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True":
-#        try:
-#            conn = boto.connect_s3()
-#            bucket = conn.get_bucket(s3_bucket)
-#            upload = True
-#        except NoAuthHandlerFound:
-#            print("Upload was requested but could not connect to S3.")
-#            upload = False
-#
-#        if upload is True:
-#            # Can we make this not hard coded and read in the report location from pytest?
-#            with open("tests/pytest-report.html", "r") as f:
-#                html = f.read()
-#            filename = join(build_id, "report.html")
-#            key = S3Key(bucket, filename)
-#            key.set_metadata("Content-Type", "text/html")
-#            key.set_contents_from_string(html, policy="public-read")
-#            print("\n%s Access report at: %s" % ("---", join(s3, filename)))
+def pytest_sessionfinish(session, exitstatus):
+    if os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True":
+        try:
+            conn = boto.connect_s3()
+            bucket = conn.get_bucket(s3_bucket)
+            upload = True
+        except NoAuthHandlerFound:
+            print("Upload was requested but could not connect to S3.")
+            upload = False
+
+        if upload is True:
+            # Can we make this not hard coded and read in the report location from pytest?
+            with open("tests/pytest-report.html", "r") as f:
+                html = f.read()
+            filename = join(build_id, "report.html")
+            key = S3Key(bucket, filename)
+            key.set_metadata("Content-Type", "text/html")
+            key.set_contents_from_string(html, policy="public-read")
+            print("\n%s Access report at: %s" % ("---", join(s3, filename)))
 
 
 @pytest.fixture(scope="session")
