cc01bfd70fcbd11701648e15426188d1c34cf50f
Sarah Bird <sarah@bonvaya.com>
2016-02-17 13:04:40 -0800

    Test whether file is ready

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,19 @@ import boto
 import pytest
 from boto.s3.key import Key as S3Key
 from boto.exception import NoAuthHandlerFound
-from os.path import join
+from os.path import join, isfile
 
 s3_bucket = "bokeh-travis"
 s3 = "https://s3.amazonaws.com/%s" % s3_bucket
 build_id = os.environ.get("TRAVIS_BUILD_ID")
+# Can we make this not hard coded and read in the report location from pytest?
+pytest_report = "tests/pytest-report.html"
 
 
 def pytest_sessionfinish(session, exitstatus):
-    if os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True":
+    try_upload = os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True"
+    report_ready = isfile(pytest_report)
+    if try_upload and report_ready:
         try:
             conn = boto.connect_s3()
             bucket = conn.get_bucket(s3_bucket)
@@ -23,8 +27,7 @@ def pytest_sessionfinish(session, exitstatus):
             upload = False
 
         if upload is True:
-            # Can we make this not hard coded and read in the report location from pytest?
-            with open("tests/pytest-report.html", "r") as f:
+            with open(pytest_report, "r") as f:
                 html = f.read()
             filename = join(build_id, "report.html")
             key = S3Key(bucket, filename)
