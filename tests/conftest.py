060576768e02c0499282770dd22e35048d62b12e
Sarah Bird <sarah@bonvaya.com>
2016-02-22 21:42:36 -0800

    Improve clarity of session finish function

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import boto
 import pytest
 from boto.s3.key import Key as S3Key
 from boto.exception import NoAuthHandlerFound
-from os.path import join, isfile
+from os.path import join
 
 s3_bucket = "bokeh-travis"
 s3 = "https://s3.amazonaws.com/%s" % s3_bucket
@@ -15,25 +15,32 @@ report_file = "tests/pytest-report.html"
 
 
 def pytest_sessionfinish(session, exitstatus):
-    try_upload = os.environ.get("UPLOAD_PYTEST_HTML", "False") == "True"
-    report_ready = isfile(report_file)
-    if try_upload and report_ready:
-        try:
-            conn = boto.connect_s3()
-            bucket = conn.get_bucket(s3_bucket)
-            upload = True
-        except NoAuthHandlerFound:
-            print("Upload was requested but could not connect to S3.")
-            upload = False
 
-        if upload is True:
-            with open(report_file, "r") as f:
-                html = f.read()
-            filename = join(build_id, "report.html")
-            key = S3Key(bucket, filename)
-            key.set_metadata("Content-Type", "text/html")
-            key.set_contents_from_string(html, policy="public-read")
-            print("\n%s Access report at: %s" % ("---", join(s3, filename)))
+    if os.environ.get("UPLOAD_PYTEST_HTML", "False") != "True":
+        return
+
+    if hasattr(session.config, 'slaveinput'):
+        # when slave nodes (xdist) finish, the report won't be ready
+        return
+
+    try:
+        conn = boto.connect_s3()
+        bucket = conn.get_bucket(s3_bucket)
+
+        with open(report_file, "r") as f:
+            html = f.read()
+
+        filename = join(build_id, "report.html")
+        key = S3Key(bucket, filename)
+        key.set_metadata("Content-Type", "text/html")
+        key.set_contents_from_string(html, policy="public-read")
+        print("\n%s Access report at: %s" % ("---", join(s3, filename)))
+
+    except NoAuthHandlerFound:
+        print("Upload was requested but could not connect to S3.")
+
+    except OSError:
+        print("Upload was requested but report was not generated.")
 
 
 @pytest.fixture(scope="session")
