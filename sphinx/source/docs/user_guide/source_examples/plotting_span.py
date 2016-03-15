3cec2cd1566a8ab5690f182f5cbc804d88967929
Luke Canavan <canavandl@gmail.com>
2016-03-15 17:06:58 -0500

    Update span and box user guide examples to use add_annotation method

--- sphinx/source/docs/user_guide/source_examples/plotting_span.py
+++ sphinx/source/docs/user_guide/source_examples/plotting_span.py
@@ -13,13 +13,12 @@ p.line(daylight_warsaw_2013.Date, daylight_warsaw_2013.Sunset,
 p.line(daylight_warsaw_2013.Date, daylight_warsaw_2013.Sunrise,
        line_dash='dotted', line_width=2, legend="Sunrise")
 
-daylight_savings_start = Span(location=dt(2013, 3, 31, 2, 0, 0).timestamp()*1000,
-                              dimension='height', line_color='green',
-                              line_dash='dashed', line_width=3)
-daylight_savings_end = Span(location=dt(2013, 10, 27, 3, 0, 0).timestamp()*1000,
-                            dimension='height', line_color='red',
-                            line_dash='dashed', line_width=3)
-p.renderers.extend([daylight_savings_start, daylight_savings_end])
+p.add_annotation(Span(location=dt(2013, 3, 31, 2, 0, 0).timestamp()*1000,
+                      dimension='height', line_color='green',
+                      line_dash='dashed', line_width=3))
+p.add_annotation(Span(location=dt(2013, 10, 27, 3, 0, 0).timestamp()*1000,
+                      dimension='height', line_color='red',
+                      line_dash='dashed', line_width=3))
 
 p.title = "2013 Sunrise and Sunset times in Warsaw"
 p.yaxis.axis_label = 'Time of Day'
