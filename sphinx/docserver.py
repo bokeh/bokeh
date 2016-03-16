3431a6c6656fdb3aeeff3f7523ee553165d00ad4
Eike Petersen <eike.petersen@uni-luebeck.de>
2016-03-16 01:46:44 +0100

    Make docserver.py run on windows by using threading instead of os.fork.

--- sphinx/docserver.py
+++ sphinx/docserver.py
@@ -2,15 +2,21 @@ from __future__ import print_function
 
 import flask
 import os
+import threading
+import time
+import webbrowser
 
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+
+
 _basedir = os.path.join("..", os.path.dirname(__file__))
 
 app = flask.Flask(__name__, static_path="/unused")
 PORT=5009
+http_server = HTTPServer(WSGIContainer(app))
 
 """this is a simple server to facilitate developing the docs.  by
 serving up static files from this server, we avoid the need to use a
@@ -32,21 +38,38 @@ def send_pic(filename):
         os.path.join(_basedir,"sphinx/_build/html/"), filename)
 
 
+def open_browser():
+    # Child process
+    time.sleep(0.5)
+    webbrowser.open("http://localhost:%d/en/latest/index.html" % PORT, new="tab")
+
+def serve_http():
+    http_server.listen(PORT)
+    IOLoop.instance().start()
+
+def shutdown_server():
+    ioloop = IOLoop.instance()
+    ioloop.add_callback(ioloop.stop)
+    print("Asked Server to shut down.")
+
+def ui():
+    time.sleep(0.5)
+    input("Press any key to exit...")
+
+
 if __name__ == "__main__":
-    http_server = HTTPServer(WSGIContainer(app))
+
     print("\nStarting Bokeh plot server on port %d..." % PORT)
     print("Visit http://localhost:%d/en/latest/index.html to see plots\n" % PORT)
 
-    pid = os.fork()
-    if pid != 0:
-        # Parent process
-        http_server.listen(PORT)
-        IOLoop.instance().start()
-    else:
-        # Child process
-        import time
-        import webbrowser
-        time.sleep(0.5)
-        webbrowser.open("http://localhost:%d/en/latest/index.html" % PORT, new="tab")
+    t_server = threading.Thread(target=serve_http)  
+    t_server.start()
+    t_browser = threading.Thread(target=open_browser)
+    t_browser.start()
 
+    ui()
 
+    shutdown_server()
+    t_server.join()
+    t_browser.join()
+    print("Server shut down.")
