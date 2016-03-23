1c9afe37394c0f3822d5f36dc7b2dbc16dd98725
Mateusz Paprocki <mattpap@gmail.com>
2016-03-23 12:45:57 +0100

    Allow Color to be a string (at least for now)
    This makes NamedColor pretty much useless.

--- bokehjs/src/coffee/api/typings/types.d.ts
+++ bokehjs/src/coffee/api/typings/types.d.ts
@@ -2,6 +2,7 @@ declare namespace Bokeh {
     export type Int = number;
     export type Percent = number;
     export type Color = NamedColor       |
+                        String           |
                         [Int, Int, Int]         |  // RGB (r, g, b)
                         [Int, Int, Int, Percent];  // RGBA(r, g, b, a)
     export type FontSize = string;
