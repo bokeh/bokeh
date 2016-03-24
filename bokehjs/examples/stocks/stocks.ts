66eaac2d386b24c716bb6604b03af3a4823b468d
Almar Klein <almar.klein@gmail.com>
2016-03-24 22:20:49 +0100

    addres reviewer comments for stock example

--- bokehjs/examples/stocks/stocks.ts
+++ bokehjs/examples/stocks/stocks.ts
@@ -1,3 +1,5 @@
+import plt = Bokeh.Plotting;
+
 console.log(`Bokeh ${Bokeh.version}`);
 Bokeh.set_log_level("debug");
 
@@ -5,15 +7,16 @@ Bokeh.set_log_level("debug");
 function make_plot(title:string, source:Bokeh.ColumnDataSource) {
     
     // Create plot
-    var xr = new Bokeh.DataRange1d({});
-    var yr = new Bokeh.DataRange1d({});
-    var plot = new Bokeh.Plot({        
+    const xr = new Bokeh.DataRange1d({});
+    const yr = new Bokeh.DataRange1d({});
+    const plot = new plt.Figure({
+        tools: "pan,wheel_zoom,save,reset",        
         x_range: xr,
         y_range: yr,         
         title: title,
         plot_width: 400,
         plot_height: 400,
-        border_fill_color: "#eeeeff",
+        background_fill_color: "#eeeeff",
     });
     
     // Add axis and grid
@@ -25,8 +28,8 @@ function make_plot(title:string, source:Bokeh.ColumnDataSource) {
     plot.add_layout(new Bokeh.Grid({ticker: yaxis.ticker, dimension: 1}));
     
     // Add a line for each entry in the source
-    var colors = ['#aa0000','#00aa00', '#0000aa', '#aaaa00', '#aa00aa', '#00aaaa'];
-    var i = -1;
+    const colors = ['#aa0000','#00aa00', '#0000aa', '#aaaa00', '#aa00aa', '#00aaaa'];
+    let i = -1;
     for (let key in source.data) {
         if (key != 't') {
             i += 1;
@@ -36,8 +39,8 @@ function make_plot(title:string, source:Bokeh.ColumnDataSource) {
         }
     }
     
-    // Add tools
-    plot.add_tools(new Bokeh.PanTool(), new Bokeh.WheelZoomTool());
+    // Add tools - not needed if we use Figure
+    //plot.add_tools(new Bokeh.PanTool(), new Bokeh.WheelZoomTool(), new Bokeh.ResetTool(), new Bokeh.PreviewSaveTool());
     
     // Tweaks to work around bootstrap problems 
     xr.renderers = plot.renderers;
@@ -47,27 +50,25 @@ function make_plot(title:string, source:Bokeh.ColumnDataSource) {
 }
 
 // Create source
-var source = new Bokeh.ColumnDataSource({data: {  t: [1000,2000,3000,4000,5000,6000,7000,8000], 
-                                                foo: [1,4,3,5,2,3,2,4],
-                                                bar: [4,5,7,6,8,6,7,4],
-                                               spam: [2,1,2,1,3,1,2,3]} });
+const source = new Bokeh.ColumnDataSource({data: {  t: [1000,2000,3000,4000,5000,6000,7000,8000], 
+                                                  foo: [1,4,3,5,2,3,2,4],
+                                                  bar: [4,5,7,6,8,6,7,4],
+                                                 spam: [2,1,2,1,3,1,2,3]} });
 
 // Make source update on an interval
-var t = 8;
-var period = 0.5;  // seconds
+let t = 8;
+const period = 0.5;  // seconds
 function new_data() {
     t += period;
-    let d = {t:t*1000, 
-             foo: Math.sin(t*0.3+0) * 2 + Math.random() + 3,
-             bar: Math.sin(t*0.5+1) * 2 + Math.random() + 3,
-            spam: Math.sin(t*0.7+2) * 2 + Math.random() + 3}
+    let d:Bokeh.Data = {'t': [t*1000], 
+                      'foo': [Math.sin(t*0.3+0) * 2 + Math.random() + 3],
+                      'bar': [Math.sin(t*0.5+1) * 2 + Math.random() + 3],
+                     'spam': [Math.sin(t*0.7+2) * 2 + Math.random() + 3]}
     source.stream(d, 50);
 }
 setInterval(new_data, period*1000);
 
 // Create plot and attach to DOM
 const plot = make_plot('Simple stocks demo', source);
-const doc = new Bokeh.Document();
-doc.add_root(plot);
 const div = document.getElementById("plot");
-Bokeh.embed.add_document_static(div, doc);
+plt.show(plot, div);
