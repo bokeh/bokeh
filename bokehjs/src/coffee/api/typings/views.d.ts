52b74a073df953c6e51177735f166a4ca808a671
Mateusz Paprocki <mattpap@gmail.com>
2016-04-07 11:49:29 +0200

    Fill-in missing BokehView's members

--- bokehjs/src/coffee/api/typings/views.d.ts
+++ bokehjs/src/coffee/api/typings/views.d.ts
@@ -1,6 +1,20 @@
 declare namespace Bokeh {
-    interface BokehView<ModelType extends Model> {
+   interface ViewOptions<ModelType extends Model> {
+      model: ModelType;
+      id?: string;
+      el?: HTMLElement | JQuery;
+    }
+
+    class BokehView<ModelType extends Model> {
+        constructor(options?: ViewOptions<ModelType>);
+
         model: ModelType;
+        id: string;
+        el: HTMLElement;
+        $el: JQuery;
+
+        render(): this;
+        remove(): this;
     }
 
     type View<ModelType extends Model> = BokehView<ModelType>;
