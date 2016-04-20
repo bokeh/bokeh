c8df475a36b3edfa6828c41818c911fa060755a9
Justace Clutter <justaceclutter@Justaces-MBP.home>
2016-04-19 21:19:43 -0400

    Add @bryevdv suggested change

--- examples/models/transform_interpolator.py
+++ examples/models/transform_interpolator.py
@@ -7,86 +7,49 @@ from bokeh.models.sources import ColumnDataSource
 from bokeh.models import CustomJS, TextInput, Select, Div
 from bokeh.models.transforms import LinearInterpolator, StepInterpolator
 
-N = 1000
-low = 0
-high = 100
-source = ColumnDataSource(data=dict(x=np.linspace(low, high, num=N), y=[None]*N))
-interp_source = ColumnDataSource(data=dict(x=[0,100], y=[0,50]))
+N = 300
 
-linterp = LinearInterpolator(x='x', y='y', data=interp_source)
-sinterp = StepInterpolator(x='x', y='y', data=interp_source)
+controls = ColumnDataSource(data=dict(x=[1, 2, 3, 4, 5], y=[2, 8, 7, 3, 5]))
+source = ColumnDataSource(data=dict(x=[], y=[]))
 
-p = figure(x_range=(low, high), y_range=(low, high))
-p.line(x='x', y='y', source=source)
-p.circle(x='x', y='y', source=interp_source)
+linear = LinearInterpolator(x='x', y='y', data=controls)
+step = StepInterpolator(x='x', y='y', data=controls)
 
-interpolation_settings_callback=CustomJS(args=dict(interp_source=interp_source, source=source, linterp=linterp, sinterp=sinterp, figure=p), code="""
-    xvals = xvals_entry.get('value')
-    yvals = yvals_entry.get('value')
+p = figure(x_range=(0, 6), y_range=(0, 10))
+p.circle(x='x', y='y', source=controls, size=15, alpha=0.5, color="firebrick")
+p.circle(x='x', y='y', source=source, size=2, alpha=0.2, color="navy")
 
-    xvals = eval(xvals)
-    yvals = eval(yvals)
+callback = CustomJS(args=dict(source=source, linear=linear, step=step), code="""
+    var mode = cb_obj.get('value');
+    var data = source.get('data');
+    var dx = 6 / %d;
 
-    if(xvals.length == yvals.length) {
-        if(xvals.length >= 2) {
-            interp_source.get('data')['x'] = xvals
-            interp_source.get('data')['y'] = yvals
+    if (mode == 'None') {
+        data['x'] = [];
+        data['y'] = [];
+    }
+    else {
+        if (mode == 'Linear') { interp = linear; }
+        else if (mode == 'Step (before)') { interp = step; step.set('mode', 'before'); }
+        else if (mode == 'Step (center)') { interp = step; step.set('mode', 'center'); }
+        else if (mode == 'Step (after)')  { interp = step; step.set('mode', 'after');  }
+
+        for (i=0; i < %d; i++) {
+            data['x'][i] = i * dx
+            data['y'][i] = interp.compute(data['x'][i])
         }
     }
 
-    interp_source.trigger('change')
-    linterp.trigger('change')
-    sinterp.trigger('change')
-
-    interp = 0
-
-    if(interp_select.get('value') == 'Linear') {
-      interp = linterp
-    }
-
-    if(interp_select.get('value') == 'Step') {
-      sinterp.set('mode', stepmode_select.get('value').toLowerCase())
-      interp = sinterp
-    }
-
-    Array.prototype.max = function() {
-      return Math.max.apply(null, this);
-    };
-
-    Array.prototype.min = function() {
-      return Math.min.apply(null, this);
-    };
-
-    N = source.get('data')['x'].length
-    sxvals = Array.apply(0, Array(N)).map(function(_,b) { return b + 1; })
-    width = xvals.max() - xvals.min()
-    for (i=0; i < sxvals.length; i++) {
-        sxvals[i] = xvals.min() + (sxvals[i]/(N/width))
-    }
-
-    var data=source.get('data')
-    for (i=0; i < data['y'].length; i++) {
-        data['x'][i] = sxvals[i]
-        data['y'][i] = interp.compute(data['x'][i])
-    }
-
     source.trigger('change')
-    figure.trigger('change')
 
-""")
+""" % (N, N))
 
-xvals = TextInput(title='Control Point X:', value = '[0, 100]', callback=interpolation_settings_callback)
-yvals = TextInput(title='Control Point Y:', value = '[0, 50]', callback=interpolation_settings_callback)
-interp_select = Select(title='Interpolation Method', value='Linear', options=['Linear', 'Step'], callback=interpolation_settings_callback)
-stepmode_select = Select(title='Step Mode', value='After', options=['Before', 'Center', 'After'], callback=interpolation_settings_callback)
-
-interpolation_settings_callback.args['xvals_entry'] = xvals
-interpolation_settings_callback.args['yvals_entry'] = yvals
-interpolation_settings_callback.args['interp_select'] = interp_select
-interpolation_settings_callback.args['stepmode_select'] = stepmode_select
-
-title = Div(text='<H1>Interpolation Parameters</H1>', height=None)
+mode = Select(
+    title='Interpolation Mode',
+    value='None',
+    options=['None', 'Linear', 'Step (before)', 'Step (center)', 'Step (after)'],
+    callback=callback)
 
 output_file("transform_interpolator.html", title="Example Transforms")
 
-show(hplot(p, vplot(title, interp_select, xvals, yvals, stepmode_select)))
+show(vplot(hplot(mode), p))
