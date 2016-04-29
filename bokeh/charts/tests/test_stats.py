35831f3d009da84eb4223af30275dd23f7d20ec4
fpliger <fabio.pliger@gmail.com>
2016-04-29 15:19:36 -0500

    add tests for new Histogram Stat

--- bokeh/charts/tests/test_stats.py
+++ bokeh/charts/tests/test_stats.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bokeh.charts.stats import Bins
+from bokeh.charts.stats import Bins, Histogram
 from bokeh.models import ColumnDataSource
 
 import pandas as pd
@@ -30,3 +30,21 @@ def test_auto_bin_count(ds):
 def test_bin_labeling(ds):
     Bins(source=ds, column='cyl', bin_count=2)
     assert len(pd.Series(ds.data['cyl_bin']).drop_duplicates()) == 2
+
+
+def test_histogram_wo_density():
+    values = range(10)
+    h = Histogram(values=values, bin_count=3)
+
+    assert len(h.bins) == 3
+    assert [b.label[0] for b in h.bins] == ['[0.0, 3.0]', '(3.0, 6.0]', '(6.0, 9.0]']
+    assert [b.values[0] for b in h.bins] == [3, 3, 4]
+
+
+def test_histogram_w_density():
+    values = range(10)
+    h = Histogram(values=values, bin_count=3, density=True)
+
+    assert len(h.bins) == 3
+    assert [b.label[0] for b in h.bins] == ['[0.0, 3.0]', '(3.0, 6.0]', '(6.0, 9.0]']
+    assert [b.values[0] for b in h.bins] == [0.1, 0.1, 0.13333333333333333]
