3d6274630af2743ee4d97f1faedc64b426c082fe
Sarah Bird <sarah@bonvaya.com>
2016-05-04 16:42:35 -0700

    Remove add_annotation method; never use p.renderers.extend
    Fixes: #4267
    
    * Remove add_annotation method in favor of add_layout (see below)
    * Always use add_layout in examples & docs (never p.renderers.extend)
    
    The add_annotation method is essentially the same as the add_layout
    method and it seems unecessary (and a little confusing, at least to me) to have both.
    
    The method has never been in a release of Bokeh so doesn't need to be deprecated.
    
    We were already adding legends (which are annotations) with the add_layout
    method, so I believe this is consistent. FWIW I don't think add_layout is the
    most intuitive name, but it's the one we've got.

--- sphinx/source/docs/user_guide/source_examples/plotting_box_annotation.py
+++ sphinx/source/docs/user_guide/source_examples/plotting_box_annotation.py
@@ -11,14 +11,15 @@ data = data.ix['2010-10-06':'2010-10-13']
 
 p = figure(x_axis_type="datetime", tools=TOOLS)
 
-p.line(data.index.to_series(), data['glucose'],
-       line_color="gray", line_width=1, legend="glucose")
+p.line(data.index.to_series(), data['glucose'], line_color="gray", line_width=1, legend="glucose")
 
-low_box = BoxAnnotation(plot=p, top=80, fill_alpha=0.1, fill_color='red')
-mid_box = BoxAnnotation(plot=p, bottom=80, top=180, fill_alpha=0.1, fill_color='green')
-high_box = BoxAnnotation(plot=p, bottom=180, fill_alpha=0.1, fill_color='red')
+low_box = BoxAnnotation(top=80, fill_alpha=0.1, fill_color='red')
+mid_box = BoxAnnotation(bottom=80, top=180, fill_alpha=0.1, fill_color='green')
+high_box = BoxAnnotation(bottom=180, fill_alpha=0.1, fill_color='red')
 
-p.renderers.extend([low_box, mid_box, high_box])
+p.add_layout(low_box)
+p.add_layout(mid_box)
+p.add_layout(high_box)
 
 p.title = "Glucose Range"
 p.xgrid[0].grid_line_color=None
