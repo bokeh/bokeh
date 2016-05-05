c513d589a6ae528ce7dbec1501f89cd3f4a6425d
fpliger <fabio.pliger@gmail.com>
2016-05-05 11:30:24 -0500

    change last histogram to use non uniform custom sized bins

--- examples/charts/file/histogram_multi.py
+++ examples/charts/file/histogram_multi.py
@@ -15,8 +15,8 @@ hist4 = Histogram(df, values='hp', color='cyl',
 
 hist5 = Histogram(df, values='mpg', bins=50,
                   title="df, values='mpg', bins=50")
-hist6 = Histogram(df, values='mpg', bins=[10, 20, 30, 40],
-                  title="df, values='mpg', bins=[10, 20, 30, 40")
+hist6 = Histogram(df, values='mpg', bins=[10, 15, 25, 100], tooltips=[('Bin', "@label")],
+                  title="df, values='mpg', bins=[10, 15, 25, 100]")
 
 output_file("histogram_multi.html", title="histogram_multi.py example")
 
