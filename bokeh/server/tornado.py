0d1164549640dbd9213b9ed45795a9e8b00ef565
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-06-02 11:38:04 -0500

    Server: Implement a RootHandler to `/` (#4415)
    * Implement a RootHandler to `/` that lists available applications or redirects
    
    * Add --disable-index flag to disable default index and redirection
    
    * Add --disable-index to test

--- bokeh/server/tornado.py
+++ bokeh/server/tornado.py
@@ -22,6 +22,7 @@ from tornado.web import StaticFileHandler
 from bokeh.resources import Resources
 from bokeh.settings import settings
 
+from .views.root_handler import RootHandler
 from .urls import per_app_patterns, toplevel_patterns
 from .connection import ServerConnection
 from .application_context import ApplicationContext
@@ -142,6 +143,7 @@ class BokehTornado(TornadoApplication):
         unused_session_lifetime_milliseconds (int) : number of milliseconds for unused session lifetime
         stats_log_frequency_milliseconds (int) : number of milliseconds between logging stats
         develop (boolean) : True for develop mode
+        use_index (boolean) : True to generate an index of the running apps in the RootHandler
 
     '''
 
@@ -160,9 +162,11 @@ class BokehTornado(TornadoApplication):
                  unused_session_lifetime_milliseconds=15000,
                  # how often to log stats
                  stats_log_frequency_milliseconds=15000,
-                 develop=False):
+                 develop=False,
+                 use_index=True):
 
         self._prefix = prefix
+        self.use_index = use_index
 
         if io_loop is None:
             io_loop = IOLoop.current()
@@ -230,8 +234,14 @@ class BokehTornado(TornadoApplication):
                 all_patterns.append((route, StaticFileHandler, { "path" : app.static_path }))
 
         for p in extra_patterns + toplevel_patterns:
-            prefixed_pat = (self._prefix+p[0],) + p[1:]
-            all_patterns.append(prefixed_pat)
+            if p[1] == RootHandler:
+                if self.use_index:
+                    data = {"applications": self._applications, "prefix": self._prefix}
+                    prefixed_pat = (self._prefix + p[0],) + p[1:] + (data,)
+                    all_patterns.append(prefixed_pat)
+            else:
+                prefixed_pat = (self._prefix + p[0],) + p[1:]
+                all_patterns.append(prefixed_pat)
 
         for pat in all_patterns:
             _whitelist(pat[1])
