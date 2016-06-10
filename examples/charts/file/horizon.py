af5acce70d2ab02a38d13323b7f51cbf37b2d01b
Luke Canavan <canavandl@gmail.com>
2016-06-10 18:13:07 -0500

    Update examples to implicitly use DatetimeIndex

--- examples/charts/file/horizon.py
+++ examples/charts/file/horizon.py
@@ -15,14 +15,13 @@ IBM = pd.read_csv(
     "http://ichart.yahoo.com/table.csv?s=IBM&a=0&b=1&c=2000&d=0&e=1&f=2010",
     parse_dates=['Date'])
 
-data = dict([
-    ('AAPL', AAPL['Adj Close']),
-    ('Date', AAPL['Date']),
-    ('MSFT', MSFT['Adj Close']),
-    ('IBM', IBM['Adj Close'])]
-)
+data = pd.DataFrame(data=dict(AAPL=AAPL['Adj Close'],
+                              MSFT=MSFT['Adj Close'],
+                              IBM=IBM['Adj Close'],
+                              Date=AAPL['Date'])).set_index('Date')
 
-hp = Horizon(data, x='Date', plot_width=800, plot_height=300, title="horizon plot using stock inputs")
+hp = Horizon(data, plot_width=800, plot_height=300, 
+             title="horizon plot using stock inputs", xlabel='Date')
 
 output_file("horizon.html", title="horizon.py example")
 
