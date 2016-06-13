1db682c397c1a8f11929bebc9e0b078bc1306670
Luke Canavan <canavandl@gmail.com>
2016-06-13 12:59:34 -0500

    Add LogColorMapper class to Bokeh

--- bokeh/models/mappers.py
+++ bokeh/models/mappers.py
@@ -50,17 +50,47 @@ class LinearColorMapper(ColorMapper):
     this are clamped to ``high``.
     """)
 
-    # TODO: (jc) what is the color code for transparent?
-    # TODO: (bev) better docstring
-    reserve_color = Color("#ffffff", help="""
-    Used by Abstract Rendering.
+    def __init__(self, palette=None, **kwargs):
+        if palette is not None: kwargs['palette'] = palette
+        super(LinearColorMapper, self).__init__(**kwargs)
+
+class LogColorMapper(ColorMapper):
+    """ Map numbers in a range [*low*, *high*] into a
+    sequence of colors (a palette) on a logscale.
+
+    For example, if the range is [0, 1] and the palette is
+    ``['red', 'green', 'blue']``, the values would be mapped as
+    follows::
+
+             x < 0  : 'red'     # values < low are clamped
+        0 >= x < 33 : 'red'
+       33 >= x < 66 : 'green'
+       66 >= x < 99 : 'blue'
+       99 >= x      : 'blue'    # values > high are clamped
+
+    """
+
+    palette = Seq(Color, help="""
+    A sequence of colors to use as the target palette for mapping.
+
+    This property can also be set as a ``String``, to the name of
+    any of the palettes shown in :ref:`bokeh.palettes`.
+    """).accepts(Enum(Palette), lambda pal: getattr(palettes, pal))
+
+    low = Float(help="""
+    The minimum value of the range to map into the palette. Values below
+    this are clamped to ``low``.
     """)
 
-    # TODO: (bev) better docstring
-    reserve_val = Float(default=None, help="""
-    Used by Abstract Rendering.
+    high = Float(help="""
+    The maximum value of the range to map into the palette. Values above
+    this are clamped to ``high``.
+    """)
+
+    log_base = Float(default=10, help="""
+    Log base.
     """)
 
     def __init__(self, palette=None, **kwargs):
         if palette is not None: kwargs['palette'] = palette
-        super(LinearColorMapper, self).__init__(**kwargs)
+        super(LogColorMapper, self).__init__(**kwargs)
