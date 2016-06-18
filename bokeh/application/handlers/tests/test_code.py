3cf93f79d791fed271cf9407c9e9acc7f178506f
Bryan Van de Ven <bryanv@continuum.io>
2016-06-18 14:44:43 -0500

    add some tests

--- bokeh/application/handlers/tests/test_code.py
+++ bokeh/application/handlers/tests/test_code.py
@@ -1,6 +1,4 @@
-from __future__ import absolute_import, print_function
-
-import unittest
+import pytest
 
 from bokeh.application.handlers import CodeHandler
 from bokeh.document import Document
@@ -36,69 +34,131 @@ curdoc().add_root(AnotherModelInTestScript())
 curdoc().add_root(SomeModelInTestScript())
 """
 
-class TestCodeHandler(unittest.TestCase):
+script_uses_iofuncs = """
+from bokeh.io import curdoc
+from bokeh.model import Model
+from bokeh.core.properties import Int
+import bokeh.io as io
 
-    def test_empty_script(self):
-        doc = Document()
-        handler = CodeHandler(source="# This script does nothing", filename="/test_filename")
-        handler.modify_document(doc)
-        if handler.failed:
-            raise RuntimeError(handler.error)
+io.output_server()
+io.output_notebook()
+io.output_file("foo")
 
-        assert not doc.roots
+class IOFuncModelInTestScript(Model):
+    bar = Int(1)
 
-    def test_script_adds_roots(self):
-        doc = Document()
-        handler = CodeHandler(source=script_adds_two_roots, filename="/test_filename")
-        handler.modify_document(doc)
-        if handler.failed:
-            raise RuntimeError(handler.error)
+curdoc().add_root(IOFuncModelInTestScript())
 
-        assert len(doc.roots) == 2
+io.save()
+io.show()
+io.push()
+io.reset_output()
 
-    def test_script_bad_syntax(self):
-        doc = Document()
-        handler = CodeHandler(source="This is a syntax error", filename="/test_filename")
-        handler.modify_document(doc)
+"""
 
-        assert handler.error is not None
-        assert 'Invalid syntax' in handler.error
+script_uses_client = """
+from bokeh.io import curdoc
+from bokeh.model import Model
+from bokeh.core.properties import Int
+import bokeh.client as client
 
-    def test_script_runtime_error(self):
-        doc = Document()
-        handler = CodeHandler(source="raise RuntimeError('nope')", filename="/test_filename")
-        handler.modify_document(doc)
+session = client.push_session()
 
-        assert handler.error is not None
-        assert 'nope' in handler.error
+class ClientModelInTestScript(Model):
+    bar = Int(1)
 
-    def test_script_sys_path(self):
-        doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("path: '%s'" % sys.path[0])""", filename="/test_filename")
-        handler.modify_document(doc)
+curdoc().add_root(ClientModelInTestScript())
 
-        assert handler.error is not None
-        assert "path: ''" in handler.error
+session.show()
+session.loop_until_closed()
 
-    def test_script_cwd(self):
-        doc = Document()
-        handler = CodeHandler(source="""import os; raise RuntimeError("cwd: '%s'" % os.getcwd())""", filename="/test_filename")
-        handler.modify_document(doc)
+"""
 
-        assert handler.error is not None
-        assert "cwd: '/'" in handler.error
 
-    def test_script_argv(self):
-        doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename")
-        handler.modify_document(doc)
+def test_empty_script():
+    doc = Document()
+    handler = CodeHandler(source="# This script does nothing", filename="/test_filename")
+    handler.modify_document(doc)
+    if handler.failed:
+        raise RuntimeError(handler.error)
 
-        assert handler.error is not None
-        assert "argv: ['test_filename']" in handler.error
+    assert not doc.roots
 
-        doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename", argv=[10, 20, 30])
-        handler.modify_document(doc)
+def test_script_adds_roots():
+    doc = Document()
+    handler = CodeHandler(source=script_adds_two_roots, filename="/test_filename")
+    handler.modify_document(doc)
+    if handler.failed:
+        raise RuntimeError(handler.error)
 
-        assert handler.error is not None
-        assert "argv: ['test_filename', 10, 20, 30]" in handler.error
+    assert len(doc.roots) == 2
+
+def test_script_bad_syntax():
+    doc = Document()
+    handler = CodeHandler(source="This is a syntax error", filename="/test_filename")
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert 'Invalid syntax' in handler.error
+
+def test_script_runtime_error():
+    doc = Document()
+    handler = CodeHandler(source="raise RuntimeError('nope')", filename="/test_filename")
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert 'nope' in handler.error
+
+def test_script_sys_path():
+    doc = Document()
+    handler = CodeHandler(source="""import sys; raise RuntimeError("path: '%s'" % sys.path[0])""", filename="/test_filename")
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert "path: ''" in handler.error
+
+def test_script_cwd():
+    doc = Document()
+    handler = CodeHandler(source="""import os; raise RuntimeError("cwd: '%s'" % os.getcwd())""", filename="/test_filename")
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert "cwd: '/'" in handler.error
+
+def test_script_argv():
+    doc = Document()
+    handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename")
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert "argv: ['test_filename']" in handler.error
+
+    doc = Document()
+    handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename", argv=[10, 20, 30])
+    handler.modify_document(doc)
+
+    assert handler.error is not None
+    assert "argv: ['test_filename', 10, 20, 30]" in handler.error
+
+def test_script_uses_iofuncs():
+    doc = Document()
+    handler = CodeHandler(source=script_uses_iofuncs, filename="/test_filename")
+    handler._logger_text = "%s, %s"
+    handler.modify_document(doc)
+    if handler.failed:
+        raise RuntimeError(handler.error)
+    assert handler.failed is False
+    assert handler.error is None
+    assert len(doc.roots) == 1
+    import sys; sys.stderr.flush()
+
+def test_script_uses_client():
+    doc = Document()
+    handler = CodeHandler(source=script_uses_client, filename="/test_filename")
+    handler._logger_text = "%s, %s"
+    handler.modify_document(doc)
+    if handler.failed:
+        raise RuntimeError(handler.error)
+    assert handler.failed is False
+    assert handler.error is None
+    assert len(doc.roots) == 1
