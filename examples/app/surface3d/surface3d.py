4b1b14410fe945f9d6ce0fcd42c7d7a0cfedc40e
Bryan Van de Ven <bryanv@continuum.io>
2016-06-21 10:24:30 -0500

    refresh and cleanup

--- examples/app/surface3d/surface3d.py
+++ examples/app/surface3d/surface3d.py
@@ -3,6 +3,10 @@ from os.path import dirname, join
 from bokeh.core.properties import Any, Dict, Instance, String
 from bokeh.models import ColumnDataSource, LayoutDOM
 
+# This defines some default options for the Graph3d feature of vis.js
+# See: http://visjs.org/graph3d_examples.html for more details. Note
+# that we are fixing the size of this component, in ``options``, but
+# with additional work it could be made more responsive.
 DEFAULTS = {
     'width':          '600px',
     'height':         '600px',
@@ -19,10 +23,33 @@ DEFAULTS = {
     }
 }
 
+# This custom extension model will have a DOM view that should layout-able in
+# Bokeh layouts, so use ``LayoutDOM`` as the base class. If you wanted to create
+# a custom tool, you could inherit from ``Tool``, or from ``Glyph`` if you
+# wanted to create a custom glyph, etc.
 class Surface3d(LayoutDOM):
 
-    __implementation__ = open(join(dirname(__file__), "surface3d.js")).read()
+    # The special class attribute ``__implementation__`` should contain a string
+    # of JavaScript (or CoffeeScript) code that implements the JavaScript side
+    # of the custom extension model.
+    __implementation__ = open(join(dirname(__file__), "surface3d.coffee")).read()
 
+    # Below are all the "properties" for this model. Bokeh properties are
+    # class attributes that define the fields (and their types) that can be
+    # communicated automatically between Python and the browser. Properties
+    # also support type validation. More information about properties in
+    # can be found here:
+    #
+    #    http://bokeh.pydata.org/en/latest/docs/reference/core.html#bokeh-core-properties
+
+    # This is a Bokeh ColumnDataSource that can be updated in the Bokeh
+    # server by Python code
+    data_source = Instance(ColumnDataSource)
+
+    # The vis.js library that we are wrapping expects data for x, y, z, and
+    # color. The data will actually be stored in the ColumnDataSource, but
+    # these properties let us specify the *name* of the column that should
+    # be used for each field.
     x = String
 
     y = String
@@ -31,6 +58,6 @@ class Surface3d(LayoutDOM):
 
     color = String
 
-    data_source = Instance(ColumnDataSource)
-
+    # Any of the available vis.js options for Graph3d can be set by changing
+    # the contents of this dictionary.
     options = Dict(String, Any, default=DEFAULTS)
