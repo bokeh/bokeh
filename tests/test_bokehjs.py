6bb915cd7d7681cfabbe87c92169d0510ffc2fb6
Bryan Van de Ven <bryanv@continuum.io>
2016-07-03 18:03:44 -0500

    use gulp.cmd on windows

--- tests/test_bokehjs.py
+++ tests/test_bokehjs.py
@@ -3,19 +3,24 @@ from __future__ import print_function
 import os
 import pytest
 from os.path import join
+import sys
 import unittest
 import subprocess
 
+if sys.platform == "win32":
+    GULP = "gulp.cmd"
+else:
+    GULP = "gulp"
 
 @pytest.mark.js
 class TestBokehJS(unittest.TestCase):
 
     def test_bokehjs(self):
         os.chdir('bokehjs')
-        proc = subprocess.Popen([join('node_modules', '.bin', 'gulp'), "test"],
+        proc = subprocess.Popen([join('node_modules', '.bin', GULP), "test"],
                                 stdout=subprocess.PIPE)
-        result = proc.wait()
-        msg = proc.stdout.read().decode('utf-8', errors='ignore')
+        out, errs = proc.communicate()
+        msg = out.decode('utf-8', errors='ignore')
         print(msg)
         if result != 0:
             assert False
