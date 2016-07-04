3635dfceca7e10045ee07fefe8792f768da1278e
Bryan Van de Ven <bryanv@continuum.io>
2016-07-03 22:39:04 -0500

    some windows file related fixes

--- bokeh/application/handlers/tests/test_directory.py
+++ bokeh/application/handlers/tests/test_directory.py
@@ -10,30 +10,9 @@ import os.path
 import jinja2
 
 from bokeh.application.handlers import DirectoryHandler
-from bokeh.document import Document
 from bokeh.core.templates import FILE
-
-class TmpDir(object):
-    def __init__(self, prefix):
-        self._dir = tempfile.mkdtemp(prefix=prefix)
-
-    def __exit__(self, type, value, traceback):
-        shutil.rmtree(path=self._dir)
-
-    def __enter__(self):
-        return self._dir
-
-def _with_directory_contents(contents, func):
-    with (TmpDir(prefix="bokeh-directory-handler-test")) as dirname:
-        for filename, file_content in contents.items():
-            filepath = os.path.join(dirname, filename)
-            filedir = os.path.dirname(filepath)
-            if not os.path.exists(filedir):
-                os.makedirs(filedir)
-            f = open(filepath, 'w')
-            f.write(file_content)
-            f.flush()
-        func(dirname)
+from bokeh.document import Document
+from bokeh.util.testing import TmpDir, with_directory_contents
 
 script_adds_two_roots_template = """
 from bokeh.io import curdoc
@@ -76,7 +55,7 @@ class TestDirectoryHandler(unittest.TestCase):
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : "# This script does nothing"
         }, load)
 
@@ -90,7 +69,7 @@ class TestDirectoryHandler(unittest.TestCase):
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : script_adds_two_roots('SomeModelInTestDirectory',
                                               'AnotherModelInTestDirectory')
         }, load)
@@ -113,7 +92,7 @@ attrs:
         foo: 14
 """
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : script_adds_two_roots('SomeModelInTestDirectoryTheme',
                                               'AnotherModelInTestDirectoryTheme') +
             """
@@ -147,7 +126,7 @@ some.foo = 57
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : script_adds_two_roots('SomeModelInTestDirectoryWithLifecycle',
                                               'AnotherModelInTestDirectoryWithLifecycle'),
             'server_lifecycle.py' : script_has_lifecycle_handlers
@@ -172,7 +151,7 @@ some.foo = 57
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : "# This script does nothing",
             'static/js/foo.js' : "# some JS"
         }, load)
@@ -193,7 +172,7 @@ some.foo = 57
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : "# This script does nothing",
         }, load)
 
@@ -212,7 +191,7 @@ some.foo = 57
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : "# This script does nothing",
             'templates/index.html' : "<div>some HTML</div>"
         }, load)
@@ -231,7 +210,7 @@ some.foo = 57
             if handler.failed:
                 raise RuntimeError(handler.error)
 
-        _with_directory_contents({
+        with_directory_contents({
             'main.py' : "# This script does nothing",
         }, load)
 
