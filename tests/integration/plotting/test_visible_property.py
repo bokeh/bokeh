4e4d48829432ce954452b4cb2851bb6fc0304c0e
Sarah Bird <sarah@bonvaya.com>
2016-07-22 18:24:33 -0500

    Add selenium test

---
+++ tests/integration/plotting/test_visible_property.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+
+from bokeh.io import save
+from bokeh.plotting import figure
+from tests.integration.utils import has_no_console_errors
+
+import pytest
+pytestmark = pytest.mark.integration
+
+@pytest.mark.screenshot
+def test_visible_property_hides_things_correctly(output_file_url, selenium, screenshot):
+
+    plot = figure()
+    l1 = plot.line([1, 2, 3], [1, 2, 3])
+    l2 = plot.line([1, 2, 3], [2, 4, 6])
+
+    plot.xaxis.visible = False
+    plot.ygrid.visible = False
+    l1.visible = False
+
+    # Save the plot and start the test
+    save(plot)
+    selenium.get(output_file_url)
+    assert has_no_console_errors(selenium)
+    assert screenshot.is_valid()
