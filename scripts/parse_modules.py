c6eb5d30678767f5248971761de761c8f33895d5
Brittain Hard <brittainhard@gmail.com>
2016-08-02 10:14:31 -0500

    Crawl functions alongside classes and methods.

--- scripts/parse_modules.py
+++ scripts/parse_modules.py
@@ -2,7 +2,7 @@ import ast, os
 import yaml
 
 
-exclude = ("tests", "static", "sampledata")
+exclude = ("tests", "static", "sampledata", "mplexplorer")
 
 
 def is_private(name):
@@ -12,20 +12,29 @@ def is_private(name):
         return False
 
 
-def output_classes(file_path, filename):
+def get_classes(file_path, filename):
     with open(file_path, "r") as f:
         source = f.read()
-        p = ast.parse(source)
-        classes = [node for node in ast.walk(p) if isinstance(node, ast.ClassDef) and not is_private(node.name)]
+        parsed = ast.parse(source)
+        classes = [node for node in ast.walk(parsed) if isinstance(node, ast.ClassDef) and not is_private(node.name)]
         class_defs = {"classes": {}}
+        functions = get_functions(parsed)
         for x in classes:
             class_defs["classes"][x.name] = {}
-            functions = []
+            methods = []
             for y in x.body:
                 if type(y) == ast.FunctionDef and not is_private(y.name):
-                    functions.append(y.name)
-            class_defs["classes"][x.name]["methods"] = functions
+                    methods.append(y.name)
+            class_defs["classes"][x.name]["methods"] = methods
+            class_defs["classes"]["functions"] = functions
     return class_defs
+
+
+def get_functions(source):
+    parsed = ast.parse(source)
+    functions = [node for node in ast.walk(parsed) if isinstance(node, ast.FunctionDef) and not is_private(node.name)]
+    functions = [x.name for x in functions]
+    return functions
 
 
 def get_filenames(directory):
@@ -45,13 +54,15 @@ def get_filenames(directory):
 def get_files_dict(filenames):
     files_dict = {}
     for x in filenames:
-        files_dict[x[0]] = output_classes(x[0], x[1])
+        files_dict[x[0]] = get_classes(x[0], x[1])
     return files_dict
 
 
-def dump_yaml(source):
-    with open("classes.yaml", "w") as stream:
-        yaml.dump(source, stream, default_flow_style=False)
+def dump_yaml(directory, output_file):
+    files = get_filenames(directory)
+    files_dict = get_files_dict(files)
+    with open(output_file, "w") as stream:
+        yaml.dump(files_dict, stream, default_flow_style=False)
 
-apple = get_files_dict(get_filenames("../bokeh"))
-dump_yaml(apple)
+
+dump_yaml("../bokeh", "classes.yaml")
