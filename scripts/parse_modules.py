87f695a4c88010134e19bde5f58126dcb7a01c2d
Brittain Hard <brittainhard@gmail.com>
2016-08-02 09:26:16 -0500

    Functions for crawling library.

--- scripts/parse_modules.py
+++ scripts/parse_modules.py
@@ -1,23 +1,57 @@
-import ast
+import ast, os
 import yaml
 
 
-def output_classes(file_path, module_name):
+exclude = ("tests", "static", "sampledata")
+
+
+def is_private(name):
+    if name.startswith("_", 0, 1) and name != "__init__":
+        return True
+    else:
+        return False
+
+
+def output_classes(file_path, filename):
     with open(file_path, "r") as f:
         source = f.read()
         p = ast.parse(source)
-        classes = [node for node in ast.walk(p) if isinstance(node, ast.ClassDef)]
-        class_defs = {module_name: []}
+        classes = [node for node in ast.walk(p) if isinstance(node, ast.ClassDef) and not is_private(node.name)]
+        class_defs = {"classes": {}}
         for x in classes:
-            methods = {x.name: {"methods": []}}
+            class_defs["classes"][x.name] = {}
             functions = []
             for y in x.body:
-                # if type(y) == ast.FunctionDef and not y.name.startswith("_", 0, 1):
-                if type(y) == ast.FunctionDef:
+                if type(y) == ast.FunctionDef and not is_private(y.name):
                     functions.append(y.name)
-            methods[x.name]["methods"] = functions
-            class_defs[module_name].append(methods)
-    with open("classes.yaml", "w") as stream:
-        yaml.dump(class_defs, stream, default_flow_style=False)
+            class_defs["classes"][x.name]["methods"] = functions
+    return class_defs
 
-output_classes("../bokeh/charts/glyphs.py", "glyphs")
+
+def get_filenames(directory):
+    files = []
+    p = os.walk(directory, topdown=True, followlinks=False)
+    for dirpath, dirnames, filenames in p:
+        for d in exclude:
+            if d in dirpath:
+                break
+        else:
+            for x in filenames:
+                if x.endswith(".py") and not x.startswith("_"):
+                    files.append((dirpath + "/" + x, x))
+    return files
+
+
+def get_files_dict(filenames):
+    files_dict = {}
+    for x in filenames:
+        files_dict[x[0]] = output_classes(x[0], x[1])
+    return files_dict
+
+
+def dump_yaml(source):
+    with open("classes.yaml", "w") as stream:
+        yaml.dump(source, stream, default_flow_style=False)
+
+apple = get_files_dict(get_filenames("../bokeh"))
+dump_yaml(apple)
