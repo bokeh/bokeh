a384d6eb4eff620f0a0f6234338fc3045f55dd14
Sarah Bird <sbird@continuum.io>
2016-08-20 00:50:39 -0700

    Update example to include color bar.

--- examples/plotting/file/color_data_map.py
+++ examples/plotting/file/color_data_map.py
@@ -1,21 +1,36 @@
 import numpy as np
 
 from bokeh.io import show
-from bokeh.models.sources import ColumnDataSource
-from bokeh.models.mappers import LinearColorMapper, LogColorMapper
-from bokeh.palettes import Viridis3
+from bokeh.layouts import gridplot
+from bokeh.models import (
+    ColumnDataSource,
+    ColorBar,
+    LinearColorMapper,
+    LogColorMapper,
+)
+from bokeh.palettes import Viridis3, Viridis256
 from bokeh.plotting import figure
 
-x = np.random.random(2500) * 10
+x = np.random.random(2500) * 100
 y = np.random.normal(size=2500) * 2 + 5
-source = ColumnDataSource(dict(x=x, y1=y, y2=y - 10))
-
-log_mapper = LogColorMapper(palette=Viridis3)
-lin_mapper = LinearColorMapper(palette=Viridis3)
-
-p = figure()
+source = ColumnDataSource(dict(x=x, y=y))
 opts = dict(x='x', line_color=None, source=source)
-p.circle(y='y1', fill_color={'field': 'x', 'transform': log_mapper}, legend="Log mapper", **opts)
-p.triangle(y='y2', fill_color={'field': 'x', 'transform': lin_mapper}, legend="Lin mapper", **opts)
 
-show(p)
+
+def make_plot(mapper, title):
+    p = figure(toolbar_location=None, tools='', title=title)
+    color_bar = ColorBar(color_mapper=mapper, location=(0, 0))
+    p.circle(
+        x='x', y='y',
+        fill_color={'field': 'x', 'transform': mapper}, line_color=None,
+        source=source
+    )
+    p.add_layout(color_bar, 'right')
+    return p
+
+p1 = make_plot(LinearColorMapper(palette=Viridis256, low=0, high=100), title='Viridis256 - Linear')
+p2 = make_plot(LogColorMapper(palette=Viridis256, low=0, high=100), title='Viridis256 - Log')
+p3 = make_plot(LinearColorMapper(palette=Viridis3, low=0, high=100), title='Viridis3 - Linear')
+p4 = make_plot(LogColorMapper(palette=Viridis3, low=0, high=100), title='Viridis3 - Log')
+
+show(gridplot([p1, p2, p3, p4], ncols=2, plot_width=400, plot_height=300, toolbar_location=None))
