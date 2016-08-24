b1546b007beb7fd2fc84c24e5349b26cb2c96ccd
Brittain Hard <brittainhard@gmail.com>
2016-08-24 12:54:11 -0500

    Simple main function on script.

--- scripts/api_report.py
+++ scripts/api_report.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
-import ast, os, copy, subprocess
+import ast, os, copy, subprocess, sys
 
 from bokeh.util.api_crawler import api_crawler, differ
 
 
-def diff_versions(old_version, new_version, output_file):
+def diff_versions(old_version, new_version):
     subprocess.call("git stash && git checkout tags/%s -- bokeh" % old_version, shell=True)
     old = api_crawler("bokeh").get_crawl_dict()
     subprocess.call("git checkout tags/%s -- bokeh" % new_version, shell=True)
@@ -16,8 +16,14 @@ def diff_versions(old_version, new_version, output_file):
     subprocess.call("git stash apply", shell=True)
 
     # Combine items removed and added into a single text file.
-    diff = differ(old, new).get_diff()
+    diff = differ(old, new)
+    pretty_diff = "\n".join(diff.get_diff()) + "\n"
 
-    with open(output_file, "w") as f:
-        for x in diff:
-            f.write("%s\n" % x)
+    return pretty_diff
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        sys.stdout.write(diff_versions(sys.argv[1], sys.argv[2]))
+    else:
+        print("Please provide an old version and a new version.")
--- scripts/tests/test_api_report.py
+++ scripts/tests/test_api_report.py
@@ -1,15 +1,13 @@
 from __future__ import absolute_import
 import os
 
-from ..parse_modules import diff_versions
+from ..api_report import diff_versions
 
 
 class TestParseModules(object):
 
     def test_working_pipeline(self):
-        diff_versions("0.11.0", "0.12.0", "scripts/tests/samples/diff.txt")
-        with open("scripts/tests/samples/diff.txt") as f:
-            first = f.read().split("\n")
+        first = diff_versions("0.11.0", "0.12.0").split("\n")
         with open("scripts/tests/samples/sample_diff.txt") as f:
             second = f.read().split("\n")
         assert not set(first) - set(second)
