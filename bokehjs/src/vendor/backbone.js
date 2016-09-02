9e5cd8abb1d487d5a78a90658243b9ece7e9103b
Mateusz Paprocki <mattpap@gmail.com>
2016-09-02 11:57:08 +0200

    Remove unneeded Backbone properties

--- bokehjs/src/vendor/backbone.js
+++ bokehjs/src/vendor/backbone.js
@@ -16,20 +16,6 @@
   // Create a local reference to a common array method we'll want to use later.
   var slice = Array.prototype.slice;
 
-  // Current version of the library. Keep in sync with `package.json`.
-  Backbone.VERSION = '1.3.3';
-
-  // Turn on `emulateHTTP` to support legacy HTTP servers. Setting this option
-  // will fake `"PATCH"`, `"PUT"` and `"DELETE"` requests via the `_method` parameter and
-  // set a `X-Http-Method-Override` header.
-  Backbone.emulateHTTP = false;
-
-  // Turn on `emulateJSON` to support legacy servers that can't deal with direct
-  // `application/json` requests ... this will encode the body as
-  // `application/x-www-form-urlencoded` instead and will send the model in a
-  // form param named `model`.
-  Backbone.emulateJSON = false;
-
   // Proxy Backbone class methods to Underscore functions, wrapping the model's
   // `attributes` object or collection's `models` array behind the scenes.
   //
