7e6bb0db3a063e9fe5951e86043449c487ef9b62
Mateusz Paprocki <mattpap@gmail.com>
2016-09-20 16:13:55 +0200

    Compute __palettes__ instead of making it a constant

--- bokeh/palettes.py
+++ bokeh/palettes.py
@@ -1313,47 +1313,11 @@ class _Palettes(object):
 
     @property
     def __palettes__(self):
-        return [
-            "YlGn3",      "YlGn4",      "YlGn5",      "YlGn6",      "YlGn7",      "YlGn8",      "YlGn9",
-            "YlGnBu3",    "YlGnBu4",    "YlGnBu5",    "YlGnBu6",    "YlGnBu7",    "YlGnBu8",    "YlGnBu9",
-            "GnBu3",      "GnBu4",      "GnBu5",      "GnBu6",      "GnBu7",      "GnBu8",      "GnBu9",
-            "BuGn3",      "BuGn4",      "BuGn5",      "BuGn6",      "BuGn7",      "BuGn8",      "BuGn9",
-            "PuBuGn3",    "PuBuGn4",    "PuBuGn5",    "PuBuGn6",    "PuBuGn7",    "PuBuGn8",    "PuBuGn9",
-            "PuBu3",      "PuBu4",      "PuBu5",      "PuBu6",      "PuBu7",      "PuBu8",      "PuBu9",
-            "BuPu3",      "BuPu4",      "BuPu5",      "BuPu6",      "BuPu7",      "BuPu8",      "BuPu9",
-            "RdPu3",      "RdPu4",      "RdPu5",      "RdPu6",      "RdPu7",      "RdPu8",      "RdPu9",
-            "PuRd3",      "PuRd4",      "PuRd5",      "PuRd6",      "PuRd7",      "PuRd8",      "PuRd9",
-            "OrRd3",      "OrRd4",      "OrRd5",      "OrRd6",      "OrRd7",      "OrRd8",      "OrRd9",
-            "YlOrRd3",    "YlOrRd4",    "YlOrRd5",    "YlOrRd6",    "YlOrRd7",    "YlOrRd8",    "YlOrRd9",
-            "YlOrBr3",    "YlOrBr4",    "YlOrBr5",    "YlOrBr6",    "YlOrBr7",    "YlOrBr8",    "YlOrBr9",
-            "Purples3",   "Purples4",   "Purples5",   "Purples6",   "Purples7",   "Purples8",   "Purples9",
-            "Blues3",     "Blues4",     "Blues5",     "Blues6",     "Blues7",     "Blues8",     "Blues9",
-            "Greens3",    "Greens4",    "Greens5",    "Greens6",    "Greens7",    "Greens8",    "Greens9",
-            "Oranges3",   "Oranges4",   "Oranges5",   "Oranges6",   "Oranges7",   "Oranges8",   "Oranges9",
-            "Reds3",      "Reds4",      "Reds5",      "Reds6",      "Reds7",      "Reds8",      "Reds9",
-            "Greys3",     "Greys4",     "Greys5",     "Greys6",     "Greys7",     "Greys8",     "Greys9",     "Greys10",     "Greys11",     "Greys256",
-            "PuOr3",      "PuOr4",      "PuOr5",      "PuOr6",      "PuOr7",      "PuOr8",      "PuOr9",      "PuOr10",      "PuOr11",
-            "BrBG3",      "BrBG4",      "BrBG5",      "BrBG6",      "BrBG7",      "BrBG8",      "BrBG9",      "BrBG10",      "BrBG11",
-            "PRGn3",      "PRGn4",      "PRGn5",      "PRGn6",      "PRGn7",      "PRGn8",      "PRGn9",      "PRGn10",      "PRGn11",
-            "PiYG3",      "PiYG4",      "PiYG5",      "PiYG6",      "PiYG7",      "PiYG8",      "PiYG9",      "PiYG10",      "PiYG11",
-            "RdBu3",      "RdBu4",      "RdBu5",      "RdBu6",      "RdBu7",      "RdBu8",      "RdBu9",      "RdBu10",      "RdBu11",
-            "RdGy3",      "RdGy4",      "RdGy5",      "RdGy6",      "RdGy7",      "RdGy8",      "RdGy9",      "RdGy10",      "RdGy11",
-            "RdYlBu3",    "RdYlBu4",    "RdYlBu5",    "RdYlBu6",    "RdYlBu7",    "RdYlBu8",    "RdYlBu9",    "RdYlBu10",    "RdYlBu11",
-            "Spectral3",  "Spectral4",  "Spectral5",  "Spectral6",  "Spectral7",  "Spectral8",  "Spectral9",  "Spectral10",  "Spectral11",
-            "RdYlGn3",    "RdYlGn4",    "RdYlGn5",    "RdYlGn6",    "RdYlGn7",    "RdYlGn8",    "RdYlGn9",    "RdYlGn10",    "RdYlGn11",
-            "Magma3",     "Magma4",     "Magma5",     "Magma6",     "Magma7",     "Magma8",     "Magma9",     "Magma10",     "Magma11",     "Magma256",
-            "Inferno3",   "Inferno4",   "Inferno5",   "Inferno6",   "Inferno7",   "Inferno8",   "Inferno9",   "Inferno10",   "Inferno11",   "Inferno256",
-            "Plasma3",    "Plasma4",    "Plasma5",    "Plasma6",    "Plasma7",    "Plasma8",    "Plasma9",    "Plasma10",    "Plasma11",    "Plasma256",
-            "Viridis3",   "Viridis4",   "Viridis5",   "Viridis6",   "Viridis7",   "Viridis8",   "Viridis9",   "Viridis10",   "Viridis11",   "Viridis256",
-            "Accent3",    "Accent4",    "Accent5",    "Accent6",    "Accent7",    "Accent8",
-            "Dark2_3",    "Dark2_4",    "Dark2_5",    "Dark2_6",    "Dark2_7",    "Dark2_8",
-            "Paired3",    "Paired4",    "Paired5",    "Paired6",    "Paired7",    "Paired8",    "Paired9",    "Paired10",    "Paired11",    "Paired12",
-            "Pastel1_3",  "Pastel1_4",  "Pastel1_5",  "Pastel1_6",  "Pastel1_7",  "Pastel1_8",  "Pastel1_9",
-            "Pastel2_3",  "Pastel2_4",  "Pastel2_5",  "Pastel2_6",  "Pastel2_7",  "Pastel2_8",
-            "Set1_3",     "Set1_4",     "Set1_5",     "Set1_6",     "Set1_7",     "Set1_8",     "Set1_9",
-            "Set2_3",     "Set2_4",     "Set2_5",     "Set2_6",     "Set2_7",     "Set2_8",
-            "Set3_3",     "Set3_4",     "Set3_5",     "Set3_6",     "Set3_7",     "Set3_8",     "Set3_9",     "Set3_10",     "Set3_11",     "Set3_12"
-        ]
+        __palettes__ = []
+        for name, palettes in sorted(self.all_palettes.items(), key=lambda arg: arg[0]):
+            name = name + "_" if name[-1].isdigit() else name
+            __palettes__ += [ name + str(index) for index in sorted(palettes.keys()) ]
+        return __palettes__
 
     def _linear_cmap_func_generator(self, name, cmap):
         import math
