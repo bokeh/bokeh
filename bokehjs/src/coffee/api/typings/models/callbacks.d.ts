4a9781d23c1f69d63a0895e55405ffa6b1f3a98e
Mateusz Paprocki <mattpap@gmail.com>
2016-10-04 18:28:42 +0200

    Change indentation from 4 to 2 spaces in *.ts files

--- bokehjs/src/coffee/api/typings/models/callbacks.d.ts
+++ bokehjs/src/coffee/api/typings/models/callbacks.d.ts
@@ -1,17 +1,17 @@
 declare namespace Bokeh {
-    export interface Callback extends Model, ICallback {}
-    export interface ICallback extends IModel {}
+  export interface Callback extends Model, ICallback {}
+  export interface ICallback extends IModel {}
 
-    export var OpenURL: { new(attributes?: IOpenURL, options?: ModelOpts): OpenURL };
-    export interface OpenURL extends Callback, IOpenURL {}
-    export interface IOpenURL extends ICallback {
-        url?: string;
-    }
+  export var OpenURL: { new(attributes?: IOpenURL, options?: ModelOpts): OpenURL };
+  export interface OpenURL extends Callback, IOpenURL {}
+  export interface IOpenURL extends ICallback {
+    url?: string;
+  }
 
-    export var CustomJS: { new(attributes?: ICustomJS, options?: ModelOpts): CustomJS };
-    export interface CustomJS extends Callback, ICustomJS {}
-    export interface ICustomJS extends ICallback {
-        args?: Map<Model>;
-        code?: string;
-    }
+  export var CustomJS: { new(attributes?: ICustomJS, options?: ModelOpts): CustomJS };
+  export interface CustomJS extends Callback, ICustomJS {}
+  export interface ICustomJS extends ICallback {
+    args?: Map<Model>;
+    code?: string;
+  }
 }
--- bokehjs/src/coffee/api/typings/models/mappers.d.ts
+++ bokehjs/src/coffee/api/typings/models/mappers.d.ts
@@ -1,15 +1,15 @@
 declare namespace Bokeh {
-    export interface ColorMapper extends Model, IColorMapper {}
-    export interface IColorMapper extends IModel {}
+  export interface ColorMapper extends Model, IColorMapper {}
+  export interface IColorMapper extends IModel {}
 
-    export var LinearColorMapper: { new(attributes?: ILinearColorMapper, options?: ModelOpts): LinearColorMapper };
-    export interface LinearColorMapper extends ColorMapper, ILinearColorMapper {}
-    export interface ILinearColorMapper extends IColorMapper {
-        palette?: Array<Color>;
+  export var LinearColorMapper: { new(attributes?: ILinearColorMapper, options?: ModelOpts): LinearColorMapper };
+  export interface LinearColorMapper extends ColorMapper, ILinearColorMapper {}
+  export interface ILinearColorMapper extends IColorMapper {
+    palette?: Array<Color>;
 
-        low?: number;
-        high?: number;
+    low?: number;
+    high?: number;
 
-        nan_color?: Color;
-    }
+    nan_color?: Color;
+  }
 }
