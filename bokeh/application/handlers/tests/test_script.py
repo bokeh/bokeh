02e76f508a1c62bcee78a8e78d45e01abc148db2
Bryan Van de Ven <bryanv@continuum.io>
2016-10-05 16:53:54 -0500

    Fix Gapminder on Py2 / handle source codings better (#5310)
    * read as bytes and pass any encoding to compile
    
    * need utf-8 encoding to read gapminder data
    
    * skip ggplot_density example

--- bokeh/application/handlers/tests/test_script.py
+++ bokeh/application/handlers/tests/test_script.py
@@ -22,3 +22,18 @@ class TestScriptHandler(unittest.TestCase):
         assert result['handler']._runner.path == result['filename']
         assert result['handler']._runner.source == source
         assert not doc.roots
+
+    def test_runner_script_with_encoding(self):
+        doc = Document()
+        source = "# -*- coding: utf-8 -*-\nimport os"
+        result = {}
+        def load(filename):
+            handler = ScriptHandler(filename=filename)
+            handler.modify_document(doc)
+            result['handler'] = handler
+            result['filename'] = filename
+        with_file_contents(source, load)
+
+        assert result['handler'].error is None
+        assert result['handler'].failed is False
+        assert not doc.roots
