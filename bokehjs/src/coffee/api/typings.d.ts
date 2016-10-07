1564dcc3ac1f859e5423927736c55cc7f27eb3b1
Mateusz Paprocki <mattpap@gmail.com>
2016-10-07 13:02:16 +0200

    Add typings for all external libraries

--- bokehjs/src/coffee/api/typings.d.ts
+++ bokehjs/src/coffee/api/typings.d.ts
@@ -1,5 +1,13 @@
-/// <reference types="@types/jquery" />
+/// <reference types="@types/es6-promise" />
 /// <reference types="@types/underscore" />
+/// <reference types="@types/jquery" />
+/// <reference types="@types/jquery-mousewheel" />
+/// <reference types="@types/jqueryui" />
+/// <reference types="@types/rbush" />
+/// <reference types="@types/proj4" />
+/// <reference types="@types/sprintf" />
+/// <reference types="@types/timezone-js" />
+/// <reference types="@types/hammerjs" />
 
 /// <reference path="./typings/bokeh.d.ts" />
 /// <reference path="./typings/charts.d.ts" />
