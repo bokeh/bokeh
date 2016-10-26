a99b97d0f4aef6a0febfdd9c650e71326e662f2a
David J Pugh <djpugh@gmail.com>
2016-10-26 20:49:48 +0100

    JQueryUI RangeSlider implementation (#5309)
    * Initial range slider implementation
    
    * jqueryUI range slider working
    
    * Changing coffescript to be consistent with new  typescript approach
    
    * Fixing docs
    
    * Addressing review comments
    
    * Addressing review comments
    
    * Fixed test failures
    
    * Tweaking default values
    
    * Removing trailing whitespace

--- bokeh/models/widgets/inputs.py
+++ bokeh/models/widgets/inputs.py
@@ -139,6 +139,50 @@ class Slider(InputWidget):
        The `mouseup` policy is intended for scenarios in which the callback is expensive in time.
     """)
 
+class RangeSlider(InputWidget):
+    """ Range-slider based range selection widget
+
+    """
+
+    range = Tuple(Float, Float, default=(0.1, 0.9), help="""
+    Initial or selected range.
+    """)
+
+    start = Float(default=0, help="""
+    The minimum allowable value.
+    """)
+
+    end = Float(default=1, help="""
+    The maximum allowable value.
+    """)
+
+    step = Float(default=0.1, help="""
+    The step between consecutive values.
+    """)
+
+    orientation = Enum("horizontal", "vertical", help="""
+    Orient the slider either horizontally (default) or vertically.
+    """)
+
+    callback = Instance(Callback, help="""
+    A callback to run in the browser whenever the current Slider value changes.
+    """)
+
+    callback_throttle = Float(default=200, help="""
+    Number of microseconds to pause between callback calls as the slider is moved.
+    """)
+
+    callback_policy = Enum(SliderCallbackPolicy, default="throttle", help="""
+    When the callback is initiated. This parameter can take on only one of three options:
+
+       "continuous": the callback will be executed immediately for each movement of the slider
+       "throttle": the callback will be executed at most every ``callback_throttle`` milliseconds.
+       "mouseup": the callback will be executed only once when the slider is released.
+
+       The `mouseup` policy is intended for scenarios in which the callback is expensive in time.
+    """)
+
+
 class DateRangeSlider(InputWidget):
     """ Slider-based date range selection widget.
 
