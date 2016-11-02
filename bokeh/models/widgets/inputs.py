8a62570a244ae349e83464af221ca1b34e032aed
Bryan Van de Ven <bryanv@continuum.io>
2016-11-02 13:53:39 -0500

    fix code quality

--- bokeh/models/widgets/inputs.py
+++ bokeh/models/widgets/inputs.py
@@ -43,7 +43,7 @@ class TextInput(InputWidget):
     A callback to run in the browser whenever the user unfocuses the TextInput
     widget by hitting Enter or clicking outside of the text box area.
     """)
-    
+
     placeholder = String(default="", help="""
     Placeholder for empty input field
     """)
