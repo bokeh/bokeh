8f4361d61aaf82de5503a426456b5cf24096685a
Mateusz Paprocki <mattpap@gmail.com>
2016-11-11 18:14:57 +0100

    Handle children arrays (not ...) in createElement()

--- bokehjs/src/coffee/core/util/dom.ts
+++ bokehjs/src/coffee/core/util/dom.ts
@@ -1,6 +1,7 @@
-import * as _ from "underscore";
+import {isBoolean, isString, isArray, flatten} from "underscore";
 
-export function createElement(type: string, props: { [name: string]: any }, ...children: (string | HTMLElement)[]): HTMLElement {
+export function createElement(type: string, props: { [name: string]: any },
+    ...children: Array<string | HTMLElement | Array<string | HTMLElement>>): HTMLElement {
   let elem;
   if (type === "fragment") {
     elem = document.createDocumentFragment();
@@ -10,18 +11,18 @@ export function createElement(type: string, props: { [name: string]: any }, ...c
       let v = props[k];
       if (k === "className")
         k = "class";
-      if (k === "class" && _.isArray(v))
+      if (k === "class" && isArray(v))
         v = v.filter(c => c != null).join(" ");
-      if (v == null || _.isBoolean(v) && v)
+      if (v == null || isBoolean(v) && !v)
         continue
       elem.setAttribute(k, v);
     }
   }
 
-  for (const v of children) {
+  for (const v of flatten(children, true)) {
     if (v instanceof HTMLElement)
       elem.appendChild(v);
-    else if (_.isString(v))
+    else if (isString(v))
       elem.appendChild(document.createTextNode(v))
   }
 
