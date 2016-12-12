70149a45b4d60b797eeb0ddbe7e0e2a245826304
Mateusz Paprocki <mattpap@gmail.com>
2016-12-12 16:16:10 +0100

    Add information about removal of font-awesome icons to bokeh.icons

---
+++ bokeh/icons.py
@@ -0,0 +1,13 @@
+"""
+
+.. warning::
+    FontAwesome icons and the associated ``bokeh.icons`` module were removed in
+    the Bokeh 0.12.4 release. Please see the :bokeh-tree:`examples/custom/font-awesome`
+    directory for a demonstration of how to use these icons with a custom extension.
+
+"""
+
+import warnings
+warnings.warn("FontAwesome icons and the associated bokeh.icons module were removed in the Bokeh 0.12.4 release. \
+Please see the examples/custom/font-awesome directory for a demonstration of how to use these icons with a custom extension.")
+del warnings
