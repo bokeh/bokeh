18802dd4fe4915d146e7eecbb3ded6b191342f51
Mateusz Paprocki <mattpap@gmail.com>
2016-12-12 18:51:14 +0100

    Configure code runner so that full class paths are preserved (#5425)
    * Configure code runner so that full class paths are preserved
    
    This, however, doesn't allow to maintain cwd, but cwd was used
    to fill in the gap of missing full class paths, so this shouldn't
    be an issue. One can always use dirname(__file__) to get apps's
    working directory.
    
    * Use shorthand implementation syntax in surface3d
    
    * add extension example back
    
    * Maintain auto-generated modules in documents and sys.modules
    
    * Class path must be resolvable for every custom model
    
    * Updated code runner's tests
    
    * Fix flake8's complaints
    
    * Restore and update a comment in handlers/code.py

--- bokeh/application/handlers/tests/test_code.py
+++ bokeh/application/handlers/tests/test_code.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, print_function
 
-from os.path import abspath, sep
 import unittest
 
 from bokeh.application.handlers import CodeHandler
@@ -26,7 +25,7 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_empty_script(self):
         doc = Document()
-        handler = CodeHandler(source="# This script does nothing", filename="/test_filename")
+        handler = CodeHandler(source="# This script does nothing", filename="path/to/test_filename")
         handler.modify_document(doc)
         if handler.failed:
             raise RuntimeError(handler.error)
@@ -35,7 +34,7 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_script_adds_roots(self):
         doc = Document()
-        handler = CodeHandler(source=script_adds_two_roots, filename="/test_filename")
+        handler = CodeHandler(source=script_adds_two_roots, filename="path/to/test_filename")
         handler.modify_document(doc)
         if handler.failed:
             raise RuntimeError(handler.error)
@@ -44,7 +43,7 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_script_bad_syntax(self):
         doc = Document()
-        handler = CodeHandler(source="This is a syntax error", filename="/test_filename")
+        handler = CodeHandler(source="This is a syntax error", filename="path/to/test_filename")
         handler.modify_document(doc)
 
         assert handler.error is not None
@@ -52,7 +51,7 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_script_runtime_error(self):
         doc = Document()
-        handler = CodeHandler(source="raise RuntimeError('nope')", filename="/test_filename")
+        handler = CodeHandler(source="raise RuntimeError('nope')", filename="path/to/test_filename")
         handler.modify_document(doc)
 
         assert handler.error is not None
@@ -60,30 +59,22 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_script_sys_path(self):
         doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("path: '%s'" % sys.path[0])""", filename="/test_filename")
+        handler = CodeHandler(source="""import sys; raise RuntimeError("path: '%s'" % sys.path[0])""", filename="path/to/test_filename")
         handler.modify_document(doc)
 
         assert handler.error is not None
-        assert "path: ''" in handler.error
-
-    def test_script_cwd(self):
-        doc = Document()
-        handler = CodeHandler(source="""import os; raise RuntimeError("cwd: '%s'" % os.getcwd())""", filename="/test_filename")
-        handler.modify_document(doc)
-
-        assert handler.error is not None
-        assert "cwd: '%s'" % abspath(sep) in handler.error
+        assert "path: 'path/to'" in handler.error
 
     def test_script_argv(self):
         doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename")
+        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="path/to/test_filename")
         handler.modify_document(doc)
 
         assert handler.error is not None
         assert "argv: ['test_filename']" in handler.error
 
         doc = Document()
-        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="/test_filename", argv=[10, 20, 30])
+        handler = CodeHandler(source="""import sys; raise RuntimeError("argv: %r" % sys.argv)""", filename="path/to/test_filename", argv=[10, 20, 30])
         handler.modify_document(doc)
 
         assert handler.error is not None
@@ -91,7 +82,7 @@ class TestCodeHandler(unittest.TestCase):
 
     def test_safe_to_fork(self):
         doc = Document()
-        handler = CodeHandler(source="# This script does nothing", filename="/test_filename")
+        handler = CodeHandler(source="# This script does nothing", filename="path/to/test_filename")
         assert handler.safe_to_fork
         handler.modify_document(doc)
         if handler.failed:
--- sphinx/source/docs/user_guide/examples/extensions_example_widget.py
+++ sphinx/source/docs/user_guide/examples/extensions_example_widget.py
@@ -10,122 +10,13 @@ from bokeh.io import show
 from bokeh.plotting import Figure
 
 
-ion_range_slider_coffee = """
-# These are similar to python imports. BokehJS vendors its own versions
-# of Underscore and JQuery. They are available as show here.
-import * as _ from "underscore"
-import * as $ from "jquery"
-# The "core/properties" module has all the property types
-import * as p from "core/properties"
-
-# We will subclass in JavaScript from the same class that was subclassed
-# from in Python
-import {InputWidget, InputWidgetView} from "models/widgets/input_widget"
-# Use our custom template
-import ionslidertemplate from "./extensions_ion_range_slider_template"
-
-# This model will actually need to render things, so we must provide
-# view. The LayoutDOM model has a view already, so we will start with that
-export class IonRangeSliderView extends InputWidgetView
-  tagName: "div"
-  template: ionslidertemplate
-
-  initialize: (options) ->
-    super(options)
-    @listenTo(@model, 'change', @render)
-    @$el.empty()
-    html = @template(@model.attributes)
-    @$el.html(html)
-    @callbackWrapper = null
-    if @model.callback_policy == 'continuous'
-      @callbackWrapper = () ->
-        @model.callback?.execute(@model)
-    if @model.callback_policy == 'throttle' and @model.callback
-      @callbackWrapper = _.throttle(() ->
-        @model.callback?.execute(@model)
-      , @model.callback_throttle)
-    @render()
-
-  render: () ->
-    # Backbone Views create <div> elements by default, accessible as @$el.
-    # Many Bokeh views ignore this default <div>, and instead do things
-    # like draw to the HTML canvas. In this case though, we change the
-    # contents of the <div>, based on the current slider value.
-    super()
-    # Set up parameters
-    max = @model.end
-    min = @model.start
-    grid = @model.grid
-    disable = @model.disabled
-    range = @model.range or [max, min]
-    [from, to] = range
-    step = @model.step or ((max - min)/50)
-    opts = {
-      type: "double",
-      grid: grid,
-      min: min,
-      max: max,
-      from: from,
-      to: to,
-      step: step,
-      disable: disable,
-      onChange: @slide,
-      onFinish: @slidestop
-    }
-
-    input = @$el.find('.slider')[0]
-    slider = jQuery(input).ionRangeSlider(opts)
-    range = [from, to]
-    @$( "##{ @model.id }" ).val( range.join(' - '))
-    @$el.find('.bk-slider-parent').height(@model.height)
-    return @
-
-
-  slidestop: (data) =>
-    if @model.callback_policy == 'mouseup' or @model.callback_policy == 'throttle'
-      @model.callback?.execute(@model)
-
-  slide: (data) =>
-    range = [data.from, data.to]
-    value = range.join(' - ')
-    @$( "##{ @model.id }" ).val( value )
-    @model.range = range
-    if @callbackWrapper then @callbackWrapper()
-
-export class IonRangeSlider extends InputWidget
-
-  # If there is an associated view, this is boilerplate.
-  default_view: IonRangeSliderView
-
-  # The ``type`` class attribute should generally match exactly the name
-  # of the corresponding Python class.
-  type: "IonRangeSlider"
-
-  # The @define block adds corresponding "properties" to the JS model. These
-  # should basically line up 1-1 with the Python model class. Most property
-  # types have counterparts, e.g. bokeh.core.properties.String will be
-  # p.String in the JS implementation. Where the JS type system is not yet
-  # as rich, you can use p.Any as a "wildcard" property type.
-  @define {
-      range:             [ p.Any,                      ]
-      start:             [ p.Number,      0            ]
-      end:               [ p.Number,      1            ]
-      step:              [ p.Number,      0.1          ]
-      grid:              [ p.Bool,        true         ]
-      orientation:       [ p.Orientation, "horizontal" ]
-      callback_throttle: [ p.Number,      200          ]
-      callback_policy:   [ p.String,      "throttle"   ]
-  }
-"""
-
-
 class IonRangeSlider(InputWidget):
     # The special class attribute ``__implementation__`` should contain a string
     # of JavaScript (or CoffeeScript) code that implements the JavaScript side
     # of the custom extension model or a string name of a JavaScript (or
     # CoffeeScript) file with the implementation.
 
-    __implementation__ = ion_range_slider_coffee
+    __implementation__ = 'extensions_ion_range_slider.coffee'
     __javascript__ = ["https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js",
                       "https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.1.4/js/ion.rangeSlider.js"]
     __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.css",
@@ -216,6 +107,7 @@ callback_ion = CustomJS(args=dict(source=source), code="""
         }
         source.trigger('change');
     """)
+
 
 slider = Slider(start=0, end=5, step=0.1, value=1, title="Bokeh Slider - Power", callback=callback_single)
 ion_range_slider = IonRangeSlider(start=0.01, end=0.99, step=0.01, range=(min(x), max(x)), title='Ion Range Slider - Range', callback=callback_ion, callback_policy='continuous')
