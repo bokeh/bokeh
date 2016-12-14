715ce30affe8783c7275ad363705584317ca5319
Sarah Bird <github@birdsbits.com>
2016-12-13 18:22:41 -0600

    Update travis.yml (#5551)
    * Update travis.yml
    
    - use saucelabs add on (as opposed to doing it ourself)
    - switch to matrix include (allows for easy targeted use of saucelabs addon)
    - clean-up file in general to hopefully make it easier to read
    
    * Use of global turned out to be important :D
    
    Otherwise even though using matrix include, tries to run one test run for every
    env
    
    * Python versions specified by the matrix include
    
    * Correctly put environment variables inside else.
    
    * Do not report warnings by default
    
    * Fix up tests so they don't generate a pytest warning
    
    * Revert "Do not report warnings by default"
    
    This reverts commit c88b6993ac63c0162b40385cae78e109e665e5bf.
    
    * Remove python 2.7 examples test run
    
    * Run flake_docs on python 3.4
    
    * Correct comment
    
    * Fabric only availabe on python2

--- bokeh/util/tests/test_options.py
+++ bokeh/util/tests/test_options.py
@@ -3,34 +3,34 @@ from __future__ import absolute_import
 from bokeh.core.properties import Int, String
 from bokeh.util.options import Options
 
-class TestOpts(Options):
+class DummyOpts(Options):
     foo = String(default="thing")
     bar = Int()
 
 def test_empty():
     empty = dict()
-    o = TestOpts(empty)
+    o = DummyOpts(empty)
     assert o.foo == "thing"
     assert o.bar == None
     assert empty == {}
 
 def test_exact():
     exact = dict(foo="stuff", bar=10)
-    o = TestOpts(exact)
+    o = DummyOpts(exact)
     assert o.foo == "stuff"
     assert o.bar == 10
     assert exact == {}
 
 def test_extra():
     extra = dict(foo="stuff", bar=10, baz=22.2)
-    o = TestOpts(extra)
+    o = DummyOpts(extra)
     assert o.foo == "stuff"
     assert o.bar == 10
     assert extra == {'baz': 22.2}
 
 def test_mixed():
     mixed = dict(foo="stuff", baz=22.2)
-    o = TestOpts(mixed)
+    o = DummyOpts(mixed)
     assert o.foo == "stuff"
     assert o.bar == None
     assert mixed == {'baz': 22.2}
