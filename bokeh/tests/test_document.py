48fa5713d9a80b9768c1411b42fe9c0f823a1ace
Bryan Van de Ven <bryanv@continuum.io>
2016-12-22 12:44:46 -0600

    add test for default template vars

--- bokeh/tests/test_document.py
+++ bokeh/tests/test_document.py
@@ -29,6 +29,11 @@ class TestDocument(unittest.TestCase):
     def test_empty(self):
         d = document.Document()
         assert not d.roots
+        
+    def test_default_template_vars(self):
+        d = = document.Document()
+        assert not d.roots
+        assert d.template_variables == {}
 
     def test_add_roots(self):
         d = document.Document()
