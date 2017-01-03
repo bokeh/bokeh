032a24879a5a7b745ab468029199d658317556ef
Bryan Van de Ven <bryanv@continuum.io>
2017-01-03 16:00:23 -0600

    update some quad/rect examples to vbar/hbar

--- examples/plotting/file/boxplot.py
+++ examples/plotting/file/boxplot.py
@@ -46,14 +46,12 @@ upper.score = [min([x,y]) for (x,y) in zip(list(qmax.loc[:,'score']),upper.score
 lower.score = [max([x,y]) for (x,y) in zip(list(qmin.loc[:,'score']),lower.score)]
 
 # stems
-p.segment(cats, upper.score, cats, q3.score, line_width=2, line_color="black")
-p.segment(cats, lower.score, cats, q1.score, line_width=2, line_color="black")
+p.segment(cats, upper.score, cats, q3.score, line_color="black")
+p.segment(cats, lower.score, cats, q1.score, line_color="black")
 
 # boxes
-p.rect(cats, (q3.score+q2.score)/2, 0.7, q3.score-q2.score,
-    fill_color="#E08E79", line_width=2, line_color="black")
-p.rect(cats, (q2.score+q1.score)/2, 0.7, q2.score-q1.score,
-    fill_color="#3B8686", line_width=2, line_color="black")
+p.vbar(cats, 0.7, q2.score, q3.score, fill_color="#E08E79", line_color="black")
+p.vbar(cats, 0.7, q1.score, q2.score, fill_color="#3B8686", line_color="black")
 
 # whiskers (almost-0 height rects simpler than segments)
 p.rect(cats, lower.score, 0.2, 0.01, line_color="black")
