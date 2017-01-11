576c107ec2f8c306fbba4e58eeba28fca468d5ae
Mateusz Paprocki <mattpap@gmail.com>
2017-01-11 13:18:29 +0100

    Use 2x icon size in font-awesome custom model example

--- examples/custom/font-awesome/font-awesome.py
+++ examples/custom/font-awesome/font-awesome.py
@@ -4,7 +4,7 @@ from bokeh.models import CustomJS
 from bokeh.models.widgets import Button
 from fontawesome_icon import FontAwesomeIcon
 
-btn = Button(icon=FontAwesomeIcon(icon_name="thumbs-o-up"),
+btn = Button(icon=FontAwesomeIcon(icon_name="thumbs-o-up", size=2),
              label="It works!",
              callback=CustomJS(code="alert('It works!');"))
 show(column(btn))
