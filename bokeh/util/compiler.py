70ffd54173d89ad93eb84f9526de9dc75432d248
Mateusz Paprocki <mattpap@gmail.com>
2017-01-11 13:17:34 +0100

    Set encoding to utf-8 when decoding output from node/npm

--- bokeh/util/compiler.py
+++ bokeh/util/compiler.py
@@ -135,7 +135,7 @@ def _run(app, argv, input=None):
     if proc.returncode != 0:
         raise RuntimeError(errout)
     else:
-        return stdout.decode()
+        return stdout.decode('utf-8')
 
 def _run_nodejs(argv, input=None):
     return _run(_nodejs, argv, input)
