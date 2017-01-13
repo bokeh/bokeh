0a0bbe3ded6ce8b535f6a17fe5d5f6c6122c7a04
Mateusz Paprocki <mattpap@gmail.com>
2017-01-13 12:32:32 +0100

    Implement min(), minBy(), max(), maxBy(), minarg() and maxarg()

--- bokehjs/src/coffee/core/util/array.ts
+++ bokehjs/src/coffee/core/util/array.ts
@@ -1,5 +1,3 @@
-const {min, max, ceil} = Math
-
 export function zip<A, B>(As: Array<A>, Bs: Array<B>): Array<[A, B]> {
   const ABs: Array<[A, B]> = []
   for (let i = 0; i < As.length && i < Bs.length; i++) {
@@ -24,7 +22,7 @@ export function range(start: number, stop?: number, step: number = 1): Array<num
     start = 0
   }
 
-  const length = max(ceil((stop - start) / step), 0)
+  const length = Math.max(Math.ceil((stop - start) / step), 0)
   const range = Array(length)
 
   for (let i = 0; i < length; i++, start += step) {
@@ -42,4 +40,76 @@ export function cumsum(array: Array<number>): Array<number> {
   const result = []
   array.reduce((a, b, i) => result[i] = a + b, 0)
   return result
+}
+
+export function min(array: Array<number>): number {
+  let value: number
+  let result = Infinity
+
+  for (let i = 0, length = array.length; i < length; i++) {
+    value = array[i]
+    if (value < result) {
+      result = value
+    }
+  }
+
+  return result
+}
+
+export function minBy<T>(array: Array<T>, key: (T) => number): T {
+  let value: T
+  let result: T
+  let computed: number
+  let resultComputed = Infinity
+
+  for (let i = 0, length = array.length; i < length; i++) {
+    value = array[i]
+    computed = key(value)
+    if (computed < resultComputed) {
+      result = value
+      resultComputed = computed
+    }
+  }
+
+  return result
+}
+
+export function max(array: Array<number>): number {
+  let value: number
+  let result = -Infinity
+
+  for (let i = 0, length = array.length; i < length; i++) {
+    value = array[i]
+    if (value > result) {
+      result = value
+    }
+  }
+
+  return result
+}
+
+export function maxBy<T>(array: Array<T>, key: (T) => number): T {
+  let value: T
+  let result: T
+  let computed: number
+  let resultComputed = -Infinity
+
+  for (let i = 0, length = array.length; i < length; i++) {
+    value = array[i]
+    computed = key(value)
+    if (computed > resultComputed) {
+      result = value
+      resultComputed = computed
+    }
+  }
+
+  return result
+}
+
+export function argmin(array: Array<number>): number {
+  return minBy(range(array.length), (i) => array[i])
+}
+
+export function argmax(array: Array<number>): number {
+  return maxBy(range(array.length), (i) => array[i])
 }
