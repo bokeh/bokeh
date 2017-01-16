bbe4bc83ea1d8669edda67368fd0831ce1ff5164
Mateusz Paprocki <mattpap@gmail.com>
2017-01-16 11:16:47 +0100

    Use charCodeAt() in uniqueId()
    Previously code relied on implicit conversion from string to number
    and failed completely: str op num -> +str op num -> NaN op num.

--- bokehjs/src/coffee/core/util/string.ts
+++ bokehjs/src/coffee/core/util/string.ts
@@ -10,8 +10,8 @@ export function uniqueId(prefix?: string): string {
   for (let i = 0; i < 32; i++) {
     s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
   }
-  s[12] = "4"                                       // bits 12-15 of the time_hi_and_version field to 0010
-  s[16] = hexDigits.substr((s[16] & 0x3) | 0x8, 1)  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[12] = "4"                                                     // bits 12-15 of the time_hi_and_version field to 0010
+  s[16] = hexDigits.substr((s[16].charCodeAt(0) & 0x3) | 0x8, 1)  // bits 6-7 of the clock_seq_hi_and_reserved to 01
 
   const uuid = s.join("")
   if (prefix != null)
