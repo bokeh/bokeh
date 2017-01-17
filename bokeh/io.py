b354adfc55eb90ca92ac4b958dff2b61776cb8b1
jlstevens <jlstevens@ed.ac.uk>
2017-01-17 00:16:32 +0000

    show now supports displaying application in notebook cell

--- bokeh/io.py
+++ bokeh/io.py
@@ -39,6 +39,10 @@ from .util.notebook import load_notebook, publish_display_data, get_comms
 from .util.string import decode_utf8
 from .util.serialization import make_id
 
+from .application import Application
+from .server.server import Server
+from tornado.ioloop import IOLoop
+
 #-----------------------------------------------------------------------------
 # Globals and constants
 #-----------------------------------------------------------------------------
@@ -260,7 +264,8 @@ def curstate():
     '''
     return _state
 
-def show(obj, browser=None, new="tab", notebook_handle=False):
+def show(obj, browser=None, new="tab", notebook_handle=False,
+                            app_path="/", notebook_url="127.0.0.1:8888"):
     ''' Immediately display a plot object.
 
     In an IPython/Jupyter notebook, the output is displayed in an output
@@ -299,9 +304,22 @@ def show(obj, browser=None, new="tab", notebook_handle=False):
         an IPython/Jupyter notebook.
 
     '''
+    if isinstance(obj, Application):
+        return _show_notebook_app_with_state(obj, _state, app_path, notebook_url)
+
     if obj not in _state.document.roots:
         _state.document.add_root(obj)
     return _show_with_state(obj, _state, browser, new, notebook_handle=notebook_handle)
+
+
+def _show_notebook_app_with_state(app, _state, app_path, notebook_url):
+    from IPython.display import HTML, display
+    loop = IOLoop.current()
+    server = Server({'/': app}, io_loop=loop, port=0, host='*',
+                    allow_websocket_origin=[notebook_url])
+    server.start()
+    script = autoload_server(model=None, url='http://127.0.0.1:%d' % server.port)
+    display(HTML(server_cell(server, script)))
 
 
 def _show_with_state(obj, state, browser, new, notebook_handle=False):
