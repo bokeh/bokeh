aa904cd5b29b3c1cbce6b78dda58668862215e79
Bryan Van de Ven <bryanv@continuum.io>
2017-01-18 13:37:59 -0600

    try -dev instead of .dev

--- bokeh/util/version.py
+++ bokeh/util/version.py
@@ -24,7 +24,7 @@ del get_versions
 
 def base_version():
     import re
-    VERSION_PAT = re.compile(r"^(\d+\.\d+\.\d+)((?:\.dev|rc).*)?")
+    VERSION_PAT = re.compile(r"^(\d+\.\d+\.\d+)((?:\-dev|rc).*)?")
     return VERSION_PAT.search(__version__).group(1)
 
 __base_version__ = base_version()
