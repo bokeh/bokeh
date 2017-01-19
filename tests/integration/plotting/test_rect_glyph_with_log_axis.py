a21c9e74d9c91814d16fe90e50e35f19fc8e9449
Claire Tang <clairetang6@gmail.com>
2017-01-18 20:35:52 -0800

    remove unnecessary models from screenshot test

--- tests/integration/plotting/test_rect_glyph_with_log_axis.py
+++ tests/integration/plotting/test_rect_glyph_with_log_axis.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from bokeh.io import save
 from bokeh.plotting import figure
-from bokeh.models.sources import ColumnDataSource
+from bokeh.models import ColumnDataSource, Plot, Rect, Range1d, LogAxis
 from tests.integration.utils import has_no_console_errors
 
 import pytest
@@ -11,16 +11,17 @@ pytestmark = pytest.mark.integration
 @pytest.mark.screenshot
 def test_rect_rendering_with_log_axis(output_file_url, selenium, screenshot):
 
-    plot = figure(x_range=(0,30), y_range=(1,100), y_axis_type="log")
+    plot = Plot(plot_height=400, plot_width=400,
+       x_range=Range1d(0,30), y_range=Range1d(1,100),
+       y_mapper_type="log")
 
     x = [10, 20]
     y = [10, 20]
 
-    sc_source = ColumnDataSource(data=dict(x=x, y=y))
-    plot.scatter(x='x', y='y', size=20, color='black', source=sc_source)
-
     source = ColumnDataSource(data=dict(x=[(x[0]+x[1])/2], y=[(y[0]+y[1])/2], width=[x[1]-x[0]], height=[y[1]-y[0]]))
-    plot.rect(x='x', y='y', width='width', height='height', source=source)
+    plot.add_glyph(source, Rect(x='x', y='y', width='width', height='height'))
+
+    plot.add_layout(LogAxis(), "left")
 
     # Save the plot and start the test
     save(plot)
