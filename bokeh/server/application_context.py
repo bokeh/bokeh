b0192f3dc0dbfc95f5a9e7ef8267f19b92ce3189
Bryan Van de Ven <bryanv@continuum.io>
2017-02-21 17:54:01 -0600

    pass HTTP request args more explicitly to session

--- bokeh/server/application_context.py
+++ bokeh/server/application_context.py
@@ -15,6 +15,16 @@ from bokeh.application.application import ServerContext, SessionContext
 from bokeh.document import Document
 from bokeh.util.tornado import _CallbackGroup, yield_for_all_futures
 
+class _RequestProxy(object):
+    def __init__(self, request):
+        args_copy = dict(request.arguments)
+        if 'bokeh-protocol-version' in args_copy: del args_copy['bokeh-protocol-version']
+        if 'bokeh-session-id' in args_copy: del args_copy['bokeh-session-id']
+        self._args = args_copy
+    @property
+    def arguments(self):
+        return self._args
+
 class BokehServerContext(ServerContext):
     def __init__(self, application_context):
         self.application_context = application_context
@@ -152,7 +162,7 @@ class ApplicationContext(object):
                                                   self.server_context,
                                                   doc)
             # using private attr so users only have access to a read-only property
-            session_context._request = request
+            session_context._request = _RequestProxy(request)
 
             # expose the session context to the document
             # use the _attribute to set the public property .session_context
