fa077704f294a1e01c5ebf624180cda33ecca8ac
jlstevens <jlstevens@ed.ac.uk>
2017-03-15 15:06:11 +0000

    Fixed flake8 issues in test_events.py

--- bokeh/tests/test_events.py
+++ bokeh/tests/test_events.py
@@ -1,25 +1,34 @@
 import pytest
 
 from bokeh.models import Plot, Button, Div
-from bokeh.events import *
-
+from bokeh import events
 
 all_events = set([v for v in globals().values()
-                  if isinstance(v,type) and issubclass(v, Event)])
+                  if isinstance(v,type) and issubclass(v, events.Event)])
 
 point_events = set([v for v in globals().values()
-                    if isinstance(v,type) and issubclass(v, PointEvent)])
+                    if isinstance(v,type) and issubclass(v, events.PointEvent)])
+
+class TestCallback(object):
+    def __init__(self, attributes=[]):
+        self.event_name = None
+        self.attributes = attributes
+        self.payload = {}
+
+    def __call__(self, event):
+        self.event_name = event.event_name
+        self.payload = {attr:getattr(event, attr) for attr in self.attributes}
 
 
 def test_event_metaclass():
     # All events currently in the namespace should be in the EVENT_CLASSES set
-    assert len(all_events - EVENT_CLASSES) == 0
+    assert len(all_events - events.EVENT_CLASSES) == 0
 
 def test_common_decode_json():
-    for event_cls in EVENT_CLASSES:
+    for event_cls in events.EVENT_CLASSES:
         if event_cls.event_name is None: continue # Skip abstract base class
-        event = Event.decode_json({'event_name':event_cls.event_name,
-                                   'event_values':{'model_id':'test-model-id'}})
+        event = events.Event.decode_json({'event_name':event_cls.event_name,
+                                          'event_values':{'model_id':'test-model-id'}})
         assert event._model_id == 'test-model-id'
 
 
@@ -27,8 +36,8 @@ def test_pointevent_subclass_decode_json():
     event_values = dict(model_id='test-model-id', sx=3, sy=-2, x=10, y=100)
     for event_cls in point_events:
         if event_cls.event_name is None: continue # Skip abstract base class
-        event = Event.decode_json({'event_name':  event_cls.event_name,
-                                   'event_values': event_values.copy()})
+        event = events.Event.decode_json({'event_name':  event_cls.event_name,
+                                         'event_values': event_values.copy()})
         assert event.sx == 3
         assert event.sy == -2
         assert event.x == 10
@@ -38,8 +47,8 @@ def test_pointevent_subclass_decode_json():
 def test_panevent_decode_json():
     event_values = dict(model_id='test-model-id', delta_x=0.1, delta_y=0.3,
                         sx=3, sy=-2, x=10, y=100)
-    event = Event.decode_json({'event_name':  Pan.event_name,
-                               'event_values': event_values.copy()})
+    event = events.Event.decode_json({'event_name':  events.Pan.event_name,
+                                     'event_values': event_values.copy()})
     assert event.delta_x == 0.1
     assert event.delta_y == 0.3
     assert event.sx == 3
@@ -50,8 +59,8 @@ def test_panevent_decode_json():
 
 def test_mousewheelevent_decode_json():
     event_values = dict(model_id='test-model-id', delta=-0.1, sx=3, sy=-2, x=10, y=100)
-    event = Event.decode_json({'event_name':  MouseWheel.event_name,
-                               'event_values': event_values.copy()})
+    event = events.Event.decode_json({'event_name':  events.MouseWheel.event_name,
+                                      'event_values': event_values.copy()})
     assert event.delta == -0.1
     assert event.sx == 3
     assert event.sy == -2
@@ -61,8 +70,8 @@ def test_mousewheelevent_decode_json():
 
 def test_pinchevent_decode_json():
     event_values = dict(model_id='test-model-id', scale=42, sx=3, sy=-2, x=10, y=100)
-    event = Event.decode_json({'event_name':  Pinch.event_name,
-                               'event_values': event_values.copy()})
+    event = events.Event.decode_json({'event_name':  events.Pinch.event_name,
+                                      'event_values': event_values.copy()})
     assert event.scale == 42
     assert event.sx == 3
     assert event.sy == -2
@@ -72,75 +81,75 @@ def test_pinchevent_decode_json():
 
 def test_event_constructor_button():
     model = Button()
-    event = Event(model)
+    event = events.Event(model)
     assert event._model_id == model._id
 
 def test_event_constructor_div():
     model = Div()
-    event = Event(model)
+    event = events.Event(model)
     assert event._model_id == model._id
 
 def test_event_constructor_plot():
     model = Plot()
-    event = Event(model)
+    event = events.Event(model)
     assert event._model_id == model._id
 
 def test_buttonclick_constructor_button():
     model = Button()
-    event = ButtonClick(model)
+    event = events.ButtonClick(model)
     assert event._model_id == model._id
 
 def test_buttonclick_constructor_div():
-    with pytest.raises(ValueError) as e:
-        ButtonClick(Div())
+    with pytest.raises(ValueError):
+        events.ButtonClick(Div())
 
 def test_buttonclick_constructor_plot():
-    with pytest.raises(ValueError) as e:
-        ButtonClick(Plot())
+    with pytest.raises(ValueError):
+        events.ButtonClick(Plot())
 
 def test_lodstart_constructor_button():
-    with pytest.raises(ValueError) as e:
-        LODStart(Button())
+    with pytest.raises(ValueError):
+        events.LODStart(Button())
 
 def test_lodstart_constructor_div():
-    with pytest.raises(ValueError) as e:
-        LODStart(Div())
+    with pytest.raises(ValueError):
+        events.LODStart(Div())
 
 def test_lodstart_constructor_plot():
     model = Plot()
-    event = LODStart(model)
+    event = events.LODStart(model)
     assert event._model_id == model._id
 
 def test_lodend_constructor_button():
-    with pytest.raises(ValueError) as e:
-        LODEnd(Button())
+    with pytest.raises(ValueError):
+        events.LODEnd(Button())
 
 def test_lodend_constructor_div():
-    with pytest.raises(ValueError) as e:
-        LODEnd(Div())
+    with pytest.raises(ValueError):
+        events.LODEnd(Div())
 
 def test_lodend_constructor_plot():
     model = Plot()
-    event = LODEnd(model)
+    event = events.LODEnd(model)
     assert event._model_id == model._id
 
 
 def test_plotevent_constructor_button():
-    with pytest.raises(ValueError) as e:
-        PlotEvent(Button())
+    with pytest.raises(ValueError):
+        events.PlotEvent(Button())
 
 def test_plotevent_constructor_div():
-    with pytest.raises(ValueError) as e:
-        PlotEvent(Div())
+    with pytest.raises(ValueError):
+        events.PlotEvent(Div())
 
 def test_plotevent_constructor_plot():
     model = Plot()
-    event = PlotEvent(model)
+    event = events.PlotEvent(model)
     assert event._model_id == model._id
 
 def test_pointEvent_constructor_plot():
     model = Plot()
-    event = PointEvent(model, sx=3, sy=-2, x=10, y=100)
+    event = events.PointEvent(model, sx=3, sy=-2, x=10, y=100)
     assert event.sx == 3
     assert event.sy == -2
     assert event.x == 10
@@ -148,12 +157,12 @@ def test_pointEvent_constructor_plot():
     assert event._model_id == model._id
 
 def test_pointevent_constructor_button():
-    with pytest.raises(ValueError) as e:
-        PointEvent(Button(), sx=3, sy=-2, x=10, y=100)
+    with pytest.raises(ValueError):
+        events.PointEvent(Button(), sx=3, sy=-2, x=10, y=100)
 
 def test_pointevent_constructor_div():
-    with pytest.raises(ValueError) as e:
-        PointEvent(Div(), sx=3, sy=-2, x=10, y=100)
+    with pytest.raises(ValueError):
+        events.PointEvent(Div(), sx=3, sy=-2, x=10, y=100)
 
 def test_pointevent_subclass_constructor_plot():
     model = Plot()
@@ -168,11 +177,11 @@ def test_pointevent_subclass_constructor_plot():
 def test_pointevent_subclass_constructor_button():
     model = Button()
     for subcls in point_events:
-        with pytest.raises(ValueError) as e:
+        with pytest.raises(ValueError):
             subcls(model, sx=3, sy=-2, x=10, y=100)
 
 def test_pointevent_subclass_constructor_div():
     model = Div()
     for subcls in point_events:
-        with pytest.raises(ValueError) as e:
+        with pytest.raises(ValueError):
             subcls(model, sx=3, sy=-2, x=10, y=100)
