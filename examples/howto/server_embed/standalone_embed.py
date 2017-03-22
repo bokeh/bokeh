62b3f8cb7631a3f6d7f8b0a6516b7a5be54bbf57
jsignell <jsignell@gmail.com>
2017-03-22 10:17:51 -0400

    making embed example more engaging

--- examples/howto/server_embed/standalone_embed.py
+++ examples/howto/server_embed/standalone_embed.py
@@ -1,5 +1,6 @@
-import numpy as np
+import pandas as pd
 from tornado.ioloop import IOLoop
+import yaml
 
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
@@ -7,26 +8,43 @@ from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, Slider
 from bokeh.plotting import figure
 from bokeh.server.server import Server
+from bokeh.themes import Theme
 
 io_loop = IOLoop.current()
 
 def modify_doc(doc):
-    x = np.linspace(0, 10, 1000)
-    y = np.log(x) * np.sin(x)
+    data_url = "http://www.neracoos.org/erddap/tabledap/B01_sbe37_all.csvp?time,temperature&depth=1&temperature_qc=0&time>=now-13months"
+    df = pd.read_csv(data_url, parse_dates=True, index_col=0)
+    df = df.rename(columns={'temperature (celsius)': 'temperature'})
+    df.index.name = 'time'
 
-    source = ColumnDataSource(data=dict(x=x, y=y))
+    source = ColumnDataSource(data=df)
 
-    plot = figure()
-    plot.line('x', 'y', source=source)
-
-    slider = Slider(start=1, end=10, value=1, step=0.1)
+    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
+                  title="Sea Surface Temperature at 43.18, -70.43")
+    plot.line('time', 'temperature', source=source)
 
     def callback(attr, old, new):
-        y = np.log(x) * np.sin(x*new)
-        source.data = dict(x=x, y=y)
+        data = df.rolling('{0}D'.format(new)).mean()
+        source.data = ColumnDataSource(data=data).data
+
+    slider = Slider(start=1, end=30, value=1, step=1, title="Smoothing by N Days")
     slider.on_change('value', callback)
 
     doc.add_root(column(slider, plot))
+
+    doc.theme = Theme(json=yaml.load("""
+        attrs:
+            Figure:
+                background_fill_color: "#DDDDDD"
+                outline_line_color: white
+                toolbar_location: above
+                height: 500
+                width: 800
+            Grid:
+                grid_line_dash: [6, 4]
+                grid_line_color: white
+    """))
 
 bokeh_app = Application(FunctionHandler(modify_doc))
 
