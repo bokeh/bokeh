d065784874671ef36edc22dfdb3e53155f219b39
jsignell <jsignell@gmail.com>
2017-03-30 12:23:18 -0400

    changed color of each column in jitter and removed unemployment_polar

--- examples/plotting/file/jitter.py
+++ examples/plotting/file/jitter.py
@@ -3,14 +3,18 @@ from bokeh.layouts import column
 from bokeh.plotting import figure, show, output_file
 from bokeh.sampledata.autompg import autompg as df
 
-p1 = figure(plot_width=600, plot_height=300, title="Years vs mpg without jittering")
-p1.circle(x=df['yr'], y=df['mpg'], color='darkred')
 
+colors = ["red", "olive", "darkred", "goldenrod", "skyblue", "orange", "salmon"]
+
+p1 = figure(plot_width=600, plot_height=300, title="Years vs mpg without jittering")
 p2 = figure(plot_width=600, plot_height=300, title="Years vs mpg with jittering")
-for year in df['yr'].unique():
-    p2.circle(x={'value': year, 'transform': Jitter(width=1)},
-              y=df[df['yr'] == year]['mpg'],
-              color='salmon')
+
+for i, year in enumerate(list(df.yr.unique())):
+    y = df[df['yr'] == year]['mpg']
+    color = colors[i % len(colors)]
+
+    p1.circle(x=year, y=y, color=color)
+    p2.circle(x={'value': year, 'transform': Jitter(width=1)}, y=y, color=color)
 
 output_file("jitter.html")
 
