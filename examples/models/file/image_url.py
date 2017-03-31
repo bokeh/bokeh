102ca5ce2103dfb08602edb26a1448ed164573e1
jsignell <jsignell@gmail.com>
2017-03-31 12:46:57 -0400

    added h and w kwarg so that images display and rotated one by 180deg

--- examples/models/file/image_url.py
+++ examples/models/file/image_url.py
@@ -33,7 +33,7 @@ plot.add_glyph(source, image1)
 image2 = ImageURL(url="url", x="x2", y="y2", w=20, h=20, anchor="top_left")
 plot.add_glyph(source, image2)
 
-image3 = ImageURL(url=dict(value=url), x=200, y=-100, anchor="bottom_right")
+image3 = ImageURL(url=dict(value=url), x=200, y=-100, w=50, h=50, angle=np.pi, anchor="bottom_right")
 plot.add_glyph(source, image3)
 
 xaxis = LinearAxis()
