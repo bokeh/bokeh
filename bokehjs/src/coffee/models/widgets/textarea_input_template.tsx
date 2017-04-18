0c60d1f6ea8fc8ac4d63c3f4edf07d860c5b78dd
Patrick Mulrooney <mulroony@gmail.com>
2017-04-18 08:32:14 -0700

    add additional fields. Also add docs and examples

--- bokehjs/src/coffee/models/widgets/textarea_input_template.tsx
+++ bokehjs/src/coffee/models/widgets/textarea_input_template.tsx
@@ -6,14 +6,18 @@ export interface TextareaInputProps {
   name: string;
   placeholder: string;
   value: string;
+  cols: string;
+  maxlength: string;
+  rows: string;
 }
 
 export default (props: TextareaInputProps): HTMLElement => {
   return (
     <fragment>
       <label for={props.id}> {props.title} </label>
-      <textarea class="bk-widget-form-input"
-        id={props.id} name={props.name} placeholder={props.placeholder}>{props.value}</textarea>
+      <textarea class="bk-widget-form-input" id={props.id} name={props.name}
+                placeholder={props.placeholder} cols={props.cols} maxlength={props.maxlength}
+                rows={props.row}>{props.value}</textarea>
     </fragment>
   )
 }
