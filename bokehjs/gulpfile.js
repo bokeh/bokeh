c7ec2870d3ff9f85f797963370b4311af0333f4c
Mateusz Paprocki <mattpap@gmail.com>
2017-05-09 18:06:24 +0200

    Finalize reimplementation of build in TypeScript

--- bokehjs/gulpfile.js
+++ bokehjs/gulpfile.js
@@ -1,3 +1,22 @@
-require("coffee-script/register");
+const chalk = require("chalk")
+const {TSError} = require("ts-node")
+
+function prettyTSError(error) {
+  const title = `${chalk.red('⨯')} Unable to compile TypeScript`
+  return `${chalk.bold(title)}\n${error.diagnostics.map((line) => line.message).join('\n')}`
+}
+
+module.constructor.prototype.require = function(modulePath) {
+  try {
+    return this.constructor._load(modulePath, this)
+  } catch (err) {
+    if (err instanceof TSError) {
+      console.error(prettyTSError(err))
+      process.exit(1)
+    } else
+      throw err
+  }
+}
+
 require("ts-node").register({project: "./gulp/tsconfig.json"});
 require("./gulp");
