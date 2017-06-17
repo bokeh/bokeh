8462f6c23e0bdc5ac400b46b12a3f5568ecf2515
Mateusz Paprocki <mattpap@gmail.com>
2017-06-17 17:06:20 +0200

    Use js_on_click() in models/server/buttons

--- examples/models/server/buttons.py
+++ examples/models/server/buttons.py
@@ -2,23 +2,45 @@ from __future__ import print_function
 
 from bokeh.client import push_session
 from bokeh.document import Document
+from bokeh.models import CustomJS
 from bokeh.models.layouts import WidgetBox
 from bokeh.models.widgets import (
     Button, Toggle, Dropdown, CheckboxGroup, RadioGroup,
     CheckboxButtonGroup, RadioButtonGroup,
 )
 
-def button_handler():
-    print("button_handler: click")
+button = Button(label="Button (enabled) - has click event", button_type="primary")
+button.on_click(lambda: print('button: click'))
+button.js_on_click(CustomJS(code="console.log('button: click', this.toString())"))
 
-def toggle_handler(active):
-    print("toggle_handler: %s" % active)
+button_disabled = Button(label="Button (disabled) - no click event", button_type="primary", disabled=True)
+button_disabled.on_click(lambda: print('button_disabled: click'))
+button_disabled.js_on_click(CustomJS(code="console.log('button_disabled: click', this.toString())"))
 
-def dropdown_handler(value):
-    print("dropdown_handler: %s" % value)
+toggle_inactive = Toggle(label="Toggle button (initially inactive)", button_type="success")
+toggle_inactive.on_click(lambda value: print('toggle_inactive: %s' % value))
+toggle_inactive.js_on_click(CustomJS(code="console.log('toggle_inactive: ' + this.active, this.toString())"))
 
-def split_handler(value):
-    print("split_handler: %s" % value)
+toggle_active = Toggle(label="Toggle button (initially active)", button_type="success", active=True)
+toggle_active.on_click(lambda value: print('toggle_active: %s' % value))
+toggle_active.js_on_click(CustomJS(code="console.log('toggle_active: ' + this.active, this.toString())"))
+
+menu = [("Item 1", "item_1_value"), ("Item 2", "item_2_value"), None, ("Item 3", "item_3_value")]
+
+dropdown = Dropdown(label="Dropdown button", button_type="warning", menu=menu, default_value="item_1_value")
+dropdown.on_click(lambda value: print('dropdown: %s' % value))
+dropdown.js_on_click(CustomJS(code="console.log('dropdown: ' + this.value, this.toString())"))
+
+dropdown_disabled = Dropdown(label="Dropdown button (disabled)", button_type="warning", menu=menu, default_value="item_1_value")
+dropdown_disabled.on_click(lambda value: print('dropdown_disabled: %s' % value))
+dropdown_disabled.js_on_click(CustomJS(code="console.log('dropdown_disabled: ' + this.value, this.toString())"))
+
+dropdown_split = Dropdown(label="Split button", button_type="danger", menu=menu) #, default_value="default")
+dropdown_split.on_click(lambda value: print('dropdown_split: %s' % value))
+dropdown_split.js_on_click(CustomJS(code="console.log('dropdown_split: ' + this.value, this.toString())"))
+
+
+
 
 def checkbox_group_handler(active):
     print("checkbox_group_handler: %s" % active)
@@ -32,23 +54,6 @@ def checkbox_button_group_handler(active):
 def radio_button_group_handler(active):
     print("radio_button_group_handler: %s" % active)
 
-button = Button(label="Button (enabled) - has click event", button_type="primary")
-button.on_click(button_handler)
-
-button_disabled = Button(label="Button (disabled) - no click event", button_type="primary", disabled=True)
-button_disabled.on_click(button_handler)
-
-toggle = Toggle(label="Toggle button", button_type="success")
-toggle.on_click(toggle_handler)
-
-menu = [("Item 1", "item_1_value"), ("Item 2", "item_2_value"), ("Item 3", "item_3_value")]
-dropdown = Dropdown(label="Dropdown button", button_type="warning", menu=menu, default_value="item_1_value")
-dropdown.on_click(dropdown_handler)
-
-split_menu = [("Item 1", "item_1_value"), ("Item 2", "item_2_value"), None, ("Item 3", "item_3_value")]
-split = Dropdown(label="Split button", button_type="danger", menu=split_menu)
-split.on_click(split_handler)
-
 checkbox_group = CheckboxGroup(labels=["Option 1", "Option 2", "Option 3"], active=[0, 1])
 checkbox_group.on_click(checkbox_group_handler)
 
@@ -61,7 +66,13 @@ checkbox_button_group.on_click(checkbox_button_group_handler)
 radio_button_group = RadioButtonGroup(labels=["Option 1", "Option 2", "Option 3"], active=0)
 radio_button_group.on_click(radio_button_group_handler)
 
-widgetBox = WidgetBox(children=[button, button_disabled, toggle, dropdown, split, checkbox_group, radio_group, checkbox_button_group, radio_button_group])
+widgetBox = WidgetBox(children=[
+    button, button_disabled,
+    toggle_inactive, toggle_active,
+    dropdown, dropdown_disabled, dropdown_split,
+    checkbox_group, radio_group,
+    checkbox_button_group, radio_button_group,
+])
 
 document = Document()
 document.add_root(widgetBox)
