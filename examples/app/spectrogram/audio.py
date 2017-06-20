f82ba26ddda1bf5ccffa2f40707c673f2ee1892f
Bryan Van de Ven <bryanv@continuum.io>
2017-06-20 16:11:39 -0500

    Collect small docs and examples improvements (#6506)
    * fix up suboptimal numpy usage
    
    * Replaced synthetic spectrogram signal with FM modulation
    
    * fix vbar link pointing to rect:
    
    * clarify title docs
    
    * add information about property loading BokehJS in reveal notebook slides
    
    * improve Figure glyph method docs
    
    * link toolbar positioning docs in layout section

--- examples/app/spectrogram/audio.py
+++ examples/app/spectrogram/audio.py
@@ -7,7 +7,7 @@ import scipy as sp
 from scipy.integrate import simps
 
 NUM_SAMPLES = 1024
-SAMPLING_RATE = 44100
+SAMPLING_RATE = 44100.
 MAX_FREQ = SAMPLING_RATE / 2
 FREQ_SAMPLES = NUM_SAMPLES / 8
 TIMESLICE = 100  # ms
@@ -45,21 +45,29 @@ except:
     print(" *** Pyaudio package not installed, using synthesized audio data ***")
     print()
 
+    def fm_modulation(x, f_carrier = 220, f_mod =220, Ind_mod = 1):
+        y = np.sin(2*np.pi*f_carrier*x + Ind_mod*np.sin(2*np.pi*f_mod*x))
+        return y
+
     # These are basically picked out of a hat to show something vaguely interesting
     _t = np.arange(0, NUM_SAMPLES/SAMPLING_RATE, 1.0/SAMPLING_RATE)
-    _f = 2000 + 3000*(2+np.sin(4*np.linspace(0, 2*np.pi, 500)))
-    _i = 0
+    _f_carrier = 2000
+    _f_mod = 1000
+    _ind_mod = 1
 
     def update_audio_data():
         while True:
-            global _i
-            A = 0.3 + 0.05 * np.random.random()
-            signal = A*np.sin(2*np.pi*_f[_i]*_t + np.sin(2*np.pi*200*_t))
+            # Generate FM signal with drifting carrier and mod frequencies
+            global _f_carrier, _f_mod, _ind_mod
+            _f_carrier = max([_f_carrier+np.random.randn()*50, 0])
+            _f_mod = max([_f_mod+np.random.randn()*20, 0])
+            _ind_mod = max([_ind_mod+np.random.randn()*0.1, 0])
+            A = 0.4 + 0.05 * np.random.random()
+            signal = A * fm_modulation(_t, _f_carrier, _f_mod, _ind_mod)
 
             fft = sp.fft(signal)
             spectrum = abs(fft)[:int(NUM_SAMPLES/2)]
             power = spectrum**2
             bins = simps(np.split(power, NUM_BINS))
             data['values'] = signal, spectrum, bins
-            _i = (_i + 1) % len(_f)
             sleep(1.0/12)
