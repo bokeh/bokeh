b0e4f997a5b8a5019a2328ff621366a6af103145
Mateusz Paprocki <mattpap@gmail.com>
2017-06-23 14:30:11 +0200

    Update documentation

--- sphinx/source/docs/user_guide/source_examples/interaction_range_slider.py
+++ sphinx/source/docs/user_guide/source_examples/interaction_range_slider.py
@@ -4,6 +4,6 @@ from bokeh.models.widgets import RangeSlider
 
 output_file("range_slider.html")
 
-range_slider = RangeSlider(start=0, end=10, range=(1,9), step=.1, title="Stuff")
+range_slider = RangeSlider(start=0, end=10, value=(1,9), step=.1, title="Stuff")
 
 show(widgetbox(range_slider))
