3c8b67c4d5f1326d1cb65c8c727ed46c64b2140a
Mateusz Paprocki <mattpap@gmail.com>
2017-06-29 15:29:14 +0200

    Display actual file sizes in gulp test:size

--- bokehjs/test/size.ts
+++ bokehjs/test/size.ts
@@ -6,25 +6,27 @@ import {expect} from "chai"
 const build_dir = path.normalize(`${__dirname}/../build`)
 
 const LIMITS: {[key: string]: number} = {
-  // css
-  "css/bokeh.min.css":          60,
-  "css/bokeh-widgets.min.css":  70,
-  "css/bokeh-tables.min.css":   30,
   // js
   "js/bokeh.min.js":           730,
   "js/bokeh-widgets.min.js":    90,
   "js/bokeh-tables.min.js":    200,
   "js/bokeh-api.min.js":        75,
   "js/bokeh-gl.min.js":         70,
+  // css
+  "css/bokeh.min.css":          60,
+  "css/bokeh-widgets.min.css":  70,
+  "css/bokeh-tables.min.css":   30,
 }
 
-for (const filename in LIMITS) {
-  const maxsize = LIMITS[filename]
+describe(`bokehjs/build/*/*.min.{js,css} file sizes`, () => {
+  for (const filename in LIMITS) {
+    const stats = fs.statSync(path.join(build_dir, filename))
+    const limit = LIMITS[filename]
 
-  describe(`${filename} file size`, () => {
-    it(`should remain below ${maxsize} kB limit`, () => {
-      const stats = fs.statSync(path.join(build_dir, filename))
-      expect(stats.size).to.be.below(maxsize*1024)
+    describe(`${filename} file size`, () => {
+      it(`should be ${Math.round(stats.size/1024)} kB <= ${limit} kB`, () => {
+        expect(stats.size).to.be.below(limit*1024)
+      })
     })
-  })
-}
+  }
+})
