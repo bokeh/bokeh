3f18a0661eaab6d18de5b9ab7fda01507efa908c
Mateusz Paprocki <mattpap@gmail.com>
2017-07-04 15:57:24 +0200

    Add a preliminary webpack.config.js

---
+++ bokehjs/webpack.config.js
@@ -0,0 +1,33 @@
+require("./ts-node").register({project: "./gulp/tsconfig.json"});
+
+const {build_dir} = require("./gulp/paths");
+
+const webpack = require("webpack");
+const {join} = require("path");
+
+const config = {
+  entry: {
+    "bokeh":         join(build_dir.tree_js, "main.js"),
+    "bokeh-api":     join(build_dir.tree_js, "api.js"),
+    "bokeh-widgets": join(build_dir.tree_js, "models/widgets/main.js"),
+    "bokeh-tables":  join(build_dir.tree_js, "models/widgets/tables/main.js"),
+    "bokeh-gl":      join(build_dir.tree_js, "models/glyphs/webgl/main.js")
+  },
+  output: {
+    filename: "[name].js",
+    path: build_dir.js,
+    library: "Bokeh",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    modules: [build_dir.tree_js, "node_modules"]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "bokeh",
+    }),
+    new webpack.IgnorePlugin(/moment$/)
+  ]
+};
+
+module.exports = config;
