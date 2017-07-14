af4af4ef879219bb417cad32a15d4e4b2ae62e13
Mateusz Paprocki <mattpap@gmail.com>
2017-07-14 16:23:24 +0200

    Add compiler:ts build task

--- bokehjs/gulp/tasks/compiler.ts
+++ bokehjs/gulp/tasks/compiler.ts
@@ -1,9 +1,20 @@
 import * as browserify from "browserify"
 import * as gulp from "gulp"
-import * as path from "path"
+import * as gutil from "gulp-util"
+const ts = require('gulp-typescript')
+import {join, resolve} from "path"
 const source = require('vinyl-source-stream')
 
 import * as paths from "../paths"
+
+gulp.task("compiler:ts", () => {
+  const dir = join(paths.base_dir, "src", "js")
+  const tsconfig = require(join(dir, "tsconfig.json"))
+  const error = (err: {message: string}) => gutil.log(err.message)
+  return gulp.src(join(dir, "compile.ts"))
+    .pipe(ts(tsconfig.compilerOptions, ts.reporter.nullReporter()).on('error', error))
+    .pipe(gulp.dest(paths.build_dir.js))
+})
 
 gulp.task("compiler:build", () => {
   const compilerOpts = {
