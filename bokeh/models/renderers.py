943896d11fda26a60cc6213d41ef442699aa25ab
Luke Canavan <lcanavan@continuum.io>
2017-07-20 16:10:54 -0300

    update bokeh graphrenderer props [skip ci]

--- bokeh/models/renderers.py
+++ bokeh/models/renderers.py
@@ -193,6 +193,18 @@ class GraphRenderer(DataRenderer):
 
     '''
 
+    x_range_name = String('default', help="""
+    A particular (named) x-range to use for computing screen
+    locations when rendering graphs on the plot. If unset, use the
+    default x-range.
+    """)
+
+    y_range_name = String('default', help="""
+    A particular (named) y-range to use for computing screen
+    locations when rendering graphs on the plot. If unset, use the
+    default -range.
+    """)
+
     layout_provider = Instance(LayoutProvider, help="""
     An instance of a LayoutProvider that supplies the layout of the network
     graph in cartesian space.
@@ -208,7 +220,7 @@ class GraphRenderer(DataRenderer):
     rendered as the graph edges.
     """)
 
-    level = Override(default="underlay") # need to fixup (prop to renderers?)
+    level = Override(default="glyph") # need to fixup (prop to renderers?)
 
 @abstract
 class GuideRenderer(Renderer):
