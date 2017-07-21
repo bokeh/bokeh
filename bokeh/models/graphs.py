b35a90cecb7ce0213cde4c3bf8ebdb3dc629c2d0
Luke Canavan <lcanavan@continuum.io>
2017-07-20 21:18:51 -0300

    Readd from_networkx method and fix tests

--- bokeh/models/graphs.py
+++ bokeh/models/graphs.py
@@ -1,6 +1,8 @@
 from ..core.has_props import abstract
 from ..core.properties import Any, Dict, Either, Int, Seq, String
 from ..model import Model
+from ..models.sources import ColumnDataSource
+
 
 @abstract
 class LayoutProvider(Model):
@@ -28,3 +30,45 @@ class StaticLayoutProvider(LayoutProvider):
             2 : [0.86, 1],
         }
     """)
+
+def from_networkx(graph, layout_function, **kwargs):
+        '''
+        Generate a GraphRenderer from a networkx.Graph object and networkx
+        layout function. Any keyword arguments will be passed to the
+        layout function.
+
+        Args:
+            graph (networkx.Graph) : a networkx graph to render
+            layout_function (function) : a networkx layout function
+
+        Returns:
+            instance (GraphRenderer)
+
+        .. warning::
+            Only two dimensional layouts are currently supported.
+
+        '''
+
+        # inline import to prevent circular imports
+        from ..models.renderers import GraphRenderer
+        from ..models.graphs import StaticLayoutProvider
+
+        nodes = graph.nodes()
+        edges = graph.edges()
+        edges_start = [edge[0] for edge in edges]
+        edges_end = [edge[1] for edge in edges]
+
+        node_source = ColumnDataSource(data=dict(index=nodes))
+        edge_source = ColumnDataSource(data=dict(
+            start=edges_start,
+            end=edges_end
+        ))
+
+        graph_renderer = GraphRenderer()
+        graph_renderer.node_renderer.data_source = node_source
+        graph_renderer.edge_renderer.data_source = edge_source
+
+        graph_layout = layout_function(graph, **kwargs)
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        return graph_renderer
--- bokeh/models/tests/test_graphs.py
+++ bokeh/models/tests/test_graphs.py
@@ -1,83 +1,34 @@
 import networkx as nx
 import numpy as np
 
-from bokeh.models.graphs import GraphSource, StaticLayoutProvider
-from bokeh.models.sources import ColumnDataSource
+from bokeh.models.graphs import StaticLayoutProvider, from_networkx
+
 
 def test_staticlayoutprovider_init_props():
     provider = StaticLayoutProvider()
     assert provider.graph_layout == {}
 
-def test_graphsource_init_props():
-    source = GraphSource()
-    assert source.nodes.data == dict(index=[])
-    assert source.edges.data == dict(start=[], end=[])
-    assert source.layout_provider is None
-
-def test_graphsource_check_malformed_graph_source_no_errors():
-    source = GraphSource()
-
-    check = source._check_malformed_graph_source()
-    assert check == []
-
-def test_graphsource_check_malformed_graph_source_no_node_index():
-    node_source = ColumnDataSource()
-    source = GraphSource(nodes=node_source)
-
-    check = source._check_malformed_graph_source()
-    assert check != []
-
-def test_graphsource_check_malformed_graph_source_no_edge_start_or_end():
-    edge_source = ColumnDataSource()
-    source = GraphSource(edges=edge_source)
-
-    check = source._check_malformed_graph_source()
-    assert check != []
-
-def test_graphsource_check_malformed_missing_index_in_start():
-    edge_source = ColumnDataSource(data=dict(
-        start=[1,2,5],
-        end=[1,2,3]))
-    node_source = ColumnDataSource(data=dict(index=[1,2,3]))
-    source = GraphSource(nodes=node_source, edges=edge_source)
-
-    check = source._check_malformed_graph_source()
-    assert check != []
-
-def test_graphsource_check_malformed_missing_index_in_end():
-    edge_source = ColumnDataSource(data=dict(
-        start=[1,2,3],
-        end=[1,2,6]))
-    node_source = ColumnDataSource(data=dict(index=[1,2,3]))
-    source = GraphSource(nodes=node_source, edges=edge_source)
-
-    check = source._check_malformed_graph_source()
-    assert check != []
-
-def test_graphsource_from_networkx():
+def test_from_networkx_method():
     G=nx.Graph()
     G.add_nodes_from([0,1,2,3])
     G.add_edges_from([[0,1], [0,2], [2,3]])
 
-    source = GraphSource.from_networkx(G, nx.circular_layout)
-    assert source.nodes.data["index"] == [0,1,2,3]
-    assert source.edges.data["start"] == [0,0,2]
-    assert source.edges.data["end"] == [1,2,3]
+    renderer = from_networkx(G, nx.circular_layout)
+    assert renderer.node_renderer.data_source.data["index"] == [0,1,2,3]
+    assert renderer.edge_renderer.data_source.data["start"] == [0,0,2]
+    assert renderer.edge_renderer.data_source.data["end"] == [1,2,3]
 
-    gl = source.layout_provider.graph_layout
+    gl = renderer.layout_provider.graph_layout
     assert set(gl.keys()) == set([0,1,2,3])
     assert np.array_equal(gl[0], np.array([1., 0.]))
 
-def test_graphsource_from_networkx_with_kwargs():
+def test_from_networkx_method_with_kwargs():
     G=nx.Graph()
     G.add_nodes_from([0,1,2,3])
     G.add_edges_from([[0,1], [0,2], [2,3]])
 
-    source = GraphSource.from_networkx(G, nx.circular_layout, scale=2)
-    assert source.nodes.data["index"] == [0,1,2,3]
-    assert source.edges.data["start"] == [0,0,2]
-    assert source.edges.data["end"] == [1,2,3]
+    renderer = from_networkx(G, nx.circular_layout, scale=2)
 
-    gl = source.layout_provider.graph_layout
+    gl = renderer.layout_provider.graph_layout
     assert set(gl.keys()) == set([0,1,2,3])
     assert np.array_equal(gl[0], np.array([2., 0.]))
