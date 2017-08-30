54745379576238bc310446b7deedd6d1545eb195
Luke Canavan <lcanavan@anaconda.com>
2017-08-29 20:21:57 -0500

    Set maximum number of embed attempts

--- bokeh/core/_templates/onload_js.js
+++ bokeh/core/_templates/onload_js.js
@@ -1,12 +1,24 @@
-var id = setInterval(embed_document, 100, window);
-
-function embed_document(root) {
-  if (root.Bokeh !== undefined) {
+function(root) {
+  function embed_document(root) {
     var docs_json = {{ docs_json }};
     var render_items = {{ render_items }};
 
     root.Bokeh.embed.embed_items(docs_json, render_items{%- if app_path -%}, "{{ app_path }}" {%- endif -%}{%- if absolute_url -%}, "{{ absolute_url }}" {%- endif -%});
-
-    clearInterval(id);
   }
-}
+
+  if (root.Bokeh !== undefined) {
+    embed_document(root);
+  } else {
+    var attempts = 0;
+    var timer = setInterval(function(root) {
+      if (root.Bokeh !== undefined) {
+        embed_document(root);
+        clearInterval(timer);
+      }
+      attempts++;
+      if (attempts > 10) {
+        clearInterval(timer);
+      }
+    }, 10, root)
+  }
+}(window);
