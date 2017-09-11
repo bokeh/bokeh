fda199bb1a894ab80fff7a55aa80314cb4b244d4
Maggie-M <maggie.mari@continuum.io>
2017-09-11 13:37:31 -0500

    Update gallery

---
+++ examples/plotting/file/bar_mixed.py
@@ -0,0 +1,28 @@
+from bokeh.io import show, output_file
+from bokeh.models import FactorRange
+from bokeh.plotting import figure
+
+output_file("bar_mixed.html")
+
+factors = [
+    ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
+    ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
+    ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
+    ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
+
+]
+
+p = figure(x_range=FactorRange(*factors), plot_height=350,
+           toolbar_location=None, tools="")
+
+x = [ 10, 12, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16 ]
+p.vbar(x=factors, top=x, width=0.9, alpha=0.5)
+
+p.line(x=["Q1", "Q2", "Q3", "Q4"], y=[12, 9, 13, 14], color="red", line_width=2)
+
+p.y_range.start = 0
+p.x_range.range_padding = 0.1
+p.xaxis.major_label_orientation = 1
+p.xgrid.grid_line_color = None
+
+show(p)
---
+++ examples/plotting/file/bar_stacked.py
@@ -0,0 +1,33 @@
+from bokeh.core.properties import value
+from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+
+output_file("bar_stacked.html")
+
+fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+years = ["2015", "2016", "2017"]
+colors = ["#c9d9d3", "#718dbf", "#e84d60"]
+
+data = {'fruits' : fruits,
+        '2015'   : [2, 1, 4, 3, 2, 4],
+        '2016'   : [5, 3, 4, 2, 4, 6],
+        '2017'   : [3, 2, 4, 4, 5, 3]}
+
+source = ColumnDataSource(data=data)
+
+p = figure(x_range=fruits, plot_height=350, title="Fruit Counts by Year",
+           toolbar_location=None, tools="")
+
+p.vbar_stack(years, x='fruits', width=0.9, color=colors, source=source,
+             legend=[value(x) for x in years])
+
+p.y_range.start = 0
+p.x_range.range_padding = 0.1
+p.xgrid.grid_line_color = None
+p.axis.minor_tick_line_color = None
+p.outline_line_color = None
+p.legend.location = "top_left"
+p.legend.orientation = "horizontal"
+
+show(p)
