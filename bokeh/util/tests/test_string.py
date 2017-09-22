84ae7709705aec39dc877182d1ca0371a79bfea0
Bryan Van de Ven <bryanv@anaconda.com>
2017-09-22 12:52:08 -0500

    move indent to bokeh.util.string

--- bokeh/util/tests/test_string.py
+++ bokeh/util/tests/test_string.py
@@ -1,26 +1,41 @@
 from __future__ import absolute_import
 
-from bokeh.util.string import snakify, format_docstring
+import bokeh.util.string as bus
+
+class Test_format_doctring(object):
+    def test_no_argument(self):
+        doc__ = "hello world"
+        assert bus.format_docstring(doc__) == doc__
+        doc__ = None
+        assert bus.format_docstring(doc__) == None
+
+    def test_arguments_unused(self):
+        doc__ = "hello world"
+        assert bus.format_docstring(doc__, 'hello ', not_used='world') == doc__
+        doc__ = None
+        assert bus.format_docstring(doc__, 'hello ', not_used='world') == None
+
+    def test_arguments(self):
+        doc__ = "-- {}{as_parameter} --"
+        assert bus.format_docstring(doc__, 'hello ', as_parameter='world') == "-- hello world --"
+        doc__ = None
+        assert bus.format_docstring(doc__, 'hello ', as_parameter='world') == None
+
+class Test_indent(object):
+    TEXT = "some text\nto indent\n  goes here"
+
+    def test_default_args(self):
+        assert bus.indent(self.TEXT) == "  some text\n  to indent\n    goes here"
+
+    def test_with_n(self):
+        assert bus.indent(self.TEXT, n=3) == "   some text\n   to indent\n     goes here"
+
+    def test_with_ch(self):
+        assert bus.indent(self.TEXT, ch="-") == "--some text\n--to indent\n--  goes here"
+
+
 
 def test_snakify():
-    assert snakify("MyClassName") == "my_class_name"
-    assert snakify("My1Class23Name456") == "my1_class23_name456"
-    assert snakify("MySUPERClassName") == "my_super_class_name"
-
-def test_format_docstring_no_argument():
-    doc__ = "hello world"
-    assert format_docstring(doc__) == doc__
-    doc__ = None
-    assert format_docstring(doc__) == None
-
-def test_format_docstring_arguments_unused():
-    doc__ = "hello world"
-    assert format_docstring(doc__, 'hello ', not_used='world') == doc__
-    doc__ = None
-    assert format_docstring(doc__, 'hello ', not_used='world') == None
-
-def test_format_docstring_arguments():
-    doc__ = "-- {}{as_parameter} --"
-    assert format_docstring(doc__, 'hello ', as_parameter='world') == "-- hello world --"
-    doc__ = None
-    assert format_docstring(doc__, 'hello ', as_parameter='world') == None
+    assert bus.snakify("MyClassName") == "my_class_name"
+    assert bus.snakify("My1Class23Name456") == "my1_class23_name456"
+    assert bus.snakify("MySUPERClassName") == "my_super_class_name"
