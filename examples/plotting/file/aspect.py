92a56872e83c849f82c04efd845c87c15fc562b5
Mateusz Paprocki <mattpap@gmail.com>
2017-10-14 14:39:33 +0200

    Disable toolbar in plotting/file/aspect example

--- examples/plotting/file/aspect.py
+++ examples/plotting/file/aspect.py
@@ -6,17 +6,16 @@ p1.rect(0, 0, 300, 300, line_color='black')
 p1.circle(x=0, y=0, radius=150, line_color='black', fill_color='grey',
           radius_units='data')
 
-
 def draw_test_figure(aspect_scale=1, width=300, height=300):
     p = figure(
         plot_width=width,
         plot_height=height,
         match_aspect=True,
         aspect_scale=aspect_scale,
-        title="Aspect scale = {0}".format(aspect_scale))
+        title="Aspect scale = {0}".format(aspect_scale),
+        toolbar_location=None)
     p.circle([-1, +1, +1, -1], [-1, -1, +1, +1])
     return p
-
 
 aspect_scales = [0.25, 0.5, 1, 2, 4]
 p2s = [draw_test_figure(aspect_scale=i) for i in aspect_scales]
