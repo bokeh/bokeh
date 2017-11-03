45b659213c865c64649666456100f0802025d150
volker <volker.jaenisch@inqbus.de>
2017-11-03 22:42:37 +0100

    Fixing ... error and delete dangling line

--- bokeh/util/compiler.py
+++ bokeh/util/compiler.py
@@ -448,10 +448,10 @@ def calc_cache_key():
     return key
 
 _bundle_cache = {}
+
 def bundle_all_models():
     key = calc_cache_key()
     bundle = _bundle_cache.get(key, None)
     if bundle is None:
-        _bundle_cache[key] = bundle = bundle_models(...) or ""
+        _bundle_cache[key] = bundle = bundle_models(Model.model_class_reverse_map.values()) or ""
     return bundle
-
