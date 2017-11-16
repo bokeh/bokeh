0c5a02f0fe8f0e8dd33fb0225bd307a11b8f350f
Mateusz Paprocki <mattpap@gmail.com>
2017-11-16 19:38:53 +0100

    Merge Ticker and ContinuousTicker's tests

--- bokehjs/test/models/tickers/continuous_ticker.ts
+++ bokehjs/test/models/tickers/continuous_ticker.ts
@@ -4,10 +4,52 @@ import {ContinuousTicker} from "models/tickers/continuous_ticker"
 
 describe("ContinuousTicker Model", () => {
 
+  class MyTicker extends ContinuousTicker {
+    get_interval(_data_low: number, _data_high: number, _desired_n_ticks: number) {
+      return 100
+    }
+  }
+
+  it("should have five major and ten minor ticks", () => {
+    const ticker = new MyTicker({num_minor_ticks: 2, desired_num_ticks: 5})
+
+    // `range` and `cross_loc` aren't used by the AdaptiveTicker, so are passed as null args
+    const ticks = ticker.get_ticks(-200, 200, null, null, {})
+    expect(ticks.major).to.be.deep.equal([-200, -100, 0, 100, 200])
+    expect(ticks.minor).to.be.deep.equal([-250, -200, -150, -100, -50, 0, 50, 100, 150, 200, 250])
+  })
+
+  it("should have five major and five minor ticks", () => {
+    const ticker = new MyTicker({num_minor_ticks: 1, desired_num_ticks: 5})
+
+    // `range` and `cross_loc` aren't used by the AdaptiveTicker, so are passed as null args
+    const ticks = ticker.get_ticks(-200, 200, null, null, {})
+    expect(ticks.major).to.be.deep.equal([-200, -100, 0, 100, 200])
+    expect(ticks.minor).to.be.deep.equal([-200, -100, 0, 100, 200])
+  })
+
+  it("should have five major and zero minor ticks", () => {
+    const ticker = new MyTicker({num_minor_ticks: 0, desired_num_ticks: 5})
+
+    // `range` and `cross_loc` aren't used by the AdaptiveTicker, so are passed as null args
+    const ticks = ticker.get_ticks(-200, 200, null, null, {})
+    expect(ticks.major).to.be.deep.equal([-200, -100, 0, 100, 200])
+    expect(ticks.minor).to.be.deep.equal([])
+  })
+
+  it("should handle empty start/end case by returning no ticks", () => {
+    const ticker = new MyTicker({num_minor_ticks: 2, desired_num_ticks: 5})
+
+    // `range` and `cross_loc` aren't used by the AdaptiveTicker, so are passed as null args
+    const ticks = ticker.get_ticks(NaN, NaN, null, null, {})
+    expect(ticks.major).to.be.deep.equal([])
+    expect(ticks.minor).to.be.deep.equal([])
+  })
+
   describe("ContinuousTicker get_min_interval method", () => {
 
     it("should return min_interval property", () => {
-      const ticker = new ContinuousTicker({min_interval: 1})
+      const ticker = new MyTicker({min_interval: 1})
       expect(ticker.get_min_interval()).to.be.equal(1)
     })
   })
@@ -15,12 +57,12 @@ describe("ContinuousTicker Model", () => {
   describe("ContinuousTicker get_max_interval method", () => {
 
     it("should return max_interval property if set", () => {
-      const ticker = new ContinuousTicker({max_interval: 2})
+      const ticker = new MyTicker({max_interval: 2})
       expect(ticker.get_max_interval()).to.be.equal(2)
     })
 
     it("should return Infinity if unset", () => {
-      const ticker = new ContinuousTicker()
+      const ticker = new MyTicker()
       expect(ticker.get_max_interval()).to.be.equal(Infinity)
     })
   })
@@ -28,19 +70,19 @@ describe("ContinuousTicker Model", () => {
   describe("ContinuousTicker get_interval method", () => {
 
     it("should return correct tick interval", () => {
-      const ticker = new ContinuousTicker()
+      const ticker = new MyTicker()
       const interval = ticker.get_ideal_interval(0, 100, 10)
       expect(interval).to.be.equal(10)
     })
 
     it("should return NaN if start/end is NaN", () => {
-      const ticker = new ContinuousTicker()
+      const ticker = new MyTicker()
       const interval = ticker.get_ideal_interval(0, NaN, 10)
       expect(interval).to.be.NaN
     })
 
     it("should return NaN if desired_n_ticks is NaN", () => {
-      const ticker = new ContinuousTicker()
+      const ticker = new MyTicker()
       const interval = ticker.get_ideal_interval(0, 100, NaN)
       expect(interval).to.be.NaN
     })
