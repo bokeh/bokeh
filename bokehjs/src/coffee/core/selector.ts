f70ba8aa826f3a6596102a48fc8088bf3ea67372
Mateusz Paprocki <mattpap@gmail.com>
2017-11-17 10:37:30 +0100

    Rewrite core/selector in TypeScript

--- bokehjs/src/coffee/core/selector.ts
+++ bokehjs/src/coffee/core/selector.ts
@@ -1,24 +1,32 @@
 import {HasProps} from "./has_props"
-import * as hittest from "./hittest"
+import {HitTestResult} from "./hittest"
 import * as p from "./properties"
 
-export class Selector extends HasProps
-  type: 'Selector'
+export class Selector extends HasProps {
 
-  update: (indices, final, append, silent=false) ->
-    @setv('timestamp', new Date(), {silent: silent})
-    @setv('final', final, {silent: silent})
-    if append
-      indices.update_through_union(@indices)
-    @setv('indices', indices, {silent: silent})
+  indices: HitTestResult
+  final: boolean
+  timestamp: Date
 
-  clear: () ->
-    @timestamp = new Date()
-    @final = true
-    @indices = hittest.create_hit_test_result()
-
-  @internal {
-    indices:   [ p.Any, () -> hittest.create_hit_test_result() ]
-    final:     [ p.Boolean ]
-    timestamp: [ p.Any ]
+  update(indices: HitTestResult, final: boolean, append: boolean, silent: boolean = false): void {
+    this.setv('timestamp', new Date(), {silent: silent})
+    this.setv('final', final, {silent: silent})
+    if (append)
+      indices.update_through_union(this.indices)
+    this.setv('indices', indices, {silent: silent})
   }
+
+  clear(): void {
+    this.timestamp = new Date()
+    this.final = true
+    this.indices = new HitTestResult()
+  }
+}
+
+Selector.prototype.type = "Selector"
+
+Selector.internal({
+  indices:   [ p.Any, () => new HitTestResult() ],
+  final:     [ p.Boolean ],
+  timestamp: [ p.Any ],
+})
