14c72c94b6cc8e3576e9fa39e86c4c8cf318d29a
Mateusz Paprocki <mattpap@gmail.com>
2017-11-20 18:51:43 +0100

    Rewrite core/util/selection in TypeScript

--- bokehjs/src/coffee/core/util/selection.ts
+++ bokehjs/src/coffee/core/util/selection.ts
@@ -1,11 +1,12 @@
-export get_indices = (data_source) ->
-  selected = data_source.selected
+export function get_indices(data_source: any): any {
+  const selected = data_source.selected
 
-  if selected['0d'].glyph
+  if (selected['0d'].glyph)
     selected['0d'].indices
-  else if selected['1d'].indices.length > 0
+  else if (selected['1d'].indices.length > 0)
     selected['1d'].indices
-  else if selected['2d'].indices.length > 0
+  else if (selected['2d'].indices.length > 0)
     selected['2d'].indices
   else
     []
+}
