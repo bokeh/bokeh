990320fa4ff3d9f08b9e2ef08740a03cea83a7b5
Mateusz Paprocki <mattpap@gmail.com>
2017-11-20 18:30:51 +0100

    Rewrite core/util/canvas in TypeScript

--- bokehjs/src/coffee/core/util/canvas.ts
+++ bokehjs/src/coffee/core/util/canvas.ts
@@ -1,56 +1,76 @@
-fixup_line_dash = (ctx) ->
-  if (!ctx.setLineDash)
-    ctx.setLineDash = (dash) ->
+import {OutputBackend} from "../enums"
+
+function fixup_line_dash(ctx: any): void {
+  if (!ctx.setLineDash) {
+    ctx.setLineDash = (dash: any): void => {
       ctx.mozDash = dash
       ctx.webkitLineDash = dash
-  if (!ctx.getLineDash)
-    ctx.getLineDash = () ->
+    }
+  }
+  if (!ctx.getLineDash) {
+    ctx.getLineDash = (): any => {
       return ctx.mozDash
+    }
+  }
+}
 
-fixup_line_dash_offset = (ctx) ->
-  ctx.setLineDashOffset = (dash_offset) ->
+function fixup_line_dash_offset(ctx: any): void {
+  ctx.setLineDashOffset = (dash_offset: any): void => {
     ctx.lineDashOffset = dash_offset
     ctx.mozDashOffset = dash_offset
     ctx.webkitLineDashOffset = dash_offset
-  ctx.getLineDashOffset = () ->
+  }
+  ctx.getLineDashOffset = (): number => {
     return ctx.mozDashOffset
+  }
+}
 
-fixup_image_smoothing = (ctx) ->
-  ctx.setImageSmoothingEnabled = (value) ->
+function fixup_image_smoothing(ctx: any): void {
+  ctx.setImageSmoothingEnabled = (value: any): void => {
     ctx.imageSmoothingEnabled = value
     ctx.mozImageSmoothingEnabled = value
     ctx.oImageSmoothingEnabled = value
     ctx.webkitImageSmoothingEnabled = value
     ctx.msImageSmoothingEnabled = value
-  ctx.getImageSmoothingEnabled = () ->
-    return ctx.imageSmoothingEnabled ? true
+  }
+  ctx.getImageSmoothingEnabled = (): boolean => {
+    const val = ctx.imageSmoothingEnabled
+    return val != null ? val : true
+  }
+}
 
-fixup_measure_text = (ctx) ->
-  if ctx.measureText and not ctx.html5MeasureText?
+function fixup_measure_text(ctx: any): void {
+  if (ctx.measureText && ctx.html5MeasureText == null) {
     ctx.html5MeasureText = ctx.measureText
 
-    ctx.measureText = (text) ->
-      textMetrics = ctx.html5MeasureText(text)
-      # fake it til you make it
+    ctx.measureText = (text: string) => {
+      const textMetrics = ctx.html5MeasureText(text)
+      // fake it til you make it
       textMetrics.ascent = ctx.html5MeasureText("m").width * 1.6
       return textMetrics
+    }
+  }
+}
 
-fixup_ellipse = (ctx) ->
-  # implementing the ctx.ellipse function with bezier curves
-  # we don't implement the startAngle, endAngle and anticlockwise arguments.
-  ellipse_bezier = (x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise = false) ->
-    c = 0.551784 # see http://www.tinaja.com/glib/ellipse4.pdf
+function fixup_ellipse(ctx: any): void {
+  // implementing the ctx.ellipse function with bezier curves
+  // we don't implement the startAngle, endAngle and anticlockwise arguments.
+  function ellipse_bezier(x: number, y: number,
+                          radiusX: number, radiusY: number,
+                          rotation: number, _startAngle: number, _endAngle: number, anticlockwise: boolean = false) {
+    const c = 0.551784 // see http://www.tinaja.com/glib/ellipse4.pdf
 
     ctx.translate(x, y)
     ctx.rotate(rotation)
 
-    rx = radiusX
-    ry = radiusY
-    if anticlockwise
+    let rx = radiusX
+    let ry = radiusY
+    if (anticlockwise) {
       rx = -radiusX
       ry = -radiusY
+    }
 
-    ctx.moveTo(-rx, 0) # start point of first curve
+    ctx.moveTo(-rx, 0) // start point of first curve
     ctx.bezierCurveTo(-rx,  ry * c, -rx * c,  ry, 0,  ry)
     ctx.bezierCurveTo( rx * c,  ry,  rx,  ry * c,  rx, 0)
     ctx.bezierCurveTo( rx, -ry * c,  rx * c, -ry, 0, -ry)
@@ -58,28 +78,31 @@ fixup_ellipse = (ctx) ->
 
     ctx.rotate(-rotation)
     ctx.translate(-x, -y)
-    return
+  }
 
   if (!ctx.ellipse)
     ctx.ellipse = ellipse_bezier
+}
 
-export fixup_ctx = (ctx) ->
+export function fixup_ctx(ctx: any): void {
   fixup_line_dash(ctx)
   fixup_line_dash_offset(ctx)
   fixup_image_smoothing(ctx)
   fixup_measure_text(ctx)
   fixup_ellipse(ctx)
+}
 
-export get_scale_ratio = (ctx, hidpi, backend) ->
-  if backend == "svg"
+export function get_scale_ratio(ctx: any, hidpi: boolean, backend: OutputBackend): number {
+  if (backend == "svg")
     return 1
-  else if hidpi
-    devicePixelRatio = window.devicePixelRatio || 1
-    backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
-                        ctx.mozBackingStorePixelRatio ||
-                        ctx.msBackingStorePixelRatio ||
-                        ctx.oBackingStorePixelRatio ||
-                        ctx.backingStorePixelRatio || 1
+  else if (hidpi) {
+    const devicePixelRatio = window.devicePixelRatio || 1
+    const backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+                              ctx.mozBackingStorePixelRatio    ||
+                              ctx.msBackingStorePixelRatio     ||
+                              ctx.oBackingStorePixelRatio      ||
+                              ctx.backingStorePixelRatio       || 1
     return devicePixelRatio / backingStoreRatio
-  else
+  } else
     return 1
+}
