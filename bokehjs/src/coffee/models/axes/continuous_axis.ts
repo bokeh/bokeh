3364d6aa9b1d5ab10956a96147965a779a1bd49d
Mateusz Paprocki <mattpap@gmail.com>
2017-12-04 20:12:56 +0100

    Rewrite models/axes/continuous_axis in TypeScript

--- bokehjs/src/coffee/models/axes/continuous_axis.ts
+++ bokehjs/src/coffee/models/axes/continuous_axis.ts
@@ -1,4 +1,5 @@
 import {Axis} from "./axis"
 
-export class ContinuousAxis extends Axis
-  type: 'ContinuousAxis'
+export class ContinuousAxis extends Axis {}
+
+ContinuousAxis.prototype.type = "ContinuousAxis"
