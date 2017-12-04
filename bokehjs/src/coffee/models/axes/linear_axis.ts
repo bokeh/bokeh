d433092536f359bd1b545ea74535c2b2c96bb471
Mateusz Paprocki <mattpap@gmail.com>
2017-12-04 20:16:13 +0100

    Rewrite models/axes/linear_axis in TypeScript

--- bokehjs/src/coffee/models/axes/linear_axis.ts
+++ bokehjs/src/coffee/models/axes/linear_axis.ts
@@ -3,14 +3,18 @@ import {ContinuousAxis} from "./continuous_axis"
 import {BasicTickFormatter} from "../formatters/basic_tick_formatter"
 import {BasicTicker} from "../tickers/basic_ticker"
 
-export class LinearAxisView extends AxisView
+export class LinearAxisView extends AxisView {}
 
-export class LinearAxis extends ContinuousAxis
-  default_view: LinearAxisView
+export class LinearAxis extends ContinuousAxis {
+  ticker: BasicTicker
+  formatters: BasicTickFormatter
+}
 
-  type: 'LinearAxis'
+LinearAxis.prototype.type = "LinearAxis"
 
-  @override {
-    ticker:    () -> new BasicTicker()
-    formatter: () -> new BasicTickFormatter()
-  }
+LinearAxis.prototype.default_view = LinearAxisView
+
+LinearAxis.override({
+  ticker:    () => new BasicTicker(),
+  formatter: () => new BasicTickFormatter(),
+})
