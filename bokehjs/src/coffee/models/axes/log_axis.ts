6fe46034b621ce5e0171a4e26013fe5eb011434d
Mateusz Paprocki <mattpap@gmail.com>
2017-12-04 20:18:31 +0100

    Rewrite models/axes/log_axis in TypeScript

--- bokehjs/src/coffee/models/axes/log_axis.ts
+++ bokehjs/src/coffee/models/axes/log_axis.ts
@@ -3,14 +3,18 @@ import {ContinuousAxis} from "./continuous_axis"
 import {LogTickFormatter} from "../formatters/log_tick_formatter"
 import {LogTicker} from "../tickers/log_ticker"
 
-export class LogAxisView extends AxisView
+export class LogAxisView extends AxisView {}
 
-export class LogAxis extends ContinuousAxis
-  default_view: LogAxisView
+export class LogAxis extends ContinuousAxis {
+  ticker:    LogTicker
+  formatter: LogTickFormatter
+}
 
-  type: 'LogAxis'
+LogAxis.prototype.type = "LogAxis"
 
-  @override {
-    ticker:    () -> new LogTicker()
-    formatter: () -> new LogTickFormatter()
-  }
+LogAxis.prototype.default_view = LogAxisView
+
+LogAxis.override({
+  ticker:    () => new LogTicker(),
+  formatter: () => new LogTickFormatter(),
+})
