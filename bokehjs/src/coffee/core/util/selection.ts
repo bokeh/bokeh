6f70a1e4edf6f3e7fd66cebdf2d77a7ab3f8b439
Bryan Van de Ven <bryanv@anaconda.com>
2017-12-05 15:12:37 -0600

    add missing return statements to get_indices TypeScript version (#7282)

--- bokehjs/src/coffee/core/util/selection.ts
+++ bokehjs/src/coffee/core/util/selection.ts
@@ -2,11 +2,11 @@ export function get_indices(data_source: any): any {
   const selected = data_source.selected
 
   if (selected['0d'].glyph)
-    selected['0d'].indices
+    return selected['0d'].indices
   else if (selected['1d'].indices.length > 0)
-    selected['1d'].indices
+    return selected['1d'].indices
   else if (selected['2d'].indices.length > 0)
-    selected['2d'].indices
+    return selected['2d'].indices
   else
-    []
+    return []
 }
