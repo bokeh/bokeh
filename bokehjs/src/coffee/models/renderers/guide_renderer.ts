e6b1983feb1f174252a52523b6d64b514ff4624c
Mateusz Paprocki <mattpap@gmail.com>
2017-12-05 14:41:52 +0100

    Rewrite models/renderers/guide_renderer in TypeScript

--- bokehjs/src/coffee/models/renderers/guide_renderer.ts
+++ bokehjs/src/coffee/models/renderers/guide_renderer.ts
@@ -2,28 +2,24 @@ import {Renderer, RendererView} from "./renderer"
 import {Plot} from "../plots/plot"
 import * as p from "core/properties"
 
-export class GuideRendererView extends RendererView
-  `
+export class GuideRendererView extends RendererView {
   visuals: GuideRenderer.Visuals
-  `
+}
 
-export class GuideRenderer extends Renderer
-  type: 'GuideRenderer'
-
-  `
+export class GuideRenderer extends Renderer {
   plot: Plot
-  `
+}
 
-  @define {
-    plot:  [ p.Instance ]
-  }
+GuideRenderer.prototype.type = "GuideRenderer"
 
-  @override {
-    level: 'overlay'
-  }
+GuideRenderer.define({
+  plot: [ p.Instance ]
+})
 
-`
+GuideRenderer.override({
+  level: "overlay"
+})
+
 export module GuideRenderer {
   export type Visuals = Renderer.Visuals
 }
-`
