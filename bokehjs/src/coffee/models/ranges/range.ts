f2c64ebdc80b25d8b5e37b476d548320404b5c59
Mateusz Paprocki <mattpap@gmail.com>
2017-12-05 23:35:38 +0100

    Rewrite models/ranges/range in TypeScript

--- bokehjs/src/coffee/models/ranges/range.ts
+++ bokehjs/src/coffee/models/ranges/range.ts
@@ -1,39 +1,45 @@
 import {Model} from "../../model"
+import {CustomJS} from "../callbacks/customjs"
 import * as p from "core/properties"
 import {isFunction} from "core/util/types"
 
-export class Range extends Model
-  type: 'Range'
+export abstract class Range extends Model {
 
-  `
   start: number
   end: number
   min: number
   max: number
-  `
+  callback?: ((obj: Range) => void) | CustomJS // XXX: Callback
 
-  initialize: (options) ->
-    super(options)
-    @connect(@change, () -> @_emit_callback())
-
-  @define {
-    callback: [ p.Any ] # TODO: p.Either(p.Instance(Callback), p.Function)
+  initialize(attrs: any, options: any): void {
+    super.initialize(attrs, options)
+    this.connect(this.change, () => this._emit_callback())
   }
 
-  @internal {
-    plots: [ p.Array, [] ]
+  reset(): void {
+    /**
+     * This method should be reimplemented by subclasses and ensure that
+     * the callback, if exists, is executed at completion.
+     */
+    this.change.emit(undefined)
   }
 
-  reset: () ->
-    """
-    This method should be reimplemented by subclasses and ensure that
-    the callback, if exists, is executed at completion.
-    """
-    @change.emit()
-
-  _emit_callback: () ->
-    if @callback?
-      if isFunction(@callback)
-        @callback(@)
+  protected _emit_callback(): void {
+    if (this.callback != null) {
+      if (isFunction(this.callback))
+        this.callback(this)
       else
-        @callback.execute(@)
+        this.callback.execute(this, {})
+    }
+  }
+}
+
+Range.prototype.type = "Range"
+
+Range.define({
+  callback: [ p.Any ] // TODO: p.Either(p.Instance(Callback), p.Function)
+})
+
+Range.internal({
+  plots: [ p.Array, [] ]
+})
