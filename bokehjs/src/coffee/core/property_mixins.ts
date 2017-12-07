0977da7ebf3b059588018c5b4df0daba1b49d96a
Mateusz Paprocki <mattpap@gmail.com>
2017-12-07 15:39:58 +0100

    Rewrite core/property_mixins in TypeScript

--- bokehjs/src/coffee/core/property_mixins.ts
+++ bokehjs/src/coffee/core/property_mixins.ts
@@ -1,46 +1,62 @@
 import * as p from "./properties"
 import {extend} from "./util/object"
 
-_gen_mixin = (mixin, prefix="") ->
-  result = {}
-  for name, type of mixin
-    result[prefix+name] = type
+function _gen_mixin(mixin: {[key: string]: any}, prefix: string) {
+  const result: {[key: string]: any} = {}
+  for (const name in mixin) {
+    const prop = mixin[name]
+    result[prefix + name] = prop
+  }
   return result
+}
 
-_line_mixin =
-  line_color:       [ p.ColorSpec,  'black'   ]
-  line_width:       [ p.NumberSpec, 1         ]
-  line_alpha:       [ p.NumberSpec, 1.0       ]
-  line_join:        [ p.LineJoin,   'miter'   ]
-  line_cap:         [ p.LineCap,    'butt'    ]
-  line_dash:        [ p.Array,      []        ]
-  line_dash_offset: [ p.Number,     0         ]
+const _line_mixin = {
+  line_color:       [ p.ColorSpec,  'black'   ],
+  line_width:       [ p.NumberSpec, 1         ],
+  line_alpha:       [ p.NumberSpec, 1.0       ],
+  line_join:        [ p.LineJoin,   'miter'   ],
+  line_cap:         [ p.LineCap,    'butt'    ],
+  line_dash:        [ p.Array,      []        ],
+  line_dash_offset: [ p.Number,     0         ],
+}
 
-export line = (prefix="") -> _gen_mixin(_line_mixin, prefix)
+export const line = (prefix: string = "") => _gen_mixin(_line_mixin, prefix)
 
-_fill_mixin =
-  fill_color: [ p.ColorSpec,  'gray' ]
-  fill_alpha: [ p.NumberSpec, 1.0    ]
+const _fill_mixin = {
+  fill_color: [ p.ColorSpec,  'gray' ],
+  fill_alpha: [ p.NumberSpec, 1.0    ],
+}
 
-export fill = (prefix="") -> _gen_mixin(_fill_mixin, prefix)
+export const fill = (prefix: string = "") => _gen_mixin(_fill_mixin, prefix)
 
-_text_mixin =
-  text_font:        [ p.Font,         'helvetica' ]
-  text_font_size:   [ p.FontSizeSpec, '12pt'      ]
-  text_font_style:  [ p.FontStyle,    'normal'    ]
-  text_color:       [ p.ColorSpec,    '#444444'   ]
-  text_alpha:       [ p.NumberSpec,   1.0         ]
-  text_align:       [ p.TextAlign,    'left'      ]
-  text_baseline:    [ p.TextBaseline, 'bottom'    ]
-  text_line_height: [ p.Number,       1.2         ]
+const _text_mixin = {
+  text_font:        [ p.Font,         'helvetica' ],
+  text_font_size:   [ p.FontSizeSpec, '12pt'      ],
+  text_font_style:  [ p.FontStyle,    'normal'    ],
+  text_color:       [ p.ColorSpec,    '#444444'   ],
+  text_alpha:       [ p.NumberSpec,   1.0         ],
+  text_align:       [ p.TextAlign,    'left'      ],
+  text_baseline:    [ p.TextBaseline, 'bottom'    ],
+  text_line_height: [ p.Number,       1.2         ],
+}
 
-export text = (prefix="") -> _gen_mixin(_text_mixin, prefix)
+export const text = (prefix: string = "") => _gen_mixin(_text_mixin, prefix)
 
-export create = (configs) ->
-  result = {}
-  for config in configs
-    [kind, prefix] = config.split(":")
-    if not @[kind]?
-      throw new Error("Unknown property mixin kind '#{kind}'")
-    result = extend(result, @[kind](prefix))
+export function create(configs: string[]) {
+  const result: {[key: string]: any} = {}
+
+  for (const config of configs) {
+    const [kind, prefix] = config.split(":")
+    let mixin: any
+    switch (kind) {
+      case "line": mixin = line; break
+      case "fill": mixin = fill; break
+      case "text": mixin = text; break
+      default:
+        throw new Error(`Unknown property mixin kind '${kind}'`)
+    }
+    extend(result, mixin(prefix))
+  }
+
   return result
+}
