a11956cb3cca7ffcbcfe118ae64d2b9632befc98
Mateusz Paprocki <mattpap@gmail.com>
2017-12-11 10:47:09 +0100

    Rewrite models/layouts/widget_box in TypeScript

--- bokehjs/src/coffee/models/layouts/widget_box.ts
+++ bokehjs/src/coffee/models/layouts/widget_box.ts
@@ -1,108 +1,132 @@
 import {logger} from "core/logging"
 import * as p from "core/properties"
 import {extend} from "core/util/object"
+import {Variable} from "core/layout/solver"
 
 import {LayoutDOM, LayoutDOMView} from "../layouts/layout_dom"
 
-export class WidgetBoxView extends LayoutDOMView
-  className: "bk-widget-box"
+export class WidgetBoxView extends LayoutDOMView {
 
-  connect_signals: () ->
-    super()
-    @connect(@model.properties.children.change, () => @rebuild_child_views())
+  connect_signals(): void {
+    super.connect_signals()
+    this.connect(this.model.properties.children.change, () => this.rebuild_child_views())
+  }
 
-  render: () ->
-    @_render_classes() # XXX: because no super()
+  render(): void {
+    this._render_classes() // XXX: because no super()
 
-    if @model.sizing_mode == 'fixed' or @model.sizing_mode == 'scale_height'
-      width = @get_width()
-      if @model._width.value != width
-        @solver.suggest_value(@model._width, width)
+    if (this.model.sizing_mode == 'fixed' || this.model.sizing_mode == 'scale_height') {
+      const width = this.get_width()
+      if (this.model._width.value != width)
+        this.solver.suggest_value(this.model._width, width)
+    }
 
-    if @model.sizing_mode == 'fixed' or @model.sizing_mode == 'scale_width'
-      height = @get_height()
-      if @model._height.value != height
-        @solver.suggest_value(@model._height, height)
+    if (this.model.sizing_mode == 'fixed' || this.model.sizing_mode == 'scale_width') {
+      const height = this.get_height()
+      if (this.model._height.value != height)
+        this.solver.suggest_value(this.model._height, height)
+    }
 
-    @solver.update_variables()
+    this.solver.update_variables()
 
-    if @model.sizing_mode == 'stretch_both'
-      @el.style.position = 'absolute'
-      @el.style.left = "#{@model._dom_left.value}px"
-      @el.style.top = "#{@model._dom_top.value}px"
-      @el.style.width = "#{@model._width.value}px"
-      @el.style.height = "#{@model._height.value}px"
-    else
-      # Note we DO NOT want to set a height (except in stretch_both). Widgets
-      # are happier sizing themselves. We've tried to tell the layout what
-      # the height is with the suggest_value. But that doesn't mean we need
-      # to put it in the dom.
-      if @model._width.value - 20 > 0
-        css_width = "#{@model._width.value - 20}px"
+    if (this.model.sizing_mode == 'stretch_both') {
+      this.el.style.position = 'absolute'
+      this.el.style.left = `${this.model._dom_left.value}px`
+      this.el.style.top = `${this.model._dom_top.value}px`
+      this.el.style.width = `${this.model._width.value}px`
+      this.el.style.height = `${this.model._height.value}px`
+    } else {
+      // Note we DO NOT want to set a height (except in stretch_both). Widgets
+      // are happier sizing themselves. We've tried to tell the layout what
+      // the height is with the suggest_value. But that doesn't mean we need
+      // to put it in the dom.
+      let css_width: string
+      if (this.model._width.value - 20 > 0)
+        css_width = `${this.model._width.value - 20}px`
       else
         css_width = "100%"
 
-      @el.style.width = css_width
+      this.el.style.width = css_width
+    }
+  }
 
-  get_height: () ->
-    height = 0
-    for own key, child_view of @child_views
-      el = child_view.el
-      style = getComputedStyle(el)
-      marginTop = parseInt(style.marginTop) || 0
-      marginBottom = parseInt(style.marginBottom) || 0
+  get_height(): number {
+    let height = 0
+    for (const key in this.child_views) {
+      const child_view = this.child_views[key]
+      const el = child_view.el
+      const style = getComputedStyle(el)
+      const marginTop = parseInt(style.marginTop!) || 0
+      const marginBottom = parseInt(style.marginBottom!) || 0
       height += el.offsetHeight + marginTop + marginBottom
+    }
     return height + 20
+  }
 
-  get_width: () ->
-    if @model.width?
-      return @model.width
-    else
-      width = @el.scrollWidth + 20
-      for own key, child_view of @child_views
-        # Take the max width of all the children as the constrainer.
-        child_width = child_view.el.scrollWidth
-        if child_width > width
+  get_width(): number {
+    if (this.model.width != null)
+      return this.model.width
+    else {
+      let width = this.el.scrollWidth + 20
+      for (const key in this.child_views) {
+        const child_view = this.child_views[key]
+        // Take the max width of all the children as the constrainer.
+        const child_width = child_view.el.scrollWidth
+        if (child_width > width)
           width = child_width
+      }
       return width
+    }
+  }
+}
 
-export class WidgetBox extends LayoutDOM
-  type: 'WidgetBox'
-  default_view: WidgetBoxView
+WidgetBoxView.prototype.className = "bk-widget-box"
 
-  initialize: (options) ->
-    super(options)
-    if @sizing_mode == 'fixed' and @width == null
-      # Set a default for fixed.
-      @width = 300
+export class WidgetBox extends LayoutDOM {
+
+  children: LayoutDOM[]
+
+  initialize(attrs: any, options: any): void {
+    super.initialize(attrs, options)
+    if (this.sizing_mode == 'fixed' && this.width == null) {
+      this.width = 300 // Set a default for fixed.
       logger.info("WidgetBox mode is fixed, but no width specified. Using default of 300.")
-    if @sizing_mode == 'scale_height'
-      logger.warn("sizing_mode `scale_height` is not experimental for WidgetBox. Please report your results to the bokeh dev team so we can improve.")
+    }
+  }
 
-  get_constrained_variables: () ->
-    vars = extend({}, super(), {
-      on_edge_align_top    : @_top
-      on_edge_align_bottom : @_height_minus_bottom
-      on_edge_align_left   : @_left
-      on_edge_align_right  : @_width_minus_right
+  get_constrained_variables(): {[key: string]: Variable} {
+    const vars = extend({}, super.get_constrained_variables(), {
+      on_edge_align_top    : this._top,
+      on_edge_align_bottom : this._height_minus_bottom,
+      on_edge_align_left   : this._left,
+      on_edge_align_right  : this._width_minus_right,
 
-      box_cell_align_top   : @_top
-      box_cell_align_bottom: @_height_minus_bottom
-      box_cell_align_left  : @_left
-      box_cell_align_right : @_width_minus_right
+      box_cell_align_top   : this._top,
+      box_cell_align_bottom: this._height_minus_bottom,
+      box_cell_align_left  : this._left,
+      box_cell_align_right : this._width_minus_right,
 
-      box_equal_size_top   : @_top
-      box_equal_size_bottom: @_height_minus_bottom
+      box_equal_size_top   : this._top,
+      box_equal_size_bottom: this._height_minus_bottom,
     })
 
-    if @sizing_mode != 'fixed'
-      vars.box_equal_size_left  = @_left
-      vars.box_equal_size_right = @_width_minus_right
+    if (this.sizing_mode != 'fixed') {
+      vars.box_equal_size_left  = this._left
+      vars.box_equal_size_right = this._width_minus_right
+    }
 
     return vars
-
-  get_layoutable_children: () -> @children
-
-  @define {
-    children: [ p.Array, [] ]
   }
+
+  get_layoutable_children(): LayoutDOM[] {
+    return this.children
+  }
+}
+
+WidgetBox.prototype.type = "WidgetBox"
+
+WidgetBox.prototype.default_view = WidgetBoxView
+
+WidgetBox.define({
+  children: [ p.Array, [] ]
+})
