3c8f54385866f21b3f9a8eacb4ae0d18bf4d38f2
Mateusz Paprocki <mattpap@gmail.com>
2017-12-13 11:49:43 +0100

    Rewrite index in TypeScript

--- bokehjs/src/coffee/index.ts
+++ bokehjs/src/coffee/index.ts
@@ -1,10 +1,17 @@
-bokehjs = () ->
-  if not window?.document?
-    throw new Error("bokehjs requires a window with a document. If your
-      runtime environment doesn't provide those, e.g. pure node.js, you
-      can use jsdom library to configure window and document.")
+function is_browser(): boolean {
+  return typeof window !== "undefined" && typeof window.document !== "undefined"
+}
 
-  Bokeh = require './main'
+function bokehjs(): any {
+  if (!is_browser()) {
+    throw new Error(`\
+bokehjs requires a window with a document. If your runtime \
+environment doesn't provide those, e.g. pure node.js, you \
+can use jsdom library to configure window and document.`)
+  }
+
+  const Bokeh = require('./main')
   return Bokeh
+}
 
-module.exports = if window?.document? then bokehjs() else bokehjs
+export = is_browser() ? bokehjs() : bokehjs
