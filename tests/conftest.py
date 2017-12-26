e4d66cba8ad46d8c09a1d4635d5812ab5ea26669
Bryan Van de Ven <bryanv@anaconda.com>
2017-12-26 12:10:54 -0600

    Resolve pytest --log-file conflict (#7346)
    Recent versions of pytest fail with this error:
    ```
    argparse.ArgumentError: argument --log-file: conflicting option string: --log-file
    ```
    
    This PR changes the cusotm option added in tests/conftest.py to be `--examples-log-file`. A grep of the CI tests did not find any usage of the old value (the default value "examples.log" was used).

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ def pytest_addoption(parser):
         "--upload", dest="upload", action="store_true", default=False, help="upload test artefacts to S3"
     )
     parser.addoption(
-        "--log-file", dest="log_file", metavar="path", action="store", default='examples.log', help="where to write the complete log"
+        "--examples-log-file", dest="log_file", metavar="path", action="store", default='examples.log', help="where to write the complete log"
     )
 
 
