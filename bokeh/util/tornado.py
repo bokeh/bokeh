52e6dac8cb2a3d6fabac6487d7b13ac2e7091c09
Bryan Van de Ven <bryanv@anaconda.com>
2018-01-05 17:47:03 -0600

    add traceback information to exception messages callbacks

--- bokeh/util/tornado.py
+++ bokeh/util/tornado.py
@@ -6,6 +6,8 @@ from __future__ import absolute_import, print_function
 import logging
 log = logging.getLogger(__name__)
 
+from traceback import format_exception
+
 from tornado import gen
 
 @gen.coroutine
@@ -80,7 +82,8 @@ class _AsyncPeriodic(object):
             if not self._stopped:
                 self._loop.add_future(invoke(), on_done)
             if future.exception() is not None:
-                log.error("Error thrown from periodic callback: %r", future.exception())
+                log.error("Error thrown from periodic callback:")
+                log.error("".join(format_exception(*future.exc_info())))
         self._loop.add_future(self.sleep(), on_done)
 
     def stop(self):
