20e60c5c87f4e47e52793e52b70b98ce2224186e
Mateusz Paprocki <mattpap@gmail.com>
2018-01-09 18:06:57 +0100

    Clean up models/sources/data_source

--- bokehjs/src/coffee/models/sources/data_source.js
+++ bokehjs/src/coffee/models/sources/data_source.js
@@ -1,43 +1,33 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-
-import {Model} from "../../model";
-import * as hittest from "core/hittest";
-import * as p from "core/properties";
+import {Model} from "../../model"
+import * as hittest from "core/hittest"
+import * as p from "core/properties"
 import {isFunction} from "core/util/types"
-;
 
 export class DataSource extends Model {
+
+  selected: hittest.HitTestResult
+  callback: any // XXX
+
   static initClass() {
-    this.prototype.type = 'DataSource';
-
-
-    selected: hittest.HitTestResult
-    ;
+    this.prototype.type = "DataSource"
 
     this.define({
-        selected: [ p.Any, hittest.create_hit_test_result() ], // TODO (bev)
-        callback: [ p.Any                                   ] // TODO: p.Either(p.Instance(Callback), p.Function) ]
-      });
+      selected: [ p.Any, () => hittest.create_hit_test_result() ], // TODO (bev)
+      callback: [ p.Any                                         ], // TODO: p.Either(p.Instance(Callback), p.Function) ]
+    })
   }
 
-  initialize(options) {
-    super.initialize(options);
-    return this.connect(this.properties.selected.change, () => {
-      const { callback } = this;
+  initialize(attrs: any, options: any): void {
+    super.initialize(options)
+    this.connect(this.properties.selected.change, () => {
+      const {callback} = this
       if (callback != null) {
-        if (isFunction(callback)) {
-          return callback(this);
-        } else {
-          return callback.execute(this);
-        }
+        if (isFunction(callback))
+          callback(this)
+        else
+          callback.execute(this)
       }
-    });
+    })
   }
 }
-DataSource.initClass();
+DataSource.initClass()
