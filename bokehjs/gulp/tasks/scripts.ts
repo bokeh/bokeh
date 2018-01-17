2eef18135cf4f743cab4f4049d22747e63c40754
Mateusz Paprocki <mattpap@gmail.com>
2018-01-17 21:28:06 +0100

    Resolve TS4020 errors

--- bokehjs/gulp/tasks/scripts.ts
+++ bokehjs/gulp/tasks/scripts.ts
@@ -25,7 +25,7 @@ function is_partial(file: string): boolean {
 }
 
 function is_accepted(code: number): boolean {
-  return code < 2000 || [2304, 2307, 2688, 6053, 6133].includes(code)
+  return (code < 2322 && code != 2305) || code > 7034 || [2688, 4020, 6053, 6133].includes(code)
 }
 
 gulp.task("scripts:ts", () => {
