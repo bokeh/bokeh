dda3b509b434bd364fd0027e3c4adee9ff2da59d
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-01-19 00:45:37 +0000

    Made BoxDrawTool more general

--- bokehjs/src/coffee/models/tools/edit/box_draw_tool.ts
+++ bokehjs/src/coffee/models/tools/edit/box_draw_tool.ts
@@ -19,52 +19,70 @@ export class BoxDrawToolView extends EditToolView {
   model: BoxDrawTool
   _basepoint: [number, number] | null
 
+  _tap(e: BkEv): void {
+    const append = e.srcEvent.shiftKey != null ? e.srcEvent.shiftKey : false;
+    this._select_event(e, append, this.model.renderers);
+  }
+
+  _keyup(e: BkEv): void {
+    if ((e.keyCode === 8) && this.model.active) {
+      for (const renderer of this.model.renderers) {
+        this._delete_selected(renderer);
+      }
+    }
+  }
+
   _pan_start(e: BkEv): void {
     this._basepoint = [e.bokeh.sx, e.bokeh.sy];
+    this._pan(e, true, false);
   }
 
   _set_extent([sx0, sx1]: [number, number], [sy0, sy1]: [number, number],
-              emit: boolean = false): void {
-    const r = this.model.renderers[0];
-    const glyph = r.glyph;
-    let [xkey, ykey] = Object.getPrototypeOf(glyph)._coords[0];
-    [xkey, ykey] = [glyph.attributes[xkey].field, glyph.attributes[ykey].field];
-    const ds = r.data_source;
+              append: boolean, emit: boolean = false): void {
+    const renderer = this.model.renderers[0];
     const frame = this.plot_model.frame;
-    const xscale = frame.xscales[r.x_range_name];
-    const yscale = frame.yscales[r.y_range_name];
+    const glyph = renderer.glyph;
+    const ds = renderer.data_source;
+    const xscale = frame.xscales[renderer.x_range_name];
+    const yscale = frame.yscales[renderer.y_range_name];
     const [x0, x1] = xscale.r_invert(sx0, sx1);
     const [y0, y1] = yscale.r_invert(sy0, sy1);
-    const new_data: {[key: string]: any[]} = {}
-    new_data[xkey] = [[x0, x0, x1, x1]];
-    new_data[ykey] = [[y1, y0, y0, y1]];
-    ds.data = new_data;
+    const [x, y] = [(x0+x1)/2., (y0+y1)/2.];
+    const [w, h] = [x1-x0, y1-y0];
+    const [xkey, ykey] = [glyph.x.field, glyph.y.field];
+    const [wkey, hkey] = [glyph.width.field, glyph.height.field];
+    if (append) {
+      ds.data[xkey].push(x)
+      ds.data[ykey].push(y)
+      ds.data[wkey].push(w)
+      ds.data[hkey].push(h)
+    } else {
+      const index = ds.data[xkey].length-1;
+      ds.data[xkey][index] = x;
+      ds.data[ykey][index] = y;
+      ds.data[wkey][index] = w;
+      ds.data[hkey][index] = h;
+    }
+    this._pad_empty_columns(ds, [xkey, ykey, wkey, hkey])
     ds.change.emit(undefined);
     if (emit) {
       ds.properties.data.change.emit(undefined);
     }
   }
 
-  _pan(e: BkEv): void {
+  _pan(e: BkEv, append: boolean = false, emit: boolean = false): void {
     const curpoint = [e.bokeh.sx, e.bokeh.sy];
     const frame = this.plot_model.frame;
     const dims = this.model.dimensions;
     const limits = this.model._get_dim_limits(this._basepoint, curpoint, frame, dims);
     if (limits != null) {
       const [sxlim, sylim] = limits;
-      this._set_extent(sxlim, sylim);
+      this._set_extent(sxlim, sylim, append, emit);
     }
   }
 
   _pan_end(e: BkEv): void {
-    const curpoint = [e.bokeh.sx, e.bokeh.sy];
-    const frame = this.plot_model.frame;
-    const dims = this.model.dimensions;
-    const limits = this.model._get_dim_limits(this._basepoint, curpoint, frame, dims);
-    if (limits != null) {
-      const [sxlim, sylim] = limits;
-      this._set_extent(sxlim, sylim, true);
-    }
+    this._pan(e, false, true)
     this._basepoint = null;
   }
 }
@@ -75,7 +93,7 @@ export class BoxDrawTool extends EditTool {
 
   tool_name = "Box Draw Tool"
   icon = "bk-tool-icon-box-draw"
-  event_type = "pan"
+  event_type = ["tap", "pan"]
   default_order = 30
 }
 
