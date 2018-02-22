65c4f47d7b369e44e16f6ed42ba4176be0b607be
hallyourbase <12599080+hallyourbase@users.noreply.github.com>
2018-02-22 17:03:26 -0500

    More generic sphinx doc sorting for bokeh-plot directive. Addresses #7505

--- bokeh/sphinxext/bokeh_plot.py
+++ bokeh/sphinxext/bokeh_plot.py
@@ -265,7 +265,7 @@ class BokehPlotDirective(Directive):
         return result
 
 def env_before_read_docs(app, env, docnames):
-    docnames.sort(key=lambda x: 0 if "examples" in x else 1)
+    docnames.sort(key=lambda x: 0 if x.startswith(tuple(env.app.config.bokeh_plot_pyfile_include_dirs)) else 1)
     for name in [x for x in docnames if env.doc2path(x).endswith(".py")]:
         if not name.startswith(tuple(env.app.config.bokeh_plot_pyfile_include_dirs)):
             env.found_docs.remove(name)
