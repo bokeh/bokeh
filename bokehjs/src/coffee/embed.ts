7c0b20a7270f721d888590dcfcbb02e8a3b4aeee
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-02-27 18:13:11 +0000

    Improved notebook comm warning

--- bokehjs/src/coffee/embed.ts
+++ bokehjs/src/coffee/embed.ts
@@ -119,7 +119,9 @@ function _init_comms(target: string, doc: Document): void {
       logger.warn(`Jupyter comms failed to register. push_notebook() will not function. (exception reported: ${e})`)
     }
   } else {
-    console.warn('Jupyter notebooks comms not available. push_notebook() will not function');
+    console.warn('Jupyter notebooks comms not available. push_notebook() will not function.'
+                 'If running JupyterLab ensure the latest jupyterlab_bokeh extension is installed.'
+				 'In an exported notebook this warning is expected.');
   }
 }
 
