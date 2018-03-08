7c21efb9ccfbb14f5ffd5d299c9214d38bebd392
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-03-08 18:30:22 +0000

    Fixed ColumnDataSource.stream when using datetimes

--- bokeh/models/sources.py
+++ bokeh/models/sources.py
@@ -423,9 +423,8 @@ class ColumnDataSource(ColumnarDataSource):
             else:
                 raise ValueError("Must stream updates to all existing columns (extra: %s)" % ", ".join(sorted(extra)))
 
+        import numpy as np
         if needs_length_check:
-            import numpy as np
-
             lengths = set()
             arr_types = (np.ndarray, pd.Series) if pd else np.ndarray
             for k, x in new_data.items():
@@ -439,10 +438,19 @@ class ColumnDataSource(ColumnarDataSource):
             if len(lengths) > 1:
                 raise ValueError("All streaming column updates must be the same length")
 
-        # slighgly awkward that we have to call converty_datetime_array here ourselves
+        # slightly awkward that we have to call convert_datetime_array here ourselves
         # but the downstream code expects things to already be ms-since-epoch
-        for key in new_data:
-            new_data[key] = convert_datetime_array(new_data[key])
+        for key, values in new_data.items():
+            if pd and isinstance(values, (pd.Series, pd.Index)):
+                values = values.values
+            old_values = self.data[key]
+            # Apply the transformation if the new data contains datetimes
+            # but the current data has already been transformed
+            if (isinstance(values, np.ndarray) and values.dtype.kind.lower() == 'm' and
+                isinstance(old_values, np.ndarray) and old_values.dtype.kind.lower() != 'm'):
+                new_data[key] = convert_datetime_array(values)
+            else:
+                new_data[key] = values
 
         self.data._stream(self.document, self, new_data, rollover, setter)
 
