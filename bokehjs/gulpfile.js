e8755b265a1f8f600ebe19b2a1e0294d366d3c02
Mateusz Paprocki <mattpap@gmail.com>
2018-03-15 17:07:01 +0100

    Add node and npm version requirement specification (#7575)
    * Add node and npm version requirement specification
    
    * Upgrade npm after installing node on travis ci

--- bokehjs/gulpfile.js
+++ bokehjs/gulpfile.js
@@ -1,5 +1,22 @@
 const crypto = require("crypto")
+const semver = require("semver")
+const cp = require("child_process")
 const fs = require("fs")
+
+const {engines} = require("./package.json")
+
+const node_version = process.version
+const npm_version = cp.execSync("npm --version").toString().trim()
+
+if (!semver.satisfies(node_version, engines.node)) {
+  console.log(`node ${engines.node} is required. Current version is ${node_version}.`)
+  process.exit(1)
+}
+
+if (!semver.satisfies(npm_version, engines.npm)) {
+  console.log(`npm ${engines.npm} is required. Current version is ${npm_version}.`)
+  process.exit(1)
+}
 
 function is_up_to_date(file) {
   const hash_file = "." + file
