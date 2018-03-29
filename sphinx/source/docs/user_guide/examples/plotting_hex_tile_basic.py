0b8554f90476bc3f18236bda9c1bd174236f369b
Bryan Van de Ven <bryanv@anaconda.com>
2018-03-28 20:16:26 -0700

    release docs for 0.12.15 (#7716)
    * release notes
    
    * basic hex user guide info

---
+++ sphinx/source/docs/user_guide/examples/plotting_hex_tile_basic.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
+from bokeh.util.hex import axial_to_cartesian
+
+output_file("hex_coords.py")
+
+q = np.array([0,  0, 0, -1, -1,  1, 1])
+r = np.array([0, -1, 1,  0,  1, -1, 0])
+
+p = figure(plot_width=400, plot_height=400, toolbar_location=None)
+p.grid.visible = False
+
+p.hex_tile(q, r, size=1, fill_color=["firebrick"]*3 + ["navy"]*4,
+           line_color="white", alpha=0.5)
+
+x, y = axial_to_cartesian(q, r, 1, "pointytop")
+
+p.text(x, y, text=["(%d, %d)" % (q,r) for (q, r) in zip(q, r)],
+       text_baseline="middle", text_align="center")
+
+show(p)
