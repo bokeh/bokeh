81b63e4571ce0e6f1c7b9901921c8ad17ecfe2ef
Bryan Van de Ven <bryanv@anaconda.com>
2018-04-25 16:43:55 -0700

    add python default for image indices

--- bokeh/models/selections.py
+++ bokeh/models/selections.py
@@ -20,7 +20,7 @@ class Selection(Model):
     line_indices = Seq(Int, default=[], help="""
     """)
 
-    image_indices = Seq(Dict(keys_type=String, values_type=Int), help="""
+    image_indices = Seq(Dict(keys_type=String, values_type=Int), default=[], help="""
     """)
 
     multiline_indices = Dict(String, Seq(Int), default={}, help="""
