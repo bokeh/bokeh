92cb4b3ce7a3582f41901a823e5de92a42daf7cd
Mateusz Paprocki <mattpap@gmail.com>
2018-05-08 23:17:50 +0200

    Rewrite test/model in TypeScript

--- bokehjs/test/model.ts
+++ bokehjs/test/model.ts
@@ -1,66 +1,81 @@
-{expect} = require "chai"
-sinon = require "sinon"
+import {expect} from "chai"
+import * as sinon from "sinon"
 
-{CustomJS} = require("models/callbacks/customjs")
-{Model} = require("model")
-p = require "core/properties"
+import {CustomJS} from "models/callbacks/customjs"
+import {Model} from "model"
+import * as p from "core/properties"
 
-class SomeModel extends Model
-  type: 'SomeModel'
+class SomeModel extends Model {
 
-  @define {
-    foo: [ p.Number, 2 ]
-    bar: [ p.String    ]
-    baz: [ p.Number, 1 ]
+  foo: number
+  bar: string
+  baz: number
+
+  static initClass(): void {
+    this.prototype.type = 'SomeModel'
+
+    this.define({
+      foo: [ p.Number, 2 ],
+      bar: [ p.String    ],
+      baz: [ p.Number, 1 ],
+    })
   }
+}
+SomeModel.initClass()
 
-describe "Model objects", ->
+describe("Model objects", () => {
 
-  describe "default creation", ->
-    m = new Model()
+  describe("default creation", () => {
+    const m = new Model()
 
-    it "should have null name", ->
+    it("should have null name", () => {
       expect(m.name).to.be.null
+    })
 
-    it "should have empty tags", ->
-      expect(m.tags).to.be.deep.equal []
+    it("should have empty tags", () => {
+      expect(m.tags).to.be.deep.equal([])
+    })
 
-    it "should have empty js_property_callbacks", ->
-      expect(m.js_property_callbacks).to.be.deep.equal {}
+    it("should have empty js_property_callbacks", () => {
+      expect(m.js_property_callbacks).to.be.deep.equal({})
+    })
+  })
 
-  describe "js callbacks", ->
+  describe("js callbacks", () => {
 
-    it "should execute on property changes", ->
+    it("should execute on property changes", () => {
+      // unfortunately spy does not seem to have per-instance
+      // resolution. This is the best test I could make work.
 
-      # unfortunately spy does not seem to have per-instance
-      # resolution. This is the best test I could make work.
+      const cb1 = new CustomJS()
+      const cb2 = new CustomJS()
+      const cb3 = new CustomJS()
 
-      cb1 = new CustomJS()
-      cb2 = new CustomJS()
-      cb3 = new CustomJS()
+      const spy = sinon.spy(cb3, 'execute')
 
-      spy = sinon.spy(cb3, 'execute')
-
-      m = new SomeModel({
+      const m = new SomeModel({
         js_property_callbacks: {
-          'change:foo': [cb1, cb2]
-          'change:bar': [cb3]
-        }
+          'change:foo': [cb1, cb2],
+          'change:bar': [cb3],
+        },
       })
 
-      # check the correct number of calls for m.foo change
+      // check the correct number of calls for m.foo change
       expect(spy.called).to.be.false
       m.foo = 10
-      expect(spy.callCount).to.be.equal 0
+      expect(spy.callCount).to.be.equal(0)
 
-      # check the correct number of calls for m.bar change
+      // check the correct number of calls for m.bar change
       spy.reset()
       expect(spy.called).to.be.false
       m.bar = "test"
-      expect(spy.callCount).to.be.equal 1
+      expect(spy.callCount).to.be.equal(1)
 
-      # check the correct number of calls for m.baz change
+      // check the correct number of calls for m.baz change
       spy.reset()
       expect(spy.called).to.be.false
       m.baz = 10
-      expect(spy.callCount).to.be.equal 0
+      expect(spy.callCount).to.be.equal(0)
+    })
+  })
+})
