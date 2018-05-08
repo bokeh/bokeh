9c54be77a79917f4200b5ced11789640b0852562
Mateusz Paprocki <mattpap@gmail.com>
2018-05-08 23:13:14 +0200

    Rewrite test/unit in TypeScript

--- bokehjs/test/unit.ts
+++ bokehjs/test/unit.ts
@@ -1,10 +1,10 @@
-# if adding or removing tests, remember to update
-#
-#     gulp/tasks/test.ts
+// if adding or removing tests, remember to update
+//
+//     gulp/tasks/test.ts
 
-require "./client"
-require "./core"
-require "./document"
-require "./model"
-require "./models"
-require "./protocol"
+import "./client"
+import "./core"
+import "./document"
+import "./model"
+import "./models"
+import "./protocol"
