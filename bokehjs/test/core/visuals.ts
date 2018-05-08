3d0f7342cd4977b20ef07cc5aa5c0e615b242013
Mateusz Paprocki <mattpap@gmail.com>
2018-05-08 15:02:56 +0200

    Rewrite test/core/visuals in TypeScript

--- bokehjs/test/core/visuals.ts
+++ bokehjs/test/core/visuals.ts
@@ -1,70 +1,70 @@
-{expect} = require "chai"
-utils = require "../utils"
-{create_glyph_renderer_view} = require "../models/glyphs/glyph_utils"
+import {expect} from "chai"
+import * as utils from "../utils"
+import {create_glyph_renderer_view} from "../models/glyphs/glyph_utils"
 
-{Visuals} = utils.require "core/visuals"
-{ColumnDataSource} = utils.require "models/sources/column_data_source"
-{CDSView} = utils.require "models/sources/cds_view"
-{IndexFilter} = utils.require "models/filters/index_filter"
-{Circle} = utils.require "models/glyphs/circle"
+import {Visuals, Fill} from "core/visuals"
+import {ColumnDataSource} from "models/sources/column_data_source"
+import {CDSView} from "models/sources/cds_view"
+import {IndexFilter} from "models/filters/index_filter"
+import {Circle, CircleView} from "models/glyphs/circle"
 
-describe "Visuals", ->
+describe("Visuals", () => {
 
-  it "should set the correct visual values when values are vectorized", ->
-    source = new ColumnDataSource({data: {
-        "fill_alpha": [0, 0.5, 1]
-    }})
-    attrs =
-      fill_alpha: {"field": "fill_alpha"}
+  it("should set the correct visual values when values are vectorized", () => {
+    const source = new ColumnDataSource({data: {fill_alpha: [0, 0.5, 1]}})
+    const attrs = {fill_alpha: {field: "fill_alpha"}}
 
-    circle = new Circle(attrs)
-    visuals = new Visuals(circle)
+    const circle = new Circle(attrs)
+    const visuals = new Visuals(circle) as Visuals & {fill: Fill}
 
     visuals.warm_cache(source)
 
-    ctx = {}
+    const ctx = {} as any
     visuals.fill.set_vectorize(ctx, 1)
     expect(ctx.globalAlpha).to.be.equal(0.5)
+  })
 
-  it "should set the correct visual values when values are vectorized and all_indices is set", ->
-    source = new ColumnDataSource({data: {
-        "fill_alpha": [0, 0.5, 1]
-    }})
-    attrs =
-      fill_alpha: {"field": "fill_alpha"}
+  it("should set the correct visual values when values are vectorized and all_indices is set", () => {
+    const source = new ColumnDataSource({data: {fill_alpha: [0, 0.5, 1]}})
+    const attrs = {fill_alpha: {field: "fill_alpha"}}
 
-    circle = new Circle(attrs)
-    visuals = new Visuals(circle)
+    const circle = new Circle(attrs)
+    const visuals = new Visuals(circle) as Visuals & {fill: Fill}
 
     visuals.warm_cache(source)
     visuals.set_all_indices([1, 2])
 
-    ctx = {}
+    const ctx = {} as any
     visuals.fill.set_vectorize(ctx, 1)
     expect(ctx.globalAlpha).to.be.equal(1)
+  })
 
-  describe "interacting with GlyphViews", ->
+  describe("interacting with GlyphViews", () => {
 
-    afterEach ->
+    afterEach(() => {
       utils.unstub_canvas()
       utils.unstub_solver()
+    })
 
-    beforeEach ->
+    beforeEach(() => {
       utils.stub_canvas()
       utils.stub_solver()
+    })
 
-    it "set_all_indices should be called by the glyph view", ->
-      attrs =
-        fill_alpha: {"field": "fill_alpha"}
+    it("set_all_indices should be called by the glyph view", () => {
+      const attrs = {fill_alpha: {field: "fill_alpha"}}
 
-      circle = new Circle(attrs)
-      renderer_view = create_glyph_renderer_view(circle, {fill_alpha: [0, 0.5, 1]})
+      const circle = new Circle(attrs)
+      const renderer_view = create_glyph_renderer_view(circle, {fill_alpha: [0, 0.5, 1]})
 
-      filter = new IndexFilter({"indices": [1, 2]})
-      renderer_view.model.view = new CDSView({'source': renderer_view.model.data_source, 'filters': [filter]})
-      #need to manually set_data because signals for renderer aren't connected by create_glyph_view util
+      const filter = new IndexFilter({indices: [1, 2]})
+      renderer_view.model.view = new CDSView({source: renderer_view.model.data_source, filters: [filter]})
+      //need to manually set_data because signals for renderer aren't connected by create_glyph_view util
       renderer_view.set_data()
 
-      ctx = {}
-      renderer_view.glyph.visuals.fill.set_vectorize(ctx, 1)
+      const ctx = {} as any
+      (renderer_view.glyph as CircleView).visuals.fill.set_vectorize(ctx, 1)
       expect(ctx.globalAlpha).to.be.equal(1)
+    })
+  })
+})
