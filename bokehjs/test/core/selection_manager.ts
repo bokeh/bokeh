c12051225aed2d245f60bc464840074df1450078
Mateusz Paprocki <mattpap@gmail.com>
2018-05-08 15:06:39 +0200

    Rewrite test/core/selection_manager in TypeScript

--- bokehjs/test/core/selection_manager.ts
+++ bokehjs/test/core/selection_manager.ts
@@ -1,75 +1,82 @@
-{expect} = require "chai"
-sinon = require "sinon"
-utils = require "../utils"
+import {expect} from "chai"
+import * as sinon from "sinon"
+import * as utils from "../utils"
 
-{Selection} = utils.require("models/selections/selection")
-{ColumnDataSource} = utils.require("models/sources/column_data_source")
-{GlyphRenderer} = utils.require("models/renderers/glyph_renderer")
-{Rect, RectView} = utils.require("models/glyphs/rect")
-{CDSView} = utils.require("models/sources/cds_view")
-{IndexFilter} = utils.require("models/filters/index_filter")
+import {Rect} from "models/glyphs/rect"
+import {CDSView} from "models/sources/cds_view"
+import {IndexFilter} from "models/filters/index_filter"
 
-{create_hit_test_result_from_hits, create_empty_hit_test_result} = utils.require("core/hittest")
-{create_glyph_renderer_view} = require("../models/glyphs/glyph_utils")
+import {create_hit_test_result_from_hits, create_empty_hit_test_result} from "core/hittest"
+import {create_glyph_renderer_view} from "../models/glyphs/glyph_utils"
 
-describe "SelectionManager", ->
+describe("SelectionManager", () => {
 
-  afterEach ->
+  afterEach(() => {
     utils.unstub_canvas()
+  })
 
-  beforeEach ->
+  beforeEach(function() {
     utils.stub_canvas()
-    @glyph = new Rect()
-    @renderer_view = create_glyph_renderer_view(@glyph, {'x': [1, 2, 3]})
-    @glyph_stub = sinon.stub(@renderer_view.glyph, "hit_test")
+    this.glyph = new Rect()
+    this.renderer_view = create_glyph_renderer_view(this.glyph, {x: [1, 2, 3]})
+    this.glyph_stub = sinon.stub(this.renderer_view.glyph, "hit_test")
+  })
 
-  describe "select", ->
+  describe("select", () => {
 
-    it "should return true and set source selected if hit_test_result is not empty", ->
-      @glyph_stub.returns(create_hit_test_result_from_hits([[0, 1]]))
-      source = @renderer_view.model.data_source
+    it("should return true and set source selected if hit_test_result is not empty", function() {
+      this.glyph_stub.returns(create_hit_test_result_from_hits([[0, 1]]))
+      const source = this.renderer_view.model.data_source
 
-      did_hit = source.selection_manager.select([@renderer_view], "geometry", true, false)
+      const did_hit = source.selection_manager.select([this.renderer_view], "geometry", true, false)
       expect(did_hit).to.be.true
       expect(source.selected.indices).to.be.deep.equal([0])
+    })
 
-    it "should set source selected correctly with a cds_view", ->
-      # hit-testing is done in subset space, whereas selected should be set in full data space
-      @glyph_stub.returns(create_hit_test_result_from_hits([[0, 1]]))
-      source = @renderer_view.model.data_source
-      filter = new IndexFilter({'indices': [1]})
-      @renderer_view.model.view = new CDSView({'filters': [filter]})
+    it("should set source selected correctly with a cds_view", function() {
+      // hit-testing is done in subset space, whereas selected should be set in full data space
+      this.glyph_stub.returns(create_hit_test_result_from_hits([[0, 1]]))
+      const source = this.renderer_view.model.data_source
+      const filter = new IndexFilter({indices: [1]})
+      this.renderer_view.model.view = new CDSView({filters: [filter]})
 
-      did_hit = source.selection_manager.select([@renderer_view], "geometry", true, false)
+      const did_hit = source.selection_manager.select([this.renderer_view], "geometry", true, false)
       expect(did_hit).to.be.true
       expect(source.selected.indices).to.be.deep.equal([1])
+    })
 
-    it "should return false and clear selections if hit_test_result is empty", ->
-      @glyph_stub.returns(create_empty_hit_test_result())
-      source = @renderer_view.model.data_source
+    it("should return false and clear selections if hit_test_result is empty", function() {
+      this.glyph_stub.returns(create_empty_hit_test_result())
+      const source = this.renderer_view.model.data_source
       source.selected.indices = [0, 1]
       expect(source.selected.is_empty()).to.be.false
 
-      did_hit = source.selection_manager.select([@renderer_view], "geometry", true, false)
+      const did_hit = source.selection_manager.select([this.renderer_view], "geometry", true, false)
       expect(did_hit).to.be.false
       expect(source.selected.is_empty()).to.be.true
+    })
+  })
 
-  describe "inspect", ->
+  describe("inspect", function() {
 
-    it "should return true and set source inspected if hit_test result is not empty", ->
-      @glyph_stub.returns(create_hit_test_result_from_hits([[1, 2]]))
-      source = @renderer_view.model.data_source
+    it("should return true and set source inspected if hit_test result is not empty", function() {
+      this.glyph_stub.returns(create_hit_test_result_from_hits([[1, 2]]))
+      const source = this.renderer_view.model.data_source
 
-      did_hit = source.selection_manager.inspect(@renderer_view, "geometry")
+      const did_hit = source.selection_manager.inspect(this.renderer_view, "geometry")
       expect(did_hit).to.be.true
       expect(source.inspected.indices).to.be.deep.equal([1])
+    })
 
-    it "should return false and clear inspections if hit_test_result is empty", ->
-      @glyph_stub.returns(create_empty_hit_test_result())
-      source = @renderer_view.model.data_source
+    it("should return false and clear inspections if hit_test_result is empty", function() {
+      this.glyph_stub.returns(create_empty_hit_test_result())
+      const source = this.renderer_view.model.data_source
       source.inspected.indices = [0, 1]
       expect(source.inspected.is_empty()).to.be.false
 
-      did_hit = source.selection_manager.inspect(@renderer_view, "geometry")
+      const did_hit = source.selection_manager.inspect(this.renderer_view, "geometry")
       expect(did_hit).to.be.false
       expect(source.inspected.is_empty()).to.be.true
+    })
+  })
+})
