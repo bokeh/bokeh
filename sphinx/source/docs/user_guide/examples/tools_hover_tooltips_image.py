b273bdc671c3ce88a73284f7fe45c819ee1fa2fe
jlstevens <jlstevens@ed.ac.uk>
2018-05-09 21:14:40 +0100

    Improved the tools_hover_tooltips_image.py example

--- sphinx/source/docs/user_guide/examples/tools_hover_tooltips_image.py
+++ sphinx/source/docs/user_guide/examples/tools_hover_tooltips_image.py
@@ -4,14 +4,13 @@ from bokeh.models import HoverTool
 
 output_file("tools_hover_tooltip_image.html")
 
-img1 = np.array([np.linspace(0, 10, 200)]*20)
-img2 = np.array([np.linspace(0, 10, 10)]*20)
-img3 = np.random.rand(25, 10) > 0.5
+ramp = np.array([np.linspace(0, 10, 200)]*20)
+steps = np.array([np.linspace(0, 10, 10)]*20)
+bitmask = np.random.rand(25, 10) > 0.5
 
-
-data = dict(image=[img1, img2, img3],
-            pattern=['smooth ramp', 'steps', 'bit mask'],
-            tenfold=[img1*10, img2*10, img3*10],
+data = dict(image=[ramp, steps, bitmask],
+            squared=[ramp**2, steps**2, bitmask**2],
+            pattern=['smooth ramp', 'steps', 'bitmask'],
             x=[0, 0, 25],
             y=[5, 20, 5],
             dw=[20,  20, 10],
@@ -20,11 +19,11 @@ data = dict(image=[img1, img2, img3],
 cds = ColumnDataSource(data=data)
 hover = HoverTool(tooltips=[
     ('index', "$index"),
+    ('pattern', '@pattern'),
     ("x", "$x"),
     ("y", "$y"),
-    ('pattern', '@pattern'),
-    ('10x', '@tenfold'),
-    ("value", "@image")], )
+    ("value", "@image"),
+    ('squared', '@squared')], )
 
 p = figure( x_range=(0, 35), y_range=(0, 35), tools=[hover, 'wheel_zoom'])
 p.image(source=cds, image='image', x='x', y='y', dw='dw', dh='dh', palette="Inferno256")
