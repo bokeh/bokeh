46928a23dbecc750f9a36f90ad512b9d5590d367
jlstevens <jlstevens@ed.ac.uk>
2018-05-10 18:27:53 +0100

    Added hover to the image.py example in the gallery

--- examples/plotting/file/image.py
+++ examples/plotting/file/image.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool
 
 N = 500
 x = np.linspace(0, 10, N)
@@ -8,7 +9,8 @@ y = np.linspace(0, 10, N)
 xx, yy = np.meshgrid(x, y)
 d = np.sin(xx)*np.cos(yy)
 
-p = figure(x_range=(0, 10), y_range=(0, 10))
+p = figure(x_range=(0, 10), y_range=(0, 10),
+           tools=[HoverTool(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])])
 
 # must give a vector of image data for image parameter
 p.image(image=[d], x=0, y=0, dw=10, dh=10, palette="Spectral11")
