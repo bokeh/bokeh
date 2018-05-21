a92f117562d0b9180307c6ad362771c7ad8faa73
Mateusz Paprocki <mattpap@gmail.com>
2018-05-21 11:33:10 +0200

    Drop gulp-less

--- bokehjs/gulp/tasks/styles.ts
+++ bokehjs/gulp/tasks/styles.ts
@@ -1,14 +1,15 @@
 import * as gulp from "gulp"
-import * as less from "gulp-less"
+import * as less from "less"
 import * as uglifycss from "uglifycss"
 
-import {write, rename} from "../utils"
+import {read, write, rename} from "../utils"
 import * as paths from "../paths"
 
-gulp.task("styles:build", () => {
-  return gulp.src(paths.less.sources)
-    .pipe(less())
-    .pipe(gulp.dest(paths.build_dir.css))
+gulp.task("styles:build", async () => {
+  for (const src of paths.less.sources) {
+    const {css} = await less.render(read(src)!, {filename: src})
+    write(rename(src, {dir: paths.build_dir.css, ext: ".css"}), css)
+  }
 })
 
 gulp.task("styles:minify", ["styles:build"], (next: () => void) => {
--- bokehjs/gulp/utils.ts
+++ bokehjs/gulp/utils.ts
@@ -6,8 +6,10 @@ export const scan = ts.sys.readDirectory
 export const read = ts.sys.readFile
 export const write = ts.sys.writeFile
 
-export function rename(p: string, options: {ext?: string}): string {
+export function rename(p: string, options: {dir?: string, ext?: string}): string {
   let {dir, name, ext} = path.parse(p)
+  if (options.dir != null)
+    dir = options.dir
   if (options.ext != null)
     ext = options.ext
   return path.format({dir, name, ext})
