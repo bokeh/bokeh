965989c1335d64ea9742560c15feabfe72f6c1d9
Karel van de Plassche <karelvandeplassche@gmail.com>
2018-05-25 22:35:00 +0200

    Fixes #3596: Warn on generating static HTML with Python callbacks (#7893)
    * Fixes #3596: Warn on generating static HTML with Python callbacks
    
    * Moved callback check to `standalone_docs_json_and_render_items`
    
    * WIP: Test for python callback check warnings
    
    * Added tests for python event/property callback warnings
    
    * Removed extra import from standalone.py
    
    * Bugfix! Changed name of SomeModel avoiding global name collision
    
    * Bugfix! Use event.Tap instead of 'tap' for python2.7 compat

--- bokeh/embed/tests/test_util.py
+++ bokeh/embed/tests/test_util.py
@@ -18,11 +18,16 @@ import pytest ; pytest
 #-----------------------------------------------------------------------------
 
 # Standard library imports
+import logging
 
 # External imports
 
 # Bokeh imports
 from bokeh.model import Model
+from bokeh.core.properties import Int, String, List
+from bokeh.document.document import Document
+from bokeh.util.logconfig import basicConfig
+from bokeh.events import Tap
 
 # Module under test
 import bokeh.embed.util as beu
@@ -30,6 +35,52 @@ import bokeh.embed.util as beu
 #-----------------------------------------------------------------------------
 # Setup
 #-----------------------------------------------------------------------------
+# needed for caplog tests to function
+basicConfig()
+
+# Taken from test_callback_manager.py
+class _GoodPropertyCallback(object):
+
+    def __init__(self):
+        self.last_name = None
+        self.last_old = None
+        self.last_new = None
+
+    def __call__(self, name, old, new):
+        self.method(name, old, new)
+
+    def method(self, name, old, new):
+        self.last_name = name
+        self.last_old = old
+        self.last_new = new
+
+    def partially_good(self, name, old, new, newer):
+        pass
+
+    def just_fine(self, name, old, new, extra='default'):
+        pass
+
+class _GoodEventCallback(object):
+
+    def __init__(self):
+        self.last_name = None
+        self.last_old = None
+        self.last_new = None
+
+    def __call__(self, event):
+        self.method(event)
+
+    def method(self, event):
+        self.event = event
+
+    def partially_good(self, arg, event):
+        pass
+
+# Taken from test_model
+class EmbedTestUtilModel(Model):
+    a = Int(12)
+    b = String("hello")
+    c = List(Int, [1, 2, 3])
 
 #-----------------------------------------------------------------------------
 # General API
@@ -81,7 +132,34 @@ class Test_script_for_render_items(object):
     pass
 
 class Test_standalone_docs_json_and_render_items(object):
-    pass
+
+    def test_log_warning_if_python_property_callback(self, caplog):
+        d = Document()
+        m1 = EmbedTestUtilModel()
+        c1 = _GoodPropertyCallback()
+        d.add_root(m1)
+
+        m1.on_change('name', c1)
+        assert len(m1._callbacks) != 0
+
+        with caplog.at_level(logging.WARN):
+            beu.standalone_docs_json_and_render_items(m1)
+            assert len(caplog.records) == 1
+            assert caplog.text != ''
+
+    def test_log_warning_if_python_event_callback(self, caplog):
+        d = Document()
+        m1 = EmbedTestUtilModel()
+        c1 = _GoodEventCallback()
+        d.add_root(m1)
+
+        m1.on_event(Tap, c1)
+        assert len(m1._event_callbacks) != 0
+
+        with caplog.at_level(logging.WARN):
+            beu.standalone_docs_json_and_render_items(m1)
+            assert len(caplog.records) == 1
+            assert caplog.text != ''
 
 class Test_wrap_in_onload(object):
 
