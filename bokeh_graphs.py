9d608f94b478656d10df9bf73642b9702b3287ae
Mubarak Elrayah <mubarakelrayah@icloud.com>
2018-06-07 00:48:26 +0300

    Remove Concatenate part

--- bokeh_graphs.py
+++ bokeh_graphs.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from bokeh.plotting import Figure, output_file, show
 import datetime
 from bokeh.models import HoverTool, DatetimeTickFormatter, ColumnarDataSource, Toggle, BoxAnnotation, CustomJS
@@ -8,58 +6,34 @@ from bokeh.models.widgets import Tabs, Panel
 import pandas
 import os, glob, io, re
 
-
-### Concatenating multi data in one csv file.
-
-def concatenate(indir="/Users/mubarak/Desktop/Demo/flow", outfile="/Users/mubarak/Desktop/Demo/Data.csv"):
-	os.chdir(indir)
-	filelist = glob.glob("*.csv")
-	dflist = []
-	colnames = ["Id","TypeId","UpBytes","DownBytes","StatTime", "DeviceName"] 
-	for filename in filelist:
-		df = pandas.read_csv(filename)
-		# df = df.drop(df.index[[0]])
-		dflist.append(df)
-	concatDf = pandas.concat(dflist, axis=0)              
-	concatDf.columns = colnames
-	concatDf.to_csv(outfile, index=0)	
-
-	print "--- Data concatenated ---"
-
 ## Read the concatenated data from above function and drow bokeh graphs.
 
 def run_graph():
-	
 	df = pandas.read_csv("Data.csv", parse_dates=["StatTime"])
 	df = df.sort_values(by='StatTime')
 	df2 = df[~(df['UpBytes'] <= 1)]
 	df3 = df2[~(df2['DownBytes'] <= 1)]
-	
 	result = df.groupby(['DeviceName','StatTime','TypeId'], as_index = False)['UpBytes', 'DownBytes'].sum()
-
-	p = Figure(width=500, height=250,logo =None, 
-		sizing_mode='scale_width', 
+	
+	p = Figure(
+		width=500, height=250,
+		logo =None,sizing_mode='scale_width',
 		tools="pan, box_zoom, wheel_zoom, save, reset, box_select",
-		x_axis_type="datetime",
-		title="Device Traffic Utilization Graph:", 
-		x_axis_label="Time Frame", 
-		y_axis_label="Traffic Utilization (GB)",
-		toolbar_location="below",
-		toolbar_sticky=False)
+		x_axis_type="datetime",title="Device Traffic Utilization Graph:",
+		x_axis_label="TimeFrame",y_axis_label="Traffic Utilization (GB)",
+		toolbar_location="below",toolbar_sticky=False)
 
 	p.xaxis.formatter = DatetimeTickFormatter(seconds=["%H:%M"],
                                             minutes=["%H:%M"],
                                             minsec=["%H:%M"],     # format the time from timestamp to readable time.
                                             hours=["%H:%M"])
-
-
+	
 	timeFrame = result["StatTime"]
 	upbyte = result["UpBytes"]
 	downbyte = result["DownBytes"]
 	devicename = result["DeviceName"].astype(str)
 	typeid = result["TypeId"]
 	
-
 	timeconv  = pandas.to_datetime(timeFrame, unit='s')
 	convnumb = upbyte / 1024 ** 3
 	
@@ -95,9 +69,6 @@ def run_graph():
 	juniper_time = pandas.to_datetime(juniperconv, unit='s')
 
 
-
-	
-	
 	####<<<<<<<< Bokeh - plotting - Section >>>>>>>#####
 
 	p.multi_line(juniper_time, juniper_up, color='#2828B0', line_width=1, legend='UpBytes')
@@ -118,9 +89,8 @@ def run_graph():
 	p.add_tools(hover)
 	
 
-	output_file("/Users/mubarak/Desktop/Demo/plotting.html", title="Device_Reports")
+	output_file("plotting.html", title="Device_Reports")
 	print "Fetching data compeleted!!"
  	show(p)
 
-concatenate()
 run_graph()
