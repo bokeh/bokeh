b1aa0056671d7e5d777619a05b82c96faab32c2c
Euan de Kock <euan@dekock.net>
2018-06-28 18:22:08 +0800

    Added x and y values to the geometries property of cb_data when taptool calls a callback

--- bokehjs/src/lib/models/tools/gestures/tap_tool.ts
+++ bokehjs/src/lib/models/tools/gestures/tap_tool.ts
@@ -2,7 +2,7 @@ import {SelectTool, SelectToolView} from "./select_tool"
 import * as p from "core/properties"
 import {TapEvent} from "core/ui_events"
 import {isFunction} from "core/util/types"
-import {Geometry, PointGeometry} from "core/geometry"
+import {PointGeometry} from "core/geometry"
 import {DataSource} from "../../sources/data_source"
 
 export class TapToolView extends SelectToolView {
@@ -22,14 +22,6 @@ export class TapToolView extends SelectToolView {
   _select(geometry: PointGeometry, final: boolean, append: boolean): void {
     const callback = this.model.callback
 
-    const cb_data: {
-      geometries: Geometry,
-      source: DataSource | null,
-    } = {
-      geometries: geometry,
-      source: null,
-    }
-
     if (this.model.behavior == "select") {
       const renderers_by_source = this._computed_renderers_by_data_source()
 
@@ -40,7 +32,16 @@ export class TapToolView extends SelectToolView {
         const did_hit = sm.select(r_views, geometry, final, append)
 
         if (did_hit && callback != null) {
-          cb_data.source = sm.source
+          const frame = this.plot_model.frame
+          const xscale = frame.xscales[renderers[0].x_range_name]
+          const yscale = frame.yscales[renderers[0].y_range_name]
+          const x = xscale.invert(geometry.sx)
+          const y = yscale.invert(geometry.sy)
+          const g = {...geometry, x, y}
+          const cb_data: {
+              geometries: { type: "point", sx: number, sy: number, x: number, y: number }
+              source: DataSource | null,
+          } = { geometries: g, source: sm.source }
           if (isFunction(callback))
             callback(this, cb_data)
           else
@@ -56,7 +57,16 @@ export class TapToolView extends SelectToolView {
         const did_hit = sm.inspect(this.plot_view.renderer_views[r.id], geometry)
 
         if (did_hit && callback != null) {
-          cb_data.source = sm.source
+          const frame = this.plot_model.frame
+          const xscale = frame.xscales[r.x_range_name]
+          const yscale = frame.yscales[r.y_range_name]
+          const x = xscale.invert(geometry.sx)
+          const y = yscale.invert(geometry.sy)
+          const g = {...geometry, x, y}
+          const cb_data: {
+              geometries: { type: "point", sx: number, sy: number, x: number, y: number }
+              source: DataSource | null,
+          } = { geometries: g, source: sm.source }
           if (isFunction(callback))
             callback(this, cb_data)
           else
