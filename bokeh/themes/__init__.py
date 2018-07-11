d15bbbdf90bce5d8cb0d49c4ca3e1ec551358584
Andrew Huang <andrew.huang.ctr@nrlmry.navy.mil>
2018-07-11 17:02:19 +0000

    Reduce sizing and add _ to privates

--- bokeh/themes/__init__.py
+++ bokeh/themes/__init__.py
@@ -7,16 +7,16 @@ from os.path import dirname, realpath, join
 
 from .theme import Theme
 
-THIS_DIR = dirname(realpath(__file__))
-FP_FMT = join(THIS_DIR, '{0}.json')
+_THIS_DIR = dirname(realpath(__file__))
+_FP_FMT = join(_THIS_DIR, '{0}.json')
 
 LIGHT_MINIMAL = 'light_minimal'
 DARK_MINIMAL = 'dark_minimal'
 
 default = Theme(json={})
 built_in_themes = {
-    LIGHT_MINIMAL: Theme(filename=FP_FMT.format(LIGHT_MINIMAL)),
-    DARK_MINIMAL: Theme(filename=FP_FMT.format(DARK_MINIMAL))
+    LIGHT_MINIMAL: Theme(filename=_FP_FMT.format(LIGHT_MINIMAL)),
+    DARK_MINIMAL: Theme(filename=_FP_FMT.format(DARK_MINIMAL))
 }
 
 del dirname, realpath, join
