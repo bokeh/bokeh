3c61044ab89a5b4454a1bdad70587e76855c4e58
Bryan Van de Ven <bryanv@anaconda.com>
2018-07-18 09:50:12 -0700

    rename quality mark to codebase, clean up codebase tests

---
+++ tests/codebase/test_no_tornado_common.py
@@ -0,0 +1,52 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest ; pytest
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+from subprocess import PIPE, Popen
+
+# External imports
+
+# Bokeh imports
+
+#-----------------------------------------------------------------------------
+# Tests
+#-----------------------------------------------------------------------------
+
+BASIC_IMPORTS = [
+    "import bokeh.client",
+    "import bokeh.embed",
+    "import bokeh.io",
+    "import bokeh.models",
+    "import bokeh.plotting",
+]
+
+@pytest.mark.codebase
+def test_no_tornado_common():
+    ''' Basic usage of Bokeh should not result in any Tornado code being
+    imported. This test ensures that importing basic modules does not bring in
+    Tornado.
+
+    '''
+    proc = Popen([
+        "python", "-c", "import sys; %s; sys.exit(1 if any('tornado' in x for x in sys.modules.keys()) else 0)" % ";".join(BASIC_IMPORTS)
+    ],stdout=PIPE)
+    proc.communicate()
+    proc.wait()
+    if proc.returncode != 0:
+        assert False
---
+++ tests/codebase/test_python_execution_with_OO.py
@@ -0,0 +1,71 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest ; pytest
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+import os
+from subprocess import PIPE, Popen
+
+# External imports
+
+# Bokeh imports
+from . import TOP_PATH
+
+#-----------------------------------------------------------------------------
+# Tests
+#-----------------------------------------------------------------------------
+
+@pytest.mark.codebase
+def test_python_execution_with_OO():
+    ''' Running python with -OO will discard docstrings (__doc__ is None)
+    which can cause problems if docstrings are naively formatted.
+
+    This test ensures that the all modules are importable, even with -OO set.
+
+    If you encounter a new problem with docstrings being formatted, try
+    using format_docstring.
+    '''
+    os.chdir(TOP_PATH)
+
+    imports = []
+    for path, _, files in os.walk("bokeh"):
+
+        if "tests" in path: continue
+
+        for file in files:
+            if not file.endswith(".py"):
+                continue
+            if file.endswith("__main__.py"):
+                continue
+
+            if file.endswith("__init__.py"):
+                mod = path.replace("/", ".")
+            else:
+                mod = path.replace("/", ".") + "." + file[:-3]
+
+            imports.append("import " + mod)
+
+    test_env = os.environ.copy()
+    test_env['BOKEH_DOCS_MISSING_API_KEY_OK'] = 'yes'
+
+    proc = Popen(["python", "-OO", "-c", ";".join(imports), ''], stdout=PIPE, env=test_env)
+    proc.communicate()
+    proc.wait()
+
+    if proc.returncode != 0:
+        assert False
