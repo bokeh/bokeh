9edc210cf0c9a628d265611b96bf63d3655fb829
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-08-07 14:59:16 +0100

    Added server selenium tests for PointDrawTool

--- tests/integration/tools/test_point_draw_tool.py
+++ tests/integration/tools/test_point_draw_tool.py
@@ -23,7 +23,8 @@ import time
 # External imports
 
 # Bokeh imports
-from bokeh.models import ColumnDataSource, CustomAction, CustomJS, Plot, Range1d, Circle, PointDrawTool
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, CustomAction, CustomJS, Plot, Range1d, Circle, PointDrawTool, Button, Div
 from bokeh._testing.util.selenium import RECORD
 
 #-----------------------------------------------------------------------------
@@ -40,11 +41,29 @@ def _make_plot(num_objects=0, add=True, drag=True):
     renderer = plot.add_glyph(source, Circle(x='x', y='y'))
     tool = PointDrawTool(num_objects=num_objects, add=add, drag=drag, renderers=[renderer])
     plot.add_tools(tool)
-    plot.toolbar.active_multi = tool 
+    plot.toolbar.active_multi = tool
     code = RECORD("x", "source.data.x") + RECORD("y", "source.data.y")
     plot.add_tools(CustomAction(callback=CustomJS(args=dict(source=source), code=code)))
     plot.toolbar_sticky = False
     return plot
+
+def _make_server_plot(expected):
+    def modify_doc(doc):
+        source = ColumnDataSource(dict(x=[1, 2], y=[1, 1]))
+        plot = Plot(plot_height=400, plot_width=400, x_range=Range1d(0, 3), y_range=Range1d(0, 3), min_border=0)
+        renderer = plot.add_glyph(source, Circle(x='x', y='y'))
+        tool = PointDrawTool(renderers=[renderer])
+        plot.add_tools(tool)
+        plot.toolbar.active_multi = tool
+        div = Div(text='False')
+        def cb(attr, old, new):
+            if new == expected:
+                div.text = 'True'
+        source.on_change('data', cb)
+        code = RECORD("matches", "div.text")
+        plot.add_tools(CustomAction(callback=CustomJS(args=dict(div=div), code=code)))
+        doc.add_root(column(plot, div))
+    return modify_doc
 
 
 @pytest.mark.integration
@@ -99,7 +118,7 @@ class Test_PointDrawTool(object):
                                 "y": [1, 1, 1.5]}
 
         assert page.has_no_console_errors()
-        
+
     def test_click_does_not_trigger_draw(self, single_plot_page):
         plot = _make_plot(add=False)
 
@@ -157,3 +176,45 @@ class Test_PointDrawTool(object):
                                 "y": [1, 1.5]}
 
         assert page.has_no_console_errors()
+
+    def test_point_draw_syncs_to_server(self, bokeh_server_page):
+
+        expected = {"x": [1, 2, 1.6216216216216217],
+                    "y": [1, 1, 1.5]}
+
+        page = bokeh_server_page(_make_server_plot(expected))
+
+        page.click_canvas_at_position(200, 200)
+        time.sleep(0.4) # hammerJS click timeout
+
+        page.click_custom_action()
+        assert page.results == {"matches": "True"}
+
+    def test_point_drag_syncs_to_server(self, bokeh_server_page):
+        expected = {"x": [1, 2, 2.1891891891891895],
+                    "y": [1, 1, 1.1024999999999998]}
+
+        page = bokeh_server_page(_make_server_plot(expected))
+
+        page.click_canvas_at_position(200, 200)
+        time.sleep(0.4) # hammerJS click timeout
+        page.drag_canvas_at_position(200, 200, 70, 53)
+
+        page.click_custom_action()
+        assert page.results == {"matches": "True"}
+    
+    def test_point_delete_syncs_to_server(self, bokeh_server_page):
+        expected = {"x": [1, 2],
+                    "y": [1, 1]}
+
+        page = bokeh_server_page(_make_server_plot(expected))
+
+        page.click_canvas_at_position(200, 200)
+        time.sleep(0.4) # hammerJS click timeout
+        page.click_canvas_at_position(200, 200)
+        time.sleep(0.4) # hammerJS click timeout
+        page.send_keys(u'\ue003')
+        time.sleep(0.4) # hammerJS click timeout
+        
+        page.click_custom_action()
+        assert page.results == {"matches": "True"}
