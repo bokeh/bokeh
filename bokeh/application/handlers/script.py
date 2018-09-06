3a2bca24d8df06182b82c163f1084bb0cb672cae
lgasnier <laurent.gasnier@eaurmc.fr>
2018-09-06 20:42:24 +0200

    add comment to ScriptHandler

--- bokeh/application/handlers/script.py
+++ bokeh/application/handlers/script.py
@@ -82,6 +82,12 @@ class ScriptHandler(CodeHandler):
             raise ValueError('Must pass a filename to ScriptHandler')
         filename = kwargs['filename']
 
+        # For Python 3, encoding must be set to utf-8 because:
+        # - when specifying an encoding in `io.open`, it doesn't
+        #   work with Python 2,
+        # - default encoding used by Python 3 `open` statement
+        #   is not 'utf-8' on Windows platform,
+        # - Python 3 `open` ignores le `coding` comment line.
         if six.PY3:
             with open(filename, 'r', encoding='utf-8') as f:
                 kwargs['source'] = f.read()
