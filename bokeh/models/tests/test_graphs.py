e46ef320ff33be0b64be9d8fbd2eea2ad86aa24c
komo-fr <31801148+komo-fr@users.noreply.github.com>
2018-09-16 09:21:10 +0900

    Convert NetworkX node/edge attributes to Bokeh node_renderer/edge_renderer data source. (#8183) (#8210)
    * Convert NetworkX node/edge attributes to Bokeh node_renderer/edge_renderer data source. (#8183)
    
    * Unify single quotation and double quotation
    
    * Make conditional expression be all on one line and change the variable name to fit within 140.
    
    * Remove unnecessary list()
    
    * Ignore node attribute 'index' and edge attribute 'start'/'end' with warning in converting from NetworkX
    
    * Remove trailing whitespace
    
    * Convert node/edge attributes to dict in common code
    
    * Remove unnecessary list()
    
    * Revert "Remove unnecessary list()"
    
    This reverts commit 4b428bb488f3ef6a5e842764037ed59e8a55f52c.
    
    * Revert "Convert node/edge attributes to dict in common code"
    
    This reverts commit 423dd36f5f691dc9f7b4195f00e78a80a6d8b9bb.
    
    * Split the one-lined list comprehension into multiple lines for readability
    
    * Remove unnecessary ‘data=True’
    
    * Remove unnecessary list()

--- bokeh/models/tests/test_graphs.py
+++ bokeh/models/tests/test_graphs.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 from numpy.testing import assert_allclose
+import pytest
 
 from bokeh.models.graphs import StaticLayoutProvider, from_networkx
 
@@ -33,3 +34,53 @@ def test_from_networkx_method_with_kwargs():
     gl = renderer.layout_provider.graph_layout
     assert set(gl.keys()) == set([0,1,2,3])
     assert_allclose(gl[0], np.array([2., 0.]), atol=1e-7)
+
+def test_from_networkx_with_attributes():
+    G = nx.Graph()
+    G.add_nodes_from([(0, {"attr_1": "a", "attr_2": 10}),
+                      (1, {"attr_1": "b"}),
+                      (2, {"attr_1": "c", "attr_2": 30})])
+    G.add_edges_from([(0, 1, {"attr_1": "A"}),
+                      (0, 2, {"attr_1": "B", "attr_2": 10})])
+
+    renderer = from_networkx(G, nx.circular_layout)
+
+    assert renderer.node_renderer.data_source.data["index"] == [0, 1, 2]
+    assert renderer.node_renderer.data_source.data["attr_1"] == ["a", "b", "c"]
+    assert renderer.node_renderer.data_source.data["attr_2"] == [10, None, 30]
+
+    assert renderer.edge_renderer.data_source.data["start"] == [0, 0]
+    assert renderer.edge_renderer.data_source.data["end"] == [1, 2]
+    assert renderer.edge_renderer.data_source.data["attr_1"] == ["A", "B"]
+    assert renderer.edge_renderer.data_source.data["attr_2"] == [None, 10]
+
+def test_from_networkx_with_bad_attributes():
+    G = nx.Graph()
+    G.add_nodes_from([(0, {"index": "a", "attr_1": 10}),
+                      (1, {"index": "b", "attr_1": 20})])
+    G.add_edges_from([[0, 1]])
+
+    with pytest.warns(UserWarning):
+        renderer = from_networkx(G, nx.circular_layout)
+        assert renderer.node_renderer.data_source.data["index"] == [0, 1]
+        assert renderer.node_renderer.data_source.data["attr_1"] == [10, 20]
+
+    G = nx.Graph()
+    G.add_nodes_from([0, 1])
+    G.add_edges_from([(0, 1, {"start": "A", "attr_1": 10})])
+
+    with pytest.warns(UserWarning):
+        renderer = from_networkx(G, nx.circular_layout)
+        assert renderer.edge_renderer.data_source.data["start"] == [0]
+        assert renderer.edge_renderer.data_source.data["end"] == [1]
+        assert renderer.edge_renderer.data_source.data["attr_1"] == [10]
+
+    G = nx.Graph()
+    G.add_nodes_from([0, 1])
+    G.add_edges_from([(0, 1, {"end": "A", "attr_1": 10})])
+
+    with pytest.warns(UserWarning):
+        renderer = from_networkx(G, nx.circular_layout)
+        assert renderer.edge_renderer.data_source.data["start"] == [0]
+        assert renderer.edge_renderer.data_source.data["end"] == [1]
+        assert renderer.edge_renderer.data_source.data["attr_1"] == [10]
