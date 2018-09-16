85ca7092126d9fcfcbf525687f246cff430d642b
Xav <xavart2011@gmail.com>
2018-09-16 17:29:15 +0200

    Remove bokeh info in ci

--- scripts/ci/appveyor/install.ps1
+++ scripts/ci/appveyor/install.ps1
@@ -1,12 +1,12 @@
 function install() {
 	conda config --set auto_update_conda off
 	conda config --set always_yes yes
-	conda config --append channels bokeh
-	conda config --append channels conda-forge
+	conda config --add channels bokeh
+	conda config --add channels conda-forge
 	conda config --get channels
 	conda update -q conda
 	conda install jinja2 pyyaml
-	conda install $(python scripts/deps.py build).split() |  % {'"' + $_ + '"'}
+	conda install $(python scripts/deps.py build).split() |  % {"""$_"""}
 }
 
 
--- scripts/ci/appveyor/test.ps1
+++ scripts/ci/appveyor/test.ps1
@@ -1,15 +1,6 @@
-function test_error($error, $message){
-	if(-NOT  ($error -eq 0)){
-		throw $message
-	}
-}
-
 function test(){
-	conda install $(python scripts/deps.py run test).split() | where {$_}
+	conda install $(python scripts/deps.py run test).split() | % {$_}
 	conda install -c conda-forge pillow #pillow from default channels raise ImportError: DLL load failed
-	
-	bokeh info
-	test_error $LastExitCode "Error to get bokeh info"
 }
 
 test
