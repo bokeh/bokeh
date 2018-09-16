26562e6c480dc17e454f20ef47dda21ffa1a067e
Xav <xavart2011@gmail.com>
2018-09-16 17:43:11 +0200

    Download sampledata for tests

--- scripts/ci/appveyor/test.ps1
+++ scripts/ci/appveyor/test.ps1
@@ -1,6 +1,7 @@
 function test(){
 	conda install $(python scripts/deps.py run test).split() | % {$_}
-	conda install -c conda-forge pillow #pillow from default channels raise ImportError: DLL load failed
+	conda install phantomjs
+	bokeh sampledata
 }
 
 test
