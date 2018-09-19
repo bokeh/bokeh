96d62e36258293ffe8d356da0c9c16c39c768c0c
Hunt J. Sparra <huntehlaboratories@gmail.com>
2018-09-19 11:02:07 -0400

    Removed changes to _version

--- bokeh/_version.py
+++ bokeh/_version.py
@@ -1,10 +1,3 @@
-#-----------------------------------------------------------------------------
-# Copyright (c) 2012 - 2018, Anaconda, Inc. All rights reserved.
-#
-# Powered by the Bokeh Development Team.
-#
-# The full license is in the file LICENSE.txt, distributed with this software.
-#-----------------------------------------------------------------------------
 # This file helps to compute a version number in source trees obtained from
 # git-archive tarball (such as those provided by githubs download-from-tag
 # feature). Distribution tarballs (built by setup.py sdist) and build
@@ -16,40 +9,14 @@
 
 """Git implementation of _version.py."""
 
-#-----------------------------------------------------------------------------
-# Boilerplate
-#-----------------------------------------------------------------------------
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import print_function
 
-import logging
-log = logging.getLogger(__name__)
-
-#-----------------------------------------------------------------------------
-# Imports
-#-----------------------------------------------------------------------------
-
-# Standard library imports
 import errno
 import os
 import re
 import subprocess
 import sys
 
-# External imports
-
-# Bokeh imports
-
-#-----------------------------------------------------------------------------
-# Globals and constants
-#-----------------------------------------------------------------------------
-
-#-----------------------------------------------------------------------------
-# General API
-#-----------------------------------------------------------------------------
-
-#-----------------------------------------------------------------------------
-# Dev API
-#-----------------------------------------------------------------------------
 
 def get_keywords():
     """Get the keywords needed to look up the version information."""
@@ -140,7 +107,6 @@ def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False,
 
 def versions_from_parentdir(parentdir_prefix, root, verbose):
     """Try to determine the version from the parent directory name.
-
     Source tarballs conventionally unpack into a directory that includes both
     the project name and a version string. We will also support searching up
     two directory levels for an appropriately named parent directory
@@ -250,7 +216,6 @@ def git_versions_from_keywords(keywords, tag_prefix, verbose):
 @register_vcs_handler("git", "pieces_from_vcs")
 def git_pieces_from_vcs(tag_prefix, root, verbose, run_command=run_command):
     """Get version from 'git describe' in the root of the source tree.
-
     This only gets called if the git-archive 'subst' keywords were *not*
     expanded, and _version.py hasn't already been rewritten with a short
     version string, meaning we're inside a checked out source tree.
@@ -353,10 +318,8 @@ def plus_or_dot(pieces):
 
 def render_pep440(pieces):
     """Build up version string, with post-release "local version identifier".
-
     Our goal: TAG[+DISTANCE.gHEX[.dirty]] . Note that if you
     get a tagged build and then dirty it, you'll get TAG+0.gHEX.dirty
-
     Exceptions:
     1: no tags. git_describe was just HEX. 0+untagged.DISTANCE.gHEX[.dirty]
     """
@@ -378,7 +341,6 @@ def render_pep440(pieces):
 
 def render_pep440_pre(pieces):
     """TAG[.post.devDISTANCE] -- No -dirty.
-
     Exceptions:
     1: no tags. 0.post.devDISTANCE
     """
@@ -394,11 +356,9 @@ def render_pep440_pre(pieces):
 
 def render_pep440_post(pieces):
     """TAG[.postDISTANCE[.dev0]+gHEX] .
-
     The ".dev0" means dirty. Note that .dev0 sorts backwards
     (a dirty tree will appear "older" than the corresponding clean one),
     but you shouldn't be releasing software with -dirty anyways.
-
     Exceptions:
     1: no tags. 0.postDISTANCE[.dev0]
     """
@@ -421,9 +381,7 @@ def render_pep440_post(pieces):
 
 def render_pep440_old(pieces):
     """TAG[.postDISTANCE[.dev0]] .
-
     The ".dev0" means dirty.
-
     Eexceptions:
     1: no tags. 0.postDISTANCE[.dev0]
     """
@@ -443,9 +401,7 @@ def render_pep440_old(pieces):
 
 def render_git_describe(pieces):
     """TAG[-DISTANCE-gHEX][-dirty].
-
     Like 'git describe --tags --dirty --always'.
-
     Exceptions:
     1: no tags. HEX[-dirty]  (note: no 'g' prefix)
     """
@@ -463,10 +419,8 @@ def render_git_describe(pieces):
 
 def render_git_describe_long(pieces):
     """TAG-DISTANCE-gHEX[-dirty].
-
     Like 'git describe --tags --dirty --always -long'.
     The distance/hash is unconditional.
-
     Exceptions:
     1: no tags. HEX[-dirty]  (note: no 'g' prefix)
     """
@@ -557,11 +511,3 @@ def get_versions():
     return {"version": "0+unknown", "full-revisionid": None,
             "dirty": None,
             "error": "unable to compute version", "date": None}
-
-#-----------------------------------------------------------------------------
-# Private API
-#-----------------------------------------------------------------------------
-
-#-----------------------------------------------------------------------------
-# Code
-#-----------------------------------------------------------------------------
