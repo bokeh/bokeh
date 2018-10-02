ee9655484c3e16708f2b77884826fb879d1e002e
Jeff <jfx319@users.noreply.github.com>
2018-10-02 14:24:57 -0400

    empty line for sphinx compatibility

--- bokeh/models/tickers.py
+++ bokeh/models/tickers.py
@@ -40,8 +40,10 @@ class ContinuousTicker(Ticker):
 
 class FixedTicker(ContinuousTicker):
     ''' Generate ticks at fixed, explicitly supplied locations.
+
     .. note::
         The ``desired_num_ticks`` property is ignored by this Ticker.
+
     '''
 
     ticks = Seq(Float, default=[], help="""
