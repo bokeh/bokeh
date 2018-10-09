afbbabbbb57c79b7bc60becac5766975633f4978
Hunt J. Sparra <huntehlaboratories@gmail.com>
2018-10-09 17:39:35 -0400

    Code quality improvements

--- bokeh/command/subcommands/__init__.py
+++ bokeh/command/subcommands/__init__.py
@@ -31,7 +31,7 @@ log = logging.getLogger(__name__)
 # Globals and constants
 #-----------------------------------------------------------------------------
 
-# __all__ defined at the bottom of the class module 
+# __all__ defined at the bottom of the class module
 
 #-----------------------------------------------------------------------------
 # General API
