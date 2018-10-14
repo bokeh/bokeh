4dc3640aee8824fa681c36742a6433e327a89247
Bryan Van de Ven <bryanv@anaconda.com>
2018-10-13 20:06:22 -0700

    load themes from python modules instead of json files (#8327)
    * load themes from python modules instead of external files
    
    * py 2.7 fix
    
    * pin sphinx to 1.7
    
    * fix up theme docs

--- bokeh/themes/__init__.py
+++ bokeh/themes/__init__.py
@@ -1,24 +1,126 @@
-''' Provides API for loading themes
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+''' Provide access to built-in themes:
+
+CALIBER
+~~~~~~~
+
+.. bokeh-plot::
+
+    from bokeh.plotting import figure, output_file, show
+    from bokeh.themes import built_in_themes
+    from bokeh.io import curdoc
+
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 6, 4, 5]
+
+    output_file("caliber.html")
+    curdoc().theme = 'caliber'
+    p = figure(title='caliber', plot_width=300, plot_height=300)
+    p.line(x, y)
+    show(p)
+
+DARK_MINIMAL
+~~~~~~~~~~~~
+
+.. bokeh-plot::
+
+    from bokeh.plotting import figure, output_file, show
+    from bokeh.themes import built_in_themes
+    from bokeh.io import curdoc
+
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 6, 4, 5]
+
+    output_file("dark_minimal.html")
+    curdoc().theme = 'dark_minimal'
+    p = figure(title='dark_minimal', plot_width=300, plot_height=300)
+    p.line(x, y)
+    show(p)
+
+
+LIGHT_MINIMAL
+~~~~~~~~~~~~~
+
+.. bokeh-plot::
+
+    from bokeh.plotting import figure, output_file, show
+    from bokeh.themes import built_in_themes
+    from bokeh.io import curdoc
+
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 6, 4, 5]
+
+    output_file("light_minimal.html")
+    curdoc().theme = 'light_minimal'
+    p = figure(title='light_minimal', plot_width=300, plot_height=300)
+    p.line(x, y)
+    show(p)
+
+as well as the ``Theme`` class that can be used to create new Themes.
+
+.. autoclass:: Theme
 
 '''
-from __future__ import absolute_import
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-from os.path import dirname, realpath, join
+import logging
+log = logging.getLogger(__name__)
 
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+
+# External imports
+
+# Bokeh imports
+from . import _caliber, _dark_minimal, _light_minimal
 from .theme import Theme
 
-_THIS_DIR = dirname(realpath(__file__))
-_FP_FMT = join(_THIS_DIR, '{0}.json')
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
 
+__all__ = (
+    'CALIBER',
+    'DARK_MINIMAL',
+    'LIGHT_MINIMAL',
+    'Theme',
+    'built_in_themes',
+    'default',
+)
+
+CALIBER       = 'caliber'
 LIGHT_MINIMAL = 'light_minimal'
-DARK_MINIMAL = 'dark_minimal'
-CALIBER = 'caliber'
+DARK_MINIMAL  = 'dark_minimal'
 
 default = Theme(json={})
+
 built_in_themes = {
-    LIGHT_MINIMAL: Theme(filename=_FP_FMT.format(LIGHT_MINIMAL)),
-    DARK_MINIMAL: Theme(filename=_FP_FMT.format(DARK_MINIMAL)),
-    CALIBER: Theme(filename=_FP_FMT.format(CALIBER))
+    CALIBER       : Theme(json=_caliber.json),
+    DARK_MINIMAL  : Theme(json=_dark_minimal.json),
+    LIGHT_MINIMAL : Theme(json=_caliber.json),
 }
 
-del dirname, realpath, join
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Code
+#----------------------------------------------------------------------------
--- bokeh/themes/theme.py
+++ bokeh/themes/theme.py
@@ -1,17 +1,54 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 ''' Provide a ``Theme`` class for specifying new default values for Bokeh
 :class:`~bokeh.model.Model` properties.
 
 '''
-from __future__ import absolute_import, print_function
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
 import yaml
 
+# External imports
+
+# Bokeh imports
 from ..core.has_props import HasProps
+
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
+
+__all__ = (
+    'Theme',
+)
 
 # whenever we cache that there's nothing themed for a class, we
 # use this same dict instance, so we don't have a zillion empty
 # dicts in our caches.
 _empty_dict = dict()
+
+#-----------------------------------------------------------------------------
+# General API
+#----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
 
 # Note: in DirectoryHandler and in general we assume this is an
 # immutable object, because we share it among sessions and we
@@ -82,53 +119,6 @@ class Theme(object):
                     'text_color': 'white'
                 }
             }
-
-    Built-in-Themes:
-
-        .. bokeh-plot::
-
-            from bokeh.plotting import figure, output_file, show
-            from bokeh.themes import built_in_themes
-            from bokeh.io import curdoc
-
-            x = [1, 2, 3, 4, 5]
-            y = [6, 7, 6, 4, 5]
-
-            output_file("dark_minimal.html")
-            curdoc().theme = 'dark_minimal'
-            p = figure(title='dark_minimal', plot_width=300, plot_height=300)
-            p.line(x, y)
-            show(p)
-
-        .. bokeh-plot::
-
-            from bokeh.plotting import figure, output_file, show
-            from bokeh.themes import built_in_themes
-            from bokeh.io import curdoc
-
-            x = [1, 2, 3, 4, 5]
-            y = [6, 7, 6, 4, 5]
-
-            output_file("light_minimal.html")
-            curdoc().theme = 'light_minimal'
-            p = figure(title='light_minimal', plot_width=300, plot_height=300)
-            p.line(x, y)
-            show(p)
-
-        .. bokeh-plot::
-
-            from bokeh.plotting import figure, output_file, show
-            from bokeh.themes import built_in_themes
-            from bokeh.io import curdoc
-
-            x = [1, 2, 3, 4, 5]
-            y = [6, 7, 6, 4, 5]
-
-            output_file("caliber.html")
-            curdoc().theme = 'caliber'
-            p = figure(title='caliber', plot_width=300, plot_height=300)
-            p.line(x, y)
-            show(p)
 
     '''
     def __init__(self, filename=None, json=None):
@@ -209,3 +199,11 @@ class Theme(object):
         # the dict.
         if len(_empty_dict) > 0:
             raise RuntimeError("Somebody put stuff in _empty_dict")
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Code
+#----------------------------------------------------------------------------
