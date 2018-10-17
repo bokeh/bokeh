d9db6172831d7077b5c95e49ed9f2a51959104e1
Bryan Van de Ven <bryanv@anaconda.com>
2018-10-17 10:56:04 -0700

    real data for multi_legend.py

--- examples/plotting/file/multi_legend.py
+++ examples/plotting/file/multi_legend.py
@@ -1,14 +1,23 @@
+import numpy as np
+
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import Legend, LegendItem
+from bokeh.sampledata.stocks import AAPL, MSFT
 
-p = figure()
-r = p.multi_line([[1,2,3], [1,2,3]], [[1,3,2], [3,4,3]],
-                 color=["orange", "red"], line_width=4)
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
+
+p = figure(background_fill_color="#fafafa", x_axis_type="datetime",
+           plot_width=800, plot_height=350)
+
+r = p.multi_line([datetime(AAPL['date']), datetime(MSFT['date'])],
+                 [AAPL['adj_close'], MSFT['adj_close']],
+                 color=["navy", "crimson"], line_width=2, alpha=0.6)
 
 legend = Legend(items=[
-    LegendItem(label="orange", renderers=[r], index=0),
-    LegendItem(label="red", renderers=[r], index=1),
-])
+    LegendItem(label="AAPL", renderers=[r], index=0),
+    LegendItem(label="MSFT", renderers=[r], index=1),
+], location="top_left")
 p.add_layout(legend)
 
 output_file("multi_legend.html")
