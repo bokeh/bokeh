89b46b405dd96ac6a9fd462fe36cea7e3e19fbc8
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-10-27 01:26:05 +0100

    Garbage collect after running DocumentLifeCycleHandler

--- bokeh/application/handlers/document_lifecycle.py
+++ bokeh/application/handlers/document_lifecycle.py
@@ -62,14 +62,23 @@ def _on_session_destroyed(session_context):
     '''
     Calls any on_session_destroyed callbacks defined on the Document
     '''
-    for callback in session_context._document._session_destroyed_callbacks:
+    callbacks = list(session_context._document._session_destroyed_callbacks)
+    session_context._document._session_destroyed_callbacks = {}
+    for callback in callbacks:
         try:
             callback(session_context)
         except Exception as e:
             log.warn('DocumentLifeCycleHandler on_session_destroyed '
-                     'callback failed on %s callback with following error: %s'
+                     'callback %s failed with following error: %s'
                      % (callback, e))
-    session_context._document._session_destroyed_callbacks = {}
+    if callbacks:
+        # If any session callbacks were defined garbage collect after
+        # deleting all references
+        del callback
+        del callbacks
+
+        import gc
+        gc.collect()
 
 #-----------------------------------------------------------------------------
 # Code
