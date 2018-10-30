54461ed1ac67f067165ffd010daf998a2fc86210
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-10-31 10:32:28 +1100

    Consistantly use single quotes in examples/app/export_csv/download.js

--- examples/app/export_csv/download.js
+++ examples/app/export_csv/download.js
@@ -25,10 +25,10 @@ var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
 if (navigator.msSaveBlob) {
     navigator.msSaveBlob(blob, filename)
 } else {
-    var link = document.createElement("a")
+    var link = document.createElement('a')
     link.href = URL.createObjectURL(blob)
     link.download = filename
-    link.target = "_blank"
+    link.target = '_blank'
     link.style.visibility = 'hidden'
     link.dispatchEvent(new MouseEvent('click'))
 }
