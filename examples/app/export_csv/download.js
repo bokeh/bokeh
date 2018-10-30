07ae7ec6be37ecbd901fc7d2400cfe5611d90f23
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-10-31 10:30:18 +1100

    Remove all trailing semicolons from download.js export csv example.

--- examples/app/export_csv/download.js
+++ examples/app/export_csv/download.js
@@ -1,35 +1,35 @@
 function table_to_csv(data_table) {
     var columns = Object.keys(data_table)
     var nrows = data_table[columns[0]].length
-    var lines = [columns.join(',')];
+    var lines = [columns.join(',')]
 
     for (var i = 0; i < nrows; i++) {
         var row = [];
         for (var j = 0; j < columns.length; j++) {
-            var column = columns[j];
-            row.push(data_table[column][i].toString());
+            var column = columns[j]
+            row.push(data_table[column][i].toString())
         }
-        lines.push(row.join(','));
+        lines.push(row.join(','))
     }
-    filetext = lines.join('\n').concat('\n');
+    filetext = lines.join('\n').concat('\n')
     return filetext
 }
 
 
-var data = source.data;
-var filetext = table_to_csv(data);
-var filename = 'data_result.csv';
-var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
+var data = source.data
+var filetext = table_to_csv(data)
+var filename = 'data_result.csv'
+var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
 
 //addresses IE
 if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, filename);
+    navigator.msSaveBlob(blob, filename)
 } else {
-    var link = document.createElement("a");
+    var link = document.createElement("a")
     link = document.createElement('a')
-    link.href = URL.createObjectURL(blob);
+    link.href = URL.createObjectURL(blob)
     link.download = filename
-    link.target = "_blank";
-    link.style.visibility = 'hidden';
+    link.target = "_blank"
+    link.style.visibility = 'hidden'
     link.dispatchEvent(new MouseEvent('click'))
 }
