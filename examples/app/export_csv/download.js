d27445f3c020d6800290509aa07f186da3e704a2
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2018-10-31 10:31:21 +1100

    Remove redundant line of code.

--- examples/app/export_csv/download.js
+++ examples/app/export_csv/download.js
@@ -26,7 +26,6 @@ if (navigator.msSaveBlob) {
     navigator.msSaveBlob(blob, filename)
 } else {
     var link = document.createElement("a")
-    link = document.createElement('a')
     link.href = URL.createObjectURL(blob)
     link.download = filename
     link.target = "_blank"
