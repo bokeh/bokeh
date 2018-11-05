a084ea11c801939a12435bd8be1081ac1b5f2a3a
dnnr <dnnr@users.noreply.github.com>
2018-11-05 21:35:04 +0100

    Fix defatult -> default

--- bokeh/models/tools.py
+++ bokeh/models/tools.py
@@ -994,7 +994,7 @@ class HoverTool(Inspection):
     have a matching value for their ``name`` attribute will be used.
     """)
 
-    renderers = Either(Auto, List(Instance(Renderer)), defatult="auto", help="""
+    renderers = Either(Auto, List(Instance(Renderer)), default="auto", help="""
     An explicit list of renderers to hit test against. If unset,
     defaults to all renderers on a plot.
     """)
