84d09b189ebc1d35084245fb2e78913cdc6803ed
anthonydouc <anthonydouc@hotmail.com>
2018-12-01 11:35:45 +1100

    swap to set data on model change

--- bokehjs/src/lib/models/annotations/arrow.ts
+++ bokehjs/src/lib/models/annotations/arrow.ts
@@ -31,14 +31,9 @@ export class ArrowView extends AnnotationView {
 
   connect_signals(): void {
     super.connect_signals()
-    this.connect(this.model.change, () => this.plot_view.request_render())
+    this.connect(this.model.change, () => this.set_data(this.model.source))
     this.connect(this.model.source.streaming, () => this.set_data(this.model.source))
     this.connect(this.model.source.patching, () => this.set_data(this.model.source))
-    this.connect(this.model.source.change, () => this.set_data(this.model.source))
-    this.connect(this.model.properties.x_start.change, () => this.set_data(this.model.source))
-    this.connect(this.model.properties.x_end.change, () => this.set_data(this.model.source))
-    this.connect(this.model.properties.y_start.change, () => this.set_data(this.model.source))
-    this.connect(this.model.properties.y_end.change, () => this.set_data(this.model.source))
   }
 
   set_data(source: ColumnarDataSource): void {
