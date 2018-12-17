ca36c9199977b6e73eca3cbba79ca410ea144858
Bryan Van de Ven <bryanv@anaconda.com>
2018-12-17 12:56:53 -0800

    Apply project boilerplate

--- bokeh/tests/test_settings.py
+++ bokeh/tests/test_settings.py
@@ -1,25 +1,71 @@
-from __future__ import absolute_import, print_function
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest ; pytest
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
 import logging
 import os
-from bokeh.settings import settings
+
+# External imports
+
+# Bokeh imports
+from bokeh._testing.util.api import verify_all
+
+# Module under test
+import bokeh.settings as bs
+
+#-----------------------------------------------------------------------------
+# Setup
+#-----------------------------------------------------------------------------
+
+ALL = (
+    'settings',
+)
 
 logging.basicConfig(level=logging.DEBUG)
 
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
 
-class TestSettings(object):
-    def test_get_list_single(self):
+Test___all__ = verify_all(bd, ALL)
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+class Test_get_list(object):
+    def test__single(self):
         os.environ["BOKEH_FOO"] = "foo"
-        result = settings._get_list("FOO", None)
+        result = bs.settings._get_list("FOO", None)
         assert result == ["foo"]
         del os.environ["BOKEH_FOO"]
 
-    def test_get_list_multiple(self):
+    def test__multiple(self):
         os.environ["BOKEH_FOO"] = "foo,bar,foobar"
-        result = settings._get_list("FOO", None)
+        result = bs.settings._get_list("FOO", None)
         assert result == ["foo", "bar", "foobar"]
         del os.environ["BOKEH_FOO"]
 
-    def test_get_list_default(self):
-        result = settings._get_list("FOO", None)
+    def test_default(self):
+        result = bs.settings._get_list("FOO", None)
         assert result is None
