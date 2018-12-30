a30868b3f54b7a38bc01283155a66fd8ee4435fe
Hunt Sparra <huntehlaboratories@gmail.com>
2018-12-30 15:15:50 -0500

    Initial boilerplate for bokeh/sphinxext

--- bokeh/sphinxext/templates.py
+++ bokeh/sphinxext/templates.py
@@ -1,8 +1,62 @@
-from __future__ import absolute_import
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2018, Anaconda, Inc. All rights reserved.
+#
+# Powered by the Bokeh Development Team.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+
+# External imports
 from jinja2 import Environment, PackageLoader
 
+# Bokeh imports
+
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
+
+__all__ = (
+    'CCB_PROLOGUE',
+    'CCB_EPILOGUE',
+    'COLOR_DETAIL',
+    'ENUM_DETAIL',
+    'GALLERY_PAGE',
+    'JINJA_DETAIL',
+    'MODEL_DETAIL',
+    'OPTIONS_DETAIL',
+    'PALETTE_DETAIL',
+    'PALETTE_GROUP_DETAIL',
+    'PLOT_PAGE',
+    'PROP_DETAIL',
+)
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
 _env = Environment(loader=PackageLoader('bokeh.sphinxext', '_templates'))
+
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
 
 CCB_PROLOGUE = _env.get_template("collapsible_code_block_prologue.html")
 CCB_EPILOGUE = _env.get_template("collapsible_code_block_epilogue.html")
@@ -26,3 +80,7 @@ PALETTE_GROUP_DETAIL = _env.get_template("palette_group_detail.html")
 PLOT_PAGE = _env.get_template("plot_page.rst")
 
 PROP_DETAIL = _env.get_template("prop_detail.rst")
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
