31009c97bd8525adc167f4754da3885e3a1c340e
Xav <xavart2011@gmail.com>
2019-01-12 16:55:23 +0100

    Custom extension ticks example replace coffee script with typescript

--- sphinx/source/docs/user_guide/examples/extensions_example_ticking.py
+++ sphinx/source/docs/user_guide/examples/extensions_example_ticking.py
@@ -1,29 +1,30 @@
 from bokeh.io import show
 from bokeh.models import TickFormatter
 from bokeh.plotting import figure
+from bokeh.util.compiler import TypeScript
 
-JS_CODE = """
-import {TickFormatter} from "models/formatters/tick_formatter"
+TS_CODE = """
+import { TickFormatter } from "models/formatters/tick_formatter"
 
-export class MyFormatter extends TickFormatter
+export class MyFormatter extends TickFormatter{
   type: "MyFormatter"
 
-  # TickFormatters should implement this method, which accepts a lisst
-  # of numbers (ticks) and returns a list of strings
-  doFormat: (ticks) ->
-    # format the first tick as-is
-    formatted = ["#{ticks[0]}"]
-
-    # format the remaining ticks as a difference from the first
-    for i in [1...ticks.length]
-       formatted.push("+#{(ticks[i]-ticks[0]).toPrecision(2)}")
-
-    return formatted
+  // TickFormatters should implement this method, which accepts a lisst
+  // of numbers (ticks) and returns a list of strings
+  doFormat(ticks: string[] | number[]){
+    // format the first tick as-is
+    const formatted = [`${ticks[0]}`]
+    for (let i = 1, len = ticks.length; i < len; i++) {
+      formatted.push(`+${(Number(ticks[i])-Number(ticks[0])).toPrecision(2)}`)
+    }
+    return formatted 
+  }
+}
 """
 
 class MyFormatter(TickFormatter):
 
-    __implementation__ = JS_CODE
+    __implementation__ = TypeScript(TS_CODE)
 
 p = figure()
 p.circle([1,2,3,4,6], [5,7,3,2,4])
