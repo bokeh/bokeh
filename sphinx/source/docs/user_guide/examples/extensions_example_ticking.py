7a5809dfa591b63afa6eb1991d9cfc33fdbb3c5c
ARTUSI <xavier.artusi@cea.fr>
2019-01-15 09:56:39 +0100

    Formatting tick example

--- sphinx/source/docs/user_guide/examples/extensions_example_ticking.py
+++ sphinx/source/docs/user_guide/examples/extensions_example_ticking.py
@@ -4,14 +4,14 @@ from bokeh.plotting import figure
 from bokeh.util.compiler import TypeScript
 
 TS_CODE = """
-import { TickFormatter } from "models/formatters/tick_formatter"
+import {TickFormatter} from "models/formatters/tick_formatter"
 
-export class MyFormatter extends TickFormatter{
-  type: "MyFormatter"
+export class MyFormatter extends TickFormatter {
+  type = "MyFormatter"
 
   // TickFormatters should implement this method, which accepts a lisst
   // of numbers (ticks) and returns a list of strings
-  doFormat(ticks: string[] | number[]){
+  doFormat(ticks: string[] | number[]) {
     // format the first tick as-is
     const formatted = [`${ticks[0]}`]
     for (let i = 1, len = ticks.length; i < len; i++) {
