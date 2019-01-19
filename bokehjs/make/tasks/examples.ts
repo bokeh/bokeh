c4ade3518e04927ada898aa33e644766b04825b4
Mateusz Paprocki <mattpap@gmail.com>
2019-01-19 11:06:02 +0100

    Allow to build bokehjs examples in one compilation run

--- bokehjs/make/tasks/examples.ts
+++ bokehjs/make/tasks/examples.ts
@@ -7,21 +7,17 @@ import {compileTypeScript} from "../compiler"
 
 const BASE_DIR = "./examples"
 
-const compile = (name: string) => {
-  compileTypeScript(join(BASE_DIR, name, "tsconfig.json"), {log})
-}
-
-const examples: string[] = []
-
-for(const name of fs.readdirSync("./examples")) {
+for (const name of fs.readdirSync("./examples")) {
   const stats = fs.statSync(join(BASE_DIR, name))
   if (stats.isDirectory() && fs.existsSync(join(BASE_DIR, name, "tsconfig.json"))) {
-    examples.push(name)
-    task(`examples:${name}`, async () => { compile(name) })
+    task(`examples:${name}`, async () => {
+      compileTypeScript(join(BASE_DIR, name, "tsconfig.json"), {log})
+    })
   }
 }
 
 const deps = argv.build === false ? [] : ["scripts:build", "styles:build"]
-const tasks = examples.map((example) => `examples:${example}`)
 
-task("examples", [...deps, ...tasks])
+task("examples", [...deps], async () => {
+  compileTypeScript(join(BASE_DIR, "tsconfig.json"), {log})
+})
