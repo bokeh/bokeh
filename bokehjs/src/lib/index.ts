f01004c71917f7ef0862c41ec6d62a6914d3ea53
Mateusz Paprocki <mattpap@gmail.com>
2019-01-19 01:40:48 +0100

    Make src/lib/index.ts a proper entry point

--- bokehjs/src/lib/index.ts
+++ bokehjs/src/lib/index.ts
@@ -1,17 +1,18 @@
-function is_browser(): boolean {
-  return typeof window !== "undefined" && typeof window.document !== "undefined"
-}
+import "./polyfill"
 
-function bokehjs(): any {
-  if (!is_browser()) {
-    throw new Error(`\
-bokehjs requires a window with a document. If your runtime \
-environment doesn't provide those, e.g. pure node.js, you \
-can use jsdom library to configure window and document.`)
-  }
+export {version} from "./version"
 
-  const Bokeh = require('./main')
-  return Bokeh
-}
+import * as embed from "./embed"
+export {embed}
 
-export = is_browser() ? bokehjs() : bokehjs
+import * as protocol from "./protocol"
+export {protocol}
+
+import * as _testing from "./testing"
+export {_testing}
+
+export {logger, set_log_level} from "./core/logging"
+export {settings}              from "./core/settings"
+export {Models, index}         from "./base"
+export {documents}             from "./document"
+export {safely}                from "./safely"
