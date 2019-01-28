aaff52b26370dcb7f226c1e8e9ba0dc7147be6ff
Bryan Van de Ven <bryanv@anaconda.com>
2019-01-28 10:41:24 -0800

    adapt to pandas changes

--- bokeh/sampledata/commits.py
+++ bokeh/sampledata/commits.py
@@ -51,9 +51,16 @@ def _read_data():
     '''
 
     '''
-    data = package_csv('commits', 'commits.txt.gz', parse_dates=True, header=None, names=['day', 'datetime'], index_col='datetime')
-    data = data.tz_localize('GMT').tz_convert('US/Central')
-    data['time'] = data.index.time
+    from ..util.dependencies import import_required
+    module = 'commits'
+    pd = import_required('pandas', '%s sample data requires Pandas (http://pandas.pydata.org) to be installed' % module)
+
+    data = package_csv(module, 'commits.txt.gz', parse_dates=True, header=None, names=['day', 'datetime'], index_col='datetime')
+
+    datetime = pd.to_datetime(data.index, utc=True).tz_convert('US/Central')
+    data.set_index(datetime)
+    data['time'] = datetime.time
+
     return data
 
 #-----------------------------------------------------------------------------
