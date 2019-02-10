af390b58ba5241c2c8ac1417615feaab41cfe66c
Mateusz Paprocki <mattpap@gmail.com>
2019-02-10 22:48:20 +0100

    Rewrite test/models/sources in TypeScript

--- bokehjs/src/lib/core/util/array.ts
+++ bokehjs/src/lib/core/util/array.ts
@@ -19,7 +19,7 @@ export function tail<T>(array: T[]): T {
   return array[array.length-1]
 }
 
-export function last<T>(array: T[]): T | undefined {
+export function last<T>(array: T[]): T {
   return array[array.length-1]
 }
 
