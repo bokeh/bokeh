c8d7e8d7ef70bb6da19b64014b3f7c2a015e2f86
Mateusz Paprocki <mattpap@gmail.com>
2019-03-16 23:08:37 +0100

    Better handling of default legend in bokehjs' Figure (#8756)

--- bokehjs/examples/legends/legends.ts
+++ bokehjs/examples/legends/legends.ts
@@ -8,7 +8,7 @@ export namespace Legends {
   Bokeh.set_log_level("info")
 
   const x = linspace(0, 4*Math.PI, 100)
-  const y = x.map((v) => Math.sin(v))
+  const y = x.map((v) => Math.sin(v + Math.PI))
 
   const y2 = y.map((v) => 2*v)
   const y3 = y.map((v) => 3*v)
@@ -16,17 +16,19 @@ export namespace Legends {
   const xr = new Bokeh.DataRange1d()
   const yr = new Bokeh.DataRange1d()
   const p1 = plt.figure({title: "Legends Example", x_range: xr, y_range: yr})
+  p1.legend.location = "top_left"
 
   p1.circle(x, y,  {legend: "sin(x)"})
   p1.circle(x, y2, {legend: "2*sin(x)", color: "orange"})
   p1.circle(x, y3, {legend: "3*sin(x)", color: "green"})
 
   const p2 = plt.figure({title: "Another Legend Example", x_range: xr, y_range: yr})
+  p2.legend.location = "top_left"
 
   p2.circle(x, y, {legend: "sin(x)"})
   p2.line(x, y, {legend: "sin(x)"})
 
-  p2.line(x, y2, {legend: "2*sin(x)", /*line_dash: [4, 4],*/ line_color: "orange", line_width: 2})
+  p2.line(x, y2, {legend: "2*sin(x)", line_dash: [4, 4], line_color: "orange", line_width: 2})
 
   p2.square(x, y3, {legend: "3*sin(x)", fill_color: null, line_color: "green"})
   p2.line(x, y3, {legend: "3*sin(x)", line_color: "green"})
