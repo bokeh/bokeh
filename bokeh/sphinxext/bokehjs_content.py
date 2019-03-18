0a3ea21852158af276eb5ac38a60a8f87864b950
Paddy Mullen <paddy@paddymullen.com>
2019-03-18 12:59:40 -0600

    added js_file option to bokehjs-content to allow inclusion of extrenal js_file

--- bokeh/sphinxext/bokehjs_content.py
+++ bokeh/sphinxext/bokehjs_content.py
@@ -46,17 +46,19 @@ log = logging.getLogger(__name__)
 #-----------------------------------------------------------------------------
 
 # Standard library imports
-from os.path import basename
-
+from os.path import basename, dirname, join
 # External imports
 from docutils import nodes
 from docutils.parsers.rst.directives import unchanged
-# Bokeh imports
 from sphinx.directives.code import CodeBlock
+from sphinx.util.nodes import set_source_info
+from sphinx.errors import SphinxError
+# Bokeh imports
 from .util import get_sphinx_resources
 from .templates import BJS_PROLOGUE, BJS_EPILOGUE, BJS_CODEPEN_INIT
-
-
+from ..settings import settings
+from ..resources import Resources
+from ..util.string import decode_utf8
 
 #-----------------------------------------------------------------------------
 # Globals and constants
@@ -84,8 +86,99 @@ class bokehjs_content(nodes.General, nodes.Element):
 
 class BokehJSContent(CodeBlock):
 
+    has_content = True
+    optional_arguments = 1
+
     option_spec = CodeBlock.option_spec
     option_spec.update(title=unchanged)
+    option_spec.update(js_file=unchanged)
+
+    def get_codeblock_node(self, code, language):
+        """this is directly copied from sphinx.directives.code.CodeBlock.run
+
+        it has been changed to accept code and language as an arguments instead
+        of reading from self
+
+        """
+
+        # type: () -> List[nodes.Node]
+        document = self.state.document
+        location = self.state_machine.get_source_and_line(self.lineno)
+
+        linespec = self.options.get('emphasize-lines')
+        if linespec:
+            try:
+                
+                nlines = len(code.split('\n'))
+                hl_lines = parselinenos(linespec, nlines)
+                if any(i >= nlines for i in hl_lines):
+                    logger.warning(__('line number spec is out of range(1-%d): %r') %
+                                   (nlines, self.options['emphasize-lines']),
+                                   location=location)
+
+                hl_lines = [x + 1 for x in hl_lines if x < nlines]
+            except ValueError as err:
+                return [document.reporter.warning(str(err), line=self.lineno)]
+        else:
+            hl_lines = None
+
+        if 'dedent' in self.options:
+            location = self.state_machine.get_source_and_line(self.lineno)
+            lines = code.split('\n')
+            lines = dedent_lines(lines, self.options['dedent'], location=location)
+            code = '\n'.join(lines)
+
+        literal = nodes.literal_block(code, code)
+        literal['language'] = language
+        literal['linenos'] = 'linenos' in self.options or \
+                             'lineno-start' in self.options
+        literal['classes'] += self.options.get('class', [])
+        extra_args = literal['highlight_args'] = {}
+        if hl_lines is not None:
+            extra_args['hl_lines'] = hl_lines
+        if 'lineno-start' in self.options:
+            extra_args['linenostart'] = self.options['lineno-start']
+        set_source_info(self, literal)
+
+        caption = self.options.get('caption')
+        if caption:
+            try:
+                literal = container_wrapper(self, literal, caption)
+            except ValueError as exc:
+                return [document.reporter.warning(text_type(exc), line=self.lineno)]
+
+        # literal will be note_implicit_target that is linked from caption and numref.
+        # when options['name'] is provided, it should be primary ID.
+        self.add_name(literal)
+
+        return [literal]
+        
+
+    def get_code_language(self):
+        """
+        This is largely copied from bokeh.sphinxext.bokeh_plot.run
+        """
+        env = self.state.document.settings.env
+
+        js_file = self.options.get("js_file", False)
+        # js_file *or* js code content, but not both
+        if js_file  and self.content:
+            raise SphinxError("bokehjs-content:: directive can't have both js_file and content")
+
+        
+        if js_file:
+            log.debug("[bokehjs-content] handling external example in %r: %s", env.docname, self.arguments[0])
+            path = js_file
+            if not js_file.startswith("/"):
+                path = join(env.app.srcdir, path)
+            source = decode_utf8(open(path).read())
+        else:
+            log.debug("[bokehjs-content] handling inline example in %r", env.docname)
+            path = env.bokeh_plot_auxdir  # code runner just needs any real path
+            source = '\n'.join(self.content)
+                
+        return [source, "javascript"]
+
 
     def run(self):
         env = self.state.document.settings.env
@@ -99,7 +192,7 @@ class BokehJSContent(CodeBlock):
 
         node = bokehjs_content()
         node['target_id'] = target_id
-        node['title'] = self.options.get('title', "bokehjs example")
+        node['title'] = self.options.get("title", "bokehjs example")
         node['include_bjs_header'] = False
 
         source_doc = self.state_machine.node.document
@@ -112,7 +205,11 @@ class BokehJSContent(CodeBlock):
             #page)
             source_doc.bjs_seen = True
             node['include_bjs_header'] = True
-        cb = CodeBlock.run(self)
+        # import pdb
+        # pdb.set_trace()
+        code_content, language = self.get_code_language()
+        cb = self.get_codeblock_node(
+            code_content, language)
         node.setup_child(cb[0])
         node.children.append(cb[0])
         return [target_node, node]
