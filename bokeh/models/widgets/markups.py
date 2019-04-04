3e33cbc8ea254c8d784bd123e686dd903d92411c
Mateusz Paprocki <mattpap@gmail.com>
2019-04-04 16:01:18 +0200

    Warn about layout and asynchronous HTML content (#8812)

--- bokeh/models/widgets/markups.py
+++ bokeh/models/widgets/markups.py
@@ -60,7 +60,16 @@ class Markup(Widget):
     '''
 
     text = String(default="", help="""
-    The contents of the widget.
+    The text or HTML contents of the widget.
+
+    .. note::
+        If the HTML content contains elements which size depends on
+        on external, asynchronously loaded resources, the size of
+        the widget may be computed incorrectly. This is in particular
+        an issue with images (``<img>``). To remedy this problem, one
+        either has to set explicit dimensions using CSS properties,
+        HTML attributes or model's ``width`` and ``height`` properties,
+        or inline images' contents using data URIs.
     """)
 
     style = Dict(String, Any, default={}, help="""
