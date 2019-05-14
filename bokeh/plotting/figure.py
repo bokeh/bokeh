7d758c924e01f40bd17e404c0b4c686a6dff6921
Bryan Van de Ven <bryan@bokeh.org>
2019-05-14 00:24:16 -0700

    vline/hline

--- bokeh/plotting/figure.py
+++ bokeh/plotting/figure.py
@@ -87,7 +87,6 @@ class Figure(Plot):
         * :func:`~bokeh.plotting.figure.Figure.image_url`
         * :func:`~bokeh.plotting.figure.Figure.inverted_triangle`
         * :func:`~bokeh.plotting.figure.Figure.line`
-        * :func:`~bokeh.plotting.figure.Figure.line_stack`
         * :func:`~bokeh.plotting.figure.Figure.multi_line`
         * :func:`~bokeh.plotting.figure.Figure.multi_polygons`
         * :func:`~bokeh.plotting.figure.Figure.oval`
@@ -116,7 +115,7 @@ class Figure(Plot):
     There are also specialized methods for stacking bars:
 
     * bars: :func:`~bokeh.plotting.figure.Figure.hbar_stack`, :func:`~bokeh.plotting.figure.Figure.vbar_stack`
-    * lines: :func:`~bokeh.plotting.figure.Figure.line_stack`
+    * lines: :func:`~bokeh.plotting.figure.Figure.hline_stack`, :func:`~bokeh.plotting.figure.Figure.vline_stack`
     * areas: :func:`~bokeh.plotting.figure.Figure.harea_stack`, :func:`~bokeh.plotting.figure.Figure.varea_stack`
 
     As well as one specialized method for making simple hexbin plots:
@@ -994,7 +993,7 @@ Examples:
             result.append(self.hbar(**kw))
         return result
 
-    def line_stack(self, x, y, **kw):
+    def _line_stack(self, x, y, **kw):
         ''' Generate multiple ``Line`` renderers for lines stacked vertically
         or horizontally.
 
@@ -1051,6 +1050,45 @@ Examples:
             return result
 
         return [self.line(x, y, **kw)]
+
+    def hline_stack(self, stackers, **kw):
+        ''' Generate multiple ``Line`` renderers for lines stacked horizontally.
+
+        Args:
+            stackers (seq[str]) : a list of data source field names to stack
+                successively for ``x`` line coordinates.
+
+        Additionally, the ``name`` of the renderer will be set to
+        the value of each successive stacker (this is useful with the
+        special hover variable ``$name``)
+
+        Any additional keyword arguments are passed to each call to ``line``.
+        If a keyword value is a list or tuple, then each call will get one
+        value from the sequence.
+
+        Returns:
+            list[GlyphRenderer]
+
+        Examples:
+
+            Assuming a ``ColumnDataSource`` named ``source`` with columns
+            *2106* and *2017*, then the following call to ``hline_stack`` with
+            stackers for the x-coordinates will will create two ``Line``
+            renderers that stack:
+
+            .. code-block:: python
+
+                p.hline_stack(['2016', '2017'], y='y', color=['blue', 'red'], source=source)
+
+            This is equivalent to the following two separate calls:
+
+            .. code-block:: python
+
+                p.line(x=stack('2016'),         y='y', color='blue', source=source, name='2016')
+                p.line(x=stack('2016', '2017'), y='y', color='red',  source=source, name='2017')
+
+        '''
+        return self._line_stack(x=stackers, **kw)
 
     def varea_stack(self, stackers, **kw):
         ''' Generate multiple ``VArea`` renderers for levels stacked bottom
@@ -1135,6 +1173,45 @@ Examples:
         for kw in _double_stack(stackers, "bottom", "top", **kw):
             result.append(self.vbar(**kw))
         return result
+
+    def vline_stack(self, stackers, **kw):
+        ''' Generate multiple ``Line`` renderers for lines stacked vertically.
+
+        Args:
+            stackers (seq[str]) : a list of data source field names to stack
+                successively for ``y`` line coordinates.
+
+        Additionally, the ``name`` of the renderer will be set to
+        the value of each successive stacker (this is useful with the
+        special hover variable ``$name``)
+
+        Any additional keyword arguments are passed to each call to ``line``.
+        If a keyword value is a list or tuple, then each call will get one
+        value from the sequence.
+
+        Returns:
+            list[GlyphRenderer]
+
+        Examples:
+
+            Assuming a ``ColumnDataSource`` named ``source`` with columns
+            *2106* and *2017*, then the following call to ``vline_stack`` with
+            stackers for the y-coordinates will will create two ``Line``
+            renderers that stack:
+
+            .. code-block:: python
+
+                p.vline_stack(['2016', '2017'], x='x', color=['blue', 'red'], source=source)
+
+            This is equivalent to the following two separate calls:
+
+            .. code-block:: python
+
+                p.line(y=stack('2016'),         x='x', color='blue', source=source, name='2016')
+                p.line(y=stack('2016', '2017'), x='x', color='red',  source=source, name='2017')
+
+        '''
+        return self._line_stack(y=stackers, **kw)
 
     def graph(self, node_source, edge_source, layout_provider, **kwargs):
         ''' Creates a network graph using the given node, edge and layout provider.
