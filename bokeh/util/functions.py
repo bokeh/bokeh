baa71f28aba2b11ee6c455ba1099a3b08c5d3ac1
Bryan Van de Ven <bryan@bokeh.org>
2019-05-29 21:48:49 -0700

    Remove bokeh.util.future (#8950)
    * remove usage of with_metaclass
    
    * remove functools.wraps fixup
    
    * use collections.abc throughout
    
    * remove func sig functions to util.functions
    
    * update docs

---
+++ bokeh/util/functions.py
@@ -0,0 +1,67 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2019, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+''' Utilities for function introspection.
+
+'''
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function
+
+import logging
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+
+# External imports
+
+# Bokeh imports
+
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
+
+__all__ = (
+    'get_param_info',
+)
+
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+
+def get_param_info(sig):
+    ''' Find parameters with defaults and return them.
+
+    Arguments:
+        sig (Signature) : a function signature
+
+    Returns:
+        tuple(list, list) : parameters with defaults
+
+    '''
+    defaults = []
+    for param in sig.parameters.values():
+        if param.default is not param.empty:
+            defaults.append(param.default)
+    return list(sig.parameters), defaults
