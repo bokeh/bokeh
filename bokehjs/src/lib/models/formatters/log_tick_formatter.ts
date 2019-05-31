0b16c293fa12c2729e0c4bd0765f57a04e8417d7
Michele Renda <michele.renda@cern.ch>
2019-05-31 13:30:47 +0300

    Added support to use_superscript_exponents

--- bokehjs/src/lib/models/formatters/log_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/log_tick_formatter.ts
@@ -51,10 +51,12 @@ export class LogTickFormatter extends TickFormatter {
     let small_interval = false
     const labels: string[] = new Array(ticks.length)
     for (let i = 0, end = ticks.length; i < end; i++) {
-      if (!this.use_superscript_exponents)
+	  
+      if (!this.use_superscript_exponents) {
 		labels[i] = `${base}^${Math.round(Math.log(ticks[i]) / Math.log(base))}`
-	  else
+	  } else {
 	    labels[i] = `${base}${(Math.round(Math.log(ticks[i]) / Math.log(base))).toString().replace(/[0-9\+\-]/g,  function(match){return translate[match]})}`
+	  }
 	  
       if (i > 0 && labels[i] == labels[i-1]) {
         small_interval = true
