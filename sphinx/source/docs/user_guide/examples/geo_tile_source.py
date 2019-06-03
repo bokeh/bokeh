8d4a722f73a1e856ba93a5511b5a8cd2a275d18b
Bryan Van de Ven <bryan@bokeh.org>
2019-06-02 17:23:08 -0700

    execute tile provider deprecations (#8973)
    * execute tile provider deprecations
    
    * fux up two examples missed

--- sphinx/source/docs/user_guide/examples/geo_tile_source.py
+++ sphinx/source/docs/user_guide/examples/geo_tile_source.py
@@ -1,9 +1,9 @@
 from bokeh.plotting import figure, show, output_file
-from bokeh.tile_providers import get_provider, Vendors
+from bokeh.tile_providers import get_provider, CARTODBPOSITRON
 
 output_file("tile.html")
 
-tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+tile_provider = get_provider(CARTODBPOSITRON)
 
 # range bounds supplied in web mercator coordinates
 p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
