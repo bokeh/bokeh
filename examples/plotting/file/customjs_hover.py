00cae5e4f41c3951ca1418fafec338a350361472
Bryan Van de Ven <bryan@bokeh.org>
2019-06-02 17:23:08 -0700

    execute tile provider deprecations (#8973)
    * execute tile provider deprecations
    
    * fux up two examples missed

--- examples/plotting/file/customjs_hover.py
+++ examples/plotting/file/customjs_hover.py
@@ -1,14 +1,14 @@
 from bokeh.io import output_file, show
 from bokeh.models import HoverTool, CustomJSHover
 from bokeh.plotting import figure
-from bokeh.tile_providers import CARTODBPOSITRON
+from bokeh.tile_providers import get_provider, CARTODBPOSITRON
 
 output_file("customjs_hover.html")
 
 # range bounds supplied in web mercator coordinates
 p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
            x_axis_type="mercator", y_axis_type="mercator")
-p.add_tile(CARTODBPOSITRON)
+p.add_tile(get_provider(CARTODBPOSITRON))
 
 p.circle(x=[0, 2000000, 4000000], y=[4000000, 2000000, 0], size=30)
 
