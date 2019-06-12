d950df63f3fbf272e774740c0d39239df6149653
Bryan Van de Ven <bryan@bokeh.org>
2019-06-12 16:20:41 -0700

    Move tests out of tree

---
+++ tests/unit/bokeh/command/subcommands/_util_subcommands.py
@@ -0,0 +1,70 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2019, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+
+# External imports
+
+# Bokeh imports
+
+#-----------------------------------------------------------------------------
+# Setup
+#-----------------------------------------------------------------------------
+
+basic_scatter_script = """
+import numpy as np
+from bokeh.plotting import figure
+from bokeh.io import curdoc
+N = 5
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+p1 = figure()
+p1.scatter(x,y, color="#FF00FF")
+doc = curdoc()
+doc.add_root(p1)
+"""
+
+basic_svg_scatter_script = """
+import numpy as np
+from bokeh.plotting import figure
+from bokeh.io import curdoc
+N = 5
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+p1 = figure(output_backend="svg")
+p1.scatter(x,y, color="#FF00FF")
+doc = curdoc()
+doc.add_root(p1)
+"""
+
+multi_svg_scatter_script = """
+import numpy as np
+from bokeh.plotting import figure
+from bokeh.layouts import Row
+from bokeh.io import curdoc
+N = 5
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+p1 = figure(output_backend="svg")
+p1.scatter(x,y, color="#FF00FF")
+p2 = figure(output_backend="svg")
+p2.scatter(x,y, color="#00FF00")
+doc = curdoc()
+doc.add_root(Row(p1, p2))
+"""
