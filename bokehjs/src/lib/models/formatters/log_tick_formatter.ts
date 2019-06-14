a8a72a12d67c49484fd43a84afd52dd56cd24474
Michele Renda <michele.renda@cern.ch>
2019-06-14 16:54:47 +0300

    Removed trailing spaces

--- bokehjs/src/lib/models/formatters/log_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/log_tick_formatter.ts
@@ -43,20 +43,20 @@ export class LogTickFormatter extends TickFormatter {
       return []
 
     const base = this.ticker != null ? this.ticker.base : 10
-    
+
     const translate: { [id: string]: string; } = { 0:"\u2070", 1:"\u00b9", 2:"\u00b2", 3:"\u00b3", 4:"\u2074", 5:"\u2075", 6:"\u2076", 7:"\u2077", 8:"\u2078", 9:"\u2079", "+":"\u207a", "-":"\u207b" }
 
     let small_interval = false
     const labels: string[] = new Array(ticks.length)
     for (let i = 0, end = ticks.length; i < end; i++) {
-      
+
       const exponent = Math.round(Math.log(ticks[i]) / Math.log(base))
-      
+
       if (!this.use_superscript_exponents)
         labels[i] = `${base}^${exponent}`
       else
         labels[i] = `${base}${exponent.toString().replace(/[0-9\+\-]/g,  match => translate[match])}`
-            
+
       if (i > 0 && labels[i] == labels[i-1]) {
         small_interval = true
         break
