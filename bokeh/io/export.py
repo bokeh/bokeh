2b00f448cc5cba04c23adeb643fcad9834c5217c
Michele Renda <michele.renda@cern.ch>
2019-06-14 14:20:30 +0300

    Moved encoding to utf-8

--- bokeh/io/export.py
+++ bokeh/io/export.py
@@ -233,7 +233,7 @@ def get_svgs(obj, driver=None, timeout=5, **kwargs):
     with _tmp_html() as tmp:
         html = get_layout_html(obj, **kwargs)
         with io.open(tmp.path, mode="wb") as file:
-            file.write(html.encode('latin-1'))
+            file.write(html.encode('utf-8'))
 
         web_driver = driver if driver is not None else webdriver_control.get()
 
