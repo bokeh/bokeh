123a65d6d95e51b3d60d17f528fc262468d2fde0
Michele Renda <michele.renda@cern.ch>
2019-06-14 15:40:34 +0300

    Fix Unnecessarily quoted property

--- bokehjs/src/lib/models/formatters/log_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/log_tick_formatter.ts
@@ -46,7 +46,7 @@ export class LogTickFormatter extends TickFormatter {
 
     const base = this.ticker != null ? this.ticker.base : 10
     
-    const translate: { [id: string]: string; } = { "0":"\u2070", "1":"\u00b9", "2":"\u00b2", "3":"\u00b3", "4":"\u2074", "5":"\u2075", "6":"\u2076", "7":"\u2077", "8":"\u2078", "9":"\u2079", "+":"\u207a", "-":"\u207b" }
+    const translate: { [id: string]: string; } = { 0:"\u2070", 1:"\u00b9", 2:"\u00b2", 3:"\u00b3", 4:"\u2074", 5:"\u2075", 6:"\u2076", 7:"\u2077", 8:"\u2078", 9:"\u2079", "+":"\u207a", "-":"\u207b" }
 
     let small_interval = false
     const labels: string[] = new Array(ticks.length)
