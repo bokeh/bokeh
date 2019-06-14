621712cffd360ae589933bcd73c5a16149b30dc5
Michele Renda <michele.renda@cern.ch>
2019-06-14 16:20:51 +0300

    Added use_superscript_exponents

--- bokehjs/src/lib/models/formatters/log_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/log_tick_formatter.ts
@@ -9,6 +9,7 @@ export namespace LogTickFormatter {
 
   export type Props = TickFormatter.Props & {
     ticker: p.Property<LogTicker | null>
+    use_superscript_exponents: p.Property<boolean>
   }
 }
 
@@ -24,6 +25,7 @@ export class LogTickFormatter extends TickFormatter {
   static initClass(): void {
     this.define<LogTickFormatter.Props>({
       ticker: [ p.Instance, null ],
+      use_superscript_exponents:   [ p.Boolean, false   ],
     })
   }
 
@@ -41,11 +43,20 @@ export class LogTickFormatter extends TickFormatter {
       return []
 
     const base = this.ticker != null ? this.ticker.base : 10
+    
+    const translate: { [id: string]: string; } = { 0:"\u2070", 1:"\u00b9", 2:"\u00b2", 3:"\u00b3", 4:"\u2074", 5:"\u2075", 6:"\u2076", 7:"\u2077", 8:"\u2078", 9:"\u2079", "+":"\u207a", "-":"\u207b" }
 
     let small_interval = false
     const labels: string[] = new Array(ticks.length)
     for (let i = 0, end = ticks.length; i < end; i++) {
-      labels[i] = `${base}^${ Math.round(Math.log(ticks[i]) / Math.log(base)) }`
+      
+      const exponent = Math.round(Math.log(ticks[i]) / Math.log(base))
+      
+      if (!this.use_superscript_exponents)
+        labels[i] = `${base}^${exponent}`
+      else
+        labels[i] = `${base}${exponent.toString().replace(/[0-9\+\-]/g,  match => translate[match])}`
+            
       if (i > 0 && labels[i] == labels[i-1]) {
         small_interval = true
         break
