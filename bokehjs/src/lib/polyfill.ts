34a9363846379a21180bd83a73778bbe41e8c6e4
Mateusz Paprocki <mattpap@gmail.com>
2019-08-18 23:17:48 +0200

    Include Object.assign() and clean up polyfill.ts

--- bokehjs/src/lib/polyfill.ts
+++ bokehjs/src/lib/polyfill.ts
@@ -1,143 +1,14 @@
 declare function require(moduleName: string): any
 
+require("es5-ext/object/assign/implement")
+require("es5-ext/number/is-integer/implement")
+require("es5-ext/string/#/repeat/implement")
+require("es5-ext/array/from/implement")
+require("es5-ext/math/log10/implement")
 require("es6-set/implement")
 require("es6-map/implement")
 require("es6-weak-map/implement")
 
 if (typeof Promise === "undefined") {
   require("es6-promise").polyfill()
-}
-
-if (typeof Math.log10 === "undefined") {
-  Math.log10 = function(x: number): number {
-    return Math.log(x) * Math.LOG10E
-  }
-}
-
-// ref: https://github.com/bokeh/bokeh/issues/7373
-if (typeof Number.isInteger === "undefined") {
-  Number.isInteger = function(value: number): boolean {
-    return typeof value === 'number' && isFinite(value) && Math.floor(value) === value
-  }
-}
-
-if (typeof String.prototype.repeat === "undefined") {
-  String.prototype.repeat = function(count: number): string {
-    if (this == null) {
-      throw new TypeError('can\'t convert ' + this + ' to object')
-    }
-    let str = '' + this
-    count = +count
-    if (count != count) {
-      count = 0
-    }
-    if (count < 0) {
-      throw new RangeError('repeat count must be non-negative')
-    }
-    if (count == Infinity) {
-      throw new RangeError('repeat count must be less than infinity')
-    }
-    count = Math.floor(count)
-    if (str.length == 0 || count == 0) {
-      return ''
-    }
-    // Ensuring count is a 31-bit integer allows us to heavily optimize the
-    // main part. But anyway, most current (August 2014) browsers can't handle
-    // strings 1 << 28 chars or longer, so:
-    if (str.length * count >= 1 << 28) {
-      throw new RangeError('repeat count must not overflow maximum string size')
-    }
-    let rpt = ''
-    for (;;) {
-      if ((count & 1) == 1) {
-        rpt += str
-      }
-      count >>>= 1
-      if (count == 0) {
-        break
-      }
-      str += str
-    }
-    // Could we try:
-    // return Array(count + 1).join(this)
-    return rpt
-  }
-}
-
-// Production steps of ECMA-262, Edition 6, 22.1.2.1
-if (typeof Array.from === "undefined") {
-  Array.from = (function() {
-    const toStr = Object.prototype.toString
-    const isCallable = function(fn: any) {
-      return typeof fn === 'function' || toStr.call(fn) === '[object Function]'
-    }
-    const toInteger = function(value: any) {
-      const number = Number(value)
-      if (isNaN(number)) { return 0 }
-      if (number === 0 || !isFinite(number)) { return number }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number))
-    }
-    const maxSafeInteger = Math.pow(2, 53) - 1
-    const toLength = function(value: any) {
-      const len = toInteger(value)
-      return Math.min(Math.max(len, 0), maxSafeInteger)
-    }
-
-    // The length property of the from method is 1.
-    return function from(this: any, arrayLike: any /*, mapFn, thisArg */) {
-      // 1. Let C be the this value.
-      const C = this
-
-      // 2. Let items be ToObject(arrayLike).
-      const items = Object(arrayLike)
-
-      // 3. ReturnIfAbrupt(items).
-      if (arrayLike == null) {
-        throw new TypeError('Array.from requires an array-like object - not null or undefined')
-      }
-
-      // 4. If mapfn is undefined, then let mapping be false.
-      const mapFn = arguments.length > 1 ? arguments[1] : void undefined
-      let T
-      if (typeof mapFn !== 'undefined') {
-        // 5. else
-        // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-        if (!isCallable(mapFn)) {
-          throw new TypeError('Array.from: when provided, the second argument must be a function')
-        }
-
-        // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        if (arguments.length > 2) {
-          T = arguments[2]
-        }
-      }
-
-      // 10. Let lenValue be Get(items, "length").
-      // 11. Let len be ToLength(lenValue).
-      const len = toLength(items.length)
-
-      // 13. If IsConstructor(C) is true, then
-      // 13. a. Let A be the result of calling the [[Construct]] internal method
-      // of C with an argument list containing the single item len.
-      // 14. a. Else, Let A be ArrayCreate(len).
-      const A = isCallable(C) ? Object(new C(len)) : new Array(len)
-
-      // 16. Let k be 0.
-      let k = 0
-      // 17. Repeat, while k < len… (also steps a - h)
-      while (k < len) {
-        const kValue = items[k]
-        if (mapFn) {
-          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k)
-        } else {
-          A[k] = kValue
-        }
-        k += 1
-      }
-      // 18. Let putStatus be Put(A, "length", len, true).
-      A.length = len
-      // 20. Return A.
-      return A
-    }
-  }())
 }
