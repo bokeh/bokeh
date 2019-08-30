39089414c9c5b43366f74470cb231fc48a828271
Mateusz Paprocki <mattpap@gmail.com>
2019-08-30 17:20:56 +0200

    Use newer conda on Appveyor

--- scripts/ci/appveyor/install.ps1
+++ scripts/ci/appveyor/install.ps1
@@ -1,12 +1,14 @@
+set-psdebug -trace 2
+
 function install() {
     conda config --set auto_update_conda off
     conda config --set always_yes yes
     conda config --add channels bokeh
     conda config --add channels conda-forge
     conda config --get channels
-    conda install --yes --quiet $Env:CONDA_REQS
-    conda install jinja2 pyyaml
-    conda install $(python scripts/deps.py build).split() |  % {"""$_"""}
+    conda install --quiet $Env:CONDA_REQS
+    conda install --quiet jinja2 pyyaml
+    conda install --quiet $(python scripts/deps.py build).split()
 }
 
 install
