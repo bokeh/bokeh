0b3eea87683fa122de2f0e7a2f3c00b77a8ed749
Connor Tann <connor_tann@hotmail.com>
2019-10-10 20:45:14 +0100

    Fix range_tool selection bug (#9269)
    * Update range_tool.ts
    
    Updated to fix range_tool scaling issue
    
    * Update range_tool.ts
    
    Removed redundant space
    
    * Fix test with update_range_end_side
    
    * Also fix function import
    
    * Fix description
    
    * Update test for compute_start_side
    
    * Update_range_start_side

--- bokehjs/src/lib/models/tools/gestures/range_tool.ts
+++ bokehjs/src/lib/models/tools/gestures/range_tool.ts
@@ -64,7 +64,7 @@ export function compute_value(value: number, scale: Scale, sdelta: number, range
   return value
 }
 
-export function compute_end_side(end: number, range: Range, side: Side): Side {
+export function update_range_end_side(end: number, range: Range, side: Side): Side {
   if (end > range.start) {
     range.end = end
     return side
@@ -75,7 +75,7 @@ export function compute_end_side(end: number, range: Range, side: Side): Side {
   }
 }
 
-export function compute_start_side(start: number, range: Range, side: Side): Side {
+export function update_range_start_side(start: number, range: Range, side: Side): Side {
   if (start < range.end) {
     range.start = start
     return side
@@ -178,10 +178,10 @@ export class RangeToolView extends GestureToolView {
         update_range(xr, xscale, new_dx, frame.x_range)
       else if (this.side == Side.Left) {
         const start = compute_value(xr.start, xscale, new_dx, frame.x_range)
-        this.side = compute_start_side(start, xr, this.side)
+        this.side = update_range_start_side(start, xr, this.side)
       } else if (this.side == Side.Right) {
         const end = compute_value(xr.end, xscale, new_dx, frame.x_range)
-        this.side = compute_end_side(end, xr, this.side)
+        this.side = update_range_end_side(end, xr, this.side)
       }
     }
 
@@ -189,13 +189,11 @@ export class RangeToolView extends GestureToolView {
       if (this.side == Side.BottomTop || this.side == Side.LeftRightBottomTop)
         update_range(yr, yscale, new_dy, frame.y_range)
       else if (this.side == Side.Bottom) {
-        yr.start = compute_value(yr.start, yscale, new_dy, frame.y_range)
         const start = compute_value(yr.start, yscale, new_dy, frame.y_range)
-        this.side = compute_start_side(start, yr, this.side)
+        this.side = update_range_start_side(start, yr, this.side)
       } else if (this.side == Side.Top) {
-        yr.end = compute_value(yr.end, yscale, new_dy, frame.y_range)
         const end = compute_value(yr.end, yscale, new_dy, frame.y_range)
-        this.side = compute_end_side(end, yr, this.side)
+        this.side = update_range_end_side(end, yr, this.side)
       }
     }
 
