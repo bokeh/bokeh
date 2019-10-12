45a11f4a25024a19530cee45b344e3e4680b9645
Franz Wöllert <franz.woellert@heidelberg.com>
2019-10-12 15:33:43 +0200

    Refactor module to expose reusable functionality.

--- scripts/deps.py
+++ scripts/deps.py
@@ -1,59 +1,106 @@
 import os
-from os.path import abspath, dirname, join
 import platform
 import sys
-
 import jinja2
 import setuptools
 import yaml
+from unittest import mock
+from functools import reduce
+from operator import add
+from pathlib import Path
 
-data = {}
-setup_src = open("setup.py").read()
-os.environ['CONDA_BUILD_STATE'] = 'RENDER'
-def _setup(**kw): data.update(kw)
-setuptools.setup = _setup
-sys.path.append(abspath(join(dirname(__file__), "..")))
-exec(setup_src)
 
 def load_setup_py_data():
-    return data
+    """Retrieve all keyword arguments of setup function from setup.py to access
+    `install_requires` and others which are mandatory to evaluate necessary
+    dependencies.
 
-meta_src = jinja2.Template(open("conda.recipe/meta.yaml").read())
-try:
-    meta_src = yaml.load(meta_src.render(load_setup_py_data=load_setup_py_data),
-                         Loader=yaml.FullLoader)
-except AttributeError as e:
-    # Loader=yaml.FullLoader added in pyyaml 5.1 because of:
-    # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
-    # isn't available on conda for python=3.5
-    # fall back to calling without loader if it isn't available
-    if 'FullLoader' in repr(e):
-        meta_src = yaml.load(meta_src.render(load_setup_py_data=load_setup_py_data))
-    else:
-        raise
+    """
 
-section = {
-    "build"  : meta_src["requirements"]["build"],
-    "deploy" : meta_src["extra"]["deploy"],
-    "run"    : meta_src["requirements"]["run"],
-    "test"   : meta_src["test"]["requires"],
-}
+    os.environ['CONDA_BUILD_STATE'] = 'RENDER'
 
-spec = []
-for name in sys.argv[1:]:
-    spec += section[name]
+    # make setup.py available for import
+    import_path = Path(__file__).parent.parent.absolute()
+    sys.path.append(str(import_path))
 
-# bare python unpins python version causing upgrade to latest
-if 'python' in spec: spec.remove('python')
+    # import setup.py and get keyword args
+    with mock.patch.object(setuptools, 'setup') as mock_setup:
+        import setup
 
-# add double quotes to specs for windows, fixes #9065
-if "windows" in platform.platform().lower():
-    spec = ['"{}"'.format(s) for s in spec]
+    return mock_setup.call_args[1]
 
-deps = ""
-deps += " ".join(s for s in spec)
-deps = deps.replace(' >=', '>=')  # conda syntax doesn't allow spaces b/w pkg name and version spec
-deps = deps.replace(' <', '<')
-deps = deps.replace(' [unix]', ' ')
 
-print(deps)
+def get_meta_src():
+    """Read conda meta.yaml to access all available dependency information.
+
+    """
+
+    meta_src = jinja2.Template(open("conda.recipe/meta.yaml").read())
+    rendered = meta_src.render(load_setup_py_data=load_setup_py_data)
+
+    try:
+        meta_src = yaml.load(rendered, Loader=yaml.FullLoader)
+
+    except AttributeError as e:
+        # Loader=yaml.FullLoader added in pyyaml 5.1 because of:
+        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
+        # isn't available on conda for python=3.5
+        # fall back to calling without loader if it isn't available
+        if 'FullLoader' in repr(e):
+            meta_src = yaml.load(rendered)
+        else:
+            raise
+
+    finally:
+        return meta_src
+
+
+def get_dependency_sections():
+    """Collect and compose dependency information into different sections
+    regarding their intended use case (build, deploy, run, test, all).
+
+    """
+
+    meta_src = get_meta_src()
+
+    section = {
+        "build"  : meta_src["requirements"]["build"],
+        "deploy" : meta_src["extra"]["deploy"],
+        "run"    : meta_src["requirements"]["run"],
+        "test"   : meta_src["test"]["requires"],
+    }
+
+    section["all"] = reduce(add, section.values())
+
+    return section
+
+
+def get_package_list(sections=None):
+    """Retrieve all dependencies from given section identifiers.
+
+    """
+
+    section = get_dependency_sections()
+    sections = sections or sys.argv[1:]
+
+    spec = []
+    for name in sections:
+        spec += section[name]
+
+    # bare python unpins python version causing upgrade to latest
+    if 'python' in spec: spec.remove('python')
+
+    # add double quotes to specs for windows, fixes #9065
+    if "windows" in platform.platform().lower():
+        spec = ['"{}"'.format(s) for s in spec]
+
+    def sanitize(dep):
+        return dep.replace(' >=', '>=')\
+                  .replace(' <', '<')\
+                  .replace(' [unix]', ' ')
+
+    return [sanitize(dep) for dep in spec]
+
+
+if __name__ == "__main__":
+    print(" ".join(get_package_list()))
