84c6633b2322fc460f0e30f4cbd6ae80a421cdc1
Mateusz Paprocki <mattpap@gmail.com>
2019-10-30 15:14:57 +0100

    Assert that ES2017 code compiles

--- bokehjs/src/lib/core/dom.ts
+++ bokehjs/src/lib/core/dom.ts
@@ -232,7 +232,7 @@ export function content_size(el: HTMLElement): Size {
   const {padding} = extents(el)
   let width = 0
   let height = 0
-  for (const child of children(el)) {
+  for (const child of el.children) {
     const rect = child.getBoundingClientRect()
     width = Math.max(width, Math.ceil(rect.left - left - padding.left + rect.width))
     height = Math.max(height, Math.ceil(rect.top - top - padding.top + rect.height))
