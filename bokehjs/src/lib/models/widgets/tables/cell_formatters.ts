667e244b3a9f4934b6127616c6d4435200ae5299
Harm Buisman <h.buisman@iknl.nl>
2019-10-30 22:09:24 +0100

    9403: Processed code review comments for ScientificFormatter

--- bokehjs/src/lib/models/widgets/tables/cell_formatters.ts
+++ bokehjs/src/lib/models/widgets/tables/cell_formatters.ts
@@ -6,7 +6,7 @@ import * as p from "core/properties"
 import {div, i} from "core/dom"
 import {Color} from "core/types"
 import {FontStyle, TextAlign, RoundingFunction} from "core/enums"
-import {isNumber, isString} from "core/util/types"
+import {isString} from "core/util/types"
 import {Model} from "../../../model"
 
 export namespace CellFormatter {
@@ -85,7 +85,7 @@ export namespace ScientificFormatter {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = StringFormatter.Props & {
-    precision: p.Property<number | 10>
+    precision: p.Property<number>
     power_limit_high: p.Property<number>
     power_limit_low: p.Property<number>
   }
@@ -102,7 +102,7 @@ export abstract class ScientificFormatter extends StringFormatter {
 
   static init_ScientificFormatter(): void {
     this.define<ScientificFormatter.Props>({
-      precision:        [ p.Any,     10     ],
+      precision:        [ p.Number,  10     ],
       power_limit_high: [ p.Number,  5      ],
       power_limit_low:  [ p.Number,  -3     ],
     })
@@ -117,20 +117,18 @@ export abstract class ScientificFormatter extends StringFormatter {
   }
 
   doFormat(row: any, cell: any, value: any, columnDef: any, dataContext: any): string {
-    let need_sci = value >= this.scientific_limit_high || value <= this.scientific_limit_low
+    const need_sci = value <= this.scientific_limit_low || value >= this.scientific_limit_high
     let precision = this.precision
 
-    if (precision == null || isNumber(precision)) {
-      // toExponential does not handle precision values < 0 correctly
-      if (precision < 1) {
-        precision = 1
-      }
+    // toExponential does not handle precision values < 0 correctly
+    if (precision < 1) {
+      precision = 1
+    }
 
-      if (need_sci) {
-        value = value.toExponential(precision || undefined)
-      } else {
-        value = value.toFixed(precision || undefined).replace(/(\.[0-9]*?)0+$/, "$1").replace(/\.$/, "")
-      }
+    if (need_sci) {
+      value = value.toExponential(precision)
+    } else {
+      value = value.toFixed(precision).replace(/(\.[0-9]*?)0+$/, "$1").replace(/\.$/, "")
     }
 
     // add StringFormatter formatting
