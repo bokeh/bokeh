eb612711d83eaa9a8d041f2e0c478298c74c86a6
Mateusz Paprocki <mattpap@gmail.com>
2019-11-01 17:15:53 +0100

    Revert changes to examples/{spectrogram,surfce3d}

--- examples/app/spectrogram/waterfall.py
+++ examples/app/spectrogram/waterfall.py
@@ -1,7 +1,11 @@
+from os.path import dirname, join
+
 from bokeh.core.properties import Color, Float, Int, Override, Seq
 from bokeh.models import Renderer
 
 class WaterfallRenderer(Renderer):
+
+    __implementation__ = join(dirname(__file__), "waterfall.ts")
 
     latest = Seq(Float)
 
