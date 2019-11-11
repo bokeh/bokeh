52fd59cb723bf31558efb03b0fa54e09cf63e62a
Bryan Van de Ven <bryan@bokeh.org>
2019-11-10 20:01:49 -0800

    use only bokeh.models for integration

--- examples/integration/widgets/tabs_with_multiselect.py
+++ examples/integration/widgets/tabs_with_multiselect.py
@@ -1,6 +1,5 @@
 from bokeh.io import save
-from bokeh.models import Tabs
-from bokeh.models.widgets import MultiSelect
+from bokeh.models import MultiSelect, Tabs
 
 select = MultiSelect(options=["First option", "Second option"])
 tabs = Tabs(tabs=[("A tab", select)], width=300)
