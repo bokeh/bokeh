036366f809a204d7ef953ac757d5b03a82b33b7d
Bryan Van de Ven <bryan@bokeh.org>
2019-11-20 13:16:11 -0800

    narrow isort check dirs

--- tests/codebase/test_isort.py
+++ tests/codebase/test_isort.py
@@ -34,7 +34,11 @@ def test_isort():
 
     """
     chdir(TOP_PATH)
-    proc = Popen(["isort", "-rc", "-c", "."], stdout=PIPE, stderr=PIPE)
+    proc = Popen(
+        ["isort", "-rc", "-c", "bokeh", "tests", "sphinx", "examples"],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
     out, _ = proc.communicate()
     assert proc.returncode == 0, "isort issues:\n%s" % out.decode("utf-8")
 
