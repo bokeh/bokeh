c4d25ec7d685592bcd26d8513236025839bea2d3
Bryan Van de Ven <bryan@bokeh.org>
2019-11-26 13:47:03 -0800

    don't include Examples dir in conda pkg

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,8 +1,2 @@
 python setup.py --quiet install --build-js --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
-
-mkdir %PREFIX%\Examples
-if errorlevel 1 exit 1
-
-copy /Y examples %PREFIX%\Examples\bokeh
-if errorlevel 1 exit 1
