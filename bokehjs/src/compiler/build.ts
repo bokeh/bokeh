5868f5bec0f21d01ae3493acd5c964de8935b9a8
Mateusz Paprocki <mattpap@gmail.com>
2019-11-26 11:20:16 +0100

    Special case tslib in extensions' build

--- bokehjs/src/compiler/build.ts
+++ bokehjs/src/compiler/build.ts
@@ -307,7 +307,7 @@ export async function build(base_dir: Path, bokehjs_dir: Path, base_setup: Build
     entries: [join(lib_dir, "index.js")],
     bases,
     cache: join(dist_dir, `${artifact}.json`),
-    excluded: (dep) => dep.startsWith("@bokehjs/"),
+    excluded: (dep) => dep == "tslib" || dep.startsWith("@bokehjs/"),
     plugin: true,
     transpile: "ES2017",
   })
