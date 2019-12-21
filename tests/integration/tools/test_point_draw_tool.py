8bbc1d33ea7f1b32e43d662de0b418bc63e37ceb
Bryan Van de Ven <bryan@bokeh.org>
2019-12-21 13:37:00 -0800

    fix skip mark

--- tests/integration/tools/test_point_draw_tool.py
+++ tests/integration/tools/test_point_draw_tool.py
@@ -201,7 +201,7 @@ class Test_PointDrawTool(object):
         assert page.results == {"matches": "True"}
 
     # TODO (bev) Fix up after GH CI switch
-    @pytest.mark.ski
+    @pytest.mark.skip
     def test_point_drag_syncs_to_server(self, bokeh_server_page):
         expected = {"x": [1, 2, 2.1891891891891895],
                     "y": [1, 1, 1.1024999999999998]}
