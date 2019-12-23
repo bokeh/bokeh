618f8edf9086cfd97d4e3b5a9ed07025137bde19
Bryan Van de Ven <bryan@bokeh.org>
2019-12-22 16:56:13 -0800

    remove old ci cruft

--- scripts/ci/github/install_bokeh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source scripts/ci/github/setup_env.sh
-
-$PYTHON_BIN setup.py install -q --build-js
--- scripts/ci/install.js
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e # exit on error
-set -x # echo commands
-
-# install NPM dependencies
-pushd bokehjs
-npm ci --no-progress
-popd
