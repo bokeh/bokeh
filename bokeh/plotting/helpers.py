cf23304747dc83f0378b09ac56742604a0250eda
Bryan Van de Ven <bryan@bokeh.org>
2020-01-17 15:18:51 -0800

    update out-of-date other parameters docs

--- bokeh/plotting/helpers.py
+++ bokeh/plotting/helpers.py
@@ -912,13 +912,114 @@ Keyword Args:
 %s
 
 Other Parameters:
-    alpha (float) : an alias to set all alpha keyword args at once
-    color (Color) : an alias to set all color keyword args at once
-    source (ColumnDataSource) : a user supplied data source
-    legend (str) : a legend tag for this glyph
-    x_range_name (str) : name an extra range to use for mapping x-coordinates
-    y_range_name (str) : name an extra range to use for mapping y-coordinates
-    level (Enum) : control the render level order for this glyph
+    alpha (float, optional) :
+        An alias to set all alpha keyword arguments at once. (default: None)
+
+        Alpha values must be between 0 (fully transparent) and 1 (fully opaque).
+
+        Any explicitly set values for ``line_alpha``, etc. will override this
+        setting.
+
+    color (Color, optional) :
+        An alias to set all color keyword arguments at once. (default: None)
+
+        Acceptable values for colors are described in the `Specifying Colors`_
+        section of the User's Guide.
+
+        Any explicitly set values for ``line_color``, etc. will override this
+        setting.
+
+        .. _Specifying Colors: https://docs.bokeh.org/en/latest/docs/user_guide/styling.html#specifying-colors
+
+    legend_field (str, optional) :
+        Specify that the glyph should produce multiple legend entried by
+        `Grouping in the Browser`_. The value of this parameter is the name of a
+        column in the data source that should be used or the grouping.
+
+        The grouping is performed *in JavaScript*, at the time time the Bokeh
+        content is rendered in the browser. If the data is subsequently updated,
+        the legend will automatically re-group.
+
+        .. note::
+            Only one of ``legend_field``, ``legend_group``, or ``legend_label``
+            should be supplied
+
+        .. _Grouping in the Browser: https://docs.bokeh.org/en/latest/docs/user_guide/annotations.html#automatic-grouping-browser
+
+    legend_group (str, optional) :
+        Specify that the glyph should produce multiple legend entried by
+        `Grouping in Python`_. The value of this parameter is the name of a
+        column in the data source that should be used or the grouping.
+
+        The grouping is performed in Python, before the Bokeh output is sent to
+        a browser. If the date is subsequently updated, the legend will *not*
+        automatically re-group.
+
+        .. note::
+            Only one of ``legend_field``, ``legend_group``, or ``legend_label``
+            should be supplied
+
+        .. _Grouping in Python: https://docs.bokeh.org/en/latest/docs/user_guide/annotations.html#automatic-grouping-python
+
+    legend_label (str, optional) :
+        Specify that the glyph should produce a single `Basic Legend Label`_ in
+        the legend. The legend entry is labeled with exactly the text supplied
+        here.
+
+        .. note::
+            Only one of ``legend_field``, ``legend_group``, or ``legend_label``
+            should be supplied
+
+        .. _Basic Legend Label: https://docs.bokeh.org/en/latest/docs/user_guide/annotations.html#basic-legend-label
+
+    muted (bool, optionall) :
+        Whether the glyph should be rendered as muted (default: False)
+
+        For this to be useful, an ``muted_glyph`` must be configured on the
+        returned ``GlyphRender``. This can be done by explicitly creating a
+        ``Glyph`` to use, or more simply by passing e.g. ``muted_color``, etc.
+        to this glyph function.
+
+    name (str, optional) :
+        An optional user-supplied name to attach to the renderer (default: None)
+
+        Bokeh does not use this value in any way, but it may be useful for
+        searching a Bokeh document to find a specific model.
+
+    source (ColumnDataSource, optional) :
+        A user-supplied data source. (defatult: None)
+
+        If not supplied, Bokeh will automatically construct an internal
+        ``ColumnDataSource`` with default column names from the coordinates and
+        other arguments that were passed-in as literal list or array values.
+
+        If supplied, Bokeh will use the supplied data source to drive the glyph.
+        In this case, literal list or arrays may not be used for coordinates or
+        other arguments. Only singular fixed valued (e.g. ``x=10``) or column
+        names in the data souce (e.g. ``x="time"``) are permitted.
+
+    view (CDSView, optional) :
+        A view for filtering the data source. (default: None)
+
+    visible (bool, optional) :
+        Whether the glyph should be rendered. (default: True)
+
+    x_range_name (str, optional) :
+        The name of an extra range to use for mapping x-coordinates.
+        (default: None)
+
+        If not supplied, then the default ``y_range`` of the plot will be used
+        for coordinate mapping.
+
+    y_range_name (str, optional) :
+        The name of an extra range to use for mapping y-coordinates.
+        (default: None)
+
+        If not supplied, then the default ``y_range`` of the plot will be used
+        for coordinate mapping.
+
+    level (RenderLevel, optional) :
+        Specify the render level order for this glyph.
 
 It is also possible to set the color and alpha parameters of a "nonselection"
 glyph. To do so, prefix any visual parameter with ``'nonselection_'``.
@@ -926,20 +1027,19 @@ For example, pass ``nonselection_alpha`` or ``nonselection_fill_alpha``.
 
 Returns:
     GlyphRenderer
+
 """
 
 def _add_sigfunc_info(func, argspecs, glyphclass, extra_docs):
     func.__name__ = glyphclass.__name__
 
+    # these are not really useful, and should also really be private, just skip them
     omissions = {'js_event_callbacks', 'js_property_callbacks', 'subscribed_events'}
 
     kwlines = []
-    kws = glyphclass.properties() - set(argspecs)
+    kws = glyphclass.properties() - set(argspecs) - omissions
     for kw in kws:
-        # these are not really useful, and should also really be private, just skip them
-        if kw in omissions: continue
-
-        descriptor = getattr(glyphclass, kw)
+        descriptor = glyphclass.lookup(kw)
         typ = descriptor.property._sphinx_type()
         if descriptor.__doc__:
             desc = "\n        ".join(textwrap.dedent(descriptor.__doc__).split("\n"))
@@ -964,13 +1064,13 @@ def _glyph_function(glyphclass, extra_docs=None):
 
     def func(self, **kwargs):
 
-        # Convert data source, if necessary
+        # convert data source, if necessary
         is_user_source = kwargs.get('source', None) is not None
         if is_user_source:
             source = kwargs['source']
             if not isinstance(source, ColumnarDataSource):
                 try:
-                    # try converting the soruce to ColumnDataSource
+                    # try converting the source to ColumnDataSource
                     source = ColumnDataSource(source)
                 except ValueError as err:
                     msg = "Failed to auto-convert {curr_type} to ColumnDataSource.\n Original error: {err}".format(
@@ -979,7 +1079,7 @@ def _glyph_function(glyphclass, extra_docs=None):
                     )
                     raise ValueError(msg).with_traceback(sys.exc_info()[2])
 
-                # update reddered_kws so that others can use the new source
+                # update kwargs so that others can use the new source
                 kwargs['source'] = source
 
         # Save off legend kwargs before we get going
