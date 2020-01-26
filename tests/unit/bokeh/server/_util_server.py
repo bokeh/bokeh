6639b36f931e3dc0790b8a00a3c40c1e93c25f7a
Mateusz Paprocki <mattpap@gmail.com>
2020-01-26 23:11:39 +0100

    Update license and copyright dates (#9610)

--- tests/unit/bokeh/server/_util_server.py
+++ tests/unit/bokeh/server/_util_server.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2012 - 2019, Anaconda, Inc., and Bokeh Contributors.
+# Copyright (c) 2012 - 2020, Anaconda, Inc., and Bokeh Contributors.
 # All rights reserved.
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
