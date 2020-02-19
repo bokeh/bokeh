e07caf65034ac4b1c9b44434ab5d295d4de93091
Bryan Van de Ven <bryan@bokeh.org>
2020-02-19 08:49:26 -0800

    Bryanv/8397 sri script tags (#9627)
    * add script integrirty hashes to released CDN resources
    
    * run unit tests by directory, not -m flag
    
    * basic tests
    
    * indentation
    
    Co-authored-by: Mateusz Paprocki <mattpap@gmail.com>

--- tests/unit/bokeh/test_resources.py
+++ tests/unit/bokeh/test_resources.py
@@ -22,6 +22,7 @@ import re
 from packaging.version import Version as V
 
 # Bokeh imports
+import bokeh.util.version as buv
 from bokeh import __version__
 from bokeh.models import Model
 from bokeh.resources import _get_cdn_urls
@@ -103,6 +104,22 @@ def test_js_resources_inline_has_no_css_resources() -> None:
     assert hasattr(r, "css_raw") is False
     assert r.messages == []
 
+def test_js_resources_hashes_mock_full(monkeypatch) -> None:
+    monkeypatch.setattr(buv, "__version__", "1.4.0")
+    monkeypatch.setattr(resources, "__version__", "1.4.0")
+    r = resources.JSResources()
+    assert r.mode == "cdn"
+    min_hashes = set(v for k, v in resources.get_sri_hashes_for_version("1.4.0").items() if k.endswith(".min.js") and "api" not in k)
+    assert set(r.hashes.values()) == min_hashes
+
+@pytest.mark.parametrize('v', ["1.4.0dev6", "1.4.0rc1", "1.4.0dev6-50-foo"])
+@pytest.mark.unit
+def test_js_resources_hashes_mock_non_full(v, monkeypatch) -> None:
+    monkeypatch.setattr(buv, "__version__", v)
+    monkeypatch.setattr(resources, "__version__", v)
+    r = resources.JSResources()
+    assert r.mode == "cdn"
+    assert r.hashes == None
 
 ## Test CSSResources
 
