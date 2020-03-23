8a1bde500fe425156c1e7aad0ba3081b055bcdae
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-03-23 14:22:31 -0400

    straightening out extra lock file, I hope

--- examples/tools/structure_graph.py
+++ examples/tools/structure_graph.py
@@ -1,7 +1,7 @@
 from bokeh.io import show
+from bokeh.layouts import column
 from bokeh.plotting import figure
 from bokeh.util import structure
-from bokeh.layouts import column
 
 # draw the structure graph of a basic figure model
 f = figure(width=400,height=400)
