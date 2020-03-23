bb59f64b2f5d161e451d47fa184cf593e6531f95
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-03-23 13:06:08 -0400

    updated README and notebook

--- bokeh/util/structure.py
+++ bokeh/util/structure.py
@@ -1,132 +1,315 @@
-#-----------------------------------------------------------------------------
-# Copyright (c) 2012 - 2020, Anaconda, Inc., and Bokeh Contributors.
-# All rights reserved.
-#
-# The full license is in the file LICENSE.txt, distributed with this software.
-#-----------------------------------------------------------------------------
-'''Functions to create the directed acyclic graph of submodels of a model in networkx format,
-   and to draw that DAG using bokeh graph rendering.
-'''
+"""Functions to create the graph of submodels of a model in networkx format,
+   and to draw that graph using bokeh so that one can explore the attributes of
+   submodels by clicking on the nodes.  Uses simple custom javascript callbacks so no server is necessary;
+   works in a jupyter notebook.
 
+   Suppose M is a bokeh model (i.e. a plot or figure).  
 
-#-----------------------------------------------------------------------------
-# Boilerplate
-#-----------------------------------------------------------------------------
-import logging # isort:skip
-log = logging.getLogger(__name__)
-
-
-#-----------------------------------------------------------------------------
-# Imports
-#-----------------------------------------------------------------------------
+      - K = BokehStructureGraph(M) creates the graph, which is itself a bokeh model.
+      - K.show() calls show on the model to display it.
+      - K.model returns the model consisting of the structure graph and associated datatable.
+      - K.graph returns the directed graph in networkx format
+      - K.property_df returns the dataframe of models, submodels and their properties.      
+"""
 
 # Standard library imports
 from itertools import permutations
 
 # External imports
 import networkx as nx
+import pandas as pd
 
 # Bokeh imports
-from bokeh.models import ColumnDataSource, HoverTool, LabelSet, Range1d
-from bokeh.models.glyphs import Circle, MultiLine
-from bokeh.plotting import figure, from_networkx
+from bokeh.layouts import column
+from bokeh.models import (
+    BoxZoomTool,
+    CDSView,
+    Circle,
+    ColumnDataSource,
+    CustomJS,
+    DataTable,
+    GlyphRenderer,
+    GroupFilter,
+    HoverTool,
+    Label,
+    LabelSet,
+    MultiLine,
+    Plot,
+    PanTool,
+    Range1d,
+    ResetTool,
+    TableColumn,
+    TapTool,
+)
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # General API
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-def make_graph(M):
-    """Return a networkx DiGraph() G so that:
-       G.nodes are the submodels of M, with node attributes
-           - "model" giving the class name of of the submodel
-           - "id" giving the id of the submodel
-           - "in" giving the attribute of the parent model to which this submodel is attached
-       Two nodes are joined by an edge if the head node is a member of (or equal to) an attribute of the tail node.
+__all__ = ['BokehStructureGraph']
 
-    Args:
-        A bokeh model M
+class BokehStructureGraph:
+    """ Class for exploring the graph of submodels of a Bokeh model.
+
+        If M is such a model and X=BokehStructureGraph(M), then:
+
+           - X.model is the interactive model itself of
+           the submodel graph; pass it to show or file_html.  Self contained so
+           works in a jupyter notebook, no server needed.
+
+           - X.graph is the networkx DiGraph of submodels.
+
+           - X.property_df is a pandas dataframe including all of the properties of
+           all of the submodels, with their docstrings.  Used for the datatable
+           displayed when clicking on X.model
     """
-    G = nx.DiGraph()
 
-    T = {}
-    for m in M.references():
-        T[m.id] = set([y.id for y in m.references()])
+    def __init__(self, model):
 
-    G.add_nodes_from(
-        [
-            (x, {"model": M.select_one({"id": x}).__class__.__name__})
-            for x in T
+        self._model = model
+        self._graph = self._make_graph(model)
+        self._graph.graph["graph"] = {"rankdir": "LR"}
+        self._prop_df = self._make_prop_dict()
+        self._graph_plot = self.make_graph_plot()
+        self._data_table = self._make_data_table()
+        self._graph_plot.title.text = "Structure of model type {} with id {}".format(
+            self._model.__class__.__name__, self._model.id
+        )
+        self._structure_graph = self._combined()
+
+    @property
+    def model(self):
+        """
+        The bokeh model consisting of the structure graph and the datatable
+        for the attributes. Can be passed to show or file_html. Self contained,
+        so remains interactive in a notebook or html file; no server needed.
+        """
+        return self._structure_graph
+
+    @property
+    def graph(self):
+        """
+        The networkx digraph of submodels of the given model.
+        """
+        return self._graph
+
+    @property
+    def property_df(self):
+        """
+        A pandas dataframe of all of the properties of the model with their
+        values, types, and docstrings.  The base information for the datatable.
+        """
+        return self._prop_df
+
+    def _make_graph(self, M):
+        """Builds a networkx DiGraph() G so that:
+           G.nodes are the submodels of M, with node attributes
+               - "model" giving the class name of of the submodel
+               - "id" giving the id of the submodel
+            An edge runs from a to b if the submodel b belongs to an attribute of
+            a
+
+        Args:
+            A bokeh model M
+        """
+        def test_condition(s, y, H):
+            answer1 = False
+            answer2 = False
+            answer3 = False
+            try:
+                answer1 = (s in getattr(H, y))
+            except TypeError:
+                pass
+            try:
+                answer2 = (s == getattr(H, y))
+            except TypeError:
+                pass
+            try:
+                answer3 = (s in getattr(H, y).values())
+            except (AttributeError, ValueError):
+                pass
+            return (answer1 | answer2 | answer3)
+
+        K = nx.DiGraph()
+        T = {}
+        for m in M.references():
+            T[m.id] = set([y.id for y in m.references()])
+
+        K.add_nodes_from(
+            [(x, {"model": M.select_one({"id": x}).__class__.__name__}) for x in T]
+        )
+        E = [(y, x) for x, y in permutations(T, 2) if T[x] <= T[y]]
+        K.add_edges_from(E)
+        dead_edges = []
+        for id in K.nodes:
+            H = M.select_one({"id": id})
+            for x in K.neighbors(id):
+                s = H.select_one({"id": x})
+                keep_edge = False
+                for y in H.properties():
+                    if test_condition(s, y, H):
+                        keep_edge = True
+                if not keep_edge:
+                    dead_edges.append((id, x))
+        K.remove_edges_from(dead_edges)
+        return K
+
+    def obj_props_to_df2(self, obj):
+        """Returns a pandas dataframe of the properties of a bokeh model, each row having
+        an attribute, its type (a bokeh property), and the docstring."""
+        obj_dict = obj.properties_with_values()
+        types = [obj.lookup(x) for x in obj_dict.keys()]
+        docs = [getattr(type(obj), x).__doc__ for x in obj_dict.keys()]
+        df = {
+            "props": list(obj_dict.keys()),
+            "values": list(obj_dict.values()),
+            "types": types,
+            "doc": docs,
+        }
+        return df
+
+    def make_graph_plot(self):
+        """
+        Builds the graph portion of the final model from the graph constructed
+        by make_graph.  
+        """
+        nodes = nx.nx_pydot.graphviz_layout(self._graph, prog="dot")
+        node_x, node_y = zip(*nodes.values())
+        models = [self._graph.nodes[x]["model"] for x in nodes]
+        node_id = list(nodes.keys())
+        node_source = ColumnDataSource(
+            {"x": node_x, "y": node_y, "index": node_id, "model": models}
+        )
+        edge_x_coords = []
+        edge_y_coords = []
+        for start_node, end_node in self._graph.edges:
+            edge_x_coords.extend([[nodes[start_node][0], nodes[end_node][0]]])
+            edge_y_coords.extend([[nodes[start_node][1], nodes[end_node][1]]])
+        edge_source = ColumnDataSource({"xs": edge_x_coords, "ys": edge_y_coords})
+
+        p2 = Plot(outline_line_alpha=0.0)
+        xinterval = max(max(node_x) - min(node_x), 200)
+        yinterval = max(max(node_y) - min(node_y), 200)
+        p2.x_range = Range1d(
+            start=min(node_x) - 0.15 * xinterval, end=max(node_x) + 0.15 * xinterval
+        )
+        p2.y_range = Range1d(
+            start=min(node_y) - 0.15 * yinterval, end=max(node_y) + 0.15 * yinterval
+        )
+
+        node_renderer = GlyphRenderer(
+            data_source=node_source,
+            glyph=Circle(x="x", y="y", size=15, fill_color="lightblue"),
+            nonselection_glyph=Circle(x="x", y="y", size=15, fill_color="lightblue"),
+            selection_glyph=Circle(x="x", y="y", size=15, fill_color="green"),
+        )
+
+        edge_renderer = GlyphRenderer(
+            data_source=edge_source, glyph=MultiLine(xs="xs", ys="ys")
+        )
+
+        node_hover_tool = HoverTool(tooltips=[("id", "@index"), ("model", "@model")])
+        node_hover_tool.renderers = [node_renderer]
+
+        tap_tool = TapTool()
+        tap_tool.renderers = [node_renderer]
+
+        labels = LabelSet(
+            x="x",
+            y="y",
+            text="model",
+            source=node_source,
+            text_font_size="8pt",
+            x_offset=-20,
+            y_offset=7,
+        )
+
+        help = Label(
+            x=20,
+            y=20,
+            x_units="screen",
+            y_units="screen",
+            text_font_size="8pt",
+            text_font_style="italic",
+            text="Click on a model to see its attributes",
+        )
+        p2.add_layout(help)
+        p2.add_layout(edge_renderer)
+        p2.add_layout(node_renderer)
+        p2.tools.extend(
+            [node_hover_tool, tap_tool, BoxZoomTool(), ResetTool(), PanTool()]
+        )
+        p2.renderers.append(labels)
+        self._node_source = node_source
+        self._edge_source = edge_source
+        return p2
+
+    def _make_prop_dict(self):
+        """
+        Creates a dataframe of all the properties of all the
+        submodels of the model being
+        analyzed. Used as datasource to show attributes.
+        """
+        df = pd.DataFrame()
+        for x in self._graph.nodes(data=True):
+            M = self._model.select_one({"id": x[0]})
+            Z = pd.DataFrame(self.obj_props_to_df2(M))
+            Z["id"] = x[0]
+            Z["model"] = str(M)
+            Z["values"] = Z["values"].map(lambda x: str(x))
+            Z["types"] = Z["types"].map(lambda x: str(x))
+            df = df.append(Z)
+        return df
+
+    def _make_data_table(self):
+        """
+        Builds the datatable portion of the final plot.
+        """
+        columns = [
+            TableColumn(field="props", title="Property"),
+            TableColumn(field="values", title="Value"),
         ]
-    )
-    E = [(y, x) for x, y in permutations(T, 2) if T[x].issubset(T[y])]
-    G.add_edges_from(E)
-    K = nx.algorithms.dag.transitive_reduction(G)
-    nx.set_node_attributes(K, dict(G.nodes(data="model")), "model")
-    nx.set_node_attributes(K, "root", "in")
-    for id in K.nodes:
-        H = M.select_one({"id": id})
-        for x in K.neighbors(id):
-            for y in H.properties():
-                if y in H.properties_containers():
-                    if H.select_one({"id": x}) in getattr(H, y):
-                        K.nodes[x]["in"] = y
-                else:
-                    if getattr(H, y) == H.select_one({"id": x}):
-                        K.nodes[x]["in"] = y
-    return K
+        prop_source = ColumnDataSource(self._prop_df)
+        model_id = self._node_source.data["index"][0]
+        groupfilter = GroupFilter(column_name="id", group=model_id)
 
+        data_table2_view = CDSView(source=prop_source, filters=[groupfilter])
+        data_table2 = DataTable(
+            source=prop_source,
+            view=data_table2_view,
+            columns=columns,
+            visible=False,
+            index_position=None,
+            fit_columns=True,
+            editable=False,
+        )
 
-def draw_model(M):
-    """Returns a Bokeh Plot Model that draws the directed graph constructed by make_graph
-    using the Bokeh graph plotting primitives.  Typically one would use show() to view the result.
+        self._groupfilter = groupfilter
+        self._prop_source = prop_source
+        return data_table2
 
-    Args:
-       A bokeh model M
-    """
-    K = make_graph(M)
-    plot = figure(title="Structure Graph for Bokeh Model", height=600, width=600)
+    def _combined(self):
+        """
+        Connects the graph and the datatable with a simple CustomJS callback
+        so that clicking on a node/submodel narrows the view in the datatable to the
+        attributes associated with that submodel.
+        """
 
-    plot.xaxis.visible = False
-    plot.yaxis.visible = False
-    plot.xgrid.visible = False
-    plot.ygrid.visible = False
+        js_code = """const index = node_source.selected.indices[0];
+            f['group']=node_source.data['index'][index];
+            table['visible']=true;
+            prop_source.change.emit();"""
 
-    K.graph["graph"] = {"rankdir": "LR"}
+        js = CustomJS(
+            args=dict(
+                node_source=self._node_source,
+                prop_source=self._prop_source,
+                f=self._groupfilter,
+                table=self._data_table,
+            ),
+            code=js_code,
+        )
 
-    graph_renderer = from_networkx(
-        K, layout_function=nx.nx_pydot.graphviz_layout, prog="dot"
-    )
-    graph_renderer.node_renderer.glyph = Circle(
-        radius=4, fill_alpha=1, fill_color="lightblue"
-    )
-
-    graph_renderer.edge_renderer.glyph = MultiLine(line_width=3, line_color="#ababab")
-    plot.renderers = [graph_renderer]
-
-    node_hover_tool = HoverTool(
-        tooltips=[("id", "@index"), ("model", "@model"), ("in", "@in")]
-    )
-    plot.add_tools(node_hover_tool)
-
-    x, y = zip(*graph_renderer.layout_provider.graph_layout.values())
-    xinterval = max(max(x) - min(x),200)
-    yinterval = max(max(y) - min(y),200)
-    plot.x_range = Range1d(
-        start=min(x) - 0.15 * xinterval, end=max(x) + 0.15 * xinterval
-    )
-    plot.y_range = Range1d(
-        start=min(y) - 0.15 * yinterval, end=max(y) + 0.15 * yinterval
-    )
-    D = {"x": x, "y": y, "model": list(dict(K.nodes(data="model")).values())}
-    labels = LabelSet(
-        x="x",
-        y="y",
-        text="model",
-        source=ColumnDataSource(D),
-        text_font_size="8pt",
-        x_offset=-20,
-        y_offset=7,
-    )
-    plot.renderers.append(labels)
-
-    return plot
+        self._node_source.selected.js_on_change("indices", js)
+        layout = column(self._graph_plot, self._data_table)
+        return layout
--- examples/tools/structure_graph.py
+++ examples/tools/structure_graph.py
@@ -1,7 +1,10 @@
 from bokeh.io import show
 from bokeh.plotting import figure
 from bokeh.util import structure
+from bokeh.layouts import column
 
 # draw the structure graph of a basic figure model
-f=figure()
-show(structure.draw_model(f))
+f = figure(width=400,height=400)
+f.line(x=[1,2,3],y=[1,2,3])
+K = structure.BokehStructureGraph(f)
+show(column(f,K.model))
