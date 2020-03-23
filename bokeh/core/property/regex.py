8a3c662b85aaba32ad36912a8aef7c216a2a6693
ayush <ayush.shrivastava2901@gmail.com>
2020-03-23 15:53:10 +0530

    resolved a few issues

--- bokeh/core/property/regex.py
+++ bokeh/core/property/regex.py
@@ -103,7 +103,7 @@ class Base64String(String):
 
     '''
 
-    def serialize_value(value : str):
+    def serialize_value(self, value):
 
         value = base64.b64encode(value.encode("utf-8"))
         return value
