0e70d56475a371507eb24c8e29f9e0429f98ee27
Philipp Rudiger <prudiger@anaconda.com>
2020-04-13 14:46:03 +0200

    Temporary fix for Django server resource handling

--- bokeh/server/django/consumers.py
+++ bokeh/server/django/consumers.py
@@ -87,7 +87,7 @@ class ConsumerHelper(AsyncConsumer):
         return self.arguments.get(name, default)
 
     def resources(self, absolute_url: Optional[str] = None) -> Resources:
-        mode = settings.resources(default="server")
+        mode = settings.resources(default="cdn")
         if mode == "server":
             root_url = urljoin(absolute_url, self._prefix) if absolute_url else self._prefix
             return Resources(mode="server", root_url=root_url, path_versioner=StaticHandler.append_version)
