33ed6e6c45087b5cf34fa1ba7d676a2096e554dc
Mateusz Paprocki <mattpap@gmail.com>
2020-04-13 18:32:25 +0200

    Update howto/django_embed example

--- examples/howto/server_embed/django_embed/django_embed/settings.py
+++ examples/howto/server_embed/django_embed/django_embed/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'channels',
     'bokeh.server.django',
 ]
