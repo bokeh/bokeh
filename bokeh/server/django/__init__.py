66a39918145cdd5ff619e180940d8c1a2e3453bb
Mateusz Paprocki <mattpap@gmail.com>
2020-04-13 18:31:48 +0200

    Add support for serving extensions in django

--- bokeh/server/django/__init__.py
+++ bokeh/server/django/__init__.py
@@ -3,6 +3,7 @@ from bokeh.util.dependencies import import_required
 
 from .apps import DjangoBokehConfig
 from .routing import autoload, directory, document
+from .static import static_extensions
 
 import_required("django", "django is required by bokeh.server.django")
 import_required("channels", "The package channels is required by bokeh.server.django and must be installed")
