f70c152bb4659df52a5490b216ac972184a186b4
Bryan Van de Ven <bryan@bokeh.org>
2020-04-18 16:59:50 -0700

    make versioneer style consistent with previous

--- bokeh/_version.py
+++ bokeh/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "git-describe"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "Bokeh-"
     cfg.versionfile_source = "bokeh/_version.py"
