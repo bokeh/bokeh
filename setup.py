ec919f1fa0eeb66ebae83cde20b99ca488087c18
Bryan Van de Ven <bryan@bokeh.org>
2020-04-19 11:19:49 -0700

    fix header in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2012 - 2017, Anaconda, Inc. All rights reserved.
-#
-# Powered by the Bokeh Development Team.
+# Copyright (c) 2012 - 2020, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
