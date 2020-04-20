908f17bbecf53dcde83f7171c034376f39740cd1
Bryan Van de Ven <bryan@bokeh.org>
2020-04-19 18:08:25 -0700

    release build first steps (#9945)

--- release/__main__.py
+++ release/__main__.py
@@ -8,21 +8,4 @@
 # Standard library imports
 import sys
 
-# Bokeh imports
-from .config import Config
-from .stages import BUILD_CHECKS, BUILD_STEPS
-from .system import System
-from .ui import banner, blue
-
-config = Config(sys.argv[1])
-system = System()
-
-banner(blue, "{:^80}".format("Starting a Bokeh release BUILD"))
-
-for step in BUILD_CHECKS:
-    step(config, system)
-
-for step in BUILD_STEPS:
-    step(config, system)
-
-banner(blue, "{:^80}".format(f"Bokeh {config.version!r} BUILD: SUCCESS"))
+print(f"GOT {sys.argv}")
