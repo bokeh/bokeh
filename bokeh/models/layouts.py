61272c123453a54517f9211bf86ae74f3f0914d2
naveensaigit <56525288+naveensaigit@users.noreply.github.com>
2020-05-08 17:02:49 +0530

    Update layouts.py

--- bokeh/models/layouts.py
+++ bokeh/models/layouts.py
@@ -115,13 +115,10 @@ class LayoutDOM(Model):
     """)
 
     margin = Tuple(Int, Int, Int, Int, default=(0, 0, 0, 0), help="""
-    Allows to create additional space around the component.
-    
+    Allows to create additional space around the component.    
     The values in the tuple are ordered as follows - Margin-Top, Margin-Right, Margin-Bottom and Margin-Left,
-    similar to CSS standards. 
-    
+    similar to CSS standards.
     Negative margins could be added too to shrink the space from any direction.
-    
     """).accepts(Tuple(Int, Int), lambda v_h: (v_h[0], v_h[1], v_h[0], v_h[1])) \
         .accepts(Int, lambda m: (m, m, m, m))
 
