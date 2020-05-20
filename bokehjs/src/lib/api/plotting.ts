d2f512aa3ee5db5a2f2bca0dddc63b279b1f7dbd
Mateusz Paprocki <mattpap@gmail.com>
2020-05-20 11:17:58 +0200

    Add support for CDSView to bokehjs' api/plotting

--- bokehjs/src/lib/api/plotting.ts
+++ bokehjs/src/lib/api/plotting.ts
@@ -14,7 +14,7 @@ import {clone} from "../core/util/object"
 import {isNumber, isString, isArray} from "../core/util/types"
 import {ViewOf} from "core/view"
 
-import {Glyph, Marker, GlyphRenderer, Axis, Grid, Range, Scale, Tool, Plot, ColumnarDataSource} from "./models"
+import {Glyph, Marker, GlyphRenderer, Axis, Grid, Range, Scale, Tool, Plot, ColumnarDataSource, CDSView} from "./models"
 import {ToolAliases} from "../models/tools/tool"
 
 import {LayoutDOM} from "models/layouts/layout_dom"
@@ -75,6 +75,7 @@ export type AuxText = {
 
 export type AuxGlyph = {
   source: ColumnarDataSource
+  view: CDSView
   legend: string
 }
 
@@ -699,9 +700,12 @@ export class Figure extends Plot {
       }
     }
 
-    const source = attrs.source != null ? (attrs.source as AuxGlyph["source"]) : new models.ColumnDataSource()
+    const source = attrs.source != null ? attrs.source as AuxGlyph["source"] : new models.ColumnDataSource()
     const data = clone(source.data)
     delete attrs.source
+
+    const view = attrs.view != null ? attrs.view as AuxGlyph["view"] : new CDSView({source})
+    delete attrs.view
 
     const legend = this._process_legend(attrs.legend as AuxGlyph["legend"], source)
     delete attrs.legend
@@ -734,6 +738,7 @@ export class Figure extends Plot {
 
     const glyph_renderer = new GlyphRenderer({
       data_source:        source,
+      view,
       glyph,
       nonselection_glyph: nsglyph,
       selection_glyph:    sglyph,
