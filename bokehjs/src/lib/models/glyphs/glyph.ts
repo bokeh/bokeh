8d50b389b651e48ca230c2b16ac2dfccd6a58cf8
Mateusz Paprocki <mattpap@gmail.com>
2020-05-20 10:10:13 +0200

    Set visuals' indices for each glyph of a renderer

--- bokehjs/src/lib/models/glyphs/glyph.ts
+++ bokehjs/src/lib/models/glyphs/glyph.ts
@@ -77,7 +77,8 @@ export abstract class GlyphView extends View {
     }
   }
 
-  set_visuals(source: ColumnarDataSource): void {
+  set_visuals(source: ColumnarDataSource, indices: number[]): void {
+    this.visuals.set_all_indices(indices)
     this.visuals.warm_cache(source)
 
     if (this.glglyph != null)
@@ -227,7 +228,6 @@ export abstract class GlyphView extends View {
   set_data(source: ColumnarDataSource, indices: number[], indices_to_update: number[] | null): void {
     let data = this.model.materialize_dataspecs(source)
 
-    this.visuals.set_all_indices(indices)
     if (indices && !(this instanceof LineView)) {
       const data_subset: {[key: string]: any} = {}
       for (const k in data) {
