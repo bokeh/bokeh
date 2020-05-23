78c0d9c14bf3c575869e299012b886feed9426fa
Mateusz Paprocki <mattpap@gmail.com>
2020-05-23 17:12:24 +0200

    Fix updating of MultiChoice widget

--- bokehjs/src/lib/models/widgets/multichoice.ts
+++ bokehjs/src/lib/models/widgets/multichoice.ts
@@ -1,7 +1,7 @@
 import Choices from "choices.js"
 import {Choices as ChoicesNS} from "choices.js"
 
-import {select, option} from "core/dom"
+import {select} from "core/dom"
 import {isString} from "core/util/types"
 import * as p from "core/properties"
 
@@ -18,15 +18,10 @@ export class MultiChoiceView extends InputWidgetView {
 
   connect_signals(): void {
     super.connect_signals()
-    this.connect(this.model.properties.value.change, () => this.render_selection())
     this.connect(this.model.properties.disabled.change, () => this.set_disabled())
-    this.connect(this.model.properties.max_items.change, () => this.render())
-    this.connect(this.model.properties.option_limit.change, () => this.render())
-    this.connect(this.model.properties.delete_button.change, () => this.render())
-    this.connect(this.model.properties.placeholder.change, () => this.render())
-    this.connect(this.model.properties.options.change, () => this.render())
-    this.connect(this.model.properties.name.change, () => this.render())
-    this.connect(this.model.properties.title.change, () => this.render())
+
+    const {value, max_items, option_limit, delete_button, placeholder, options, name, title} = this.model.properties
+    this.on_change([value, max_items, option_limit, delete_button, placeholder, options, name, title], () => this.render())
   }
 
   styles(): string[] {
@@ -36,53 +31,44 @@ export class MultiChoiceView extends InputWidgetView {
   render(): void {
     super.render()
 
-    const options = this.model.options.map((opt) => {
-      let value, _label
-      if (isString(opt))
-        value = _label  = opt
-      else
-        [value, _label] = opt
-
-      return option({value}, _label)
-    })
-
     this.select_el = select({
       multiple: true,
       class: bk_input,
       name: this.model.name,
       disabled: this.model.disabled,
-    }, options)
+    })
 
     this.group_el.appendChild(this.select_el)
-    this.render_selection()
 
-    let item = "choices__item"
-    let button = "choices__button"
-    if (this.model.solid) {
-      item = item+" solid"
-      button = button+" solid"
-    } else {
-      item = item+" light"
-      button = button+" light"
-    }
-    const opts: Partial<ChoicesNS.Options> = {
-      removeItemButton: this.model.delete_button,
-      classNames: ({item, button} as ChoicesNS.ClassNames),
-    }
-    if (this.model.placeholder !== null)
-      opts.placeholderValue = this.model.placeholder
-    if (this.model.max_items !== null)
-      opts.maxItemCount = this.model.max_items
-    if (this.model.option_limit !== null)
-      opts.renderChoiceLimit = this.model.option_limit
-    this.choice_el = new Choices(this.select_el, opts)
-    this.select_el.addEventListener("change", () => this.change_input())
-  }
-
-  render_selection(): void {
     const selected = new Set(this.model.value)
-    for (const el of Array.from(this.el.querySelectorAll('option')))
-      el.selected = selected.has(el.value)
+    const choices = this.model.options.map((opt) => {
+      let value, label
+      if (isString(opt))
+        value = label  = opt
+      else
+        [value, label] = opt
+      return {value, label, selected: selected.has(value)}
+    })
+
+    const fill = this.model.solid ? "solid" : "light"
+    const item = `choices__item ${fill}`
+    const button = `choices__button ${fill}`
+
+    const options: Partial<ChoicesNS.Options> = {
+      choices,
+      duplicateItemsAllowed: false,
+      removeItemButton: this.model.delete_button,
+      classNames: {item, button} as ChoicesNS.ClassNames, // XXX: bad typings, missing Partial<>
+    }
+    if (this.model.placeholder != null)
+      options.placeholderValue = this.model.placeholder
+    if (this.model.max_items != null)
+      options.maxItemCount = this.model.max_items
+    if (this.model.option_limit != null)
+      options.renderChoiceLimit = this.model.option_limit
+
+    this.choice_el = new Choices(this.select_el, options)
+    this.select_el.addEventListener("change", () => this.change_input())
   }
 
   set_disabled(): void {
@@ -93,10 +79,10 @@ export class MultiChoiceView extends InputWidgetView {
   }
 
   change_input(): void {
-    const is_focused = this.el.querySelector('select:focus') != null
+    const is_focused = this.el.querySelector("select:focus") != null
 
     const values = []
-    for (const el of Array.from(this.el.querySelectorAll('option'))) {
+    for (const el of this.el.querySelectorAll("option")) {
       if (el.selected)
         values.push(el.value)
     }
