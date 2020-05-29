83f466ab65c9826166ff6f7113871f41bc0d54d3
Bryan Van de Ven <bryan@bokeh.org>
2020-05-28 22:45:24 -0700

    only use local bokeh package in tests (#10088)
    * only use local bokeh package in tests
    
    * try explicit package file

--- ci/install_bokeh_package.sh
+++ ci/install_bokeh_package.sh
@@ -2,7 +2,7 @@
 
 set -x #echo on
 
-conda install --yes --quiet -c file:///tmp/conda-bld bokeh
+conda install --yes --quiet --offline /tmp/conda-bld/noarch/bokeh-*.bz2
 
 # TODO (bev) remove this when move from bokeh -> src dir struture
 python setup.py --install-js
