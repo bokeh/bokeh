cf61e2804c74e29dad616b0a707a6ef7ba8129cc
Mateusz Paprocki <mattpap@gmail.com>
2020-06-03 17:27:35 +0200

    React to changes to Toolbar.active_* properties (#10095)
    * React to changes to Toolbar.active_* properties
    
    * Active tool proxy if a sub-tool was activated

--- bokehjs/src/lib/models/tools/on_off_button.ts
+++ bokehjs/src/lib/models/tools/on_off_button.ts
@@ -1,18 +1,16 @@
 import {ButtonToolButtonView} from "./button_tool"
 import {bk_active} from "styles/mixins"
+import {classes} from "core/dom"
 
 export class OnOffButtonView extends ButtonToolButtonView {
 
   render(): void {
     super.render()
-    if (this.model.active)
-      this.el.classList.add(bk_active)
-    else
-      this.el.classList.remove(bk_active)
+    classes(this.el).toggle(bk_active, this.model.active)
   }
 
   protected _clicked(): void {
-    const active = this.model.active
+    const {active} = this.model
     this.model.active = !active
   }
 }
