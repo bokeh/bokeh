19d235fb79d4778f52aee61dc62dda2200d40e11
Mateusz Paprocki <mattpap@gmail.com>
2020-06-06 00:42:04 +0200

    Finalize redesign of core.util.eq

--- bokehjs/src/lib/core/util/eq.ts
+++ bokehjs/src/lib/core/util/eq.ts
@@ -1,9 +1,6 @@
-//     Underscore.js 1.8.3
-//     http://underscorejs.org
-//     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
-//     Underscore may be freely distributed under the MIT license.
+// Based on Underscore.js 1.8.3 (http://underscorejs.org)
 
-import {isFunction} from "./types"
+import {PlainObject} from "../types"
 
 export const equals = Symbol("equals")
 
@@ -19,107 +16,136 @@ export class Comparator {
   private readonly a_stack: unknown[] = []
   private readonly b_stack: unknown[] = []
 
-eq(a: any, b: any): boolean {
-  // Identical objects are equal. `0 === -0`, but they aren't identical.
-  // See the [Harmony `egal` proposal](http://wiki.ecmascript.org/doku.php?id=harmony:egal).
-  if (a === b)
-    return a !== 0 || 1 / a === 1 / b
-  if (a === wildcard || b === wildcard)
-    return true
-  // A strict comparison is necessary because `null == undefined`.
-  if (a == null || b == null)
-    return a === b
-  // Compare `[[Class]]` names.
-  const className = toString.call(a)
-  if (className !== toString.call(b)) return false
-  switch (className) {
-    // Strings, numbers, regular expressions, dates, and booleans are compared by value.
-    case '[object RegExp]':
-    // RegExps are coerced to strings for comparison (Note: '' + /a/i === '/a/i')
-    case '[object String]':
-      // Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
-      // equivalent to `new String("5")`.
-      return '' + a === '' + b
-    case '[object Number]':
-      return this.numbers(a, b)
-    case '[object Date]':
-    case '[object Boolean]':
-      // Coerce dates and booleans to numeric primitive values. Dates are compared by their
-      // millisecond representations. Note that invalid dates with millisecond representations
-      // of `NaN` are not equivalent.
-      return +a === +b
-  }
+  eq(a: any, b: any): boolean {
+    if (Object.is(a, b))
+      return true
 
-  const areArrays = className === '[object Array]'
-  if (!areArrays) {
-    if (typeof a != 'object' || typeof b != 'object') return false
+    if (a === wildcard || b === wildcard)
+      return true
 
-    // Objects with different constructors are not equivalent, but `Object`s or `Array`s
-    // from different frames are.
-    const aCtor = a.constructor, bCtor = b.constructor
-    if (aCtor !== bCtor && !(isFunction(aCtor) && aCtor instanceof aCtor &&
-                             isFunction(bCtor) && bCtor instanceof bCtor)
-                        && ('constructor' in a && 'constructor' in b)) {
+    if (a == null || b == null)
+      return a === b
+
+    const class_name = toString.call(a)
+    if (class_name != toString.call(b))
       return false
+
+    switch (class_name) {
+      case '[object Number]':
+        return this.numbers(a, b)
+      case '[object RegExp]':
+      case '[object String]':
+        return `${a}` == `${b}`
+      case '[object Date]':
+      case '[object Boolean]':
+        return +a === +b
     }
-  }
-  // Assume equality for cyclic structures. The algorithm for detecting cyclic
-  // structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
 
-  // Initializing stack of traversed objects.
-  // It's done here since we only need them for objects and arrays comparison.
-  const {a_stack, b_stack} = this
-  let length = a_stack.length
-  while (length--) {
-    // Linear search. Performance is inversely proportional to the number of
-    // unique nested structures.
-    if (a_stack[length] === a) return b_stack[length] === b
-  }
+    // Assume equality for cyclic structures. The algorithm for detecting cyclic
+    // structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
 
-  // Add the first object to the stack of traversed objects.
-  a_stack.push(a)
-  b_stack.push(b)
-
-  if (equals in a && equals in b) {
-    return a[equals](b, this)
-  }
-
-  // Recursively compare objects and arrays.
-  if (areArrays) {
-    return this.arrays(a, b)
-  } else {
-    // Deep compare objects.
-    const keys = Object.keys(a)
-    let key
-    length = keys.length
-    // Ensure that both objects contain the same number of properties before comparing deep equality.
-    if (Object.keys(b).length !== length)
-      return false
+    // Initializing stack of traversed objects.
+    // It's done here since we only need them for objects and arrays comparison.
+    const {a_stack, b_stack} = this
+    let length = a_stack.length
     while (length--) {
-      // Deep compare each member
-      key = keys[length]
-      if (!(b.hasOwnProperty(key) && this.eq(a[key], b[key])))
-        return false
+      // Linear search. Performance is inversely proportional to the number of
+      // unique nested structures.
+      if (a_stack[length] === a)
+        return b_stack[length] === b
     }
+
+    a_stack.push(a)
+    b_stack.push(b)
+
+    const result = (() => {
+      if (equals in a && equals in b) {
+        return a[equals](b, this)
+      }
+
+      switch (class_name) {
+        case "[object Array]":
+        case "[object Uint8Array]":
+        case "[object Int8Array]":
+        case "[object Uint16Array]":
+        case "[object Int16Array]":
+        case "[object Uint32Array]":
+        case "[object Int32Array]":
+        case "[object Float32Array]":
+        case "[object Float64Array]": {
+          return this.arrays(a, b)
+        }
+        case "[object Map]":
+        case "[object Set]":
+          return this.iterables(a, b)
+        case "[object Object]": {
+          if (a.constructor == b.constructor && (a.constructor == null || a.constructor === Object)) {
+            return this.objects(a, b)
+          }
+        }
+        case "[object Function]": {
+          if (a.constructor == b.constructor && a.constructor === Function) {
+            return this.eq(`${a}`, `${b}`)
+          }
+        }
+      }
+
+      throw Error(`can't compare objects of type ${class_name}`)
+    })()
+
+    a_stack.pop()
+    b_stack.pop()
+
+    return result
   }
-  // Remove the first object from the stack of traversed objects.
-  a_stack.pop()
-  b_stack.pop()
-  return true
-}
 
   numbers(a: number, b: number): boolean {
     return Object.is(a, b)
   }
 
   arrays(a: ArrayLike<unknown>, b: ArrayLike<unknown>): boolean {
-    let {length} = a
+    const {length} = a
+
     if (length != b.length)
       return false
-    while (length--) {
-      if (!this.eq(a[length], b[length]))
+
+    for (let i = 0; i < length; i++) {
+      if (!this.eq(a[i], b[i]))
         return false
     }
+
+    return true
+  }
+
+  iterables(a: Iterable<unknown>, b: Iterable<unknown>): boolean {
+    const ai = a[Symbol.iterator]()
+    const bi = b[Symbol.iterator]()
+
+    while (true) {
+      const an = ai.next()
+      const bn = bi.next()
+
+      if (an.done && bn.done)
+        return true
+      if (an.done || bn.done)
+        return false
+
+      if (!this.eq(an.value, bn.value))
+        return false
+    }
+  }
+
+  objects(a: PlainObject, b: PlainObject): boolean {
+    const keys = Object.keys(a)
+
+    if (!this.arrays(keys, Object.keys(b)))
+      return false
+
+    for (const key of keys) {
+      if (!this.eq(a[key], b[key]))
+        return false
+    }
+
     return true
   }
 }
