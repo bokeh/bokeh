e294c98419ff23dadde44e3261a61d48b88dc7dc
Ashwani Rathee <54855463+ashwani-rathee@users.noreply.github.com>
2020-06-10 12:43:59 +0530

    Update __init__.py

--- bokeh/themes/__init__.py
+++ bokeh/themes/__init__.py
@@ -133,7 +133,7 @@ log = logging.getLogger(__name__)
 #-----------------------------------------------------------------------------
 
 # Bokeh imports
-from . import _caliber, _contrast, _dark_minimal, _light_minimal, _night_sky, _ggplot
+from . import _caliber, _contrast, _dark_minimal, _ggplot, _light_minimal, _night_sky
 from .theme import Theme
 
 #-----------------------------------------------------------------------------
