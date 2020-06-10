7f143a49dd64efb5ffc29c85b6ff113f11f027c1
Ashwani Rathee <54855463+ashwani-rathee@users.noreply.github.com>
2020-06-10 18:59:37 +0530

    Create _ggplot.py

---
+++ bokeh/themes/_ggplot.py
@@ -0,0 +1,78 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2020, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+json = {
+    "attrs": {
+        "Figure" : {
+            "background_fill_color": "#E5E5E5",
+            "border_fill_color": "#FFFFFF",
+            "outline_line_color": "#000000",
+            "outline_line_alpha": 0.25
+        },
+
+        "Grid": {
+            "grid_line_color": "#FFFFFF",
+            "grid_line_alpha": 1
+        },
+
+        "Axis": {
+            "major_tick_line_alpha": 0.30,
+            "major_tick_line_color": "#000000",
+
+            "minor_tick_line_alpha": 0.40,
+            "minor_tick_line_color": "#000000",
+
+            "axis_line_alpha": 0,
+            "axis_line_color": "#000000",
+
+            "major_label_text_color": "#000000",
+            "major_label_text_font": "Helvetica",
+            "major_label_text_font_size": "1.025em",
+
+            "axis_label_standoff": 10,
+            "axis_label_text_color": "#000000",
+            "axis_label_text_font": "Helvetica",
+            "axis_label_text_font_size": "1.25em",
+            "axis_label_text_font_style": "normal"
+        },
+
+        "Legend": {
+            "spacing": 8,
+            "glyph_width": 15,
+
+            "label_standoff": 8,
+            "label_text_color": "#000000",
+            "label_text_font": "Arial",
+            "label_text_font_size": "0.95em",
+
+            "border_line_alpha": 1,
+            "background_fill_alpha": 0.25,
+            "background_fill_color": "#000000"
+        },
+
+        "ColorBar": {
+            "title_text_color": "#E0E0E0",
+            "title_text_font": "Helvetica",
+            "title_text_font_size": "1.025em",
+            "title_text_font_style": "normal",
+
+            "major_label_text_color": "#E0E0E0",
+            "major_label_text_font": "Helvetica",
+            "major_label_text_font_size": "1.025em",
+
+            "background_fill_color": "#000000",
+            "major_tick_line_alpha": 0,
+            "bar_line_alpha": 0
+        },
+
+        "Title": {
+            "text_color": "#000000",
+            "text_font": "Helvetica",
+            "text_font_size": "1.10em"
+        }
+    }
+}
