b6a30780e9b93b41a6090f08bf75fca543ff330b
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-06-16 13:47:43 -0400

    added a unit test for structure

---
+++ tests/unit/bokeh/util/test_structure.py
@@ -0,0 +1,11 @@
+import pytest ; pytest
+from bokeh.model import collect_models
+from bokeh.plotting import figure
+from bokeh.util.structure import generate_structure_plot
+
+
+def test_structure():
+    f = figure(width=400,height=400)
+    f.line(x=[1,2,3],y=[1,2,3])
+    K = generate_structure_plot(f)
+    assert 43 == len(collect_models(K))
