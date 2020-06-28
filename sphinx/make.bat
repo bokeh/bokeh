bc7ab919f2a229ca0fc42145f3c72022f3950827
Bryan Van de Ven <bryan@bokeh.org>
2020-06-28 15:51:09 -0700

    match docs build dir on windows

--- sphinx/make.bat
+++ sphinx/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=_build
+set BUILDDIR=build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
