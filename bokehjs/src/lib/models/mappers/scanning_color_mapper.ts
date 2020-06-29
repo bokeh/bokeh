e3b252204ce9fce6f72df11b7289b02a5f60e925
jlstevens <jlrstevens@gmail.com>
2020-06-29 09:55:43 +0200

    Updated cmap method of ScanningColorMapper to access edges correctly

--- bokehjs/src/lib/models/mappers/scanning_color_mapper.ts
+++ bokehjs/src/lib/models/mappers/scanning_color_mapper.ts
@@ -1,7 +1,6 @@
 import {ContinuousColorMapper} from "./continuous_color_mapper"
 import {Arrayable} from "core/types"
 import * as p from "core/properties"
-import {assert} from "core/util/assert"
 
 export namespace ScanningColorMapper {
   export type Attrs = p.AttrsOf<Props>
@@ -20,20 +19,19 @@ export abstract class ScanningColorMapper extends ContinuousColorMapper {
 
   metrics: {min: number, max: number, binning: Arrayable<number>}
 
-  protected cmap<T>(d: number, palette: Arrayable<T>, low_color: T, high_color: T, edges: Arrayable<number>): T {
-    assert(edges.length > 0)
+    protected cmap<T>(d: number, palette: Arrayable<T>, low_color: T, high_color: T, edges: any): T {
 
-    if (d < edges[0]) {
+    if (d < edges.binning[0]) {
       return low_color
     }
-    if (d > edges[edges.length-1]) {
+    if (d > edges.binning[edges.binning.length-1]) {
       return high_color
     }
 
     let key = 0
-    for (let i = 0, end = edges.length - 2; i < end; i++) {
-      const low_edge = edges[i]
-      const high_edge = edges[i+1]
+    for (let i = 0, end = edges.binning.length - 2; i < end; i++) {
+      const low_edge = edges.binning[i]
+      const high_edge = edges.binning[i+1]
       key = i
       if (low_edge <= d && d < high_edge) {
         break
