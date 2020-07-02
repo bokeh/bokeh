11de54f355475cfd6eca2b5c7fb2020fd7641413
Philipp Rudiger <prudiger@anaconda.com>
2020-07-02 04:08:13 +0200

    Optimizations

--- bokehjs/src/lib/models/mappers/eqhist_color_mapper.ts
+++ bokehjs/src/lib/models/mappers/eqhist_color_mapper.ts
@@ -13,28 +13,6 @@ export namespace EqHistColorMapper {
   }
 }
 
-function eq_hist(data: Arrayable<number>, low: number, high: number, nbins: number, n: number): number[] {
-  const eq_bin_edges = linspace(low, high, nbins+1)
-  const hist = bin_counts(data, eq_bin_edges)
-
-  const eq_bin_centers = new Array(nbins)
-  for (let i = 0, length = eq_bin_edges.length; i < (length-1); i++) {
-    const left = eq_bin_edges[i]
-    const right = eq_bin_edges[i+1]
-    eq_bin_centers[i] = (left+right)/2
-  }
-
-  // CDFs
-  const cdf = cumsum(hist)
-  const cdf_max = cdf[cdf.length - 1]
-  const norm_cdf = map(cdf, (x) => x / cdf_max)
-
-  // Interpolate
-  const palette_edges = range(0, n)
-  const palette_cdf = map(norm_cdf, (x) => x*(n-1))
-  return (interpolate(palette_edges, palette_cdf, eq_bin_centers) as number[])
-}
-
 export interface EqHistColorMapper extends EqHistColorMapper.Attrs {}
 
 export class EqHistColorMapper extends ScanningColorMapper {
@@ -54,20 +32,41 @@ export class EqHistColorMapper extends ScanningColorMapper {
     const low = this.low != null ? this.low : min(data)
     const high = this.high != null ? this.high : max(data)
 
-    // Iteratively find as many finite bins as there are colors
     const nbins = this.bins
+    const eq_bin_edges = linspace(low, high, nbins+1)
+    const hist = bin_counts(data, eq_bin_edges)
+
+    const eq_bin_centers = new Array(nbins)
+    for (let i = 0, length = eq_bin_edges.length; i < (length-1); i++) {
+      const left = eq_bin_edges[i]
+      const right = eq_bin_edges[i+1]
+      eq_bin_centers[i] = (left+right)/2
+    }
+
+    // CDFs
+    const cdf = cumsum(hist)
+    const cdf_max = cdf[cdf.length - 1]
+    const norm_cdf = map(cdf, (x) => x / cdf_max)
+
+    // Iteratively find as many finite bins as there are colors
     let finite_bins = n-1
     let binning: number[] = []
     let iterations = 0
-    let guess = n*1.5
-    while (finite_bins != n && iterations<10 && (finite_bins != 0)) {
+    let guess = n*2
+    while (finite_bins != n && iterations<4 && (finite_bins != 0)) {
       guess = Math.round(Math.max(n * guess/finite_bins, n))
-      binning = eq_hist(data, low, high, nbins, guess)
+
+      // Interpolate
+      const palette_edges = range(0, guess)
+      const palette_cdf = map(norm_cdf, (x) => x*(guess-1))
+      binning = (interpolate(palette_edges, palette_cdf, eq_bin_centers) as number[])
+
+      // Evaluate binning
       const uniq_bins = uniq(binning)
       finite_bins = uniq_bins.length-1
       iterations++
     }
-	if (finite_bins == 0)
+    if (finite_bins == 0)
       binning = [low, high]
     else
       binning = binning.slice(binning.length-n)
