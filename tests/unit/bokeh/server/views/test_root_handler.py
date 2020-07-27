4bed2404d939596c242642cccbc2afb2c364ca7e
Bryan Van de Ven <bryan@bokeh.org>
2020-07-27 12:13:36 -0700

    add basic view tests to maintain AuthMixin

--- tests/unit/bokeh/server/views/test_root_handler.py
+++ tests/unit/bokeh/server/views/test_root_handler.py
@@ -14,44 +14,22 @@ import pytest ; pytest
 # Imports
 #-----------------------------------------------------------------------------
 
-# Standard library imports
-import logging
-
-# External imports
-from tornado.websocket import WebSocketClosedError
-
 # Bokeh imports
 from bokeh.server.views.auth_mixin import AuthMixin
-from bokeh.util.logconfig import basicConfig
 
 # Module under test
-from bokeh.server.views.ws import WSHandler # isort:skip
+from bokeh.server.views.root_handler import RootHandler # isort:skip
 
 #-----------------------------------------------------------------------------
 # Setup
 #-----------------------------------------------------------------------------
 
-# needed for caplog tests to function
-basicConfig()
-
 #-----------------------------------------------------------------------------
 # General API
 #-----------------------------------------------------------------------------
 
-async def test_send_message_raises(caplog) -> None:
-    class ExcMessage:
-        def send(self, handler):
-            raise WebSocketClosedError()
-    assert len(caplog.records) == 0
-    with caplog.at_level(logging.WARN):
-        # fake self not great but much easier than setting up a real view
-        ret = await WSHandler.send_message("self", ExcMessage())
-        assert len(caplog.records) == 1
-        assert caplog.text.endswith("Failed sending message as connection was closed\n")
-        assert ret is None
-
 def test_uses_auth_mixin() -> None:
-    assert issubclass(WSHandler, AuthMixin)
+    assert issubclass(RootHandler, AuthMixin)
 
 #-----------------------------------------------------------------------------
 # Dev API
