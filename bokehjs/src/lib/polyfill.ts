4d96475689d25bbad6ee4028acc926bf6963045a
Mateusz Paprocki <mattpap@gmail.com>
2020-07-29 22:34:13 +0200

    Fix polyfill of Object.is()

--- bokehjs/src/lib/polyfill.ts
+++ bokehjs/src/lib/polyfill.ts
@@ -14,8 +14,11 @@ import "es6-symbol/implement"
 import "es6-promise/auto"
 
 if (typeof Object.is === "undefined") {
-  Object.is = function(val1: any, val2: any): boolean {
-    return val1 === val2 ? val1 !== 0 || 1 / val1 === 1 / val2 : isNaN(val1) && isNaN(val2)
+  Object.is = function(a: any, b: any): boolean {
+    if (a === b)
+      return a !== 0 || 1/a === 1/b
+    else
+      return a !== a && b !== b
   }
 }
 
