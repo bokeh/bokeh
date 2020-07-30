0fc7738a58fd5d1a9ee4fc529d7132e409b0a446
Mateusz Paprocki <mattpap@gmail.com>
2020-07-30 16:37:32 +0200

    Increase bundle size allowance

--- bokehjs/test/codebase/size.ts
+++ bokehjs/test/codebase/size.ts
@@ -8,7 +8,7 @@ const build_dir = path.normalize(`${__dirname}/../..`) // build/test/codebase ->
 
 const LIMITS = new Map([
   // es2017
-  ["js/bokeh.min.js",                750],
+  ["js/bokeh.min.js",                800],
   ["js/bokeh-widgets.min.js",        300],
   ["js/bokeh-tables.min.js",         350],
   ["js/bokeh-api.min.js",             90],
