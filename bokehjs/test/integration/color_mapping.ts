08b903a8d8a6a3e929fb4e75895759b25a4fa45a
Mateusz Paprocki <mattpap@gmail.com>
2020-08-01 16:35:00 +0200

    Test multiple domain color mappers

--- bokehjs/test/integration/color_mapping.ts
+++ bokehjs/test/integration/color_mapping.ts
@@ -1,8 +1,8 @@
-import {display, fig} from "./utils"
+import {display, fig, grid} from "./utils"
 
 import pipeline from "./data"
 
-import {EqHistColorMapper} from "@bokehjs/models"
+import {Plot, Range1d, GlyphRenderer, LinearColorMapper, EqHistColorMapper} from "@bokehjs/models"
 import {Plasma256} from "@bokehjs/api/palettes"
 import {Float64NDArray} from "@bokehjs/core/util/ndarray"
 import {linspace} from "@bokehjs/core/util/array"
@@ -125,6 +125,112 @@ describe("Color mapping", () => {
       const cells = sparse_cells()
       const p = eqhist_mapped_plot(cells)
       await display(p, [350, 350])
+    })
+  })
+
+  describe("with multiple domains", () => {
+    function make_grid() {
+      const random = new Random(1)
+
+      function data_flat(c: number) {
+        const N = 500
+        const x = random.floats(N)
+        const y = random.floats(N)
+        const r = linspace(0, 1, N)
+        return [
+          x.map((xi) => 100*xi),
+          y.map((yi) => 100*yi),
+          r.map((ri) => c*ri),
+        ]
+      }
+
+      function data_sloped(c: number) {
+        const N = 500
+        const x = random.floats(N)
+        const y = random.floats(N)
+        const r = linspace(0, 1, N)
+        return [
+          x.map((xi) => 100*xi),
+          y.map((yi) => 100*yi),
+          r.map((ri, i) => c*(x[i] + y[i])*ri),
+        ]
+      }
+
+      const mapper = new LinearColorMapper({palette: Plasma256, low_color: "white", high_color: "black"})
+
+      const range = () => new Range1d({start: 0, end: 100})
+
+      const p0 = fig([250, 250], {x_range: range(), y_range: range()})
+      const [x0, y0, r0] = data_flat(0.8)
+      const g0 = p0.circle({
+        x: x0, y: y0, radius: r0,
+        fill_color: {field: "radius", transform: mapper} as any, // XXX: bad types in VectorSpec, ColorMapper and Transform
+        fill_alpha: 0.6, line_color: null,
+      })
+      mapper.domain.push([g0, "radius"])
+
+      const p1 = fig([250, 250], {x_range: range(), y_range: range()})
+      const [x1, y1, r1] = data_flat(1.0)
+      const g1 = p1.circle({
+        x: x1, y: y1, radius: r1,
+        fill_color: {field: "radius", transform: mapper} as any,
+        fill_alpha: 0.6, line_color: null,
+      })
+      mapper.domain.push([g1, "radius"])
+
+      const p2 = fig([250, 250], {x_range: range(), y_range: range()})
+      const [x2, y2, r2] = data_flat(1.2)
+      const g2 = p2.circle({
+        x: x2, y: y2, radius: r2,
+        fill_color: {field: "radius", transform: mapper} as any,
+        fill_alpha: 0.6, line_color: null,
+      })
+      mapper.domain.push([g2, "radius"])
+
+      const p3 = fig([250, 250], {x_range: range(), y_range: range()})
+      const [x3, y3, r3] = data_sloped(2.0)
+      const g3 = p3.circle({
+        x: x3, y: y3, radius: r3,
+        fill_color: {field: "radius", transform: mapper} as any,
+        fill_alpha: 0.6, line_color: null,
+      })
+      mapper.domain.push([g3, "radius"])
+
+      // XXX: force change due to using Array.push()
+      mapper.properties.domain.change.emit()
+
+      return grid([[p0, p1], [p2, p3]])
+    }
+
+    it("should work", async () => {
+      const grid = make_grid()
+      await display(grid, [550, 550])
+    })
+
+    it("should work after panning", async () => {
+      const grid = make_grid()
+      const {view} = await display(grid, [550, 550])
+      const pv3 = view.child_views[3] as Plot["__view_type__"]
+      const p3 = pv3.model
+      // TODO: synchronized
+      p3.x_range.start = -60
+      p3.x_range.end = 40
+      p3.y_range.start = -60
+      p3.y_range.end = 40
+      await pv3.ready
+    })
+
+    it("should work after selection", async () => {
+      const grid = make_grid()
+      const {view} = await display(grid, [550, 550])
+      const pv3 = view.child_views[3] as Plot["__view_type__"]
+      const g3 = pv3.model.renderers[0] as GlyphRenderer
+      const gv3 = pv3.renderer_views.get(g3) as GlyphRenderer["__view_type__"]
+      const sel = gv3.model.get_selection_manager()
+      const [sx0, sx1] = pv3.frame.x_scale.r_compute(20, 40)
+      const [sy0, sy1] = pv3.frame.y_scale.r_compute(20, 40)
+      sel.select([gv3], {type: "rect", sx0, sy0, sx1, sy1}, true, "append")
+      await pv3.ready
     })
   })
 })
