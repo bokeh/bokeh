d55c77c2e149933392b2dbbc9a1a53125b8df208
Philipp Rudiger <prudiger@anaconda.com>
2020-08-05 16:00:49 +0200

    Fixed formatting

--- bokeh/models/widgets/tables.py
+++ bokeh/models/widgets/tables.py
@@ -228,7 +228,8 @@ class NumberFormatter(StringFormatter):
     """)
 
     nan_format = String(help="""
-    Formatting to apply to NaN and None values (falls back to Numbro formatting if not set).""")
+    Formatting to apply to NaN and None values (falls back to Numbro formatting if not set).
+    """)
 
     rounding = Enum(RoundingFunction, help="""
     Rounding functions (round, floor, ceil) and their synonyms (nearest, rounddown, roundup).
