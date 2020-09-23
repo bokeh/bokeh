4dfca93ebbc12808e016c797e4a2368ce2d84dfc
Bryan Van de Ven <bryan@bokeh.org>
2020-09-23 11:16:47 -0700

    fix internal precision loss (#10513)
    * fix internal precision loss
    
    * use Float64Array internally
    
    * update integration tests images
    
    * regression test
    
    * lint
    
    * remaining baselines

--- bokehjs/test/integration/regressions.ts
+++ bokehjs/test/integration/regressions.ts
@@ -1,5 +1,6 @@
 import {display, fig, row, column, grid} from "./utils"
 
+import {linspace} from "@bokehjs/core/util/array"
 import {
   Arrow, ArrowHead, NormalHead, BoxAnnotation, LabelSet, Legend, LegendItem,
   Range1d, DataRange1d, FactorRange,
@@ -557,6 +558,18 @@ describe("Bug", () => {
       const {view} = await display(widget, [250, 100])
       widget.disabled = true
       await view.ready
+    })
+  })
+
+  describe("in issue #10488", () => {
+    it("disallows correct placement of Rect glyph with datetime values", async () => {
+      const t0 = 1600755745624.793
+      const source = new ColumnDataSource({data: {
+        x: linspace(t0, t0 + 2 * 3600 * 1000, 50),
+      }})
+      const p = fig([800, 300])
+      p.rect({x: {field: "x"}, y: 0, width: 100000, height: 1, line_color: "red", fill_alpha: 0.5, line_alpha: 0.5, source})
+      await display(p, [800, 300])
     })
   })
 })
