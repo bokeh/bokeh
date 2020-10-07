fd60dec827ae7cab3e39bca70b77358b56b0f25e
tcmetzger <ticon@gmx.net>
2020-10-07 14:35:14 -0700

    Fix erroneous change

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -71,7 +71,7 @@ rst_epilog = """
 
 autodoc_member_order = 'groupwise'
 
-bokeh_missing_google_api_key_ok = True
+bokeh_missing_google_api_key_ok = False
 
 if not bokeh_missing_google_api_key_ok:
     if "GOOGLE_API_KEY" not in os.environ:
