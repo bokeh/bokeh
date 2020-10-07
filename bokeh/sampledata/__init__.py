08c12b012f6416068492c77d322da9ac202941f4
tcmetzger <ticon@gmx.net>
2020-10-07 14:35:27 -0700

    Fix typos

--- bokeh/sampledata/__init__.py
+++ bokeh/sampledata/__init__.py
@@ -22,11 +22,11 @@ programmatically.
     >>> import bokeh.sampledata
     >>> bokeh.sampledata.download()
 
-By default, data is downloaded and stored to a directory ``$HOME/.bokeh/data``.
+By default, data is downloaded and stored to a directory ``$HOME/.bokeh/data``
 (The directory is created if it does not already exist.) Bokeh looks for
 a YAML configuration file at ``$HOME/.bokeh/config``. The YAML key
 ``sampledata_dir`` can be set to the absolute path of a directory where
-the data should be stored. For instance adding the following line to the
+the data should be stored. For instance, adding the following line to the
 config file:
 
 .. code-block:: sh
