139a9ee079d632c0a06950548e79c8c919884b4b
Mateusz Paprocki <mattpap@gmail.com>
2020-10-17 13:57:36 +0200

    Enable func-call-spacing rule

--- bokehjs/test/unit/document/events.ts
+++ bokehjs/test/unit/document/events.ts
@@ -86,7 +86,7 @@ describe("events module", () => {
   })
 
   describe("ModelChangedEvent", () => {
-    it ("should throw an error if attr=id", () => {
+    it("should throw an error if attr=id", () => {
       const d = new Document()
       const m = new TestModel()
       const evt = new events.ModelChangedEvent(d, m, "id", 1, 2)
