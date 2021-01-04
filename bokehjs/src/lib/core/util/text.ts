3767a679679e6fa77a99d2d029608bc0f3542c25
Mateusz Paprocki <mattpap@gmail.com>
2021-01-04 01:10:23 +0100

    Remove measure_text()

--- bokehjs/src/lib/core/util/text.ts
+++ bokehjs/src/lib/core/util/text.ts
@@ -1,4 +1,3 @@
-import {Size} from "../types"
 import {div, span, offset} from "../dom"
 
 const _font_cache: Map<string, FontMetrics> = new Map()
@@ -33,30 +32,5 @@ export function measure_font(font: string): FontMetrics {
     return result
   } finally {
     document.body.removeChild(elem)
-  }
-}
-
-const _text_cache: Map<string, Map<string, Size>> = new Map()
-
-export function measure_text(text: string, font: string): Size {
-  let size_cache = _text_cache.get(font)
-  if (size_cache != null) {
-    const size = size_cache.get(text)
-    if (size != null)
-      return size
-  } else {
-    size_cache = new Map()
-    _text_cache.set(font, size_cache)
-  }
-
-  const el = div({style: {display: "inline-block", "white-space": "nowrap", font}}, text)
-  document.body.appendChild(el)
-
-  try {
-    const {width, height} = el.getBoundingClientRect()
-    size_cache.set(text, {width, height})
-    return {width, height}
-  } finally {
-    document.body.removeChild(el)
   }
 }
