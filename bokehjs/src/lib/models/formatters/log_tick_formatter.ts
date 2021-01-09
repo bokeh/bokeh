88d673eb50131a2750b1c9d2bc7ac7ec84748fc7
Mateusz Paprocki <mattpap@gmail.com>
2021-01-09 01:26:38 +0100

    Add support for graphical tick formatting

--- bokehjs/src/lib/models/formatters/log_tick_formatter.ts
+++ bokehjs/src/lib/models/formatters/log_tick_formatter.ts
@@ -1,7 +1,10 @@
 import {TickFormatter} from "./tick_formatter"
 import {BasicTickFormatter} from "./basic_tick_formatter"
 import {LogTicker} from "../tickers/log_ticker"
+import {GraphicsBox, BaseExpo, TextBox} from "core/graphics"
 import * as p from "core/properties"
+
+const {log, round} = Math
 
 export namespace LogTickFormatter {
   export type Attrs = p.AttrsOf<Props>
@@ -33,25 +36,43 @@ export class LogTickFormatter extends TickFormatter {
     this.basic_formatter = new BasicTickFormatter()
   }
 
+  format_graphics(ticks: number[], opts: {loc: number}): GraphicsBox[] {
+    if (ticks.length == 0)
+      return []
+
+    const base = this.ticker?.base ?? 10
+    const expos = this._exponents(ticks, base)
+
+    if (expos == null)
+      return this.basic_formatter.format_graphics(ticks, opts)
+    else
+      return expos.map((expo) => new BaseExpo(new TextBox({text: `${base}`}), new TextBox({text: expo})))
+  }
+
+  protected _exponents(ticks: number[], base: number): string[] | null {
+    let last_expo = null
+    const exponents = []
+    for (const tick of ticks) {
+      const expo = `${round(log(tick)/log(base))}`
+      if (last_expo != expo) {
+        last_expo = expo
+        exponents.push(expo)
+      } else
+        return null
+    }
+    return exponents
+  }
+
   doFormat(ticks: number[], opts: {loc: number}): string[] {
     if (ticks.length == 0)
       return []
 
-    const base = this.ticker != null ? this.ticker.base : 10
+    const base = this.ticker?.base ?? 10
+    const expos = this._exponents(ticks, base)
 
-    let small_interval = false
-    const labels: string[] = new Array(ticks.length)
-    for (let i = 0, end = ticks.length; i < end; i++) {
-      labels[i] = `${base}^${ Math.round(Math.log(ticks[i]) / Math.log(base)) }`
-      if (i > 0 && labels[i] == labels[i-1]) {
-        small_interval = true
-        break
-      }
-    }
-
-    if (small_interval)
+    if (expos == null)
       return this.basic_formatter.doFormat(ticks, opts)
     else
-      return labels
+      return expos.map((expo) => `${base}^${expo}`)
   }
 }
