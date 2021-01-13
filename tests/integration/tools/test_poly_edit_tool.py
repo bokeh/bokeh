43ff5af6f666cbf4d1ed1c26c7a128e9b10cd86c
Jonathan Okasinski <nitrocalcite@users.noreply.github.com>
2021-01-13 08:33:35 -0700

    Update vertex_renderer properly in PolyEditTool (#10719)
    * Update PolyEditTool vertices when the underlying data source is updated
    
    * Update properly after drag event by tracking selected index
    
    * Don't spuriously update when drawing
    
    * Add regression tests & clean up the others
    
    * Remove unused import
    
    (flaky no longer used in polyedit tests)

--- tests/integration/tools/test_poly_edit_tool.py
+++ tests/integration/tools/test_poly_edit_tool.py
@@ -19,7 +19,6 @@ import pytest ; pytest
 import time
 
 # External imports
-from flaky import flaky
 
 # Bokeh imports
 from bokeh._testing.util.compare import cds_data_almost_equal
@@ -63,10 +62,10 @@ def _make_plot():
     return plot
 
 def _make_server_plot(expected):
+    data = {"xs": [[1, 2], [1.6, 2.45]],
+            "ys": [[1, 1], [1.5, 0.75]]}
+    source = ColumnDataSource(data)
     def modify_doc(doc):
-        data = {"xs": [[1, 2], [1.6, 2.45]],
-                "ys": [[1, 1], [1.5, 0.75]]}
-        source = ColumnDataSource(data)
         plot = Plot(plot_height=400, plot_width=400, x_range=Range1d(0, 3), y_range=Range1d(0, 3), min_border=0)
         renderer = plot.add_glyph(source, MultiLine(xs='xs', ys='ys'))
         tool = PolyEditTool(renderers=[renderer])
@@ -78,18 +77,21 @@ def _make_server_plot(expected):
         plot.toolbar_sticky = False
         div = Div(text='False')
         def cb(attr, old, new):
-            if cds_data_almost_equal(new, expected):
-                div.text = 'True'
+            try:
+                if cds_data_almost_equal(new, expected):
+                    div.text = 'True'
+            except ValueError:
+                return
         source.on_change('data', cb)
         code = RECORD("matches", "div.text")
         plot.add_tools(CustomAction(callback=CustomJS(args=dict(div=div), code=code)))
         doc.add_root(column(plot, div))
-    return modify_doc
+    return modify_doc, source
 
 
 @pytest.mark.selenium
 class Test_PolyEditTool:
-    def _test_selected_by_default(self, single_plot_page):
+    def test_selected_by_default(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -99,7 +101,7 @@ class Test_PolyEditTool:
 
         assert page.has_no_console_errors()
 
-    def _test_can_be_deselected_and_selected(self, single_plot_page):
+    def test_can_be_deselected_and_selected(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -120,7 +122,7 @@ class Test_PolyEditTool:
 
         assert page.has_no_console_errors()
 
-    def _test_double_click_triggers_edit(self, single_plot_page):
+    def test_double_click_triggers_edit(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -134,13 +136,13 @@ class Test_PolyEditTool:
         time.sleep(0.5)
         page.click_custom_action()
 
-        expected = {'xs': [[1, 2], [1.6, 2.45, 2.027027027027027, 1.6]],
-                    'ys': [[1, 1], [1.5, 0.75, 1.8749999999999998, 1.5]]}
+        expected = {'xs': [[1, 2], [1.6, 2.45, 2.027027027027027]],
+                    'ys': [[1, 1], [1.5, 0.75, 1.8749999999999998]]}
         assert cds_data_almost_equal(page.results, expected)
 
         assert page.has_no_console_errors()
 
-    def _test_double_click_snaps_to_vertex(self, single_plot_page):
+    def test_double_click_snaps_to_vertex(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -162,7 +164,7 @@ class Test_PolyEditTool:
 
         assert page.has_no_console_errors()
 
-    def _test_drag_moves_vertex(self, single_plot_page):
+    def test_drag_moves_vertex(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -185,7 +187,7 @@ class Test_PolyEditTool:
 
         assert page.has_no_console_errors()
 
-    def _test_backspace_removes_vertex(self, single_plot_page):
+    def test_backspace_removes_vertex(self, single_plot_page):
         plot = _make_plot()
 
         page = single_plot_page(plot)
@@ -206,14 +208,12 @@ class Test_PolyEditTool:
         expected = {"xs": [[1, 2], [1.6, 2.027027027027027]],
                     "ys": [[1, 1], [1.5, 1.8749999999999998]]}
         assert cds_data_almost_equal(page.results, expected)
-
         assert page.has_no_console_errors()
 
-    @flaky(max_runs=10)
-    def _test_poly_edit_syncs_to_server(self, bokeh_server_page):
+    def test_poly_edit_syncs_to_server(self, bokeh_server_page):
         expected = {'xs': [[1, 2], [1.6, 2.45, 2.027027027027027]],
                     'ys': [[1, 1], [1.5, 0.75, 1.8749999999999998]]}
-        page = bokeh_server_page(_make_server_plot(expected))
+        page = bokeh_server_page(_make_server_plot(expected)[0])
 
         # ensure double clicking shows vertices and edits them
         page.double_click_canvas_at_position(200, 200)
@@ -225,13 +225,13 @@ class Test_PolyEditTool:
 
         page.click_custom_action()
         assert page.results == {"matches": "True"}
+        assert page.has_no_console_errors()
 
-    @flaky(max_runs=10)
-    def _test_poly_drag_syncs_to_server(self, bokeh_server_page):
+    def test_poly_drag_syncs_to_server(self, bokeh_server_page):
         expected = {"xs": [[1, 2], [1.6, 2.45, 2.5945945945945947]],
                     "ys": [[1, 1], [1.5, 0.75, 1.5]]}
 
-        page = bokeh_server_page(_make_server_plot(expected))
+        page = bokeh_server_page(_make_server_plot(expected)[0])
 
         # ensure drag moves vertex
         page.double_click_canvas_at_position(200, 200)
@@ -246,14 +246,40 @@ class Test_PolyEditTool:
 
         page.click_custom_action()
         assert page.results == {"matches": "True"}
+        assert page.has_no_console_errors()
 
-    # TODO (bev) Fix up after GH CI switch
-    @pytest.mark.skip
-    @flaky(max_runs=10)
+    def test_poly_drag_sync_after_source_edit(self, bokeh_server_page):
+        expected = {"xs": [[1, 2], [1.6, 2.45, 2.5945945945945947]],
+                    "ys": [[1, 1], [1.5, 0.75, 1.5]]}
+
+        mod, ds = _make_server_plot(expected)
+        page = bokeh_server_page(mod)
+
+        # ensure drag moves vertex
+        page.double_click_canvas_at_position(200, 200)
+        time.sleep(0.5)
+        page.double_click_canvas_at_position(298, 298)
+        time.sleep(0.5)
+        page.click_canvas_at_position(250, 150)
+        time.sleep(0.5)
+        page.send_keys("\ue00c")  # Escape
+        time.sleep(0.5)
+
+        def f(): ds.data = dict(ds.data)  # update the data source
+        ds.document.add_next_tick_callback(f)
+        time.sleep(0.5)
+
+        page.drag_canvas_at_position(250, 150, 70, 50)
+        time.sleep(0.5)
+
+        page.click_custom_action()
+        assert page.results == {"matches": "True"}
+        assert page.has_no_console_errors()
+
     def test_poly_delete_syncs_to_server(self, bokeh_server_page) -> None:
         expected = {"xs": [[1, 2], [1.6, 2.027027027027027]],
                     "ys": [[1, 1], [1.5, 1.8749999999999998]]}
-        page = bokeh_server_page(_make_server_plot(expected))
+        page = bokeh_server_page(_make_server_plot(expected)[0])
 
         # ensure backspace removes vertex
         page.double_click_canvas_at_position(200, 200)
@@ -269,3 +295,4 @@ class Test_PolyEditTool:
 
         page.click_custom_action()
         assert page.results == {"matches": "True"}
+        assert page.has_no_console_errors()
