c2d9eac7fad80766c34e5d4889109c93099cde0a
Mateusz Paprocki <mattpap@gmail.com>
2021-01-27 13:02:25 +0100

    Update unit tests

--- bokehjs/test/unit/models/axes/axis.ts
+++ bokehjs/test/unit/models/axes/axis.ts
@@ -28,7 +28,8 @@ describe("Axis", () => {
     const plot_view = (await build_view(plot)).build()
     const axis_view = plot_view.renderer_view(axis)!
 
-    expect(axis_view.compute_labels([0, 2, 4.0, 6, 8, 10])).to.be.equal(["zero", "2", "four", "6", "8", "ten"])
+    const labels = axis_view.compute_labels([0, 2, 4.0, 6, 8, 10])
+    expect(labels.map((l) => l.text)).to.be.equal(["zero", "2", "four", "6", "8", "ten"])
   })
 
   it("loc should return numeric fixed_location", async () => {
@@ -146,25 +147,5 @@ describe("AxisView", () => {
   it("_tick_extent should return the major_tick_out property", async () => {
     const {axis, axis_view} = await build()
     expect(axis_view._tick_extent()).to.be.equal(axis.major_tick_out)
-  })
-
-  it("_axis_label_extent should be greater than axis_label_standoff", async () => {
-    const {axis, axis_view} = await build({axis_label: 'Left axis label'})
-    expect(axis_view._axis_label_extent()).to.be.above(axis.axis_label_standoff)
-  })
-
-  it("_axis_label_extent should be greater than the font_size", async () => {
-    const {axis_view} = await build({axis_label: 'Left axis label'})
-    expect(axis_view._axis_label_extent()).to.be.above(13.3333)
-  })
-
-  it("_axis_label_extent should be 0 if axis_label is null", async () => {
-    const {axis_view} = await build({axis_label: null})
-    expect(axis_view._axis_label_extent()).to.be.equal(0)
-  })
-
-  it("_axis_label_extent should be 0 if axis_label is empty", async () => {
-    const {axis_view} = await build({axis_label: ""})
-    expect(axis_view._axis_label_extent()).to.be.equal(0)
   })
 })
