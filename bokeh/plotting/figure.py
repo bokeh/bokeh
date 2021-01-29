79f8f31f2be2058f2e3013ba9e86110ce33c9e09
Mateusz Paprocki <mattpap@gmail.com>
2021-01-29 02:36:08 +0100

    Make FigureOpts.{x,y}_axis_label nullable

--- bokeh/plotting/figure.py
+++ bokeh/plotting/figure.py
@@ -1629,11 +1629,11 @@ class FigureOptions(Options):
     Where the y-axis should be located.
     """)
 
-    x_axis_label = String(default="", help="""
+    x_axis_label = Nullable(String, default="", help="""
     A label for the x-axis.
     """)
 
-    y_axis_label = String(default="", help="""
+    y_axis_label = Nullable(String, default="", help="""
     A label for the y-axis.
     """)
 
