bokeh/bokeh

commit 62f64960b85aa22395565a9bf3d62c6f92dff087

Redesign ColorBar in terms of the layout (#10779)
* Statically type parent view in build_view*()

* Redesign ColorBar in terms of the layout

* Simplify configuration of min_border

* Allow panels to provide own layout

* Add support for categorical color bars

* Propagate visuals to color bar's components

* Add Title.standoff instead of using magic constants

* Rename SidePanel to SideLayout

* Use the correct inner_bbox in PlotView

* Allow center panel to have a layout

* Update tests

* Allow nullable ColorBar.title

* Loosen checks in CartesianFrame._get_scales()

* Make ColorBar update somewhat work

* Remove pointless visual property overrides

* Update visual baselines

* Allow to test ColorBar's internal state

* Simplify ColorBar's usage examples

* Change ColorBar.{ticker,formatter} default to "auto"

* Move attrs_of() to core/property_mixins

* Simplify interactive_hit() interface

* Use Anchor enum instead of LegendLocation

* Add support for "auto" ColorBar.orientation

* Lazy initialize renderer's coordinate system

* Handle ColorBar.visible property correctly

* Add migration notes

* Handle absolute/relative layout uniformly

* Add layout debug mode (settings.wireframe)

* Allow plain source objects in bokehjs API

* Allow to generate random floats in a range

* Allow to select random items from a sequence

* Fix axis location when orientations don't match

* Correctly compute ColorBar's padding box

* Handle margin when added to the center panel

* Compute row/col dimensions in one pass

* Finalize positioning within the central panel

* Hack around layout's border support

* Remove AnchorLayout, use NodeLayout

* Don't redefine layout's `absolute` attribute

* Fix computation of Grid's absolute inner bbox

* Restore support for location=[x, y]

* Restore ColorBar's "nice" sizing heuristic

* Add migration note regarding CartesianFrame

bokehjs/src/lib/core/layout/grid.ts
@@ -41,11 +41,14 @@ export type ItemSizeHint = {
   size_hint: SizeHint
 }
 
-export type GridSizeHint = {
+export type GridSize = {
   size: Size
-  size_hints: Container<ItemSizeHint>
   row_heights: number[]
   col_widths: number[]
+}
+
+export type GridSizeHint = GridSize & {
+  size_hints: Container<ItemSizeHint>
 }
 
 type TrackAlign = "auto" | Align
@@ -137,12 +140,16 @@ class Container<T> {
 }
 
 export class Grid extends Layoutable {
+  *[Symbol.iterator]() {
+    for (const {layout} of this.items) {
+      yield layout
+    }
+  }
+
   rows: RowsSizing = "auto"
   cols: ColsSizing = "auto"
 
   spacing: number | [number, number] = 0
-
-  absolute: boolean = false
 
   private _state: GridState
 
@@ -346,7 +353,7 @@ export class Grid extends Layoutable {
     return {size, row_heights, col_widths, size_hints}
   }
 
-  protected _measure_grid(viewport: Size): GridSizeHint {
+  protected _measure_grid(viewport: Size): GridSize {
     const {nrows, ncols, rows, cols, rspacing, cspacing} = this._state
 
     const preferred = this._measure_cells((y, x) => {
@@ -458,15 +465,10 @@ export class Grid extends Layoutable {
       }
     }
 
-    const {row_heights, col_widths, size_hints} = this._measure_cells((y, x) => {
-      return {
-        width: preferred.col_widths[x],
-        height: preferred.row_heights[y],
-      }
-    })
-
+    const {row_heights, col_widths} = preferred
     const size = this._measure_totals(row_heights, col_widths)
-    return {size, row_heights, col_widths, size_hints}
+
+    return {size, row_heights, col_widths}
   }
 
   protected _measure(viewport: Size): SizeHint {
@@ -479,7 +481,13 @@ export class Grid extends Layoutable {
 
     const {nrows, ncols, rspacing, cspacing} = this._state
 
-    const {row_heights, col_widths, size_hints} = this._measure_grid(outer)
+    const {row_heights, col_widths} = this._measure_grid(outer)
+    const {size_hints} = this._measure_cells((y, x) => {
+      return {
+        width: col_widths[x],
+        height: row_heights[y],
+      }
+    })
 
     const rows = this._state.rows.map((row, r): RowSpec & {top: number, height: number, bottom: number} => {
       return {...row, top: 0, height: row_heights[r], get bottom() { return this.top + this.height }}
@@ -587,10 +595,15 @@ export class Grid extends Layoutable {
     items.foreach(({r0, c0, r1, c1}, item) => {
       const {size_hint, outer} = item
 
-      function inner_bbox({left, right, top, bottom}: Extents) {
-        const width = outer.width - left - right
-        const height = outer.height - top - bottom
-        return new BBox({left, top, width, height})
+      const inner_bbox = (extents: Extents) => {
+        const outer_bbox = this.absolute ? outer : outer.relative()
+
+        const left = outer_bbox.left + extents.left
+        const top = outer_bbox.top + extents.top
+        const right = outer_bbox.right - extents.right
+        const bottom = outer_bbox.bottom - extents.bottom
+
+        return new BBox({left, top, right, bottom})
       }
 
       if (size_hint.inner != null) {

bokehjs/test/integration/regressions.ts
@@ -86,7 +86,7 @@ describe("Bug", () => {
       let y = 0
       const w = 1, h = 1
 
-      for (const anchor of Anchor) {
+      for (const anchor of [...Anchor].slice(0, 9)) {
         p.image_url({url: [img], x: 0, y, w, h, anchor, angle: 0})
         p.image_url({url: [img], x: 1, y, w, h, anchor, angle: Math.PI/6})
         p.image_url({url: [img], x: 2, y, w, h, anchor, angle: Math.PI/4})
@@ -401,7 +401,7 @@ describe("Bug", () => {
       const img = svg_image(svg)
 
       const plots = []
-      for (const anchor of Anchor) {
+      for (const anchor of [...Anchor].slice(0, 9)) {
         const x_range = new DataRange1d()
         const y_range = new DataRange1d()
         const p = fig([200, 200], {x_range, y_range, title: anchor, match_aspect: true})

commit d59587f8439fb578a8a66ac18c56fd0c986f01ba

Fix layout regressions after PR #10779 (#10894)
* Reevaluate initial sizing in grid layout

* Use correct layout in app/movies

* Allow to use an intermediate el in display()

* Add visual integration tests

bokehjs/src/lib/core/layout/grid.ts
@@ -356,6 +356,110 @@ export class Grid extends Layoutable {
   protected _measure_grid(viewport: Size): GridSize {
     const {nrows, ncols, rows, cols, rspacing, cspacing} = this._state
 
+    const distribute_height = (size_hint: GridSizeHint) => {
+      let available_height: number
+      if (this.sizing.height_policy == "fixed" && this.sizing.height != null)
+        available_height = this.sizing.height
+      else if (viewport.height != Infinity && this.is_height_expanding())
+        available_height = viewport.height
+      else
+        available_height = size_hint.size.height
+
+      let height_flex = 0
+      for (let y = 0; y < nrows; y++) {
+        const row = rows[y]
+        if (row.policy == "fit" || row.policy == "max")
+          height_flex += row.flex
+        else
+          available_height -= size_hint.row_heights[y]
+      }
+
+      available_height -= (nrows - 1)*rspacing
+
+      if (height_flex != 0 && available_height > 0) {
+        for (let y = 0; y < nrows; y++) {
+          const row = rows[y]
+          if (row.policy == "fit" || row.policy == "max") {
+            const height = round(available_height * (row.flex/height_flex))
+            available_height -= height
+            size_hint.row_heights[y] = height
+            height_flex -= row.flex
+          }
+        }
+      } else if (available_height < 0) {
+        let nadjustable = 0
+        for (let y = 0; y < nrows; y++) {
+          const row = rows[y]
+          if (row.policy != "fixed")
+            nadjustable++
+        }
+
+        let overflow_height = -available_height
+        for (let y = 0; y < nrows; y++) {
+          const row = rows[y]
+          if (row.policy != "fixed") {
+            const height = size_hint.row_heights[y]
+            const cutoff = round(overflow_height/nadjustable)
+            size_hint.row_heights[y] = max(height - cutoff, 0)
+            overflow_height -= cutoff > height ? height : cutoff
+            nadjustable--
+          }
+        }
+      }
+    }
+
+    const distribute_width = (size_hint: GridSizeHint) => {
+      let available_width: number
+      if (this.sizing.width_policy == "fixed" && this.sizing.width != null)
+        available_width = this.sizing.width
+      else if (viewport.width != Infinity && this.is_width_expanding())
+        available_width = viewport.width
+      else
+        available_width = size_hint.size.width
+
+      let width_flex = 0
+      for (let x = 0; x < ncols; x++) {
+        const col = cols[x]
+        if (col.policy == "fit" || col.policy == "max")
+          width_flex += col.flex
+        else
+          available_width -= size_hint.col_widths[x]
+      }
+
+      available_width -= (ncols - 1)*cspacing
+
+      if (width_flex != 0 && available_width > 0) {
+        for (let x = 0; x < ncols; x++) {
+          const col = cols[x]
+          if (col.policy == "fit" || col.policy == "max") {
+            const width = round(available_width * (col.flex/width_flex))
+            available_width -= width
+            size_hint.col_widths[x] = width
+            width_flex -= col.flex
+          }
+        }
+      } else if (available_width < 0) {
+        let nadjustable = 0
+        for (let x = 0; x < ncols; x++) {
+          const col = cols[x]
+          if (col.policy != "fixed")
+            nadjustable++
+        }
+
+        let overflow_width = -available_width
+        for (let x = 0; x < ncols; x++) {
+          const col = cols[x]
+          if (col.policy != "fixed") {
+            const width = size_hint.col_widths[x]
+            const cutoff = round(overflow_width/nadjustable)
+            size_hint.col_widths[x] = max(width - cutoff, 0)
+            overflow_width -= cutoff > width ? width : cutoff
+            nadjustable--
+          }
+        }
+      }
+    }
+
     const preferred = this._measure_cells((y, x) => {
       const row = rows[y]
       const col = cols[x]
@@ -365,107 +469,20 @@ export class Grid extends Layoutable {
       }
     })
 
-    let available_height: number
-    if (this.sizing.height_policy == "fixed" && this.sizing.height != null)
-      available_height = this.sizing.height
-    else if (viewport.height != Infinity && this.is_height_expanding())
-      available_height = viewport.height
-    else
-      available_height = preferred.size.height
+    distribute_height(preferred)
+    distribute_width(preferred)
 
-    let height_flex = 0
-    for (let y = 0; y < nrows; y++) {
-      const row = rows[y]
-      if (row.policy == "fit" || row.policy == "max")
-        height_flex += row.flex
-      else
-        available_height -= preferred.row_heights[y]
-    }
-
-    available_height -= (nrows - 1)*rspacing
-
-    if (height_flex != 0 && available_height > 0) {
-      for (let y = 0; y < nrows; y++) {
-        const row = rows[y]
-        if (row.policy == "fit" || row.policy == "max") {
-          const height = round(available_height * (row.flex/height_flex))
-          available_height -= height
-          preferred.row_heights[y] = height
-          height_flex -= row.flex
-        }
+    const adjusted = this._measure_cells((y, x) => {
+      return {
+        width: preferred.col_widths[x],
+        height: preferred.row_heights[y],
       }
-    } else if (available_height < 0) {
-      let nadjustable = 0
-      for (let y = 0; y < nrows; y++) {
-        const row = rows[y]
-        if (row.policy != "fixed")
-          nadjustable++
-      }
+    })
 
-      let overflow_height = -available_height
-      for (let y = 0; y < nrows; y++) {
-        const row = rows[y]
-        if (row.policy != "fixed") {
-          const height = preferred.row_heights[y]
-          const cutoff = round(overflow_height/nadjustable)
-          preferred.row_heights[y] = max(height - cutoff, 0)
-          overflow_height -= cutoff > height ? height : cutoff
-          nadjustable--
-        }
-      }
-    }
+    distribute_height(adjusted)
+    distribute_width(adjusted)
 
-    let available_width: number
-    if (this.sizing.width_policy == "fixed" && this.sizing.width != null)
-      available_width = this.sizing.width
-    else if (viewport.width != Infinity && this.is_width_expanding())
-      available_width = viewport.width
-    else
-      available_width = preferred.size.width
-
-    let width_flex = 0
-    for (let x = 0; x < ncols; x++) {
-      const col = cols[x]
-      if (col.policy == "fit" || col.policy == "max")
-        width_flex += col.flex
-      else
-        available_width -= preferred.col_widths[x]
-    }
-
-    available_width -= (ncols - 1)*cspacing
-
-    if (width_flex != 0 && available_width > 0) {
-      for (let x = 0; x < ncols; x++) {
-        const col = cols[x]
-        if (col.policy == "fit" || col.policy == "max") {
-          const width = round(available_width * (col.flex/width_flex))
-          available_width -= width
-          preferred.col_widths[x] = width
-          width_flex -= col.flex
-        }
-      }
-    } else if (available_width < 0) {
-      let nadjustable = 0
-      for (let x = 0; x < ncols; x++) {
-        const col = cols[x]
-        if (col.policy != "fixed")
-          nadjustable++
-      }
-
-      let overflow_width = -available_width
-      for (let x = 0; x < ncols; x++) {
-        const col = cols[x]
-        if (col.policy != "fixed") {
-          const width = preferred.col_widths[x]
-          const cutoff = round(overflow_width/nadjustable)
-          preferred.col_widths[x] = max(width - cutoff, 0)
-          overflow_width -= cutoff > width ? width : cutoff
-          nadjustable--
-        }
-      }
-    }
-
-    const {row_heights, col_widths} = preferred
+    const {row_heights, col_widths} = adjusted
     const size = this._measure_totals(row_heights, col_widths)
 
     return {size, row_heights, col_widths}

examples/app/movies/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas.io.sql as psql
 
 from bokeh.io import curdoc
-from bokeh.layouts import column, layout
+from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Div, Select, Slider, TextInput
 from bokeh.plotting import figure
 from bokeh.sampledata.movies_data import movie_path
@@ -103,12 +103,9 @@ controls = [reviews, boxoffice, genre, min_year, max_year, oscars, director, cas
 for control in controls:
     control.on_change('value', lambda attr, old, new: update())
 
-inputs = column(*controls, width=320, height=1000)
-inputs.sizing_mode = "fixed"
-l = layout([
-    [desc],
-    [inputs, p],
-], sizing_mode="scale_both")
+inputs = column(*controls, width=320)
+
+l = column(desc, row(inputs, p), sizing_mode="scale_both")
 
 update()  # initial load of the data
 

bokehjs/test/baselines/Bug__in_issue_#10851__results_in_incorrect_layout_when_viewport_is_larger_than_optimal_size
@@ -1,0 +1,7 @@
+Row bbox=[0, 0, 450, 225]
+  Plot bbox=[0, 0, 225, 225]
+    LinearAxis bbox=[28, 200, 192, 25]
+    LinearAxis bbox=[0, 5, 28, 195]
+  Plot bbox=[225, 0, 225, 225]
+    LinearAxis bbox=[28, 200, 192, 25]
+    LinearAxis bbox=[0, 5, 28, 195]

bokehjs/test/baselines/Bug__in_issue_#10851__results_in_incorrect_layout_when_viewport_is_smaller_than_optimal_size
@@ -1,0 +1,7 @@
+Row bbox=[0, 0, 350, 175]
+  Plot bbox=[0, 0, 175, 175]
+    LinearAxis bbox=[28, 150, 142, 25]
+    LinearAxis bbox=[0, 5, 28, 145]
+  Plot bbox=[175, 0, 175, 175]
+    LinearAxis bbox=[28, 150, 142, 25]
+    LinearAxis bbox=[0, 5, 28, 145]

bokehjs/test/framework.ts
@@ -255,7 +255,7 @@ async function _run_test(suites: Suite[], test: Test): Promise<PartialResult> {
   return {error, time}
 }
 
-export async function display<T extends LayoutDOM>(obj: T, viewport?: [number, number]): Promise<{view: ViewOf<T>, el: HTMLElement}> {
+export async function display<T extends LayoutDOM>(obj: T, viewport?: [number, number], el?: HTMLElement): Promise<{view: ViewOf<T>, el: HTMLElement}> {
   const margin = 50
   const [width, height] = (() => {
     if (viewport != null)
@@ -269,12 +269,12 @@ export async function display<T extends LayoutDOM>(obj: T, viewport?: [number, n
       }
     }
   })()
-  const el = div({style: {width: `${width}px`, height: `${height}px`, overflow: "hidden"}})
-  document.body.appendChild(el)
-  const view = await show(obj, el)
+  const vp = div({style: {width: `${width}px`, height: `${height}px`, overflow: "hidden"}}, el)
+  document.body.appendChild(vp)
+  const view = await show(obj, el ?? vp)
   current_test!.view = view
-  current_test!.el = el
-  return {view, el}
+  current_test!.el = vp
+  return {view, el: vp}
 }
 
 import {sum} from "@bokehjs/core/util/array"

bokehjs/test/integration/regressions.ts
@@ -29,6 +29,7 @@ import {Matrix} from "@bokehjs/core/util/matrix"
 import {defer} from "@bokehjs/core/util/defer"
 import {Figure, MarkerArgs} from "@bokehjs/api/plotting"
 import {Spectral11} from "@bokehjs/api/palettes"
+import {div} from "@bokehjs/core/dom"
 
 const n_marker_types = [...MarkerType].length
 
@@ -776,7 +777,6 @@ describe("Bug", () => {
 
   describe("in issue #10541", () => {
     it("prevents Slope with gradient=0", async () => {
-
       const p = fig([200, 200], {x_range: [-5, 5], y_range: [-5, 5]})
 
       for (const gradient of [1, -1, 0, 2, -0.5]){
@@ -811,6 +811,28 @@ describe("Bug", () => {
 
       p.circle(0, 0, {radius: 1})
       await view.ready
+    })
+  })
+
+  describe("in issue #10851", () => {
+    function box(width: number, height: number): HTMLElement {
+      return div({style: {width: `${width}px`, height: `${height}px`, backgroundColor: "red"}})
+    }
+
+    function layout() {
+      const p0 = fig([200, 200], {sizing_mode: "scale_width", background_fill_alpha: 0.5, border_fill_alpha: 0.5})
+      const p1 = fig([200, 200], {sizing_mode: "scale_width", background_fill_alpha: 0.5, border_fill_alpha: 0.5})
+      p0.circle([0, 1, 2], [3, 4, 5])
+      p1.circle([1, 2, 3], [4, 5, 6])
+      return row([p0, p1], {sizing_mode: "scale_width", background: "orange"})
+    }
+
+    it("results in incorrect layout when viewport is smaller than optimal size", async () => {
+      await display(layout(), [500, 300], box(350, 175))
+    })
+
+    it("results in incorrect layout when viewport is larger than optimal size", async () => {
+      await display(layout(), [500, 300], box(450, 225))
     })
   })
 