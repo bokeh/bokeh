b2ed773db76f87cc1fb0e29a74ba46276ede1ecc
azjps <azjps@users.noreply.github.com>
2021-03-08 12:34:44 +1300

    Minor, apply black formatting for example

--- examples/models/file/datetime_tz_formatter.py
+++ examples/models/file/datetime_tz_formatter.py
@@ -1,7 +1,7 @@
 """
 Example demonstrating DatetimeTickFormatter with tick labels displayed in different timezones.
 """
-from bokeh.io import output_notebook, output_file, show
+from bokeh.io import output_file, show  # output_notebook
 from bokeh.models import ColumnDataSource, FuncTickFormatter, DatetimeTickFormatter
 from bokeh.models.axes import DatetimeAxis
 from bokeh.models.tools import HoverTool
@@ -20,7 +20,8 @@ t = pd.Timestamp("2021/02/11 23:01-05:00")
 # one can store sub-millisecond values in a separate column for a given
 # ColumnDataSource and use the printf formatter to display those values.
 interval = np.timedelta64(250, "ms") + np.timedelta64(50, "us")
-df = pd.DataFrame({"t": [t, t + interval, t + 2*interval], "v": [1, 3, 2]})
+df = pd.DataFrame({"t": [t, t + interval, t + 2 * interval], "v": [1, 3, 2]})
+
 
 def make_datetime_tz_formatter(display_timezone, include_date=False):
     """Creates a bokeh.TickFormatter to display datetimes with TZ
@@ -56,6 +57,7 @@ return result;
     js_code = Template(js_code_template).render(display_timezone=display_timezone, include_date=include_date)
     return FuncTickFormatter(code=js_code)
 
+
 def make_figure():
     # note: bokeh datetime tooltip formatter is limited to millisecond precision. Sub-millisecond precision
     # would have to be provided separately.
@@ -70,28 +72,35 @@ def make_figure():
         HoverTool(tooltips=[("time", "($x{%H:%M:%S.%N}, $y)")], formatters={"$x": "datetime"}),
     ]
     f = figure(x_axis_type="datetime", height=800, width=800, tools=tools, toolbar_location="above")
-    
+
     datetime_fmt = {
-            "microseconds": "%H:%M:%S.%f",
-            "milliseconds": "%H:%M:%S.%f",
-            "seconds": "%H:%M:%S",
-            "minsec": "%H:%M:%S",
-            "minutes": "%H:%M:%S",
-            "hourmin": "%H:%M:%S",
-            "hours": "%H:%M:%S",
-            "days": "%x",
+        "microseconds": "%H:%M:%S.%f",
+        "milliseconds": "%H:%M:%S.%f",
+        "seconds": "%H:%M:%S",
+        "minsec": "%H:%M:%S",
+        "minutes": "%H:%M:%S",
+        "hourmin": "%H:%M:%S",
+        "hours": "%H:%M:%S",
+        "days": "%x",
     }
-    
+
     source = ColumnDataSource(df)
     f.line(x="t", y="v", source=source)
     extra_axis = DatetimeAxis(axis_label="DatetimeTickFormatter", formatter=DatetimeTickFormatter(**datetime_fmt))
     f.add_layout(extra_axis, "below")
     for tz in ("UTC", "US/Eastern", "Asia/Tokyo", "Asia/Calcutta"):
-        extra_axis = DatetimeAxis(axis_label="FuncTickFormatter:" + tz, formatter=make_datetime_tz_formatter(tz, include_date=True))
+        extra_axis = DatetimeAxis(
+            axis_label="FuncTickFormatter:" + tz,
+            formatter=make_datetime_tz_formatter(tz, include_date=True),
+        )
         f.add_layout(extra_axis, "below")
-        extra_axis = DatetimeAxis(axis_label="DatetimeTickFormatter:" + tz, formatter=DatetimeTickFormatter(timezone=tz, **datetime_fmt))
+        extra_axis = DatetimeAxis(
+            axis_label="DatetimeTickFormatter:" + tz,
+            formatter=DatetimeTickFormatter(timezone=tz, **datetime_fmt),
+        )
         f.add_layout(extra_axis, "below")
     return f
+
 
 output_file("datetime_tz_formatter.html", title="Timezones")
 show(make_figure())
