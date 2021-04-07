da5022d8156f603207d39587e06c05bc0b74acaa
Bryan Van de Ven <bryan@bokeh.org>
2021-04-07 15:17:38 -0700

    copyright comment block for conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2021, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
 # Standard library imports
 import os
 from datetime import date
