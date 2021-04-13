9df3e99fe6551bd35e0122376c3253ab009d6bfb
Mateusz Paprocki <mattpap@gmail.com>
2021-04-14 00:48:26 +0200

    Update plotting/file/periodic

--- examples/plotting/file/periodic.py
+++ examples/plotting/file/periodic.py
@@ -16,6 +16,32 @@ df = df[df.group != "-"]
 df = df[df.symbol != "Lr"]
 df = df[df.symbol != "Lu"]
 
+# 1s2
+# [He] 2s2 2p6
+# [Ne] 3s2 3p6
+# [Ar] 3d10 4s2 4p4
+# ->
+# 1s2 2s2 2p6 3s2 3p6 3d10 4s2 4p4
+import re
+econf = "electronic configuration"
+ecs = []
+for ec in df[econf]:
+    n = 1
+    while n != 0:
+        replace = lambda m: df[df.symbol == m.group("name")][econf].values[0]
+        (ec, n) = re.subn(r"\[(?P<name>\w+)\]", replace, ec)
+
+    sc = {}
+    for m in re.finditer(r"(?P<shell>\d+)\w(?P<count>\d+)", ec):
+        s = int(m.group("shell"))
+        c = int(m.group("count"))
+        sc.setdefault(s, 0)
+        sc[s] += c
+
+    ecs.append(list(sc.values()))
+df["electron shells"] = ecs
+print(df["electron shells"])
+
 cmap = {
     "alkali metal"         : "#a6cee3",
     "alkaline earth metal" : "#1f78b4",
@@ -36,9 +62,81 @@ TOOLTIPS = [
     ("Electronic configuration", "@{electronic configuration}"),
 ]
 
+from math import sin, cos, radians
+import numpy as np
+def f():
+    fs = []
+    for sc in df["electron shells"]:
+        n = len(sc)
+        e = 0.2
+        bohr_diagram = figure(
+            x_range=(-n-e, n+e), y_range=(-n-e, n+e),
+            width=200, height=200,
+            x_axis_type=None, y_axis_type=None,
+            toolbar_location=None, outline_line_color=None,
+        )
+        #sc = [2, 8, 18, 32, 32, 18, 8]
+        # shells
+        #g = RendererGroup()
+        #print(sc)
+        bohr_diagram.circle(x=0, y=0, radius=list(range(1, n+1)), fill_color=None, line_color="black")
+        # electrons
+        for i, c in enumerate(sc):
+            da = 360/c
+            r = i + 1
+            A = np.radians(np.arange(0, 360, da))
+            x = r*np.cos(A)
+            y = r*np.sin(A)
+            bohr_diagram.circle(x=x, y=y)
+            #bohr_diagram.polar.circle(r=i+1, phi=list(range(0, 360, da)), phi_units="deg")
+        fs.append(bohr_diagram)
+#f()
+    return fs
+
+from bokeh.models.dom import Style, Template, Table, TableRow, Div, Span, Index, ValueRef, ColorRef, VBox
+
+def g():
+    bohr_diagram = f()
+
+    grid = Div(style=dict(display="grid", grid_template_columns="auto auto"))
+    grid.children = [
+        Span(),                     Span(children=["#", Index()]),
+        "Name",                     Span(style=dict(font_weight="bold"), children=[ValueRef(field="name")]),
+        "Atomic number",            ValueRef(field="atomic number"),
+        "Atomic mass",              ValueRef(field="atomic mass"),
+        "Type",                     ValueRef(field="metal"),
+        "CPK color",                ColorRef(field="CPK", hex=True, swatch=True),
+        "Electronic configuration", ValueRef(field="electronic configuration"),
+        Span(),                     bohr_diagram[25],
+    ]
+    return Template(children=[grid])
+
+    """
+    _TOOLTIPS = Table(children=[
+        TableRow(children=["",                         Span(children=["#", Index()])]),
+        TableRow(children=["Name",                     ValueRef(field="name")]),
+        TableRow(children=["Atomic number",            ValueRef(field="atomic number")]),
+        TableRow(children=["Atomic mass",              ValueRef(field="atomic mass")]),
+        TableRow(children=["Type",                     ValueRef(field="metal")]),
+        TableRow(children=["CPK color",                ColorRef(field="CPK", hex=True, swatch=True)]),
+        TableRow(children=["Electronic configuration", ValueRef(field="electronic configuration")]),
+        TableRow(children=["",                         bohr_diagram]),
+        TableRow(children=["Electronic configuration", VBox(children=[
+            ValueRef(field="electronic configuration"),
+            bohr_diagram,
+        ])]),
+    ])
+    return Template(children=[_TOOLTIPS])
+    """
+
+    #g = Grid(style=Style(border_width="1px"))
+    #g.add
+
+#g()
+
 p = figure(title="Periodic Table (omitting LA and AC Series)", width=1000, height=450,
            x_range=groups, y_range=list(reversed(periods)),
-           tools="hover", toolbar_location=None, tooltips=TOOLTIPS)
+           tools="hover", toolbar_location=None, tooltips=g()) #TOOLTIPS) #g())
 
 r = p.rect("group", "period", 0.95, 0.95, source=df, fill_alpha=0.6, legend_field="metal",
            color=factor_cmap('metal', palette=list(cmap.values()), factors=list(cmap.keys())))
