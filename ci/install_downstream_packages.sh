c4495ff2c57660bb6254e0a3360a41547a9da158
James Bourbeau <jrbourbeau@gmail.com>
2021-04-26 11:43:28 -0500

    Include Dask testing dependencies in downstream CI

--- ci/install_downstream_packages.sh
+++ ci/install_downstream_packages.sh
@@ -2,8 +2,8 @@
 
 set -x #echo on
 
-conda install --yes --quiet dask distributed
 conda install --yes --quiet -c pyviz/label/dev holoviews nose
+pip install dask["test"] distributed  # "test" extra installs additional testing dependencies
 pip install pandas_bokeh
 pip install geopandas
 git clone https://github.com/PatrikHlobil/Pandas-Bokeh.git
