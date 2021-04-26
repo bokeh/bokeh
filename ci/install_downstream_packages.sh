d6b9f90c7467906123796fd7ae38cc2f247e9bc0
James Bourbeau <jrbourbeau@users.noreply.github.com>
2021-04-26 17:36:37 -0500

    Include Dask testing dependencies in downstream CI (#11207)
    * Include Dask testing dependencies in downstream CI
    
    * Uninstall flaky in downstream CI build
    
    * Add --yes
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

--- ci/install_downstream_packages.sh
+++ ci/install_downstream_packages.sh
@@ -2,8 +2,11 @@
 
 set -x #echo on
 
-conda install --yes --quiet dask distributed
 conda install --yes --quiet -c pyviz/label/dev holoviews nose
+pip install dask["test"] distributed  # "test" extra installs additional testing dependencies
+# This is a temporary workaround to avoid clashing between `flaky` and `pytest-rerunfailures`.
+# See https://github.com/bokeh/bokeh/issues/11211
+pip uninstall --yes flaky
 pip install pandas_bokeh
 pip install geopandas
 git clone https://github.com/PatrikHlobil/Pandas-Bokeh.git
