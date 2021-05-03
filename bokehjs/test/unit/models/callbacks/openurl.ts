e6da09a7d6d7ac93f732219d2ba5383c12ffa70e
Bryan Van de Ven <bryan@bokeh.org>
2021-05-03 16:59:16 -0700

    more tests

--- bokehjs/test/unit/models/callbacks/openurl.ts
+++ bokehjs/test/unit/models/callbacks/openurl.ts
@@ -8,20 +8,77 @@ import {ColumnDataSource} from "@bokehjs/models/sources/column_data_source"
 describe("OpenURL", () => {
   describe("default creation", () => {
     const cb = new OpenURL()
-
     it("should have default url", () => {
       expect(cb.url).to.be.equal("http://")
     })
   })
-  describe("URL escaping", () => {
-    it("should properly escape urls with slashes", () => {
-      const source = new ColumnDataSource({data: {foo: ["bar"]}})
+
+  describe("URL replacements", () => {
+    it("should handle fieldnames with spaces", () => {
+      const source = new ColumnDataSource({data: {"foo bar": ["baz"]}})
       source.selected.indices = [0]
-      const cb = new OpenURL({url: "http://@foo.com"})
+      const cb = new OpenURL({url: "http://stuff.com/@{foo bar}.html"})
       const raise_spy = sinon.spy(cb, "raise")
       cb.execute(undefined, {source})
       expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://stuff.com/baz.html"])
+    })
+    it("should escape spaces", () => {
+      const source = new ColumnDataSource({data: {foo: ["bar baz/index.html"]}})
+      source.selected.indices = [0]
+      const cb = new OpenURL({url: "http://stuff.com/@foo"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://stuff.com/bar%20baz/index.html"])
+    })
+    it("should not escape slashes", () => {
+      const source = new ColumnDataSource({data: {foo: ["bar/baz/index.html"]}})
+      source.selected.indices = [0]
+      const cb = new OpenURL({url: "http://stuff.com/@foo"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://stuff.com/bar/baz/index.html"])
+    })
+    it("should not escape anchors", () => {
+      const source = new ColumnDataSource({data: {foo: ["index.html#foo"]}})
+      source.selected.indices = [0]
+      const cb = new OpenURL({url: "http://stuff.com/@foo"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://stuff.com/index.html#foo"])
+    })
+    it("should not escape request args", () => {
+      const source = new ColumnDataSource({data: {foo: ["index.html?name=ferret&color=purple"]}})
+      source.selected.indices = [0]
+      const cb = new OpenURL({url: "http://stuff.com/@foo"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://stuff.com/index.html?name=ferret&color=purple"])
+    })
+  })
+
+  describe("Selection handling", () => {
+    it("should should not raise for empty selection", () => {
+      const source = new ColumnDataSource({data: {foo: ["bar", "baz", "quux"]}})
+      source.selected.indices = []
+      const cb = new OpenURL({url: "http://@foo.com"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.called).to.be.false
+    })
+    it("should should raise for every selection index", () => {
+      const source = new ColumnDataSource({data: {foo: ["bar", "baz", "quux"]}})
+      source.selected.indices = [0, 2]
+      const cb = new OpenURL({url: "http://@foo.com"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledTwice).to.be.true
       expect(raise_spy.args[0]).to.be.equal(["http://bar.com"])
+      expect(raise_spy.args[1]).to.be.equal(["http://quux.com"])
     })
   })
 })
