7fb8b527512e19f7e0dc37af98e17242238ed278
Bryan Van de Ven <bryan@bokeh.org>
2021-05-03 09:03:12 -0700

    initial tests

--- bokehjs/test/unit/models/callbacks/openurl.ts
+++ bokehjs/test/unit/models/callbacks/openurl.ts
@@ -1,13 +1,27 @@
 import {expect} from "assertions"
 
+import sinon from "sinon"
+
 import {OpenURL} from "@bokehjs/models/callbacks/open_url"
+import {ColumnDataSource} from "@bokehjs/models/sources/column_data_source"
 
 describe("OpenURL", () => {
   describe("default creation", () => {
-    const r = new OpenURL()
+    const cb = new OpenURL()
 
     it("should have default url", () => {
-      expect(r.url).to.be.equal("http://")
+      expect(cb.url).to.be.equal("http://")
+    })
+  })
+  describe("URL escaping", () => {
+    it("should properly escape urls with slashes", () => {
+      const source = new ColumnDataSource({data: {foo: ["bar"]}})
+      source.selected.indices = [0]
+      const cb = new OpenURL({url: "http://@foo.com"})
+      const raise_spy = sinon.spy(cb, "raise")
+      cb.execute(undefined, {source})
+      expect(raise_spy.calledOnce).to.be.true
+      expect(raise_spy.args[0]).to.be.equal(["http://bar.com"])
     })
   })
 })
