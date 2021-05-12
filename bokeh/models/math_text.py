32141170a6a43c9c2af9216c3214595e65a9fa17
Iury Piva <iurypiva@gmail.com>
2021-05-12 10:17:21 -0300

    Add complete boilerplate and update help strings

--- bokeh/models/math_text.py
+++ bokeh/models/math_text.py
@@ -1,11 +1,50 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2021, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+''' Display a mathematics notation from a string value.
+'''
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import annotations
+
+import logging # isort:skip
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
 # Bokeh imports
 from ..core.properties import Nullable, String
 from ..model import Model
 
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
+
+__all__ = (
+    'MathText',
+)
+
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
 
 class MathText(Model):
     """
-    Class for signaling you want you text converted to LaTex
+    Class for indicating that a string value should be interpreted as
+    mathematics notation.
+
+    Note: it uses MathJax as the rendering engine and it implements only
+    the math-mode macros of TeX and LaTeX, not the text-mode macros.
+
+    You can see more of the supported input and differences between "real"
+    TeX/LaTeX here: https://docs.mathjax.org/en/latest/input/tex/differences.html
     """
 
     text = Nullable(String, help="""
@@ -16,3 +55,15 @@ class MathText(Model):
         super().__init__(**kwargs)
         if len(args) == 1:
             self.text = args[0]
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
