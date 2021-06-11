b909c321de1643609e69cfcd7b1b4f70cf70152a
Mateusz Paprocki <mattpap@gmail.com>
2021-06-11 22:48:53 +0200

    Install typings for boto and pyyaml from pip

--- typings/yaml.pyi
+++
@@ -1,8 +0,0 @@
-from typing import IO, Any, Text, Union
-
-class SafeLoader:
-  pass
-
-def load(stream: Union[bytes, IO[bytes], Text, IO[Text]], Loader: Any = ...) -> Any: ...
-
-def safe_load(stream: Union[bytes, IO[bytes], Text, IO[Text]], Loader: Any = ...) -> Any: ...
