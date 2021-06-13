4b0a694443c897d40e35a86840f132850dc4ee9c
Iury Piva <iurypiva@gmail.com>
2021-06-13 09:24:49 -0300

    change example to use plot instead of figure

--- sphinx/source/docs/user_guide/examples/styling_labels_math_text.py
+++ sphinx/source/docs/user_guide/examples/styling_labels_math_text.py
@@ -1,17 +1,23 @@
 from numpy import arange, pi, sin
-from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource, MathText
+
+from bokeh.models import Circle, ColumnDataSource, LinearAxis, MathText, Plot
+from bokeh.plotting import show
+
 
 x = arange(-2*pi, 2*pi, 0.1)
 y = sin(x)
+
 source = ColumnDataSource(
     data=dict(x=x, y=y)
 )
 
+plot = Plot(min_border=80)
+circle = Circle(x="x", y="y", fill_color="red", size=5)
 x_axis_label = MathText(r"x\cdot\pi")
 y_axis_label = MathText(r"\sin(x)")
 
-p = figure(x_axis_label=x_axis_label, y_axis_label=y_axis_label)
-p.circle(x=x, y=y, fill_color="red", size=5)
+plot.add_glyph(source, circle)
+plot.add_layout(LinearAxis(axis_label=x_axis_label), 'below')
+plot.add_layout(LinearAxis(axis_label=y_axis_label), 'left')
 
-show(p)
+show(plot)
