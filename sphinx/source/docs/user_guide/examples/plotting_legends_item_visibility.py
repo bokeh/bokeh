015ccf23b9c6c17418898fa719fd541f8cd03904
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2021-07-15 11:06:57 -0700

    Update docstring

--- sphinx/source/docs/user_guide/examples/plotting_legends_item_visibility.py
+++ sphinx/source/docs/user_guide/examples/plotting_legends_item_visibility.py
@@ -15,7 +15,7 @@ main = p.circle(x, y, legend_label="Main")
 aux = p.line(x, 2*y, legend_label="Auxillary",
              line_dash=[4, 4], line_color="orange", line_width=2)
 
-# set legend label for aux_1 to False
+# set legend label for second renderer to False
 p.legend.items[1].visible = False
 
 show(p)
