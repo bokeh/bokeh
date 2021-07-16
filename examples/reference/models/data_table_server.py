cc9edd4b3ae1ffe5cff9fe3b3d7c41e42ae6d769
Bryan Van de Ven <bryan@bokeh.org>
2021-07-16 11:02:40 -0700

    Add visible property to Table columns

--- examples/reference/models/data_table_server.py
+++ examples/reference/models/data_table_server.py
@@ -2,7 +2,7 @@
 
 from bokeh.io import curdoc
 from bokeh.layouts import row
-from bokeh.models import ColumnDataSource, DataTable, TableColumn
+from bokeh.models import ColumnDataSource, DataTable, TableColumn, Toggle
 from bokeh.plotting import figure
 
 x=[3,4,6,12,10,1]
@@ -17,17 +17,25 @@ plot_figure.scatter('x', 'y', source=source, size=10)
 
 columns = [
     TableColumn(field="x", title="x"),
-   TableColumn(field="y", title="y")
+    TableColumn(field="y", title="y")
 ]
 data_table = DataTable(
     source=source,
     columns=columns,
     index_position=None,
-    width=80,
+    # width=80,
+    autosize_mode="fit_columns",
     editable=True,
 )
 
-layout=row(plot_figure,data_table)
+toggle = Toggle()
+
+def cb(attr, old, new):
+    columns[0].visible = toggle.active
+
+toggle.on_change('active', cb)
+
+layout=row(plot_figure, data_table, toggle)
 
 curdoc().add_root(layout)
 curdoc().title = "Data-Table Bokeh Server"
