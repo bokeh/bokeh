993fb28be4a0801cc6bb1ed0335cb40abee4ef2c
Andrea PIERRÉ <andrea_pierre@brown.edu>
2021-07-18 12:08:04 -0400

    fix: pandas future warning

--- bokeh/sampledata/commits.py
+++ bokeh/sampledata/commits.py
@@ -58,7 +58,7 @@ def _read_data():
     pd = import_required('pandas', '%s sample data requires Pandas (http://pandas.pydata.org) to be installed' % module)
 
     data = package_csv(module, 'commits.txt.gz', parse_dates=True, header=None, names=['day', 'datetime'], index_col='datetime')
-    data.index = pd.to_datetime(data.index, utc=True,).astype('datetime64[ns]').tz_localize('utc').tz_convert('US/Central')
+    data.index = pd.to_datetime(data.index, utc=True,).tz_convert('US/Central')
     data['time'] = data.index.time
 
     return data
