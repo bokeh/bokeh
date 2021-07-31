1a65bd0eca02b69c22a5fadcd623a25bf1ef775d
Mateusz Paprocki <mattpap@gmail.com>
2021-07-31 12:25:18 +0200

    Use file:// protocol correctly

--- bokeh/io/export.py
+++ bokeh/io/export.py
@@ -235,7 +235,7 @@ def get_screenshot_as_png(obj: LayoutDOM | Document, *, driver: WebDriver | None
 
         web_driver = driver if driver is not None else webdriver_control.get()
         web_driver.maximize_window()
-        web_driver.get("file:///" + tmp.path)
+        web_driver.get(f"file://{tmp.path}")
         wait_until_render_complete(web_driver, timeout)
         [width, height, dpr] = _maximize_viewport(web_driver)
         png = web_driver.get_screenshot_as_png()
@@ -255,7 +255,7 @@ def get_svg(obj: LayoutDOM | Document, *, driver: WebDriver | None = None, timeo
             file.write(html)
 
         web_driver = driver if driver is not None else webdriver_control.get()
-        web_driver.get("file:///" + tmp.path)
+        web_driver.get(f"file://{tmp.path}")
         wait_until_render_complete(web_driver, timeout)
         svgs = cast(List[str], web_driver.execute_script(_SVG_SCRIPT))
 
@@ -271,7 +271,7 @@ def get_svgs(obj: LayoutDOM | Document, *, driver: WebDriver | None = None, time
             file.write(html)
 
         web_driver = driver if driver is not None else webdriver_control.get()
-        web_driver.get("file:///" + tmp.path)
+        web_driver.get(f"file://{tmp.path}")
         wait_until_render_complete(web_driver, timeout)
         svgs = cast(List[str], web_driver.execute_script(_SVGS_SCRIPT))
 
