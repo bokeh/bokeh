b8581c8c595a1dbaf707257b16681277f8a8c20c
Mateusz Paprocki <mattpap@gmail.com>
2021-08-02 00:23:07 +0200

    Simplify canvas' SVG tests

--- bokehjs/test/unit/models/canvas/canvas.ts
+++ bokehjs/test/unit/models/canvas/canvas.ts
@@ -2,6 +2,7 @@ import {expect} from "assertions"
 
 import {Canvas} from "@bokehjs/models/canvas/canvas"
 import {build_view} from "@bokehjs/core/build_views"
+import {SVGRenderingContext2D} from "@bokehjs/core/util/svg"
 
 describe("Canvas", () => {
   describe("should support composing layers", () => {
@@ -50,31 +51,31 @@ describe("Canvas", () => {
       canvas_view.overlays.ctx.fillRect(300, 400, 200, 100)
 
       // TODO: expose SVG canvas/context type information
-      const primary_svg = (canvas_view.primary.ctx as any).get_serialized_svg(true)
-      expect(primary_svg).to.be.equal("\
-<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"600\" height=\"600\">\
+      const primary_svg = (canvas_view.primary.ctx as any as SVGRenderingContext2D).get_serialized_svg(true)
+      expect(primary_svg).to.be.equal('\
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="600" height="600">\
 <defs/>\
-<path fill=\"blue\" stroke=\"none\" paint-order=\"stroke\" d=\"M 100.5 200.5 L 400.5 200.5 L 400.5 600.5 L 100.5 600.5 L 100.5 200.5\"/>\
+<path fill="blue" stroke="none" paint-order="stroke" d="M 100.5 200.5 L 400.5 200.5 L 400.5 600.5 L 100.5 600.5 L 100.5 200.5"/>\
 </svg>\
-")
+')
 
-      const overlays_svg = (canvas_view.overlays.ctx as any).get_serialized_svg(true)
-      expect(overlays_svg).to.be.equal("\
-<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"600\" height=\"600\">\
+      const overlays_svg = (canvas_view.overlays.ctx as any as SVGRenderingContext2D).get_serialized_svg(true)
+      expect(overlays_svg).to.be.equal('\
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="600" height="600">\
 <defs/>\
-<path fill=\"green\" stroke=\"none\" paint-order=\"stroke\" d=\"M 300.5 400.5 L 500.5 400.5 L 500.5 500.5 L 300.5 500.5 L 300.5 400.5\"/>\
+<path fill="green" stroke="none" paint-order="stroke" d="M 300.5 400.5 L 500.5 400.5 L 500.5 500.5 L 300.5 500.5 L 300.5 400.5"/>\
 </svg>\
-")
+')
 
       const composite_layer = canvas_view.compose()
-      const composite_svg = (composite_layer.ctx as any).get_serialized_svg(true)
-      expect(composite_svg).to.be.equal("\
-<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"600\" height=\"600\">\
+      const composite_svg = (composite_layer.ctx as any as SVGRenderingContext2D).get_serialized_svg(true)
+      expect(composite_svg).to.be.equal('\
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="600" height="600">\
 <defs/>\
-<path fill=\"blue\" stroke=\"none\" paint-order=\"stroke\" d=\"M 100.5 200.5 L 400.5 200.5 L 400.5 600.5 L 100.5 600.5 L 100.5 200.5\"/>\
-<path fill=\"green\" stroke=\"none\" paint-order=\"stroke\" d=\"M 300.5 400.5 L 500.5 400.5 L 500.5 500.5 L 300.5 500.5 L 300.5 400.5\"/>\
+<path fill="blue" stroke="none" paint-order="stroke" d="M 100.5 200.5 L 400.5 200.5 L 400.5 600.5 L 100.5 600.5 L 100.5 200.5"/>\
+<path fill="green" stroke="none" paint-order="stroke" d="M 300.5 400.5 L 500.5 400.5 L 500.5 500.5 L 300.5 500.5 L 300.5 400.5"/>\
 </svg>\
-")
+')
     })
   })
 })
