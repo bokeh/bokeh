0b45558db912a6e9a73e6116aeaa6c0ae4d2ae65
Bryan Van de Ven <bryan@bokeh.org>
2021-08-13 13:49:39 -0700

    Docs Refs Cleanup (part 4) (#11499)
    * get rid of half-baked session cb clone
    
    * remove callback cruft on server contexts
    
    * callback ids mandatory except user-facing APIs
    
    * fix some errors via fixing mypy
    
    * some diagnostic/helper output in downstream tests
    
    * clean up downstream runner

--- ci/run_downstream_tests.sh
+++ ci/run_downstream_tests.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
+banner() {
+  echo
+  echo "+------------------------------------------------------------------------------------+"
+  printf "| %-80s   |\n" "$@"
+  echo "+------------------------------------------------------------------------------------+"
+  echo
+}
+
+banner_and_restore() {
+        banner "$*"
+        case "$save_flags" in
+         (*x*)  set -x
+        esac
+}
+
+alias banner='{ save_flags="$-"; set +x;} 2> /dev/null; banner_and_restore'
+
 set -x #echo on
 
 set +e
 
-pushd `python -c "import site; print(site.getsitepackages()[0])"`
-pytest dask/diagnostics
-pytest distributed/dashboard
-pytest panel/tests
-nosetests holoviews/tests/plotting/bokeh
-popd
+pushd "$(python -c 'import site; print(site.getsitepackages()[0])')" || exit
 
+banner "Dask -- dask/diagnostics" 2> /dev/null
+pytest dask/diagnostics
+
+banner "Dask -- distributed/dashboard" 2> /dev/null
+pytest distributed/dashboard
+
+banner "Panel" 2> /dev/null
+pytest panel/tests
+
+banner "Holoviews" 2> /dev/null
+nosetests holoviews/tests/plotting/bokeh
+
+popd || exit
+
+banner "PandasBokeh" 2> /dev/null
 pytest Pandas-Bokeh/Tests/test_PandasBokeh.py
+
+banner "GeoPandasBokeh" 2> /dev/null
 pytest Pandas-Bokeh/Tests/test_GeoPandasBokeh.py
 
 exit 0
