fbcdd7cda5df17a6f0ab66f54ee1c6a2addfc58b
Bryan Van de Ven <bryan@bokeh.org>
2021-08-13 12:00:41 -0700

    update for pep440 versions

--- tests/unit/bokeh/test_resources.py
+++ tests/unit/bokeh/test_resources.py
@@ -83,7 +83,7 @@ class TestSRIHashes:
     # TODO: (bev) conda build on CI is generating bogus versions like "0+untagged.1.g19dd2c8"
     @pytest.mark.skip
     def test_get_all_hashes_no_future_keys(self) -> None:
-        current = V(__version__.split("-", 1)[0])  # remove git hash, "-dirty", etc
+        current = V(__version__.split("+", 1)[0])  # remove git hash, "-dirty", etc
         all_hashes = resources.get_all_sri_hashes()
         for key in all_hashes:
             assert (
