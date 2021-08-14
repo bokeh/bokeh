2dc2c07dac0bff4a090554dbd12357e5bff4137a
Mateusz Paprocki <mattpap@gmail.com>
2021-08-14 10:54:32 +0200

    Make SpatialIndex.bounds() respect search region

--- bokehjs/test/unit/core/util/spatial.ts
+++ bokehjs/test/unit/core/util/spatial.ts
@@ -5,12 +5,15 @@ import * as bbox from "@bokehjs/core/util/bbox"
 
 describe("core/util/spatial module", () => {
   it("support SpatialIndex.bounds()", () => {
-    const ndx = new SpatialIndex(3)
-    ndx.add_rect(0, 1, 1, 2)
-    ndx.add_rect(0, 100, 1, 200)
-    ndx.add_rect(0, 10, 1, 20)
-    ndx.finish()
-    expect(ndx.bounds(bbox.positive_x())).to.be.equal({x0: 0, x1: 1, y0: 1, y1: 200})
-    expect(ndx.bounds(bbox.positive_y())).to.be.equal({x0: 0, x1: 1, y0: 1, y1: 200})
+    const index = new SpatialIndex(6)
+    index.add_rect(0, 1, 1, 2)
+    index.add_rect(0.0001, 1, 1, 2)
+    index.add_rect(0, 100, 1, 200)
+    index.add_rect(0.0001, 100, 1, 200)
+    index.add_rect(0, 10, 1, 20)
+    index.add_rect(0.0001, 10, 1, 20)
+    index.finish()
+    expect(index.bounds(bbox.positive_x())).to.be.equal({x0: 0.0001, x1: 1, y0: 1, y1: 200})
+    expect(index.bounds(bbox.positive_y())).to.be.equal({x0: 0, x1: 1, y0: 1, y1: 200})
   })
 })
