063a4c80fd24673cfb9ed1b246949dcdec89a9b0
Bryan Van de Ven <bryan@bokeh.org>
2021-08-17 21:29:43 -0700

    streamline ci install for downstream

--- ci/install_downstream_packages.sh
+++ ci/install_downstream_packages.sh
@@ -2,11 +2,8 @@
 
 set -x #echo on
 
-conda install --yes --quiet -c pyviz/label/dev holoviews nose
+conda install --yes --quiet -c pyviz/label/dev holoviews nose scipy
 pip install dask["test"] distributed  # "test" extra installs additional testing dependencies
-# This is a temporary workaround to avoid clashing between `flaky` and `pytest-rerunfailures`.
-# See https://github.com/bokeh/bokeh/issues/11211
-pip uninstall --yes flaky
 pip install pandas_bokeh
 pip install geopandas
 git clone https://github.com/PatrikHlobil/Pandas-Bokeh.git
