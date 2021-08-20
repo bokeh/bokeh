97dbf27ff21491f93720baf12e93b1eb6a3a68ca
Bryan Van de Ven <bryan@bokeh.org>
2021-08-20 16:17:02 -0700

    install dask from source

--- ci/install_downstream_packages.sh
+++ ci/install_downstream_packages.sh
@@ -3,7 +3,9 @@
 set -x #echo on
 
 conda install --yes --quiet -c pyviz/label/dev holoviews nose scipy
-pip install dask["test"] distributed  # "test" extra installs additional testing dependencies
+git clone https://github.com/dask/dask.git
+pip install -e "./dask[test]"  # "test" extra installs additional testing dependencies
+git clone https://github.com/dask/distributed.git
 pip install pandas_bokeh
 pip install geopandas
 git clone https://github.com/PatrikHlobil/Pandas-Bokeh.git
