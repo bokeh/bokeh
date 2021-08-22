bd595781703ee07106983fd508b64d7b014fa38a
Bryan Van de Ven <bryan@bokeh.org>
2021-08-22 11:52:23 -0700

    Update install_downstream_packages.sh

--- ci/install_downstream_packages.sh
+++ ci/install_downstream_packages.sh
@@ -3,9 +3,10 @@
 set -x #echo on
 
 conda install --yes --quiet -c pyviz/label/dev holoviews nose scipy
+git clone https://github.com/dask/distributed.git
+pip install -e "./distributed"
 git clone https://github.com/dask/dask.git
 pip install -e "./dask[test]"  # "test" extra installs additional testing dependencies
-git clone https://github.com/dask/distributed.git
 pip install pandas_bokeh
 pip install geopandas
 git clone https://github.com/PatrikHlobil/Pandas-Bokeh.git
