f3e96608be27ea5eb4dfc06618522d26a1d3561e
Iury Piva <iurypiva@gmail.com>
2021-08-26 08:11:49 -0300

    add link to terser issue

--- bokehjs/src/compiler/linker.ts
+++ bokehjs/src/compiler/linker.ts
@@ -873,6 +873,7 @@ export async function minify(module: ModuleInfo, source: string, ecma: terser.EC
     ecma,
     format: {
       comments: /^!|copyright|license|\(c\)/i,
+      // https://github.com/terser/terser/issues/410
       ascii_only: true,
     },
     sourceMap: {
