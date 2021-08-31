4f7574405b36b24fbbb5c3a0446e4a0173ba1854
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2021-08-31 15:04:33 -0700

    Update examples

--- sphinx/source/docs/user_guide/examples/styling_math_text_tick_labels.py
+++ sphinx/source/docs/user_guide/examples/styling_math_text_tick_labels.py
@@ -1,12 +1,12 @@
 from numpy import arange
 
-from bokeh.models import MathText, FixedTicker
+from bokeh.models import FixedTicker
 from bokeh.plotting import figure, show
 
 x = arange(1, 4.5, 0.25)
 y = 1 / x
 
-plot = figure(min_border=80, height=400)
+plot = figure(height=200)
 plot.circle(x, y, fill_color="blue", size=5)
 
 plot.xaxis.axis_label = "Resistance"
@@ -14,10 +14,10 @@ plot.xaxis.ticker = FixedTicker(ticks=[1,2,3,4])
 plot.yaxis.axis_label = "Current at 1 V"
 
 plot.xaxis.major_label_overrides = {
-    1: MathText(text=r"1\Omega"),
-    2: MathText(text=r"2\Omega"),
-    3: MathText(text=r"3\Omega"),
-    4: MathText(text=r"4\Omega"),
+    1: r"$1\Omega$",
+    2: r"$2\Omega$",
+    3: r"$3\Omega$",
+    4: r"$4\Omega$",
     }
 
 show(plot)
