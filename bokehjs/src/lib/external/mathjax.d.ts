f9702ee8d303d727787cd8f75370ade8b6bb57b1
Iury Piva <iurypiva@gmail.com>
2021-09-03 16:54:23 -0300

    accept svg options on mathjax

--- bokehjs/src/lib/external/mathjax.d.ts
+++ bokehjs/src/lib/external/mathjax.d.ts
@@ -1,7 +1,60 @@
 declare namespace MathJax {
   type TeXMacros = {[key: string]: string | [string, number]}
+  type MathJaxOptions = {
+    format?: string
+    /** a boolean specifying whether the math is in display-mode or not (for TeX input). Default is true. */
+    display?: boolean
+    end?: number
+    /** a number giving the number of pixels in an ex for the surrounding font. Default is 8. */
+    ex?: number
+    /** a number giving the number of pixels in an em for the surrounding font. Default is 16. */
+    em?: number
+    /** a number giving the width of the container, in pixels. Default is 80 times the ex value. */
+    containerWidth?: number
+    /**  a number giving the line-breaking width in em units. Default is a very large number (100000), so effectively no line breaking. */
+    lineWidth?: number
+    svg?: {
+      /** a number giving a scaling factor to apply to the resulting conversion. Default is 1. */
+      scale?: number
+      family?: string
+      /** smallest scaling factor to use */
+      minScale?: number
+      /** true to make mtext elements use surrounding font */
+      mtextInheritFont?: boolean
+      /** true to make merror text use surrounding font */
+      merrorInheritFont?: boolean
+      /** font to use for mtext, if not inheriting (empty means use MathJax fonts) */
+      mtextFont?: string
+      /** font to use for merror, if not inheriting (empty means use MathJax fonts) */
+      merrorFont?: string
+      /** true for MathML spacing rules, false for TeX rules */
+      mathmlSpacing?: boolean
+      /** RFDa and other attributes NOT to copy to the output */
+      skipAttributes?: {}
+      /** default size of ex in em units */
+      exFactor?: number
+      /** default for indentalign when set to 'auto' */
+      displayAlign?: string
+      /** default for indentshift when set to 'auto' */
+      displayIndent?: string
+      /** The wrapper factory to use */
+      wrapperFactory?: any
+      /** The FontData object to use */
+      font?: any
+      /** The CssStyles object to use */
+      cssStyles?: any
+      /** insert <title> tags with speech content */
+      internalSpeechTitles?: boolean
+      /** initial id number to use for aria-labeledby titles */
+      titleID?: number
+      /** or 'global' or 'none' */
+      fontCache?: "global" | "local"
+      /** ID to use for local font cache (for single equation processing) */
+      localID?: any
+    }
+  }
 
-  function tex2svg(input: string, macros?: TeXMacros): HTMLElement
-  function ascii2svg(input: string): HTMLElement
-  function mathml2svg(input: string): HTMLElement
+  function tex2svg(input: string, options?: MathJaxOptions, macros?: TeXMacros): HTMLElement
+  function ascii2svg(input: string, options?: MathJaxOptions): HTMLElement
+  function mathml2svg(input: string, options?: MathJaxOptions): HTMLElement
 }
--- bokehjs/src/lib/models/text/mathjax/index.ts
+++ bokehjs/src/lib/models/text/mathjax/index.ts
@@ -10,24 +10,70 @@ import {AllPackages} from "mathjax-full/js/input/tex/AllPackages.js"
 const adaptor = browserAdaptor()
 RegisterHTMLHandler(adaptor)
 
-const svg = new SVG({fontCache: "local"})
-
-const options = {
-  display: true,
-  em: 16,
-  ex: 8,
-  containerWidth: 80*16,
-}
-
 type TeXMacros = {[key: string]: string | [string, number]}
 
-export function tex2svg(formula: string, macros: TeXMacros = {}): HTMLElement {
-  const tex = new TeX({packages: AllPackages, macros})
-  const tex_to_svg = mathjax.document("", {InputJax: tex, OutputJax: svg})
-  return tex_to_svg.convert(formula, options)
+type MathJaxOptions = {
+  format?: string
+  /** a boolean specifying whether the math is in display-mode or not (for TeX input). Default is true. */
+  display?: boolean
+  end?: number
+  /** a number giving the number of pixels in an ex for the surrounding font. Default is 8. */
+  ex?: number
+  /** a number giving the number of pixels in an em for the surrounding font. Default is 16. */
+  em?: number
+  /** a number giving the width of the container, in pixels. Default is 80 times the ex value. */
+  containerWidth?: number
+  /**  a number giving the line-breaking width in em units. Default is a very large number (100000), so effectively no line breaking. */
+  lineWidth?: number
+  svg?: {
+    /** a number giving a scaling factor to apply to the resulting conversion. Default is 1. */
+    scale?: number
+    family?: string
+    /** smallest scaling factor to use */
+    minScale?: number
+    /** true to make mtext elements use surrounding font */
+    mtextInheritFont?: boolean
+    /** true to make merror text use surrounding font */
+    merrorInheritFont?: boolean
+    /** font to use for mtext, if not inheriting (empty means use MathJax fonts) */
+    mtextFont?: string
+    /** font to use for merror, if not inheriting (empty means use MathJax fonts) */
+    merrorFont?: string
+    /** true for MathML spacing rules, false for TeX rules */
+    mathmlSpacing?: boolean
+    /** RFDa and other attributes NOT to copy to the output */
+    skipAttributes?: {}
+    /** default size of ex in em units */
+    exFactor?: number
+    /** default for indentalign when set to 'auto' */
+    displayAlign?: string
+    /** default for indentshift when set to 'auto' */
+    displayIndent?: string
+    /** The wrapper factory to use */
+    wrapperFactory?: any
+    /** The FontData object to use */
+    font?: any
+    /** The CssStyles object to use */
+    cssStyles?: any
+    /** insert <title> tags with speech content */
+    internalSpeechTitles?: boolean
+    /** initial id number to use for aria-labeledby titles */
+    titleID?: number
+    /** or 'global' or 'none' */
+    fontCache?: "global" | "local"
+    /** ID to use for local font cache (for single equation processing) */
+    localID?: any
+  }
 }
 
-export function ascii2svg(_formula: string): HTMLElement {
+export function tex2svg(formula: string, mathjax_options: MathJaxOptions = {}, macros: TeXMacros = {}): HTMLElement {
+  const tex = new TeX({packages: AllPackages, macros})
+  const svg = new SVG({fontCache: "local", ...mathjax_options.svg})
+  const tex_to_svg = mathjax.document("", {InputJax: tex, OutputJax: svg})
+  return tex_to_svg.convert(formula, mathjax_options)
+}
+
+export function ascii2svg(_formula: string, _mathjax_options: MathJaxOptions = {}): HTMLElement {
   // TODO:
   // const ascii = new AsciiMath({})
   // const ascii_to_svg = mathjax.document("", {InputJax: ascii, OutputJax: svg})
@@ -35,8 +81,9 @@ export function ascii2svg(_formula: string): HTMLElement {
   throw new Error("not implemented")
 }
 
-export function mathml2svg(formula: string): HTMLElement {
+export function mathml2svg(formula: string, mathjax_options: MathJaxOptions = {}): HTMLElement {
   const mathml = new MathML({})
+  const svg = new SVG({fontCache: "local"})
   const mathml_to_svg = mathjax.document("", {InputJax: mathml, OutputJax: svg})
-  return mathml_to_svg.convert(formula, options)
+  return mathml_to_svg.convert(formula, mathjax_options)
 }
