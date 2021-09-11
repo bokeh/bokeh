d5308dd4db661ac774f725ef7eb990d5d00d4c47
Iury Piva <iurypiva@gmail.com>
2021-09-11 13:34:02 -0300

    update find math function

--- bokehjs/src/lib/models/text/utils.ts
+++ bokehjs/src/lib/models/text/utils.ts
@@ -1,13 +1,14 @@
-import {PlainText, TeX} from "../index"
+import {PlainText} from "./plain_text"
+import {TeX} from "./math_text"
 
-export function find_math_parts(text: string): (PlainText | TeX)[] {
-  type Delimiter = {
-    start: string
-    end: string
-    inline: boolean
-    nextIndex?: number
-  }
+type Delimiter = {
+  start: string
+  end: string
+  inline: boolean
+  nextIndex?: number
+}
 
+export function find_math_parts(mathstring: string): (PlainText | TeX)[] {
   const delimiters: Delimiter[] = [
     {start: "$$", end: "$$", inline: false},
     {start: "\\[", end: "\\]", inline: false},
@@ -15,46 +16,79 @@ export function find_math_parts(text: string): (PlainText | TeX)[] {
   ]
 
   const result: (PlainText | TeX)[] = []
-  let remaining_text = text
 
-  const find_end = (delimiter?: Delimiter) => {
-    if (!delimiter) {
-      result.push(new PlainText({text: remaining_text}))
-      remaining_text = ""
-      return
-    }
-
-    if (remaining_text.includes(delimiter.start)) {
-      const index = remaining_text.indexOf(delimiter.start)
-
-      if (remaining_text.slice(index + 2).includes(delimiter.end)) {
-        result.push(new PlainText({text: remaining_text.slice(0, index)}))
-        remaining_text = remaining_text.slice(index + 2)
-
-        const closing_index = remaining_text.indexOf(delimiter.end)
-        result.push(
-          new TeX({
-            text: remaining_text.slice(0, closing_index),
-            inline: delimiter.inline,
-          })
-        )
-        remaining_text = remaining_text.slice(closing_index + 2)
-      }
-    }
-  }
-
-  const find_next_delimiter = () =>
+  // for each delimiter
+  const find_next_delimiter = (text: string) =>
     delimiters
+      // get their position on text
       .map((delimiter) => ({
         ...delimiter,
-        nextIndex: remaining_text.indexOf(delimiter.start),
+        nextIndex: text.indexOf(delimiter.start),
       }))
-      .sort((a, b) => a.nextIndex - b.nextIndex)
-      .filter((delimiter) => delimiter.nextIndex >= 0)[0]
+      // remove delimiters not found
+      .filter((delimiter) => delimiter.nextIndex >= 0)
+      // return the delimiter closer to start of text
+      .sort((a, b) => a.nextIndex - b.nextIndex)[0]
 
-  while (remaining_text) {
-    find_end(find_next_delimiter())
+  const find_end = (text: string, delimiter?: Delimiter): string => {
+    // if there is no delimiter then the whole text is a plain string
+    if (!delimiter) {
+      result.push(new PlainText({text}))
+
+      return ""
+    }
+
+    const index = text.indexOf(delimiter.start)
+
+    // first delimiter is the end of the string
+    if (index === text.length - 2) {
+      result.push(new PlainText({text}))
+      return ""
+    }
+
+    // end delimiter found
+    if (text.slice(index + 2).includes(delimiter.end)) {
+      // string before open delimiter is plain text
+      result.push(new PlainText({text: text.slice(0, index)}))
+
+      const text_after_delimiter = text.slice(index + 2)
+      const closing_index = text_after_delimiter.indexOf(delimiter.end)
+
+      // from open delimiter to end delimiter is input
+      result.push(
+        new TeX({
+          text: text_after_delimiter.slice(0, closing_index),
+          inline: delimiter.inline,
+        })
+      )
+
+      // remove end delimiter from return
+      return text_after_delimiter.slice(closing_index + 2)
+    }
+
+    // if there is not a closing delimiter
+    // check if there are other open delimiters
+    if (find_next_delimiter(text.slice(index + 2))) {
+      while (text) {
+        text = find_end(text, find_next_delimiter(text.slice(index + 2)))
+      }
+
+      return text
+    }
+
+    // no ending delimiter was found then its a plain text
+    result.push(new PlainText({text}))
+    return ""
   }
 
-  return result.filter(el => el.text)
+  let remaining_text = mathstring
+
+  while (remaining_text) {
+    remaining_text = find_end(
+      remaining_text,
+      find_next_delimiter(remaining_text)
+    )
+  }
+
+  return result.filter((el) => el.text)
 }
