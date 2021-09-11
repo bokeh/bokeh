936e05fe84776dd12e6f540eb0355a33389ef5b4
Iury Piva <iurypiva@gmail.com>
2021-09-11 00:00:58 -0300

    use mathjax namespace

--- bokehjs/src/lib/models/text/mathjax/index.ts
+++ bokehjs/src/lib/models/text/mathjax/index.ts
@@ -10,25 +10,30 @@ import {AllPackages} from "mathjax-full/js/input/tex/AllPackages.js"
 const adaptor = browserAdaptor()
 RegisterHTMLHandler(adaptor)
 
-const svg = new SVG({fontCache: "local"})
+declare namespace MathJax {
+  type ConvertOptions = {
+    display?: boolean
+    em?: number
+    ex?: number
+    containerWidth?: number
+  }
+  type TeXMacros = {[key: string]: string | [string, number]}
 
-type ConvertOptions = {
-  display?: boolean
-  em?: number
-  ex?: number
-  containerWidth?: number
+  function tex2svg(formula: string, options?: ConvertOptions, macros?: TeXMacros): HTMLElement
+  function ascii2svg(_formula: string): HTMLElement
+  function mathml2svg(formula: string): HTMLElement
 }
 
-type TeXMacros = {[key: string]: string | [string, number]}
+const svg = new SVG({fontCache: "local"})
 
-const defaults: ConvertOptions = {
+const defaults: MathJax.ConvertOptions = {
   display: true,
   em: 16,
   ex: 8,
   containerWidth: 80*16,
 }
 
-export function tex2svg(formula: string, options?: ConvertOptions, macros: TeXMacros = {}): HTMLElement {
+export function tex2svg(formula: string, options?: MathJax.ConvertOptions, macros: MathJax.TeXMacros = {}): HTMLElement {
   const tex = new TeX({packages: AllPackages, macros})
   const tex_to_svg = mathjax.document("", {InputJax: tex, OutputJax: svg})
   return tex_to_svg.convert(formula, {...defaults, ...options})
@@ -47,3 +52,5 @@ export function mathml2svg(formula: string): HTMLElement {
   const mathml_to_svg = mathjax.document("", {InputJax: mathml, OutputJax: svg})
   return mathml_to_svg.convert(formula, defaults)
 }
+
+export default MathJax
