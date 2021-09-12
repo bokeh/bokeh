b72f77597ca2259274d06085821f88cfc6efaf33
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2021-09-11 19:02:24 -0700

    Update spacing in example

--- sphinx/source/docs/user_guide/examples/styling_math_text_latex_axis_labels.py
+++ sphinx/source/docs/user_guide/examples/styling_math_text_latex_axis_labels.py
@@ -2,13 +2,13 @@ from numpy import arange, pi, sin
 
 from bokeh.plotting import figure, show
 
-x = arange(-2 * pi, 2 * pi, 0.1)
+x = arange(-2*pi, 2*pi, 0.1)
 y = sin(x)
 
 plot = figure(height=200)
 plot.circle(x, y, fill_color="red", size=5)
 
 plot.yaxis.axis_label = r"\[\sin(x)\]"
-plot.xaxis.axis_label = r"\(x\cdot\pi\)"
+plot.xaxis.axis_label = r"\[x\cdot\pi\]"
 
 show(plot)
