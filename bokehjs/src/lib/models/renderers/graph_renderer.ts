5be0f241a5d694c3e11c70db9d4ce7c7d70d1ce7
g-parki <61096711+g-parki@users.noreply.github.com>
2021-09-13 02:51:29 -0700

    Updateable layout_provider/graph_layout (#11547)
    * Apply coordinates on layout provider change
    
    * Add tests, lint
    
    * Create CoordinateTransform node/edge subclasses
    
    * Add coordinate getters, update test
    
    * Move coordinate classes to static_layout_provider
    
    * Elevate coordinate classes to layout_provider
    
    * Lint
    
    * Abstract graphcoordinates, add vis regression test
    
    * Add graph layout visual test baselines
    
    * Space before brackets, explicit return types, lint

--- bokehjs/src/lib/models/renderers/graph_renderer.ts
+++ bokehjs/src/lib/models/renderers/graph_renderer.ts
@@ -2,7 +2,6 @@ import {DataRenderer, DataRendererView} from "./data_renderer"
 import {GlyphRenderer, GlyphRendererView} from "./glyph_renderer"
 import {Renderer} from "./renderer"
 import {GlyphView} from "../glyphs/glyph"
-import {Expression} from "../expressions/expression"
 import {LayoutProvider} from "../graphs/layout_provider"
 import {GraphHitTestPolicy, NodesOnly} from "../graphs/graph_hit_test_policy"
 import * as p from "core/properties"
@@ -11,9 +10,6 @@ import {SelectionManager} from "core/selection_manager"
 import {XYGlyph} from "../glyphs/xy_glyph"
 import {MultiLine} from "../glyphs/multi_line"
 import {Patches} from "../glyphs/patches"
-import {ColumnarDataSource} from "../sources/columnar_data_source"
-import {Arrayable} from "core/types"
-import {assert} from "core/util/assert"
 
 export class GraphRendererView extends DataRendererView {
   override model: GraphRenderer
@@ -27,47 +23,25 @@ export class GraphRendererView extends DataRendererView {
 
   override async lazy_initialize(): Promise<void> {
     await super.lazy_initialize()
-
-    const graph = this.model
-
-    // TODO: replace this with bi-variate transforms
-    let xs_ys: [Arrayable<number>[], Arrayable<number>[]] | null = null
-    let x_y: [Arrayable<number>, Arrayable<number>] | null = null
-
-    const xs_expr = new class extends Expression {
-      _v_compute(source: ColumnarDataSource) {
-        assert(xs_ys == null)
-        const [xs] = xs_ys = graph.layout_provider.get_edge_coordinates(source)
-        return xs
-      }
-    }
-    const ys_expr = new class extends Expression {
-      _v_compute(_source: ColumnarDataSource) {
-        assert(xs_ys != null)
-        const [, ys] = xs_ys
-        xs_ys = null
-        return ys
-      }
-    }
-
-    const x_expr = new class extends Expression {
-      _v_compute(source: ColumnarDataSource) {
-        assert(x_y == null)
-        const [x] = x_y = graph.layout_provider.get_node_coordinates(source)
-        return x
-      }
-    }
-    const y_expr = new class extends Expression {
-      _v_compute(_source: ColumnarDataSource) {
-        assert(x_y != null)
-        const [, y] = x_y
-        x_y = null
-        return y
-      }
-    }
-
+    this.apply_coordinates()
+    const {parent} = this
     const {edge_renderer, node_renderer} = this.model
+    this.edge_view = await build_view(edge_renderer, {parent})
+    this.node_view = await build_view(node_renderer, {parent})
+  }
 
+  override connect_signals(): void {
+    super.connect_signals()
+    this.connect(this.model.layout_provider.change, () => {
+      this.apply_coordinates()
+      this.edge_view.set_data()
+      this.node_view.set_data()
+      this.request_render()
+    })
+  }
+
+  protected apply_coordinates(): void {
+    const {edge_renderer, node_renderer} = this.model
     // TODO: XsYsGlyph or something
     if (!(edge_renderer.glyph instanceof MultiLine || edge_renderer.glyph instanceof Patches)) {
       throw new Error(`${this}.edge_renderer.glyph must be a MultiLine glyph`)
@@ -76,30 +50,21 @@ export class GraphRendererView extends DataRendererView {
       throw new Error(`${this}.node_renderer.glyph must be a XYGlyph glyph`)
     }
 
+    const edge_coords = this.model.layout_provider.edge_coordinates
+    const node_coords = this.model.layout_provider.node_coordinates
+
     edge_renderer.glyph.properties.xs.internal = true
     edge_renderer.glyph.properties.ys.internal = true
-
     node_renderer.glyph.properties.x.internal = true
     node_renderer.glyph.properties.y.internal = true
 
-    edge_renderer.glyph.xs = {expr: xs_expr}
-    edge_renderer.glyph.ys = {expr: ys_expr}
+    edge_renderer.glyph.xs = {expr: edge_coords.x}
+    edge_renderer.glyph.ys = {expr: edge_coords.y}
+    node_renderer.glyph.x = {expr: node_coords.x}
+    node_renderer.glyph.y = {expr: node_coords.y}
 
-    node_renderer.glyph.x = {expr: x_expr}
-    node_renderer.glyph.y = {expr: y_expr}
-
-    const {parent} = this
-    this.edge_view = await build_view(edge_renderer, {parent})
-    this.node_view = await build_view(node_renderer, {parent})
-  }
-
-  override connect_signals(): void {
-    super.connect_signals()
-    this.connect(this.model.layout_provider.change, () => {
-      this.edge_view.set_data()
-      this.node_view.set_data()
-      this.request_render()
-    })
+    this.model.edge_renderer = edge_renderer
+    this.model.node_renderer = node_renderer
   }
 
   override remove(): void {
