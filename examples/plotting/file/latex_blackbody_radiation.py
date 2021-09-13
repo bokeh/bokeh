bb6d2991f38b1621e076da44fc81c6294a71f63f
Bryan Van de Ven <bryan@bokeh.org>
2021-09-13 13:55:59 -0700

    add latex examples to gallery

--- examples/plotting/file/latex_blackbody_radiation.py
+++ examples/plotting/file/latex_blackbody_radiation.py
@@ -1,11 +1,21 @@
+''' A plot of spectral radiance curves at various temperatures. This example
+demonstrates the use of mathtext on axes and in ``Div`` objects.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.Figure.line
+    :refs: :ref:`userguide_styling` >
+    :keywords: mathtext, latex
+
+'''
 import numpy as np
 
 from bokeh.io import curdoc
-from bokeh.models import TeX
+from bokeh.layouts import column
+from bokeh.models import Div
 from bokeh.palettes import Spectral
 from bokeh.plotting import figure, show
 
-fig = figure(
+p = figure(
     width=700, height=500, toolbar_location=None,
     title="Black body spectral radiance as a function of frequency")
 
@@ -21,18 +31,27 @@ nu = np.linspace(0, 1e15, 500)  # Frequency (1/s)
 
 for i, T in enumerate(Ts):
     B_nu = spectral_radiance(nu, T)
-    fig.line(nu/1e15, B_nu/1e-9, line_width=2, legend_label=f"T = {T} K", line_color=palette[i])
+    p.line(nu/1e15, B_nu/1e-9, line_width=2,
+             legend_label=f"T = {T} K", line_color=palette[i])
+p.legend.items = list(reversed(p.legend.items))
 
 # Peak radiance line.
 Ts = np.linspace(1900, 6101, 50)
 peak_freqs = Ts*5.879e10
 peak_radiance = spectral_radiance(peak_freqs, Ts)
-fig.line(peak_freqs/1e15, peak_radiance/1e-9, line_color="silver", line_dash="dashed", line_width=2, legend_label="Peak radiance")
+p.line(peak_freqs/1e15, peak_radiance/1e-9, line_color="silver",
+         line_dash="dashed", line_width=2, legend_label="Peak radiance")
 
 curdoc().theme = 'dark_minimal'
-fig.y_range.start = 0
-fig.xaxis.axis_label = TeX(text=r"\color{white} \nu \:(10^{15} s^{-1})")
-fig.yaxis.axis_label = TeX(
-    text=r"\color{white} B_\nu(\nu, T) = \frac{2h\nu^3}{c^2} \frac{1}{\exp(h\nu/kT)-1} \quad(10^{-9} J s m^{-3})")
+p.y_range.start = 0
+p.xaxis.axis_label = r"$$\color{white} \nu \:(10^{15} s^{-1})$$"
+p.yaxis.axis_label = r"$$\color{white} B_\nu(\nu, T) \quad(10^{-9} J s m^{-3})$$"
 
-show(fig)
+div = Div(text=r"""
+A plot of the spectral radiance, defined as a function of the frequency $$\nu$$, is given by the formula
+<p \>
+$$
+\qquad B_\nu(\nu, T) = \frac{2h\nu^3}{c^2} \frac{1}{\exp(h\nu/kT)-1}\ .
+$$
+""")
+show(column(p, div))
--- examples/plotting/file/latex_normal_distribution.py
+++ examples/plotting/file/latex_normal_distribution.py
@@ -1,55 +1,75 @@
+''' A plot of the Normal (Gaussian) distribution. his example demonstrates the
+use of mathtext on axes and in ``Div`` objects.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.Figure.line, bokeh.plotting.Figure.quad, bokeh.models.TeX
+    :refs: :ref:`userguide_styling` >
+    :keywords: mathtext, latex
+
+'''
 import numpy as np
 
-from bokeh.models import FixedTicker, TeX
+from bokeh.layouts import column
+from bokeh.models import Div, TeX
 from bokeh.plotting import figure, show
 
-fig = figure(width=670, height=400, toolbar_location=None, title="Normal (Gaussian) Distribution")
+p = figure(width=670, height=400, toolbar_location=None,
+           title="Normal (Gaussian) Distribution")
 
 n = 1000
 rng = np.random.default_rng(825914)
 x = rng.normal(loc=4.7, scale=12.3, size=n)
 
-# Scale random data so that it has mean of 0 and standard deviation of 1.
+# Scale random data so that it has mean of 0 and standard deviation of 1
 xbar = x.mean()
 sigma = x.std()
 scaled = (x - xbar) / sigma
 
-# Histogram.
+# Histogram
 bins = np.linspace(-3, 3, 40)
 hist, edges = np.histogram(scaled, density=True, bins=bins)
-fig.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
-         fill_color="skyblue", line_color="white", legend_label=f"{n} random samples")
+p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+         fill_color="skyblue", line_color="white",
+         legend_label=f"{n} random samples")
 
-# Probability density function.
+# Probability density function
 x = np.linspace(-3.0, 3.0, 100)
 pdf = np.exp(-0.5*x**2) / np.sqrt(2.0*np.pi)
-fig.line(x, pdf, line_width=2, line_color="navy", legend_label="Probability Density Function")
+p.line(x, pdf, line_width=2, line_color="navy",
+       legend_label="Probability Density Function")
 
-fig.y_range.start = 0
+p.y_range.start = 0
+p.xaxis.axis_label = "x"
+p.yaxis.axis_label = "PDF(x)"
 
-fig.xaxis.axis_label = TeX(text=r"x")
-fig.yaxis.axis_label = TeX(
-    text=r"PDF(x) = \frac{1}{\sigma\sqrt{2\pi}} \exp\left[-\frac{1}{2} "
-    r"\left(\frac{x-\overline{x}}{\sigma}\right)^2 \right]")
-
-fig.xaxis.ticker = FixedTicker(ticks=[-3, -2, -1, 0, 1, 2, 3])
-fig.xaxis.major_label_overrides = {
-    -3: TeX(text=r"\overline{x} - 3\sigma"),
-    -2: TeX(text=r"\overline{x} - 2\sigma"),
-    -1: TeX(text=r"\overline{x} - \sigma"),
-    0: TeX(text=r"\overline{x}"),
-    1: TeX(text=r"\overline{x} + \sigma"),
-    2: TeX(text=r"\overline{x} + 2\sigma"),
-    3: TeX(text=r"\overline{x} + 3\sigma"),
+p.xaxis.ticker = [-3, -2, -1, 0, 1, 2, 3]
+p.xaxis.major_label_overrides = {
+    -3: TeX(r"\overline{x} - 3\sigma"),
+    -2: TeX(r"\overline{x} - 2\sigma"),
+    -1: TeX(r"\overline{x} - \sigma"),
+     0: TeX(r"\overline{x}"),
+     1: TeX(r"\overline{x} + \sigma"),
+     2: TeX(r"\overline{x} + 2\sigma"),
+     3: TeX(r"\overline{x} + 3\sigma"),
 }
 
-fig.yaxis.ticker = FixedTicker(ticks=[0, 0.1, 0.2, 0.3, 0.4])
-fig.yaxis.major_label_overrides = {
-    0: TeX(text=r"0"),
-    0.1: TeX(text=r"0.1/\sigma"),
-    0.2: TeX(text=r"0.2/\sigma"),
-    0.3: TeX(text=r"0.3/\sigma"),
-    0.4: TeX(text=r"0.4/\sigma"),
+p.yaxis.ticker = [0, 0.1, 0.2, 0.3, 0.4]
+p.yaxis.major_label_overrides = {
+    0: TeX(r"0"),
+    0.1: TeX(r"0.1/\sigma"),
+    0.2: TeX(r"0.2/\sigma"),
+    0.3: TeX(r"0.3/\sigma"),
+    0.4: TeX(r"0.4/\sigma"),
 }
 
-show(fig)
+div = Div(text=r"""
+A histogram of a samples from a Normal (Gaussian) distribution, together with
+the ideal probability density function, given by the equation:
+<p />
+$$
+\qquad PDF(x) = \frac{1}{\sigma\sqrt{2\pi}} \exp\left[-\frac{1}{2}
+\left(\frac{x-\overline{x}}{\sigma}\right)^2 \right]
+$$ fdsf
+""")
+
+show(column(p, div))
