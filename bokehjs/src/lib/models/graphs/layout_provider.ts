3285a744fe2c13c45a33226c9dea163ef28d5338
g-parki <61096711+g-parki@users.noreply.github.com>
2021-09-13 02:51:29 -0700

    Updateable layout_provider/graph_layout (#11547)
    * Apply coordinates on layout provider change
    
    * Add tests, lint
    
    * Create CoordinateTransform node/edge subclasses
    
    * Add coordinate getters, update test
    
    * Move coordinate classes to static_layout_provider
    
    * Elevate coordinate classes to layout_provider
    
    * Lint
    
    * Abstract graphcoordinates, add vis regression test
    
    * Add graph layout visual test baselines
    
    * Space before brackets, explicit return types, lint

--- bokehjs/src/lib/models/graphs/layout_provider.ts
+++ bokehjs/src/lib/models/graphs/layout_provider.ts
@@ -1,5 +1,6 @@
 import {Model} from "../../model"
 import {ColumnarDataSource} from "../sources/columnar_data_source"
+import {CoordinateTransform} from "models/expressions/coordinate_transform"
 import {Arrayable} from "core/types"
 import * as p from "core/properties"
 
@@ -21,4 +22,75 @@ export abstract class LayoutProvider extends Model {
   abstract get_node_coordinates(graph_source: ColumnarDataSource): [Arrayable<number>, Arrayable<number>]
 
   abstract get_edge_coordinates(graph_source: ColumnarDataSource): [Arrayable<number>[], Arrayable<number>[]]
+
+  get node_coordinates(): NodeCoordinates {
+    return new NodeCoordinates({layout: this})
+  }
+
+  get edge_coordinates(): EdgeCoordinates {
+    return new EdgeCoordinates({layout: this})
+  }
+}
+
+export namespace GraphCoordinates {
+  export type Attrs = p.AttrsOf<Props>
+  export type Props = CoordinateTransform.Props & {
+    layout: p.Property<LayoutProvider>
+  }
+}
+
+export interface GraphCoordinates extends GraphCoordinates.Attrs {}
+
+export abstract class GraphCoordinates extends CoordinateTransform {
+  override properties: GraphCoordinates.Props
+
+  constructor(attrs?: Partial<GraphCoordinates.Attrs>) {
+    super(attrs)
+  }
+
+  static {
+    this.define<GraphCoordinates.Props>(({Ref}) => ({
+      layout: [ Ref(LayoutProvider)],
+    }))
+  }
+}
+
+export namespace NodeCoordinates {
+  export type Attrs = p.AttrsOf<Props>
+  export type Props = GraphCoordinates.Props
+}
+
+export interface NodeCoordinates extends NodeCoordinates.Attrs {}
+
+export class NodeCoordinates extends GraphCoordinates {
+  override properties: NodeCoordinates.Props
+
+  constructor(attrs?: Partial<NodeCoordinates.Attrs>) {
+    super(attrs)
+  }
+
+  _v_compute(source: ColumnarDataSource): {x: Arrayable<number>, y: Arrayable<number>} {
+    const [x, y] = this.layout.get_node_coordinates(source)
+    return {x, y}
+  }
+}
+
+export namespace EdgeCoordinates {
+  export type Attrs = p.AttrsOf<Props>
+  export type Props = GraphCoordinates.Props
+}
+
+export interface EdgeCoordinates extends EdgeCoordinates.Attrs {}
+
+export class EdgeCoordinates extends GraphCoordinates {
+  override properties: EdgeCoordinates.Props
+
+  constructor(attrs?: Partial<EdgeCoordinates.Attrs>) {
+    super(attrs)
+  }
+
+  _v_compute(source: ColumnarDataSource): {x: Arrayable<number>[], y: Arrayable<number>[]} {
+    const [x, y] = this.layout.get_edge_coordinates(source)
+    return {x, y}
+  }
 }
