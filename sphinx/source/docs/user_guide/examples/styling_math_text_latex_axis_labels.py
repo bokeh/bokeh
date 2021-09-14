8ee68aac1cf5496fd9a64090d8c1be41a9a5f20d
Bryan Van de Ven <bryan@bokeh.org>
2021-09-14 07:18:08 -0700

    add and update examples (#11604)

--- sphinx/source/docs/user_guide/examples/styling_math_text_latex_axis_labels.py
+++ sphinx/source/docs/user_guide/examples/styling_math_text_latex_axis_labels.py
@@ -6,7 +6,7 @@ x = arange(-2*pi, 2*pi, 0.1)
 y = sin(x)
 
 plot = figure(height=200)
-plot.circle(x, y, fill_color="red", size=5)
+plot.circle(x, y, alpha=0.6, size=7)
 
 plot.yaxis.axis_label = r"\[\sin(x)\]"
 plot.xaxis.axis_label = r"\[x\cdot\pi\]"
