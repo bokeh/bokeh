d86cd15c1d8354f570349d1a223bdd734a199481
Bryan Van de Ven <bryan@bokeh.org>
2021-09-16 04:09:04 -0700

    Reduce Tornado imports (#11618)
    * reduce unnessary tornado imports
    
    * factor test utils and tighten no import tests

--- tests/codebase/__init__.py
+++ tests/codebase/__init__.py
@@ -5,20 +5,3 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
-
-from __future__ import annotations # isort:skip
-
-# Standard library imports
-from pathlib import Path
-from subprocess import run
-from typing import List
-
-TOP_PATH = Path(__file__).resolve().parent.parent.parent
-
-def ls_files(*patterns: str) -> List[str]:
-    proc = run(["git", "ls-files", "--", *patterns], capture_output=True)
-    return proc.stdout.decode("utf-8").split("\n")
-
-def verify_clean_imports(target: str, modules: List[str]) -> str:
-    imports =  ";".join(f"import {m}" for m in modules)
-    return f"import sys; {imports}; sys.exit(1 if any({target!r} in x for x in sys.modules.keys()) else 0)"
