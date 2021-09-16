e9ec68d0a561c46bbeb5b006beed6fc6ba5cd2cf
Bryan Van de Ven <bryan@bokeh.org>
2021-09-15 20:18:26 -0700

    factor test utils and tighten no import tests

--- tests/codebase/test_no_ipython_common.py
+++ tests/codebase/test_no_ipython_common.py
@@ -21,27 +21,32 @@ import pytest ; pytest
 from subprocess import run
 from sys import executable as python
 
-from . import verify_clean_imports
+# Bokeh imports
+from bokeh._testing.util.project import ls_modules, verify_clean_imports
 
 #-----------------------------------------------------------------------------
 # Tests
 #-----------------------------------------------------------------------------
 
-modules = [
-    "bokeh.application",
-    "bokeh.client",
-    "bokeh.embed",
-    "bokeh.io",
-    "bokeh.models",
-    "bokeh.plotting",
-    "bokeh.server",
-]
+# There should not be unprotected IPython imports /anywhere/
+IPYTHON_ALLOWED = (
+    "bokeh._testing",
+)
 
-def test_no_ipython_common() -> None:
+MODULES = ls_modules(skip_prefixes=IPYTHON_ALLOWED)
+
+# This test takes a long time to run, but if the combined test fails then
+# uncommenting it will locate exactly what module(s) are the problem
+# @pytest.mark.parametrize('module', MODULES)
+# def test_no_ipython_common_individual(module) -> None:
+#     proc = run([python, "-c", verify_clean_imports('IPython', [module])])
+#     assert proc.returncode == 0, f"IPython imported in common module {module}"
+
+def test_no_ipython_common_combined() -> None:
     ''' Basic usage of Bokeh should not result in any IPython code being
     imported. This test ensures that importing basic modules does not bring in
     IPython.
 
     '''
-    proc = run([python, "-c", verify_clean_imports('IPython', modules)])
+    proc = run([python, "-c", verify_clean_imports('IPython', MODULES)])
     assert proc.returncode == 0, "IPython imported in common modules"
