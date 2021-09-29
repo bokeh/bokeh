5d9a99f537d69b3ebb3516bdd58af1c13ff4f8f6
Mateusz Paprocki <mattpap@gmail.com>
2021-09-29 15:17:40 +0200

    Expose plotting subcoordinates API in bokehjs (#11697)
    * Split off Glyph API and support subcoordinates
    
    * Cut import cycle in core/util/types
    
    * Add Floating interface and Fraction class
    
    * Port brightness() function from bokeh
    
    * Add PolarSubcoordinates example

--- bokehjs/src/lib/core/class.ts
+++ bokehjs/src/lib/core/class.ts
@@ -4,3 +4,18 @@ export interface Class<T, Args extends any[] = any[]> {
 }
 
 export type Constructor<T = {}> = new (...args: any[]) => T
+
+export function extend(ctor: Class<any>, ...mixins: any[]): void {
+  for (const mixin of mixins) {
+    for (const name of Object.getOwnPropertyNames(mixin.prototype)) {
+      if (name == "constructor")
+        continue
+
+      Object.defineProperty(
+        ctor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(mixin.prototype, name) ?? Object.create(null),
+      )
+    }
+  }
+}
