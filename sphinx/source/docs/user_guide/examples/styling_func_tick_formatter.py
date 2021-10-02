ad64a9b294e5ffa9047b40736c6246889bfb9d7a
Ben Russell <bprussell80@gmail.com>
2021-10-02 02:28:54 -0700

    Change name of FuncTickFormatter to CustomJSTickFormatter (#11696)
    * Change name to CustomJSTickFormatter
    
    * Add entry to 3.0.0 migration guide
    
    * Add simple unit test for CustomJSTickFormatter
    
    * Fix test imports
    
    Co-authored-by: Ben Russell <bprussell80@protonmail.com>

--- sphinx/source/docs/user_guide/examples/styling_func_tick_formatter.py
+++ sphinx/source/docs/user_guide/examples/styling_func_tick_formatter.py
@@ -1,4 +1,4 @@
-from bokeh.models import FuncTickFormatter
+from bokeh.models import CustomJSTickFormatter
 from bokeh.plotting import figure, output_file, show
 
 output_file("formatter.html")
@@ -6,7 +6,7 @@ output_file("formatter.html")
 p = figure(width=500, height=500)
 p.circle([0, 2, 4, 6, 8, 10], [6, 2, 4, 10, 8, 0], size=30)
 
-p.yaxis.formatter = FuncTickFormatter(code="""
+p.yaxis.formatter = CustomJSTickFormatter(code="""
     return Math.floor(tick) + " + " + (tick % 1).toFixed(2)
 """)
 
