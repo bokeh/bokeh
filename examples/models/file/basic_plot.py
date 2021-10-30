51945e7f691c3fd983f92bcd96a62db2ea789489
Bill Lanyon <billlanyon@me.com>
2021-10-30 21:38:24 +0200

    11765 basic plot example metadata (#11774)
    * initial commit
    
    * eliminated trailing whitespace
    
    * Update examples/models/file/basic_plot.py
    
    Co-authored-by: Timo Cornelius Metzger <39711796+tcmetzger@users.noreply.github.com>
    
    * Update examples/models/file/basic_plot.py
    
    Co-authored-by: Timo Cornelius Metzger <39711796+tcmetzger@users.noreply.github.com>
    
    Co-authored-by: Timo Cornelius Metzger <39711796+tcmetzger@users.noreply.github.com>

--- examples/models/file/basic_plot.py
+++ examples/models/file/basic_plot.py
@@ -1,3 +1,14 @@
+''' A scatter plot of a smooth periodic oscillation. This example demonstrates red
+circle scatter markers with black outlines, using the low-level ``bokeh.models``
+API.
+
+.. bokeh-example-metadata::
+    :sampledata: sine wave
+    :apis: bokeh.models.Circle, bokeh.models.Plot, bokeh.models.ColumnDataSource, bokeh.models.LinearAxis, bokeh.models.PanTool, bokeh.models.WheelZoomTool
+    :refs: :ref:`userguide_plotting` > :ref:`userguide_plotting_scatter_markers`
+    :keywords: circle, figure, scatter
+
+'''
 from numpy import arange, pi, sin
 
 from bokeh.document import Document
