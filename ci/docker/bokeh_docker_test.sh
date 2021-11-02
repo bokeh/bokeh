9baf3fcf5ebc9be11e37cce790ce8c8b72187bb9
Ian Thomas <ianthomas23@gmail.com>
2021-11-02 13:35:44 +0000

    Docker dev image (#11352)

---
+++ ci/docker/bokeh_docker_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Note do not exit on error, as want remaining tests to run.
+set -u
+
+echo "Start of $0"
+
+python -m bokeh info
+bokeh sampledata
+
+# Run some of the tests.
+pytest tests/codebase
+cd bokehjs && node make test
+
+echo "End of $0"
