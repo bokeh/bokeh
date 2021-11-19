8d939c9dfd322dff4a1443654459d9d5a4e87b53
Mateusz Paprocki <mattpap@gmail.com>
2021-11-19 17:01:54 +0100

    Force webgl backend when requested for testing
    webgl backend normally falls back to canvas rendering, but that's
    undesirable when testing specifically webgl support. For testing
    the fallback mechanism, use `settings.force_webgl = false`.

--- bokehjs/src/lib/core/settings.ts
+++ bokehjs/src/lib/core/settings.ts
@@ -1,6 +1,7 @@
 export class Settings {
   private _dev = false
   private _wireframe = false
+  private _force_webgl = false
 
   set dev(dev: boolean) {
     this._dev = dev
@@ -16,6 +17,14 @@ export class Settings {
 
   get wireframe(): boolean {
     return this._wireframe
+  }
+
+  set force_webgl(force_webgl: boolean) {
+    this._force_webgl = force_webgl
+  }
+
+  get force_webgl(): boolean {
+    return this._force_webgl
   }
 }
 
