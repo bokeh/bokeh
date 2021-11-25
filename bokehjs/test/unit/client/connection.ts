2c018f7abd6723ec9514864bfd6917991e4c65c1
Mateusz Paprocki <mattpap@gmail.com>
2021-11-25 23:14:55 +0100

    Update unit/client/connection tests

--- bokehjs/test/unit/client/connection.ts
+++ bokehjs/test/unit/client/connection.ts
@@ -2,16 +2,24 @@ import {expect} from "assertions"
 
 import {pull_session} from "@bokehjs/client/connection"
 import {Range1d} from "@bokehjs/models/ranges/range1d"
+import {unique_id} from "@bokehjs/core/util/string"
+
+const port = 5009
+const url = `ws://localhost:${port}/ws`
+
+function token(session_id: string = unique_id(), session_expiry: number = Date.now() + 300) {
+  return btoa(JSON.stringify({session_id, session_expiry})).replace(/=+$/, "")
+}
 
 describe("ClientSession", () => {
 
-  it.with_server("should be able to connect", async (url) => {
-    const session = await pull_session(url)
+  it("should be able to connect", async () => {
+    const session = await pull_session(url, token())
     session.close()
   })
 
-  it.with_server("should pass request string to connection", async (url) => {
-    const session = await pull_session(url, undefined, "foo=10&bar=20")
+  it("should pass request string to connection", async () => {
+    const session = await pull_session(url, token(), "foo=10&bar=20")
     try {
       expect((session as any)._connection.args_string).to.be.equal("foo=10&bar=20") // XXX
     } finally {
@@ -19,30 +27,30 @@ describe("ClientSession", () => {
     }
   })
 
-  it.with_server("should be able to connect again", async (url) => {
-    const session = await pull_session(url)
+  it("should be able to connect again", async () => {
+    const session = await pull_session(url, token())
     session.close()
   })
 
-  it.with_server("should get server info", async (url) => {
-    const session = await pull_session(url)
+  it("should get server info", async () => {
+    const session = await pull_session(url, token())
     try {
       const info = await session.request_server_info()
-      expect("version_info" in Object(info)).to.be.true
+      expect("version_info" in info).to.be.true
     } finally {
       session.close()
     }
   })
 
-  it.with_server("should sync a document between two connections", async (url) => {
-    const session1 = await pull_session(url)
+  it("should sync a document between two connections", async () => {
+    const session1 = await pull_session(url, token())
     try {
       const root = new Range1d({start: 123, end: 456})
       session1.document.add_root(root)
       session1.document.set_title("Hello Title")
       await session1.force_roundtrip()
 
-      const session2 = await pull_session(url, session1.id)
+      const session2 = await pull_session(url, token(session1.id))
       try {
         expect(session2.document.roots().length).to.be.equal(1)
         const root = session2.document.roots()[0]
