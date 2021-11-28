7581ed5b15c37add2ab4918514f4377ac09e4c3c
Abod <abod80000@gmail.com>
2021-11-28 22:16:37 +0200

    bokeh/bokeh#11640 Document Tool.icon in user guide (#11825)
    * #11640 Document Tool.icon in user guide
    
    * Remove unrelated  changes
    
    * Apply suggestions from code review
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>
    
    * remove PIL icon example
    
    * Apply suggestions from code review #2
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>
    
    * Adding blank lines between code blocks
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

---
+++ sphinx/source/docs/user_guide/examples/customizing_tools_icon.py
@@ -0,0 +1,18 @@
+from bokeh.models.tools import BoxSelectTool
+from bokeh.plotting import figure, show
+
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 2, 4, 5]
+
+# create a new figure with no tools
+p = figure(tools="", width=400, height=400)
+
+# Adding a tool with custom icon
+p.add_tools(BoxSelectTool(icon="xpan"))
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend_label="Temp.", line_width=2)
+
+# show the results
+show(p)
