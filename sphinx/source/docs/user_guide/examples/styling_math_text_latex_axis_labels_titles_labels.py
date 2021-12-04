877c7f0aa18d3ae8c73f6ae06b575c2317143a2e
Timo Cornelius Metzger <39711796+tcmetzger@users.noreply.github.com>
2021-12-03 16:11:37 -0800

    Update docs for math text styling (#11660)
    * Update styling.rst
    
    * Update blackbody example
    
    * Update LaTeX infos
    
    * Update LaTeX examples

---
+++ sphinx/source/docs/user_guide/examples/styling_math_text_latex_axis_labels_titles_labels.py
@@ -0,0 +1,22 @@
+from numpy import arange, pi, sin
+
+from bokeh.models.annotations.labels import Label
+from bokeh.plotting import figure, show
+
+x = arange(-2*pi, 2*pi, 0.1)
+y = sin(x)
+
+p = figure(height=250, title=r"\[\sin(x)\text{ for }x\text{ between }-2\pi\text{ and }2\pi\]")
+p.circle(x, y, alpha=0.6, size=7)
+
+label = Label(
+    text="$$y = \sin(x)\$$",
+    x=150, y=130,
+    x_units="screen", y_units="screen",
+)
+p.add_layout(label)
+
+p.yaxis.axis_label = r"\[\sin(x)\]"
+p.xaxis.axis_label = r"\[x\pi\]"
+
+show(p)
