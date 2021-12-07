1ae4c55f3426864f5b7315ff5705a0a2d8c9c790
shihua sun <nora.sun@mail.utoronto.ca>
2021-12-06 22:13:38 -0500

    Add metadata to examples/plotting/file/bar_stacked_grouped.py

--- examples/plotting/file/bar_stacked_grouped.py
+++ examples/plotting/file/bar_stacked_grouped.py
@@ -1,6 +1,14 @@
-from bokeh.io import output_file, show
+''' A stacked bar chart grouped into four groups using data in plain Python lists.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.models.ColumnDataSource, bokeh.models.FactorRange, bokeh.plotting.Figure.vbar_stack
+    :refs: :ref:`userguide_categorical` > :ref:`userguide_categorical_bars` > :ref:`userguide_categorical_bars_stacked_and_grouped`
+    :keywords: bars, categorical, stacked, grouped
+
+'''
+from bokeh.io import output_file
 from bokeh.models import ColumnDataSource, FactorRange
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 
 output_file("bar_stacked_grouped.html")
 
