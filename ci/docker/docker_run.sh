fc09cbb504e3f92cd712f7f9e70ea91895f991c2
Ian Thomas <ianthomas23@gmail.com>
2021-12-09 17:28:54 +0000

    Docker image build and use (#11856)
    * Load image into docker images
    
    * Try optional running docker container
    
    * Run docker container with correct uid and gid
    
    * Run tests after build in docker container
    
    * Upload test results to artifact
    
    * Remove duplicated source checkout
    
    * Install conda into docker filesystem not volume mount on host filesystem
    
    * Add bash start script and choice of python version
    
    * Tag docker image with chrome version
    
    * Don't run docker interactively if only want chrome version

---
+++ ci/docker/docker_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Usage: docker_run.sh bokeh-dev:latest
+# Can put env vars first, e.g.
+#   BOKEH_DOCKER_PY=3.7 docker_run.sh bokeh-dev:latest
+
+set -eu
+
+if [ $# -ne 1 ]; then
+    echo "Usage: docker_run.sh <docker image and tag>, e.g. docker_run.sh bokeh-dev:latest"
+    exit 1
+fi
+
+IMAGE_AND_TAG=$1
+UID_GID="`id --user`:`id --group`"
+
+ENV_VARS=""
+for name in BOKEH_DOCKER_CONDA BOKEH_DOCKER_PY BOKEH_DOCKER_BUILD BOKEH_DOCKER_TEST BOKEH_DOCKER_CHROME_VERSION; do
+    if [ -n "${!name+set}" ]; then
+        ENV_VARS="$ENV_VARS -e $name=${!name}"
+    fi
+done
+
+INTERACTIVE="-it"
+if [ "${BOKEH_DOCKER_CHROME_VERSION:-0}" == 1 ]; then
+    # If only want chrome version, do not need to run interactively.
+    INTERACTIVE=""
+fi
+
+CMD="docker run -v $PWD:/bokeh -u $UID_GID -p 5006:5006 $ENV_VARS $INTERACTIVE $IMAGE_AND_TAG"
+echo $CMD
+$CMD
