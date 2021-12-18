deb8f00934866b5a02887488202c037ab6fdbbab
Mateusz Paprocki <mattpap@gmail.com>
2021-12-18 22:25:54 +0100

    Monitor **/package.json for changes

--- bokehjs/prepare.js
+++ bokehjs/prepare.js
@@ -1,12 +1,19 @@
 const crypto = require("crypto")
 const fs = require("fs")
+const {join, dirname, basename} = require("path")
 
 function write_hash(file) {
   const hash = crypto.createHash("sha256")
                      .update(fs.readFileSync(file))
                      .digest("hex")
 
-  fs.writeFileSync("." + file, hash)
+  const path = join(dirname(file), `.${basename(file)}`)
+  fs.writeFileSync(path, hash)
 }
 
-write_hash("package.json")
+const {workspaces} = require("./package.json")
+
+for (const workspace of ["", ...workspaces]) {
+  const path = join(workspace, "package.json")
+  write_hash(path)
+}
