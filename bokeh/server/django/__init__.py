711df90aa6f0cdf089e2c1c58cc3b9ec65ea1b28
Mateusz Paprocki <mattpap@gmail.com>
2022-02-20 16:38:07 +0100

    Drop default_app_config

--- bokeh/server/django/__init__.py
+++ bokeh/server/django/__init__.py
@@ -8,5 +8,3 @@ from .static import static_extensions
 
 import_required("django", "django is required by bokeh.server.django")
 import_required("channels", "The package channels is required by bokeh.server.django and must be installed")
-
-default_app_config = "bokeh.server.django.DjangoBokehConfig"
