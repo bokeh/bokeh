ea72d800e805530b007b435872ce1e44de8f5b3b
Ian Thomas <ianthomas23@gmail.com>
2022-03-21 21:18:33 +0000

    Marker glyph should support line join test

--- bokehjs/test/integration/glyphs/markers.ts
+++ bokehjs/test/integration/glyphs/markers.ts
@@ -1,7 +1,7 @@
 import {display, fig, row} from "../_util"
 
 import {HatchPattern} from "@bokehjs/core/property_mixins"
-import {MarkerType, OutputBackend} from "@bokehjs/core/enums"
+import {LineJoin, MarkerType, OutputBackend} from "@bokehjs/core/enums"
 import {Random} from "@bokehjs/core/util/random"
 import {assert} from "@bokehjs/core/util/assert"
 
@@ -119,18 +119,19 @@ describe("Marker glyph", () => {
       "dot", "ring", "horizontal_line", "vertical_line", "cross", "horizontal_dash",
       "vertical_dash", "spiral", "right_diagonal_line", "left_diagonal_line", "diagonal_cross",
       "right_diagonal_dash", "left_diagonal_dash", "horizontal_wave", "vertical_wave",
-      "criss_cross", "dot", "ring", "horizontal_line", "vertical_line", "cross",
+      "criss_cross", "dot", "ring", "horizontal_line", "vertical_line", "cross", "horizontal_dash",
     ]
 
     const markers: MarkerType[] = [
       "circle", "circle_cross", "circle_dot", "circle_x", "circle_y", "diamond", "diamond_cross",
       "diamond_dot", "hex", "hex_dot", "inverted_triangle", "square", "square_cross", "square_dot",
       "square_pin", "square_x", "star", "star_dot", "triangle", "triangle_dot", "triangle_pin",
+      "plus",
     ]
 
     function make_plot(output_backend: OutputBackend) {
       const p = fig([300, 300], {
-        output_backend, title: output_backend, x_range: [-0.5, 4.5], y_range: [-0.6, 4.6],
+        output_backend, title: output_backend, x_range: [-0.5, 4.7], y_range: [-0.6, 4.6],
       })
       const n = hatch_patterns.length
       for (let i = 0; i < n; i++) {
@@ -138,6 +139,33 @@ describe("Marker glyph", () => {
           x: i / 5, y: i % 5, marker: markers[i], size: 45, hatch_pattern: hatch_patterns[i],
           line_color: "red", line_alpha: 0.5, line_width: 2, fill_color: "gainsboro",
           hatch_color: "blue",
+        })
+      }
+      return p
+    }
+
+    const p0 = make_plot("canvas")
+    const p1 = make_plot("svg")
+    const p2 = make_plot("webgl")
+
+    await display(row([p0, p1, p2]))
+  })
+
+  it("should support line join", async () => {
+    const markers: MarkerType[] = [
+      "diamond", "hex", "plus", "square", "square_pin", "star", "triangle", "triangle_pin",
+    ]
+    const line_joins: LineJoin[] = ["round", "miter", "bevel"]
+
+    function make_plot(output_backend: OutputBackend) {
+      const p = fig([175, 420], {
+        output_backend, title: output_backend, x_range: [-0.05, 0.25], y_range: [-0.05, 0.76],
+      })
+      p.grid.visible = false
+      for (let i = 0; i < markers.length; i++) {
+        p.scatter({
+          x: [0, 0.1, 0.2], y: 0.1*(7-i), marker: markers[i], size: 35, line_width: 7,
+          line_join: line_joins, line_alpha: 0.5, fill_color: "bisque",
         })
       }
       return p
