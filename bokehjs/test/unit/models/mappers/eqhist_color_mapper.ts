b65ba911ecad6f01dea1664a643dbc2f927ee5ca
Ian Thomas <ianthomas23@gmail.com>
2022-04-28 10:17:31 +0100

    Better numerical unit tests

--- bokehjs/test/unit/models/mappers/eqhist_color_mapper.ts
+++ bokehjs/test/unit/models/mappers/eqhist_color_mapper.ts
@@ -6,33 +6,27 @@ describe("EqHistColorMapper module", () => {
 
   describe("EqHistColorMapper.scan method", () => {
 
-    function scan_check(values: number[]) {
+    function scan_check(values: number[], expected_binning: number[]) {
       const n = values.length
-      const palette = new Array(n).fill("red")  // Don't care what colors are
+      const palette = new Array(n).fill("red")  // Colors not used but needed for constructor
       const color_mapper = new EqHistColorMapper({palette})
       const scan = color_mapper.scan(values, n)
 
       const sorted = values.sort()
       expect(scan.min).to.be.equal(sorted[0])
       expect(scan.max).to.be.equal(sorted[n-1])
-      expect(scan.binning[0]).to.be.equal(sorted[0])
-      expect(scan.binning[n]).to.be.equal(sorted[n-1])
-
-      for (let i = 1; i < n; i++) {
-        expect(scan.binning[i]).to.be.above(sorted[i-1])
-        expect(scan.binning[i]).to.be.below(sorted[i])
-      }
+      expect(scan.binning).to.be.similar(expected_binning, 1e-6)
     }
 
     it("Should scan equally-spaced values", () => {
-      scan_check([1, 2, 3])
-      scan_check([3, 2, 1])
-      scan_check([3, 1, 2])
+      scan_check([1, 2, 3], [1, 1.999975, 2.999964, 3])
+      scan_check([3, 2, 1], [1, 1.999975, 2.999964, 3])
+      scan_check([3, 1, 2], [1, 1.999975, 2.999964, 3])
     })
 
     it("Should scan non-equally-spaced values", () => {
-      scan_check([1, 1.001, 2])
-      scan_check([1, 1.999, 2])
+      scan_check([1, 1.001, 2], [1, 1.000994, 1.999982, 2])
+      scan_check([1, 1.999, 2], [1, 1.998995, 1.999982, 2])
     })
   })
 })
