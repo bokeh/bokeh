3394c7eca0f08bc6eed3f010ea6021f4e3d7c4a6
Michael Brown <michael@msbrown.net>
2022-05-13 12:10:47 -0400

    Fix full-circle wedge hit test (#12124)
    When Wedge and AnnularWedge glyphs go all the way around a full circle,
    the hit test would always fail. This is because `angle_between()`
    normalizes between 0 and 2*pi, then returns false if they are equal.
    This adds an additional check in the glyph's hit testing such that if
    the start and end angle are more than a full circle apart, it is
    considered a hit because the wedge is going all the way around in a
    circle.

--- bokehjs/test/unit/models/glyphs/wedge.ts
+++ bokehjs/test/unit/models/glyphs/wedge.ts
@@ -61,5 +61,51 @@ describe("Glyph (using Wedge as a concrete Glyph)", () => {
         expect(result.indices).to.be.equal(expected_hits[i])
       }
     })
+
+    it("should hit test full circle wedges against an index", async () => {
+      const data = {
+        // Single wedge going all the way around
+        start_angle: [0],
+        end_angle: [2*Math.PI],
+      }
+      const glyph = new Wedge({
+        x: {value: 50},
+        y: {value: 50},
+        radius: {value: 50},
+        start_angle: {field: "start_angle"},
+        end_angle: {field: "end_angle"},
+      })
+
+      const glyph_renderer = await create_glyph_renderer_view(glyph, data, {axis_type: "linear"})
+      const glyph_view = glyph_renderer.glyph
+
+      const geometries: Geometry[] = [
+        // Points just inside and outside the circle
+        {type: "point", sx: 100 +  90, sy: 100},        // Right, inside
+        {type: "point", sx: 100 + 110, sy: 100},        // Right, outside
+        {type: "point", sx: 100,       sy: 100 -  90},  // Top, inside
+        {type: "point", sx: 100,       sy: 100 - 110},  // Top, outside
+        {type: "point", sx: 100 -  90, sy: 100},        // Left, inside
+        {type: "point", sx: 100 - 110, sy: 100},        // Left, outside
+        {type: "point", sx: 100,       sy: 100 +  90},  // Bottom, inside
+        {type: "point", sx: 100,       sy: 100 + 110},  // Bottom, outside
+      ]
+
+      const expected_hits = [
+        [0],
+        [],
+        [0],
+        [],
+        [0],
+        [],
+        [0],
+        [],
+      ]
+
+      for (let i = 0; i < geometries.length; i++) {
+        const result = glyph_view.hit_test(geometries[i])!
+        expect(result.indices).to.be.equal(expected_hits[i])
+      }
+    })
   })
 })
