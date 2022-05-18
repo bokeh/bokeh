b433e0444a1cfd83f6ccc7cc46607568ea86a1d8
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-05-18 16:55:13 -0700

    Add check for GA code during  docs build

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -5,3 +5,10 @@ set -x #echo on
 cd sphinx
 export GOOGLE_API_KEY=${GOOGLE_API_KEY:-"unset"}
 make SPHINXOPTS=-v all
+
+if grep -e "UA-27761864-7" build/html/index.html > /dev/null; then
+    echo "Expected Google Analytics id code found in built documentation"
+else
+    echo "Expected Google Analytics id code not found in built documentation"
+    exit 1
+fi
