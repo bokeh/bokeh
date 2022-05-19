84f097bea9005d3345b1245656773393beb8ddff
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-05-18 20:39:45 -0700

    Update test search string

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -6,9 +6,9 @@ cd sphinx
 export GOOGLE_API_KEY=${GOOGLE_API_KEY:-"unset"}
 make SPHINXOPTS=-v all
 
-if grep -e "UA-27761864-7" build/html/index.html > /dev/null; then
-    echo "Expected Google Analytics id code found in built documentation"
+if grep -e 'src="https://www.google-analytics.com' build/html/index.html > /dev/null; then
+    echo "Expected Google Analytics script source found in built documentation"
 else
-    echo "Expected Google Analytics id code not found in built documentation"
+    echo "Expected Google Analytics script source not found in built documentation"
     exit 1
 fi
