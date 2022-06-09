7ba79d1a0e03986c8ce737b3283f0a95e2cd6510
Jada Lilleboe <82007190+jadalilleboe@users.noreply.github.com>
2022-06-09 16:35:12 -0500

    Add metadata to choropleth.py and sliders.py (#12137)

--- examples/models/file/sliders.py
+++ examples/models/file/sliders.py
@@ -1,3 +1,11 @@
+'''Demonstrates the use of many Bokeh sliders with examples such as a numerical slider, a disabled slider, a date picker,
+and a color picker.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.models.Column, bokeh.models.CustomJS, bokeh.models.DateRangeSlider, bokeh.models.DateSlider, bokeh.models.Div, bokeh.models.RangeSlider, bokeh.models.Row, bokeh.models.Slider, bokeh.document.document, bokeh.embed.file_html, bokeh.util.browser.view # noqa: E501
+    :refs: :ref:`userguide_interaction` > :ref:`userguide_interaction_widgets`
+    :keywords: slider
+'''
 from datetime import date
 
 from bokeh.document import Document
