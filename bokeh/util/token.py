e6d99cf03ea0da807c09501bdeb59f9c886989bd
Bryan Van de Ven <bryan@bokeh.org>
2022-07-06 20:38:21 -0700

    slight mypy cleanup

--- bokeh/util/token.py
+++ bokeh/util/token.py
@@ -34,12 +34,7 @@ import hmac
 import json
 import time
 import zlib
-from typing import (
-    Any,
-    Dict,
-    Tuple,
-    Union,
-)
+from typing import Any, Dict
 
 # Bokeh imports
 from ..core.types import ID
@@ -94,7 +89,7 @@ def generate_jwt_token(session_id: ID,
                        signed: bool = settings.sign_sessions(),
                        extra_payload: TokenPayload | None = None,
                        expiration: int = 300) -> str:
-    """Generates a JWT token given a session_id and additional payload.
+    """ Generates a JWT token given a session_id and additional payload.
 
     Args:
         session_id (str):
@@ -247,7 +242,7 @@ class _BytesDecoder(json.JSONDecoder):
             return _base64_decode(obj["bytes"])
         return obj
 
-def _get_sysrandom() -> Tuple[Any, bool]:
+def _get_sysrandom() -> tuple[Any, bool]:
     # Use the system PRNG for session id generation (if possible)
     # NOTE: secure random string generation implementation is adapted
     #       from the Django project. Reference:
@@ -268,7 +263,7 @@ def _get_sysrandom() -> Tuple[Any, bool]:
         using_sysrandom = False
         return random, using_sysrandom
 
-def _ensure_bytes(secret_key: Union[str, bytes, None]) -> bytes | None:
+def _ensure_bytes(secret_key: str | bytes | None) -> bytes | None:
     if secret_key is None:
         return None
     elif isinstance(secret_key, bytes):
@@ -290,7 +285,7 @@ def _reseed_if_needed(using_sysrandom: bool, secret_key: bytes | None) -> None:
         random.seed(hashlib.sha256(data).digest())
 
 
-def _base64_encode(decoded: Union[bytes, str]) -> str:
+def _base64_encode(decoded: bytes | str) -> str:
     # base64 encode both takes and returns bytes, we want to work with strings.
     # If 'decoded' isn't bytes already, assume it's utf-8
     decoded_as_bytes = _ensure_bytes(decoded)
@@ -301,7 +296,7 @@ def _base64_encode(decoded: Union[bytes, str]) -> str:
     return str(encoded.rstrip('='))
 
 
-def _base64_decode(encoded: Union[bytes, str]) -> bytes:
+def _base64_decode(encoded: bytes | str) -> bytes:
     # base64 lib both takes and returns bytes, we want to work with strings
     encoded_as_bytes = codecs.encode(encoded, 'ascii') if isinstance(encoded, str) else encoded
     # put the padding back
@@ -322,11 +317,12 @@ def _get_random_string(
         length: int = 44,
         allowed_chars: str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
         secret_key: bytes | None = settings.secret_key_bytes()) -> str:
-    """
-    Return a securely generated random string.
+    """ Return a securely generated random string.
+
     With the a-z, A-Z, 0-9 character set:
     Length 12 is a 71-bit value. log_2((26+26+10)^12) =~ 71
     Length 44 is a 261-bit value. log_2((26+26+10)^44) = 261
+
     """
     secret_key = _ensure_bytes(secret_key)
     _reseed_if_needed(using_sysrandom, secret_key)
