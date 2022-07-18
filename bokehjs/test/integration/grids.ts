d62d2bfd17c2c4fdcb93a20c24e233b846a1ad9d
Mateusz Paprocki <mattpap@gmail.com>
2022-07-18 03:17:52 +0200

    Add support for cross bounds to grid lines (#12244)

---
+++ bokehjs/test/integration/grids.ts
@@ -0,0 +1,18 @@
+import {display, fig} from "./_util"
+
+describe("Grid", () => {
+  it("should support cross bounds", async () => {
+    const p = fig([300, 300])
+    p.circle({x: [4, 5, 6], y: [1, 2, 3], size: 20})
+
+    p.xaxis.fixed_location = 1.5
+    p.xaxis.bounds = [4, 5]
+    p.xgrid.cross_bounds = [1, 3]
+
+    p.yaxis.fixed_location = 4.5
+    p.yaxis.bounds = [1, 3]
+    p.ygrid.cross_bounds = [4, 5]
+
+    await display(p)
+  })
+})
