f4fbaa18e44ac4a0f1ed8197e1948add1155c080
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-07-23 22:10:58 -0700

    Upodate examples

--- sphinx/source/docs/user_guide/examples/interaction_autocompleteinput.py
+++ sphinx/source/docs/user_guide/examples/interaction_autocompleteinput.py
@@ -1,5 +1,5 @@
-from bokeh.models import AutocompleteInput
 from bokeh.io import show
+from bokeh.models import AutocompleteInput
 from bokeh.sampledata.world_cities import data
 
 completion_list = data["name"].tolist()
--- sphinx/source/docs/user_guide/examples/interaction_numericinput.py
+++ sphinx/source/docs/user_guide/examples/interaction_numericinput.py
@@ -1,6 +1,5 @@
-
-from bokeh.models import NumericInput
 from bokeh.io import show
+from bokeh.models import NumericInput
 
 numeric_input = NumericInput(value=1, low=1, high=10, title="Enter a number between 1 and 10:")
 
