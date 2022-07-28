ae5c05e7032f0f394b0fbb96ad0b8837dd52de62
Mateusz Paprocki <mattpap@gmail.com>
2022-07-29 00:24:06 +0200

    Move normal random generator from np to Random (#12269)

--- bokehjs/src/lib/core/util/random.ts
+++ bokehjs/src/lib/core/util/random.ts
@@ -1,4 +1,5 @@
 const MAX_INT32 = 2147483647
+const {PI, log, sin, cos, sqrt} = Math
 
 // Park-Miller LCG
 export class Random {
@@ -35,6 +36,23 @@ export class Random {
       result[i] = items[this.integer() % k]
     }
     return result
+  }
+
+  normal(loc: number, scale: number, size: number): Float64Array {
+    const [mu, sigma] = [loc, scale]
+
+    const array = new Float64Array(size)
+    for (let i = 0; i < size; i += 2) {
+      // Box-Muller transform from uniform to normal distribution.
+      const u = this.float()
+      const v = this.float()
+      const common = sqrt(-2.0*log(u))
+      array[i] = mu + sigma*(common*cos(2.0*PI*v))
+      if (i + 1 < size)
+        array[i + 1] = mu + sigma*(common*sin(2.0*PI*v))
+    }
+
+    return array
   }
 }
 
