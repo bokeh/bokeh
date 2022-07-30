207d41e26e718d57d8cd85cb987473e9af1e3437
Mateusz Paprocki <mattpap@gmail.com>
2022-07-30 20:56:18 +0200

    Add visual integration tests for icons

--- bokehjs/src/lib/core/kinds.ts
+++ bokehjs/src/lib/core/kinds.ts
@@ -424,6 +424,16 @@ export namespace Kinds {
       return `NonNegative(${this.base_type.toString()})`
     }
   }
+
+  export class DOMNode extends Kind<Node> {
+    valid(value: unknown): value is Node {
+      return value instanceof Node
+    }
+
+    override toString(): string {
+      return "DOMNode"
+    }
+  }
 }
 
 export const Any = new Kinds.Any()
@@ -449,6 +459,7 @@ export const Enum = <T extends string | number>(...values: T[]) => new Kinds.Enu
 export const Ref = <ObjType extends object>(obj_type: Constructor<ObjType>) => new Kinds.Ref<ObjType>(obj_type)
 export const AnyRef = <ObjType extends object>() => new Kinds.AnyRef<ObjType>()
 export const Function = <Args extends unknown[], Ret>() => new Kinds.Function<Args, Ret>()
+export const DOMNode = new Kinds.DOMNode()
 
 export const NonNegative = <BaseType extends number>(base_type: Kind<BaseType>) => new Kinds.NonNegative(base_type)
 
