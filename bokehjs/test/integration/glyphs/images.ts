cd1ee0cda7de2b12d92c25a7f7cf65da225c17b1
Ian Thomas <ianthomas23@gmail.com>
2022-08-02 12:07:18 +0100

    Visual integration tests

--- bokehjs/test/integration/glyphs/images.ts
+++ bokehjs/test/integration/glyphs/images.ts
@@ -1,6 +1,7 @@
 import {display, fig, row} from "../_util"
 
 import {Anchor, ImageOrigin} from "@bokehjs/core/enums"
+import {encode_rgba} from "@bokehjs/core/util/color"
 import {load_image} from "@bokehjs/core/util/image"
 import {ndarray} from "@bokehjs/core/util/ndarray"
 import {DataRange1d, ImageRGBA} from "@bokehjs/models"
@@ -15,7 +16,7 @@ function get_image_data(image: HTMLImageElement): ImageData {
   return ctx.getImageData(0, 0, width, height)
 }
 
-async function plot(anchor: ImageRGBA["anchor"], x_flipped: boolean = false, y_flipped: boolean = false) {
+async function plot(anchor: ImageRGBA["anchor"], x_flipped: boolean = false, y_flipped: boolean = false, global_alpha: number = 1.0) {
   const {data, width, height} = get_image_data(await load_image("/assets/images/logo.svg"))
   const image = ndarray(data.buffer, {dtype: "uint32", shape: [width, height]})
 
@@ -25,7 +26,7 @@ async function plot(anchor: ImageRGBA["anchor"], x_flipped: boolean = false, y_f
     const y_range = new DataRange1d({flipped: y_flipped})
 
     const p = fig([200, 200], {title: `Origin: ${origin}`, x_range, y_range})
-    p.image_rgba({image: {value: image}, x: 0, y: 0, dw: 10, dh: 10, origin, anchor})
+    p.image_rgba({image: {value: image}, x: 0, y: 0, dw: 10, dh: 10, origin, anchor, global_alpha})
 
     plots.push(p)
   }
@@ -55,4 +56,34 @@ describe("ImageRGBA glyph", () => { // TODO: async describe
       await plot(["center", 0.2], x_flipped, y_flipped)
     })
   }
+
+  it("should support combine alpha", async () => {
+   function rgba_image() {
+      const N = 5
+      const d = new Uint32Array(N*N)
+      const dv = new DataView(d.buffer)
+
+      const {trunc} = Math
+      for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+          const r = trunc(i/N*255)
+          const g = 0
+          const b = 255 - r
+          const a = trunc(j/N*255)
+          dv.setUint32(4*(i*N + j), encode_rgba([r, g, b, a]))
+        }
+      }
+      return ndarray(d, {shape: [N, N]})
+    }
+
+    const dw = 1
+    const dh = 1
+
+    const p = fig([350, 150])
+    p.image_rgba({image: {value: rgba_image()}, x: 0, y: 0, dw, dh, global_alpha: 1.0})
+    p.image_rgba({image: {value: rgba_image()}, x: 1, y: 0, dw, dh, global_alpha: 0.66})
+    p.image_rgba({image: {value: rgba_image()}, x: 2, y: 0, dw, dh, global_alpha: 0.33})
+
+    await display(p)
+  })
 })
