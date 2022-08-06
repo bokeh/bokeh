44ca83a9fb0778fd18bc96737798b01f907a9785
Mateusz Paprocki <mattpap@gmail.com>
2022-08-06 20:42:34 +0200

    Resolve selenium 5.x webdriver related deprecations

--- typings/selenium/webdriver/firefox/webdriver.pyi
+++ typings/selenium/webdriver/firefox/webdriver.pyi
@@ -1,11 +1,10 @@
+from selenium.webdriver.firefox.service import Service
+
 from ..remote.webdriver import WebDriver as RemoteWebDriver
-from .firefox_binary import FirefoxBinary
 from .options import Options
 
 class WebDriver(RemoteWebDriver):
     def __init__(self,
+        service: Service | None = ...,
         options: Options | None = ...,
-        firefox_binary: FirefoxBinary | None = ...,
-        executable_path: str | None = ...,
-        service_log_path: str = ...,
     ) -> None: ...
