eac731b92aff9a7deec3baa2d1c96b137cb4f16f
Bryan Van de Ven <bryan@bokeh.org>
2022-08-17 22:05:00 -0700

    remove cruft

--- ci/install_bokeh_package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x #echo on
-
-$CONDA/bin/conda install --yes --quiet --offline /tmp/conda-bld/noarch/bokeh-*.bz2
-
-# TODO (bev) remove this when move from bokeh -> src dir struture
-rm -rf bokeh
