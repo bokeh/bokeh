17f70831dc12ed58ae5370e119a9bb8ffdf61c5a
Mateusz Paprocki <mattpap@gmail.com>
2022-08-18 10:03:00 +0200

    Help mypy type checking trivial expressions

--- src/bokeh/colors/color.py
+++ src/bokeh/colors/color.py
@@ -298,10 +298,10 @@ class RGB(Color):
     def from_tuple(cls, value: RGBTuple) -> RGB:
         ''' Initialize ``RGB`` instance from a 3- or 4-tuple. '''
         if len(value) == 3:
-            r, g, b = value
+            r, g, b = value         # type:ignore # https://github.com/python/mypy/issues/1178
             return RGB(r, g, b)
         else:
-            r, g, b, a = value
+            r, g, b, a = value      # type:ignore # https://github.com/python/mypy/issues/1178
             return RGB(r, g, b, a)
 
     @classmethod
