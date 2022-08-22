19d236ec25e8a6fab1ba3494c5bb81b826b72a71
Bryan Van de Ven <bryan@bokeh.org>
2022-08-21 17:38:38 -0700

    More CI work (part 5) (#12324)
    * remove node make install task
    
    * remove pep440 cruft form release code
    
    * update OS version on release jobs
    
    * finer grained build/deploy steps on GH

--- release/__main__.py
+++ release/__main__.py
@@ -10,36 +10,62 @@ from __future__ import annotations
 import sys
 
 # Bokeh imports
+from . import stages
 from .config import Config
 from .pipeline import Pipeline
-from .stages import (
-    BUILD_CHECKS,
-    BUILD_STEPS,
-    DEPLOY_CHECKS,
-    DEPLOY_STEPS,
-)
 from .system import System
+from .util import load_config, save_config
 
 system = System()
 
-if sys.argv[1] == "build":
+stage_generators = {
+    "generate-build-checks": stages.BUILD_CHECKS,
+    "generate-build-steps": stages.BUILD_STEPS,
+    "generate-deploy-checks": stages.DEPLOY_CHECKS,
+    "generate-deploy-steps": stages.DEPLOY_STEPS,
+}
+
+if len(sys.argv) == 3 and sys.argv[1] == "generate-config":
+    config = Config(sys.argv[2])
+    save_config(config)
+    sys.exit(0)
+
+if len(sys.argv) == 2 and sys.argv[1] in stage_generators:
+    print([func.__name__ for func in stage_generators[sys.argv[1]]])
+    sys.exit(0)
+
+if len(sys.argv) == 2 and sys.argv[1] in dir(stages):
+    config = load_config()
+
+    func = getattr(stages, sys.argv[1])
+
+    pipe = Pipeline([func], config, system)
+    pipe.execute()
+
+    save_config(config)
+
+    sys.exit(0)
+
+if len(sys.argv) == 3 and sys.argv[1] == "build":
     config = Config(sys.argv[2])
 
-    check = Pipeline(BUILD_CHECKS, config, system)
+    check = Pipeline(stages.BUILD_CHECKS, config, system)
     check.execute()
 
-    steps = Pipeline(BUILD_STEPS, config, system)
+    steps = Pipeline(stages.BUILD_STEPS, config, system)
     steps.execute()
 
     sys.exit(0)
 
-if sys.argv[1] == "deploy":
+if len(sys.argv) == 3 and sys.argv[1] == "deploy":
     config = Config(sys.argv[2])
 
-    check = Pipeline(DEPLOY_CHECKS, config, system)
+    check = Pipeline(stages.DEPLOY_CHECKS, config, system)
     check.execute()
 
-    steps = Pipeline(DEPLOY_STEPS, config, system)
+    steps = Pipeline(stages.DEPLOY_STEPS, config, system)
     steps.execute()
 
     sys.exit(0)
+
+raise RuntimeError(f"Unrecognized args: {sys.argv[1:]}")
