147e2ed82e6d79e3ff32de03eda07da0519bf786
Mateusz Paprocki <mattpap@gmail.com>
2022-08-28 19:56:17 +0200

    Add preliminary visual tests for FlexBox

--- bokehjs/test/integration/layouts.ts
+++ bokehjs/test/integration/layouts.ts
@@ -1,7 +1,7 @@
 import {expect} from "../unit/assertions"
 import {display, fig, row, grid} from "./_util"
 
-import {Spacer, Tabs, TabPanel, GridBox, GroupBox, Column} from "@bokehjs/models/layouts"
+import {Spacer, Tabs, TabPanel, GridBox, GroupBox, Row, Column} from "@bokehjs/models/layouts"
 import {Pane} from "@bokehjs/models/ui"
 import {TextInput, Button} from "@bokehjs/models/widgets"
 import {SizingPolicy} from "@bokehjs/core/layout"
@@ -11,7 +11,7 @@ import {range} from "@bokehjs/core/util/array"
 import {Matrix} from "@bokehjs/core/util/matrix"
 import {color2css, is_dark, RGBA} from "@bokehjs/core/util/color"
 import {figure, gridplot} from "@bokehjs/api/plotting"
-import {BasicTickFormatter} from "@bokehjs/models/formatters"
+import {BasicTickFormatter, LinearAxis} from "@bokehjs/models"
 
 describe("UIElement", () => {
   it("should support dashed, snake and camel CSS property names in styles", async () => {
@@ -226,6 +226,73 @@ describe("3x3 GridBox", () => {
       cols: {"*": {policy: "auto", align: "end"}},
     })
     await display(l, viewport)
+  })
+})
+
+describe("FlexBox", () => {
+  const s = (color: Color) => new Spacer({
+    sizing_mode: "fixed",
+    width: 100,
+    height: 100,
+    styles: {background_color: color2css(color)},
+  })
+
+  function plot(a: number, b: number, color: Color) {
+    const p = fig([200, 200])
+    p.add_layout(new LinearAxis(), "above")
+    p.add_layout(new LinearAxis(), "right")
+    p.xaxis.each((axis) => (axis.formatter as BasicTickFormatter).use_scientific = false)
+    p.yaxis.each((axis) => (axis.formatter as BasicTickFormatter).use_scientific = false)
+    p.xaxis.major_label_orientation = "vertical"
+    p.yaxis.major_label_orientation = "horizontal"
+    const xs = [1, 2, 3].map((c) => c*a)
+    const ys = [1, 2, 3].map((c) => c*b)
+    p.circle(xs, ys, {size: 10, color})
+    return p
+  }
+
+  describe("should support Row layout", () => {
+    it("with 3 spacers of 100x100 size", async () => {
+      const row = new Row({
+        children: [s("red"), s("green"), s("blue")],
+      })
+
+      await display(row, [350, 150])
+    })
+
+    it("with 3 plots of 200x200 size", async () => {
+      const row = new Row({
+        children: [
+          plot(10**0, 10**0, "red"),
+          plot(10**2, 10**2, "green"),
+          plot(10**4, 10**4, "blue"),
+        ],
+      })
+
+      await display(row, [650, 250])
+    })
+  })
+
+  describe("should support Column layout", () => {
+    it("with 3 spacers of 100x100 size", async () => {
+      const row = new Column({
+        children: [s("red"), s("green"), s("blue")],
+      })
+
+      await display(row, [150, 350])
+    })
+
+    it("with 3 plots of 200x200 size", async () => {
+      const row = new Column({
+        children: [
+          plot(10**0, 10**0, "red"),
+          plot(10**2, 10**2, "green"),
+          plot(10**4, 10**4, "blue"),
+        ],
+      })
+
+      await display(row, [250, 650])
+    })
   })
 })
 
