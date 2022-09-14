2818852e41cb57bef6e57e49a113e49a621e5880
Bryan Van de Ven <bryan@bokeh.org>
2022-09-13 19:31:09 -0700

    Use wheel for testing

--- scripts/ci/build_conda_pacakge.sh
+++ scripts/ci/build_conda_pacakge.sh
@@ -7,7 +7,3 @@ git status
 
 export VERSION="$(echo "$(ls dist/*.whl)" | cut -d- -f2)"
 conda build conda/recipe --no-test --no-anaconda-upload --no-verify
-pushd $CONDA_PREFIX
-tar cvzf conda-bld-noarch.tgz conda-bld/noarch
-popd
-mv $CONDA_PREFIX/conda-bld-noarch.tgz /tmp
