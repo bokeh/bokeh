1ae01eb25ce81282d566ae734a90425799409d81
Bryan Van de Ven <bryan@bokeh.org>
2022-09-15 04:36:27 -0700

    fix stocks app (#12393)

--- examples/app/stocks/README.md
+++ examples/app/stocks/README.md
@@ -4,17 +4,10 @@ Create a simple stocks correlation dashboard.
 
 <img src="https://static.bokeh.org/stocks.png" width="80%"></img><
 
-## Setting Up
-
-To run the stocks example, you first need to download a sample data file.
-Execute the following command in this directory:
-
-    python download_sample_data.py
-
 ## Running
 
 To view the app directly from a Bokeh server, navigate to the parent directory
-[`examples/app`](https://github.com/bokeh/bokeh/tree/master/examples/app),
+[`examples/app`](https://github.com/bokeh/bokeh/tree/branch-3.0/examples/app),
 and execute the command:
 
     bokeh serve --show stocks
--- examples/app/stocks/main.py
+++ examples/app/stocks/main.py
@@ -1,11 +1,7 @@
-''' Create a simple stocks correlation dashboard.
+""" Create a simple stocks correlation dashboard.
 
 Choose stocks to compare in the drop down widgets, and make selections
 on the plots to update the summary and histograms accordingly.
-
-.. note::
-    Running this example requires downloading sample data. See
-    the included `README`_ for more information.
 
 Use the ``bokeh serve`` command to run the example by executing:
 
@@ -15,119 +11,97 @@ at your command prompt. Then navigate to the URL
 
     http://localhost:5006/stocks
 
-.. _README: https://github.com/bokeh/bokeh/blob/master/examples/app/stocks/README.md
-
-'''
+"""
 from functools import lru_cache
-from os.path import dirname, join
 
-import pandas as pd
-
-from bokeh.io import curdoc
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, PreText, Select
-from bokeh.plotting import figure
+from bokeh.plotting import curdoc, figure
 
-DATA_DIR = join(dirname(__file__), 'daily')
+DEFAULT_TICKERS = ["AAPL", "GOOG", "INTC", "NVDA", "MSFT"]
 
-DEFAULT_TICKERS = ['AAPL', 'GOOG', 'INTC', 'BRCM', 'YHOO']
+SERVER_CONTEXT = curdoc().session_context.server_context
 
 def nix(val, lst):
     return [x for x in lst if x != val]
 
-@lru_cache()
-def load_ticker(ticker):
-    fname = join(DATA_DIR, 'table_%s.csv' % ticker.lower())
-    data = pd.read_csv(fname, header=None, parse_dates=['date'],
-                       names=['date', 'foo', 'o', 'h', 'l', 'c', 'v'])
-    data = data.set_index('date')
-    return pd.DataFrame({ticker: data.c, ticker+'_returns': data.c.diff()})
-
-@lru_cache()
+@lru_cache
 def get_data(t1, t2):
-    df1 = load_ticker(t1)
-    df2 = load_ticker(t2)
-    data = pd.concat([df1, df2], axis=1)
-    data = data.dropna()
-    data['t1'] = data[t1]
-    data['t2'] = data[t2]
-    data['t1_returns'] = data[t1+'_returns']
-    data['t2_returns'] = data[t2+'_returns']
+    df1 = getattr(SERVER_CONTEXT, t1)
+    df2 = getattr(SERVER_CONTEXT, t2)
+    data = df1.join(df2, lsuffix=f"_{t1}", rsuffix=f"_{t2}").dropna()
+    data["t1"] = data[f"Close_{t1}"]
+    data["t2"] = data[f"Close_{t2}"]
+    data["t1_returns"] = data[f"Returns_{t1}"]
+    data["t2_returns"] = data[f"Returns_{t2}"]
     return data
-
-# set up widgets
-
-stats = PreText(text='', width=500)
-ticker1 = Select(value='AAPL', options=nix('GOOG', DEFAULT_TICKERS))
-ticker2 = Select(value='GOOG', options=nix('AAPL', DEFAULT_TICKERS))
-
-# set up plots
 
 source = ColumnDataSource(data=dict(date=[], t1=[], t2=[], t1_returns=[], t2_returns=[]))
 source_static = ColumnDataSource(data=dict(date=[], t1=[], t2=[], t1_returns=[], t2_returns=[]))
-tools = 'pan,wheel_zoom,xbox_select,reset'
 
-corr = figure(width=350, height=350,
-              tools='pan,wheel_zoom,box_select,reset')
-corr.circle('t1_returns', 't2_returns', size=2, source=source,
-            selection_color="orange", alpha=0.6, nonselection_alpha=0.1, selection_alpha=0.4)
+corr = figure(width=370, height=350,  min_border_left=60,
+              tools="pan,wheel_zoom,box_select,reset", active_drag="box_select")
+corr.circle("t1_returns", "t2_returns", size=3, source=source,
+            selection_color="orange", alpha=0.8,
+            nonselection_alpha=0.1, selection_alpha=0.5)
 
-ts1 = figure(width=900, height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
-ts1.line('date', 't1', source=source_static)
-ts1.circle('date', 't1', size=1, source=source, color=None, selection_color="orange")
+ts1 = figure(width=900, height=200, x_axis_type="datetime",
+             tools="pan,wheel_zoom,xbox_select,reset", active_drag="xbox_select")
+ts1.line("Date", "t1", source=source_static)
+ts1.circle("Date", "t1", size=3, source=source, color=None, selection_color="orange")
 
-ts2 = figure(width=900, height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts2 = figure(width=900, height=200, x_axis_type="datetime",
+             tools="pan,wheel_zoom,xbox_select,reset", active_drag="xbox_select")
+ts2.line("Date", "t2", source=source_static)
+ts2.circle("Date", "t2", size=3, source=source, color=None, selection_color="orange")
+
 ts2.x_range = ts1.x_range
-ts2.line('date', 't2', source=source_static)
-ts2.circle('date', 't2', size=1, source=source, color=None, selection_color="orange")
 
-# set up callbacks
+stats = PreText(text="", width=500)
 
+ticker1 = Select(value="AAPL", options=nix("GOOG", DEFAULT_TICKERS))
 def ticker1_change(attrname, old, new):
     ticker2.options = nix(new, DEFAULT_TICKERS)
     update()
+ticker1.on_change("value", ticker1_change)
 
+ticker2 = Select(value="GOOG", options=nix("AAPL", DEFAULT_TICKERS))
 def ticker2_change(attrname, old, new):
     ticker1.options = nix(new, DEFAULT_TICKERS)
     update()
+ticker2.on_change("value", ticker2_change)
+
+def update_stats(data, t1, t2):
+    stats.text = str(data[[f"Close_{t1}", f"Close_{t2}", f"Returns_{t1}", f"Returns_{t1}"]].describe())
 
 def update(selected=None):
     t1, t2 = ticker1.value, ticker2.value
+    data = get_data(t1, t2)
 
-    df = get_data(t1, t2)
-    data = df[['t1', 't2', 't1_returns', 't2_returns']]
     source.data = data
+    source.selected.indices = []
     source_static.data = data
-
-    update_stats(df, t1, t2)
-
-    corr.title.text = '%s returns vs. %s returns' % (t1, t2)
+    corr.title.text = f"{t1} returns vs. {t2} returns"
     ts1.title.text, ts2.title.text = t1, t2
 
-def update_stats(data, t1, t2):
-    stats.text = str(data[[t1, t2, t1+'_returns', t2+'_returns']].describe())
-
-ticker1.on_change('value', ticker1_change)
-ticker2.on_change('value', ticker2_change)
+    update_stats(data, t1, t2)
 
 def selection_change(attrname, old, new):
     t1, t2 = ticker1.value, ticker2.value
     data = get_data(t1, t2)
-    selected = source.selected.indices
-    if selected:
+
+    if selected := source.selected.indices:
         data = data.iloc[selected, :]
+
     update_stats(data, t1, t2)
 
-source.selected.on_change('indices', selection_change)
+source.selected.on_change("indices", selection_change)
 
-# set up layout
-widgets = column(ticker1, ticker2, stats)
-main_row = row(corr, widgets)
-series = column(ts1, ts2)
-layout = column(main_row, series)
-
-# initialize
 update()
 
-curdoc().add_root(layout)
+widgets = column(row(ticker1, ticker2), stats)
+main_row = row(corr, widgets)
+series = column(ts1, ts2)
+
+curdoc().add_root(column(main_row, series))
 curdoc().title = "Stocks"
