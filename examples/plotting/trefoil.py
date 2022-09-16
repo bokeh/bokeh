cbc93cac83f3a9b5edd6718b649a88aa95b4750d
Ayush Shah <shahayush001@gmail.com>
2022-09-16 11:31:14 +0530

    update trefoil metadata

--- examples/plotting/trefoil.py
+++ examples/plotting/trefoil.py
@@ -1,10 +1,10 @@
-''' This example plots Radiation Warning Symbol (Trefoil). It demonstrates
+''' This example plots a Radiation Warning Symbol (Trefoil). It demonstrates
 rendering annular wegdes, different arrow heads and adding arc and segment
 glyphs.
 
 .. bokeh-example-metadata::
-    :apis: bokeh.plotting.Figure.annular_wedge, bokeh.plotting.Figure.circle, bokeh.plotting.Figure.arc, bokeh.plotting.Figure.segment, bokeh.plotting.Figure.text, bokeh.io.showing.show, bokeh.models.Title.text, bokeh.models.Title.align, bokeh.core.properties.value, bokeh.models.TeeHead, bokeh.models.VeeHead
-    :refs: :ref:`userguide_annotations` > :ref:`userguide_annotations_arrows`, :ref:`userguide_annotations` > :ref:`userguide_annotations_titles`,:ref:`userguide_styling` > :ref:`userguide_styling_line_properties`, :ref:`userguide_styling` > :ref:`userguide_styling_text_properties`, :ref:`userguide_styling` > :ref:`userguide_styling_adding_mathematical_notations`, :ref:`userguide_styling` > :ref:`userguide_styling_fill_properties`
+    :apis: bokeh.plotting.Figure.annular_wedge, bokeh.plotting.Figure.arc, bokeh.plotting.Figure.segment, bokeh.models.TeeHead, bokeh.models.VeeHead
+    :refs: :ref:`userguide_annotations` > :ref:`userguide_annotations_arrows`, :ref:`userguide_styling` > :ref:`userguide_styling_adding_mathematical_notations`
     :keywords: trefoil, symbol, teehead, veehead, arrow, arrow head, segment, arc, circle, annular wedge
 
 '''
