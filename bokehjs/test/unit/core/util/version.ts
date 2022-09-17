d38daa218b8e671b10d888d023a62369f7f5e988
Mateusz Paprocki <mattpap@gmail.com>
2022-09-17 21:12:34 +0200

    Fix test description and remove unnecessary async

--- bokehjs/test/unit/core/util/version.ts
+++ bokehjs/test/unit/core/util/version.ts
@@ -1,27 +1,19 @@
 import {expect} from "assertions"
 import {pyify_version} from "@bokehjs/core/util/version"
 
-describe("core/util/throttle module", () => {
-
-  it("should implement pyify_version() function for full versions", async () => {
-
+describe("core/util/version module", () => {
+  it("should implement pyify_version() function for full versions", () => {
     const out = pyify_version("1.2.3")
-
     expect(out).to.be.equal("1.2.3")
   })
 
-  it("should implement pyify_version() function for dev versions", async () => {
-
+  it("should implement pyify_version() function for dev versions", () => {
     const out = pyify_version("1.2.3-dev.18")
-
     expect(out).to.be.equal("1.2.3.dev18")
   })
 
-  it("should implement pyify_version() function for rc versions", async () => {
-
+  it("should implement pyify_version() function for rc versions", () => {
     const out = pyify_version("1.2.3-rc.18")
-
     expect(out).to.be.equal("1.2.3.rc18")
   })
-
 })
