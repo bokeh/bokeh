2b4a96bdbc935ae386e99fe6b0aabd0c01c9f319
Mateusz Paprocki <mattpap@gmail.com>
2022-09-23 22:19:08 +0200

    Allow to collect overlays from tool views

--- bokehjs/src/lib/models/plots/plot_canvas.ts
+++ bokehjs/src/lib/models/plots/plot_canvas.ts
@@ -270,8 +270,8 @@ export class PlotView extends LayoutDOMView implements Renderable {
     this.canvas_view = await build_view(canvas, {parent: this})
     this.canvas_view.plot_views = [this]
 
-    await this.build_renderer_views()
     await this.build_tool_views()
+    await this.build_renderer_views()
 
     this._range_manager.update_dataranges()
   }
@@ -665,8 +665,8 @@ export class PlotView extends LayoutDOMView implements Renderable {
     })
 
     this.connect(this.model.toolbar.properties.tools.change, async () => {
-      await this.build_renderer_views()
       await this.build_tool_views()
+      await this.build_renderer_views()
     })
 
     const {x_ranges, y_ranges} = this.frame
