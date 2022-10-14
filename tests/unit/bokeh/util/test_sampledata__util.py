463614b65db5e04ee7e84c6ac740c32b178550be
Bryan Van de Ven <bryan@bokeh.org>
2022-10-13 20:56:49 -0700

    mark sampledata tests

--- tests/unit/bokeh/util/test_sampledata__util.py
+++ tests/unit/bokeh/util/test_sampledata__util.py
@@ -30,24 +30,29 @@ import bokeh.util.sampledata as bus # isort:skip
 # General API
 #-----------------------------------------------------------------------------
 
+@pytest.mark.sampledata
 def test_external_path_bad() -> None:
     pat = "Could not locate external data file (.*)junkjunk. Please execute bokeh.sampledata.download()"
     with pytest.raises(RuntimeError, match=pat):
         bus.external_path("junkjunk")
 
+@pytest.mark.sampledata
 def test_package_dir() -> None:
     path = bus.package_dir()
     assert path.exists()
     assert str(path).endswith("sampledata/_data")
 
+@pytest.mark.sampledata
 def test_package_csv() -> None:
     with patch('pandas.read_csv') as mock_read_csv:
         bus.package_csv("module", "foo", bar=10)
     assert mock_read_csv.has_call(call(bus.package_path("foo"), bar=10))
 
+@pytest.mark.sampledata
 def test_package_path() -> None:
     assert bus.package_path("foo") == bus.package_dir() /"foo"
 
+@pytest.mark.sampledata
 def test_open_csv() -> None:
     with patch('builtins.open') as mock_open:
         bus.open_csv("foo")
