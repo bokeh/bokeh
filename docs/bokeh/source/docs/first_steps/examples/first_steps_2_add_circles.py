9b14e313b2f0a092dd3f19af31c90dd6ca817532
Victoria Adesoba <victoria@makepath.com>
2023-01-03 14:08:45 -0600

    Update first_steps_2_add_circles.py (#12703)
    * Update first_steps_2_add_circles.py
    
    * Update first_steps_2_add_circles.py

--- docs/bokeh/source/docs/first_steps/examples/first_steps_2_add_circles.py
+++ docs/bokeh/source/docs/first_steps/examples/first_steps_2_add_circles.py
@@ -10,9 +10,9 @@ y3 = [4, 5, 5, 7, 2]
 p = figure(title="Multiple glyphs example", x_axis_label="x", y_axis_label="y")
 
 # add multiple renderers
-p.line(x, y1, legend_label="Temp.", color="blue", line_width=2)
-p.line(x, y2, legend_label="Rate", color="red", line_width=2)
-p.circle(x, y3, legend_label="Objects", color="yellow", size=12)
+p.line(x, y1, legend_label="Temp.", color="#004488", line_width=3)
+p.line(x, y2, legend_label="Rate", color="#906c18", line_width=3)
+p.circle(x, y3, legend_label="Objects", color="#bb5566", size=16)
 
 # show the results
 show(p)
