bde5741ff52451bc6d06d629790a1e1811f1ee5d
Bryan Van de Ven <bryan@bokeh.org>
2023-01-27 18:35:59 -0800

    update examples

--- examples/topics/geo/eclipse.py
+++ examples/topics/geo/eclipse.py
@@ -16,28 +16,24 @@ from os.path import dirname, join, realpath
 import pandas as pd
 import shapefile as shp
 
-from bokeh.models import ColorBar, ColumnDataSource, Label, LinearColorMapper
-from bokeh.palettes import TolYlOrBr5
+from bokeh.models import ColumnDataSource, Label
 from bokeh.plotting import figure, show
 from bokeh.sampledata.us_states import data
+from bokeh.transform import linear_cmap
 
 ROOT = dirname(realpath(__file__))
 
-states = pd.DataFrame.from_dict(data.copy(), orient="index")
+states = pd.DataFrame.from_dict(data, orient="index")
 states.drop(["AK", "HI"], inplace=True)
 
 trends = pd.read_csv(join(ROOT, "eclipse_data/trends.csv"))
 
-states.set_index("name", inplace=True)
-trends.set_index("Region", inplace=True)
+totality_path = shp.Reader(join(ROOT, "eclipse_data/upath17")).shapes()[0]
 
-states["trend"] = trends["solar eclipse"]
-
-upath17 = shp.Reader(join(ROOT, "eclipse_data/upath17"))
-(totality_path,) = upath17.shapes()
-
-p = figure(width=1000, height=600, background_fill_color="#333344",
-           tools="", toolbar_location=None, x_axis_location=None, y_axis_location=None)
+p = figure(
+    width=1000, height=600, background_fill_color="#333344",
+    tools="", toolbar_location=None, x_axis_location=None, y_axis_location=None
+)
 
 p.grid.grid_line_color = None
 
@@ -46,48 +42,44 @@ p.title.align = "center"
 p.title.text_font_size = "21px"
 p.title.text_color = "#333344"
 
-mapper = LinearColorMapper(palette=list(TolYlOrBr5), low=0, high=100)
-
 source = ColumnDataSource(data=dict(
-    state_xs=list(states.lons),
-    state_ys=list(states.lats),
-    trend=states.trend,
+    state_xs=states.lons,
+    state_ys=states.lats,
+    trend=trends["solar eclipse"],
 ))
-us = p.patches("state_xs", "state_ys",
-    fill_color=dict(field="trend", transform=mapper),
-    source=source,
-    line_color="#333344", line_width=1)
 
-p.x_range.renderers = [us]
-p.y_range.renderers = [us]
+us = p.patches(
+    "state_xs", "state_ys", source=source,
+    fill_color=linear_cmap("trend", palette="TolYlOrBr5", low=0, high=100),
+    line_color="#333344", line_width=1
+)
+
+p.x_range.renderers = p.y_range.renderers = [us]
 
 totality_x, totality_y = zip(*totality_path.points)
-p.patch(totality_x, totality_y,
-    fill_color="black", fill_alpha=0.7,
-    line_color=None)
+p.patch(
+    totality_x, totality_y,
+    fill_color="black", fill_alpha=0.7, line_color=None
+)
 
-path = Label(
-    x=-76.3, y=31.4,
-    angle=-36.5, angle_units="deg",
+p.add_layout(Label(
     text="Solar eclipse path of totality",
-    text_baseline="middle", text_font_size="11px", text_color="silver")
-p.add_layout(path)
+    x=-76.3, y=31.4, angle=-36.5, angle_units="deg",
+    text_baseline="middle", text_font_size="11px", text_color="silver"
+))
 
-color_bar = ColorBar(
-    color_mapper=mapper,
+p.add_layout(us.construct_color_bar(
+    title='Popularity of "solar eclipse" search term',
     location="bottom_left", orientation="horizontal",
-    title="Popularity of \"solar eclipse\" search term",
     title_text_font_size="16px", title_text_font_style="bold",
     title_text_color="lightgrey", major_label_text_color="lightgrey",
-    background_fill_alpha=0.0)
-p.add_layout(color_bar)
+    background_fill_alpha=0.0
+))
 
-notes = Label(
-    x=0, y=0, x_units="screen", y_units="screen",
-    x_offset=40, y_offset=20,
+p.add_layout(Label(
     text="Source: Google Trends, NASA Scientific Visualization Studio",
-    level="overlay",
-    text_font_size="11px", text_color="gray")
-p.add_layout(notes)
+    x=0, y=0, x_units="screen", y_units="screen", x_offset=40, y_offset=20,
+    text_font_size="11px", text_color="gray", level="overlay",
+))
 
 show(p)
