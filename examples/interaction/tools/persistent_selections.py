d1481c11a99a85775760b6336a1341e77977cdf5
Mateusz Paprocki <mattpap@gmail.com>
2023-01-31 12:13:31 +0100

    Refactor persistent_selections example

--- examples/interaction/tools/persistent_selections.py
+++ examples/interaction/tools/persistent_selections.py
@@ -7,23 +7,21 @@ N = 2000
 x = np.random.random(size=N) * 100
 y = np.random.random(size=N) * 100
 radii = np.random.random(size=N) * 1.5
-colors = np.array([ [r, g, 150] for r, g in zip(50 + 2*x, 30 + 2*y) ], dtype="uint8")
+colors = np.array([[r, g, 150] for r, g in zip(50 + 2*x, 30 + 2*y)], dtype="uint8")
 
-TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,save,box_select,poly_select,lasso_select,help"
+p = figure()
 
-p = figure(tools=TOOLS)
+box_select = BoxSelectTool(persistent=True)
+poly_select = PolySelectTool(persistent=True)
+lasso_select = LassoSelectTool(persistent=True)
 
-p.scatter(x, y, radius=radii,
-          fill_color=colors, fill_alpha=0.6,
-          line_color=None)
+p.add_tools(box_select, poly_select, lasso_select)
+p.add_tools("crosshair", "hover", "zoom_in", "zoom_out", "undo", "redo")
 
-box_select = p.select_one(BoxSelectTool)
-box_select.persistent = True
-
-poly_select = p.select_one(PolySelectTool)
-poly_select.persistent = True
-
-lasso_select = p.select_one(LassoSelectTool)
-lasso_select.persistent = True
+p.scatter(
+    x, y, radius=radii,
+    fill_color=colors, fill_alpha=0.6,
+    line_color=None,
+)
 
 show(p)
