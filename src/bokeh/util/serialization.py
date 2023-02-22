5eb0a3e7f4f393f4af3f2f42d9d98634115180cf
Ben Russell <bprussell80@protonmail.com>
2023-02-22 10:31:29 -0800

    12821 serialize pandas typed arrays (#12826)
    * Add check for serializing PandasArrays
    
    * Remove trailing whitespace
    
    * Add pandas array to type annotation

--- src/bokeh/util/serialization.py
+++ src/bokeh/util/serialization.py
@@ -45,6 +45,7 @@ from .strings import format_docstring
 if TYPE_CHECKING:
     import numpy.typing as npt
     import pandas as pd
+    from pandas.core.arrays import PandasArray
     from typing_extensions import TypeGuard
 
 #-----------------------------------------------------------------------------
@@ -338,7 +339,7 @@ def transform_array(array: npt.NDArray[Any]) -> npt.NDArray[Any]:
 
     return array
 
-def transform_series(series: pd.Series[Any] | pd.Index) -> npt.NDArray[Any]:
+def transform_series(series: pd.Series[Any] | pd.Index | PandasArray) -> npt.NDArray[Any]:
     ''' Transforms a Pandas series into serialized form
 
     Args:
@@ -349,11 +350,14 @@ def transform_series(series: pd.Series[Any] | pd.Index) -> npt.NDArray[Any]:
 
     '''
     import pandas as pd
+    from pandas.core.arrays import PandasArray
 
     # not checking for pd here, this function should only be called if it
     # is already known that series is a Pandas Series type
     if isinstance(series, pd.PeriodIndex):
         vals = series.to_timestamp().values  # type: ignore
+    elif isinstance(series, PandasArray):
+        vals = series.to_numpy()
     else:
         vals = series.values
     return vals
