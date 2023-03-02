5885e84c032939ed2fb96fc84cabfd91cdbeeb83
Mateusz Paprocki <mattpap@gmail.com>
2023-03-02 21:24:10 +0100

    Update `plotting/css_classes.py` to use stylesheets

--- examples/plotting/css_classes.py
+++ examples/plotting/css_classes.py
@@ -1,54 +1,52 @@
 from bokeh.io import show
 from bokeh.layouts import column
-from bokeh.models import Div, Paragraph
+from bokeh.models import Div, InlineStyleSheet
 
-template = """
-{% block postamble %}
-<style>
-.bk.custom {
+stylesheet = InlineStyleSheet(css="""
+:host(.custom) {
     border-radius: 0.5em;
     padding: 1em;
 }
-.bk.custom-1 {
+:host(.custom-1) {
     border: 3px solid #2397D8;
 }
-.bk.custom-2 {
+:host(.custom-2) {
     border: 3px solid #14999A;
     background-color: whitesmoke;
 }
-</style>
-{% endblock %}
-"""
+""")
 
-p = Paragraph(text="The divs below were configured with additional css_classes:")
+p = Div(text="The divs below were configured with additional <code>css_classes</code>:")
 
 div1 = Div(text="""
-<p> This Bokeh Div adds the style classes:<p>
+<p>This Bokeh Div adds the style classes:<p>
 <pre>
-.bk.custom {
+:host(.custom) {
     border-radius: 0.5em;
     padding: 1em;
 }
-.bk.custom-1 {
+:host(.custom-1) {
     border: 3px solid #2397D8;
 }
 </pre>
 """)
 div1.css_classes = ["custom", "custom-1"]
+div1.stylesheets = [stylesheet]
 
 div2 = Div(text="""
-<p> This Bokeh Div adds the style classes:<p>
+<p>This Bokeh Div adds the style classes:<p>
 <pre>
-.bk.custom {
+:host(.custom) {
     border-radius: 0.5em;
     padding: 1em;
 }
-.bk.custom-2 {
+:host(.custom-2) {
     border: 3px solid #14999A;
     background-color: whitesmoke;
 }
 </pre>
 """)
 div2.css_classes = ["custom", "custom-2"]
+div2.stylesheets = [stylesheet]
 
-show(column(p, div1, div2), template=template)
+show(column(p, div1, div2))
