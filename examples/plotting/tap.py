c90311359b0690c0e95820751532610602fde145
Bhaswati Roy <roybhaswati24@gmail.com>
2023-03-07 19:19:29 +0530

    Added metadata to tap.py for Tap Plots

--- examples/plotting/tap.py
+++ examples/plotting/tap.py
@@ -1,3 +1,14 @@
+''' A interactive numerical tap plot based on simple Python array of data.
+    One particiular datapoint can be focused by tapping on it. 
+    This highlights that datapoint, and displays other datapoints in faded color.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.figure.circle, bokeh.plotting.figure.segment
+    :refs: :ref:`ug_basic_tap`
+    :keywords: circle, tapplot, text, select_one, renderers
+'''
+
+
 import numpy as np
 
 from bokeh.models import TapTool
