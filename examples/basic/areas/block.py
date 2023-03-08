495ce7f68e98e0c338d3c5cd66e964ee0c2b3ba5
Medhavi Singh <78318301+Med16-11@users.noreply.github.com>
2023-03-09 01:59:22 +0530

    Update block.py

--- examples/basic/areas/block.py
+++ examples/basic/areas/block.py
@@ -1,3 +1,13 @@
+"""
+Creates a block plot using Bokeh and displays it using the show() function.
+
+.. bokeh-example-metadata::
+    :sampledata: None
+    :apis: bokeh.plotting.figure, bokeh.plotting.show, bokeh.models.Block
+    :refs: :ref:`uuserguide_plotting_visualization_glyphs`
+    :keywords: block, plot
+"""
+
 from bokeh.plotting import figure, show
 
 p = figure(width=400, height=400)
