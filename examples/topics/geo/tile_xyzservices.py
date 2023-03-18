334c9f6e01fecc2c52ff120f74cfe83474142362
Ajoke Yusuf <ajokeyusuf10@gmail.com>
2023-03-18 17:53:58 +0100

    git commit -m "Update metadata :memo:"
    There was no metadata for "tile_xyzservices.py". I added a metadata to give users more information about tile_xyzservices

--- examples/topics/geo/tile_xyzservices.py
+++ examples/topics/geo/tile_xyzservices.py
@@ -1,3 +1,17 @@
+''' A geogra plot that demonstrates how to use XYZ tile services to create an interactive map. The 'tile_xyz' service provides map tiles, 
+which are typically used to display maps in web applications.The data used to create the plot is sourced from public XYZ tile services.
+This example demonstrates how to create a plot in Bokeh with a map background using tile_providers.
+The chart shows a map of a specific geographical region, which is defined by the range bounds supplied in web mercator coordinates.
+
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.Figure, bokeh.tile_providers.get_provider, bokeh.tile_provider.Vendors
+    :more info: [Geographical Plot](https://docs.bokeh.org/en/latest/docs/user_guide/topics/geo.html)
+    :keywords: Bokeh, tile services, XYZ, mapping, interactive, geography
+
+
+'''
+
 import xyzservices.providers as xyz
 
 from bokeh.plotting import figure, show
