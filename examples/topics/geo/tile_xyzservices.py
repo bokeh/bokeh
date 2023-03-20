108f7cb7dd07041284541a7e5c4e49acf4451649
Ajoke Yusuf <ajokeyusuf10@gmail.com>
2023-03-20 06:47:55 +0100

    Update tile_xyzservices.py metadata

--- examples/topics/geo/tile_xyzservices.py
+++ examples/topics/geo/tile_xyzservices.py
@@ -1,8 +1,8 @@
 ''' A geographical plot that demonstrates how to use XYZ tile services to create an interactive map.
-The xyzservices provides map tiles,which are typically used to display maps visualizations 
-and geographical location in web applications.The data used to create the plot is 
-sourced from public XYZ tile services.This example demonstrates how to create a plot in Bokeh
-with a map background using tile_providers.The chart shows a map of a specific geographical region, 
+The xyzservices library provides map tiles, which are typically used to display maps visualizations 
+and geographical location in web applications. The data used to create the plot is 
+sourced from public XYZ tile services. This example demonstrates how to create a plot in Bokeh
+with a map background using tile_providers. The chart shows a map of a specific geographical region, 
 which is defined by the range bounds supplied in web mercator coordinates.
 
 .. bokeh-example-metadata::
@@ -12,17 +12,13 @@ which is defined by the range bounds supplied in web mercator coordinates.
 
 
 '''
-
 import xyzservices.providers as xyz
+
 from bokeh.plotting import figure, show
-from bokeh.tile_providers import get_provider, Vendors
 
 # range bounds supplied in web mercator coordinates
 p = figure(x_range=(-2000000, 6000000), y_range=(-1000000, 7000000),
            x_axis_type="mercator", y_axis_type="mercator")
-
-# Retrieve the OpenStreetMap.Mapnik tile source from the tile providers
-tile_provider = get_provider(Vendors.CARTODBPOSITRON)
-p.add_tile(tile_provider)
+p.add_tile(xyz.OpenStreetMap.Mapnik)
 
 show(p)
