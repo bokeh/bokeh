992c0c04e74f0d044eee57a58e3851497604c96d
Azaya89 <akoritaisaiah@gmail.com>
2023-03-21 00:01:14 +0100

    Added metadata to line_on_off.py

--- examples/plotting/line_on_off.py
+++ examples/plotting/line_on_off.py
@@ -1,4 +1,11 @@
-""" Example demonstrating turning lines on and off - with JS only
+""" Example demonstrating turning lines on and off - with JS only.
+It involves checking and unchecking the checkboxes representing
+the plotted lines to turn the lines on/off.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.figure.line, bokeh.layouts.row
+    :refs: :ref:`ug_basic_lines_line_multiple`
+    :keywords: line_on_off, checkbox, CustomJS
 
 """
 
@@ -10,7 +17,7 @@ from bokeh.palettes import Viridis3
 from bokeh.plotting import figure, show
 
 p = figure()
-props = dict(line_width=4, line_alpha=0.7)
+props = {"line_width":4, "line_alpha":0.7}
 x = np.linspace(0, 4 * np.pi, 100)
 l0 = p.line(x, np.sin(x), color=Viridis3[0], legend_label="Line 0", **props)
 l1 = p.line(x, 4 * np.cos(x), color=Viridis3[1], legend_label="Line 1", **props)
@@ -19,7 +26,7 @@ l2 = p.line(x, np.tan(x), color=Viridis3[2], legend_label="Line 2", **props)
 checkbox = CheckboxGroup(labels=["Line 0", "Line 1", "Line 2"],
                          active=[0, 1, 2], width=100)
 
-callback = CustomJS(args=dict(l0=l0, l1=l1, l2=l2, checkbox=checkbox), code="""
+callback = CustomJS(args={"l0":l0, "l1":l1, "l2":l2,"checkbox":checkbox}, code="""
 l0.visible = checkbox.active.includes(0)
 l1.visible = checkbox.active.includes(1)
 l2.visible = checkbox.active.includes(2)
