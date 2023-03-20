76739b655d91f89a141aa45152cdefba3d92066d
Arpita kesharwani <107834813+KesharwaniArpita@users.noreply.github.com>
2023-03-20 09:19:47 +0530

    Update examples/basic/lines/arcs.py
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

--- examples/basic/lines/arcs.py
+++ examples/basic/lines/arcs.py
@@ -3,8 +3,6 @@ demonstrates the use of the ``arc`` method to make a graph
 by drawing three arcs of defined radius, defined start and 
 end angles at defined point(coordinates) .
 
-
-
 .. bokeh-example-metadata::
     :apis: bokeh.plotting.figure.arc
     :refs: :ref:`ug_basic_lines_arcs`
