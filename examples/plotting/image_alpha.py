1346cb8ef14c26e4de1a1c316520f33c66b45954
Azaya89 <akoritaisaiah@gmail.com>
2023-03-21 12:36:15 +0100

    added metadata to image_alpha.py

--- examples/plotting/image_alpha.py
+++ examples/plotting/image_alpha.py
@@ -1,3 +1,13 @@
+''' An example of plots showing how to add alpha value (transparency) 
+to images for three different image methods.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.figure, bokeh.plotting.figure.image, bokeh.layouts.Column
+    :refs: :ref:`ug_topics_images`
+    :keywords: image, alpha, line
+
+'''
+
 import numpy as np
 
 from bokeh.layouts import Column
