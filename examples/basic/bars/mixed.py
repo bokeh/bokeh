39aa73654844cf1ce2dc5b97fe536ab5615a55a0
Bhaswati Roy <roybhaswati24@gmail.com>
2023-03-26 11:20:19 +0530

    Updated color palette to TolYlOrBr3

--- examples/basic/bars/mixed.py
+++ examples/basic/bars/mixed.py
@@ -10,6 +10,7 @@ factors.
 '''
 from bokeh.models import FactorRange
 from bokeh.plotting import figure, show
+from bokeh.palettes import TolYlOrBr3
 
 factors = [
     ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
@@ -18,13 +19,15 @@ factors = [
     ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
 ]
 
-p = figure(x_range=FactorRange(*factors), height=350,
+p = figure(x_range=FactorRange(*factors), height=500,
            toolbar_location=None, tools="")
 
-x = [ 10, 12, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16 ]
-p.vbar(x=factors, top=x, width=0.9, alpha=0.5)
+orange, brown = TolYlOrBr3[1], TolYlOrBr3[2]
 
-p.line(x=["Q1", "Q2", "Q3", "Q4"], y=[12, 9, 13, 14], color="red", line_width=2)
+x = [ 10, 12, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16 ]
+p.vbar(x=factors, top=x, width=0.9, color=orange, alpha=0.7)
+
+p.line(x=["Q1", "Q2", "Q3", "Q4"], y=[12, 9, 13, 14], color=brown, line_width=2)
 
 p.y_range.start = 0
 p.x_range.range_padding = 0.1
