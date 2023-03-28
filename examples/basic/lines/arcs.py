1f9cd04ea9f741369f36c6cafa20f8e1aafc6efc
KesharwaniArpita <arpitakshrwn@gmail.com>
2023-03-28 14:19:26 +0530

    required changes made

--- examples/basic/lines/arcs.py
+++ examples/basic/lines/arcs.py
@@ -1,3 +1,14 @@
+''' A ac graph using pre-defined data points. This example
+demonstrates the use of the ``arc`` method to make a graph
+by drawing three arcs of defined radius, defined start and
+end angles at defined point(coordinates).
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.figure.arc
+    :refs: :ref:`ug_basic_lines_arcs
+    :keywords: arc, figure, glyph
+
+'''
 from bokeh.plotting import figure, show
 
 p = figure(width=400, height=400)
