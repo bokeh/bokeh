3afd865017a4b3b706428e75af3a2f0e61b77e7e
Bhaswati Roy <roybhaswati24@gmail.com>
2023-04-21 10:38:24 +0530

    Fixed errors

--- examples/basic/bars/mixed.py
+++ examples/basic/bars/mixed.py
@@ -9,22 +9,26 @@ factors.
 
 '''
 from bokeh.models import FactorRange
+from bokeh.palettes import TolPRGn4
 from bokeh.plotting import figure, show
-from bokeh.palettes import TolYlOrBr4
 
-factors = [
+quarters =("Q1", "Q2", "Q3", "Q4")
+
+months = (
     ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
     ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
     ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
     ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
-]
+)
+
+fill_color, line_color = TolPRGn4[2:]
 
 p = figure(x_range=FactorRange(*months), height=500, tools="",
            background_fill_color="#fafafa", toolbar_location=None)
 
 monthly = [10, 13, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16]
 p.vbar(x=months, top=monthly, width=0.8,
-       fill_color=fill_color, fill_alpha=0.8, line_color=line_color)
+       fill_color=fill_color, fill_alpha=0.8, line_color=line_color, line_width=1.2)
 
 quarterly = [13, 9, 13, 14]
 p.line(x=quarters, y=quarterly, color=line_color, line_width=3)
