6a971ea624f918ab24df3be36a01ee8deb99f625
Bryan Van de Ven <bryan@bokeh.org>
2023-05-10 10:36:24 -0700

    Remove GA script checks

--- scripts/ci/build_docs.sh
+++ scripts/ci/build_docs.sh
@@ -8,11 +8,4 @@ make SPHINXOPTS=-v all
 
 { set +x ;} 2> /dev/null #echo off
 
-if grep -e 'src="https://www.google-analytics.com' build/html/index.html > /dev/null; then
-    echo "Expected Google Analytics script source found in built documentation"
-else
-    echo "Expected Google Analytics script source not found in built documentation"
-    exit 1
-fi
-
 tar cvzf docs-html.tgz build/html
