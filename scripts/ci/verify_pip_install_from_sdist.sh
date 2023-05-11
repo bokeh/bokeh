9a87afd9256c14e6aa5d29d2d4471d28222016a6
Bryan Van de Ven <bryan@bokeh.org>
2023-05-11 08:53:43 -0700

    Don't require tags for sdist install (#13123)

--- scripts/ci/verify_pip_install_from_sdist.sh
+++ scripts/ci/verify_pip_install_from_sdist.sh
@@ -5,10 +5,12 @@ set -e #exit on error
 
 export VERSION="$(echo $(basename "$(ls dist/*.tar.gz)" .tar.gz) | cut -d- -f2)"
 
-pushd dist
+cp "dist/bokeh-$VERSION.tar.gz" /tmp
+pushd /tmp
 tar xvzf "bokeh-$VERSION.tar.gz"
 cd "bokeh-$VERSION"
 pip install .
 popd
+
 bokeh info
 python -m bokeh.util.package $VERSION bokehjs/build
--- scripts/ci/verify_pip_install_using_sdist.sh
+++ scripts/ci/verify_pip_install_using_sdist.sh
@@ -5,8 +5,10 @@ set -e #exit on error
 
 export VERSION="$(echo $(basename "$(ls dist/*.tar.gz)" .tar.gz) | cut -d- -f2)"
 
-pushd dist
+cp "dist/bokeh-$VERSION.tar.gz" /tmp
+pushd /tmp
 pip install "bokeh-$VERSION.tar.gz"
 popd
+
 bokeh info
 python -m bokeh.util.package $VERSION bokehjs/build
