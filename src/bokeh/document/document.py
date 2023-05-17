627023e6aa715335880f3ada8416366b410e8cc1
Mateusz Paprocki <mattpap@gmail.com>
2023-05-17 16:13:52 +0200

    Make DocJson.{defs,callbacks} actually optional

--- src/bokeh/document/document.py
+++ src/bokeh/document/document.py
@@ -738,10 +738,13 @@ class Document:
         doc_json = DocJson(
             version=__version__,
             title=self.title,
-            defs=defs,
             roots=roots,
-            callbacks=callbacks,
         )
+
+        if data_models:
+            doc_json["defs"] = defs
+        if self.callbacks._js_event_callbacks:
+            doc_json["callbacks"] = callbacks
 
         self.models.flush_synced()
         return doc_json
