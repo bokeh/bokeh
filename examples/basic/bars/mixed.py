e1d781e3a5921b481e86eea9d51943b4e7588be9
Bhaswati Roy <roybhaswati24@gmail.com>
2023-05-31 10:38:13 +0530

    Updated colors of Mixed Plots (#13031)
    * Updated color palette to TolYlOrBr3
    
    * Updated colors
    
    * Updated borders, markers, colors
    
    * Fixed errors
    
    * Delete mixed.png
    
    * Delete mixed@2x.png
    
    * Add files via upload

--- examples/basic/bars/mixed.py
+++ examples/basic/bars/mixed.py
@@ -9,22 +9,31 @@ factors.
 
 '''
 from bokeh.models import FactorRange
+from bokeh.palettes import TolPRGn4
 from bokeh.plotting import figure, show
 
-factors = [
+quarters =("Q1", "Q2", "Q3", "Q4")
+
+months = (
     ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
     ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
     ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
     ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
-]
+)
 
-p = figure(x_range=FactorRange(*factors), height=350,
-           toolbar_location=None, tools="")
+fill_color, line_color = TolPRGn4[2:]
 
-x = [ 10, 12, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16 ]
-p.vbar(x=factors, top=x, width=0.9, alpha=0.5)
+p = figure(x_range=FactorRange(*months), height=500, tools="",
+           background_fill_color="#fafafa", toolbar_location=None)
 
-p.line(x=["Q1", "Q2", "Q3", "Q4"], y=[12, 9, 13, 14], color="red", line_width=2)
+monthly = [10, 13, 16, 9, 10, 8, 12, 13, 14, 14, 12, 16]
+p.vbar(x=months, top=monthly, width=0.8,
+       fill_color=fill_color, fill_alpha=0.8, line_color=line_color, line_width=1.2)
+
+quarterly = [13, 9, 13, 14]
+p.line(x=quarters, y=quarterly, color=line_color, line_width=3)
+p.circle(x=quarters, y=quarterly, size=10,
+         line_color=line_color, fill_color="white", line_width=3)
 
 p.y_range.start = 0
 p.x_range.range_padding = 0.1
