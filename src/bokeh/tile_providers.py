acc90f4124de51584c1350cd528b0999a500ea96
Moritz Schreiber <moritz.schreiber@ifesca.de>
2023-07-28 08:43:53 +0200

    Revert "use autofunction to render docstring from function with correct format"
    This reverts commit e823412511a2567b005fa9e3d08379960f86dce5.

--- src/bokeh/tile_providers.py
+++ src/bokeh/tile_providers.py
@@ -4,6 +4,139 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+''' Pre-configured tile sources for common third party tile services.
+
+get_provider
+    Use this function to retrieve an instance of a predefined tile provider.
+
+    .. warning::
+        get_provider is deprecated as of Bokeh 3.0.0 and will be removed in a future
+        release. Use ``add_tile`` directly instead.
+
+    Args:
+        provider_name (Union[str, Vendors, xyzservices.TileProvider])
+            Name of the tile provider to supply.
+
+            Use a ``tile_providers.Vendors`` enumeration value, or the string
+            name of one of the known providers. Use
+            :class:`xyzservices.TileProvider` to pass custom tile providers.
+
+    Returns:
+        WMTSTileProviderSource: The desired tile provider instance
+
+    Raises:
+        ValueError, if the specified provider can not be found
+
+    **Example:**
+
+    .. code-block:: python
+
+        >>> from bokeh.tile_providers import get_provider, Vendors
+        >>> get_provider(Vendors.CARTODBPOSITRON)
+        <class 'bokeh.models.tiles.WMTSTileSource'>
+        >>> get_provider('CARTODBPOSITRON')
+        <class 'bokeh.models.tiles.WMTSTileSource'>
+
+        >>> import xyzservices.providers as xyz
+        >>> get_provider(xyz.CartoDB.Positron)
+        <class 'bokeh.models.tiles.WMTSTileSource'>
+
+The available built-in tile providers are listed in the ``Vendors`` enum:
+
+.. bokeh-enum:: Vendors
+    :module: bokeh.tile_providers
+    :noindex:
+
+.. warning::
+    The built-in Vendors are deprecated as of Bokeh 3.0.0 and will be removed in a future
+    release. You can pass the same strings to ``add_tile`` directly.
+
+Any of these values may be be passed to the ``get_provider`` function in order
+to obtain a tile provider to use with a Bokeh plot. Representative samples of
+each tile provider are shown below.
+
+CARTODBPOSITRON
+---------------
+
+Tile Source for CartoDB Tile Service
+
+.. raw:: html
+
+    <img src="https://tiles.basemaps.cartocdn.com/light_all/14/2627/6331.png" />
+
+CARTODBPOSITRON_RETINA
+----------------------
+
+Tile Source for CartoDB Tile Service (tiles at 'retina' resolution)
+
+.. raw:: html
+
+    <img src="https://tiles.basemaps.cartocdn.com/light_all/14/2627/6331@2x.png" />
+
+ESRI_IMAGERY
+------------
+
+Tile Source for ESRI public tiles.
+
+.. raw:: html
+
+    <img src="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/14/6331/2627.jpg" />
+
+OSM
+---
+
+Tile Source for Open Street Maps.
+
+.. raw:: html
+
+    <img src="https://c.tile.openstreetmap.org/14/2627/6331.png" />
+
+STAMEN_TERRAIN
+--------------
+
+Tile Source for Stamen Terrain Service
+
+.. raw:: html
+
+    <img src="https://stamen-tiles.a.ssl.fastly.net/terrain/14/2627/6331.png" />
+
+STAMEN_TERRAIN_RETINA
+---------------------
+
+Tile Source for Stamen Terrain Service (tiles at 'retina' resolution)
+
+.. raw:: html
+
+    <img src="https://stamen-tiles.a.ssl.fastly.net/terrain/14/2627/6331@2x.png" />
+
+STAMEN_TONER
+------------
+
+Tile Source for Stamen Toner Service
+
+.. raw:: html
+
+    <img src="https://stamen-tiles.a.ssl.fastly.net/toner/14/2627/6331.png" />
+
+STAMEN_TONER_BACKGROUND
+-----------------------
+
+Tile Source for Stamen Toner Background Service which does not include labels
+
+.. raw:: html
+
+    <img src="https://stamen-tiles.a.ssl.fastly.net/toner-background/14/2627/6331.png" />
+
+STAMEN_TONER_LABELS
+-------------------
+
+Tile Source for Stamen Toner Service which includes only labels
+
+.. raw:: html
+
+    <img src="https://stamen-tiles.a.ssl.fastly.net/toner-labels/14/2627/6331.png" />
+
+'''
 
 #-----------------------------------------------------------------------------
 # Boilerplate
@@ -62,40 +195,6 @@ class _TileProvidersModule(types.ModuleType):
     Vendors = deprecated_vendors()
 
     def get_provider(self, provider_name):
-        """Use this function to retrieve an instance of a predefined tile provider.
-
-        .. warning::
-            get_provider is deprecated as of Bokeh 3.0.0 and will be removed in a future
-            release. Use ``add_tile`` directly instead.
-
-        Args:
-            provider_name (Union[str, Vendors, xyzservices.TileProvider])
-                Name of the tile provider to supply.
-
-                Use a ``tile_providers.Vendors`` enumeration value, or the string
-                name of one of the known providers. Use
-                :class:`xyzservices.TileProvider` to pass custom tile providers.
-
-        Returns:
-            WMTSTileProviderSource: The desired tile provider instance
-
-        Raises:
-            ValueError, if the specified provider can not be found
-
-        Example:
-
-            .. code-block:: python
-
-                >>> from bokeh.tile_providers import get_provider, Vendors
-                >>> get_provider(Vendors.CARTODBPOSITRON)
-                <class 'bokeh.models.tiles.WMTSTileSource'>
-                >>> get_provider('CARTODBPOSITRON')
-                <class 'bokeh.models.tiles.WMTSTileSource'>
-
-                >>> import xyzservices.providers as xyz
-                >>> get_provider(xyz.CartoDB.Positron)
-                <class 'bokeh.models.tiles.WMTSTileSource'>
-        """
         deprecated((3, 0, 0), "get_provider", "add_tile directly")
         from bokeh.models import WMTSTileSource
 
