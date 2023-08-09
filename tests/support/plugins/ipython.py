1171ea5584082bef7152be765ff80ec6ef0f0d02
Azaya <99359668+Azaya89@users.noreply.github.com>
2023-08-09 17:10:24 +0100

    modified files in the plugins directory. (#13313)
    * modified files in the plugins directory.
    
    * revert changes in bokehjs/package-lock.json

--- tests/support/plugins/ipython.py
+++ tests/support/plugins/ipython.py
@@ -4,7 +4,7 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 #-----------------------------------------------------------------------------
-''' Provide a Pytest plugin for handling tests when Pandas may be missing.
+''' Provide a Pytest plugin for handling tests when IPython may be missing.
 
 '''
 
