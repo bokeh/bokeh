b93b1f75387d41fb39e19e9f77849caa010c7218
Mateusz Paprocki <mattpap@gmail.com>
2023-08-23 17:08:52 +0200

    Upgrade to ruff 0.0.285 (#13342)
    * Upgrade to ruff 0.0.285
    
    * Disable RUF012 (too verbose right now)
    
    * Fix RUF015 (use next(iter(...)) instead of list(...)[0])
    
    * Fix F841 (for a TypeVar)
    
    * Fix E721 (use isinstance() instead of type())
    
    * Add a return type

--- src/bokeh/layouts.py
+++ src/bokeh/layouts.py
@@ -293,7 +293,7 @@ def gridplot(
             else:
                 raise ValueError("Only UIElement and LayoutDOM items can be inserted into a grid")
 
-    def merge(cls: type[Tool], group: list[Tool]):
+    def merge(cls: type[Tool], group: list[Tool]) -> Tool | ToolProxy | None:
         if issubclass(cls, (SaveTool, CopyTool, ExamineTool, FullscreenTool)):
             return cls()
         else:
@@ -314,8 +314,8 @@ def gridplot(
     active_taps = [ toolbar.active_tap for toolbar in toolbars ]
     active_multis = [ toolbar.active_multi for toolbar in toolbars ]
 
-    T = TypeVar("T")
-    def assert_unique(values: list[T], name: str) -> T | UndefinedType:
+    V = TypeVar("V")
+    def assert_unique(values: list[V], name: str) -> V | UndefinedType:
         n = len(set(values))
         if n == 0:
             return Undefined
