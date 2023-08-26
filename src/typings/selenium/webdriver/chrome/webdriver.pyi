3d719e7a433005b44772663cf11f7af08599edcf
Mateusz Paprocki <mattpap@gmail.com>
2023-08-26 18:33:57 +0200

    Add typings for webdriver.chrome.service

--- src/typings/selenium/webdriver/chrome/webdriver.pyi
+++ src/typings/selenium/webdriver/chrome/webdriver.pyi
@@ -1,5 +1,6 @@
 from ..remote.webdriver import WebDriver as RemoteWebDriver
 from .options import Options
+from .service import Service
 
 class WebDriver(RemoteWebDriver):
-    def __init__(self, options: Options | None = ...) -> None: ...
+    def __init__(self, service: Service | None = ..., options: Options | None = ...) -> None: ...
