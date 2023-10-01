5cdb0af7e5f5d7e22b449868e2a0b2c9df6b1d4f
Moritz Schreiber <moritz.schreiber@ifesca.de>
2023-10-01 11:43:13 +0200

    fix tile_providery.py

--- src/bokeh/tile_providers.py
+++ src/bokeh/tile_providers.py
@@ -6,40 +6,7 @@
 #-----------------------------------------------------------------------------
 ''' Pre-configured tile sources for common third party tile services.
 
-get_provider
-    Use this function to retrieve an instance of a predefined tile provider.
-
-    .. warning::
-        get_provider is deprecated as of Bokeh 3.0.0 and will be removed in a future
-        release. Use ``add_tile`` directly instead.
-
-    Args:
-        provider_name (Union[str, Vendors, xyzservices.TileProvider])
-            Name of the tile provider to supply.
-
-            Use a ``tile_providers.Vendors`` enumeration value, or the string
-            name of one of the known providers. Use
-            :class:`xyzservices.TileProvider` to pass custom tile providers.
-
-    Returns:
-        WMTSTileProviderSource: The desired tile provider instance
-
-    Raises:
-        ValueError, if the specified provider can not be found
-
-    Example:
-
-        .. code-block:: python
-
-                >>> from bokeh.tile_providers import get_provider, Vendors
-                >>> get_provider(Vendors.CARTODBPOSITRON)
-                <class 'bokeh.models.tiles.WMTSTileSource'>
-                >>> get_provider('CARTODBPOSITRON')
-                <class 'bokeh.models.tiles.WMTSTileSource'>
-
-                >>> import xyzservices.providers as xyz
-                >>> get_provider(xyz.CartoDB.Positron)
-                <class 'bokeh.models.tiles.WMTSTileSource'>
+.. autofunction:: bokeh.tile_providers.get_provider
 
 The available built-in tile providers are listed in the ``Vendors`` enum:
 
@@ -194,7 +161,41 @@ class _TileProvidersModule(types.ModuleType):
 
     Vendors = deprecated_vendors()
 
-    def get_provider(self, provider_name):
+    def get_provider(self, provider_name: str | Vendors | xyzservices.TileProvider):
+        """Use this function to retrieve an instance of a predefined tile provider.
+
+        .. warning::
+            get_provider is deprecated as of Bokeh 3.0.0 and will be removed in a future
+            release. Use ``add_tile`` directly instead.
+
+        Args:
+            provider_name (Union[str, Vendors, xyzservices.TileProvider]):
+                Name of the tile provider to supply.
+
+                Use a ``tile_providers.Vendors`` enumeration value, or the string
+                name of one of the known providers. Use
+                :class:`xyzservices.TileProvider` to pass custom tile providers.
+
+        Returns:
+            WMTSTileProviderSource: The desired tile provider instance.
+
+        Raises:
+            ValueError: if the specified provider can not be found.
+
+        Example:
+
+            .. code-block:: python
+
+                    >>> from bokeh.tile_providers import get_provider, Vendors
+                    >>> get_provider(Vendors.CARTODBPOSITRON)
+                    <class 'bokeh.models.tiles.WMTSTileSource'>
+                    >>> get_provider('CARTODBPOSITRON')
+                    <class 'bokeh.models.tiles.WMTSTileSource'>
+
+                    >>> import xyzservices.providers as xyz
+                    >>> get_provider(xyz.CartoDB.Positron)
+                    <class 'bokeh.models.tiles.WMTSTileSource'>
+        """
         deprecated((3, 0, 0), "get_provider", "add_tile directly")
         from bokeh.models import WMTSTileSource
 
