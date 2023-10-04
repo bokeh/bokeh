5e09bfaf222c75bbce146bc744f23a9417b82399
Isaiah Akorita <akoritaisaiah@gmail.com>
2023-10-04 12:22:17 +0100

    refactored correlograms.py file

--- examples/topics/categorical/correlograms.py
+++ examples/topics/categorical/correlograms.py
@@ -1,5 +1,7 @@
 ''' A categorical plot showing the correlations in mineral content for 214 samples of glass fragments
-obtained during forensic work. The dataset contains seven variables measuring the amounts of magnesium (Mg),
+obtained during forensic work.
+
+The dataset contains seven variables measuring the amounts of magnesium (Mg),
 calcium (Ca), iron (Fe), potassium (K), sodium (Na), aluminum (Al), and barium (Ba) found in each glass fragment.
 The colored circles represent the correlations between pairs of these variables.
 The magnitude of each correlation is encoded in the size of the circles.
@@ -11,56 +13,65 @@ The magnitude of each correlation is encoded in the size of the circles.
     :keywords: scatter, correlogram
 
 '''
+
+#from itertools import combinations
 import numpy as np
 import pandas as pd
 
 from bokeh.models import ColorBar, ColumnDataSource, FixedTicker, LinearColorMapper
 from bokeh.plotting import figure, show
-from bokeh.sampledata.forensic_glass import data
+from bokeh.sampledata.forensic_glass import data as df
 from bokeh.transform import transform
 
-df = data.copy()
+elements = ("Mg", "Ca", "Fe", "K", "Na", "Al", "Ba")
+pairs = []
+for i in range(len(elements)):
+    for j in range(len(elements) - 1, i, -1):
+        pair = (elements[i], elements[j])
+        pairs.append(pair)
 
-pairs = ["Mg-Ba", "Mg-Al", "Mg-Na", "Mg-K", "Mg-Fe", "Mg-Ca", "Ca-Ba",
-         "Ca-Al", "Ca-Na", "Ca-K", "Ca-Fe", "Fe-Ba", "Fe-Al", "Fe-Na",
-         "Fe-K", "K-Ba", "K-Al", "K-Na", "Na-Ba", "Na-Al", "Al-Ba"]
+#pairs = list(combinations(elements, 2))
 
-pair_split = [pair.split("-") for pair in pairs]
+x, y = list(zip(*pairs))
+
 correlations = []
-for pair in pair_split:
-    matrix = np.corrcoef(df[f"{pair[0]}"], df[f"{pair[1]}"])
-    correlation = matrix[0, 1]
-    correlations.append(correlation)
+for x, y in pairs:
+    matrix = np.corrcoef(df[x], df[y])
+    correlations.append(matrix[0, 1])
 
-new_df = pd.DataFrame({"oxide_1": [x[0] for x in pair_split], "oxide_2": [x[1] for x in pair_split],
-                       "correlation": correlations, "dot_size": [abs(corr) * 120 for corr in correlations]})
+new_df = pd.DataFrame({
+    "oxide_1": [x[0] for x in pairs],
+    "oxide_2": [x[1] for x in pairs],
+    "correlation": correlations,
+    "dot_size": [abs(corr) * 120 for corr in correlations]
+})
 
 x_range = new_df["oxide_1"].unique()
 y_range = list(reversed(new_df["oxide_2"].unique()))
 
 source = ColumnDataSource(new_df)
 
-colors = ["#8B4513", "#D8AF85", "#CD853F", "#E2CAB7", "#DEB887",
-          "#A4D2D2", "#8ABDBD", "#ADD8E6", "#009999", "#188A8A"]
-
-mapper = LinearColorMapper(palette=colors, low=-0.5, high=0.5)
+mapper = LinearColorMapper(palette="BrBG10", low=-0.5, high=0.5)
 
 p = figure(x_axis_location="above", toolbar_location=None, x_range=x_range, y_range=y_range)
 
 p.scatter(x="oxide_1", y="oxide_2", size="dot_size", source=source, fill_color=transform("correlation", mapper), line_color=None)
 
-color_bar = ColorBar(color_mapper=mapper, location=(200, 0), ticker=FixedTicker(ticks=[-0.5, 0.0, 0.5]), title="correlation",
-                     title_text_align="center", title_text_font_style="normal", major_tick_line_color=None, width=150, height=20)
+color_bar = ColorBar(
+    color_mapper=mapper,
+    location=(200, 0),
+    ticker=FixedTicker(ticks=[-0.5, 0.0, 0.5]),
+    title="correlation",
+    major_tick_line_color=None,
+    width=150,
+    height=20
+)
 
 p.add_layout(color_bar, "below")
 
-p.xaxis.major_tick_line_color = None
-p.xaxis.major_tick_out = 0
-p.xaxis.axis_line_color = None
-
-p.yaxis.major_tick_out = 0
-p.yaxis.major_tick_line_color = None
-p.yaxis.axis_line_color = None
+p.axis.major_tick_line_color = None
+p.axis.major_tick_out = 0
+p.axis.axis_line_color = None
 
 p.grid.grid_line_color = None
 p.outline_line_color = None
