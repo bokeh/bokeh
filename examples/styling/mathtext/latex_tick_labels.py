57207ff0b72afa032a4bef9d4a62b29ab3af1888
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2023-10-20 11:44:55 +0200

    simplify and use more latex ticks (#13470)

--- examples/styling/mathtext/latex_tick_labels.py
+++ examples/styling/mathtext/latex_tick_labels.py
@@ -6,6 +6,10 @@ by adding a dictionary with pairs of position and mathtext.
     :refs: :ref:`ug_styling_mathtext`
     :keywords: mathtext, latex
 '''
+from numpy import arange
+
+from bokeh.plotting import figure, show
+
 x = arange(1, 4.5, 0.25)
 y = 1 / x
 
@@ -17,10 +21,10 @@ plot.line(x, y, color="darkgrey")
 plot.xaxis.axis_label = "Resistance"
 plot.xaxis.ticker = [1, 2, 3, 4]
 plot.xaxis.major_label_overrides = {
-    1: "1 $$\Omega$$",
-    2: "2 $$\Omega$$",
-    3: "3 $$\Omega$$",
-    4: "4 $$\Omega$$",
+    1: r"1 $$\Omega$$",
+    2: r"2 $$\Omega$$",
+    3: r"3 $$\Omega$$",
+    4: r"4 $$\Omega$$",
 }
 
 plot.yaxis.axis_label = "Current"
