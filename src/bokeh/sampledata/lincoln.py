7786e9aa768dc0c5b8d7b7e321789a3d42e1de04
Azaya <99359668+Azaya89@users.noreply.github.com>
2023-10-24 17:01:18 +0100

    SinaPlot gallery example. (#13433)
    * added sample data
    
    * created sinaplot
    
    * added documentation
    
    * fixed format
    
    * added gallery images and json.
    
    * renamed source file
    
    * improved comments clarity

---
+++ src/bokeh/sampledata/lincoln.py
@@ -0,0 +1,70 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2023, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+''' Mean daily temperatures in Lincoln, Nebraska, 2016.
+
+License: `Public Domain`_
+
+This module contains one pandas Dataframe: ``data``.
+
+.. rubric:: ``data``
+
+:bokeh-dataframe:`bokeh.sampledata.lincoln.data`
+
+.. bokeh-sampledata-xref:: lincoln
+'''
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+from __future__ import annotations
+
+import logging # isort:skip
+log = logging.getLogger(__name__)
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+# Standard library imports
+from typing import TYPE_CHECKING
+
+# Bokeh imports
+from ..util.sampledata import package_csv
+
+if TYPE_CHECKING:
+    import pandas as pd
+
+#-----------------------------------------------------------------------------
+# Globals and constants
+#-----------------------------------------------------------------------------
+
+__all__ = (
+    'data',
+)
+
+#-----------------------------------------------------------------------------
+# General API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Dev API
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# Private API
+#-----------------------------------------------------------------------------
+
+def _read_data() -> pd.DataFrame:
+    data = package_csv('lincoln', 'lincoln_weather.csv')
+
+    return data
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+
+data = _read_data()
