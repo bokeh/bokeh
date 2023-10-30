ff2c9249258540f83df5c7b79ddd37e8c65bd978
Mateusz Paprocki <mattpap@gmail.com>
2023-10-30 21:17:21 +0100

    Make RangeTool overlay's nodes unique (#13457)

--- bokehjs/src/lib/models/coordinates/node.ts
+++ bokehjs/src/lib/models/coordinates/node.ts
@@ -9,6 +9,45 @@ export type ImplicitTarget = typeof ImplicitTarget["__type__"]
 export const NodeTarget = Or(Ref(Model), ImplicitTarget)
 export type NodeTarget = typeof NodeTarget["__type__"]
 
+type BoxSymbol = "left" | "right" | "top" | "bottom"
+
+export class BoxNodes {
+  constructor(readonly target: ImplicitTarget, readonly frozen: boolean = false) {}
+
+  private _node(symbol: BoxSymbol): Node {
+    const {target, frozen} = this
+    const node = new Node({target, symbol})
+    if (frozen) {
+      this[`_${symbol}`] = node
+    }
+    return node
+  }
+
+  private _left: Node | null = null
+  get left(): Node {
+    return this._left ?? this._node("left")
+  }
+
+  private _right: Node | null = null
+  get right(): Node {
+    return this._right ?? this._node("right")
+  }
+
+  private _top: Node | null = null
+  get top(): Node {
+    return this._top ?? this._node("top")
+  }
+
+  private _bottom: Node | null = null
+  get bottom(): Node {
+    return this._bottom ?? this._node("bottom")
+  }
+
+  freeze(): BoxNodes {
+    return new BoxNodes(this.target, true)
+  }
+}
+
 export namespace Node {
   export type Attrs = p.AttrsOf<Props>
   export type Props = Coordinate.Props & {
@@ -34,9 +73,14 @@ export class Node extends Coordinate {
       offset: [ Int, 0 ],
     }))
   }
-}
 
-export const frame_left = new Node({target: "frame", symbol: "left"})
-export const frame_right = new Node({target: "frame", symbol: "right"})
-export const frame_top = new Node({target: "frame", symbol: "top"})
-export const frame_bottom = new Node({target: "frame", symbol: "bottom"})
+  private static _frame_nodes = new BoxNodes("frame")
+  static get frame(): BoxNodes {
+    return this._frame_nodes
+  }
+
+  private static _canvas_nodes = new BoxNodes("canvas")
+  static get canvas(): BoxNodes {
+    return this._canvas_nodes
+  }
+}
