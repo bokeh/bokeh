7f2cd0c402d93f4eccf4d68ab8b3ce769ef7759c
Andy Maloney <60523020+ndmlny-qs@users.noreply.github.com>
2023-11-01 14:21:14 -0500

    Rename x & y coordinates
    This commit modifies the name of the x & y coordinates that use the box
    edit tool, as the original name collided with a few glyph types.

--- bokehjs/src/lib/models/tools/edit/box_edit_tool.ts
+++ bokehjs/src/lib/models/tools/edit/box_edit_tool.ts
@@ -59,73 +59,73 @@ export class BoxEditToolView extends EditToolView {
       return
     const {glyph} = renderer
     const cds = renderer.data_source
-    const [x0, x1] = renderer_view.coordinates.x_scale.r_invert(sx0, sx1)
-    const [y0, y1] = renderer_view.coordinates.y_scale.r_invert(sy0, sy1)
+    const [rx0, rx1] = renderer_view.coordinates.x_scale.r_invert(sx0, sx1)
+    const [ry0, ry1] = renderer_view.coordinates.y_scale.r_invert(sy0, sy1)
     const fields = (() => {
       if (glyph instanceof Rect) {
         const {x, y, width, height} = glyph
         if (isField(x) && isField(y) && isField(width) && isField(height)) {
           return {
-            [x.field]: (x0 + x1)/2,
-            [y.field]: (y0 + y1)/2,
-            [width.field]:  x1 - x0,
-            [height.field]: y1 - y0,
+            [x.field]: (rx0 + rx1)/2,
+            [y.field]: (ry0 + ry1)/2,
+            [width.field]:  rx1 - rx0,
+            [height.field]: ry1 - ry0,
           }
         }
       } else if (glyph instanceof Block) {
         const {x, y, width, height} = glyph
         if (isField(x) && isField(y) && isField(width) && isField(height)) {
           return {
-            [x.field]:      x0,
-            [y.field]:      y0,
-            [width.field]:  x1 - x0,
-            [height.field]: y1 - y0,
+            [x.field]:      rx0,
+            [y.field]:      ry0,
+            [width.field]:  rx1 - rx0,
+            [height.field]: ry1 - ry0,
           }
         }
       } else if (glyph instanceof Quad) {
         const {right, bottom, left, top} = glyph
         if (isField(right) && isField(bottom) && isField(left) && isField(top)) {
           return {
-            [right.field]:  x1,
-            [bottom.field]: y0,
-            [left.field]:   x0,
-            [top.field]:    y1,
+            [right.field]:  rx1,
+            [bottom.field]: ry0,
+            [left.field]:   rx0,
+            [top.field]:    ry1,
           }
         }
       } else if (glyph instanceof HBar) {
         const {left, y, height, right} = glyph
         if (isField(left) && isField(y) && isField(height) && isField(right)) {
           return {
-            [left.field]:   x0,
-            [y.field]:      y1,
-            [height.field]: y1 - y0,
-            [right.field]:  x1,
+            [left.field]:   rx0,
+            [y.field]:      ry1,
+            [height.field]: ry1 - ry0,
+            [right.field]:  rx1,
           }
         }
       } else if (glyph instanceof VBar) {
         const {x, bottom, width, top} = glyph
         if (isField(x) && isField(bottom) && isField(width) && isField(top)) {
           return {
-            [x.field]:      x0,
-            [bottom.field]: y0,
-            [width.field]:  x1 - x0,
-            [top.field]:    y1,
+            [x.field]:      rx0,
+            [bottom.field]: ry0,
+            [width.field]:  rx1 - rx0,
+            [top.field]:    ry1,
           }
         }
       } else if (glyph instanceof HStrip) {
         const {y0, y1} = glyph
         if (isField(y0) && isField(y1)) {
           return {
-            [y0.field]: y0,
-            [y1.field]: y1,
+            [y0.field]: ry0,
+            [y1.field]: ry1,
           }
         }
       } else if (glyph instanceof VStrip) {
         const {x0, x1} = glyph
         if (isField(x0) && isField(x1)) {
           return {
-            [x0.field]: x0,
-            [x1.field]: x1,
+            [x0.field]: rx0,
+            [x1.field]: rx1,
           }
         }
       } else {
