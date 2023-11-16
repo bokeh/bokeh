71cbadb4e3f4ac763f5a639ce4a2009cfd074f73
Isaiah Akorita <akoritaisaiah@gmail.com>
2023-11-16 09:03:11 +0100

    additional file refactoring

--- examples/topics/stats/density.py
+++ examples/topics/stats/density.py
@@ -15,26 +15,25 @@ import numpy as np
 from sklearn.neighbors import KernelDensity
 
 from bokeh.models import ColumnDataSource, Label, PrintfTickFormatter
-from bokeh.palettes import Dark2
+from bokeh.palettes import Dark2_5 as colors
 from bokeh.plotting import figure, show
 from bokeh.sampledata.cows import data as df
 
-grouped = df.groupby('breed')
-colors = Dark2[5]
+breed_groups = df.groupby('breed')
 x = np.linspace(2, 8, 1000)
 source = ColumnDataSource(dict(x=x))
 
 p = figure(title="Multiple density estimates", height=300, x_range=(2.5, 7.5), x_axis_label="butterfat contents", y_axis_label="density")
 
-for i, (breed, group_df) in enumerate(grouped):
-    data = group_df['butterfat'].values
+for (breed, breed_df), color in zip(breed_groups, colors):
+    data = breed_df['butterfat'].values
     kde = KernelDensity(kernel="gaussian", bandwidth=0.2).fit(data[:, np.newaxis])
-    log_dens = kde.score_samples(x[:, np.newaxis])
-    y = np.exp(log_dens)
+    log_density = kde.score_samples(x[:, np.newaxis])
+    y = np.exp(log_density)
     source.add(y, breed)
-    p.varea(x="x", y1=breed, y2=0, source=source, fill_alpha=0.3, fill_color=colors[i])
+    p.varea(x="x", y1=breed, y2=0, source=source, fill_alpha=0.3, fill_color=color)
 
-    # Find the highest point and annotate with the label
+    # Find the highest point and annotate with a label
     max_idx = np.argmax(y)
     highest_point_label = Label(
         x=x[max_idx],
@@ -43,11 +42,11 @@ for i, (breed, group_df) in enumerate(grouped):
         text_font_size="10pt",
         x_offset=10,
         y_offset=-5,
-        text_color=colors[i],
+        text_color=color,
     )
     p.add_layout(highest_point_label)
 
-# Convert x-axis labels to percentages
+# Display x-axis labels as percentages
 p.xaxis.formatter = PrintfTickFormatter(format="%d%%")
 
 p.axis.axis_line_color = None
