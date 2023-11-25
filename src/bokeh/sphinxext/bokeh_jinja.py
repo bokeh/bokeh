ddd44e73469b6bfa976c5c9e443c5106852aa9b0
Moritz Schreiber <moritz.schreiber@ifesca.de>
2023-11-25 14:05:38 +0100

    fix indentation in sphinx extentions

--- src/bokeh/sphinxext/bokeh_jinja.py
+++ src/bokeh/sphinxext/bokeh_jinja.py
@@ -17,8 +17,12 @@ Any template parameters will be displayed and the template source code will
 be rendered in a collapsible code block. For example, the usage above will
 generate the following output:
 
-    .. bokeh-jinja:: bokeh.core.templates.FILE
-        :noindex:
+=====
+
+.. bokeh-jinja:: bokeh.core.templates.FILE
+    :noindex:
+
+=====
 
 To enable this extension, add `"bokeh.sphinxext.bokeh_jinja"` to the
 extensions list in your Sphinx configuration module.
