5606bc2e42fe21da089374fc563864472ba4c712
Hui Jeong Lee <huijeong1015@gmail.com>
2023-12-08 13:57:49 -0500

    Revert changes in examples

--- examples/interaction/widgets/textinput.py
+++ examples/interaction/widgets/textinput.py
@@ -1,4 +1,4 @@
-from bokeh.io import show,curdoc
+from bokeh.io import show
 from bokeh.models import CustomJS, TextInput
 
 text_input = TextInput(value="default", title="Label:")
@@ -6,5 +6,4 @@ text_input.js_on_change("value", CustomJS(code="""
     console.log('text_input: value=' + this.value, this.toString())
 """))
 
-curdoc().add_root(text_input)
 show(text_input)
