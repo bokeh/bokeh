2bec5c03943698485f410695ef0e9b10e7e0d4c2
Isaiah Akorita <akoritaisaiah@gmail.com>
2023-12-15 11:04:32 +0100

    added bar labels to plot

--- examples/topics/stats/pyramid.py
+++ examples/topics/stats/pyramid.py
@@ -17,27 +17,39 @@ sex_group = df.groupby("sex")
 female_ages = sex_group.get_group("female")["age"].dropna()
 male_ages = sex_group.get_group("male")["age"].dropna()
 
-bins = np.arange(0, 80, 3)
+bin_width = 5
+bins = np.arange(0, 72, bin_width)
 m_hist, edges = np.histogram(male_ages, bins=bins)
 f_hist, edges = np.histogram(female_ages, bins=bins)
 
-p = figure(title="A pyramid plot showing the age distribution of the male and female Titanic passengers", height=400, width=600,
-           x_range=(-60, 40), x_axis_label="count", y_axis_label="age (years)")
+colors = ("#055BB2", "#CB6805", "#808080")
+bar_width = 4
 
-p.hbar(right=m_hist * -1, y=edges[1:], height=2, color="#055BB2")
+p = figure(title="Age population pyramid of titanic passengers, by gender", height=400, width=600,
+           x_range=(-90, 90), x_axis_label="count")
 
-p.hbar(right=f_hist, y=edges[1:], height=2, color="#CB6805")
+p.hbar(right=m_hist * -1, y=edges[1:], height=bar_width, color=colors[0], line_width=0)
 
+p.hbar(right=f_hist, y=edges[1:], height=bar_width, color=colors[1], line_width=0)
+
+# add text to every other bar
+for i, (count, age) in enumerate(zip(f_hist, edges[1:])):
+    if i % 2 == 0:
+        p.text(x=count, y=edges[1:][i], text=[f"{age-bin_width}-{age}yrs"], x_offset=5, y_offset=7, text_font_size="12px", text_color=colors[2])
+        
 # customise x-axis and y-axis
-p.xaxis.ticker = (-40, -20, 0, 20, 40)
-p.yaxis.ticker = (0, 20, 40, 60)
+p.xaxis.ticker = (-60, -40, -20, 0, 20, 40, 60)
+p.xaxis.major_tick_out = 0
+p.y_range.start = 3
+p.ygrid.grid_line_color = None
+p.yaxis.visible = False
 
-# apply the custom formatter to the x-axis using CustomJSTickFormatter
+# apply a custom formatter to the x-axis using CustomJSTickFormatter
 p.xaxis.formatter = CustomJSTickFormatter(args=dict(), code="return Math.abs(tick);")
 
 # add labels
-m_label = Label(x=-40, y=70, text="male", text_font_size="15pt", x_offset=5)
-f_label = Label(x=20, y=70, text="female", text_font_size="15pt", x_offset=5)
+m_label = Label(x=-40, y=70, text="Men", text_color=colors[0], x_offset=5)
+f_label = Label(x=20, y=70, text="Women", text_color=colors[1], x_offset=5)
 
 p.add_layout(m_label)
 p.add_layout(f_label)
