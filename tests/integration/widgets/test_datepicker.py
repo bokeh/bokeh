d322647254356c969ea9260f4a0f868397afd2de
Mateusz Paprocki <mattpap@gmail.com>
2024-01-04 10:17:26 +0100

    Update license and copyright statements to 2024 (#13630)

--- tests/integration/widgets/test_datepicker.py
+++ tests/integration/widgets/test_datepicker.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# Copyright (c) 2012 - 2023, Anaconda, Inc. All rights reserved.
+# Copyright (c) 2012 - 2024, Anaconda, Inc. All rights reserved.
 #
 # Powered by the Bokeh Development Team.
 #
