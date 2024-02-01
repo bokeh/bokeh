71f835269feb973382ef7e9453f2f24dbde1fa48
Timo Cornelius Metzger <39711796+tcmetzger@users.noreply.github.com>
2024-02-01 05:42:28 +0000

    Automatically pick newest env file based on Python version

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,18 @@
-# Create conda env from file
-conda env create -n bkdev -f conda/environment-test-3.12.yml
+# Change to the directory containing the conda env .yml files
+cd conda
+
+# Find all .yml files, extract Python version numbers, sort, and get the highest version
+highest_version_file=$(ls *-*.yml | sort -V | tail -1)
+
+# Check if a file was found
+if [ -z "$highest_version_file" ]; then
+    echo "No conda environment.yml file found"
+    exit 1
+fi
+
+# Create the conda environment using the file with the highest Python version
+conda env create -n bkdev -f "$highest_version_file"
+cd ..
 
 # Activate the bkdev conda environment whenever a new shell is opened
 conda init bash && . /root/.bashrc && conda activate bkdev
