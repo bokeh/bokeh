44136923f1be65ecc6418e294d9ee683fb763ce5
Mateusz Paprocki <mattpap@gmail.com>
2024-02-01 15:44:53 +0100

    Improve examples/basic/ui/dialog.py

--- examples/basic/ui/dialog.py
+++ examples/basic/ui/dialog.py
@@ -1,40 +1,63 @@
 import numpy as np
 
+from bokeh.core.enums import OutputBackendType
 from bokeh.io import show
-from bokeh.models import Button, Column, CustomJS, Dialog, OpenDialog
+from bokeh.layouts import column
+from bokeh.models import Button, CloseDialog, Column, Dialog, OpenDialog
+from bokeh.models.dom import HTML
 from bokeh.plotting import figure
 
 
-def plot(N: int):
+def ui(N: int, output_backend: OutputBackendType):
     x = np.random.random(size=N) * 100
     y = np.random.random(size=N) * 100
     radii = np.random.random(size=N) * 1.5
     colors = np.array([(r, g, 150) for r, g in zip(50+2*x, 30+2*y)], dtype="uint8")
 
-    p = figure(active_scroll="wheel_zoom", lod_threshold=None, title=f"Plot with N={N} circles", sizing_mode="stretch_both")
-    p.circle(x, y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
-
-    return p
-
-show_plot = Button(label="Show plot ...")
-
-close_dialog = Button(label="Close")
-dialog = Dialog(
-    title="Dialog with a plot",
-    content=Column(
+    plot = figure(
+        title=f"Plot with N={N} circles",
+        output_backend=output_backend,
+        lod_threshold=None,
+        active_scroll="wheel_zoom",
         sizing_mode="stretch_both",
-        children=[
-            plot(500),
-            close_dialog,
-        ],
-    ),
-)
-show_plot.js_on_click(OpenDialog(dialog=dialog))
-close_dialog.js_on_click(
-    CustomJS(
-        args=dict(dialog=dialog),
-        code="export default ({dialog}, _obj, _data, {index}) => index.get_one(dialog).close()",
-    ),
-)
+    )
+    plot.circle(x, y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
 
-show(show_plot)
+    show_plot = Button(
+        label=HTML(f"Show plot using <b>{output_backend}</b> backend ..."),
+        sizing_mode="stretch_width",
+    )
+
+    close_plot = Button(label="Close")
+    dialog = Dialog(
+        title=HTML(f"Dialog with a plot using <b>{output_backend}</b> backend"),
+        content=Column(
+            sizing_mode="stretch_both",
+            children=[
+                plot,
+                close_plot,
+            ],
+        ),
+    )
+    show_plot.js_on_click(OpenDialog(dialog=dialog))
+    close_plot.js_on_click(CloseDialog(dialog=dialog))
+
+    # Alternatively one can use a CustomJS callback to achieve the same effect:
+    #
+    # from bokeh.models import CustomJS
+    # close_plot.js_on_click(
+    #     CustomJS(
+    #         args=dict(dialog=dialog),
+    #         code="export default ({dialog}, _obj, _data, {index}) => index.get_one(dialog).close()",
+    #     ),
+    # )
+
+    return show_plot
+
+layout = column([
+    ui(1000, "canvas"),
+    ui(1000, "svg"),
+    ui(1000, "webgl"),
+])
+
+show(layout)
