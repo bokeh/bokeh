8a48e57b30e6153ca1ac4814c22441cc3ba3f8a3
Mateusz Paprocki <mattpap@gmail.com>
2024-02-07 01:57:54 +0100

    Add rudimentary integration tests for UI elements

--- bokehjs/test/integration/layouts.ts
+++ bokehjs/test/integration/layouts.ts
@@ -3,7 +3,6 @@ import {display, fig} from "./_util"
 import {mouse_click} from "../interactive"
 
 import {Spacer, Tabs, TabPanel, GridBox, GroupBox, ScrollBox, Row, Column, HBox, VBox} from "@bokehjs/models/layouts"
-import {Pane} from "@bokehjs/models/ui"
 import {TextInput, Button} from "@bokehjs/models/widgets"
 import type {SizingPolicy} from "@bokehjs/core/layout"
 import type {Color} from "@bokehjs/core/types"
@@ -15,21 +14,6 @@ import {color2css, is_dark} from "@bokehjs/core/util/color"
 import {figure, gridplot} from "@bokehjs/api/plotting"
 import type {BasicTickFormatter, Plot} from "@bokehjs/models"
 import {LinearAxis} from "@bokehjs/models"
-
-describe("UIElement", () => {
-  it("should support dashed, snake and camel CSS property names and CSS variables in styles", async () => {
-    const [width, height] = [100, 100]
-    const common = {width: `${width}px`, height: `${height}px`}
-
-    const p0 = new Pane({styles: {...common, "background-color": "red"}})
-    const p1 = new Pane({styles: {...common, background_color: "green"}})
-    const p2 = new Pane({styles: {...common, backgroundColor: "blue"}})
-    const p3 = new Pane({styles: {...common, "--bg-color": "yellow"}, stylesheets: [":host { background-color: var(--bg-color); }"]})
-
-    const layout = new Pane({styles: {display: "inline-flex"}, elements: [p0, p1, p2, p3]})
-    await display(layout, [layout.elements.length*width + 50, height + 50])
-  })
-})
 
 const spacer =
   (width_policy: SizingPolicy, height_policy: SizingPolicy,
