210385bc8e299b577c57d5b3ea19b168de66f80c
Thomas Kientz <thomas@kientz.net>
2024-02-09 16:00:56 +0100

    fix examples

--- examples/integration/widgets/data_table_formatting.py
+++ examples/integration/widgets/data_table_formatting.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from bokeh.io import save
-from bokeh.models import DataTable, StringFormatter
+from bokeh.models import DataTable, StringFormatter, DateFormatter
 
 # Sample size
 sample_size = 30
@@ -10,13 +10,14 @@ sample_size = 30
 # Data generation
 dataframe = pd.DataFrame({
     "text_color_col": np.random.choice(["red", "green", "blue", "black", "grey"], size=sample_size),
-    "other_text_color_col": ["value = blue"] * sample_size,
+    "other_text_color_col": ["some text"] * sample_size,
     "background_color_col": np.random.choice(["red", "green", "blue", "yellow"], size=sample_size),
-    "other_background_color_col": ["value = yellow"] * sample_size,
+    "other_background_color_col": ["some text"] * sample_size,
     "font_style_col": np.random.choice(["normal", "italic", "bold", "bold italic"], size=sample_size),
-    "other_font_style_col": ["value = bold"] * sample_size,
+    "other_font_style_col": ["some text"] * sample_size,
     "text_align_col": np.random.choice(["left", "center", "right"], size=sample_size),
-    "other_text_align_col": ["value = center"] * sample_size,
+    "other_text_align_col": ["some text"] * sample_size,
+    "date_col": pd.date_range(start="2020-01-01", periods=sample_size),
 })
 
 formatters = [
@@ -28,6 +29,7 @@ formatters = [
     StringFormatter(font_style="bold"),
     StringFormatter(text_align="text_align_col"),
     StringFormatter(text_align="center"),
+    DateFormatter(format="%Y-%m-%d", text_color="text_color_col", font_style="font_style_col"),
 ]
 
 datatable = DataTable.from_data(dataframe)
