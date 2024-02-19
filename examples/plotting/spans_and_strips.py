1f1a112530baf019f8389749ae88ec09631401ee
AkintolaRichard <princerichard17a@gmail.com>
2024-02-19 13:09:36 +0100

    Add metadata to spans and strips plotting example

--- examples/plotting/spans_and_strips.py
+++ examples/plotting/spans_and_strips.py
@@ -1,3 +1,11 @@
+''' This example demonstrates the use of ``hspan``, ``wspan``, and ``vstrip``.
+It also demonstrates how the default ``HoverTool`` interacts with these glphs.
+
+.. bokeh-example-metadata::
+    :apis: bokeh.plotting.figure.hspan, bokeh.plotting.figure.hstrip, bokeh.plotting.figure.vstrip
+    :keywords: strips, hover tool
+'''
+
 from bokeh.io import show
 from bokeh.models import HoverTool
 from bokeh.plotting import figure
