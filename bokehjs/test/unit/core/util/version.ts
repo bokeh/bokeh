e3e7901bcf9b9fd72919c323fae0bbe36b943523
Mateusz Paprocki <mattpap@gmail.com>
2024-03-06 01:11:17 +0100

    Avoid spurious warning about Py/Js version mismatch

--- bokehjs/test/unit/core/util/version.ts
+++ bokehjs/test/unit/core/util/version.ts
@@ -1,19 +1,25 @@
 import {expect} from "assertions"
-import {pyify_version} from "@bokehjs/core/util/version"
+import {Version, ReleaseType} from "@bokehjs/core/util/version"
 
 describe("core/util/version module", () => {
-  it("should implement pyify_version() function for full versions", () => {
-    const out = pyify_version("1.2.3")
-    expect(out).to.be.equal("1.2.3")
+  it("should implement Version.from() function for full versions", () => {
+    const out = Version.from("1.2.3")
+    expect(out).to.be.equal(new Version(1, 2, 3))
   })
 
-  it("should implement pyify_version() function for dev versions", () => {
-    const out = pyify_version("1.2.3-dev.18")
-    expect(out).to.be.equal("1.2.3.dev18")
+  it("should implement Version.from() function for dev versions", () => {
+    const ver0 = Version.from("1.2.3-dev.18")
+    expect(ver0).to.be.equal(new Version(1, 2, 3, ReleaseType.Dev, 18))
+
+    const ver1 = Version.from("1.2.3.dev18")
+    expect(ver1).to.be.equal(new Version(1, 2, 3, ReleaseType.Dev, 18))
   })
 
-  it("should implement pyify_version() function for rc versions", () => {
-    const out = pyify_version("1.2.3-rc.18")
-    expect(out).to.be.equal("1.2.3.rc18")
+  it("should implement Version.from() function for rc versions", () => {
+    const ver0 = Version.from("1.2.3-rc.18")
+    expect(ver0).to.be.equal(new Version(1, 2, 3, ReleaseType.Candidate, 18))
+
+    const ver1 = Version.from("1.2.3rc18")
+    expect(ver1).to.be.equal(new Version(1, 2, 3, ReleaseType.Candidate, 18))
   })
 })
