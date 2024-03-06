a76afc69cb382776170b9015ee522abba437e75a
Mateusz Paprocki <mattpap@gmail.com>
2024-03-06 16:37:22 +0100

    Update bokehjs' unit tests

--- bokehjs/test/unit/core/logging.ts
+++ bokehjs/test/unit/core/logging.ts
@@ -2,6 +2,7 @@ import {expect} from "assertions"
 
 import type {LogLevel} from "@bokehjs/core/logging"
 import {Logger, logger, set_log_level} from "@bokehjs/core/logging"
+import {version} from "@bokehjs/version"
 
 import {trap} from "../../util"
 
@@ -26,48 +27,51 @@ describe("logging module", () => {
 
       it("trace", () => {
         const out = trap(() => original = set_log_level("trace"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'trace'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'trace'\n`)
         expect(logger.level).to.be.equal(Logger.TRACE)
       })
 
       it("debug", () => {
         const out = trap(() => original = set_log_level("debug"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'debug'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'debug'\n`)
         expect(logger.level).to.be.equal(Logger.DEBUG)
       })
 
       it("info", () => {
         const out = trap(() => original = set_log_level("info"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'info'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'info'\n`)
         expect(logger.level).to.be.equal(Logger.INFO)
       })
       it("warn", () => {
         const out = trap(() => original = set_log_level("warn"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'warn'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'warn'\n`)
         expect(logger.level).to.be.equal(Logger.WARN)
       })
       it("error", () => {
         const out = trap(() => original = set_log_level("error"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'error'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'error'\n`)
         expect(logger.level).to.be.equal(Logger.ERROR)
       })
 
       it("fatal", () => {
         const out = trap(() => original = set_log_level("fatal"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'fatal'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'fatal'\n`)
         expect(logger.level).to.be.equal(Logger.FATAL)
       })
 
       it("off", () => {
         const out = trap(() => original = set_log_level("off"))
-        expect(out.log).to.be.equal("[bokeh] setting log level to: 'off'\n")
+        expect(out.log).to.be.equal(`[bokeh ${version}] setting log level to: 'off'\n`)
         expect(logger.level).to.be.equal(Logger.OFF)
       })
     })
 
     it("ignores unknown levels", () => {
       const out = trap(() => set_log_level("bad"))
-      expect(out.log).to.be.equal("[bokeh] unrecognized logging level 'bad' passed to Bokeh.set_log_level(), ignoring\n[bokeh] valid log levels are: trace, debug, info, warn, error, fatal, off\n")
+      expect(out.log).to.be.equal(`\
+[bokeh ${version}] unrecognized logging level 'bad' passed to Bokeh.set_log_level(), ignoring
+[bokeh ${version}] valid log levels are: trace, debug, info, warn, error, fatal, off
+`)
     })
   })
 })
