8c4b9d8a13c435cdb6ecc3ea5020a1dfc0f06940
Andrew <15331990+ahuang11@users.noreply.github.com>
2024-03-15 10:20:07 -0700

    Add carbon theme (#13675)
    * Add theme
    
    * Update title font size
    
    * Commas

---
+++ src/bokeh/themes/_carbon.py
@@ -0,0 +1,74 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2012 - 2022, Anaconda, Inc., and Bokeh Contributors.
+# All rights reserved.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
+
+json = {
+    "attrs": {
+        "figure": {
+            "background_fill_color": "#1b1e23",
+            "border_fill_color": "#1b1e23",
+            "outline_line_alpha": 0,
+        },
+        "Grid": {
+            "grid_line_color": "#808080",
+            "grid_line_alpha": 0.1,
+        },
+        "Axis": {
+            # tick color and alpha
+            "major_tick_line_color": "#4d4f51",
+            "minor_tick_line_alpha": 0,
+            # tick labels
+            "major_label_text_font": "Courier New",
+            "major_label_text_color": "#808080",
+            "major_label_text_align": "left",
+            "major_label_text_font_size": "0.95em",
+            "major_label_text_font_style": "normal",
+            # axis labels
+            "axis_label_text_font": "Courier New",
+            "axis_label_text_font_style": "normal",
+            "axis_label_text_font_size": "1.15em",
+            "axis_label_text_color": "lightgrey",
+            "axis_line_color": "#4d4f51",
+        },
+        "Legend": {
+            "spacing": 8,
+            "glyph_width": 15,
+
+            "label_standoff": 8,
+            "label_text_color": "#808080",
+            "label_text_font": "Courier New",
+            "label_text_font_size": "0.95em",
+            "label_text_font_style": "bold",
+
+            "border_line_alpha": 0,
+            "background_fill_alpha": 0,
+            "inactive_fill_alpha": 0,
+        },
+        "BaseColorBar": {
+            # axis labels
+            "title_text_color": "lightgrey",
+            "title_text_font": "Courier New",
+            "title_text_font_size": "0.95em",
+            "title_text_font_style": "normal",
+            # tick labels
+            "major_label_text_color": "#808080",
+            "major_label_text_font": "Courier New",
+            "major_label_text_font_size": "0.95em",
+            "major_label_text_font_style": "normal",
+
+            "background_fill_color": "#1b1e23",
+            "major_tick_line_alpha": 0,
+            "bar_line_alpha": 0,
+        },
+        "Title": {
+            "text_font": "Courier New",
+            "text_font_style": "normal",
+            "text_font_size": "1.15em",
+
+            "text_color": "lightgrey",
+        },
+    },
+}
