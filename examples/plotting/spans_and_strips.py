f0b048b2cce0fabd8cef7c42ad5385cc0ea59b32
Bryan Van de Ven <bryan@bokeh.org>
2024-03-15 12:02:38 -0700

    Update examples/plotting/spans_and_strips.py

--- examples/plotting/spans_and_strips.py
+++ examples/plotting/spans_and_strips.py
@@ -1,6 +1,5 @@
 ''' This example demonstrates the use of ``hspan``, ``vspan``, ``hstrip`` and
-``vstrip``.
-It also demonstrates how the default ``HoverTool`` interacts with these glyphs.
+``vstrip``. It also demonstrates how the default ``HoverTool`` interacts with these glyphs.
 
 .. bokeh-example-metadata::
     :apis: bokeh.plotting.figure.hspan, bokeh.plotting.figure.hstrip, bokeh.plotting.figure.vstrip
