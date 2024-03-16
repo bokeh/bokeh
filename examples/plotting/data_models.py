57ce2e444a0253d1545aac4ac8b126076dfa0c0b
AkintolaRichard <princerichard17a@gmail.com>
2024-03-16 11:36:42 +0100

    fix: Add typo and docstring format changes

--- examples/plotting/data_models.py
+++ examples/plotting/data_models.py
@@ -1,7 +1,7 @@
-'''This example shows creating sinusoidal wave using bokeh DataModel.
+''' This example shows creating sinusoidal wave using bokeh DataModel.
 
 .. bokeh-example-metadata::
-    :apis: bokeh.model.DataModel, bokeh.models.ColumnDataSource, bokeh.models.CustomJS, bokeh.core.properties.Float
+    :apis: bokeh.models.DataModel, bokeh.models.ColumnDataSource, bokeh.models.CustomJS, bokeh.core.properties.Float
     :keywords: wave, sinusoidal, phase, frequency, amplitude
 
 '''
