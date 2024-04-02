d985c8fa9067ef0c54c71693c55a5e4b1ee209a2
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-04-02 17:35:04 +0200

    Fix indentation issue (#13793)
    * fix indentation for strip_leading_zeros  and typos
    
    * remove trailing whitespaces

--- src/bokeh/models/formatters.py
+++ src/bokeh/models/formatters.py
@@ -642,9 +642,10 @@ class DatetimeTickFormatter(TickFormatter):
     Whether to strip any leading zeros in the formatted ticks.
 
     Valid values are:
+
     * ``True`` or ``False`` (default) to set stripping across all resolutions.
     * A sequence of resolution types, e.g. ``["microseconds", "milliseconds"]``, to enable
-    scale-dependent stripping of leading zeros.
+      scale-dependent stripping of leading zeros.
     """)
 
     boundary_scaling = Bool(default=True, help="""
