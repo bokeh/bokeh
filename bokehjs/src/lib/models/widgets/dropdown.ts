1c41a13b88484c7afcb256fff5652b8016f67dc8
hunter7lee <hunter.e.lee@vanderbilt.edu>
2024-04-21 17:47:14 -0500

    dropdowns fixed

--- bokehjs/src/lib/models/widgets/dropdown.ts
+++ bokehjs/src/lib/models/widgets/dropdown.ts
@@ -1,5 +1,4 @@
 import {AbstractButton, AbstractButtonView} from "./abstract_button"
-
 import {ButtonClick, MenuItemClick} from "core/bokeh_events"
 import type {StyleSheetLike} from "core/dom"
 import {div, display, undisplay} from "core/dom"
@@ -7,7 +6,6 @@ import type * as p from "core/properties"
 import {isString} from "core/util/types"
 import type {CallbackLike1} from "core/util/callbacks"
 import {execute} from "core/util/callbacks"
-
 import * as buttons from "styles/buttons.css"
 import dropdown_css, * as dropdown from "styles/dropdown.css"
 import carets_css, * as carets from "styles/caret.css"
@@ -16,11 +14,15 @@ export class DropdownView extends AbstractButtonView {
   declare model: Dropdown
 
   protected _open: boolean = false
-
   protected menu: HTMLElement
 
   override stylesheets(): StyleSheetLike[] {
     return [...super.stylesheets(), dropdown_css, carets_css]
+  }
+
+  override initialize(): void {
+    super.initialize()
+    this.connect(this.model.properties.menu.change, () => this.rebuild_menu())
   }
 
   override render(): void {
@@ -37,19 +39,9 @@ export class DropdownView extends AbstractButtonView {
       this.group_el.appendChild(toggle)
     }
 
-    const items = this.model.menu.map((item, i) => {
-      if (item == null) {
-        return div({class: dropdown.divider})
-      } else {
-        const label = isString(item) ? item : item[0]
-        const el = div(label)
-        el.addEventListener("click", () => this._item_click(i))
-        return el
-      }
-    })
-
-    this.menu = div({class: [dropdown.menu, dropdown.below]}, items)
+    this.menu = div({class: [dropdown.menu, dropdown.below]})
     this.shadow_el.appendChild(this.menu)
+    this.rebuild_menu()
     undisplay(this.menu)
   }
 
@@ -95,16 +87,38 @@ export class DropdownView extends AbstractButtonView {
 
   protected _item_click(i: number): void {
     this._hide_menu()
-
     const item = this.model.menu[i]
     if (item != null) {
+      const label = isString(item) ? item : item[0] // Extract label from the menu item
       const value_or_callback = isString(item) ? item : item[1]
+
+      this.model.label = label
+
       if (isString(value_or_callback)) {
         this.model.trigger_event(new MenuItemClick(value_or_callback))
       } else {
         void execute(value_or_callback, this.model, {index: i})
       }
+
+      this.render()
     }
+  }
+
+  rebuild_menu(): void {
+    while (this.menu.firstChild !== null) {
+      this.menu.removeChild(this.menu.firstChild)
+    }
+    const items = this.model.menu.map((item, i) => {
+      if (item == null) {
+        return div({class: dropdown.divider})
+      } else {
+        const label = isString(item) ? item : item[0]
+        const el = div(label)
+        el.addEventListener("click", () => this._item_click(i))
+        return el
+      }
+    })
+    items.forEach(item => this.menu.appendChild(item))
   }
 }
 
