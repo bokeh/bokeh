61cfcb54199fb0fee0c8456920a67a553ffd1294
Mateusz Paprocki <mattpap@gmail.com>
2024-05-10 10:05:31 +0200

    Add a preliminary example

--- examples/interaction/js_callbacks/doc_js_events.py
+++ examples/interaction/js_callbacks/doc_js_events.py
@@ -2,7 +2,8 @@
 
 from bokeh.events import Event
 from bokeh.io import curdoc
-from bokeh.models import Button, CustomJS
+from bokeh.layouts import column
+from bokeh.models import Button, CustomJS, FileInput
 
 
 def py_ready(event: Event):
@@ -27,16 +28,19 @@ document.body.insertAdjacentHTML("beforeend", html)
 curdoc().on_event("connection_lost", py_connection_lost)
 curdoc().js_on_event("connection_lost", js_connection_lost)
 
+file_input = FileInput()
+
 def py_clicked(event: Event):
     print("CLICKED!")
+    file_input.clear()
 
 js_clicked = CustomJS(code="""
 const html = "<div>CLICKED!</div>"
 document.body.insertAdjacentHTML("beforeend", html)
 """)
 
-button = Button(label="Click me")
-button.on_event("button_click", py_clicked)
+button = Button(label="Click me to clear the selected file")
+button.on_click(py_clicked) # or button.on_event("button_click", py_clicked)
 button.js_on_event("button_click", js_clicked)
 
-curdoc().add_root(button)
+curdoc().add_root(column(file_input, button))
