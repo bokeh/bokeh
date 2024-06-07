291c26e05bf036769e79a81ef4f403a813c1b0e8
Simon Høxbro Hansen <simon.hansen@me.com>
2024-06-07 13:35:10 +0200

    Clean up

--- bokehjs/src/lib/models/widgets/file_input.ts
+++ bokehjs/src/lib/models/widgets/file_input.ts
@@ -50,12 +50,6 @@ export class FileInputView extends InputWidgetView {
     })
   }
 
-  override connect_signals(): void {
-    super.connect_signals()
-    const {_clear_input} = this.model.properties
-    this.on_change(_clear_input, this.clear_input)
-  }
-
   async load_files(files: FileList): Promise<void> {
     const values: string[] = []
     const filenames: string[] = []
@@ -107,11 +101,6 @@ export class FileInputView extends InputWidgetView {
       reader.readAsDataURL(file)
     })
   }
-
-  protected clear_input(): void {
-    this.input_el.value = ""
-    this.model.setv({value: "", filename: "", mime_type: ""})
-  }
 }
 
 export namespace FileInput {
@@ -123,7 +112,6 @@ export namespace FileInput {
     accept: p.Property<string | string[]>
     multiple: p.Property<boolean>
     directory: p.Property<boolean>
-    _clear_input: p.Property<number>
   }
 }
 
@@ -140,14 +128,13 @@ export class FileInput extends InputWidget {
   static {
     this.prototype.default_view = FileInputView
 
-    this.define<FileInput.Props>(({Bool, Str, List, Or, Int}) => ({
+    this.define<FileInput.Props>(({Bool, Str, List, Or}) => ({
       value:     [ Or(Str, List(Str)), p.unset, {readonly: true} ],
       mime_type: [ Or(Str, List(Str)), p.unset, {readonly: true} ],
       filename:  [ Or(Str, List(Str)), p.unset, {readonly: true} ],
       accept:    [ Or(Str, List(Str)), "" ],
       multiple:  [ Bool, false ],
       directory: [ Bool, false ],
-      _clear_input: [ Int, 0],
     }))
   }
 }
