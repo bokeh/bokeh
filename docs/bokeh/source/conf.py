2d1351e505131a498a1853816063ab3b0c62bcf4
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-06-25 19:33:03 +0200

    add release to project configuration (#13949)
    * add release to project configuration
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>
    
    ---------
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

--- docs/bokeh/source/conf.py
+++ docs/bokeh/source/conf.py
@@ -25,7 +25,7 @@ copyright = f"©{year} {author}."
 
 project = "Bokeh"
 
-version = settings.docs_version() or __version__
+release = version = settings.docs_version() or __version__
 
 # -- Sphinx configuration -----------------------------------------------------
 
