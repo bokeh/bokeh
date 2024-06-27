25c1165b11f5e1162dc27d9f56e1e1404af7b309
Mateusz Paprocki <mattpap@gmail.com>
2024-06-27 10:05:43 +0200

    Rename {_width,_height} -> image_{width,height}

--- bokehjs/src/lib/models/glyphs/image_base.ts
+++ bokehjs/src/lib/models/glyphs/image_base.ts
@@ -114,8 +114,8 @@ export abstract class ImageBaseView extends XYGlyphView {
     if (!this._can_inherit_image_data) {
       if (typeof this.image_data === "undefined" || this.image_data.length != n) {
         this._define_attr<ImageBase.Data>("image_data", new Array(n).fill(null))
-        this._define_attr<ImageBase.Data>("_width", new Uint32Array(n))
-        this._define_attr<ImageBase.Data>("_height", new Uint32Array(n))
+        this._define_attr<ImageBase.Data>("image_width", new Uint32Array(n))
+        this._define_attr<ImageBase.Data>("image_height", new Uint32Array(n))
       }
 
       const {image_dimension} = this
@@ -129,16 +129,16 @@ export abstract class ImageBaseView extends XYGlyphView {
         assert(img.dimension == image_dimension, `expected a ${image_dimension}D array, not ${img.dimension}D`)
 
         const [height, width] = img.shape
-        this._width[i] = width
-        this._height[i] = height
+        this.image_width[i] = width
+        this.image_height[i] = height
 
         const buf8 = this._flat_img_to_buf8(img)
         this._set_image_data_from_buffer(i, buf8)
       }
     } else {
       this._inherit_attr<ImageBase.Data>("image_data")
-      this._inherit_attr<ImageBase.Data>("_width")
-      this._inherit_attr<ImageBase.Data>("_height")
+      this._inherit_attr<ImageBase.Data>("image_width")
+      this._inherit_attr<ImageBase.Data>("image_height")
     }
   }
 
@@ -171,13 +171,13 @@ export abstract class ImageBaseView extends XYGlyphView {
   protected _get_or_create_canvas(i: number): HTMLCanvasElement {
     assert(this.image_data != null)
     const image_data_i = this.image_data[i]
-    if (image_data_i != null && image_data_i.width  == this._width[i]
-                             && image_data_i.height == this._height[i]) {
+    if (image_data_i != null && image_data_i.width  == this.image_width[i]
+                             && image_data_i.height == this.image_height[i]) {
       return image_data_i
     } else {
       const canvas = document.createElement("canvas")
-      canvas.width = this._width[i]
-      canvas.height = this._height[i]
+      canvas.width = this.image_width[i]
+      canvas.height = this.image_height[i]
       return canvas
     }
   }
@@ -186,7 +186,7 @@ export abstract class ImageBaseView extends XYGlyphView {
     assert(this.image_data != null)
     const canvas = this._get_or_create_canvas(i)
     const ctx = canvas.getContext("2d")!
-    const image_data = ctx.getImageData(0, 0, this._width[i], this._height[i])
+    const image_data = ctx.getImageData(0, 0, this.image_width[i], this.image_height[i])
     image_data.data.set(buf8)
     ctx.putImageData(image_data, 0, 0)
     this.image_data[i] = canvas
@@ -220,8 +220,8 @@ export abstract class ImageBaseView extends XYGlyphView {
 
   protected _image_index(index: number, x: number, y: number): ImageIndex {
     const [l, r, t, b] = this._lrtb(index)
-    const width = this._width[index]
-    const height = this._height[index]
+    const width = this.image_width[index]
+    const height = this.image_height[index]
     const dx = (r - l) / width
     const dy = (t - b) / height
     const i = Math.floor((x - l) / dx)
@@ -270,11 +270,11 @@ export namespace ImageBase {
     image_data: Arrayable<ImageData> | undefined
     inherited_image_data: boolean
 
-    _width: Uint32Array
-    inherited__width: boolean
+    image_width: Uint32Array
+    inherited_image_width: boolean
 
-    _height: Uint32Array
-    inherited__height: boolean
+    image_height: Uint32Array
+    inherited_image_height: boolean
   }
 }
 
