63955e5fbb2dec80f00f7fbbd5840aba54da1ec8
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-07-13 13:52:53 +0200

    solution on js side

--- examples/styling/plots/glyph_selection_models.py
+++ examples/styling/plots/glyph_selection_models.py
@@ -4,8 +4,8 @@ from bokeh.plotting import figure, show
 plot = figure(width=400, height=400, tools="tap", title="Select a circle")
 renderer = plot.scatter([1, 2, 3, 4, 5], [2, 5, 8, 2, 7], size=50)
 
-selected_scatter = Scatter(fill_alpha=1, fill_color="firebrick", line_color=None, size=50)
-nonselected_scatter = Scatter(fill_alpha=0.2, fill_color="blue", line_color="firebrick", size=50)
+selected_scatter = Scatter(fill_alpha=1, fill_color="firebrick", line_color=None)
+nonselected_scatter = Scatter(fill_alpha=0.2, fill_color="blue", line_color="firebrick")
 
 renderer.selection_glyph = selected_scatter
 renderer.nonselection_glyph = nonselected_scatter
