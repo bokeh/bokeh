97d2b92ed248c296aab8454de46513469f6ee220
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-09-07 01:26:43 +0200

    clone renderer in glypg_selection_models.py

--- examples/styling/plots/glyph_selection_models.py
+++ examples/styling/plots/glyph_selection_models.py
@@ -4,10 +4,7 @@ from bokeh.plotting import figure, show
 plot = figure(width=400, height=400, tools="tap", title="Select a circle")
 renderer = plot.scatter([1, 2, 3, 4, 5], [2, 5, 8, 2, 7], size=50)
 
-selected_scatter = Scatter(fill_alpha=1, fill_color="firebrick", line_color=None)
-nonselected_scatter = Scatter(fill_alpha=0.2, fill_color="blue", line_color="firebrick")
-
-renderer.selection_glyph = selected_scatter
-renderer.nonselection_glyph = nonselected_scatter
+renderer.selection_glyph = renderer.glyph.clone(fill_alpha=1, fill_color="firebrick", line_color=None)
+renderer.nonselection_glyph = renderer.glyph.clone(fill_alpha=0.2, fill_color="blue", line_color="firebrick")
 
 show(plot)
