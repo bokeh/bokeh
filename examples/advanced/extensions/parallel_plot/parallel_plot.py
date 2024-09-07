9232e882bf6adf9f28b8f87fb0404b7d67664aae
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-09-07 21:21:42 +0200

    fix advanced example parallel_plot.py

--- examples/advanced/extensions/parallel_plot/parallel_plot.py
+++ examples/advanced/extensions/parallel_plot/parallel_plot.py
@@ -3,7 +3,7 @@ import numpy as np
 from bokeh.layouts import column
 from bokeh.models import (BasicTickFormatter, ColumnDataSource,
                           CustomJSTickFormatter, Div, FixedTicker,
-                          LinearAxis, LinearColorMapper, MultiLine, Range1d)
+                          LinearAxis, LinearColorMapper, Range1d)
 from bokeh.plotting import figure
 from bokeh.sampledata.autompg import autompg_clean as df
 
@@ -34,9 +34,9 @@ def parallel_plot(df, color=None, palette=None):
     p = figure(x_range=(-1, ndims),
                y_range=(0, 1),
                width=1000,
-               tools="pan, box_zoom")
+               tools="pan,box_zoom,tap")
 
-    # Create x axis ticks from columns contained in dataframe
+    # Create x-axis ticks from columns contained in dataframe
     fixed_x_ticks = FixedTicker(
         ticks=np.arange(ndims), minor_ticks=[])
     formatter_x_ticks = CustomJSTickFormatter(
@@ -47,7 +47,7 @@ def parallel_plot(df, color=None, palette=None):
     p.yaxis.visible = False
     p.y_range.start = 0
     p.y_range.end = 1
-    p.y_range.bounds = (-0.1, 1.1) # add a little padding around y axis
+    p.y_range.bounds = (-0.1, 1.1)  # add a little padding around y-axis
     p.xgrid.visible = False
     p.ygrid.visible = False
 
@@ -67,23 +67,16 @@ def parallel_plot(df, color=None, palette=None):
         p.add_layout(LinearAxis(fixed_location=index, y_range_name=col,
                                 ticker=fixedticks, formatter=tickformatter), 'right')
 
-    # create the data renderer ( MultiLine )
-    # specify selected and non selected style
+    # create the data renderer (MultiLine)
+    # specify selected and non-selected style
     non_selected_line_style = dict(line_color='grey', line_width=0.1, line_alpha=0.5)
 
     selected_line_style = dict(line_color={'field': 'color', 'transform': cmap}, line_width=1)
 
-    parallel_renderer = p.multi_line(
-        xs="xs", ys="ys", source=data_source, **non_selected_line_style)
+    parallel_renderer = p.multi_line(xs="xs", ys="ys", source=data_source, **non_selected_line_style)
 
-    # Specify selection style
-    selected_lines = MultiLine(**selected_line_style)
-
-    # Specify non selection style
-    nonselected_lines = MultiLine(**non_selected_line_style)
-
-    parallel_renderer.selection_glyph = selected_lines
-    parallel_renderer.nonselection_glyph = nonselected_lines
+    parallel_renderer.selection_glyph = parallel_renderer.glyph.clone(**selected_line_style)
+    parallel_renderer.nonselection_glyph = parallel_renderer.glyph.clone(**non_selected_line_style)
     p.y_range.start = p.y_range.bounds[0]
     p.y_range.end = p.y_range.bounds[1]
 
@@ -106,12 +99,13 @@ def parallel_plot(df, color=None, palette=None):
     p.toolbar.active_drag = selection_tool
     return p
 
+
 if __name__ == '__main__':
     from bokeh.io import show
     from bokeh.palettes import Viridis256
     del df['origin']
     del df['mfr']
     del df['name']
-    p = parallel_plot(df=df, color=df[df.columns[0]], palette=Viridis256)
+    plot = parallel_plot(df=df, color=df[df.columns[0]], palette=Viridis256)
     div = Div(text="Select up and down column grid lines to define filters. Double click a filter to reset it.")
-    show(column(div, p))
+    show(column(div, plot))
