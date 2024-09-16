5e7ad6c64ce47e13f6393b29b39862a5bcd0b33b
Mateusz Paprocki <mattpap@gmail.com>
2024-09-16 13:36:04 +0200

    Rewrite tests to avoid `any` type casts

--- bokehjs/test/unit/core/bokeh_events.ts
+++ bokehjs/test/unit/core/bokeh_events.ts
@@ -4,12 +4,13 @@ import {display} from "../_util"
 import {Serializer} from "@bokehjs/core/serialization"
 import type {BokehEvent} from "@bokehjs/core/bokeh_events"
 import {AxisClick, server_event, UserEvent} from "@bokehjs/core/bokeh_events"
-import {paint} from "@bokehjs/core/util/defer"
 import type {Model} from "@bokehjs/model"
 import {CategoricalAxis, CategoricalScale, FactorRange, Plot, Range1d, LinearAxis, LogAxis} from "@bokehjs/models"
 import type {MessageSent, Patch} from "@bokehjs/document"
 import {TextInput} from "@bokehjs/models/widgets"
 import {PlotActions, xy} from "../../interactive"
+import type {Point} from "../../interactive"
+import type {Side} from "@bokehjs/core/enums"
 
 @server_event("some_library.do_something")
 class DoSomething extends UserEvent {
@@ -66,8 +67,8 @@ describe("BokehEvent", () => {
 })
 
 describe("AxisClick event", () => {
-  for (const {side, pt} of [{side: "left", pt: xy(5, 200)}, {side: "right", pt: xy(395, 200)}, {side: "above", pt: xy(200, 5)}, {side: "below", pt: xy(200, 395)}]) {
-    it(`should support linear axes on side: ${side}`, async () => {
+  describe("should support linear axes", () => {
+    async function test(side: Side, pt: Point) {
       const plot = new Plot({
         width: 400,
         height: 400,
@@ -77,24 +78,28 @@ describe("AxisClick event", () => {
         y_range: new Range1d({start: 0, end: 10}),
       })
       const axis = new LinearAxis()
-      plot.add_layout(axis, side as any)
+      plot.add_layout(axis, side)
 
       const events: AxisClick[] = []
-      axis.on_event(AxisClick, (event: any) => events.push(event))
+      axis.on_event(AxisClick, (event) => events.push(event))
 
       const {view} = await display(plot)
 
       const actions = new PlotActions(view, {units: "screen"})
-      await paint()
       await actions.tap(pt)
 
       expect(events.length).to.be.equal(1)
       expect(events[0].value).to.be.equal(5)
-    })
-  }
+    }
 
-  for (const {side, pt} of [{side: "left", pt: xy(5, 200)}, {side: "right", pt: xy(395, 200)}, {side: "above", pt: xy(200, 5)}, {side: "below", pt: xy(200, 395)}]) {
-    it(`should support log axes on side: ${side}`, async () => {
+    it("on the left side",  async () => test("left", xy(5, 200)))
+    it("on the right side", async () => test("right", xy(395, 200)))
+    it("on the above side", async () => test("above", xy(200, 5)))
+    it("on the below side", async () => test("below", xy(200, 395)))
+  })
+
+  describe("should support log axes", () => {
+    async function test(side: Side, pt: Point) {
       const plot = new Plot({
         width: 400,
         height: 400,
@@ -104,24 +109,28 @@ describe("AxisClick event", () => {
         y_range: new Range1d({start: 1, end: 10000}),
       })
       const axis = new LogAxis()
-      plot.add_layout(axis, side as any)
+      plot.add_layout(axis, side)
 
       const events: AxisClick[] = []
-      axis.on_event(AxisClick, (event: any) => events.push(event))
+      axis.on_event(AxisClick, (event) => events.push(event))
 
       const {view} = await display(plot)
 
       const actions = new PlotActions(view, {units: "screen"})
-      await paint()
       await actions.tap(pt)
 
       expect(events.length).to.be.equal(1)
       expect(events[0].value).to.be.equal(2) // 10^2 == 100
-    })
-  }
+    }
 
-  for (const {side, pt} of [{side: "left", pt: xy(5, 200)}, {side: "right", pt: xy(395, 200)}, {side: "above", pt: xy(200, 5)}, {side: "below", pt: xy(200, 395)}]) {
-    it(`should support categorical axes on side: ${side}`, async () => {
+    it("on the left side",  async () => test("left", xy(5, 200)))
+    it("on the right side", async () => test("right", xy(395, 200)))
+    it("on the above side", async () => test("above", xy(200, 5)))
+    it("on the below side", async () => test("below", xy(200, 395)))
+  })
+
+  describe("should support categorical axes", () => {
+    async function test(side: Side, pt: Point) {
       const plot = new Plot({
         width: 400,
         height: 400,
@@ -133,19 +142,23 @@ describe("AxisClick event", () => {
         y_range: new FactorRange({factors: ["a", "b", "c", "d", "e"]}),
       })
       const axis = new CategoricalAxis()
-      plot.add_layout(axis, side as any)
+      plot.add_layout(axis, side)
 
       const events: AxisClick[] = []
-      axis.on_event(AxisClick, (event: any) => events.push(event))
+      axis.on_event(AxisClick, (event) => events.push(event))
 
       const {view} = await display(plot)
 
       const actions = new PlotActions(view, {units: "screen"})
-      await paint()
       await actions.tap(pt)
 
       expect(events.length).to.be.equal(1)
       expect(events[0].value).to.be.equal("c")
-    })
-  }
+    }
+
+    it("on the left side",  async () => test("left", xy(5, 200)))
+    it("on the right side", async () => test("right", xy(395, 200)))
+    it("on the above side", async () => test("above", xy(200, 5)))
+    it("on the below side", async () => test("below", xy(200, 395)))
+  })
 })
