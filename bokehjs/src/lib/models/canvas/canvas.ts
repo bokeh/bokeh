77191db2bfbc14203d05e51c80518e6ecb8f3106
Mateusz Paprocki <mattpap@gmail.com>
2024-10-27 22:01:19 +0100

    Add support for focus management to Plot

--- bokehjs/src/lib/models/canvas/canvas.ts
+++ bokehjs/src/lib/models/canvas/canvas.ts
@@ -96,7 +96,7 @@ export class CanvasView extends UIElementView {
     this.primary = this.create_layer()
     this.overlays = this.create_layer()
     this.overlays_el = div({class: canvas_css.layer})
-    this.events_el = div({class: [canvas_css.layer, canvas_css.events]})
+    this.events_el = div({class: [canvas_css.layer, canvas_css.events], tabIndex: 0})
 
     this.ui_event_bus = new UIEventBus(this)
   }
