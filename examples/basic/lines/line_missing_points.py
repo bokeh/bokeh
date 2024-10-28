e807a96f588cfdeed6cd82f888e062d0accb1d8a
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-10-28 17:21:28 +0100

    show how None behaves

--- examples/basic/lines/line_missing_points.py
+++ examples/basic/lines/line_missing_points.py
@@ -6,5 +6,8 @@ p = figure(width=400, height=400)
 
 # add a line renderer with NaN values
 p.line([1, 2, 3, nan, 4, 5], [6, 7, 2, 4, 4, 5], line_width=2)
+# don't use native None in a line renderer, because it will be drawn as 0
+p.line([1, 2, 3, None, 4, 5], [6, 7, 2, 4, 4, 5], line_width=2,
+       line_dash="dashed", color="red")
 
 show(p)
