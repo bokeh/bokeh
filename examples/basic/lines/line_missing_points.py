c0f7a8b4a0ac3de1eee1bb4b835855f766dfb46c
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2024-10-28 18:50:25 +0100

    apply suggestions

--- examples/basic/lines/line_missing_points.py
+++ examples/basic/lines/line_missing_points.py
@@ -5,9 +5,11 @@ from bokeh.plotting import figure, show
 p = figure(width=400, height=400)
 
 # add a line renderer with NaN values
-p.line([1, 2, 3, nan, 4, 5], [6, 7, 2, 4, 4, 5], line_width=2)
+p.line([1, 2, 3, nan, 4, 5], [6, 7, 2, 4, 4, 5], line_width=8,
+       legend_label="line with NaN", alpha=0.5)
+
 # don't use native None in a line renderer, because it will be drawn as 0
 p.line([1, 2, 3, None, 4, 5], [6, 7, 2, 4, 4, 5], line_width=2,
-       line_dash="dashed", color="red")
+       legend_label="line with None (BAD)", line_dash="dashed", color="red")
 
 show(p)
