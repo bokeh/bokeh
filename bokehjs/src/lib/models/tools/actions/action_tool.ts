ded23a668d46e8122ae461c68388031213656d56
Mateusz Paprocki <mattpap@gmail.com>
2024-11-07 11:14:37 +0100

    Allow ClickButtons to have state

--- bokehjs/src/lib/models/tools/actions/action_tool.ts
+++ bokehjs/src/lib/models/tools/actions/action_tool.ts
@@ -1,4 +1,5 @@
 import {Tool, ToolView} from "../tool"
+import type {ToolButton} from "../tool_button"
 import {ClickButton} from "../click_button"
 import type {LayoutDOMView} from "../../layouts/layout_dom"
 import {Signal} from "core/signaling"
@@ -33,7 +34,7 @@ export abstract class ActionTool extends Tool {
 
   do = new Signal<string | undefined, this>(this, "do")
 
-  override tool_button(): ClickButton {
+  override tool_button(): ToolButton {
     return new ClickButton({tool: this})
   }
 }
--- bokehjs/src/lib/models/tools/click_button.ts
+++ bokehjs/src/lib/models/tools/click_button.ts
@@ -1,11 +1,11 @@
-import {ToolButton, ToolButtonView} from "./tool_button"
+import {OnOffButton, OnOffButtonView} from "./on_off_button"
 import type {ActionTool} from "./actions/action_tool"
 import type * as p from "core/properties"
 
-export class ClickButtonView extends ToolButtonView {
+export class ClickButtonView extends OnOffButtonView {
   declare model: ClickButton
 
-  protected _clicked(): void {
+  protected override _clicked(): void {
     this.model.tool.do.emit(undefined)
   }
 }
@@ -13,7 +13,7 @@ export class ClickButtonView extends ToolButtonView {
 export namespace ClickButton {
   export type Attrs = p.AttrsOf<Props>
 
-  export type Props = ToolButton.Props & {
+  export type Props = OnOffButton.Props & {
     tool: p.Property<ActionTool>
   }
 }
@@ -22,7 +22,7 @@ export interface ClickButton extends ClickButton.Attrs {
   tool: ActionTool
 }
 
-export class ClickButton extends ToolButton {
+export class ClickButton extends OnOffButton {
   declare properties: ClickButton.Props
   declare __view_type__: ClickButtonView
 
