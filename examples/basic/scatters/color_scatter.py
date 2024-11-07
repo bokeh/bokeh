53a20b30e924f3b0a9b891b83c3b214fb65867d5
Mateusz Paprocki <mattpap@gmail.com>
2024-11-07 13:02:09 +0100

    Add fullscreen tool to color_scatter.py

--- examples/basic/scatters/color_scatter.py
+++ examples/basic/scatters/color_scatter.py
@@ -17,7 +17,7 @@ y = np.random.random(size=N) * 100
 radii = np.random.random(size=N) * 1.5
 colors = np.array([(r, g, 150) for r, g in zip(50+2*x, 30+2*y)], dtype="uint8")
 
-TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,examine,help"
+TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,examine,fullscreen,help"
 
 p = figure(tools=TOOLS)
 
