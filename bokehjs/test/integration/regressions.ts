5b22f260924daaaec9acaaaf11bad1c7f5926dff
muendlein <max.muendlein@gmail.com>
2025-02-12 22:38:17 +0100

    added regression test

--- bokehjs/test/integration/regressions.ts
+++ bokehjs/test/integration/regressions.ts
@@ -4326,4 +4326,22 @@ describe("Bug", () => {
       await view.ready
     })
   })
+
+  describe("in issue #14280", () => {
+    it("triggers JS error when adding tile without defining range", async () => {
+      const osm = new TileRenderer({tile_source: osm_source.clone()})
+
+      const p0 = fig([300, 200], {
+        x_axis_type: "mercator",
+        y_axis_type: "mercator",
+        sizing_mode: "stretch_height",
+      })
+
+      const {view} = await display(p0)
+
+      p0.renderers = [osm]
+
+      await view.ready
+    })
+  })
 })
