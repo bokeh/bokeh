a35fa6786a9417983eb616feecfc4c99ca0d7c1f
Bryan Van de Ven <bryan@bokeh.org>
2025-03-05 09:45:07 -0800

    fix enum comparison

--- bokehjs/src/lib/models/plots/plot_canvas.ts
+++ bokehjs/src/lib/models/plots/plot_canvas.ts
@@ -1130,7 +1130,7 @@ export class PlotView extends LayoutDOMView implements Paintable {
       }
     }
 
-    if (this._range_manager.invalidate_dataranges || this.model.window_axis != null) {
+    if (this._range_manager.invalidate_dataranges || this.model.window_axis != "none") {
       this._range_manager.update_dataranges()
       this._invalidate_layout_if_needed()
     }
