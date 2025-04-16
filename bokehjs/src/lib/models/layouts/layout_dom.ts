e76e09aed277f608f7a4588f165657cddd3bb1e5
Philipp Rudiger <prudiger@anaconda.com>
2025-04-16 13:20:44 +0200

    Implement LayoutDOM.update_children without explicitly removing nodes

--- bokehjs/src/lib/models/layouts/layout_dom.ts
+++ bokehjs/src/lib/models/layouts/layout_dom.ts
@@ -153,28 +153,50 @@ export abstract class LayoutDOMView extends PaneView {
   protected _update_children(): void {}
 
   async update_children(): Promise<void> {
+    let current_views = [...this.child_views]
     const created = await this.build_child_views()
     const created_children = new Set(created)
 
-    // First remove and then either reattach existing elements or render and
-    // attach new elements, so that the order of children is consistent, while
-    // avoiding expensive re-rendering of existing views.
-    for (const child_view of this.child_views) {
-      child_view.el.remove()
-    }
+    // The newly generated child_views are added to the shadow_el one-by-one
+    // In order to determine the correct ordering we compute the existing
+    // order and then either insert each item before an existing node or append it.
+    // This ensures correct ordering without removing and then re-adding DOM nodes
+    // which can cause issues for certain virtual DOM implementations (e.g. React).
+    const current_elements = Array.from(this.shadow_el.children).filter(el => {
+      return this.child_views.some(view => view.el === el)
+    })
+    current_views = current_views.filter(view => !current_elements.includes(view.el))
 
-    for (const child_view of this.child_views) {
+    const added = new Set()
+    for (let i = 0; i < this.child_views.length; i++) {
+      const child_view = this.child_views[i]
       const is_new = created_children.has(child_view)
+      const target = child_view.rendering_target()
 
-      const target = child_view.rendering_target() ?? this.shadow_el
       if (is_new) {
-        child_view.render_to(target)
-      } else {
-        target.append(child_view.el)
+        child_view.render()
       }
-    }
-    this.r_after_render()
 
+      if (target != null) {
+        if (!target.contains(child_view.el)) {
+          if (child_view.el.parentNode) {
+            child_view.el.remove()
+          }
+          target.append(child_view.el)
+        }
+      } else {
+	// Compute insertion point for view in previous ordering
+        const next_view = current_views.find(view => current_elements.includes(view.el) && !added.has(view))
+        if (next_view) {
+          this.shadow_el.insertBefore(child_view.el, next_view.el)
+        } else {
+          this.shadow_el.appendChild(child_view.el)
+        }
+      }
+      added.add(child_view)
+    }
+
+    this.r_after_render()
     this._update_children()
     this.invalidate_layout()
   }
