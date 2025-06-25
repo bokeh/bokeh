d59cc8f1b31e7bf224490e004b2d30e9c9fc7ca3
Mateusz Paprocki <mattpap@gmail.com>
2025-06-26 00:32:36 +0200

    Add examples/basic/annotations/size_bar.py

---
+++ examples/basic/annotations/size_bar.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from bokeh.models import SizeBar
+from bokeh.plotting import figure, show
+
+N = 100
+x = np.random.random(size=N) * 100
+y = np.random.random(size=N) * 100
+radii = np.random.random(size=N) * 10
+colors = np.array([(r, g, 150) for r, g in zip(50 + 2*x, 30 + 2*y)], dtype=np.uint8)
+
+p = figure()
+cr = p.circle(x, y, radius=radii, fill_color=colors, fill_alpha=0.6, line_color=None)
+
+size_bar = SizeBar(renderer=cr, title="SizeBar component", orientation="horizontal")
+p.add_layout(size_bar, "below")
+
+show(p)
