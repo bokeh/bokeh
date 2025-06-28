65209525f4318f7865bac325f2285cc7b7cb448c
syhhyl <syhhyl926@126.com>
2025-06-29 00:53:26 +0800

    fix lorenz.py

--- examples/basic/lines/lorenz.py
+++ examples/basic/lines/lorenz.py
@@ -19,11 +19,11 @@ rho = 28
 beta = 8.0/3
 theta = 3 * np.pi / 4
 
-def lorenz(xyz, t):
+def lorenz(xyz: tuple[float, float, float], t: float) -> list[float]:
     x, y, z = xyz
     x_dot = sigma * (y - x)
     y_dot = x * rho - x * z - y
-    z_dot = x * y - beta* z
+    z_dot = x * y - beta * z
     return [x_dot, y_dot, z_dot]
 
 initial = (-10, -7, 35)
@@ -40,7 +40,11 @@ colors = ["#C6DBEF", "#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#08519C", "#08
 
 p = figure(title="Lorenz attractor example", background_fill_color="#fafafa")
 
-p.multi_line(np.array_split(xprime, 7), np.array_split(z, 7),
-             line_color=colors, line_alpha=0.8, line_width=1.5)
+p.multi_line(
+    np.array_split(xprime, 7), #type: ignore
+    np.array_split(z, 7),      #type: ignore
+    line_color=colors,
+    line_alpha=0.8,
+    line_width=1.5)
 
 show(p)
