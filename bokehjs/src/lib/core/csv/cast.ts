f673933acd478be90fef5fe4b413df3d1166cef8
gabalafou <gabriel@fouasnon.com>
2025-07-29 20:12:50 +0200

    use pretty module to stringify object types

--- bokehjs/src/lib/core/csv/cast.ts
+++ bokehjs/src/lib/core/csv/cast.ts
@@ -1,10 +1,12 @@
+import {to_string} from "core/util/pretty"
+
 // The default function for casting data to strings for CSV fields
 export function default_cast(field: unknown): string {
   if (field instanceof Date) {
     return field.toISOString()
   } else if (typeof field === "object") {
     // includes null => 'null'
-    return JSON.stringify(field)
+    return to_string(field)
   } else {
     return String(field)
   }
