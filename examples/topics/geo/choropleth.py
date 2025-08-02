9f0bc8154e89c725f3bd36acd77ee4996b7fd1ab
Moritz Schreiber <mosc9575@gmail.com>
2025-08-02 18:09:17 +0200

    remove save in a comment

--- examples/topics/geo/choropleth.py
+++ examples/topics/geo/choropleth.py
@@ -50,4 +50,4 @@ p.patches(county_xs, county_ys,
 p.patches(state_xs, state_ys, fill_alpha=0.0,
           line_color="#884444", line_width=2, line_alpha=0.3)
 
-show(p)  # Change to save(p) to save but not show the HTML file
+show(p)
