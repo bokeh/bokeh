f0a9385f2688d72ef7bf34b8b0b36f9e5f15d347
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2025-08-04 00:15:48 +0200

    use show instead of save in examples (#14581)
    * replace save with show in examples
    
    * remove save in a comment
    
    * revert changes in css_layouts.py

--- examples/basic/annotations/legend_name.py
+++ examples/basic/annotations/legend_name.py
@@ -1,5 +1,5 @@
 from bokeh.models import Legend, LinearAxis, Range1d
-from bokeh.plotting import figure, save
+from bokeh.plotting import figure, show
 
 p = figure(y_range=Range1d(0.9, 3.1), toolbar_location="above")
 
@@ -12,4 +12,4 @@ p.add_layout(LinearAxis(y_range_name="foo"), 'right')
 p.line([1, 2, 3], [1, 2, 3], color='blue', legend_label="blue line")
 p.line([1, 2, 3], [0, -2, -3], color='red', legend_label="red line", legend_name="right", y_range_name="foo")
 
-save(p)
+show(p)
