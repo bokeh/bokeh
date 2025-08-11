99fd88b6b875b5bb559465574f983067567a84a5
Bryan Van de Ven <bryan@bokeh.org>
2025-08-10 22:52:22 -0700

    update Flatbush and remove custom Flatbush modifications

--- bokehjs/src/lib/core/util/spatial.ts
+++ bokehjs/src/lib/core/util/spatial.ts
@@ -4,113 +4,12 @@ import type {Rect} from "../types"
 import {Indices} from "../types"
 import {empty} from "./bbox"
 
-function upperBound(value: number, arr: ArrayLike<number>): number {
-  let i = 0
-  let j = arr.length - 1
-  while (i < j) {
-    const m = (i + j) >> 1
-    if (arr[m] > value) {
-      j = m
-    } else {
-      i = m + 1
-    }
-  }
-  return arr[i]
-}
-
-class _FlatBush extends FlatBush {
-
-  search_apply(minX: number, minY: number, maxX: number, maxY: number, nodeFunction: (index: number, node: Rect) => void): void {
-    if (this._pos !== this._boxes.length) {
-      throw new Error("Data not yet indexed - call index.finish().")
-    }
-
-    let nodeIndex: number | undefined = this._boxes.length - 4
-    const queue = []
-
-    while (nodeIndex !== undefined) {
-      // find the end index of the node
-      const end = Math.min(nodeIndex + this.nodeSize * 4, upperBound(nodeIndex, this._levelBounds))
-
-      // search through child nodes
-      for (let pos: number = nodeIndex; pos < end; pos += 4) {
-        const nodeMinX = this._boxes[pos + 0]
-        const nodeMinY = this._boxes[pos + 1]
-        const nodeMaxX = this._boxes[pos + 2]
-        const nodeMaxY = this._boxes[pos + 3]
-
-        if (maxX < nodeMinX || maxY < nodeMinY || minX > nodeMaxX || minY > nodeMaxY) {
-          continue
-        }
-
-        if (minX <= nodeMinX && minY <= nodeMinY && maxX >= nodeMaxX && maxY >= nodeMaxY) {
-          let pos_start = pos
-          let pos_end = pos
-
-          // depth search while not leaf
-          while (pos_start >= this.numItems * 4) {
-            pos_start = this._indices[pos_start >> 2] | 0
-            const pos_end_start = this._indices[pos_end >> 2] | 0
-            pos_end = Math.min(pos_end_start + this.nodeSize * 4, upperBound(pos_end_start, this._levelBounds))-4
-          }
-
-          for (let leaf_pos: number = pos_start; leaf_pos <= pos_end; leaf_pos += 4) {
-            const nodeMinX = this._boxes[leaf_pos + 0]
-            const nodeMinY = this._boxes[leaf_pos + 1]
-            const nodeMaxX = this._boxes[leaf_pos + 2]
-            const nodeMaxY = this._boxes[leaf_pos + 3]
-            const leaf_index = this._indices[leaf_pos >> 2]
-            nodeFunction(leaf_index, {x0: nodeMinX, y0: nodeMinY, x1: nodeMaxX, y1: nodeMaxY})
-          }
-          continue
-        }
-
-        const index = this._indices[pos >> 2] | 0
-
-        if (nodeIndex < this.numItems * 4) {
-          nodeFunction(index, {x0: nodeMinX, y0: nodeMinY, x1: nodeMaxX, y1: nodeMaxY})
-        } else {
-          queue.push(index) // node; add it to the search queue
-        }
-
-      }
-
-      nodeIndex = queue.pop()
-    }
-  }
-
-  search_indices(minX: number, minY: number, maxX: number, maxY: number): Indices {
-    const result = new Indices(this.numItems)
-    this.search_apply(minX, minY, maxX, maxY, (index) => result.set_unchecked(index))
-    return result
-  }
-
-  search_bounds(minX: number, minY: number, maxX: number, maxY: number): Rect {
-    const result = empty()
-    this.search_apply(minX, minY, maxX, maxY, (_index, node) => {
-      if (node.x0 >= minX && node.x0 < result.x0) {
-        result.x0 = node.x0
-      }
-      if (node.x1 <= maxX && node.x1 > result.x1) {
-        result.x1 = node.x1
-      }
-      if (node.y0 >= minY && node.y0 < result.y0) {
-        result.y0 = node.y0
-      }
-      if (node.y1 <= maxY && node.y1 > result.y1) {
-        result.y1 = node.y1
-      }
-    })
-    return result
-  }
-}
-
 export class SpatialIndex {
-  private readonly index: _FlatBush | null = null
+  private readonly index: FlatBush | null = null
 
   constructor(size: number) {
     if (size > 0) {
-      this.index = new _FlatBush(size)
+      this.index = new FlatBush(size)
     }
   }
 
@@ -161,18 +60,40 @@ export class SpatialIndex {
   indices(rect: Rect): Indices {
     if (this.index == null) {
       return new Indices(0)
-    } else {
-      const {x0, y0, x1, y1} = this._normalize(rect)
-      return this.index.search_indices(x0, y0, x1, y1)
     }
+
+    const {x0, y0, x1, y1} = this._normalize(rect)
+    const result = new Indices(this.index.numItems)
+    this.index.search(x0, y0, x1, y1, (index) => {
+      result.set_unchecked(index)
+      return false
+    })
+    return result
+
   }
 
   bounds(rect: Rect): Rect {
     if (this.index == null) {
       return empty()
-    } else {
-      const {x0, y0, x1, y1} = this._normalize(rect)
-      return this.index.search_bounds(x0, y0, x1, y1)
     }
+
+    const {x0, y0, x1, y1} = this._normalize(rect)
+    const result = empty()
+    this.index.search(x0, y0, x1, y1, (_, node_x0, node_y0, node_x1, node_y1) => {
+      if (node_x0 >= x0 && node_x0 < result.x0) {
+        result.x0 = node_x0
+      }
+      if (node_x1 <= x1 && node_x1 > result.x1) {
+        result.x1 = node_x1
+      }
+      if (node_y0 >= y0 && node_y0 < result.y0) {
+        result.y0 = node_y0
+      }
+      if (node_y1 <= y1 && node_y1 > result.y1) {
+        result.y1 = node_y1
+      }
+      return false
+    })
+    return result
   }
 }
