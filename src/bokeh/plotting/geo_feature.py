c4bc72ba3dec28eb99b8a9713857e770a66487a7
Moritz Schreiber <mosc9575@gmail.com>
2025-11-05 18:03:50 +0100

    add wrapper function "requires_cartopy"

--- src/bokeh/plotting/geo_feature.py
+++ src/bokeh/plotting/geo_feature.py
@@ -1,18 +1,28 @@
-import sys
-if sys.modules.get("cartopy"):
-    import cartopy.feature as cfeature
-    import cartopy.crs as ccrs
-else:
-    raise ImportError("Missing optional dependency 'Cartopy'. ")
 import numpy as np
-from typing import Iterable
+from functools import wraps
+from bokeh.util.dependencies import import_optional
+
+cartopy = import_optional("cartopy")
 
 
+def requires_cartopy(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if cartopy is None:
+            raise ModuleNotFoundError(
+                f"Missing optional dependency 'cartopy' for {type(f).__name__!r}. "
+                "To use this function, please install 'cartopy'.",
+            )
+        return f(*args, **kwargs)
+    return wrapper
+
+
+@requires_cartopy
 def _collect_line_geometries(projection, geometries_collection):
     xs = []
     ys = []
     for geometry in geometries_collection.geometries():
-        lines = projection.project_geometry(geometry, src_crs=ccrs.PlateCarree())
+        lines = projection.project_geometry(geometry, src_crs=cartopy.crs.PlateCarree())
         for line in lines.geoms:
             x, y = np.array(line.xy)
             xs.append(x)
@@ -20,17 +30,18 @@ def _collect_line_geometries(projection, geometries_collection):
     return xs, ys
 
 
+@requires_cartopy
 def _collect_polygon_geometries(projection, geometries_collection, **kwargs):
     xs = []
     ys = []
     color = kwargs.get("color")
-    color_selection = isinstance(color, Iterable) and not isinstance(color, str)
+    color_selection = isinstance(color, (list, tuple))
     selected_colors = []
     if color_selection:
         n_color = len(color)
 
     for i, geometry in enumerate(geometries_collection.geometries()):
-        projected_geometrie = projection.project_geometry(geometry, src_crs=ccrs.PlateCarree())
+        projected_geometrie = projection.project_geometry(geometry, src_crs=cartopy.crs.PlateCarree())
         for geom in projected_geometrie.geoms:
             x, y = np.array(geom.exterior.xy)
             x_geom = [x]
@@ -58,12 +69,14 @@ def _collect_lines_from_ploygons(polygon_xs, polygon_ys):
     return line_xs, line_ys
 
 
+@requires_cartopy
 def add_line_geometries(p, projection, geometries_collection, **line_kwargs):
     xs, ys = _collect_line_geometries(projection, geometries_collection)
     p.multi_line(xs, ys, **line_kwargs)
     return p
 
 
+@requires_cartopy
 def add_polygon_geometries(p, projection, geometries_collection, **poly_kwargs):
     draw_border = poly_kwargs.pop("draw_polygon_border", False)
     border_color= poly_kwargs.pop("polygon_border_color", "black")
@@ -75,42 +88,49 @@ def add_polygon_geometries(p, projection, geometries_collection, **poly_kwargs):
     return p
 
 
+@requires_cartopy
 def add_borders(p, projection, scale="110m", **line_kwargs):
     line_kwargs.setdefault("color", "lightgray")
-    borders = cfeature.BORDERS.with_scale(scale)
+    borders = cartopy.feature.BORDERS.with_scale(scale)
     return add_line_geometries(p, projection, borders, **line_kwargs)
 
 
+@requires_cartopy
 def add_coastlines(p, projection, scale="110m", **line_kwargs):
     line_kwargs.setdefault("color", "black")
-    coastline = cfeature.COASTLINE.with_scale(scale)
+    coastline = cartopy.feature.COASTLINE.with_scale(scale)
     return add_line_geometries(p, projection, coastline, **line_kwargs)
 
 
+@requires_cartopy
 def add_land(p, projection, scale="110m", **poly_kwargs):
     poly_kwargs.setdefault("color", "#EFEFDB")
-    land = cfeature.LAND.with_scale(scale)
+    land = cartopy.feature.LAND.with_scale(scale)
     return add_polygon_geometries(p, projection, land, **poly_kwargs)
 
 
+@requires_cartopy
 def add_lakes(p, projection, scale="110m", **poly_kwargs):
     poly_kwargs.setdefault("color", "#9FDBF3")
-    lakes = cfeature.LAKES.with_scale(scale)
+    lakes = cartopy.feature.LAKES.with_scale(scale)
     return add_polygon_geometries(p, projection, lakes, **poly_kwargs)
 
 
+@requires_cartopy
 def add_ocean(p, projection, scale="110m", **poly_kwargs):
     poly_kwargs.setdefault("color", "#9FDBF3")
-    ocean = cfeature.OCEAN.with_scale(scale)
+    ocean = cartopy.feature.OCEAN.with_scale(scale)
     return add_polygon_geometries(p, projection, ocean, **poly_kwargs)
 
 
+@requires_cartopy
 def add_rivers(p, projection, scale="110m", **line_kwargs):
     line_kwargs.setdefault("color", "#9FDBF3")
-    rivers = cfeature.RIVERS.with_scale(scale)
+    rivers = cartopy.feature.RIVERS.with_scale(scale)
     return add_line_geometries(p, projection, rivers, **line_kwargs)
 
 
+@requires_cartopy
 def add_projection_boundary(p, projection, **line_kwargs):
     line_kwargs.setdefault("color", "black")
     x, y = np.array(projection.boundary.xy)
@@ -118,12 +138,14 @@ def add_projection_boundary(p, projection, **line_kwargs):
     return p
 
 
+@requires_cartopy
 def add_provinces(p, projection, scale="110m", **line_kwargs):
     line_kwargs.setdefault("color", "lightgray")
-    provinces = cfeature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', scale)
+    provinces = cartopy.feature.NaturalEarthFeature('cultural', 'admin_1_states_provinces_lines', scale)
     return add_line_geometries(p, projection, provinces, **line_kwargs)
 
 
+@requires_cartopy
 def add_states(p, projection, scale="110m", **poly_kwargs):
-    states = cfeature.STATES.with_scale(scale)
+    states = cartopy.feature.STATES.with_scale(scale)
     return add_polygon_geometries(p, projection, states, **poly_kwargs)
