cbba807832523115ec7e1e0508ebed118edd06a6
Saurabh Misra <misra.saurabh1@gmail.com>
2025-11-11 12:05:10 -0800

    cleanup
    Signed-off-by: Saurabh Misra <misra.saurabh1@gmail.com>

--- src/bokeh/plotting/_tools.py
+++ src/bokeh/plotting/_tools.py
@@ -11,7 +11,6 @@
 from __future__ import annotations
 
 import logging # isort:skip
-
 log = logging.getLogger(__name__)
 
 #-----------------------------------------------------------------------------
