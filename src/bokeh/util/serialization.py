8463facdc3e826e4a058a932da9350a6ce3f2aa4
Kevin Turcios <turcioskevinr@gmail.com>
2025-12-03 19:22:30 -0500

    Use datetime.combine() for faster date conversion
    Resolves #14747

--- src/bokeh/util/serialization.py
+++ src/bokeh/util/serialization.py
@@ -144,8 +144,7 @@ def convert_date_to_datetime(obj: dt.date) -> float:
         datetime
 
     '''
-    dt_obj = dt.datetime(obj.year, obj.month, obj.day, tzinfo=dt.timezone.utc)
-    return (dt_obj - DT_EPOCH).total_seconds() * 1000
+    return (dt.datetime.combine(obj, dt.time(), tzinfo=dt.timezone.utc) - DT_EPOCH).total_seconds() * 1000
 
 def convert_timedelta_type(obj: dt.timedelta | np.timedelta64) -> float:
     ''' Convert any recognized timedelta value to floating point absolute
