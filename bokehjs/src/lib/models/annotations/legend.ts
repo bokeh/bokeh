d874d302dcf8dfe78468f8daa8e275424f841fb5
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-12-10 13:27:36 -0500

    Use background_fill color if available for bottom background

--- bokehjs/src/lib/models/annotations/legend.ts
+++ bokehjs/src/lib/models/annotations/legend.ts
@@ -391,6 +391,7 @@ export class LegendView extends AnnotationView {
       const {color} = this.visuals.background_fill.computed_values()
       this.style.append(`
       :host {
+        --background-color: ${color};
         background-color: ${color};
       }
       `)
@@ -428,7 +429,7 @@ export class LegendView extends AnnotationView {
              linear-gradient(to right, ${color} ${dash[0]}px, transparent ${dash[0]}px) left bottom/var(--border-line-full-length) ${width}px repeat-x,
              linear-gradient(to bottom, ${color} ${dash[0]}px, transparent ${dash[0]}px) right top/${width}px var(--border-line-full-length) repeat-y,
              linear-gradient(to bottom, ${color} ${dash[0]}px, transparent ${dash[0]}px) left top/${width}px var(--border-line-full-length) repeat-y ${extra_patterns.length > 0 ? `${extra_patterns}` : "" },
-             var(--inverted-color);
+             var(--background-color, --inverted-color);
         }
         `)
       // Empty dash array (solid border) or string case
