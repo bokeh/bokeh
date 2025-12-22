7b72e229d8e42580980a6bb3f4b967246e354780
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-12-22 11:06:12 -0500

    Only change dashed and dotted patterns array equivalent values

--- bokehjs/src/lib/core/visuals/line.ts
+++ bokehjs/src/lib/core/visuals/line.ts
@@ -15,9 +15,9 @@ export function resolve_line_dash(line_dash: LineDash | string | number[]): numb
     switch (line_dash) {
       case "solid":   return []
       case "dashed":  return [3, 2]
-      case "dotted":  return [1, 1]
-      case "dotdash": return [1, 1, 3, 2]
-      case "dashdot": return [3, 2, 1, 1]
+      case "dotted":  return [2, 1]
+      case "dotdash": return [2, 4, 6, 4]
+      case "dashdot": return [6, 4, 2, 4]
       default:
         return line_dash.split(" ").map(Number).filter(isInteger)
     }
