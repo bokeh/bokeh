5a8d998c9f48414fa409bec4796b032f7139bfa1
Mateusz Paprocki <mattpap@gmail.com>
2026-01-25 14:18:01 +0100

    Use `UInt8Array.{to,from}Base64()` if available

--- bokehjs/src/lib/es.d.ts
+++ bokehjs/src/lib/es.d.ts
@@ -39,3 +39,37 @@ interface Float64Array {
 interface Element {
   webkitRequestFullscreen(options?: FullscreenOptions): Promise<void>
 }
+
+// from lib.esnext.d.ts (needs TS 6/7)
+interface Uint8Array<TArrayBuffer extends ArrayBufferLike> {
+  /**
+   * Converts the `Uint8Array` to a base64-encoded string.
+   * @param options If provided, sets the alphabet and padding behavior used.
+   * @returns A base64-encoded string.
+   */
+  toBase64(
+    options?: {
+      alphabet?: "base64" | "base64url" | undefined
+      omitPadding?: boolean | undefined
+    },
+  ): string
+}
+
+// from lib.esnext.d.ts (needs TS 6/7)
+interface Uint8ArrayConstructor {
+  /**
+   * Creates a new `Uint8Array` from a base64-encoded string.
+   * @param string The base64-encoded string.
+   * @param options If provided, specifies the alphabet and handling of the last chunk.
+   * @returns A new `Uint8Array` instance.
+   * @throws {SyntaxError} If the input string contains characters outside the specified alphabet, or if the last
+   * chunk is inconsistent with the `lastChunkHandling` option.
+   */
+  fromBase64(
+    string: string,
+    options?: {
+      alphabet?: "base64" | "base64url" | undefined
+      lastChunkHandling?: "loose" | "strict" | "stop-before-partial" | undefined
+    },
+  ): Uint8Array<ArrayBuffer>
+}
