5ecd07ec1f2dfec5f149db431cdca0b96548badd
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2026-02-06 15:00:31 +0100

    don't add a BBox if axis has fixed location

--- bokehjs/src/lib/models/axes/axis.ts
+++ bokehjs/src/lib/models/axes/axis.ts
@@ -71,10 +71,10 @@ export abstract class AxisView extends GuideRendererView {
   /*private*/ _major_label_views: Map<string | number, BaseTextView> = new Map()
 
   override get bbox(): BBox {
-    // TODO Fixed axes should not participate in layout at all.
     if (this.layout != null && this.model.fixed_location == null) {
       return this.layout.bbox
-    } else if (this.is_renderable) {
+    } else if (this.is_renderable && this.model.fixed_location == null) {
+      // Fixed axes return an empty BBox and do not effect the layout.
       const {extents} = this
       const depth = Math.round(extents.tick + extents.tick_label + extents.axis_label)
 
--- bokehjs/test/integration/axes.ts
+++ bokehjs/test/integration/axes.ts
@@ -1,4 +1,4 @@
-import {display, row, with_internal} from "./_util"
+import {display, fig, row, with_internal} from "./_util"
 
 import type {Axis} from "@bokehjs/models"
 import {
@@ -14,6 +14,7 @@ import {
   Plot,
   Range1d,
   TeX,
+  Toolbar,
 } from "@bokehjs/models"
 
 import type {Factor} from "@bokehjs/models/ranges/factor_range"
@@ -263,6 +264,21 @@ import {radians} from "@bokehjs/core/util/math"
             ]),
           })
         })
+      })
+    })
+
+    describe("with fixed location", () => {
+      it("should be added without effecting the position of the toolbar", async () => {
+        const p = fig([300, 300], {toolbar_location: "right"})
+        p.scatter([1, 2, 3], [1, 2, 3])
+        p.extra_x_ranges = {["x"]: new Range1d({start: 0.9, end: 3.1})}
+        p.extra_y_ranges = {["y"]: new Range1d({start: 0.9, end: 3.1})}
+        p.add_layout(new LinearAxis({x_range_name: "x", fixed_location: 1.5}), "below")
+        p.add_layout(new LinearAxis({y_range_name: "y", fixed_location: 2.5}), "right")
+        p.add_layout(new Toolbar({tools: []}), "above")
+        p.add_layout(new Toolbar({tools: []}), "left")
+        p.add_layout(new Toolbar({tools: []}), "below")
+        await display(p)
       })
     })
   })
