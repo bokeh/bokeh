fbe08ae592d371ff3fd7863368f77aad5afdafc3
Mateusz Paprocki <mattpap@gmail.com>
2026-02-17 22:08:11 +0100

    Improve the example

--- examples/basic/layouts/tabs_scrollable.py
+++ examples/basic/layouts/tabs_scrollable.py
@@ -1,23 +1,27 @@
+""" Example demonstrating scrollable tabs when there are many tabs.
 """
-Example demonstrating scrollable tabs when there are many tabs.
+from math import ceil
 
-This addresses issue #14417 - restoring scroll functionality for large number of tabs.
-"""
 from bokeh.models import TabPanel
 from bokeh.models.layouts import Tabs
-from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import Inferno256
+from bokeh.plotting import figure, show
 
-output_file("tabs_scrollable.html")
+N = 20
+step = ceil(len(Inferno256)/N)
 
 tab_panels = []
 
-for i in range(20):
+for i in range(N):
     p = figure(width=400, height=400, title=f"Plot {i+1}")
-    p.scatter([1, 2, 3, 4, 5], [i+1, i+2, i+3, i+4, i+5],
-              size=10, color="navy", alpha=0.5, marker="circle")
+    p.scatter(
+        [1, 2, 3, 4, 5], [i+1, i+2, i+3, i+4, i+5],
+        size=20, color=Inferno256[(N-1-i)*step], alpha=0.5, marker="circle",
+    )
+
     tab = TabPanel(child=p, title=f"Tab {i+1}")
     tab_panels.append(tab)
 
-tabs = Tabs(tabs=tab_panels)
+tabs = Tabs(tabs=tab_panels, width=600, resizable=True)
 
 show(tabs)
