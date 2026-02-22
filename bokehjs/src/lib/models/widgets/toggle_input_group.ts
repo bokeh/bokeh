63f417536161abc42a262b3c9538425d2831be19
Mateusz Paprocki <mattpap@gmail.com>
2026-02-22 15:40:16 +0100

    Don't inherit from Control in ToggleInputGroup

--- bokehjs/src/lib/models/widgets/toggle_input_group.ts
+++ bokehjs/src/lib/models/widgets/toggle_input_group.ts
@@ -1,14 +1,12 @@
-import {Control, ControlView} from "./control"
+import {Widget, WidgetView} from "./widget"
 import type {StyleSheetLike} from "core/dom"
 import type * as p from "core/properties"
 import inputs_css from "styles/widgets/inputs.css"
 import checkbox_css from "styles/widgets/checkbox.css"
 
-export abstract class ToggleInputGroupView extends ControlView {
+export abstract class ToggleInputGroupView extends WidgetView {
   declare readonly model: ToggleInputGroup
   declare readonly signals: p.SignalsOf<ToggleInputGroup.Props>
-
-  *controls() {}
 
   override stylesheets(): StyleSheetLike[] {
     return [...super.stylesheets(), inputs_css, checkbox_css]
@@ -18,7 +16,7 @@ export abstract class ToggleInputGroupView extends ControlView {
 export namespace ToggleInputGroup {
   export type Attrs = p.AttrsOf<Props>
 
-  export type Props = Control.Props & {
+  export type Props = Widget.Props & {
     labels: p.Property<string[]>
     inline: p.Property<boolean>
   }
@@ -26,7 +24,7 @@ export namespace ToggleInputGroup {
 
 export interface ToggleInputGroup extends ToggleInputGroup.Attrs {}
 
-export abstract class ToggleInputGroup extends Control {
+export abstract class ToggleInputGroup extends Widget {
   declare properties: ToggleInputGroup.Props & {active: p.Property<unknown>}
   declare __view_type__: ToggleInputGroupView
 
