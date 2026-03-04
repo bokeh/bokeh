0ee82b1da09c1402f39c4debaef3d59e7dcf7690
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2026-03-04 12:51:13 -0500

    Apply suggestions from code review
    Co-authored-by: Mateusz Paprocki <mattpap@gmail.com>

--- bokehjs/src/lib/models/widgets/theme_switch.ts
+++ bokehjs/src/lib/models/widgets/theme_switch.ts
@@ -17,7 +17,7 @@ export class ThemeSwitchView extends SwitchView {
   }
 
   protected _update_theme(): void {
-    const theme = this.model.active? "light": "dark"
+    const theme = this.model.active ? "light" : "dark"
     document.documentElement.style.setProperty("--color-scheme", theme)
   }
 }
