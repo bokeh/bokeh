f11e84575f1368058acba45b031d2cc30ec81970
Mateusz Paprocki <mattpap@gmail.com>
2026-03-10 18:44:10 +0100

    Remove unnecessary esbuild plugin

--- bokehjs/make/tasks/esm.ts
+++ bokehjs/make/tasks/esm.ts
@@ -1,11 +1,9 @@
 import {join} from "node:path"
 
 import * as esbuild from "esbuild"
-import type {Plugin} from "esbuild"
 
 import {task, passthrough} from "../task.js"
 import * as paths from "../paths.js"
-import {exists, is_file, is_dir} from "./_util.js"
 
 task("scripts:bundle:esm", [passthrough("scripts:compile")], async () => {
   const packages = [paths.bundles.all]
@@ -29,40 +27,7 @@ task("scripts:bundle:esm", [passthrough("scripts:compile")], async () => {
     treeShaking: true,
     sourcemap: true,
     metafile: true,
-    plugins: [
-      TSConfigPathsPlugin(),
-    ],
   })
 })
-
-export function TSConfigPathsPlugin(): Plugin {
-  return {
-    name: "tsconfig-paths",
-    setup({onResolve}) {
-      onResolve({filter: /.*/}, async (args) => {
-        const {path} = args
-
-        if (path.startsWith("./") || path.startsWith("../")) {
-          return null
-        }
-
-        let resolved = join(paths.build_dir.lib, path)
-        if (!exists(resolved)) {
-          resolved = `${resolved}.js`
-          if (!exists(resolved) || !is_file(resolved)) {
-            return null
-          }
-        } else if (is_dir(resolved)) {
-          resolved = join(resolved, "index.js")
-          if (!exists(resolved) || !is_file(resolved)) {
-            return null
-          }
-        }
-
-        return {path: resolved}
-      })
-    },
-  }
-}
 
 task("lib:build", ["scripts:bundle", "scripts:bundle:esm"])
