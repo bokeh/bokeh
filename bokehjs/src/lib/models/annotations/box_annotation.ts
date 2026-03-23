66b027f8ca4c74b9af7a8dfe686c8e2234104bb2
muendlein <max.muendlein@gmail.com>
2026-03-23 21:36:47 +0100

    move editable check into _can_hit

--- bokehjs/src/lib/models/annotations/box_annotation.ts
+++ bokehjs/src/lib/models/annotations/box_annotation.ts
@@ -548,6 +548,9 @@ export class BoxAnnotationView extends AnnotationView implements Pannable, Pinch
   }
 
   private _can_hit(target: Box.HitTarget): boolean {
+    if (!this.model.editable && !this.model.is_handle) {
+      return false
+    }
     const {left, right, top, bottom} = this.resizable
     switch (target) {
       case "top_left":     return top && left
@@ -823,10 +826,6 @@ export class BoxAnnotationView extends AnnotationView implements Pannable, Pinch
   override cursor(sx: number, sy: number): string | null {
     const target = this._pan_state?.target ?? this._hit_test(sx, sy)
     if (target == null || !this._can_hit(target)) {
-      return null
-    }
-
-    if (!this.model.editable && !this.model.is_handle) {
       return null
     }
 
