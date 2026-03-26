d829c91a1629cfded7bbde33dff46a2c37b808d4
Simon Høxbro Hansen <simon.hansen@me.com>
2026-03-26 10:31:54 +0100

    Enable running unittest in parallel (#14960)

--- tests/unit/bokeh/document/test_locking.py
+++ tests/unit/bokeh/document/test_locking.py
@@ -81,7 +81,13 @@ def test_without_document_lock_accepts_async_function() -> None:
         i += 1
     callback_obj = d.add_next_tick_callback(cb)
 
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        # Errors on Python 3.14
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
     loop.run_until_complete(callback_obj.callback())
 
     assert callback_obj.callback.nolock is True
