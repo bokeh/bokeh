9bf4150c89d793cee0147d64678148dd5fc79b21
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-04-06 11:24:50 -0500

    Update light_dark example to include usage of document color_scheme config and auto/system value

--- examples/interaction/widgets/light_dark.py
+++ examples/interaction/widgets/light_dark.py
@@ -1,15 +1,25 @@
+from bokeh import events
+from bokeh.core.enums import ColorScheme
 from bokeh.io import show
 from bokeh.layouts import row
-from bokeh.models import CustomJS, Div, LightDark
+from bokeh.models import CustomJS, Div, LightDark, Dropdown
+
+menu = [("Dark", ColorScheme.dark), ("Auto", ColorScheme.auto), ("Light", ColorScheme.light)]
+color_scheme_dropdown = Dropdown(label="Update color scheme", menu=menu)
+color_scheme_dropdown.js_on_event(events.MenuItemClick, CustomJS(code="""
+    cb_obj.origin.document.config.color_scheme = cb_obj.item
+"""))
+
+div = Div(text="Current scheme: Light")
 
 light_dark = LightDark(active=True)
-div = Div(text="Current state: Light")
 light_dark.js_on_change("active", CustomJS(args=dict(div=div), code="""
-    const state = this.active ? "Light" : "Dark"
-    div.text = `Current state: ${state}`
+    const state = this.active ? "Light" : this.active != null ? "Dark" : "Auto"
+    div.text = `Current scheme: ${state}`
 """))
+
 show(
-    row([light_dark, div], stylesheets=[
+    row([color_scheme_dropdown, light_dark, div], stylesheets=[
         ":host { background-color: light-dark(white, black); color: light-dark(black, white);}",
     ]),
 )
