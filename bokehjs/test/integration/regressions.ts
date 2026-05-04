8a089e30bec97ff30bba98c7794a25d4a74e2b71
muendlein <71090451+muendlein@users.noreply.github.com>
2026-05-04 22:06:28 +0200

    Show updates to `num_minor_ticks` (#15062)
    * reflect ticker updates
    
    * added regression test
    
    * fix linting
    
    * added baseline image
    
    * Update bokehjs/test/integration/regressions.ts
    
    Co-authored-by: Mateusz Paprocki <mattpap@gmail.com>
    
    * fix import
    
    ---------
    
    Co-authored-by: Mateusz Paprocki <mattpap@gmail.com>

--- bokehjs/test/integration/regressions.ts
+++ bokehjs/test/integration/regressions.ts
@@ -19,13 +19,13 @@ import {
   TeX,
   Toolbar, ToolProxy,
   PanTool, PolySelectTool, LassoSelectTool, HoverTool, ZoomInTool, ZoomOutTool, RangeTool,
-  WheelPanTool, BoxSelectTool, WheelZoomTool, UndoTool, RedoTool, ResetTool,
+  WheelPanTool, BoxSelectTool, BoxZoomTool, WheelZoomTool, UndoTool, RedoTool, ResetTool,
   TileRenderer, WMTSTileSource,
   ImageURLTexture,
   Row, Column, Spacer,
   Pane,
   Tabs, TabPanel,
-  FixedTicker, MercatorTicker, MercatorTickFormatter,
+  FixedTicker, MercatorTicker, MercatorTickFormatter, ContinuousTicker,
   Jitter,
   ParkMillerLCG,
   GridPlot,
@@ -4875,6 +4875,48 @@ describe("Bug", () => {
       const actions0 = new PlotActions(pv0)
       await actions0.hover(xy(-2, 1.5))
 
+      await view.ready
+    })
+  })
+
+  describe("in issue #14218", () => {
+    it("allows RangeTool with start gesture pan and PanTool to be active at the same time", async () => {
+      const range_tool = new RangeTool({
+        x_range: new Range1d({start: 2, end: 4}),
+        start_gesture: "pan",
+      })
+      const p = fig([400, 200], {tools: ["pan", range_tool], toolbar_location: "above"})
+      const random = new Random(1)
+      const x = random.floats(100, 0, 9)
+      const y = random.floats(100, 0, 1)
+      p.scatter(x, y, {size: 10})
+      await display(p)
+    })
+
+    it("should respect active setting", async () => {
+      const range_tool = new RangeTool({
+        x_range: new Range1d({start: 1, end: 2}),
+        start_gesture: "pan",
+      })
+      const box_zoom_tool = new BoxZoomTool()
+      const p = fig([400, 200], {tools: ["pan", range_tool, box_zoom_tool], toolbar_location: "above"})
+      const random = new Random(1)
+      const x = random.floats(100, 0, 9)
+      const y = random.floats(100, 0, 1)
+      p.scatter(x, y, {size: 10})
+      p.toolbar.active_drag = range_tool
+      await display(p)
+    })
+  })
+
+  describe("in issue #15015", () => {
+    it("doesn't show updates to num_minor_ticks", async () => {
+      const p = fig([200, 200], {x_range: [0, 5], y_range: [0, 5]})
+      const {view} = await display(p)
+      for (const axis of p.yaxis) {
+        assert(axis.ticker instanceof ContinuousTicker)
+        axis.ticker.num_minor_ticks = 0
+      }
       await view.ready
     })
   })
