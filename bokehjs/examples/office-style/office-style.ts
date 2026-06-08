ac1ec7a2b1420a02f631075fd18da10840c5b503
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-06-08 12:55:03 -0500

    Office like style BokehJS example

---
+++ bokehjs/examples/office-style/office-style.ts
@@ -0,0 +1,158 @@
+import Bokeh from "/static/js/bokeh.esm.js"
+import "/static/js/bokeh-widgets.esm.js"
+import "/static/js/bokeh-api.esm.js"
+
+export namespace Office {
+  import plt = Bokeh.Plotting
+  console.log(`Bokeh ${Bokeh.version}`)
+  Bokeh.set_log_level("info")
+
+  const {Column, Row} = Bokeh
+  const {
+    AutocompleteInput,
+    Button,
+    CheckboxButtonGroup,
+    CheckboxGroup,
+    Dropdown,
+    LightDark,
+    MultiChoice,
+    PasswordInput,
+    RadioButtonGroup,
+    RadioGroup,
+    Select,
+    Slider,
+    TextInput,
+    Toggle,
+  } = Bokeh.Widgets
+
+  const url = "/assets/fonts/inter/Inter-VariableFont_opsz,wght.ttf"
+  // @ts-ignore - instance of abstract class
+  const font_style = new Bokeh.Models.GlobalInlineStyleSheet({
+    css: `
+        @font-face {
+          font-family: 'Inter';
+          src: url(${url});
+        }
+    `,
+  })
+
+  const switch_style = `
+    .bk-bar, .bk-knob {
+      text-transform: inherit;
+      border: var(--border);
+      border-radius: var(--border-radius);
+      cursor: pointer;
+    }
+  `
+
+  const light_dark = new LightDark({active: true, stylesheets: [switch_style]})
+
+  const button_style = `
+    .bk-btn {
+      background-color: transparent;
+      padding: 6px 12px;
+      font-size: 14px;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      transition: background-color 0.2s, border-color 0.2s;
+    }
+  `
+
+  const slider_style = `
+    .noUi-target, .noUi-handle {
+      text-transform: inherit;
+      border: var(--border);
+      border-radius: var(--border-radius);
+      cursor: pointer;
+    }
+  `
+
+  const input_style = `
+    .bk-input {
+      text-transform: inherit;
+    }
+  `
+
+  const choices_style = `
+  `
+
+  const legend_style = `
+    :host {
+      border: var(--border);
+      border-radius: var(--border-radius);
+      cursor: pointer;
+    }
+  `
+
+  const w0 = new Button({label: "Button", stylesheets: [button_style]})
+  const w1 = new Toggle({label: "Toggle", stylesheets: [button_style]})
+  const w2 = new Dropdown({label: "Dropdown", stylesheets: [button_style]})
+  const w3 = new CheckboxGroup({labels: ["Option 1", "Option 2", "Option 3"], active: [0, 1]})
+  const w4 = new RadioGroup({labels: ["Option 1", "Option 2", "Option 3"], active: 0})
+  const w5 = new CheckboxButtonGroup({labels: ["Option 1", "Option 2", "Option 3"], active: [0, 1], stylesheets: [button_style]})
+  const w6 = new RadioButtonGroup({labels: ["Option 1", "Option 2", "Option 3"], active: 0, stylesheets: [button_style]})
+  const w7 = new TextInput({title: "Initial temperature:", placeholder: "Enter temperature ...", prefix: "T", suffix: "\u2103", stylesheets: [input_style]})
+  const w8 = new PasswordInput({value: "foo", stylesheets: [input_style]})
+  const w9 = new AutocompleteInput({
+    placeholder: "Enter value ...",
+    completions: ["aaa", "aab", "aac", "baa", "caa"],
+    stylesheets: [input_style],
+  })
+  const w10 = new MultiChoice({options: ["Option 1", "Option 2", "Option 3"], stylesheets: [choices_style]})
+  const w11 = new Select({options: ["Option 1", "Option 2", "Option 3"], value: "Option 1", stylesheets: [input_style]})
+  const w12 = new Slider({value: 10, start: 0, end: 100, step: 0.5, stylesheets: [slider_style]})
+
+  const p = plt.figure()
+
+  p.line([2, 3, 4], [2, 4, 3], {color: "orange", legend_label: "orange", line_width: 4})
+  p.line([2, 3, 4], [4, 5, 4], {color: "red", legend_label: "red", line_width: 4})
+  p.line([4, 3, 2], [4, 3, 2], {color: "blue", legend_label: "blue", line_width: 4})
+
+  p.xaxis.axis_label = "X-Axis"
+  p.xaxis.axis_label_text_font = "Segoe UI, Inter"
+  p.xaxis.axis_label_text_font_size = "22px"
+  p.xaxis.major_label_text_font = "Segoe UI, Inter"
+  p.xaxis.major_label_text_font_size = "12px"
+  p.xaxis.major_tick_line_color = "transparent"
+  p.xaxis.minor_tick_line_color = "transparent"
+
+  p.yaxis.axis_label = "Y-Axis"
+  p.yaxis.axis_label_text_font = "Segoe UI, Inter"
+  p.yaxis.axis_label_text_font_size = "26px"
+  p.yaxis.major_label_text_font = "Segoe UI, Inter"
+  p.yaxis.major_label_text_font_size = "12px"
+  p.yaxis.major_tick_line_color = "transparent"
+  p.yaxis.minor_tick_line_color = "transparent"
+
+  p.legend.label_text_font = "Segoe UI, Inter"
+  p.legend.stylesheets = [legend_style]
+
+  const w_columns = [
+    new Column({children: [light_dark, w0, w1, w2, w3, w4, w5, w6]}),
+    new Column({children: [w5, w6, w7, w8, w9, w10, w11, w12, p]}),
+  ]
+  const layout = new Row({children: w_columns, sizing_mode: "stretch_both", stylesheets: [
+    // @ts-ignore - GlobalInlineStyleSheet not recognized as StyleSheet type
+    font_style,
+    `
+    :host {
+      --bokeh-base-font: Segoe UI, Inter;
+      --border-color: var(--color);
+      --border-width: 1px;
+      --default-border-color: var(--color);
+      --bokeh-font-size: 1rem;
+      --border-radius: 2px;
+      cursor: pointer;
+      transition: transform 0.1s ease;
+      background-color: var(--background-color);
+    }`,
+  ]})
+
+  const doc = new Bokeh.Document()
+  doc.add_root(layout)
+
+  const div = document.getElementById("dashboard")!
+  void Bokeh.embed.add_document_standalone(doc, div)
+}
