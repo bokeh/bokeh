8c432e9168e82c5eb9a3be54dc42b9a5d2e9c7c1
wswope-etiometry <137447254+wswope-etiometry@users.noreply.github.com>
2026-07-17 17:34:31 -0400

    Auth login/logout URLs fixed to respect server prefix (#11991) (#15046)
    * Auth login/logout URLs fixed to respect server prefix (#11991)
    
    * Login URL prefix joining hardened with urljoin.
    
    * Logout URL prefix joining hardened with urljoin.
    
    * Login and logout URL prefix joining covered by tests.
    
    * Test-related fixes applied.
    
    * Minor explanatory comments added.
    
    * Logout URL tests moved from tornado-server module to auth-request-handler module.
    
    * Revert "Logout URL tests moved from tornado-server module to auth-request-handler module."
    
    This reverts commit 2a5757b5f652b3c2940cbc18bc4e6379f5438c8d.
    
    ---------
    
    Co-authored-by: Bryan Van de Ven <bryan@bokeh.org>

--- src/bokeh/server/views/auth_request_handler.py
+++ src/bokeh/server/views/auth_request_handler.py
@@ -22,6 +22,7 @@ log = logging.getLogger(__name__)
 
 # Standard library imports
 from typing import Any, cast
+from urllib.parse import urljoin
 
 # External imports
 from tornado.web import RequestHandler
@@ -67,7 +68,9 @@ class AuthRequestHandler(RequestHandler):
         if auth_provider.get_login_url is not None:
             return auth_provider.get_login_url(self)
         if auth_provider.login_url is not None:
-            return auth_provider.login_url
+            # second arg must be lstrip'd to avoid dropping the prefix
+            # (urljoin treats a leading-slash second arg as an absolute path and discards the base)
+            return urljoin(self.bokeh_app.prefix + "/", auth_provider.login_url.lstrip("/"))
         raise RuntimeError('login_url or get_login_url() must be supplied when authentication hooks are enabled')
 
     def get_current_user(self) -> Any:
