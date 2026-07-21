b796736e645ad1bef6a44194f78d8b9a280aa69c
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2026-07-21 19:40:58 +0200

    add tests for ts

--- bokehjs/test/integration/regressions.ts
+++ bokehjs/test/integration/regressions.ts
@@ -81,6 +81,8 @@ import {np} from "@bokehjs/api/linalg"
 import {open_picker} from "./widgets"
 import {Model} from "@bokehjs/model"
 
+import {with_log_level} from "@bokehjs/core/logging"
+
 function svg_data_url(svg: string): string {
   return `data:image/svg+xml;utf-8,${svg}`
 }
@@ -5202,6 +5204,25 @@ describe("Bug", () => {
       })
 
       await display(table, [1000, 250])
+    })
+  })
+
+  describe("in issue #15119", () => {
+    it("doesn't render if desired_tick_numbers are too large", async () => {
+
+      const p = fig([200, 200])
+      p.scatter([1, 3, 5, 7], [2, 5, 3, 8], {size: 12})
+      p.xaxis.ticker.desired_num_ticks = 100000000000000000000000000000
+
+      const output = await async_trap(async () => {
+        await display(p)
+      })
+      expect(output.error.includes(
+        "Caught a structural limit error, not an engine-wide OOM: invalid array length")
+      ).to.be.true
+      expect(output.warn.includes(
+        "Caught an error calculating the ticks for 1e+29 desired_num_ticks and 5 num_minor_ticks. The default values are used instead.")
+      ).to.be.true
     })
   })
 })
