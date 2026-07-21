1288d06a2a0718d7f970276e77cced7669161544
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2026-07-21 19:40:05 +0200

    add try-catch and log message to array.ts

--- bokehjs/src/lib/core/util/array.ts
+++ bokehjs/src/lib/core/util/array.ts
@@ -3,7 +3,7 @@
 //     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
 
-// import {logger} from "../logging"
+import {logger} from "../logging"
 import type {Arrayable} from "../types"
 import {randomIn} from "./math"
 import {assert} from "./assert"
@@ -105,25 +105,21 @@ export function range(start: number, stop?: number, step: number = 1): number[] 
 
   const {max, ceil, abs} = Math
 
-  let length = max(ceil(abs(stop - start) / step), 0)
-  // Max length of an Array object is 2**32 -1 = 4 294 967 295, see
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length#value
-  // If the maximum length is exceeded, the length is set to the maximum and the step is adapted
-  // to avoid an invalid length error.
-  if (2**32 - 1 < length) {
-    length = 2**32 - 1
-    step = ceil(abs(start - stop) / length)
-    // logger.warn(`Maximum size of a range with the step size ${step} is exceeded. `,
-    //   "The step size is adapted to stay inside the limit.",
-    // )
-  }
   const delta = start <= stop ? step : -step
-  const range = new Array(length)
-
-  for (let i = 0; i < length; i++, start += delta) {
-    range[i] = start
+  let length = max(ceil(abs(stop - start) / step), 0)
+  try {
+    // Creating an array too large for V8's allowed array lengths raises an error
+    const range = new Array(length)
+    for (let i = 0; i < length; i++, start += delta) {
+      range[i] = start
+    }
+    return range
+  } catch (error) {
+    if (error instanceof RangeError) {
+      logger.error("Caught a structural limit error, not an engine-wide OOM:", error.message);
+    }
+    throw(error)
   }
-  return range
 }
 
 export function linspace(start: number, stop: number, num: number = 100): number[] {
