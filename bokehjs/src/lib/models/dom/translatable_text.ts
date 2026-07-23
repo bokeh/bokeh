e7eb414a5144f0ea776b5361cae0b0c2344a1a75
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-07-23 12:19:32 -0500

    Remove unnecessary declare keyword and attributes on translatable text classes

--- bokehjs/src/lib/models/dom/translatable_text.ts
+++ bokehjs/src/lib/models/dom/translatable_text.ts
@@ -3,8 +3,7 @@ import type * as p from "core/properties"
 
 export class TranslatableTextView extends TextView {
   declare model: TranslatableText
-  declare translated_text: string
-  declare el: globalThis.Text
+  translated_text: string
 
   override connect_signals(): void {
     super.connect_signals()
