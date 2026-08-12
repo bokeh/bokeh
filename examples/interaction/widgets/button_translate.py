12ed1bd21936d1e7762e8b5db614d75976886f38
dalthviz <16781833+dalthviz@users.noreply.github.com>
2026-08-12 11:36:29 -0500

    Initial formatting support during interpolation using Intl web API. Update example to show usage

--- examples/interaction/widgets/button_translate.py
+++ examples/interaction/widgets/button_translate.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from bokeh import events
 from bokeh.io import curdoc, show
 from bokeh.layouts import column
@@ -5,7 +7,34 @@ from bokeh.models import Button, CustomJS
 from bokeh.models.dom import TranslatableText
 
 button = Button(label="Change to a random language!")
-button1 = Button(label=TranslatableText(content="button1.label", options={"interpolation": {"locale": "en"}}))
+button1 = Button(
+    label=TranslatableText(
+        content="button1.label",
+        options={
+            "interpolation": {
+                "locale": {
+                    "value": "en",
+                    "formatting": {
+                        "format": "display",
+                        "options": {"type": "language"},
+                    },
+                },
+                "current_date": {
+                    "value": date.today(),
+                    "formatting": {
+                        "format": "date",
+                        "options": {
+                          "weekday": "long",
+                          "year": "numeric",
+                          "month": "long",
+                          "day": "numeric",
+                        },
+                    },
+                },
+            },
+        },
+    ),
+)
 button2 = Button(label="String used as key itself to get its translation")
 
 button.js_on_event("button_click", CustomJS(args=dict(button1=button1), code="""
@@ -13,7 +42,29 @@ button.js_on_event("button_click", CustomJS(args=dict(button1=button1), code="""
      const locale_selection = available_locales[Math.floor(Math.random() * available_locales.length)]
 
      cb_obj.origin.document.config.i18n.set_locale(locale_selection)
-     button1.label.options = {interpolation: {locale: locale_selection}}
+     button1.label.options = {
+         interpolation: {
+             locale: {
+                 value: locale_selection,
+                 formatting: {
+                     format: "display",
+                     options: {type: "language"}
+                 }
+             },
+             current_date: {
+                 value: new Date(),
+                 formatting: {
+                     format: "date",
+                     options: {
+                       weekday: "long",
+                       year: "numeric",
+                       month: "long",
+                       day: "numeric"
+                     }
+                 }
+             }
+         }
+     }
 """))
 
 curdoc().on_event(events.DocumentReady, CustomJS(code="""
@@ -21,30 +72,30 @@ curdoc().on_event(events.DocumentReady, CustomJS(code="""
       ["en", "es-CO", "pl-PL", "fr-FR", "de-DE", "hi-IN", "pt-BR", "ar"],
       `{
         "en": {
-            "button1": { "label": "Test {{locale}}" },
+            "button1": { "label": "Test {{locale}} - {{current_date}}" },
             "String used as key itself to get its translation": "String used as key itself to get its translation (manually set)"
         },
         "es-CO": {
-            "button1": {"label": "Prueba {{locale}}" },
+            "button1": {"label": "Prueba {{locale}} - {{current_date}}" },
             "String used as key itself to get its translation": "Cadena usada en si misma como llave para obtener su traducción (traducida manualmente)"
         },
         "pl-PL": {
-            "button1": { "label": "Test {{locale}}" }
+            "button1": { "label": "Test {{locale}} - {{current_date}}" }
         },
         "fr-FR": {
-            "button1": { "label": "Test {{locale}}" }
+            "button1": { "label": "Test {{locale}} - {{current_date}}" }
         },
         "de-DE": {
-            "button1": { "label": "Prüfen {{locale}}" }
+            "button1": { "label": "Prüfen {{locale}} - {{current_date}}" }
         },
         "hi-IN": {
-            "button1": { "label": "परीक्षा {{locale}}" }
+            "button1": { "label": "परीक्षा {{locale}} - {{current_date}}" }
         },
         "pt-BR": {
-            "button1": { "label": "Teste {{locale}}" }
+            "button1": { "label": "Teste {{locale}} - {{current_date}}" }
         },
         "ar": {
-            "button1": { "label": "امتحان ar" }
+            "button1": { "label": "امتحان {{locale}} - {{current_date}}" }
         }
        }`,
       [
