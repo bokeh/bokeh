5b2f70de89fe5550d0909fe386baea708dce50dc
Bryan Van de Ven <bryan@bokeh.org>
2026-08-14 17:42:23 -0700

    Test singleton WebGL GCD inputs

--- bokehjs/test/unit/models/glyphs/webgl/math.ts
+++ bokehjs/test/unit/models/glyphs/webgl/math.ts
@@ -9,6 +9,11 @@ describe("WebGL math utilities", () => {
     expect(gcd([0, 0])).to.be.equal(0)
   })
 
+  it("should return the absolute value for a single input", () => {
+    expect(gcd([6])).to.be.equal(6)
+    expect(gcd([-6])).to.be.equal(6)
+  })
+
   it("should ignore zero values and operand signs", () => {
     expect(gcd([0, 6, 9])).to.be.equal(3)
     expect(gcd([-8, 0, 12])).to.be.equal(4)
